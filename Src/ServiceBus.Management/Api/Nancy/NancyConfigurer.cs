b5329a19901db62c14ee53744e31801a71ccbd1a
andreasohlund <andreasohlund2@gmail.com>
2013-01-30 13:35:57 +0100

    Nancy is now started and stopped correctly

---
+++ Src/ServiceBus.Management/Api/Nancy/NancyConfigurer.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceBus.Management.Api
+{
+    using System;
+    using NServiceBus;
+    using Nancy.Hosting.Self;
+
+    public class NancyConfigurer : INeedInitialization
+    {
+        public void Init()
+        {
+            var nancyHost = new NancyHost(new Uri("http://localhost:8888/"));
+
+            Configure.Instance.Configurer.RegisterSingleton<NancyHost>(nancyHost);
+
+        }
+    }
+}
---
+++ Src/ServiceBus.Management/Api/Nancy/NancyRunner.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceBus.Management.Api
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using Nancy.Hosting.Self;
+
+    public class NancyRunner : IWantToRunWhenBusStartsAndStops
+    {
+        public NancyHost NancyHost { get; set; }
+        public void Start()
+        {
+            NancyHost.Start();
+
+            Logger.InfoFormat("Api is now accepting requests");
+        }
+
+        public void Stop()
+        {
+            NancyHost.Stop();
+            Logger.InfoFormat("Api is now stopped");
+        }
+
+        static ILog Logger = LogManager.GetLogger("api");
+    }
+}
