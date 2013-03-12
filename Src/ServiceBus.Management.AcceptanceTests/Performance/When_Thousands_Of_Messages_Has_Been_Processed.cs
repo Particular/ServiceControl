5e7f405dc2108a9a90e6771be00a82e2ecb10398
indualagarsamy <indu@bitsnbytes.us>
2013-03-12 11:47:27 -0700

    added a new test that will send messages to audit log and has options to send messages to error q for testing profiler

---
+++ Src/ServiceBus.Management.AcceptanceTests/Performance/When_Thousands_Of_Messages_Has_Been_Processed.cs
@@ -0,0 +1,100 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.Performance
+{
+    using System;
+    using System.Diagnostics;
+    using System.Messaging;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus.Config;
+    using ServiceBus.Management.AcceptanceTests.Contexts;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_Thousands_Of_Messages_Has_Been_Processed : HttpUtil
+    {
+        const string auditQueue = "audit";
+        const string errorQueue = "error";
+        const string breakWhenContentContains = "66";
+        const int maxLoadToGenerate = 1000;
+        [Test]
+        public void Generate_Load_Test()
+        {
+            var context = new MyContext();
+            Scenario.Define(() => context)
+                    .WithEndpoint<ProcessOrderEndpoint>(condition => condition.Given(bus =>
+                        {
+                            int index = 0;
+                            Parallel.For(index, maxLoadToGenerate,
+                                         (s, u) =>
+                                             {
+                                                 bus.SendLocal(new ProcessOrder
+                                                     {
+                                                         OrderId = Guid.NewGuid(),
+                                                         OrderPlacedOn = DateTime.Now,
+                                                         Content = index.ToString()
+                                                     });
+                                                 index++;
+                                             });
+                        }))
+                    .Done(c => true)
+                    .Run();
+
+        }
+
+        #region ProcessOrder endpoint
+        public class ProcessOrderEndpoint : EndpointConfigurationBuilder
+        {
+            public ProcessOrderEndpoint()
+            {
+                EndpointSetup<DefaultServer>()
+                    .WithConfig<TransportConfig>(c => c.MaximumConcurrencyLevel = 10)
+                    .AuditTo(Address.Parse(auditQueue));
+
+            }
+
+            public class ProcessOrderHandler : IHandleMessages<ProcessOrder>
+            {
+                public MyContext Context { get; set; }
+
+              
+                public IBus Bus { get; set; }
+                public void Handle(ProcessOrder message)
+                {
+                    if (!string.IsNullOrEmpty(breakWhenContentContains) && message.Content.Contains(breakWhenContentContains))
+                    {
+                        throw new ArgumentException("Message content contains the configured error string : {0}", breakWhenContentContains);
+                    }
+
+                    Bus.Publish<OrderReceived>(m =>
+                    {
+                        m.OrderId = message.OrderId;
+                        m.OrderReceivedAt = DateTime.Now;
+                    });
+                }
+            }
+        }
+        #endregion
+
+        #region Commands & Events used in the acceptance test
+        public class ProcessOrder : ICommand
+        {
+            public Guid OrderId { get; set; }
+            public DateTime OrderPlacedOn { get; set; }
+            public string Content { get; set; }
+        }
+
+        public class OrderReceived : IEvent
+        {
+            public Guid OrderId { get; set; }
+            public DateTime OrderReceivedAt { get; set; }
+        }
+        #endregion
+
+        public class MyContext : ScenarioContext
+        {
+            public bool IsSendComplete { get; set; }
+        }
+    }
+}
