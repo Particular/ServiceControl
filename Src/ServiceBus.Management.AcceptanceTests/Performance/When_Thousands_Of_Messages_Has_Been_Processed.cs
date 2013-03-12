1b7749a7f676065ed32ff89e04c58b75cc9c10ba
indualagarsamy <indu@bitsnbytes.us>
2013-03-12 14:00:55 -0700

    No regions!

--- Src/ServiceBus.Management.AcceptanceTests/Performance/When_Thousands_Of_Messages_Has_Been_Processed.cs
+++ Src/ServiceBus.Management.AcceptanceTests/Performance/When_Thousands_Of_Messages_Has_Been_Processed.cs
@@ -14,10 +14,10 @@
     [TestFixture]
     public class When_Thousands_Of_Messages_Has_Been_Processed : HttpUtil
     {
-        const string auditQueue = "audit";
-        const string errorQueue = "error";
-        const string breakWhenContentContains = "66";
-        const int maxLoadToGenerate = 1000;
+        const string AuditQueue = "audit";
+        const string ErrorQueue = "error";
+        const string BreakWhenContentContains = "66";
+        const int MaxLoadToGenerate = 1000;
         [Test]
         public void Generate_Load_Test()
         {
@@ -26,7 +26,7 @@
                     .WithEndpoint<ProcessOrderEndpoint>(condition => condition.Given(bus =>
                         {
                             int index = 0;
-                            Parallel.For(index, maxLoadToGenerate,
+                            Parallel.For(index, MaxLoadToGenerate,
                                          (s, u) =>
                                              {
                                                  bus.SendLocal(new ProcessOrder
@@ -43,16 +43,15 @@
 
         }
 
-        #region ProcessOrder endpoint
         public class ProcessOrderEndpoint : EndpointConfigurationBuilder
         {
             public ProcessOrderEndpoint()
             {
                 EndpointSetup<DefaultServer>()
                     .WithConfig<TransportConfig>(c => c.MaxRetries = 0)
-                    .WithConfig<MessageForwardingInCaseOfFaultConfig>(e => e.ErrorQueue = errorQueue)
+                    .WithConfig<MessageForwardingInCaseOfFaultConfig>(e => e.ErrorQueue = ErrorQueue)
                     .WithConfig<SecondLevelRetriesConfig>(slr => slr.Enabled = false)
-                    .AuditTo(Address.Parse(auditQueue));
+                    .AuditTo(Address.Parse(AuditQueue));
 
             }
 
@@ -67,14 +66,14 @@
                 public void Handle(ProcessOrder message)
                 {
                     var current = Interlocked.Increment(ref numberOfMessagesProcessed);
-                    if (current >= maxLoadToGenerate)
+                    if (current >= MaxLoadToGenerate)
                     {
                         Context.IsProcessingComplete = true;
                     }
 
-                    if (!string.IsNullOrEmpty(breakWhenContentContains) && message.Content.Contains(breakWhenContentContains))
+                    if (!string.IsNullOrEmpty(BreakWhenContentContains) && message.Content.Contains(BreakWhenContentContains))
                     {
-                        throw new ArgumentException("Message content contains the configured error string : {0}", breakWhenContentContains);
+                        throw new ArgumentException("Message content contains the configured error string : {0}", BreakWhenContentContains);
                     }
 
                     Bus.Publish<OrderReceived>(m =>
@@ -86,9 +85,12 @@
                 }
             }
         }
-        #endregion
 
-        #region Commands & Events used in the acceptance test
+        public class ProcessOrderAcceptedEndpoint: EndpointConfigurationBuilder
+        {
+            
+        }
+        
         public class ProcessOrder : ICommand
         {
             public Guid OrderId { get; set; }
@@ -101,8 +103,7 @@
             public Guid OrderId { get; set; }
             public DateTime OrderReceivedAt { get; set; }
         }
-        #endregion
-
+        
         public class MyContext : ScenarioContext
         {
             public bool IsProcessingComplete { get; set; }
