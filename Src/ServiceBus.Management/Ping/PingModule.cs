b5087d8897f4e6bb1f853b73e92a2a510577d79d
andreasohlund <andreasohlund2@gmail.com>
2013-06-03 21:10:54 +0200

    Adding a GET /version to allow clients to check the api version

--- Src/ServiceBus.Management/Ping/PingModule.cs
+++ Src/ServiceBus.Management/Ping/PingModule.cs
@@ -4,6 +4,7 @@ namespace ServiceBus.Management.Ping
 {
     public class PingModule : NancyModule
     {
+        //todo: remove this as soon as the profiler is updated to use /version instead
         public PingModule()
         {
             Get["/ping"] = _ =>
