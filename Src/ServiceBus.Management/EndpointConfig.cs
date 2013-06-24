185c8cac0c21b24e7c6ebb3dbf0bf3077c2b7254
John Simons <john.simons@nservicebus.com>
2013-06-24 10:58:26 +1000

    Added embedded license for endpoint, #SB-88 done

--- Src/ServiceBus.Management/EndpointConfig.cs
+++ Src/ServiceBus.Management/EndpointConfig.cs
@@ -1,23 +1,25 @@
 namespace ServiceBus.Management
 {
+    using System.IO;
+    using System.Reflection;
     using NServiceBus;
 
     /*
         This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
         can be found here: http://nservicebus.com/GenericHost.aspx
     */
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomLogging
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomLogging, IWantCustomInitialization
     {
         public void Init()
         {
             SetLoggingLibrary.Log4Net(() => log4net.Config.XmlConfigurator.Configure());
-        }
-    }
 
-    class TransactionSetup : INeedInitialization
-    {
-        public void Init()
-        {
+            using (var licenseStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ServiceBus.Management.License.xml"))
+            using (var sr = new StreamReader(licenseStream))
+            {
+                Configure.Instance.License(sr.ReadToEnd());
+            }
+
             Configure.Transactions.Advanced(t => t.DisableDistributedTransactions());
         }
     }
