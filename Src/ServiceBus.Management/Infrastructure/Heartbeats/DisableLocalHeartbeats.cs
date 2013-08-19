47b95959315f75cc864509dcb29dd062ad0563bb
andreasohlund <andreasohlund2@gmail.com>
2013-08-19 21:31:17 +0200

    First spike that just sends an empty heartbeat

---
+++ Src/ServiceBus.Management/Infrastructure/Heartbeats/DisableLocalHeartbeats.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceBus.Management.Infrastructure.Heartbeats
+{
+    using NServiceBus;
+    using ServiceControl.EndpointPlugins.Heartbeat;
+
+    public class DisableLocalHeartbeats:INeedInitialization
+    {
+        public void Init()
+        {
+            //Configure.Features.Disable<ServiceControl.EndpointPlugins.Heartbeat.Heartbeats>(); //avoid sending heartbeats to our self
+        }
+    }
+}
