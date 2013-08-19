ab92993f9cc9a3074bf78be34849558c92465524
andreasohlund <andreasohlund2@gmail.com>
2013-08-19 22:04:23 +0200

    Added a extention point on the endpoint side so that we/users can plugin new heartbeat providers. Added a provider for endpoint SLA just to demo the heartbeats.

---
+++ Src/ServiceControl.EndpointPlugins.Heartbeat/EndpointHeartbeat.cs
@@ -0,0 +1,24 @@
+﻿namespace ServiceControl.EndpointPlugins.Heartbeat
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EndpointHeartbeat
+    {
+        public Dictionary<string, string> Configuration
+        {
+            get
+            {
+                if (configuration == null)
+                    configuration = new Dictionary<string, string>();
+
+                return configuration;
+            }
+            set { configuration = value; }
+        }
+
+        public DateTime ExecutedAt { get; set; }
+
+        Dictionary<string, string> configuration;
+    }
+}
---
+++ Src/ServiceControl.EndpointPlugins.Heartbeat/HeartbeatInfoProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceControl.EndpointPlugins.Heartbeat
+{
+    //todo: better name?
+    public abstract class HeartbeatInfoProvider :IHeartbeatInfoProvider
+    {
+        public abstract void HeartbeatExecuted(EndpointHeartbeat heartbeat);
+    }
+
+    //we need this for now until we can patch builder.BuildAll to support abstract base classes
+    public interface IHeartbeatInfoProvider
+    {
+        void HeartbeatExecuted(EndpointHeartbeat heartbeat);
+    }
+}
