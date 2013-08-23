5d29198e68a5afeefbdf28c762fb14b3cdcfccde
John Simons <john.simons@nservicebus.com>
2013-08-23 17:00:27 +1000

    Cleaning up MessageStreamerConnection

--- Src/ServiceBus.Management/SignalR/MessageStreamerConnection.cs
+++ Src/ServiceBus.Management/SignalR/MessageStreamerConnection.cs
@@ -1,6 +1,5 @@
 ﻿namespace ServiceBus.Management.SignalR
 {
-    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.AspNet.SignalR;
     using NServiceBus;
@@ -9,28 +8,9 @@
     {
         public IBus Bus { get; set; }
 
-        public MessageStreamerConnection()
-        {
-            timer = new Timer(Callback, null, 5000, 5000);
-        }
-
-        void Callback(object state)
-        {
-            if (Connection == null)
-            {
-                return;
-            }
-
-            
-            Connection.Broadcast("Are you still there?");
-        }
-
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
             return Connection.Broadcast(string.Format("Hello John!, we have a Bus={0}", Bus != null));
         }
-
-        // ReSharper disable once NotAccessedField.Local
-        Timer timer;
     }
 }
