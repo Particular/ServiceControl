a1b6d0b0db83624ca93f466615118252b4ba4510
andreasohlund <andreasohlund2@gmail.com>
2013-09-03 16:29:43 +0200

    Making sure that we always include the local endpoint when doing a publish. We do this by using a custom subscription storage

---
+++ Src/ServiceControl/Infrastructure/Subscriptions/AlwaysIncludeLocalEndpointStorage.cs
@@ -0,0 +1,46 @@
+﻿namespace ServiceControl.Infrastructure.Subscriptions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NServiceBus;
+    using NServiceBus.Persistence.InMemory.SubscriptionStorage;
+    using NServiceBus.Unicast.Subscriptions;
+    using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    public class AlwaysIncludeLocalEndpointStorage:ISubscriptionStorage
+    {
+        public void Subscribe(Address client, IEnumerable<MessageType> messageTypes)
+        {
+            innerStorage.Subscribe(client,messageTypes);
+        }
+
+        public void Unsubscribe(Address client, IEnumerable<MessageType> messageTypes)
+        {
+            innerStorage.Unsubscribe(client, messageTypes);
+        }
+
+        public IEnumerable<Address> GetSubscriberAddressesForMessage(IEnumerable<MessageType> messageTypes)
+        {
+            var subscribers = innerStorage.GetSubscriberAddressesForMessage(messageTypes).ToList();
+
+            subscribers.Add(Address.Local);
+
+            return subscribers;
+        }
+
+        public void Init()
+        {
+            
+        }
+
+        readonly ISubscriptionStorage innerStorage = new InMemorySubscriptionStorage();
+
+        class Initializer:INeedInitialization
+        {
+            public void Init()
+            {
+                Configure.Component<AlwaysIncludeLocalEndpointStorage>(DependencyLifecycle.SingleInstance);
+            }
+        }
+    }
+}
