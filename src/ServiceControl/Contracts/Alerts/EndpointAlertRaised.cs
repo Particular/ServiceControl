a6908e6e828f03639f53f331c7208dca48fc0b59
indualagarsamy <indu.alagarsamy@gmail.com>
2013-09-08 20:46:02 -0700

    Added alert related contracts.

---
+++ src/ServiceControl/Contracts/Alerts/EndpointAlertRaised.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Contracts.Alerts
+{
+    using System;
+    using NServiceBus;
+    public class EndpointAlertRaised : IEvent
+    {
+        public Guid AlertId { get; set; }
+        public DateTime AlertReceivedAt { get; set; }
+        //TODO: Do we need endpoint instance Id? Who sets the Endpoint instance Id? 
+        public string Endpoint { get; set; }
+        public string MachineName { get; set; }
+    }
+}
---
+++ src/ServiceControl/Contracts/Alerts/HeartbeatFailedAlert.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceControl.Contracts.Alerts
+{
+    using System;
+    public class HeartbeatFailedAlert : EndpointAlertRaised
+    {
+        public DateTime LastHeartbeatReceivedAt { get; set; }
+    }
+}
