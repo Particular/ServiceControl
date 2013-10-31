b8ff2bdc761fbf7844801a82726d79ee332065cb
John Simons <john.simons@nservicebus.com>
2013-11-01 09:21:16 +1100

    Updated copyrights

--- src/ServiceControl.EndpointPlugin/Properties/AssemblyInfo.cs
+++ src/ServiceControl.EndpointPlugin/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ServiceControl.EndpointPlugins.Heartbeat")]
-[assembly: AssemblyProduct("ServiceControl.EndpointPlugins.Heartbeat")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTitle("ServiceControl.EndpointPlugins")]
+[assembly: AssemblyProduct("Particular ServiceControl EndpointPlugins")]
+[assembly: AssemblyCopyright("Copyright 2010-2013 NServiceBus. All rights reserved")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
