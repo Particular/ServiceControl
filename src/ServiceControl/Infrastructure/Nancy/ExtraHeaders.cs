37092af0a86db792da6c869d414394a216f2cdc1
indualagarsamy <indu.alagarsamy@gmail.com>
2013-11-02 22:52:23 -0700

    Setting the cache for a short time. The default cache time is 5 minutes.

--- src/ServiceControl/Infrastructure/Nancy/ExtraHeaders.cs
+++ src/ServiceControl/Infrastructure/Nancy/ExtraHeaders.cs
@@ -12,13 +12,13 @@
                     .WithHeader("Cache-Control", "private, max-age=300, must-revalidate"); //By default cache for 5min
             }
 
-
             ctx.Response
                 .WithHeader("Access-Control-Expose-Headers",
                     "ETag, Last-Modified, Link, Total-Count, X-Particular-Version")
                 .WithHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
                 .WithHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-                .WithHeader("Access-Control-Allow-Origin", "*");
+                .WithHeader("Access-Control-Allow-Origin", "*")
+                .WithHeader("Cache-Control", "private, max-age=10, must-revalidate");
         }
     }
 }
