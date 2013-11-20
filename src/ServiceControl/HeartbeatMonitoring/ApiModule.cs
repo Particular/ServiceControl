e962d46d9daf0c612b8d61c0c280f4251c6f3bf0
John Simons <john.simons@nservicebus.com>
2013-11-20 13:31:28 +1100

    Added ability to delete heartbeat for a endpoint

--- src/ServiceControl/HeartbeatMonitoring/ApiModule.cs
+++ src/ServiceControl/HeartbeatMonitoring/ApiModule.cs
@@ -28,6 +28,24 @@ namespace ServiceControl.HeartbeatMonitoring
                 }
             };
 
+            Delete["/heartbeats/{id}"] = parameters =>
+            {
+                using (var session = Store.OpenSession())
+                {
+                    string id = parameters.id;
+
+                    var heartbeat = session.Load<Heartbeat>(string.Format("heartbeats/{0}", id));
+
+                    if (heartbeat != null)
+                    {
+                        session.Delete(heartbeat);
+                        session.SaveChanges();
+                    }
+                }
+
+                return HttpStatusCode.NoContent;
+            };
+
             Get["/heartbeats"] = _ =>
             {
                 using (var session = Store.OpenSession())
