c70ba78bf15dc96eac42cfa1830e91a8b5c12496
Andreas Öhlund <andreas.ohlund@particular.net>
2013-12-03 13:32:41 +0100

    Making sure that we tries to recreate urlacls as needed. Also fail the install/start if we can't register. Closes #134

--- src/ServiceControl/Infrastructure/Nancy/UrlAclInstaller.cs
+++ src/ServiceControl/Infrastructure/Nancy/UrlAclInstaller.cs
@@ -52,37 +52,83 @@ netsh http add urlacl url={1} user=""{0}""", uri, identity);
             }
         }
 
-        static internal void StartNetshProcess(string identity, Uri uri)
+        static void StartNetshProcess(string identity, Uri uri,bool deleteExisting = true)
         {
-            var startInfo = new ProcessStartInfo
+            var startInfo = GetProcessStartInfo(identity, uri);
+
+            string error;
+
+            if (ExecuteNetshCommand(startInfo, out error))
             {
-                CreateNoWindow = true,
-                Verb = "runas",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                Arguments = string.Format(@"http add urlacl url={0} user=""{1}""", uri, identity),
-                FileName = "netsh",
-                WorkingDirectory = Path.GetTempPath()
-            };
+                logger.InfoFormat("Granted user '{0}' HttpListener permissions for {1}.", identity, uri);
+
+                return;
+            }
+
+
+            if (deleteExisting && error.Contains("Error: 183"))
+            {
+                startInfo = GetProcessStartInfo(identity, uri,true);
+
+                logger.Info(string.Format(@"Failed to grant to grant user '{0}' HttpListener permissions.  The error message from running the above command is: {1} Will try to delete the existing urlacl", identity, error));
+
+
+                if (ExecuteNetshCommand(startInfo, out error))
+                {
+                    logger.InfoFormat("Deleted user '{0}' HttpListener permissions for {1}.", identity, uri);
+                    StartNetshProcess(identity, uri, false);
+                    return;
+                }
+            }
+
+            throw new Exception(string.Format(
+    @"Failed to grant to grant user '{0}' HttpListener permissions.
+Try running the following command from an admin console:
+netsh http add urlacl url={2} user=""{0}""
+
+The error message from running the above command is: 
+{1}", identity, error, uri));
+        }
+
+        static bool ExecuteNetshCommand(ProcessStartInfo startInfo,out string error)
+        {
+            error = null;
+
             using (var process = Process.Start(startInfo))
             {
                 process.WaitForExit(5000);
 
                 if (process.ExitCode == 0)
                 {
-                    logger.InfoFormat("Granted user '{0}' HttpListener permissions for {1}.", identity, uri);
-                    return;
+                    return true;
                 }
-                var error = process.StandardOutput.ReadToEnd().Trim();
-                var message = string.Format(
-@"Failed to grant to grant user '{0}' HttpListener permissions. Processing will continue. 
-Try running the following command from an admin console:
-netsh http add urlacl url={2} user=""{0}""
+                
+                error = process.StandardOutput.ReadToEnd().Trim();
 
-The error message from running the above command is: 
-{1}", identity, error, uri);
-                logger.Warn(message);
+                return false;                
             }
+        }
+
+        static ProcessStartInfo GetProcessStartInfo(string identity, Uri uri, bool delete = false)
+        {
+            var arguments = string.Format(@"http {1} urlacl url={0}", uri, delete ? "delete" : "add");
+
+            if (!delete)
+            {
+                arguments += string.Format(" user={0}", identity);
+            }
+
+            var startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                Verb = "runas",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                Arguments = arguments,
+                FileName = "netsh",
+                WorkingDirectory = Path.GetTempPath()
+            };
+            return startInfo;
         }
     }
 
