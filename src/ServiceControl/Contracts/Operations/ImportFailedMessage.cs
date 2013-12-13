719a6a7d87c05f5aee1f51810ad174ea19b0e61e
Andreas Öhlund <andreas.ohlund@particular.net>
2013-12-13 09:26:44 +0100

    Cleanup

--- src/ServiceControl/Contracts/Operations/ImportFailedMessage.cs
+++ src/ServiceControl/Contracts/Operations/ImportFailedMessage.cs
@@ -41,27 +41,6 @@
         }
     }
 
-    public class MessageMetadata
-    {
-        public string[] SearchTokens { get; set; }
-        public object Value { get; set; }
-        public string Name { get; set; }
-        public bool IncludeInSearch {
-            get
-            {
-                return SearchTokens != null;
-            } 
-        }
-
-
-        public MessageMetadata(string name, object value,string[] searchTokens = null)
-        {
-            SearchTokens = searchTokens;
-            Value = value;
-            Name = name;
-        }
-    }
-
 
     public class ImportFailedMessage : ImportMessage
     {
---
+++ src/ServiceControl/Contracts/Operations/MessageMetadata.cs
@@ -0,0 +1,16 @@
+﻿namespace ServiceControl.Contracts.Operations
+{
+    public class MessageMetadata
+    {
+        public string[] SearchTokens { get; set; }
+        public object Value { get; set; }
+        public string Name { get; set; }
+
+        public MessageMetadata(string name, object value,string[] searchTokens = null)
+        {
+            SearchTokens = searchTokens;
+            Value = value;
+            Name = name;
+        }
+    }
+}
