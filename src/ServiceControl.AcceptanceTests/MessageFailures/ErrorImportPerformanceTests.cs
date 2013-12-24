3247f977e192a0953aca4c8bf0f5b4c73b5f851f
Andreas Öhlund <andreas.ohlund@particular.net>
2013-12-24 13:12:37 +0100

    REproducing the session dispose issue

---
+++ src/ServiceControl.AcceptanceTests/MessageFailures/ErrorImportPerformanceTests.cs
@@ -0,0 +1,75 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.MessageFailures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contexts;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Features;
+    using NUnit.Framework;
+    using ServiceControl.CompositeViews.Messages;
+    using ServiceControl.Infrastructure;
+
+    public class ErrorImportPerformanceTests : AcceptanceTest
+    {
+
+        [Test]
+        public void Should_import_all_messages()
+        {
+            var context = new MyContext();
+
+
+            Scenario.Define(context)
+                .WithEndpoint<ManagementEndpoint>(c => c.AppConfig(PathToAppConfig))
+                .WithEndpoint<Receiver>(b => b.Given(bus =>
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        bus.SendLocal(new MyMessage());
+                    }
+                }))
+                .Done(c =>
+                {
+                    List<MessagesView> messages;
+
+                    if (!TryGetMany("/api/messages", out messages))
+                    {
+                        return false;
+                    }
+                    Console.Out.WriteLine("Messages found: " + messages.Count());
+                    return messages.Count() == 100;
+                })
+                .Run();
+
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServerWithoutAudit>(c=>Configure.Features.Disable<SecondLevelRetries>())
+                    .AuditTo(Address.Parse("audit"));
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+              
+                public void Handle(MyMessage message)
+                {
+                    throw new Exception("Simulated exception");
+                }
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+        }
+
+        public class MyContext : ScenarioContext
+        {
+          
+        }
+    }
+}
