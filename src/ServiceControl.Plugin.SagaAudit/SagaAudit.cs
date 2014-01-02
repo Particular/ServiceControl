aeee7f07840afa695aaba21a22e9355742ab15a1
Andreas Öhlund <andreas.ohlund@particular.net>
2014-01-02 22:47:32 +0100

    Making the saga audit a feature so that it can be turned on and off using out common feature api

---
+++ src/ServiceControl.Plugin.SagaAudit/SagaAudit.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Features
+{
+    using NServiceBus.Features;
+
+    public class SagaAudit:Feature
+    {
+        public override void Initialize()
+        {
+            
+        }
+
+        public override bool IsEnabledByDefault
+        {
+            get { return true; }
+        }
+
+        public override bool ShouldBeEnabled()
+        {
+            return Feature.IsEnabled<Sagas>();
+        }
+    }
+}
