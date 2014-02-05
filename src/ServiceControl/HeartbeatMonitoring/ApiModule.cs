bad80a957d7999e22f412acf51e656dc65d55a7f
John Simons <john.simons@nservicebus.com>
2014-02-05 13:47:30 +1100

    No need for unnecessary heartbeats api

--- src/ServiceControl/HeartbeatMonitoring/ApiModule.cs
+++ src/ServiceControl/HeartbeatMonitoring/ApiModule.cs
@@ -1,12 +1,7 @@
 namespace ServiceControl.HeartbeatMonitoring
 {
-    using System;
-    using Infrastructure.Extensions;
-    using Raven.Client;
-    using ServiceBus.Management.Infrastructure.Extensions;
     using ServiceBus.Management.Infrastructure.Nancy.Modules;
     using Nancy;
-    using System.Linq;
 
     public class ApiModule : BaseModule
     {
@@ -23,42 +18,6 @@ namespace ServiceControl.HeartbeatMonitoring
                     heartbeatsStats.Active,
                     Failing = heartbeatsStats.Dead
                 });
-            };
-
-            Delete["/heartbeats/{id}"] = parameters =>
-            {
-                using (var session = Store.OpenSession())
-                {
-                    Guid id = parameters.id;
-
-                    var heartbeat = session.Load<Heartbeat>(id);
-
-                    if (heartbeat != null)
-                    {
-                        session.Delete(heartbeat);
-                        session.SaveChanges();
-                    }
-                }
-
-                return HttpStatusCode.NoContent;
-            };
-
-            Get["/heartbeats"] = _ =>
-            {
-                using (var session = Store.OpenSession())
-                {
-                    RavenQueryStatistics stats;
-                    var results =
-                        session.Query<Heartbeat>()
-                            .Statistics(out stats)
-                            .Paging(Request)
-                            .ToArray();
-
-                    return Negotiate
-                        .WithModel(results)
-                        .WithPagingLinksAndTotalCount(stats, Request)
-                        .WithEtagAndLastModified(stats);
-                }
             };
         }
     }
