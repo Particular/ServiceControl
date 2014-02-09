c89a8032db3f9d95c776214d83a817310df1c011
John Simons <john.simons@nservicebus.com>
2014-02-09 16:47:54 +1100

    Modified SagaRelationshipsEnricher to include saga change

--- src/ServiceControl.AcceptanceTests/SagaAudit/When_a_message_that_is_handled_by_a_saga.cs
+++ src/ServiceControl.AcceptanceTests/SagaAudit/When_a_message_that_is_handled_by_a_saga.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.Remoting.Messaging;
     using Contexts;
     using NServiceBus;
     using NServiceBus.AcceptanceTesting;
--- src/ServiceControl/SagaAudit/SagaRelationshipsEnricher.cs
+++ src/ServiceControl/SagaAudit/SagaRelationshipsEnricher.cs
@@ -10,13 +10,37 @@
     {
         public override void Enrich(ImportMessage message)
         {
+            string sagasInvokedRaw;
 
-            string sagasInvokedHeader;
-
-            if (message.PhysicalMessage.Headers.TryGetValue("NServiceBus.InvokedSagas", out sagasInvokedHeader))
+            if (message.PhysicalMessage.Headers.TryGetValue("NServiceBus.InvokedSagas", out sagasInvokedRaw))
             {
-                var sagas = sagasInvokedHeader.Split(';')
-                    .Select(saga => new SagaInfo { SagaType = saga.Split(':')[0], SagaId = Guid.Parse(saga.Split(':')[1]) })
+                string sagasChangeRaw;
+                var sagasChanges = new Dictionary<string, string>();
+                if (message.PhysicalMessage.Headers.TryGetValue("ServiceControl.SagaStateChange", out sagasChangeRaw))
+                {
+                    var multiSagaChanges = sagasChangeRaw.Split(';');
+
+                    foreach (var part in multiSagaChanges.Select(s => s.Split(':')))
+                    {
+                        sagasChanges.Add(part[0], part[1]);
+                    }
+                }
+
+                var sagas = sagasInvokedRaw.Split(';')
+                    .Select(saga =>
+                    {
+                        var sagaInvoked = saga.Split(':');
+                        string changeText;
+
+                        sagasChanges.TryGetValue(sagaInvoked[1], out changeText);
+
+                        return new SagaInfo
+                        {
+                            SagaId = Guid.Parse(sagaInvoked[1]),
+                            SagaType = sagaInvoked[0],
+                            ChangeStatus = changeText,
+                        };
+                    })
                     .ToList();
 
                 message.Metadata.Add("InvokedSagas", sagas);
