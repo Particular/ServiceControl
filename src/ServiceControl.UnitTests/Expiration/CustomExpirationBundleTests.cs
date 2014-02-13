4fb3e39c05a4cab49b3c6b01ec6d010e66e0a104
synhershko <itamar@code972.com>
2014-02-13 12:30:17 +0200

    Remove redundant code

--- src/ServiceControl.UnitTests/Expiration/CustomExpirationBundleTests.cs
+++ src/ServiceControl.UnitTests/Expiration/CustomExpirationBundleTests.cs
@@ -2,20 +2,17 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Threading;
+    using Infrastructure.RavenDB;
     using MessageAuditing;
     using MessageFailures;
     using NUnit.Framework;
     using Raven.Client;
-    using Raven.Client.Embedded;
-    using Raven.Database.Server;
-    using Raven.Json.Linq;
     using ServiceBus.Management.Infrastructure.Settings;
     using ServiceControl.CompositeViews.Messages;
 
     [TestFixture]
-    public class CustomExpirationBundleTests
+    public class CustomExpirationBundleTests : TestWithRavenDB
     {
         [Test]
         public void Processed_messages_are_being_expired()
@@ -135,37 +132,5 @@
         }
 
         IDocumentStore documentStore;
-
-        public static void WaitForIndexing(IDocumentStore store, string db = null, TimeSpan? timeout = null)
-        {
-            var databaseCommands = store.DatabaseCommands;
-            if (db != null)
-                databaseCommands = databaseCommands.ForDatabase(db);
-            Assert.True(SpinWait.SpinUntil(() => databaseCommands.GetStatistics().StaleIndexes.Length == 0, timeout ?? TimeSpan.FromSeconds(10)));
-        }
-        public static void WaitForUserToContinueTheTest(EmbeddableDocumentStore documentStore, bool debug = true)
-        {
-            if (debug && Debugger.IsAttached == false)
-                return;
-
-            documentStore.SetStudioConfigToAllowSingleDb();
-
-            documentStore.DatabaseCommands.Put("Pls Delete Me", null,
-
-                                               RavenJObject.FromObject(new { StackTrace = new StackTrace(true) }),
-                                               new RavenJObject());
-
-            documentStore.Configuration.AnonymousUserAccessMode = AnonymousUserAccessMode.Admin;
-            using (var server = new HttpServer(documentStore.Configuration, documentStore.DocumentDatabase))
-            {
-                server.StartListening();
-                Process.Start(documentStore.Configuration.ServerUrl); // start the server
-
-                do
-                {
-                    Thread.Sleep(100);
-                } while (documentStore.DatabaseCommands.Get("Pls Delete Me") != null && (debug == false || Debugger.IsAttached));
-            }
-        }
     }
 }
