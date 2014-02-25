08b1c9b93ec1487cb0f917ba0e10e6ab1d1d0fb1
Simon Cropp <simon.cropp@gmail.com>
2014-02-25 18:53:13 +1100

    handle error code

--- src/Chocolatey/elevatedUninstall.ps1
+++ src/Chocolatey/elevatedUninstall.ps1
@@ -1,15 +1,18 @@
-﻿try {
+﻿$productName = "ServiceControl";
+$version = "MajorMinorPatch";
 
-    Start-Transcript -Path $args[0] -Force
-
-	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "ServiceControl*"  -and ($_.Version -eq "MajorMinorPatch") }
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "$productName*"  -and ($_.Version -eq "$version") }
+if($app -eq $null) 
+{
+	Write-Warning "Could not find an installed program for $productName matching version $version to uninstall. It may have been manualy removed. To check the cuurent status ensure that no instance of $productName exists in 'Programs and Features'."
+}
+else
+{
+	Write "Attempting to uninstall $productName $version from Programs."
 	$result = $app.Uninstall();
-
-}
-catch {
-    $_ | Out-Default
-	throw $_  
-}
-finally {
-    Stop-Transcript
+	$returnValue = [int]$result.ReturnValue
+	if ($returnValue -ne 0)
+	{
+		Write-Error "Could not uninstall $productName $version. The return code was $returnValue. For information on error codes see http://msdn.microsoft.com/en-us/library/aa372835 http://msdn.microsoft.com/en-us/library/aa376931."
+	}
 }
