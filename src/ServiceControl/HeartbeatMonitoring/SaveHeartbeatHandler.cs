be4d863030cfaa09b99d7ffc869286da1d6a33b9
indualagarsamy <indu.alagarsamy@gmail.com>
2014-03-20 15:54:30 -0700

    Since the message schema for EndpointHeartbeat was changed, if the endpoints are using oldPlugin versions, then the values will be null and will cause issues down the line.

--- src/ServiceControl/HeartbeatMonitoring/SaveHeartbeatHandler.cs
+++ src/ServiceControl/HeartbeatMonitoring/SaveHeartbeatHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace ServiceControl.HeartbeatMonitoring
 {
+    using System;
     using Contracts.HeartbeatMonitoring;
     using Contracts.Operations;
     using EndpointControl;
@@ -16,6 +17,13 @@
 
         public void Handle(EndpointHeartbeat message)
         {
+            if (string.IsNullOrEmpty(message.EndpointName))
+                throw new ArgumentException("Received an EndpointHeartbeat message without proper initialization of the EndpointName in the schema", "message.EndpointName");
+            if (string.IsNullOrEmpty(message.Host))
+                throw new ArgumentException("Received an EndpointHeartbeat message without proper initialization of the Host in the schema", "message.Host");
+            if (message.HostId == Guid.Empty)
+                throw new ArgumentException("Received an EndpointHeartbeat message without proper initialization of the HostId in the schema", "message.HostId");
+
             var id = DeterministicGuid.MakeId(message.EndpointName, message.HostId.ToString());
 
             Heartbeat heartbeat = null;
