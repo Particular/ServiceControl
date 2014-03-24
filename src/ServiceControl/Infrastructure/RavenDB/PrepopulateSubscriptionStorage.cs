4535a39adae253a4ae76d0df09c17dd5bbb6bcf6
Yves Goeleven <yves@goeleven.com>
2014-03-24 16:15:59 +0100

    fixed subscription for non message driven subscribers

--- src/ServiceControl/Infrastructure/RavenDB/PrepopulateSubscriptionStorage.cs
+++ src/ServiceControl/Infrastructure/RavenDB/PrepopulateSubscriptionStorage.cs
@@ -2,15 +2,28 @@ namespace ServiceControl.Infrastructure.RavenDB
 {
     using NServiceBus;
     using NServiceBus.Config;
+    using NServiceBus.Transports;
     using NServiceBus.Unicast.Subscriptions;
     using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
 
     class PrepopulateSubscriptionStorage:IWantToRunWhenConfigurationIsComplete
     {
-        public ISubscriptionStorage SubscriptionStorage { get; set; }    
+        public IManageSubscriptions SusbcriptionManager { get; set; }
+
         public void Run()
         {
-            Configure.Instance.ForAllTypes<IEvent>(eventType => SubscriptionStorage.Subscribe(Address.Local, new[] { new MessageType(eventType)}));
+            var messageDrivenSubscriptionManager = SusbcriptionManager as MessageDrivenSubscriptionManager;
+            if (messageDrivenSubscriptionManager != null)
+            {
+                Configure.Instance.ForAllTypes<IEvent>(eventType => messageDrivenSubscriptionManager.SubscriptionStorage.Subscribe(Address.Local, new[]
+                {
+                    new MessageType(eventType)
+                }));
+            }
+            else
+            {
+                Configure.Instance.ForAllTypes<IEvent>(eventType => SusbcriptionManager.Subscribe(eventType, Address.Local));
+            }
         }
     }
 }
