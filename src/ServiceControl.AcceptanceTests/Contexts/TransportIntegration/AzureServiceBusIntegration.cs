7955dd8d283e71682f6bf59eb2066626ecaac330
synhershko <itamar@code972.com>
2014-04-10 16:37:50 +0300

    Add Azure transports support to AcceptanceTests

---
+++ src/ServiceControl.AcceptanceTests/Contexts/TransportIntegration/AzureServiceBusIntegration.cs
@@ -0,0 +1,28 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.Contexts.TransportIntegration
+{
+    using System;
+    using NServiceBus;
+
+    public class AzureServiceBusIntegration : ITransportIntegration
+    {
+        public AzureServiceBusIntegration()
+        {
+            ConnectionString = ""; // empty on purpse
+        }
+
+        public string Name { get { return "AzureServiceBus"; } }
+        public Type Type { get { return typeof(AzureServiceBus); } }
+        public string TypeName { get { return "NServiceBus.AzureServiceBus, NServiceBus.Azure.Transports.WindowsAzureServiceBus"; } }
+        public string ConnectionString { get; set; }
+
+        public void SetUp()
+        {
+
+        }
+
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ src/ServiceControl.AcceptanceTests/Contexts/TransportIntegration/AzureStorageQueuesIntegration.cs
@@ -0,0 +1,28 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.Contexts.TransportIntegration
+{
+    using System;
+    using NServiceBus;
+
+    public class AzureStorageQueuesIntegration : ITransportIntegration
+    {
+        public AzureStorageQueuesIntegration()
+        {
+            ConnectionString = ""; // empty on purpse
+        }
+
+        public string Name { get { return "AzureStorageQueues"; } }
+        public Type Type { get { return typeof(AzureStorageQueue); } }
+        public string TypeName { get { return "NServiceBus.AzureStorageQueue, NServiceBus.Azure.Transports.WindowsAzureStorageQueues"; } }
+        public string ConnectionString { get; set; }
+
+        public void SetUp()
+        {
+
+        }
+
+        public void TearDown()
+        {
+
+        }
+    }
+}
