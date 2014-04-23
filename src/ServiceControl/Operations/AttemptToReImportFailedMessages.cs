0adbd57771858b58e31edc351b4c8be3081e6dd3
John Simons <john.simons@nservicebus.com>
2014-04-23 17:14:22 +1000

    Added re-import of failed messages at startup

---
+++ src/ServiceControl/Operations/AttemptToReImportFailedMessages.cs
@@ -0,0 +1,52 @@
+﻿namespace ServiceControl.Operations
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Transports;
+    using Raven.Client;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    class AttemptToReImportFailedMessages: IWantToRunWhenBusStartsAndStops
+    {
+        readonly IDocumentStore store;
+        readonly ISendMessages messageSender;
+        CancellationTokenSource source;
+
+        public AttemptToReImportFailedMessages(IDocumentStore store, ISendMessages messageSender)
+        {
+            this.store = store;
+            this.messageSender = messageSender;
+        }
+
+        public void Start()
+        {
+            source = new CancellationTokenSource();
+
+            Task.Factory.StartNew(() => Run<FailedAuditImport>(Settings.AuditQueue), source.Token);
+            Task.Factory.StartNew(() => Run<FailedErrorImport>(Settings.ErrorQueue), source.Token);
+        }
+
+        public void Stop()
+        {
+            source.Cancel();
+        }
+
+        void Run<T>(Address queue)
+        {
+            var session = store.OpenSession();
+            var query = session.Query<T>();
+            using (var ie = session.Advanced.Stream(query))
+            {
+                while (ie.MoveNext())
+                {
+                    var transportMessage = ((dynamic)ie.Current.Document).Message;
+
+                    messageSender.Send(transportMessage, queue);
+
+                    store.DatabaseCommands.Delete(ie.Current.Key, null);
+                }
+            }
+        }
+    }
+}
