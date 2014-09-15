4917ac71ffbfb6f8bb323174f2f0aa6ab14f2a69
SzymonPobiega <szymon.pobiega@gmail.com>
2014-09-15 12:43:19 +0200

    Fixed the integration test subscription mappings

--- src/ServiceControl.AcceptanceTests/Contexts/ConfigureExtensions.cs
+++ src/ServiceControl.AcceptanceTests/Contexts/ConfigureExtensions.cs
@@ -30,44 +30,6 @@
             return config.UseTransport(transport.Type, () => transport.ConnectionString);
         }
 
-        public static Configure DefineSerializer(this Configure config, string serializer)
-        {
-            if (string.IsNullOrEmpty(serializer))
-            {
-                Configure.Serialization.Xml();
-                return config;
-            }
-
-            var type = Type.GetType(serializer);
-
-            if (type == typeof(XmlMessageSerializer))
-            {
-                Configure.Serialization.Xml();
-                return config;
-            }
-
-
-            if (type == typeof(JsonMessageSerializer))
-            {
-                Configure.Serialization.Json();
-                return config;
-            }
-
-            if (type == typeof(BsonMessageSerializer))
-            {
-                Configure.Serialization.Bson();
-                return config;
-            }
-
-            if (type == typeof(BinaryMessageSerializer))
-            {
-                Configure.Serialization.Binary();
-                return config;
-            }
-
-            throw new InvalidOperationException("Unknown serializer:" + serializer);
-        }
-
         public static Configure DefineBuilder(this Configure config, string builder)
         {
             if (string.IsNullOrEmpty(builder))
