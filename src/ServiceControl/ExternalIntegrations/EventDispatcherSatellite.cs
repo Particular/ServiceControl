affb254067fc6904aa9050925bf4f6bceb0eb7af
SzymonPobiega <szymon.pobiega@gmail.com>
2014-09-15 10:45:50 +0200

    First draft for external integration (setellite and a handler)

---
+++ src/ServiceControl/ExternalIntegrations/EventDispatcherSatellite.cs
@@ -0,0 +1,40 @@
+﻿namespace Particular.ServiceControl.ExternalIntegrations
+{
+    using global::ServiceControl.Contracts.Failures;
+    using NServiceBus;
+    using NServiceBus.Satellites;
+
+    public class EventDispatcherSatellite : ISatellite
+    {
+        public const string EntityIdHeaderKey = "ServiceControl.EntityId";
+        public const string MessageTypeHeaderKey = "ServiceControl.ExternalEventType";
+        public IBus Bus { get; set; }
+
+        public bool Handle(TransportMessage message)
+        {
+            var entityId = message.Headers[EntityIdHeaderKey];
+            var messageType = message.Headers[MessageTypeHeaderKey];
+
+            Bus.Publish(new MessageFailed());
+
+            return true;
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public Address InputAddress
+        {
+            get { return Address.Local.SubScope("ExternalIntegrations"); }
+        }
+        public bool Disabled
+        {
+            get { return false; }
+        }
+    }
+}
---
+++ src/ServiceControl/ExternalIntegrations/EventMappingHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Particular.ServiceControl.ExternalIntegrations
+{
+    using global::ServiceControl.Contracts.MessageFailures;
+    using NServiceBus;
+    using NServiceBus.Transports;
+
+    public class EventMappingHandler : IHandleMessages<IEvent>
+    {
+        public ISendMessages MessageSender { get; set; }
+
+        public void Handle(IEvent message)
+        {
+            var failedMessageEvent = message as MessageFailed;
+            if (failedMessageEvent == null)
+            {
+                return;
+            }
+            var pushRequest = new TransportMessage();
+            pushRequest.Headers[EventDispatcherSatellite.EntityIdHeaderKey] = failedMessageEvent.FailedMessageId;
+            pushRequest.Headers[EventDispatcherSatellite.MessageTypeHeaderKey] = "MessagFailed";
+            MessageSender.Send(pushRequest, Address.Local.SubScope("ExternalIntegrations"));
+        }
+    }
+}
