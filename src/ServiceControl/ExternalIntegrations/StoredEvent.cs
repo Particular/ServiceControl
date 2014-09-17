14067dce62d6f044c4dd04da3b1bf2ba1e35fe7d
SzymonPobiega <szymon.pobiega@gmail.com>
2014-09-17 16:39:48 +0200

    Switched to use RavenDB-stored events

---
+++ src/ServiceControl/ExternalIntegrations/StoredEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.ExternalIntegrations
+{
+    using System;
+    using NServiceBus;
+
+    public class StoredEvent
+    {
+        public string Type { get; set; }
+        public IEvent Payload { get; set; }
+        public bool Dispatched { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ src/ServiceControl/ExternalIntegrations/StoredEventsDispatchingIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceControl.ExternalIntegrations
+{
+    using System.Linq;
+    using Raven.Client.Indexes;
+
+    public class StoredEventsDispatchingIndex : AbstractIndexCreationTask<StoredEvent>
+    {
+        public StoredEventsDispatchingIndex()
+        {
+            Map = events => from c in events
+                select new
+                {
+                    c.Dispatched,
+                    c.RegistrationDate
+                };
+        }
+    }
+}
