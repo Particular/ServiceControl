cc52ba3a58ff9c9a4e34634d75019fc1343878e2
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-22 12:07:02 +0200

    Moving contracts to external repo

--- src/ServiceControl.Contracts/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Particular.ServiceControl")]
-[assembly: AssemblyProduct("Particular ServiceControl")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
--- src/ServiceControl.IntegrationDemo/MessageFailedHandler.cs
+++ src/ServiceControl.IntegrationDemo/MessageFailedHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace ServiceControl.IntegrationDemo
 {
+    using System;
     using Contracts.Failures;
     using NServiceBus;
 
@@ -7,7 +8,7 @@
     {
         public void Handle(MessageFailed message)
         {
-            //Console.Out.WriteLine("Message with id {0} failed with reason {1}",message.FailedMessageId,message.FailureDetails.Exception.Message);
+            Console.Out.WriteLine("Message with id {0} failed with reason {1}",message.FailedMessageId,message.FailureDetails.Exception.Message);
 
             //if more info about the exception is needed you need to make calls to the http api
           
