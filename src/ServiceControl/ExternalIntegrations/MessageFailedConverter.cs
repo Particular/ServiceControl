11128f8b602ca98850b0b4cd7c17f685dd03e153
SzymonPobiega <szymon.pobiega@gmail.com>
2014-09-23 18:52:00 +0200

    Fixed the unit tests

---
+++ src/ServiceControl/ExternalIntegrations/MessageFailedConverter.cs
@@ -0,0 +1,70 @@
+namespace ServiceControl.ExternalIntegrations
+{
+    using System.Linq;
+    using ServiceControl.MessageFailures;
+
+    public static class MessageFailedConverter
+    {
+        public static Contracts.MessageFailed ToEvent(this FailedMessage message)
+        {
+            var last = message.ProcessingAttempts.Last();
+            var sendingEndpoint = (Contracts.Operations.EndpointDetails)last.MessageMetadata["SendingEndpoint"];
+            var receivingEndpoint = (Contracts.Operations.EndpointDetails)last.MessageMetadata["ReceivingEndpoint"];
+            return new Contracts.MessageFailed()
+            {
+                FailedMessageId = message.UniqueMessageId,
+                MessageType = (string)last.MessageMetadata["MessageType"],
+                NumberOfProcessingAttempts = message.ProcessingAttempts.Count,
+                Status = message.Status == FailedMessageStatus.Archived
+                    ? Contracts.MessageFailed.MessageStatus.ArchivedFailure
+                    : message.ProcessingAttempts.Count == 1
+                        ? Contracts.MessageFailed.MessageStatus.Failed
+                        : Contracts.MessageFailed.MessageStatus.RepeatedFailure,
+                ProcessingDetails = new Contracts.MessageFailed.ProcessingInfo
+                {
+                    SendingEndpoint = new Contracts.MessageFailed.ProcessingInfo.Endpoint()
+                    {
+                        Host = sendingEndpoint.Host,
+                        HostId = sendingEndpoint.HostId,
+                        Name = sendingEndpoint.Name
+                    },
+                    ProcessingEndpoint = new Contracts.MessageFailed.ProcessingInfo.Endpoint()
+                    {
+                        Host = receivingEndpoint.Host,
+                        HostId = receivingEndpoint.HostId,
+                        Name = receivingEndpoint.Name
+                    },
+                },
+                MessageDetails = new Contracts.MessageFailed.Message()
+                {
+                    Headers = last.Headers,
+                    ContentType = (string)last.MessageMetadata["ContentType"],
+                    Body = GetBody(last),
+                    MessageId = last.MessageId,
+                },
+                FailureDetails = new Contracts.MessageFailed.FailureInfo
+                {
+                    AddressOfFailingEndpoint = last.FailureDetails.AddressOfFailingEndpoint,
+                    TimeOfFailure = last.FailureDetails.TimeOfFailure,
+                    Exception = new Contracts.MessageFailed.FailureInfo.ExceptionInfo
+                    {
+                        ExceptionType = last.FailureDetails.Exception.ExceptionType,
+                        Message = last.FailureDetails.Exception.Message,
+                        Source = last.FailureDetails.Exception.Source,
+                        StackTrace = last.FailureDetails.Exception.StackTrace,
+                    },
+                },
+            };
+        }
+
+        static string GetBody(FailedMessage.ProcessingAttempt last)
+        {
+            object body;
+            if (last.MessageMetadata.TryGetValue("Body", out body))
+            {
+                return (string) body;
+            }
+            return null;
+        }
+    }
+}
--- src/ServiceControl/ExternalIntegrations/MessageFailedPublisher.cs
+++ src/ServiceControl/ExternalIntegrations/MessageFailedPublisher.cs
@@ -21,7 +21,7 @@ namespace ServiceControl.ExternalIntegrations
         {
             var documentIds = references.Select(x => x.FailedMessageId).Cast<ValueType>().ToArray();
             var failedMessageData = session.Load<FailedMessage>(documentIds);
-            return failedMessageData.Select(ConvertToEvent);
+            return failedMessageData.Select(x => x.ToEvent());
         }
 
         public class Reference
@@ -29,56 +29,6 @@ namespace ServiceControl.ExternalIntegrations
             public Guid FailedMessageId { get; set; }
         }
 
-        private static Contracts.MessageFailed ConvertToEvent(FailedMessage message)
-        {
-            var last = message.ProcessingAttempts.Last();
-            var sendingEndpoint = (Contracts.Operations.EndpointDetails)last.MessageMetadata["SendingEndpoint"];
-            var receivingEndpoint = (Contracts.Operations.EndpointDetails)last.MessageMetadata["ReceivingEndpoint"];
-            return new Contracts.MessageFailed()
-            {
-                FailedMessageId = message.UniqueMessageId,
-                MessageType = (string)last.MessageMetadata["MessageType"],
-                NumberOfProcessingAttempts = message.ProcessingAttempts.Count,
-                Status = message.Status == FailedMessageStatus.Archived
-                    ? Contracts.MessageFailed.MessageStatus.ArchivedFailure
-                    : message.ProcessingAttempts.Count == 1
-                        ? Contracts.MessageFailed.MessageStatus.Failed
-                        : Contracts.MessageFailed.MessageStatus.RepeatedFailure,
-                ProcessingDetails = new Contracts.MessageFailed.ProcessingInfo
-                {
-                    SendingEndpoint = new Contracts.MessageFailed.ProcessingInfo.Endpoint()
-                    {
-                        Host = sendingEndpoint.Host,
-                        HostId = sendingEndpoint.HostId,
-                        Name = sendingEndpoint.Name
-                    },
-                    ProcessingEndpoint = new Contracts.MessageFailed.ProcessingInfo.Endpoint()
-                    {
-                        Host = receivingEndpoint.Host,
-                        HostId = receivingEndpoint.HostId,
-                        Name = receivingEndpoint.Name
-                    },
-                },
-                MessageDetails = new Contracts.MessageFailed.Message()
-                {
-                    Headers = last.Headers,
-                    ContentType = (string)last.MessageMetadata["ContentType"],
-                    Body = (string)last.MessageMetadata["Body"],
-                    MessageId = last.MessageId,
-                },
-                FailureDetails = new Contracts.MessageFailed.FailureInfo
-                {
-                    AddressOfFailingEndpoint = last.FailureDetails.AddressOfFailingEndpoint,
-                    TimeOfFailure = last.FailureDetails.TimeOfFailure,
-                    Exception = new Contracts.MessageFailed.FailureInfo.ExceptionInfo
-                    {
-                        ExceptionType = last.FailureDetails.Exception.ExceptionType,
-                        Message = last.FailureDetails.Exception.Message,
-                        Source = last.FailureDetails.Exception.Source,
-                        StackTrace = last.FailureDetails.Exception.StackTrace,
-                    },
-                },
-            };
-        }
+        
     }
 }
