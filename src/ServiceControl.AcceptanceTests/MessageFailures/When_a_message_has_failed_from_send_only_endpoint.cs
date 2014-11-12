9f584ae0b6f8449136cf86b78127fe922c73d4d3
indualagarsamy <indu.alagarsamy@gmail.com>
2014-11-11 18:12:00 -0800

    ReceivingEndpoint value is now correct for failed msgs
    When messages are sent from SendOnly endpoints and endup in the error queue, the value assigned to the ReceivingEndpoint was incorrect. Added acceptance tests to cover this. Fixes https://github.com/Particular/ServiceControl/issues/405

---
+++ src/ServiceControl.AcceptanceTests/MessageFailures/When_a_message_has_failed_from_send_only_endpoint.cs
@@ -0,0 +1,120 @@
+﻿namespace ServiceBus.Management.AcceptanceTests
+{
+    using System;
+    using System.Runtime.Remoting.Contexts;
+    using Contexts;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Transports;
+    using NUnit.Framework;
+    using ServiceControl.MessageFailures.Api;
+
+    public class When_a_message_has_failed_from_send_only_endpoint : AcceptanceTest
+    {
+        [Test]
+        public void Should_be_listed_in_the_error_list_when_processing_endpoint_header_is_not_present()
+        {
+            var context = new MyContext
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                IncludeProcessingEndpointHeader = false
+            };
+         
+            FailedMessageView failure = null;
+            Scenario.Define(context)
+                .WithEndpoint<ManagementEndpoint>(c => c.AppConfig(PathToAppConfig))
+                .WithEndpoint<SendOnlyEndpoint>()
+                .Done(c => TryGetSingle("/api/errors", out failure, r => r.MessageId == c.MessageId))
+                .Run();
+            Assert.IsNotNull(failure);
+            Assert.IsTrue(failure.ReceivingEndpoint.Name.Contains("SomeEndpoint"), string.Format("The sending endpoint should be SomeEndpoint and not {0}", failure.ReceivingEndpoint.Name));
+            
+         }
+
+        [Test]
+        public void Should_be_listed_in_the_error_list_when_processing_endpoint_header_is_present()
+        {
+            var context = new MyContext
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                IncludeProcessingEndpointHeader = true
+            };
+
+            FailedMessageView failure = null;
+            Scenario.Define(context)
+                .WithEndpoint<ManagementEndpoint>(c => c.AppConfig(PathToAppConfig))
+                .WithEndpoint<SendOnlyEndpoint>()
+                .Done(c => TryGetSingle("/api/errors", out failure, r => r.MessageId == c.MessageId))
+                .Run();
+            Assert.IsNotNull(failure);
+            Assert.IsTrue(failure.ReceivingEndpoint.Name.Contains("SomeEndpoint"), string.Format("The sending endpoint should be SomeEndpoint and not {0}", failure.ReceivingEndpoint.Name));
+
+        }
+
+        public class SendOnlyEndpoint : EndpointConfigurationBuilder
+        {
+            public SendOnlyEndpoint()
+            {
+                EndpointSetup<DefaultServerWithoutAudit>();
+            }
+
+            class Foo : IWantToRunWhenBusStartsAndStops
+            {
+                public ISendMessages SendMessages { get; set; }
+
+                public MyContext MyContext { get; set; }
+
+                public void Start()
+                {
+                    //hack until we can fix the types filtering in default server
+                    if (MyContext == null || string.IsNullOrEmpty(MyContext.MessageId))
+                    {
+                        return;
+                    }
+
+                    if (Configure.EndpointName != "Particular.ServiceControl")
+                    {
+                        return;
+                    }
+
+                    // Transport message has no headers for Processing endpoint and the ReplyToAddress is set to null
+                    var transportMessage = new TransportMessage();
+                    if (MyContext.IncludeProcessingEndpointHeader)
+                    {
+                        transportMessage.Headers[Headers.ProcessingEndpoint] = "SomeEndpoint";
+                    }
+                    transportMessage.Headers[Headers.MessageId] = MyContext.MessageId;
+                    transportMessage.Headers[Headers.ConversationId] = "a59395ee-ec80-41a2-a728-a3df012fc707";
+                    transportMessage.Headers["$.diagnostics.hostid"] = "bdd4b0510bff5a6d07e91baa7e16a804";
+                    transportMessage.Headers["$.diagnostics.hostdisplayname"] = "SELENE";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.ExceptionType"] = "2014-11-11 02:26:57:767462 Z";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.Message"] = "An error occurred while attempting to extract logical messages from transport message NServiceBus.TransportMessage";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.InnerExceptionType"] = "System.Exception";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.HelpLink"] = "";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.Source"] = "NServiceBus.Core";
+                    transportMessage.Headers["NServiceBus.ExceptionInfo.StackTrace"] = "";
+                    transportMessage.Headers["NServiceBus.FailedQ"] = "SomeEndpoint@SELENE";
+                    transportMessage.Headers["NServiceBus.TimeOfFailure"] = "2014-11-11 02:26:58:000462 Z";
+                    transportMessage.Headers["NServiceBus.TimeSent"] = "2014-11-11 02:26:01:174786 Z";
+                    transportMessage.Headers[Headers.EnclosedMessageTypes] = "SendOnlyError.SendSomeCommand, TestSendOnlyError, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+                    transportMessage.ReplyToAddress = null;
+                    SendMessages.Send(transportMessage, Address.Parse("error"));
+                }
+
+                public void Stop()
+                {
+                }
+            }
+        }
+
+        public class MyMessage : IMessage
+        {
+        }
+
+        public class MyContext : ScenarioContext
+        {
+            public string MessageId { get; set; }
+            public bool IncludeProcessingEndpointHeader { get; set; }
+        }
+    }
+}
