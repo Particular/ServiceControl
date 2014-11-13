7d5d28be6c2020de3f06b976a9dcdd5c19ac7c9c
Andreas Öhlund <andreas.ohlund@particular.net>
2014-11-13 17:00:17 +0100

    Added detection of new endpoints
    And also a a GET /routes/{endpoint}

---
+++ src/ServiceControl/ScaleOut/DetectRoutes.cs
@@ -0,0 +1,49 @@
+﻿namespace ServiceControl.MessageFailures.Api
+{
+    using System.Collections.Generic;
+    using NServiceBus;
+    using Raven.Client;
+    using ServiceControl.Contracts.EndpointControl;
+
+    public class DetectRoutes : IHandleMessages<EndpointStarted>
+    {
+        public IDocumentSession Session { get; set; }
+
+        public void Handle(EndpointStarted message)
+        {
+            var parts = message.EndpointDetails.Name.Split('-');
+
+
+            if (parts.Length != 2)
+            {
+                return;
+            }
+
+            var logicalEndpoint = parts[0];
+
+            var enpointRoutes = Session.Load<EndpointRoutes>(logicalEndpoint) ?? new EndpointRoutes{Id = logicalEndpoint};
+
+            //todo: we need to get the real address
+            var address = message.EndpointDetails.Name + "@" + message.EndpointDetails.Host;
+
+            if (enpointRoutes.Routes.Contains(address))
+            {
+                return;
+            }
+
+            enpointRoutes.Routes.Add(address);
+
+            Session.Store(enpointRoutes);
+        }
+    }
+
+    public class EndpointRoutes
+    {
+        public EndpointRoutes()
+        {
+            Routes = new List<string>();
+        }
+        public string Id { get; set; }
+        public List<string> Routes { get; set; }
+    }
+}
---
+++ src/ServiceControl/ScaleOut/GetRoutesForEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceControl.MessageFailures.Api
+{
+    using Nancy;
+    using ServiceBus.Management.Infrastructure.Nancy.Modules;
+
+    public class GetRoutesForEndpoint : BaseModule
+    {
+        public GetRoutesForEndpoint()
+        {
+            Get["/routes/{id}"] = parameters =>
+            {
+                string endpoint = parameters.id;
+
+                using(var session = Store.OpenSession())
+                {
+                    var availableRoutes = session.Load<EndpointRoutes>(endpoint) ?? new EndpointRoutes();
+
+                    return Negotiate.WithModel(availableRoutes.Routes);    
+                }
+                
+            };
+        }
+
+    }
+
+}
