34a30fb5a228644da70be9afa96ccb2adc435361
Andreas Öhlund <andreas.ohlund@particular.net>
2014-11-17 14:38:08 +0100

    Removed the auto add by convention

---
+++ src/ServiceControl/ScaleOut/EnlistInScaleOutGroup.cs
@@ -0,0 +1,53 @@
+﻿namespace ServiceControl.MessageFailures.Api
+{
+    using System.Collections.Generic;
+    using Nancy;
+    using Nancy.ModelBinding;
+    using ServiceBus.Management.Infrastructure.Nancy.Modules;
+
+    public class EnlistInScaleOutGroup : BaseModule
+    {
+        public EnlistInScaleOutGroup()
+        {
+            Post["/scaleoutgroups/{id}/enlist"] = parameters =>
+            {
+                string scaleOutGroupId = parameters.id;
+
+                var request = this.Bind<EnlistRequest>();
+
+                var address = request.Address;
+
+                using (var session = Store.OpenSession())
+                {
+                    var scaleOutGroup = session.Load<ScaleOutGroup>(scaleOutGroupId) ?? new ScaleOutGroup { Id = scaleOutGroupId };
+
+
+                    if (!scaleOutGroup.Routes.Contains(address))
+                    {
+                        scaleOutGroup.Routes.Add(address);
+
+                        session.Store(scaleOutGroup);
+                        session.SaveChanges();
+                    }
+
+                    return HttpStatusCode.OK;
+                }
+
+            };
+        }
+        class EnlistRequest
+        {
+            public string Address { get; set; }
+        }
+    }
+
+    public class ScaleOutGroup
+    {
+        public ScaleOutGroup()
+        {
+            Routes = new List<string>();
+        }
+        public string Id { get; set; }
+        public List<string> Routes { get; set; }
+    }
+}
--- src/ServiceControl/ScaleOut/GetRoutesForEndpoint.cs
+++ src/ServiceControl/ScaleOut/GetRoutesForEndpoint.cs
@@ -4,7 +4,6 @@
     using System.Linq;
     using System.Runtime.InteropServices;
     using Nancy;
-    using Nancy.ModelBinding;
     using ServiceBus.Management.Infrastructure.Nancy.Modules;
 
     public class GetRoutesForScaleOutGroup : BaseModule
@@ -32,42 +31,6 @@
 
 
 
-        public class EnlistInScaleOutGroup : BaseModule
-        {
-            public EnlistInScaleOutGroup()
-            {
-                Post["/scaleoutgroups/{id}/enlist"] = parameters =>
-                {
-                    string scaleOutGroupId = parameters.id;
-
-                    var request = this.Bind<EnlistRequest>();
-
-                    var address = request.Address;
-
-                    using (var session = Store.OpenSession())
-                    {
-                        var scaleOutGroup = session.Load<ScaleOutGroup>(scaleOutGroupId) ?? new ScaleOutGroup{Id = scaleOutGroupId};
-
-                     
-                        if (!scaleOutGroup.Routes.Contains(address))
-                        {
-                            scaleOutGroup.Routes.Add(address);
-
-                            session.Store(scaleOutGroup);
-                            session.SaveChanges();
-                        }
-
-                        return HttpStatusCode.OK;
-                    }
-
-                };
-            }
-
-        }
-    }
-
-    public class EnlistRequest
-    {
-        public string Address { get; set; }
+    
     }
 }
