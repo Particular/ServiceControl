f5ff408e9ceb4859d564aea675c9ac1dc4c5fbb6
Simon Cropp <simon.cropp@gmail.com>
2014-11-20 09:46:58 +1100

    also log message Id when we fail to get endpoint name

--- src/ServiceControl/Infrastructure/TransportMessageExtensions.cs
+++ src/ServiceControl/Infrastructure/TransportMessageExtensions.cs
@@ -15,24 +15,24 @@
                 return endpoint;
             }
 
-
-            if (message.ReplyToAddress == null)
+            if (message.ReplyToAddress != null)
             {
-                // If the ReplyToAddress is null, then the message came from a send-only endpoint. 
-                // This message could be a failed message.  
-                if (message.Headers.TryGetValue("NServiceBus.FailedQ", out endpoint))
-                {
-                    return endpoint;
-                }
-                string messageTypes;
-                if (!message.Headers.TryGetValue(Headers.EnclosedMessageTypes, out messageTypes))
-                {
-                    messageTypes = "Unknown";
-                }
-
-                throw new InvalidOperationException(string.Format("No processing endpoint could be determined for message ({0})", messageTypes));
+                return message.ReplyToAddress.Queue;
             }
-            return message.ReplyToAddress.Queue;
+
+            // If the ReplyToAddress is null, then the message came from a send-only endpoint. 
+            // This message could be a failed message.  
+            if (message.Headers.TryGetValue("NServiceBus.FailedQ", out endpoint))
+            {
+                return endpoint;
+            }
+            string messageTypes;
+            if (message.Headers.TryGetValue(Headers.EnclosedMessageTypes, out messageTypes))
+            {
+                throw new Exception(string.Format("No processing endpoint could be determined for message ({0}) with EnclosedMessageTypes ({1})", message.Id, messageTypes));
+            }
+
+            throw new Exception(string.Format("No processing endpoint could be determined for message ({0})", message.Id));
         }
 
         public static string UniqueId(this TransportMessage message)
