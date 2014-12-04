d22af7758a04988981dc5ed65c244c48e073d79d
John Simons <john.simons@nservicebus.com>
2014-12-04 12:10:02 +1000

    Only return 404 when no routes exist
    If there are routes but they are no connected ones then that is an error

--- src/ServiceControl/ScaleOut/GetRoutesForScaleOutGroup.cs
+++ src/ServiceControl/ScaleOut/GetRoutesForScaleOutGroup.cs
@@ -21,17 +21,21 @@
                     var availableRoutes = session.Query<ScaleOutGroupRegistration>()
                         .Where(r => r.GroupId == groupId && r.Status == ScaleOutGroupRegistrationStatus.Connected)
                         .Statistics(out stats)
-                        .Take(1024)                    
+                        .Take(1024)
                         .Select(r => r.Address)
-                        .ToArray();
+                        .ToListAsync();
 
-                    if (availableRoutes.Length == 0)
+                    var count = session.Query<ScaleOutGroupRegistration>()
+                        .Where(r => r.GroupId == groupId)
+                        .CountAsync();
+
+                    if (count.Result == 0)
                     {
                         return HttpStatusCode.NotFound;
                     }
 
-                    return Negotiate.WithModel(availableRoutes)
-                        .WithEtagAndLastModified(stats);
+                    return Negotiate.WithModel(availableRoutes.Result)
+                            .WithEtagAndLastModified(stats);
                 }
             };
         }
