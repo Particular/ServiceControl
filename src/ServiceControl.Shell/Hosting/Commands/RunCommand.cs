70489b4f4408ecf58d58e0873c9879f93ab7a25d
John Simons <john.simons@nservicebus.com>
2015-03-25 13:31:23 +1000

    Doing a bit of locking to stop the race condition

--- src/ServiceControl.Shell/Hosting/Commands/RunCommand.cs
+++ src/ServiceControl.Shell/Hosting/Commands/RunCommand.cs
@@ -20,24 +20,35 @@
 
             using (var service = new Host{ ServiceName = args.ServiceName} )
             {
-
-
                 using (var waitHandle = new ManualResetEvent(false))
                 {
+                    var lockObject = new Object();
                     service.OnStopping = () =>
                     {
+                        if (!Monitor.TryEnter(lockObject))
+                        {
+                            return;
+                        }
+
                         service.OnStopping = () => { };
                         waitHandle.Set();
+                        Monitor.Exit(lockObject);
                     };
-
-                    service.Run();
 
                     Console.CancelKeyPress += (sender, e) =>
                     {
+                        if (!Monitor.TryEnter(lockObject))
+                        {
+                            return;
+                        }
+
                         service.OnStopping = () => { };
                         e.Cancel = true;
                         waitHandle.Set();
+                        Monitor.Exit(lockObject);
                     };
+
+                    service.Run();
 
                     Console.WriteLine("Press Ctrl+C to exit");
                     waitHandle.WaitOne();
