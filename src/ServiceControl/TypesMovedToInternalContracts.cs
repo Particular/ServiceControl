8cb2b28a98775989e5ef429e9a7792049afb59b7
SzymonPobiega <szymon.pobiega@gmail.com>
2015-04-27 11:57:09 +0200

    Added type forwards for classes moved from ServiceControl assembly that RavenDB is deserializing without knowing their actual type.

---
+++ src/ServiceControl/TypesMovedToInternalContracts.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using ServiceControl.Contracts.Operations;
+using ServiceControl.ExternalIntegrations;
+
+//in the FailedMessage's metadata
+[assembly: TypeForwardedTo(typeof(EndpointDetails))]
+
+//in ExternalIntegrationDispatchRequest document
+[assembly: TypeForwardedTo(typeof(CustomCheckFailedPublisher.DispatchContext))]
+[assembly: TypeForwardedTo(typeof(CustomCheckSucceededPublisher.DispatchContext))]
+[assembly: TypeForwardedTo(typeof(HeartbeatRestoredPublisher.DispatchContext))]
+[assembly: TypeForwardedTo(typeof(HeartbeatStoppedPublisher.DispatchContext))]
+[assembly: TypeForwardedTo(typeof(MessageFailedPublisher.DispatchContext))]
