f2c69a1c1c9c75d561fbfaff0599e111bfd6b4b0
SzymonPobiega <szymon.pobiega@gmail.com>
2015-04-28 12:49:21 +0200

    Fixed inspection problem

--- src/Particular.Backend.Debugging.AcceptanceTests/When_a_message_has_been_successfully_processed.cs
+++ src/Particular.Backend.Debugging.AcceptanceTests/When_a_message_has_been_successfully_processed.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading;
     using global::ServiceControl.Contracts.Operations;
     using NServiceBus;
     using NServiceBus.AcceptanceTesting;
