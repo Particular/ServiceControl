1466ecb25efe370d18981dd072721355e6c65e84
weronika.labaj <weronika.labaj@gmail.com>
2015-06-07 23:29:33 +0200

    publish an event every time a new group is created

---
+++ src/ServiceControl.InternalContracts/Messages/MessageFailures/NewFailedMessagesGroupCreated.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceControl.InternalContracts.Messages.MessageFailures
+{
+    using NServiceBus;
+
+    public class NewFailedMessagesGroupCreated : IEvent
+    {
+        public string Id { get; set; }
+    }
+}
