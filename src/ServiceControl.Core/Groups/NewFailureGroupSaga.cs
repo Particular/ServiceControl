61412fd16244f0448d4d2e698a59a24635af366c
weronika.labaj <weronika.labaj@gmail.com>
2015-06-09 14:44:57 +0200

    Introduce saga to prevent raising FailureGroupCreated event multiple times add group name to event and command

---
+++ src/ServiceControl.Core/Groups/NewFailureGroupSaga.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ServiceControl.Groups
+{
+    using NServiceBus.Saga;
+    using ServiceControl.InternalContracts.Messages.MessageFailures;
+
+    public class NewFailureGroupSagaData : ContainSagaData
+    {
+        [Unique]
+        public string FailureGroupId { get; set; }
+    }
+
+    public class NewFailureGroupSaga : Saga<NewFailureGroupSagaData>, IAmStartedByMessages<RaiseNewFailureGroupDetectedEvent>
+    {
+        public void Handle(RaiseNewFailureGroupDetectedEvent message)
+        {
+            if (string.IsNullOrEmpty(Data.FailureGroupId))
+            {
+                Data.FailureGroupId = message.GroupId;
+                Bus.Publish(new NewFailureGroupDetected
+                    {
+                        GroupId = message.GroupId,
+                        GroupName = message.GroupName
+                    });
+            }
+        }
+
+        public override void ConfigureHowToFindSaga()
+        {
+            ConfigureMapping<RaiseNewFailureGroupDetectedEvent>(m => m.GroupId)
+                .ToSaga(s => s.FailureGroupId);
+        }
+    }
+}
---
+++ src/ServiceControl.InternalContracts/Messages/MessageFailures/RaiseNewFailureGroupDetectedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceControl.InternalContracts.Messages.MessageFailures
+{
+    using NServiceBus;
+
+    public class RaiseNewFailureGroupDetectedEvent : ICommand
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
