efa5a88bee0ebb8a350af488556df71abea9c09d
Greg Bielleman <greg.bielleman@particular.net>
2015-06-11 14:12:13 +1000

    Update for new Deployment process

---
+++ src/Setup/chocolatey/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$productName = "ServiceControl"
+$url = "https://github.com/Particular/$productName/releases/download/$env:chocolateyPackageVersion/Particular.$productName-$env:chocolateyPackageVersion.exe"
+Install-ChocolateyPackage $productName 'exe' '/quiet' $url
---
+++ src/Setup/chocolatey/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+﻿$psFile = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'elevatedUninstall.ps1'
+Start-ChocolateyProcessAsAdmin "& `'$psFile`'"
