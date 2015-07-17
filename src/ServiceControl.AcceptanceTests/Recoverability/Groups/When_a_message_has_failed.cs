e9385c8692979a03d6707f45903128bf05beb4a1
Mike Minutillo <michael.minutillo@gmail.com>
2015-07-17 13:59:54 +0800

    Basic Acceptance Tests for Grouping Functionality

---
+++ src/ServiceControl.AcceptanceTests/Recoverability/Groups/When_a_message_has_failed.cs
@@ -0,0 +1,71 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.Recoverability.Groups
+{
+    using System;
+    using System.Linq;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Features;
+    using NUnit.Framework;
+    using ServiceBus.Management.AcceptanceTests.Contexts;
+    using ServiceControl.Infrastructure;
+    using ServiceControl.MessageFailures;
+
+    class When_a_message_has_failed : AcceptanceTest
+    {
+        [Test]
+        public void Should_be_grouped()
+        {
+            var context = new MyContext();
+
+            FailedMessage failedMessage = null;
+
+            Scenario.Define(context)
+                .WithEndpoint<ManagementEndpoint>(c => c.AppConfig(PathToAppConfig))
+                .WithEndpoint<Receiver>(b => b.Given(bus => bus.SendLocal(new MyMessage())))
+                .Done(c => c.MessageId != null && TryGet("/api/errors/" + c.UniqueMessageId, out failedMessage, msg => msg.FailureGroups.Any()))
+                .Run();
+
+            Assert.AreEqual(context.UniqueMessageId, failedMessage.UniqueMessageId);
+            Assert.IsNotEmpty(failedMessage.FailureGroups, "The returned message should have failure groups");
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServerWithoutAudit>(c => Configure.Features.Disable<SecondLevelRetries>())
+                    .AuditTo(Address.Parse("audit"));
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                public MyContext Context { get; set; }
+
+                public IBus Bus { get; set; }
+
+                public void Handle(MyMessage message)
+                {
+                    Context.EndpointNameOfReceivingEndpoint = Configure.EndpointName;
+                    Context.MessageId = Bus.CurrentMessageContext.Id.Replace(@"\", "-");
+                    throw new Exception("Simulated exception");
+                }
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+        }
+
+        public class MyContext : ScenarioContext
+        {
+            public string MessageId { get; set; }
+            public string EndpointNameOfReceivingEndpoint { get; set; }
+
+            public string UniqueMessageId
+            {
+                get { return DeterministicGuid.MakeId(MessageId, EndpointNameOfReceivingEndpoint).ToString(); }
+            }
+        }
+    }
+}
