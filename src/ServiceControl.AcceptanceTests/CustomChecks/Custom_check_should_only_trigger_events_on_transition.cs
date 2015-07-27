a99aa8c1d87b7078e819551923308377b9ee8d06
John Simons <john.simons@nservicebus.com>
2015-07-27 11:11:19 +1000

    Enabled another test that was ignored

--- src/ServiceControl.AcceptanceTests/CustomChecks/Custom_check_should_only_trigger_events_on_transition.cs
+++ src/ServiceControl.AcceptanceTests/CustomChecks/Custom_check_should_only_trigger_events_on_transition.cs
@@ -14,11 +14,10 @@ namespace ServiceBus.Management.AcceptanceTests.CustomChecks
     [TestFixture]
     public class Custom_check_should_only_trigger_events_on_transition : AcceptanceTest
     {
-        [Ignore]
         [Test]
         public void Should_result_in_a_custom_check_failed_event()
         {
-            var context = new When_a_periodic_custom_check_fails.MyContext();
+            var context = new MyContext();
 
             EventLogItem entry = null;
 
@@ -28,12 +27,9 @@ namespace ServiceBus.Management.AcceptanceTests.CustomChecks
                 .Done(c => TryGetSingle("/api/eventlogitems/", out entry, e => e.EventType == typeof(CustomCheckFailed).Name))
                 .Run();
 
-            Assert.AreEqual(Severity.Error, entry.Severity, "Failed custom checks should be treated as info");
+            Assert.AreEqual(Severity.Error, entry.Severity, "Failed custom checks should be treated as error");
             Assert.IsTrue(entry.RelatedTo.Any(item => item == "/customcheck/EventuallyFailingCustomCheck"));
-            Assert.IsTrue(entry.RelatedTo.Any(item => item.StartsWith("/endpoint/CustomChecks.EndpointWithFailingCustomCheck")));
-
         }
-
 
         public class MyContext : ScenarioContext
         {
@@ -53,7 +49,7 @@ namespace ServiceBus.Management.AcceptanceTests.CustomChecks
                 private static int counter;
 
                 public EventuallyFailingCustomCheck()
-                    : base("A test check", "Testing", TimeSpan.FromSeconds(1)) { }
+                    : base("EventuallyFailingCustomCheck", "Testing", TimeSpan.FromSeconds(1)) { }
 
                 public override CheckResult PerformCheck()
                 {
