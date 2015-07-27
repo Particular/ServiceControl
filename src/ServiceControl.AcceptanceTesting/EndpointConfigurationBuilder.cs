c63261cb69183adeb2598c1d586075d507e665ba
John Simons <john.simons@nservicebus.com>
2015-07-27 12:48:08 +1000

    Bringing in AcceptanceTesting framework
    So we can hack it :)

---
+++ src/ServiceControl.AcceptanceTesting/EndpointConfigurationBuilder.cs
@@ -0,0 +1,113 @@
+﻿namespace NServiceBus.AcceptanceTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using Support;
+
+    public class EndpointConfigurationBuilder : IEndpointConfigurationFactory
+    {
+        public EndpointConfigurationBuilder()
+        {
+            configuration.EndpointMappings = new Dictionary<Type, Type>();
+        }
+
+        public EndpointConfigurationBuilder AuditTo<T>()
+        {
+            configuration.AuditEndpoint = typeof(T);
+            return this;
+        }
+
+        public EndpointConfigurationBuilder AuditTo(Address addressOfAuditQueue)
+        {
+            configuration.AddressOfAuditQueue = addressOfAuditQueue;
+            return this;
+        }
+
+        public EndpointConfigurationBuilder CustomMachineName(string customMachineName)
+        {
+            configuration.CustomMachineName = customMachineName;
+
+            return this;
+        }
+
+        public EndpointConfigurationBuilder CustomEndpointName(string customEndpointName)
+        {
+            configuration.CustomEndpointName = customEndpointName;
+
+            return this;
+        }
+
+        public EndpointConfigurationBuilder AllowExceptions()
+        {
+            configuration.AllowExceptions = true;
+
+            return this;
+        }
+
+        public EndpointConfigurationBuilder AddMapping<T>(Type endpoint)
+        {
+            configuration.EndpointMappings.Add(typeof(T),endpoint);
+
+            return this;
+        }
+
+        EndpointConfiguration CreateScenario()
+        {
+            configuration.BuilderType = GetType();
+
+            return configuration;
+        }
+
+        public EndpointConfigurationBuilder EndpointSetup<T>() where T : IEndpointSetupTemplate
+        {
+            return EndpointSetup<T>(c => { });
+        }
+
+        public EndpointConfigurationBuilder EndpointSetup<T>(Action<Configure> configCustomization) where T: IEndpointSetupTemplate
+        {
+            configuration.GetConfiguration = (settings,routingTable) =>
+                {
+                    var config = ((IEndpointSetupTemplate)Activator.CreateInstance<T>()).GetConfiguration(settings, configuration, new ScenarioConfigSource(configuration, routingTable));
+
+                    configCustomization(config);
+
+                    return config;
+                };
+
+            return this;
+        }
+
+        EndpointConfiguration IEndpointConfigurationFactory.Get()
+        {
+            return CreateScenario();
+        }
+
+       
+        readonly EndpointConfiguration configuration = new EndpointConfiguration();
+
+        public EndpointConfigurationBuilder WithConfig<T>(Action<T> action)
+        {
+            var config = Activator.CreateInstance<T>();
+
+            action(config);
+
+            configuration.UserDefinedConfigSections[typeof (T)] = config;
+            
+            return this;
+        }
+
+        public EndpointConfigurationBuilder ExcludeType<T>()
+        {
+            configuration.TypesToExclude.Add(typeof(T));
+
+            return this;
+        }
+
+        public EndpointConfigurationBuilder IncludeType<T>()
+        {
+            configuration.TypesToInclude.Add(typeof(T));
+
+            return this;
+        }
+    }
+}
---
+++ src/ServiceControl.AcceptanceTesting/Support/EndpointRunner.cs
@@ -0,0 +1,197 @@
+﻿namespace NServiceBus.AcceptanceTesting.Support
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Remoting.Lifetime;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Installation.Environments;
+    using Logging;
+    using NServiceBus.Support;
+
+    [Serializable]
+    public class EndpointRunner : MarshalByRefObject
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (EndpointRunner));
+        private readonly SemaphoreSlim contextChanged = new SemaphoreSlim(0);
+        private readonly IList<Guid> executedWhens = new List<Guid>();
+        private EndpointBehavior behavior;
+        private IStartableBus bus;
+        private Configure config;
+        private EndpointConfiguration configuration;
+        private Task executeWhens;
+        private ScenarioContext scenarioContext;
+        private bool stopped;
+
+        public Result Initialize(RunDescriptor run, EndpointBehavior endpointBehavior,
+            IDictionary<Type, string> routingTable, string endpointName)
+        {
+            try
+            {
+                behavior = endpointBehavior;
+                scenarioContext = run.ScenarioContext;
+                configuration =
+                    ((IEndpointConfigurationFactory) Activator.CreateInstance(endpointBehavior.EndpointBuilderType))
+                        .Get();
+                configuration.EndpointName = endpointName;
+
+                if (!string.IsNullOrEmpty(configuration.CustomMachineName))
+                {
+                    RuntimeEnvironment.MachineNameAction = () => configuration.CustomMachineName;
+                }
+
+                //apply custom config settings
+                endpointBehavior.CustomConfig.ForEach(customAction => customAction(config));
+                config = configuration.GetConfiguration(run, routingTable);
+
+                if (scenarioContext != null)
+                {
+                    config.Configurer.RegisterSingleton(scenarioContext.GetType(), scenarioContext);
+                    scenarioContext.ContextPropertyChanged += scenarioContext_ContextPropertyChanged;
+                }
+
+                bus = config.CreateBus();
+
+                Configure.Instance.ForInstallationOn<Windows>().Install();
+
+                executeWhens = Task.Factory.StartNew(() =>
+                {
+                    while (!stopped)
+                    {
+                        if (!contextChanged.Wait(TimeSpan.FromSeconds(5)))
+                            //we spin around each 5s since the callback mechanism seems to be shaky
+                        {
+                            continue;
+                        }
+
+                        lock (behavior)
+                        {
+                            foreach (var when in behavior.Whens)
+                            {
+                                if (executedWhens.Contains(when.Id))
+                                {
+                                    continue;
+                                }
+
+                                if (when.ExecuteAction(scenarioContext, bus))
+                                {
+                                    executedWhens.Add(when.Id);
+                                }
+                            }
+                        }
+                    }
+                });
+
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to initialize endpoint " + endpointName, ex);
+                return Result.Failure(ex);
+            }
+        }
+
+        private void scenarioContext_ContextPropertyChanged(object sender, EventArgs e)
+        {
+            contextChanged.Release();
+        }
+
+        public Result Start()
+        {
+            try
+            {
+                foreach (var given in behavior.Givens)
+                {
+                    var action = given.GetAction(scenarioContext);
+
+                    action(bus);
+                }
+
+                bus.Start();
+
+
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to start endpoint " + configuration.EndpointName, ex);
+
+                return Result.Failure(ex);
+            }
+        }
+
+        public Result Stop()
+        {
+            try
+            {
+                stopped = true;
+
+                scenarioContext.ContextPropertyChanged -= scenarioContext_ContextPropertyChanged;
+
+                executeWhens.Wait();
+                contextChanged.Dispose();
+
+                bus.Dispose();
+
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to stop endpoint " + configuration.EndpointName, ex);
+
+                return Result.Failure(ex);
+            }
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            var lease = (ILease) base.InitializeLifetimeService();
+            if (lease.CurrentState == LeaseState.Initial)
+            {
+                lease.InitialLeaseTime = TimeSpan.FromMinutes(2);
+                lease.SponsorshipTimeout = TimeSpan.FromMinutes(2);
+                lease.RenewOnCallTime = TimeSpan.FromSeconds(2);
+            }
+            return lease;
+        }
+
+        public string Name()
+        {
+            return AppDomain.CurrentDomain.FriendlyName;
+        }
+
+        [Serializable]
+        public class Result : MarshalByRefObject
+        {
+            public Exception Exception { get; set; }
+
+            public bool Failed
+            {
+                get { return Exception != null; }
+            }
+
+            public static Result Success()
+            {
+                return new Result();
+            }
+
+            public static Result Failure(Exception ex)
+            {
+                var baseException = ex.GetBaseException();
+
+                if (ex.GetType().IsSerializable)
+                {
+                    return new Result
+                    {
+                        Exception = baseException
+                    };
+                }
+
+                return new Result
+                {
+                    Exception = new Exception(baseException.Message)
+                };
+            }
+        }
+    }
+}
