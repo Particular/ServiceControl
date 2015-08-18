252c504dd8c5b6fcc5cfb315d2f517d5f22ea70c
John Simons <john.simons@nservicebus.com>
2015-08-19 08:51:42 +1000

    Not needed

--- src/ServiceControl.AcceptanceTests/CustomChecks/When_a_custom_check_fails.cs
+++ src/ServiceControl.AcceptanceTests/CustomChecks/When_a_custom_check_fails.cs
@@ -31,7 +31,6 @@
 
         public class MyContext : ScenarioContext
         {
-            public string CustomCheckId { get; set; }
         }
 
         public class EndpointWithFailingCustomCheck : EndpointConfigurationBuilder
