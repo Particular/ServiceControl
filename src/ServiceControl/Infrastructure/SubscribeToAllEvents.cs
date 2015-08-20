0edd66cbb2a8304652b6a429c6b5fcbc2ac06b05
Mike Minutillo <michael.minutillo@gmail.com>
2015-08-20 15:33:02 +0800

    Subscribe to all local events for brokers

---
+++ src/ServiceControl/Infrastructure/SubscribeToAllEvents.cs
@@ -0,0 +1,31 @@
+﻿namespace ServiceControl.Infrastructure
+{
+    using NServiceBus;
+    using NServiceBus.Settings;
+    using NServiceBus.Transports;
+
+    class SubscribeToAllEvents : IWantToRunWhenBusStartsAndStops
+    {
+        public IManageSubscriptions SubscriptionManager { get; set; }
+
+        public TransportDefinition TransportDefinition { get; set; }
+
+        public ReadOnlySettings Settings { get; set; }
+
+        public void Stop()
+        {
+        }
+
+        public void Start()
+        {
+            // Subscribe to events for brokers
+            if (TransportDefinition.HasSupportForCentralizedPubSub)
+            {
+                foreach (var eventType in Settings.GetAvailableTypes().Implementing<IEvent>())
+                {
+                    SubscriptionManager.Subscribe(eventType, Settings.LocalAddress());
+                }
+            }
+        }
+    }
+}
