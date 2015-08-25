05444b8840350836eb067fbabfd18b8cf728ebe0
Mike Minutillo <michael.minutillo@gmail.com>
2015-08-25 12:49:44 +0800

    Move Reclassify Errors at Startup to a Startup Task of the Failed Message Classification feature

--- src/ServiceControl/Recoverability/Grouping/FailedMessageClassification.cs
+++ src/ServiceControl/Recoverability/Grouping/FailedMessageClassification.cs
@@ -2,18 +2,35 @@
 {
     using NServiceBus;
     using NServiceBus.Features;
+    using ServiceControl.MessageFailures.InternalMessages;
 
     class FailedMessageClassification : Feature
     {
         public FailedMessageClassification()
         {
             EnableByDefault();
+            RegisterStartupTask<ReclassifyErrorsAtStartup>();
         }
 
         protected override void Setup(FeatureConfigurationContext context)
         {
             context.Container.ConfigureComponent<ExceptionTypeAndStackTraceMessageGrouper>(DependencyLifecycle.SingleInstance);
             context.Container.ConfigureComponent<ClassifyFailedMessageEnricher>(DependencyLifecycle.SingleInstance);
+        }
+
+        class ReclassifyErrorsAtStartup : FeatureStartupTask
+        {
+            readonly IBus bus;
+
+            public ReclassifyErrorsAtStartup(IBus bus)
+            {
+                this.bus = bus;
+            }
+
+            protected override void OnStart()
+            {
+                bus.SendLocal(new ReclassifyErrors());
+            }
         }
     }
 }
