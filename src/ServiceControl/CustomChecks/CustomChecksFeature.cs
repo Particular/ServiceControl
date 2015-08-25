d89de9fc5f587cf18fb88cb2e223b92a277a899d
Mike Minutillo <michael.minutillo@gmail.com>
2015-08-25 14:32:50 +0800

    Move Custom Checks Infrastructure under new Custom Checks Feature

---
+++ src/ServiceControl/CustomChecks/CustomChecksFeature.cs
@@ -0,0 +1,44 @@
+﻿namespace ServiceControl.CustomChecks
+{
+    using System;
+    using NServiceBus;
+    using NServiceBus.Features;
+    using Raven.Client;
+
+    class CustomChecksFeature : Feature
+    {
+        public CustomChecksFeature()
+        {
+            EnableByDefault();
+            RegisterStartupTask<WireUpCustomCheckNotifications>();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Container.ConfigureComponent<CustomCheckNotifications>(DependencyLifecycle.SingleInstance)
+        }
+
+        class WireUpCustomCheckNotifications : FeatureStartupTask
+        {
+            CustomCheckNotifications notifications;
+            IDocumentStore store;
+            IDisposable subscription;
+
+            public WireUpCustomCheckNotifications(CustomCheckNotifications notifications, IDocumentStore store)
+            {
+                this.notifications = notifications;
+                this.store = store;
+            }
+
+            protected override void OnStart()
+            {
+                subscription = store.Changes().ForIndex(new CustomChecksIndex().IndexName).Subscribe(notifications);
+            }
+
+            protected override void OnStop()
+            {
+                subscription.Dispose();
+            }
+        }
+    }
+}
