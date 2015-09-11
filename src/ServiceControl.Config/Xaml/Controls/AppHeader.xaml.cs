b840c721c9537f0faa69a98c92910c082e25decb
Cameron MacFarland <distantcam@gmail.com>
2015-09-11 12:11:56 +0800

    Removing silly R# errors from a generated code file that no one looks at anyway

--- src/ServiceControl.Config/Xaml/Controls/AppHeader.xaml.cs
+++ src/ServiceControl.Config/Xaml/Controls/AppHeader.xaml.cs
@@ -1,24 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ServiceControl.Config.Xaml.Controls
+﻿namespace ServiceControl.Config.Xaml.Controls
 {
-    /// <summary>
-    /// Interaction logic for AppHeader.xaml
-    /// </summary>
-    public partial class AppHeader : UserControl
+    partial class AppHeader
     {
         public AppHeader()
         {
