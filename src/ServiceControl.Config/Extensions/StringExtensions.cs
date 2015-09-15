e65456451f5c42412e3d59d1946e032913f890e7
Greg Bielleman <greg.bielleman@particular.net>
2015-09-15 15:35:37 +1000

    Renamed method as it also splits on "."

--- src/ServiceControl.Config/Extensions/StringExtensions.cs
+++ src/ServiceControl.Config/Extensions/StringExtensions.cs
@@ -1,14 +1,12 @@
 ﻿namespace ServiceControl.Config.Extensions
 {
     using System;
-
+    
     static class StringExtensions
     {
-        public static string Truncate(this string value, int maxLength)
+        public static string TruncateSentence(this string value, int maxLength)
         {
-
             var sentence = value.Split('.')[0];
-
             return string.IsNullOrEmpty(sentence) ? sentence : sentence.Substring(0, Math.Min(sentence.Length, maxLength)) + "...";
         }
     }
