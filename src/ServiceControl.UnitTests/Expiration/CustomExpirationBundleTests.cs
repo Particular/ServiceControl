e559f43933453d554caca61722ed60adeb71dff8
Simon Cropp <simon.cropp@gmail.com>
2015-09-16 14:47:03 +1000

    redundant using

--- src/ServiceControl.UnitTests/Expiration/CustomExpirationBundleTests.cs
+++ src/ServiceControl.UnitTests/Expiration/CustomExpirationBundleTests.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Threading;
     using MessageAuditing;
     using MessageFailures;
     using NUnit.Framework;
