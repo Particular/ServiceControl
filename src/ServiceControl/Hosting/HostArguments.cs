69810b6934c2b87ca652bb592abbd160c896b356
Greg Bielleman <greg.bielleman@particular.net>
2015-10-26 16:30:36 +1100

    Remove service config/uninstall/install  command line options for SC.  Maint and help retained

--- src/ServiceControl/Hosting/HostArguments.cs
+++ src/ServiceControl/Hosting/HostArguments.cs
@@ -24,61 +24,10 @@ namespace Particular.ServiceControl.Hosting
             Username = String.Empty;
             Password = String.Empty;
 
-            defaultOptions = new OptionSet
+            var defaultOptions = new OptionSet
             {
                 {
                     "?|h|help", "Help about the command line options.", key => { Help = true; }
-                },
-                {
-                    "d|set={==}", "The configuration {0:option} to set to the specified {1:value}", (key, value) =>
-                    {
-                        options[key] = value;
-
-                        Commands = new List<Type>
-                        {
-                            typeof(WriteOptionsCommand),
-                        };
-                    }
-                },
-                {
-                    "restart",
-                    @"Restarts the endpoint."
-                    , s =>
-                    {
-                        Commands = new List<Type>
-                        {
-                            typeof(WriteOptionsCommand),
-                            typeof(RestartCommand),
-                        };
-                    }
-                },
-                {
-                    "start",
-                    @"Starts the endpoint."
-                    , s =>
-                    {
-                        Commands = new List<Type>
-                        {
-                            typeof(WriteOptionsCommand),
-                            typeof(StartCommand),
-                        };
-                    }
-                },
-                {
-                    "stop",
-                    @"Stops the endpoint."
-                    , s =>
-                    {
-                        Commands = new List<Type>
-                        {
-                            typeof(StopCommand),
-                        };
-                    }
-                },
-                {
-                    "serviceName=",
-                    @"Specify the service name for the installed service."
-                    , s => { ServiceName = s; }
                 },
             };
 
@@ -97,28 +46,7 @@ namespace Particular.ServiceControl.Hosting
                                                }
                                            };
 
-         
-            uninstallOptions = new OptionSet
-            {
-                {
-                    "?|h|help", "Help about the command line options.", key => { Help = true; }
-                },
-                {
-                    "u|uninstall",
-                    @"Uninstall the endpoint as a Windows service."
-                    , s =>
-                    {
-                        Commands = new List<Type> {typeof(UninstallCommand)};
-                        executionMode = ExecutionMode.Uninstall;
-                    }
-                },
-                {
-                    "serviceName=",
-                    @"Specify the service name for the installed service."
-                    , s => { ServiceName = s; }
-                }
-            };
-
+            // Not documented in help - Used bt SC installer only
             var externalInstallerOptions = new OptionSet
             {
                 {
@@ -142,116 +70,15 @@ namespace Particular.ServiceControl.Hosting
                     , s => { Username = s; }
                 },
             };
-
-            installOptions = new OptionSet
-            {
-                {
-                    "?|h|help",
-                    "Help about the command line options.",
-                    key => { Help = true; }
-                },
-                {
-                    "i|install",
-                    @"Install the endpoint as a Windows service."
-                    , s =>
-                    {
-                        Commands = new List<Type>
-                        {
-                            typeof(WriteOptionsCommand),
-                            typeof(CheckMandatoryInstallOptionsCommand),
-                            typeof(RunBootstrapperAndNServiceBusInstallers),
-                            typeof(InstallCommand)
-                        };
-                        executionMode = ExecutionMode.Install;
-                    }
-                },
-                {
-                    "serviceName=",
-                    @"Specify the service name for the installed service."
-                    , s => { ServiceName = s; }
-                },
-                {
-                    "displayName=",
-                    @"Friendly name for the installed service."
-                    , s => { DisplayName = s; }
-                },
-                {
-                    "description=",
-                    @"Description for the service."
-                    , s => { Description = s; }
-                },
-                {
-                    "username=",
-                    @"Username for the account the service should run under."
-                    , s => { Username = s; }
-                },
-                {
-                    "password=",
-                    @"Password for the service account."
-                    , s => { Password = s; }
-                },
-                {
-                    "localservice",
-                    @"Run the service with the local service account."
-                    , s => { ServiceAccount = ServiceAccount.LocalService; }
-                },
-                {
-                    "networkservice",
-                    @"Run the service with the network service permission."
-                    , s => { ServiceAccount = ServiceAccount.NetworkService; }
-                },
-                {
-                    "user",
-                    @"Run the service with the specified username and password. Alternative the system will prompt for a valid username and password if values for both the username and password are not specified."
-                    , s => { ServiceAccount = ServiceAccount.User; }
-                },
-                {
-                    "delayed",
-                    @"The service should start automatically (delayed)."
-                    , s => { StartMode = StartMode.Delay; }
-                },
-                {
-                    "autostart",
-                    @"The service should start automatically (default)."
-                    , s => { StartMode = StartMode.Automatic; }
-                },
-                {
-                    "disabled",
-                    @"The service should be set to disabled."
-                    , s => { StartMode = StartMode.Disabled; }
-                },
-                {
-                    "manual",
-                    @"The service should be started manually."
-                    , s => { StartMode = StartMode.Manual; }
-                },
-                {
-                    "d|set={==}", "The configuration {0:option} to set to the specified {1:value}", (key, value) =>
-                    {
-                        options[key] = value;
-                    }
-                },
-            };
-
+            
             try
             {
-                installOptions.Parse(args);
-                if (executionMode == ExecutionMode.Install)
-                {
-                    return;
-                }
-
                 externalInstallerOptions.Parse(args);
                 if (executionMode == ExecutionMode.Install)
                 {
                     return;
                 }
-
-                uninstallOptions.Parse(args);
-                if (executionMode == ExecutionMode.Uninstall)
-                {
-                    return;
-                }
+                
                 maintenanceOptions.Parse(args);
                 if (executionMode == ExecutionMode.Maintenance)
                 {
@@ -282,7 +109,6 @@ namespace Particular.ServiceControl.Hosting
 
         public StartMode StartMode { get; private set; }
 
-        public string OutputPath { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public ServiceAccount ServiceAccount { get; set; }
@@ -305,24 +131,10 @@ namespace Particular.ServiceControl.Hosting
                     }
                 }
             }
-
-            installOptions.WriteOptionDescriptions(new StringWriter(sb));
-            var installOptionsHelp = sb.ToString();
-
-            sb.Clear();
-            uninstallOptions.WriteOptionDescriptions(new StringWriter(sb));
-            var uninstallOptionsHelp = sb.ToString();
-
-            sb.Clear();
-            defaultOptions.WriteOptionDescriptions(new StringWriter(sb));
-            var defaultOptionsHelp = sb.ToString();
-
-            Console.Out.WriteLine(helpText, defaultOptionsHelp, installOptionsHelp, uninstallOptionsHelp);
+            
+            Console.Out.WriteLine(helpText);
         }
 
-        readonly OptionSet installOptions;
-        readonly OptionSet uninstallOptions;
-        readonly OptionSet defaultOptions;
         Dictionary<string, string> options = new Dictionary<string, string>();
     }
 
