ba3b6c82900d631728830017a0264bb999cd512f
Greg Bielleman <greg.bielleman@particular.net>
2015-11-03 11:47:09 +1100

    Add splash screen to SCMU

---
+++ src/ServiceControl.Config/Splash.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ServiceControl.Config
+{
+    public static class Splash
+    {
+
+        public static void Show()
+        {
+            var splash = new SplashScreen(typeof(Splash).Assembly, "Splash.png");
+            splash.Show(true);
+        }
+    }
+}
