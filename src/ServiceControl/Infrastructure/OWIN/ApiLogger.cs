4bbe0230184de421a4f77162b810af99b9fa5291
Mike Minutillo <michael.minutillo@gmail.com>
2016-02-15 10:19:48 +0800

    Log all API calls

---
+++ src/ServiceControl/Infrastructure/OWIN/ApiLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Infrastructure.OWIN
+{
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
+    using NServiceBus.Logging;
+
+    class LogApiCalls : OwinMiddleware
+    {
+        public LogApiCalls(OwinMiddleware next) : base(next) { }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            log.DebugFormat("Begin {0}: {1}", context.Request.Method, context.Request.Uri.ToString());
+
+            await Next.Invoke(context);
+
+            log.DebugFormat("End {0}: {1}", context.Request.Method, context.Request.Uri.ToString());
+        }
+
+        static ILog log = LogManager.GetLogger<LogApiCalls>();
+    }
+}
