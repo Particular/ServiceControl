c1e091c5a6c0e4537c4fbe4a4a4daf62dfc7a81f
John Simons <john.simons@nservicebus.com>
2016-02-15 16:51:38 +1000

    Archiving by index

--- src/ServiceControl/Recoverability/Grouping/Archiving/ArchiveAllInGroupHandler.cs
+++ src/ServiceControl/Recoverability/Grouping/Archiving/ArchiveAllInGroupHandler.cs
@@ -1,68 +1,56 @@
 namespace ServiceControl.Recoverability
 {
-    using System.Collections.Generic;
+    using System.Globalization;
     using NServiceBus;
     using Raven.Abstractions.Data;
-    using Raven.Abstractions.Exceptions;
     using Raven.Client;
-    using Raven.Client.Linq;
+    using Raven.Json.Linq;
     using ServiceControl.MessageFailures;
 
     public class ArchiveAllInGroupHandler : IHandleMessages<ArchiveAllInGroup>
     {
         public void Handle(ArchiveAllInGroup message)
         {
-            var query = Session.Query<FailureGroupMessageView, FailedMessages_ByGroup>()
-                .Where(m => m.FailureGroupId == message.GroupId && m.Status == FailedMessageStatus.Unresolved)
-                .ProjectFromIndexFieldsInto<FailureGroupMessageView>();
-
-            string groupName = null;
-            var messageIds = new List<string>();
-
-            using (var stream = Session.Advanced.Stream(query))
-            {
-                while (stream.MoveNext())
-                {
-                    if (stream.Current.Document.Status != FailedMessageStatus.Unresolved)
-                    {
-                        continue;
-                    }
-
-                    if (groupName == null)
-                    {
-                        groupName = stream.Current.Document.FailureGroupName;
-                    }
-
-                    try { 
-                        Session.Advanced.DocumentStore.DatabaseCommands.Patch(
-                            stream.Current.Document.Id,
+            RavenJToken result = Session.Advanced.DocumentStore.DatabaseCommands.UpdateByIndex(
+                            "FailedMessages/ByGroup",
+                            new IndexQuery
+                            {
+                                Query = string.Format(CultureInfo.InvariantCulture, "FailureGroupId:{0} AND Status:{1}", message.GroupId, (int)FailedMessageStatus.Unresolved)
+                            },
                             new[]
                             {
                                 new PatchRequest
                                 {
                                     Type = PatchCommandType.Set,
                                     Name = "Status",
-                                    Value = (int) FailedMessageStatus.Archived,
-                                    PrevVal = (int) FailedMessageStatus.Unresolved
+                                    Value = (int) FailedMessageStatus.Archived
                                 }
-                            });
+                            }).WaitForCompletion();
 
-                        messageIds.Add(stream.Current.Document.MessageId);
+            //TODO: What to do with result, it looks like:
+            /*
+            [
+                {
+                    "Document": "failedmessages/46aa0a73-fa28-81be-63b4-813895af1d6f",
+                    "Result": {
+                        "PatchResult": "Patched",
+                        "Document": null
                     }
-                    catch (ConcurrencyException)
-                    {
-                        // Ignore concurrency exceptions
+                },
+                {
+                    "Document": "failedmessages/40432c54-2ecd-dc35-8ec1-6996a0002913",
+                    "Result": {
+                        "PatchResult": "Patched",
+                        "Document": null
                     }
-
-                    
                 }
-            }
-
+            ]
+            */
             Bus.Publish<FailedMessageGroupArchived>(m =>
             {
                 m.GroupId = message.GroupId;
-                m.GroupName = groupName;
-                m.MessageIds = messageIds.ToArray();
+                m.GroupName = message.GroupId;
+                m.MessageIds = new []{ result.ToString() };
             });
         }
 
