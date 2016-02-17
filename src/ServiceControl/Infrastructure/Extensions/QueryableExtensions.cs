d4a8bc035d3abf9c8053913c465c540107580f4c
John Simons <john.simons@nservicebus.com>
2016-02-17 10:23:49 +1000

    Added paging to /eventlogitems
    Before it was an unbouded query

--- src/ServiceControl/Infrastructure/Extensions/QueryableExtensions.cs
+++ src/ServiceControl/Infrastructure/Extensions/QueryableExtensions.cs
@@ -183,7 +183,39 @@ namespace ServiceControl.Infrastructure.Extensions
 
             return source;
         }
-     
+
+        public static IOrderedQueryable<TSource> Paging<TSource>(this IOrderedQueryable<TSource> source, Request request)
+        {
+            var maxResultsPerPage = 50;
+
+            if (request.Query.per_page.HasValue)
+            {
+                maxResultsPerPage = request.Query.per_page;
+            }
+
+            if (maxResultsPerPage < 1)
+            {
+                maxResultsPerPage = 50;
+            }
+
+            var page = 1;
+
+            if (request.Query.page.HasValue)
+            {
+                page = request.Query.page;
+            }
+
+            if (page < 1)
+            {
+                page = 1;
+            }
+
+            var skipResults = (page - 1) * maxResultsPerPage;
+
+            return (IOrderedQueryable<TSource>)source.Skip(skipResults)
+                .Take(maxResultsPerPage);
+        }
+
         public static IRavenQueryable<TSource> Paging<TSource>(this IRavenQueryable<TSource> source, Request request)
         {
             var maxResultsPerPage = 50;
