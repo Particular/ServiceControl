c9d4bed009c31a383ba30310dd1719d35a0a637b
John Simons <john.simons@nservicebus.com>
2016-03-18 13:36:13 +1000

    Including more diagnostic details into /api/configuration

--- src/ServiceControl/Infrastructure/Nancy/Modules/RootModule.cs
+++ src/ServiceControl/Infrastructure/Nancy/Modules/RootModule.cs
@@ -1,6 +1,6 @@
 ﻿namespace ServiceBus.Management.Infrastructure.Nancy.Modules
 {
-    using System.IO;
+    using System;
     using Particular.ServiceControl.Licensing;
     using Settings;
     using global::Nancy;
@@ -27,21 +27,55 @@
                     Name = SettingsReader<string>.Read("Name", "ServiceControl"),
                     Description = SettingsReader<string>.Read("Description", "The management backend for the Particular Service Platform"),
                     LicenseStatus = License.IsValid ? "valid" : "invalid",
-                    InstanceInfo = BaseUrl + "/instance-info"
+                    Configuration = BaseUrl + "/configuration"
                 };
 
                 return Negotiate
                     .WithModel(model);
             };
 
-            Get["/instance-info"] = p => Negotiate
-                    .WithModel(new
-                                  {
-                                      WindowsService = Settings.ServiceName,
-                                      LogfilePath = Path.Combine(LoggingSettings.LogPath, "logfile.txt"),
-                                      Settings.TransportType,
-                                      RavenDBPath = Settings.DbPath
-                                  });
+            Func<dynamic, dynamic> configuration = p => Negotiate
+                .WithModel(new
+                {
+                    Host = new
+                    {
+                        Settings.ServiceName,
+                        RavenDBPath = Settings.DbPath,
+                        Logging = new
+                        {
+                            LoggingSettings.LogPath,
+                            LoggingLevel = LoggingSettings.LoggingLevel.Name,
+                            RavenDBLogLevel = LoggingSettings.RavenDBLogLevel.Name
+                        }
+                    },
+                    DataRetention = new
+                    {
+                        AuditRetentionPeriod = Settings.AuditRetentionPeriod,
+                        ErrorRetentionPeriod = Settings.ErrorRetentionPeriod
+                    },
+                    PerformanceTunning = new
+                    {
+                        Settings.MaximumMessageThroughputPerSecond,
+                        MaxBodySizeToStore= Settings.MaxBodySizeToStore,
+                        Settings.HttpDefaultConnectionLimit,
+                        Settings.ExternalIntegrationsDispatchingBatchSize
+                    },
+                    Transport = new
+                    {
+                        Settings.TransportType,
+                        AuditLogQueue = Settings.AuditLogQueue.Queue,
+                        AuditQueue = Settings.AuditQueue.Queue,
+                        ErrorLogQueue = Settings.ErrorLogQueue.Queue,
+                        ErrorQueue = Settings.ErrorQueue.Queue,
+                        Settings.ForwardAuditMessages
+                    },
+                    Plugins = new
+                    {
+                        HeartbeatGracePeriod = Settings.HeartbeatGracePeriod
+                    }
+                });
+
+            Get["/instance-info"] = Get["/configuration"] = configuration;
         }
 
         public ActiveLicense License { get; set; }
@@ -54,7 +88,7 @@
             public string EndpointsMessagesUrl { get; set; }
             public string EndpointsUrl { get; set; }
             public string ErrorsUrl { get; set; }
-            public string InstanceInfo { get; set; }
+            public string Configuration { get; set; }
             public string MessageSearchUrl { get; set; }
             public string LicenseStatus { get; set; }
             public string Name { get; set; }
