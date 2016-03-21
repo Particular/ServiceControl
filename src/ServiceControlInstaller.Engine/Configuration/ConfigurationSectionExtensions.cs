5085f4bde7660d5f8ec9d4bf46ee54d77cf44b59
Greg Bielleman <greg.bielleman@particular.net>
2016-03-21 19:05:22 +1100

    Add backward compat overload for config settings extension

--- src/ServiceControlInstaller.Engine/Configuration/ConfigurationSectionExtensions.cs
+++ src/ServiceControlInstaller.Engine/Configuration/ConfigurationSectionExtensions.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Configuration;
+    using System.Linq;
 
     static class ConfigurationSectionExtensions
     {
         public static void Set(this ConnectionStringSettingsCollection collection, string key, string value)
         {
             collection.Remove(key);
-            if (!String.IsNullOrWhiteSpace(value))
+            if (!string.IsNullOrWhiteSpace(value))
             {
                 collection.Add(new ConnectionStringSettings(key, value));
             }
@@ -17,7 +18,21 @@
         public static void Set(this KeyValueConfigurationCollection collection, string key, string value)
         {
             collection.Remove(key);
-            if (!String.IsNullOrWhiteSpace(value))
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                collection.Add(new KeyValueConfigurationElement(key, value));
+            }
+        }
+        /// <summary>
+        /// if the key matches a value in unsupportedKeys then this is a no-op.  This is provide some backward compat when a new SCMU is editing an old SC instance
+        /// </summary>
+        public static void Set(this KeyValueConfigurationCollection collection, string key, string value, string[] unsupportedKeys)
+        {
+            if ((unsupportedKeys != null) && (unsupportedKeys.Any(p => p.Equals(key, StringComparison.OrdinalIgnoreCase))))
+                    return;
+
+            collection.Remove(key);
+            if (!string.IsNullOrWhiteSpace(value))
             {
                 collection.Add(new KeyValueConfigurationElement(key, value));
             }
