7fabf247d04332123e8ad76100ee46402e9f50b3
John Simons <john.simons@nservicebus.com>
2016-06-03 16:17:51 +1000

    Moved tooltip to top
    The mouse pointer was hiding the tolltip

--- src/ServiceControl.Config/Xaml/Converters/BoolToVisibilityConverter.cs
+++ src/ServiceControl.Config/Xaml/Converters/BoolToVisibilityConverter.cs
@@ -50,4 +50,23 @@ namespace ServiceControl.Config.Xaml.Converters
             return this;
         }
     }
+
+    public class NegaterConverter : System.Windows.Markup.MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var angle = (double)value;
+
+            return -angle;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
 }
