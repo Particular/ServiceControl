d7ca1102830041fb55993ff5044927cb5d001234
John Simons <john.simons@nservicebus.com>
2016-06-23 10:37:02 +1000

    Added sql dtc option

---
+++ src/ServiceControl.SqlServerWithDTC/ServiceControlDTCSupport.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceControl.Transports.SqlServerWithDTC
+{
+    using NServiceBus;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    public class ServiceControlDTCSupport : INeedInitialization
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            if (SettingsReader<bool>.Read("EnableDtc"))
+            {
+                configuration.Transactions()
+                    .EnableDistributedTransactions()
+                    .WrapHandlersExecutionInATransactionScope();
+            }
+        }
+    }
+}
