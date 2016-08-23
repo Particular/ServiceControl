168f300994880874c35d515a7cc72b52c6c1c59f
Greg Bielleman <greg.bielleman@particular.net>
2016-08-23 14:41:39 +1000

    Fix Inspections

--- src/ServiceControl.Config/UI/AdvancedOptions/CertificateDialogViewModel.cs
+++ src/ServiceControl.Config/UI/AdvancedOptions/CertificateDialogViewModel.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Security.Cryptography.X509Certificates;
     using System.Windows;
     using System.Windows.Input;
