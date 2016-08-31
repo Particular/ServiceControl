1ddb719761f51b681bf73c6a34b9c8a3cfd186d0
Simon Cropp <simon.cropp@gmail.com>
2016-09-01 09:27:13 +1000

    cleanup

--- src/ServiceControl/SagaAudit/SagaAuditing.cs
+++ src/ServiceControl/SagaAudit/SagaAuditing.cs
@@ -26,11 +26,12 @@
             {
                 string sagasInvokedRaw;
 
-                if (message.PhysicalMessage.Headers.TryGetValue("NServiceBus.InvokedSagas", out sagasInvokedRaw))
+                var headers = message.PhysicalMessage.Headers;
+                if (headers.TryGetValue("NServiceBus.InvokedSagas", out sagasInvokedRaw))
                 {
                     string sagasChangeRaw;
                     var sagasChanges = new Dictionary<string, string>();
-                    if (message.PhysicalMessage.Headers.TryGetValue("ServiceControl.SagaStateChange", out sagasChangeRaw))
+                    if (headers.TryGetValue("ServiceControl.SagaStateChange", out sagasChangeRaw))
                     {
                         var multiSagaChanges = sagasChangeRaw.Split(';');
 
@@ -46,11 +47,12 @@
                             var sagaInvoked = saga.Split(':');
                             string changeText;
 
-                            sagasChanges.TryGetValue(sagaInvoked[1], out changeText);
+                            var guid = sagaInvoked[1];
+                            sagasChanges.TryGetValue(guid, out changeText);
 
                             return new SagaInfo
                             {
-                                SagaId = Guid.Parse(sagaInvoked[1]),
+                                SagaId = Guid.Parse(guid),
                                 SagaType = sagaInvoked[0],
                                 ChangeStatus = changeText,
                             };
@@ -64,13 +66,13 @@
                     string sagaId;
 
                     //for backwards compatibility
-                    if (message.PhysicalMessage.Headers.TryGetValue(Headers.SagaId, out sagaId))
+                    if (headers.TryGetValue(Headers.SagaId, out sagaId))
                     {
                         // A failure when a MarkAsComplete control message is received causes a saga message to be received in
                         // the error queue without a Headers.SagaType header.
-                        // Hence the reason for the check!
+                        // Hence the reason for the check
                         string sagaType;
-                        if (message.PhysicalMessage.Headers.TryGetValue(Headers.SagaType, out sagaType))
+                        if (headers.TryGetValue(Headers.SagaType, out sagaType))
                         {
                             sagaType = sagaType.Split(',').First();
                         }
@@ -92,11 +94,11 @@
 
                 string originatingSagaId;
 
-                if (message.PhysicalMessage.Headers.TryGetValue(Headers.OriginatingSagaId, out originatingSagaId))
+                if (headers.TryGetValue(Headers.OriginatingSagaId, out originatingSagaId))
                 {
                     // I am not sure if we need this logic here as well, but just in case see comment above.
                     string sagaType;
-                    if (message.PhysicalMessage.Headers.TryGetValue(Headers.OriginatingSagaType, out sagaType))
+                    if (headers.TryGetValue(Headers.OriginatingSagaType, out sagaType))
                     {
                         sagaType = sagaType.Split(',').First();
                     }
