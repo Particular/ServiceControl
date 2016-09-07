72df6387a9261e3a552b6c48e967c88f3476b0c3
Greg Bielleman <greg.bielleman@particular.net>
2016-09-07 12:15:56 +1000

    Fix setting backward compat

--- src/ServiceControlInstaller.Engine/Configuration/ConfigurationWriter.cs
+++ src/ServiceControlInstaller.Engine/Configuration/ConfigurationWriter.cs
@@ -51,8 +51,8 @@
             settings.Set(SettingsList.HostName, details.HostName);
             settings.Set(SettingsList.LogPath, details.LogPath);
             settings.Set(SettingsList.DBPath, details.DBPath);
-            settings.Set(SettingsList.IngestionCachePath, details.IngestionCachePath);
-            settings.Set(SettingsList.BodyStoragePath, details.BodyStoragePath);
+            settings.Set(SettingsList.IngestionCachePath, details.IngestionCachePath, version);
+            settings.Set(SettingsList.BodyStoragePath, details.BodyStoragePath,version);
             settings.Set(SettingsList.ForwardAuditMessages, details.ForwardAuditMessages.ToString());
             settings.Set(SettingsList.ForwardErrorMessages, details.ForwardErrorMessages.ToString(), version);
             settings.Set(SettingsList.TransportType, Transports.FindByName(details.TransportPackage).TypeName, version);
