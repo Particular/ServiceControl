d0f1c8c03cc802bfd1fff56f09ed4da281d27a99
John Simons <john.simons@nservicebus.com>
2016-09-12 15:50:19 +1000

    Wrong label

--- src/ServiceControl/Operations/Error/ProcessErrors.cs
+++ src/ServiceControl/Operations/Error/ProcessErrors.cs
@@ -41,7 +41,7 @@
 
         public void Start()
         {
-            breaker = new RepeatedFailuresOverTimeCircuitBreaker("ProcessErrors", TimeSpan.FromMinutes(2), ex => criticalError.Raise("Repeated failures when processing audits.", ex),
+            breaker = new RepeatedFailuresOverTimeCircuitBreaker("ProcessErrors", TimeSpan.FromMinutes(2), ex => criticalError.Raise("Repeated failures when processing errors.", ex),
                 TimeSpan.FromSeconds(2));
             stop = false;
             task = ProcessWithRetries();
