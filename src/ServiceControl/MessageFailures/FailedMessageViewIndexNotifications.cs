abdbc806ee49e9cc8cbbdbff533bf8e856af8fdc
John Simons <john.simons@nservicebus.com>
2016-09-16 09:42:45 +1000

    Use multiget

--- src/ServiceControl/MessageFailures/FailedMessageViewIndexNotifications.cs
+++ src/ServiceControl/MessageFailures/FailedMessageViewIndexNotifications.cs
@@ -48,21 +48,24 @@
         {
             using (var session = store.OpenSession())
             {
-                var failedUnresolvedMessageCount = session.Query<FailedMessage, FailedMessageViewIndex>().Count(p => p.Status == FailedMessageStatus.Unresolved);
-                var failedArchivedMessageCount = session.Query<FailedMessage, FailedMessageViewIndex>().Count(p => p.Status == FailedMessageStatus.Archived);
+                var failedUnresolvedMessageCount = session.Query<FailedMessage, FailedMessageViewIndex>().Where(p => p.Status == FailedMessageStatus.Unresolved).CountLazily();
+                var failedArchivedMessageCount = session.Query<FailedMessage, FailedMessageViewIndex>().Where(p => p.Status == FailedMessageStatus.Archived).CountLazily();
+                
+                session.Advanced.Eagerly.ExecuteAllPendingLazyOperations();
 
-                if (lastUnresolvedCount == failedUnresolvedMessageCount && lastArchivedCount == failedArchivedMessageCount)
+                if (lastUnresolvedCount == failedUnresolvedMessageCount.Value && lastArchivedCount == failedArchivedMessageCount.Value)
                 {
                     return;
                 }
-                lastUnresolvedCount = failedUnresolvedMessageCount;
-                lastArchivedCount = failedArchivedMessageCount;
+
+                lastUnresolvedCount = failedUnresolvedMessageCount.Value;
+                lastArchivedCount = failedArchivedMessageCount.Value;
 
                 bus.Publish(new MessageFailuresUpdated
                 {
-                    Total = failedUnresolvedMessageCount, // Left here for backwards compatibility, to be removed eventually.
-                    UnresolvedTotal = failedUnresolvedMessageCount,
-                    ArchivedTotal = failedArchivedMessageCount
+                    Total = lastUnresolvedCount, // Left here for backwards compatibility, to be removed eventually.
+                    UnresolvedTotal = lastUnresolvedCount,
+                    ArchivedTotal = lastArchivedCount
                 });
             }
         }
