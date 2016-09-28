cbd2e11c48ff8de790f1691b31ca49b3d874a29f
WilliamBZA <william.brander@gmail.com>
2016-09-28 16:16:00 +0200

    Add batch counts to the retry data

--- src/ServiceControl/Recoverability/Retries/RetryBatch.cs
+++ src/ServiceControl/Recoverability/Retries/RetryBatch.cs
@@ -6,9 +6,10 @@ namespace ServiceControl.Recoverability
     {
         public string Id { get; set; }
         public string Context { get; set; }
-        public string GroupId { get; set; }
         public string RetrySessionId { get; set; }
         public string StagingId { get; set; }
+        public string GroupId { get; set; }
+        public int? TotalRetryBatchesInGroup { get; set; }
         public RetryBatchStatus Status { get; set; }
         public IList<string> FailureRetries { get; set; }
 
