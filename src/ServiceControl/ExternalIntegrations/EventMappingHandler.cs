c9e7e82bc9ad00ba8f7afbf19802ee439ec2cbb9
Mike Minutillo <michael.minutillo@gmail.com>
2016-10-06 12:42:45 +0800

    External Events - Immediate Dispatch rather than queuing via database

--- src/ServiceControl/ExternalIntegrations/EventMappingHandler.cs
+++ src/ServiceControl/ExternalIntegrations/EventMappingHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace ServiceControl.ExternalIntegrations
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using NServiceBus;
@@ -10,11 +11,13 @@
     {
         private readonly IDocumentStore store;
         private readonly IEnumerable<IEventPublisher> eventPublishers;
+        private readonly IBus bus;
 
-        public EventMappingHandler(IDocumentStore store, IEnumerable<IEventPublisher> eventPublishers)
+        public EventMappingHandler(IDocumentStore store, IEnumerable<IEventPublisher> eventPublishers, IBus bus)
         {
             this.store = store;
             this.eventPublishers = eventPublishers;
+            this.bus = bus;
         }
         public void Handle(IEvent message)
         {
@@ -24,21 +27,42 @@
 
             using (var session = store.OpenSession())
             {
-                foreach (var dispatchContext in dispatchContexts)
-                {
-                    if (Logger.IsDebugEnabled)
-                    {
-                        Logger.Debug("Storing dispatch request.");
-                    }
-                    var dispatchRequest = new ExternalIntegrationDispatchRequest
-                    {
-                        DispatchContext = dispatchContext
-                    };
+                Dispatch(dispatchContexts, session);
+            }
+        }
 
-                    session.Store(dispatchRequest);
+        private void Dispatch(IEnumerable<object> dispatchContexts, IDocumentSession session)
+        {
+            var eventsToBePublished = eventPublishers.SelectMany(p => p.PublishEventsForOwnContexts(dispatchContexts, session));
+            foreach (var eventToBePublished in eventsToBePublished)
+            {
+                if (Logger.IsDebugEnabled)
+                {
+                    Logger.Debug("Publishing external event on the bus.");
                 }
 
-                session.SaveChanges();
+                try
+                {
+                    bus.Publish(eventToBePublished);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error("Failed dispatching external integration event.", e);
+
+                    var m = new ExternalIntegrationEventFailedToBePublished
+                    {
+                        EventType = eventToBePublished.GetType()
+                    };
+                    try
+                    {
+                        m.Reason = e.GetBaseException().Message;
+                    }
+                    catch (Exception)
+                    {
+                        m.Reason = "Failed to retrieve reason!";
+                    }
+                    bus.Publish(m);
+                }
             }
         }
 
