4f71c0307b331ccb43d9a6f5d26fdd8a467f28cb
John Simons <john.simons@nservicebus.com>
2016-10-12 20:29:13 +1000

    Update to NServiceBus v5.2.19
    Removed PipelineExecutor from satellites

--- src/ServiceControl/Operations/AuditQueueImport.cs
+++ src/ServiceControl/Operations/AuditQueueImport.cs
@@ -4,30 +4,42 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using Contracts.Operations;
     using NServiceBus;
     using NServiceBus.Logging;
     using NServiceBus.ObjectBuilder;
-    using NServiceBus.Pipeline;
-    using NServiceBus.Pipeline.Contexts;
     using NServiceBus.Satellites;
     using NServiceBus.Transports;
     using NServiceBus.Unicast;
-    using NServiceBus.Unicast.Messages;
     using NServiceBus.Unicast.Transport;
     using Raven.Client;
     using ServiceBus.Management.Infrastructure.Settings;
-    using ServiceControl.Infrastructure.RavenDB;
+    using ServiceControl.Contracts.Operations;
+    using ServiceControl.MessageAuditing;
 
     public class AuditQueueImport : IAdvancedSatellite, IDisposable
     {
-        public IBuilder Builder { get; set; }
-        public ISendMessages Forwarder { get; set; }
-        public PipelineExecutor PipelineExecutor { get; set; }
-        public LogicalMessageFactory LogicalMessageFactory { get; set; }
-        public CriticalError CriticalError { get; set; }
-        public LoggingSettings LoggingSettings { get; set; }
-        public Settings Settings { get; set; }
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(AuditQueueImport));
+        private readonly IBuilder builder;
+        private readonly CriticalError criticalError;
+        private readonly IEnrichImportedMessages[] enrichers;
+        private readonly ISendMessages forwarder;
+        private readonly LoggingSettings loggingSettings;
+        private readonly Settings settings;
+        private readonly IDocumentStore store;
+        private SatelliteImportFailuresHandler satelliteImportFailuresHandler;
+
+        public AuditQueueImport(IBuilder builder, ISendMessages forwarder, IDocumentStore store, CriticalError criticalError, LoggingSettings loggingSettings, Settings settings)
+        {
+            this.builder = builder;
+            this.forwarder = forwarder;
+            this.store = store;
+
+            this.criticalError = criticalError;
+            this.loggingSettings = loggingSettings;
+            this.settings = settings;
+
+            enrichers = builder.BuildAll<IEnrichImportedMessages>().ToArray();
+        }
 
         public bool Handle(TransportMessage message)
         {
@@ -36,92 +48,30 @@
             return true;
         }
 
-        void InnerHandle(TransportMessage message)
-        {
-            var receivedMessage = new ImportSuccessfullyProcessedMessage(message);
-
-            using (var childBuilder = Builder.CreateChildBuilder())
-            {
-                PipelineExecutor.CurrentContext.Set(childBuilder);
-
-                foreach (var enricher in childBuilder.BuildAll<IEnrichImportedMessages>())
-                {
-                    enricher.Enrich(receivedMessage);
-                }
-
-                var logicalMessage = LogicalMessageFactory.Create(receivedMessage);
-
-                var context = new IncomingContext(PipelineExecutor.CurrentContext, message)
-                {
-                    LogicalMessages = new List<LogicalMessage>
-                    {
-                        logicalMessage
-                    },
-                    IncomingLogicalMessage = logicalMessage
-                };
-
-                context.Set("NServiceBus.CallbackInvocationBehavior.CallbackWasInvoked", false);
-
-                var behaviors = behavioursToAddFirst.Concat(PipelineExecutor.Incoming.SkipWhile(r => r.StepId != WellKnownStep.LoadHandlers).Select(r => r.BehaviorType));
-
-                PipelineExecutor.InvokePipeline(behaviors, context);
-            }
-
-            if (Settings.ForwardAuditMessages)
-            {
-                TransportMessageCleaner.CleanForForwarding(message);
-                Forwarder.Send(message, new SendOptions(Settings.AuditLogQueue));
-            }
-        }
-
-        Type[] behavioursToAddFirst = new[] { typeof(RavenUnitOfWorkBehavior) };
-
         public void Start()
         {
             if (!TerminateIfForwardingIsEnabledButQueueNotWritable())
             {
-                Logger.InfoFormat("Audit import is now started, feeding audit messages from: {0}", InputAddress);
+                Logger.Info($"Audit import is now started, feeding audit messages from: {InputAddress}");
             }
         }
-
-        bool TerminateIfForwardingIsEnabledButQueueNotWritable()
-        {
-            if (!Settings.ForwardAuditMessages)
-            {
-                return false;
-            }
-
-            try
-            {
-                //Send a message to test the forwarding queue
-                var testMessage = new TransportMessage(Guid.Empty.ToString("N"), new Dictionary<string, string>());
-                Forwarder.Send(testMessage, new SendOptions(Settings.AuditLogQueue));
-                return false;
-            }
-            catch (Exception messageForwardingException)
-            {
-                CriticalError.Raise("Audit Import cannot start", messageForwardingException);
-                return true;
-            }
-        }
-
 
         public void Stop()
         {
         }
 
-        public Address InputAddress => Settings.AuditQueue;
+        public Address InputAddress => settings.AuditQueue;
 
         public bool Disabled => false;
 
         public Action<TransportReceiver> GetReceiverCustomization()
         {
-            satelliteImportFailuresHandler = new SatelliteImportFailuresHandler(Builder.Build<IDocumentStore>(),
-                Path.Combine(LoggingSettings.LogPath, @"FailedImports\Audit"), tm => new FailedAuditImport
+            satelliteImportFailuresHandler = new SatelliteImportFailuresHandler(builder.Build<IDocumentStore>(),
+                Path.Combine(loggingSettings.LogPath, @"FailedImports\Audit"), tm => new FailedAuditImport
                 {
-                    Message = tm,
+                    Message = tm
                 },
-                CriticalError);
+                criticalError);
 
             return receiver => { receiver.FailureManager = satelliteImportFailuresHandler; };
         }
@@ -131,8 +81,58 @@
             satelliteImportFailuresHandler?.Dispose();
         }
 
-        SatelliteImportFailuresHandler satelliteImportFailuresHandler;
+        private void InnerHandle(TransportMessage message)
+        {
+            var entity = ConvertToSaveMessage(message);
+            using (var session = store.OpenSession())
+            {
+                session.Store(entity);
+                session.SaveChanges();
+            }
 
-        static readonly ILog Logger = LogManager.GetLogger(typeof(AuditQueueImport));
+            if (settings.ForwardAuditMessages)
+            {
+                TransportMessageCleaner.CleanForForwarding(message);
+                forwarder.Send(message, new SendOptions(settings.AuditLogQueue));
+            }
+        }
+
+        private ProcessedMessage ConvertToSaveMessage(TransportMessage message)
+        {
+            var receivedMessage = new ImportSuccessfullyProcessedMessage(message);
+
+            foreach (var enricher in enrichers)
+            {
+                enricher.Enrich(receivedMessage);
+            }
+
+            var auditMessage = new ProcessedMessage(receivedMessage)
+            {
+                // We do this so Raven does not spend time assigning a hilo key
+                Id = $"ProcessedMessages/{Guid.NewGuid()}"
+            };
+            return auditMessage;
+        }
+
+        private bool TerminateIfForwardingIsEnabledButQueueNotWritable()
+        {
+            if (!settings.ForwardAuditMessages)
+            {
+                return false;
+            }
+
+            try
+            {
+                //Send a message to test the forwarding queue
+                var testMessage = new TransportMessage(Guid.Empty.ToString("N"), new Dictionary<string, string>());
+                forwarder.Send(testMessage, new SendOptions(settings.AuditLogQueue));
+                return false;
+            }
+            catch (Exception messageForwardingException)
+            {
+                criticalError.Raise("Audit Import cannot start", messageForwardingException);
+                return true;
+            }
+        }
     }
 }
