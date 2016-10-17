30118daf1ea731ba66f825a6ebab3d7a13a2cc50
Greg Bielleman <greg.bielleman@particular.net>
2016-10-17 15:30:40 +1100

    Make unattended error message the same as UI message

--- src/ServiceControlInstaller.Engine/Unattended/UnattendInstaller.cs
+++ src/ServiceControlInstaller.Engine/Unattended/UnattendInstaller.cs
@@ -262,7 +262,7 @@ namespace ServiceControlInstaller.Engine.Unattended
             {
                 if (license.Details.ReleaseNotCoveredByMaintenance(releaseDate))
                 {
-                    return new CheckLicenseResult(false, "This version was released afer the upgrade protection expiration date specified in the license");
+                    return new CheckLicenseResult(false, "License does not cover this release of ServiceControl.Upgrade protection expired");
                 }
             }
             else
