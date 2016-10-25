91fb6cedcb835cd5f4a6950f8b1d4e52aae15dc0
John Simons <john.simons@nservicebus.com>
2016-10-25 12:08:19 +1000

    Using patch api in error importer

--- src/ServiceControl/Infrastructure/DeterministicGuid.cs
+++ src/ServiceControl/Infrastructure/DeterministicGuid.cs
@@ -1,26 +1,56 @@
 ﻿namespace ServiceControl.Infrastructure
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Security.Cryptography;
     using System.Text;
 
     public static class DeterministicGuid
     {
-        public static Guid MakeId(params string[] data)
+        static ConcurrentDictionary<string, Guid> cacheData = new ConcurrentDictionary<string, Guid>();
+
+        public static Guid MakeId(string data, bool cache = true)
         {
-            return DeterministicGuidBuilder(String.Concat(data));
+            return DeterministicGuidBuilder(data, cache);
         }
 
-        static Guid DeterministicGuidBuilder(string input)
+        public static Guid MakeId(string data1, string data2, bool cache = true)
         {
+            return DeterministicGuidBuilder($"{data1}{data2}", cache);
+        }
+
+        public static Guid MakeId(string data1, string data2, string data3, bool cache = true)
+        {
+            return DeterministicGuidBuilder($"{data1}{data2}{data3}", cache);
+        }
+
+        private static Guid DeterministicGuidBuilder(string input, bool cache)
+        {
+            Guid g;
+
+            if (cache)
+            {
+                if (cacheData.TryGetValue(input, out g))
+                {
+                    return g;
+                }
+            }
+
             // use MD5 hash to get a 16-byte hash of the string
             using (var provider = new MD5CryptoServiceProvider())
             {
                 var inputBytes = Encoding.Default.GetBytes(input);
                 var hashBytes = provider.ComputeHash(inputBytes);
                 // generate a guid from the hash:
-                return new Guid(hashBytes);
+                g = new Guid(hashBytes);
             }
+
+            if (cache)
+            {
+                cacheData.TryAdd(input, g);
+            }
+
+            return g;
         }
     }
 }
