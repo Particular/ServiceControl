98f8c339f45cce7a7e4825f5817234fc249c28ee
John Simons <john.simons@nservicebus.com>
2016-10-27 13:11:39 +1000

    CachedRavenDBPersistence implementation

---
+++ src/ServiceControl/Infrastructure/RavenDB/Subscriptions/CachedRavenDBPersistence.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Infrastructure.RavenDB.Subscriptions
+{
+    using NServiceBus.Features;
+    using NServiceBus.Persistence;
+
+    public class CachedRavenDBPersistence : PersistenceDefinition
+    {
+        public CachedRavenDBPersistence()
+        {
+            Supports<StorageType.Subscriptions>(s => s.EnableFeatureByDefault<SubscriptionStorage>());
+        }
+    }
+}
