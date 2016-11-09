dc5da83e56a72b9d9a2812c72c716a6bd1052e16
SzymonPobiega <szymon.pobiega@gmail.com>
2016-11-09 11:44:57 +0100

    Fixed inspections.

--- src/ServiceControl.AcceptanceTests/HeartbeatMonitoring/When_an_unmonitored_endpoint_starts_sending_heartbeats.cs
+++ src/ServiceControl.AcceptanceTests/HeartbeatMonitoring/When_an_unmonitored_endpoint_starts_sending_heartbeats.cs
@@ -1,6 +1,5 @@
 ﻿namespace ServiceBus.Management.AcceptanceTests.HeartbeatMonitoring
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
