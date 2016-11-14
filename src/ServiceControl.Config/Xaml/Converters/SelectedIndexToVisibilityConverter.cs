f5c8520763543990dea6b94c81d383165f0345f4
Greg Bielleman <greg.bielleman@particular.net>
2016-11-14 15:06:06 +1100

    Implemented "Please Select..." on FormComboBox  and split converters into multiple files

---
+++ src/ServiceControl.Config/Xaml/Converters/SelectedIndexToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceControl.Config.Xaml.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class SelectedIndexToVisibilityConverter : System.Windows.Markup.MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var i = System.Convert.ToInt32(value);
+            return i > -1 ? Visibility.Hidden : Visibility.Visible;
+
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
