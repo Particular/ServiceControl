a2c37f736155e1b2a1baec2b2cd60f9e72341177
Mike Minutillo <michael.minutillo@gmail.com>
2016-11-15 15:25:49 +0800

    [WIP] Migrations (#851)
    * Introduce the concept of migrations
    
    * Detect and split FailedMessage documents that represent more than one processing attempt
    
    * Remove original failed document if there are no processing attempts left in it.
    
    * Add unit tests for migration

---
+++ src/ServiceControl/DbMigrations/MigrationsManager.cs
@@ -0,0 +1,66 @@
+﻿namespace Particular.ServiceControl.DbMigrations
+{
+    using System;
+    using System.Linq;
+    using NServiceBus.Logging;
+    using Raven.Client;
+
+    public class MigrationsManager
+    {
+        private IDocumentStore store;
+        private IMigration[] migrations;
+
+        public MigrationsManager(IDocumentStore store, IMigration[] migrations)
+        {
+            this.store = store;
+            this.migrations = migrations;
+        }
+
+        public void ApplyMigrations()
+        {
+            var migrationsRecord = LoadMigrationsRecord();
+
+            var appliedMigrations = migrationsRecord.AppliedMigrations.Select(m => m.MigrationId).ToList();
+            var migrationsToApply = migrations.Where(m => appliedMigrations.Contains(m.MigrationId) == false).ToArray();
+
+            foreach (var migration in migrationsToApply)
+            {
+                try
+                {
+                    log.Info($"Applying migration [{migration.MigrationId}]");
+                    using (var session = store.OpenSession())
+                    {
+                        migration.Apply(session);
+
+                        session.Load<Migrations>(Migrations.DocumentId).Add(migration.MigrationId);
+
+                        session.SaveChanges();
+                    }
+                    log.Info($"Applying migration [{migration.MigrationId}] complete");
+                }
+                catch (Exception ex)
+                {
+                    log.Fatal($"Error while applying migration [{migration.MigrationId}]", ex);
+                    throw;
+                }
+            }
+        }
+
+        private Migrations LoadMigrationsRecord()
+        {
+            using (var session = store.OpenSession())
+            {
+                var document = session.Load<Migrations>(Migrations.DocumentId);
+                if (document == null)
+                {
+                    document = new Migrations();
+                    session.Store(document, Migrations.DocumentId);
+                    session.SaveChanges();
+                }
+                return document;
+            }
+        }
+
+        private static ILog log = LogManager.GetLogger<MigrationsManager>();
+    }
+}
---
+++ src/ServiceControl/DbMigrations/MigrationsModule.cs
@@ -0,0 +1,14 @@
+﻿namespace Particular.ServiceControl.DbMigrations
+{
+    using Autofac;
+
+    public class MigrationsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<MigrationsManager>().SingleInstance();
+            builder.RegisterAssemblyTypes(ThisAssembly).As<IMigration>();
+        }
+    }
+}
