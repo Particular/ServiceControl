039b821c969ce5ab3c003cf08528bf1e1d2f3ed8
Brad M. Miller <brad@bmmiller.com>
2016-12-30 14:41:05 -0500

    Added class FailedMessageArchivedPublisher to allow external ServiceControl.Contracts.MessageFailed events with a Status of MessageStatus.ArchivedFailure to be published.

---
+++ src/ServiceControl/ExternalIntegrations/FailedMessageArchivedPublisher.cs
@@ -0,0 +1,29 @@
+﻿namespace ServiceControl.ExternalIntegrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts.MessageFailures;
+    using Raven.Client;
+
+    public class FailedMessageArchivedPublisher : EventPublisher<FailedMessageArchived, MessageFailedPublisher.DispatchContext>
+    {
+        protected override MessageFailedPublisher.DispatchContext CreateDispatchRequest(FailedMessageArchived @event)
+        {
+            return new MessageFailedPublisher.DispatchContext
+            {
+                FailedMessageId = new Guid(@event.FailedMessageId)
+            };
+        }
+
+        protected override IEnumerable<object> PublishEvents(IEnumerable<MessageFailedPublisher.DispatchContext> contexts, IDocumentSession session)
+        {
+            // FailedMessageArchived events are published externally as ServiceControl.Contracts.MessageFailed events
+            // with the Status property set to MessageStatus.ArchivedFailure. This is handled by the PublishEvents
+            // method in the MessageFailedPublisher class. Returning an empty collection here to avoid multiple 
+            // external events from being published.
+
+            return Enumerable.Empty<object>();
+        }
+    }
+}
