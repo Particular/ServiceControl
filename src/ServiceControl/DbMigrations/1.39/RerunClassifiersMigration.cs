4243bb454cbefdf2b3e60e23e72b4a1335b72108
WilliamBZA <william.brander@gmail.com>
2017-03-30 11:14:23 +0200

    Rerun error classifications

---
+++ src/ServiceControl/DbMigrations/1.39/RerunClassifiersMigration.cs
@@ -0,0 +1,33 @@
+﻿namespace Particular.ServiceControl.DbMigrations
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using global::ServiceControl;
+    using global::ServiceControl.Infrastructure;
+    using global::ServiceControl.MessageFailures;
+    using global::ServiceControl.Recoverability;
+    using Raven.Client;
+    using Raven.Client.Document;
+
+    public class RerunClassifiersMigration : IMigration
+    {
+        public string Apply(IDocumentStore store)
+        {
+            using (var session = store.OpenSession())
+            {
+                var reclassifySettings = session.Load<ReclassifyErrorSettings>(ReclassifyErrorSettings.IdentifierCase);
+
+                if (reclassifySettings != null)
+                {
+                    reclassifySettings.ReclassificationDone = false;
+                }
+
+                session.SaveChanges();
+            }
+
+            return "Reclassification settings updated.";
+        }
+
+        public string MigrationId { get; } = "Rerun error classifiers";
+    }
+}
