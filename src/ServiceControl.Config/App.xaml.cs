3e4fd44a4774e973a22dae40ddf60b1615825b85
Greg Bielleman <greg.bielleman@particular.net>
2017-04-03 11:42:35 +1000

    Remove App.config in mgmt util

--- src/ServiceControl.Config/App.xaml.cs
+++ src/ServiceControl.Config/App.xaml.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
     using ServiceControl.Config.UI.MessageBox;
 
     public partial class App
@@ -17,12 +19,29 @@
         [STAThread]
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += (s, e) => ResolveAssembly(e.Name);
+
             Splash.Show();
             if (args.Length == 0)
             {
                 var app = new App();
                 app.Run();
             }
+        }
+
+        static Assembly ResolveAssembly(string name)
+        {
+            var assemblyLocation = Assembly.GetEntryAssembly().Location;
+            var appDirectory = Path.GetDirectoryName(assemblyLocation);
+            var requestingName = new AssemblyName(name).Name;
+
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var combine = Path.Combine(appDirectory, requestingName + ".dll");
+            if (!File.Exists(combine))
+            {
+                return null;
+            }
+            return Assembly.LoadFrom(combine);
         }
     }
 }
