cfefc45659a12fe0136e5ae19ab82a86e315ea39
WilliamBZA <william.brander@gmail.com>
2017-04-04 12:09:23 +0200

    Acknowledge Retries as well as Archive operations

--- src/ServiceControl/Recoverability/API/UnacknowledgedGroupsApi.cs
+++ src/ServiceControl/Recoverability/API/UnacknowledgedGroupsApi.cs
@@ -13,11 +13,31 @@ namespace ServiceControl.Recoverability
         private dynamic AcknowledgeOperation(dynamic parameters)
         {
             var groupId = parameters.groupId;
+            if (ArchiveOperationManager.IsArchiveInProgressFor(groupId))
+            {
+                ArchiveOperationManager.DismissArchiveOperation(groupId, ArchiveType.FailureGroup);
+                return HttpStatusCode.OK;
+            }
+           
+            using (var session = Store.OpenSession())
+            {
+                var retryHistory = session.Load<RetryHistory>(RetryHistory.MakeId());
+                if (retryHistory != null)
+                {
+                    if (retryHistory.Acknowledge(groupId, RetryType.FailureGroup))
+                    {
+                        session.Store(retryHistory);
+                        session.SaveChanges();
 
-            ArchiveOperationManager.DismissArchiveOperation(groupId, ArchiveType.FailureGroup);
+                        return HttpStatusCode.OK;
+                    }
+                }
+            }
 
-            return HttpStatusCode.OK;
+            return HttpStatusCode.NotFound;
         }
+
 		public ArchivingManager ArchiveOperationManager { get; set; }
+		public RetryingManager RetryingOperationManager { get; set; }
     }
 }
