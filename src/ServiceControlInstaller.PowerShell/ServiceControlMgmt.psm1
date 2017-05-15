61a00899629562d3a7b204e358556c0e617c083b
Greg Bielleman <greg.bielleman@particular.net>
2017-05-15 10:56:04 +1000

    Make PS cmdlet names consisent

--- src/ServiceControlInstaller.PowerShell/ServiceControlMgmt.psm1
+++ src/ServiceControlInstaller.PowerShell/ServiceControlMgmt.psm1
@@ -18,29 +18,24 @@ if (!$requiredDLL) {
     throw "This module was imported incorrectly - use 'Import-Module .\ServiceControlMgmt.psd1' to load this module"  
 }
 
-New-Alias    -Value Get-ServiceControlInstances -Name sc-instances 
-New-Alias    -Value Invoke-ServiceControlInstanceUpgrade -Name  sc-upgrade 
-New-Alias    -Value Remove-ServiceControlInstance -Name  sc-delete 
-New-Alias    -Value New-ServiceControlInstance -Name  sc-add 
+New-Alias -Value Get-ServiceControlInstances -Name sc-instances 
+New-Alias -Value Invoke-ServiceControlInstanceUpgrade -Name  sc-upgrade 
+New-Alias -Value Remove-ServiceControlInstance -Name  sc-delete 
+New-Alias -Value New-ServiceControlInstance -Name  sc-add 
+New-Alias -Value Get-MonitoringInstances -Name mon-instances 
+New-Alias -Value Invoke-MonitoringinstanceUpgrade -Name  mon-upgrade 
+New-Alias -Value Remove-MonitoringInstance -Name  mon-delete 
+New-Alias -Value New-MonitoringInstance -Name  mon-add 
+New-Alias -Value Get-ServiceControlLicense -Name  sc-findlicense 
+New-Alias -Value Import-ServiceControlLicense -Name  sc-addlicense 
+New-Alias -Value Test-IfPortIsAvailable -Name  port-check
+New-Alias -Value Get-SecurityIdentifier -Name  user-sid
+New-Alias -Value Get-UrlAcls -Name  urlacl-list 
+New-Alias -Value Add-UrlAcl -Name  urlacl-add 
+New-Alias -Value Remove-UrlAcl -Name  urlacl-delete
+New-Alias -Value Get-ServiceControlTransportTypes -Name  sc-transportsinfo
+New-Alias -Value Get-ServiceControlMgmtCommands -Name  sc-help
 
-New-Alias    -Value Get-MonitoringInstances -Name mon-instances 
-New-Alias    -Value Invoke-MonitoringUpgrade -Name  mon-upgrade 
-New-Alias    -Value Remove-MonitoringInstance -Name  mon-delete 
-New-Alias    -Value New-MonitoringInstance -Name  mon-add 
-
-
-New-Alias    -Value Get-ServiceControlLicense -Name  sc-findlicense 
-New-Alias    -Value Import-ServiceControlLicense -Name  sc-addlicense 
-
-New-Alias    -Value Test-IfPortIsAvailable -Name  port-check
-New-Alias    -Value Get-SecurityIdentifier -Name  user-sid
-New-Alias    -Value Get-UrlAcls -Name  urlacl-list 
-New-Alias    -Value Add-UrlAcl -Name  urlacl-add 
-New-Alias    -Value Remove-UrlAcl -Name  urlacl-delete
-
-New-Alias    -Value Get-ServiceControlTransportTypes -Name  sc-transportsinfo
-
-New-Alias    -Value Get-ServiceControlMgmtCommands -Name  sc-help
 Export-ModuleMember * -Alias * 
 
 Write-Verbose "Use SC-help to list ServiceControl Management commands" -Verbose
