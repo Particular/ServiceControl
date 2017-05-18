9da45ca279b1a02698e2578add7a8230cb8c59eb
Greg Bielleman <greg.bielleman@particular.net>
2017-05-18 10:38:42 +1000

    Cleanup

--- src/ServiceControl.Config/UI/SharedInstanceEditor/SharedMontioringEditorViewModelValidator.cs
+++ src/ServiceControl.Config/UI/SharedInstanceEditor/SharedMontioringEditorViewModelValidator.cs
@@ -1,6 +1,5 @@
 namespace ServiceControl.Config.Validation
 {
-    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
@@ -27,23 +26,6 @@ namespace ServiceControl.Config.Validation
         }
 
         // We need this to ignore the instance that represents the edit screen
-        protected List<string> UsedQueueNames(TransportInfo transportInfo = null, string instanceName = null, string connectionString = null)
-        {
-            var transport = (transportInfo == null) ? string.Empty : transportInfo.Name;
-            var instancesByTransport = MonitoringInstances.Where(p => (p.TransportPackage.Equals(transport, StringComparison.OrdinalIgnoreCase)) &&
-                            (string.Equals(p.ConnectionString, connectionString, StringComparison.OrdinalIgnoreCase))).ToList();
-
-            return instancesByTransport
-                .Where(p => string.IsNullOrWhiteSpace(instanceName) || p.Name != instanceName)
-                .SelectMany(p => new[]
-                {
-                    p.ErrorQueue,
-                })
-                .Distinct()
-                .ToList();
-        }
-
-        // We need this to ignore the instance that represents the edit screen
         protected List<string> UsedPorts(string instanceName = null)
         {
             return MonitoringInstances
@@ -65,12 +47,7 @@ namespace ServiceControl.Config.Validation
             RuleFor(x => x.HostName)
                 .NotEmpty().When(x => x.SubmitAttempted);
 
-            RuleFor(x => x.PortNumber)
-                .NotEmpty()
-                .ValidPort()
-                .MustNotBeIn(x => UsedPorts(x.InstanceName))
-                .WithMessage(Validations.MSG_MUST_BE_UNIQUE, "Ports")
-                .When(x => x.SubmitAttempted);
+           
 
             RuleFor(x => x.LogPath)
                 .NotEmpty()
