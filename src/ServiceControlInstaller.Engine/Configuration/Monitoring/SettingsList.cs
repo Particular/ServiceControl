6ae8b5352ea5e97de722bae9072757cc99690130
Tomasz Masternak <tomasz.masternak@particular.net>
2017-08-23 08:26:12 +0200

    SC.Monitoring port setting name fix and update to newest alpha (#1000)
    * fixing sc.monitoring port setting name
    
    * updating sc.monitoring.packaging to the newest alpha

--- src/ServiceControlInstaller.Engine/Configuration/Monitoring/SettingsList.cs
+++ src/ServiceControlInstaller.Engine/Configuration/Monitoring/SettingsList.cs
@@ -4,7 +4,7 @@ namespace ServiceControlInstaller.Engine.Configuration.Monitoring
     {
         //TODO : FIX UP Names
 
-        public static SettingInfo Port = new SettingInfo { Name =  "Monitoring/Port" };
+        public static SettingInfo Port = new SettingInfo { Name =  "Monitoring/HttpPort" };
         public static SettingInfo HostName = new SettingInfo { Name = "Monitoring/HostName" };
         public static SettingInfo LogPath = new SettingInfo { Name = "Monitoring/LogPath" };
         public static SettingInfo TransportType = new SettingInfo { Name = "Monitoring/TransportType" };
