5a54aa89c7e388f1149590924b9986ad05714218
Mike Minutillo <michael.minutillo@gmail.com>
2017-08-24 14:20:03 +0800

    Fix setting name

--- src/ServiceControlInstaller.Engine/Configuration/Monitoring/SettingsList.cs
+++ src/ServiceControlInstaller.Engine/Configuration/Monitoring/SettingsList.cs
@@ -5,7 +5,7 @@ namespace ServiceControlInstaller.Engine.Configuration.Monitoring
         //TODO : FIX UP Names
 
         public static SettingInfo Port = new SettingInfo { Name =  "Monitoring/HttpPort" };
-        public static SettingInfo HostName = new SettingInfo { Name = "Monitoring/HostName" };
+        public static SettingInfo HostName = new SettingInfo { Name = "Monitoring/HttpHostName" };
         public static SettingInfo LogPath = new SettingInfo { Name = "Monitoring/LogPath" };
         public static SettingInfo TransportType = new SettingInfo { Name = "Monitoring/TransportType" };
         public static SettingInfo ErrorQueue = new SettingInfo { Name = "Monitoring/ErrorQueue" };
