47a09faa50f1b5852bae745a81942a67ec3f8c0d
Bob Langley <bob.langley@particular.net>
2017-10-05 09:35:28 -0700

    Create learning transport acceptance test project

---
+++ src/ServiceControl.LearningTransport.AcceptanceTests/App_Packages/NSB.AcceptanceTests.5.2.24/Encryption/When_using_Rijndael_with_multikey.cs
@@ -0,0 +1,99 @@
+﻿namespace NServiceBus.AcceptanceTests.Encryption
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_using_Rijndael_with_multikey : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_receive_decrypted_message()
+        {
+            Scenario.Define<Context>()
+                    .WithEndpoint<Sender>(b => b.Given((bus, context) =>
+                     {
+                         bus.Send(new MessageWithSecretData
+                         {
+                             Secret = "betcha can't guess my secret",
+                         });
+                         bus.Send(new RegularMessage());
+                     }))
+                    .WithEndpoint<Receiver>()
+                    .Done(c => c.Done)
+                    .Repeat(r => r.For(Transports.Default))
+                    .Should(c =>
+                    {
+                        Assert.AreEqual("betcha can't guess my secret", c.Secret);
+                        Assert.IsFalse(c.HasKeyOnRegularMessage.Value, "Key identifier header present in message without encrypted properties.");
+                    })
+                    .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool Done { get; set; }
+            public string Secret { get; set; }
+            public bool? HasKeyOnRegularMessage { get; set; }
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(builder => builder.RijndaelEncryptionService("1st", Encoding.ASCII.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6")))
+                    .AddMapping<MessageWithSecretData>(typeof(Receiver))
+                    .AddMapping<RegularMessage>(typeof(Receiver))
+                    ;
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                var key = Encoding.ASCII.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6");
+                var keys = new Dictionary<string, byte[]>
+                {
+                    {"2nd", Encoding.ASCII.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6") },
+                    {"1st", key  }
+                };
+
+                var expiredKeys = new[] { key };
+                EndpointSetup<DefaultServer>(builder => builder.RijndaelEncryptionService("2nd", keys, expiredKeys));
+            }
+
+            public class Handler : IHandleMessages<MessageWithSecretData>, IHandleMessages<RegularMessage>
+            {
+                public Context Context { get; set; }
+                public IBus Bus { get; set; }
+
+                public void Handle(MessageWithSecretData message)
+                {
+                    Context.Secret = message.Secret.Value;
+                    Context.Done = true;
+                }
+
+                public void Handle(RegularMessage message)
+                {
+                    var hasKey = null != Bus.GetMessageHeader(message, Headers.RijndaelKeyIdentifier);
+                    Context.HasKeyOnRegularMessage = hasKey;
+                }
+            }
+        }
+
+        [Serializable]
+        public class MessageWithSecretData : IMessage
+        {
+            public WireEncryptedString Secret { get; set; }
+        }
+
+        [Serializable]
+        public class RegularMessage : IMessage
+        {
+        }
+    }
+}
---
+++ src/ServiceControl.LearningTransport.AcceptanceTests/App_Packages/NSB.AcceptanceTests.5.2.24/Encryption/When_using_Rijndael_without_incoming_key_identifier.cs
@@ -0,0 +1,90 @@
+﻿namespace NServiceBus.AcceptanceTests.Encryption
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NServiceBus.MessageMutator;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_using_Rijndael_without_incoming_key_identifier : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_process_decrypted_message_without_key_identifier()
+        {
+            Scenario.Define<Context>()
+                    .WithEndpoint<Sender>(b => b.Given((bus, context) => bus.Send(new MessageWithSecretData
+                        {
+                            Secret = "betcha can't guess my secret",
+                        })))
+                    .WithEndpoint<Receiver>()
+                    .Done(c => c.Done)
+                    .Repeat(r => r.For(Transports.Default))
+                    .Should(c => Assert.AreEqual("betcha can't guess my secret", c.Secret))
+                    .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool Done { get; set; }
+            public string Secret { get; set; }
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(builder => builder.RijndaelEncryptionService("will-be-removed-by-transport-mutator", Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")))
+                    .AddMapping<MessageWithSecretData>(typeof(Receiver));
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                var keys = new Dictionary<string, byte[]>
+                {
+                    {"new", Encoding.ASCII.GetBytes("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb") },
+                };
+
+                var expiredKeys = new[] { Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") };
+                EndpointSetup<DefaultServer>(builder => builder.RijndaelEncryptionService("new", keys, expiredKeys));
+
+            }
+
+            public class Handler : IHandleMessages<MessageWithSecretData>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(MessageWithSecretData message)
+                {
+                    Context.Secret = message.Secret.Value;
+                    Context.Done = true;
+                }
+            }
+        }
+
+        [Serializable]
+        public class MessageWithSecretData : IMessage
+        {
+            public WireEncryptedString Secret { get; set; }
+        }
+
+
+        class RemoveKeyIdentifierHeaderMutator : IMutateIncomingTransportMessages, INeedInitialization
+        {
+            public void MutateIncoming(TransportMessage transportMessage)
+            {
+                transportMessage.Headers.Remove(Headers.RijndaelKeyIdentifier);
+            }
+
+            public void Customize(BusConfiguration configuration)
+            {
+                configuration.RegisterComponents(c => c.ConfigureComponent<RemoveKeyIdentifierHeaderMutator>(DependencyLifecycle.InstancePerCall));
+            }
+        }
+    }
+}
