3089a4b6294e111cd8fe88951482a388ffa50670
Hadi Eskandari <h.eskandari@gmail.com>
2017-10-11 19:18:34 +1100

    New overlay for the add new instance popup

---
+++ src/ServiceControl.Config/UI/Shell/NewInstanceOverlay.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ServiceControl.Config.UI.Shell
+{
+	using System;
+
+	/// <summary>
+	/// Interaction logic for NewInstanceOverlay.xaml
+	/// </summary>
+	public partial class NewInstanceOverlay
+	{
+		public NewInstanceOverlay()
+		{
+			InitializeComponent();
+		}
+
+		private void ViewPreviewMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Console.WriteLine(sender.GetType().Name);
+		}
+	}
+}
