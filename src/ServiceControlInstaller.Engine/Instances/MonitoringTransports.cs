2f1b460524f1e1288d000c61348d15ea67ab0322
Mike Minutillo <michael.minutillo@gmail.com>
2017-11-17 17:07:08 +0800

    Split transports by instance type

---
+++ src/ServiceControlInstaller.Engine/Instances/MonitoringTransports.cs
@@ -0,0 +1,57 @@
+﻿namespace ServiceControlInstaller.Engine.Instances
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MonitoringTransports
+    {
+        public static TransportInfo FindByName(string name)
+        {
+            return All.FirstOrDefault(p => string.Equals(p.Name, name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static List<TransportInfo> All => new List<TransportInfo>
+        {
+            new TransportInfo
+            {
+                Name = "AzureServiceBus",
+                TypeName = "NServiceBus.AzureServiceBusTransport, NServiceBus.Azure.Transports.WindowsAzureServiceBus",
+                MatchOn = "NServiceBus.AzureServiceBus",
+                SampleConnectionString = "Endpoint=sb://[namespace].servicebus.windows.net; SharedSecretIssuer=<owner>;SharedSecretValue=<someSecret>"
+
+            },
+            new TransportInfo
+            {
+                Name = "AzureStorageQueue",
+                TypeName = "NServiceBus.AzureStorageQueueTransport, NServiceBus.Azure.Transports.WindowsAzureStorageQueues",
+                MatchOn  = "NServiceBus.AzureStorageQueue",
+                SampleConnectionString = "DefaultEndpointsProtocol=[http|https];AccountName=<MyAccountName>;AccountKey=<MyAccountKey>"
+            },
+            new TransportInfo
+            {
+                Name = "MSMQ",
+                TypeName = "NServiceBus.MsmqTransport, NServiceBus.Core",
+                MatchOn = "NServiceBus.Msmq",
+                SampleConnectionString = string.Empty,
+                Default = true
+            },
+            new TransportInfo
+            {
+                Name = "SQLServer",
+                TypeName = "NServiceBus.SqlServerTransport, NServiceBus.Transports.SQLServer",
+                MatchOn = "NServiceBus.SqlServer",
+                SampleConnectionString = "Data Source=<SQLInstance>;Initial Catalog=nservicebus;Integrated Security=True",
+                Help = "When integrated authentication is specified in the SQL connection string the the current installing user is used to create the required SQL tables structure not the service account."
+            },
+            new TransportInfo
+            {
+                Name = "RabbitMQ",
+                TypeName = "NServiceBus.RabbitMQTransport, NServiceBus.Transports.RabbitMQ",
+                MatchOn = "NServiceBus.RabbitMQ",
+                SampleConnectionString = "host=<HOSTNAME>;username=<USERNAME>;password=<PASSWORD>"
+            }
+        };
+
+    }
+}
