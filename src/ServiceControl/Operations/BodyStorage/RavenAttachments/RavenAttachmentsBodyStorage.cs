bceaa92d3de4c9f17f3cdfa869883bc05a31a1e2
SzymonPobiega <szymon.pobiega@gmail.com>
2017-12-08 08:32:56 +0100

    Preallocated locks

--- src/ServiceControl/Operations/BodyStorage/RavenAttachments/RavenAttachmentsBodyStorage.cs
+++ src/ServiceControl/Operations/BodyStorage/RavenAttachments/RavenAttachmentsBodyStorage.cs
@@ -1,6 +1,8 @@
 ﻿namespace ServiceControl.Operations.BodyStorage.RavenAttachments
 {
+    using System;
     using System.IO;
+    using System.Linq;
     using Raven.Client;
     using Raven.Json.Linq;
 
@@ -8,15 +10,29 @@
     {
         public IDocumentStore DocumentStore { get; set; }
 
+        public RavenAttachmentsBodyStorage()
+        {
+            locks = Enumerable.Range(0, 42).Select(i => new object()).ToArray(); //because 42 is the answer
+        }
+
         public string Store(string bodyId, string contentType, int bodySize, Stream bodyStream)
         {
-            DocumentStore.DatabaseCommands.PutAttachment("messagebodies/" + bodyId, null, bodyStream, new RavenJObject
-            {
-                {"ContentType", contentType},
-                {"ContentLength", bodySize}
-            });
+            /*
+             * The locking here is a workaround for RavenDB bug DocumentDatabase.PutStatic that allows multiple threads to enter a critical section.
+             */
+            var idHash = Math.Abs(bodyId.GetHashCode());
+            var lockIndex = idHash % locks.Length; //I think using bit manipulation is not worth the effort
 
-            return $"/messages/{bodyId}/body";
+            lock (locks[lockIndex])
+            {
+                DocumentStore.DatabaseCommands.PutAttachment("messagebodies/" + bodyId, null, bodyStream, new RavenJObject
+                {
+                    {"ContentType", contentType},
+                    {"ContentLength", bodySize}
+                });
+
+                return $"/messages/{bodyId}/body";
+            }
         }
 
         public bool TryFetch(string bodyId, out Stream stream)
@@ -32,5 +48,7 @@
             stream = attachment.Data();
             return true;
         }
+
+        object[] locks;
     }
 }
