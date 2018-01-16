1155db81e1264b0ebe92a5812dbbcbfd7baab57f
Bob Langley <bob.langley@particular.net>
2018-01-16 14:57:12 -0800

    Simplify GetMessages to page without tracking offsets

--- src/ServiceControl/CompositeViews/Messages/GetMessages.cs
+++ src/ServiceControl/CompositeViews/Messages/GetMessages.cs
@@ -19,34 +19,17 @@ namespace ServiceControl.CompositeViews.Messages
     {
         static JsonSerializer jsonSerializer = JsonSerializer.Create(JsonNetSerializer.CreateDefault());
         static HttpClient httpClient = new HttpClient();
-        static string[] secondaries = {"http://localhost:33334/api"};
+        static string[] remotes = {"http://localhost:33334/api"};
 
         public GetMessages()
         {
             Get["/messages", true] = async (parameters, token) =>
             {
-                var queryString = Request.Query;
-
-                var pageSize = 50; //SC default value
-                if (queryString.per_page.HasValue)
-                {
-                    pageSize = int.Parse(queryString.per_page);
-                }
-                var offsetValues = new int[secondaries.Length + 1];
-                if (queryString.offset.HasValue)
-                {
-                    string offset = queryString.offset;
-                    string[] offsetsSplit = offset.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                    offsetValues = offsetsSplit.Select(int.Parse).ToArray();
-                    var pageNumber = offsetValues[0] / pageSize + 1;
-                    queryString.page = pageNumber;
-                }
-
-                MessagesView[] masterResults;
+                MessagesView[] localMessages;
                 RavenQueryStatistics stats;
                 using (var session = Store.OpenSession())
                 {
-                    masterResults = session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
+                    localMessages = session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
                         .IncludeSystemMessagesWhere(Request)
                         .Statistics(out stats)
                         .Sort(Request)
@@ -55,42 +38,28 @@ namespace ServiceControl.CompositeViews.Messages
                         .ToArray();
                 }
 
-                if (secondaries.Length == 0)
+                // Return if no secondaries
+                if (remotes.Length == 0)
                 {
-                    return Negotiate.WithModel(masterResults)
+                    return Negotiate.WithModel(localMessages)
                         .WithPagingLinksAndTotalCount(stats, Request)
                         .WithEtagAndLastModified(stats);
                 }
 
-                var browseRight = offsetValues[0] % pageSize == 0;
+                var queryResults = await DistributeQuery(Request);
 
-                string sortBy = queryString.sort_by.HasValue
-                    ? queryString.sort_by
-                    : "time_sent";
-
-                string sortOrder = queryString.sort_order.HasValue
-                    ? queryString.sort_order
-                    : "desc";
-
-                var slaveOffsets = offsetValues.Skip(1).ToArray();
-                var slaveResults = await DistributeQuery(pageSize, sortBy, sortOrder, slaveOffsets, Request.Path);
-
-                var allResult = slaveResults.ToList();
-                allResult.Insert(0, new PartialQueryResult
+                queryResults.Insert(0, new PartialQueryResult
                 {
-                    Messages = masterResults,
-                    StartOffset = offsetValues[0],
+                    Messages = localMessages,
                     TotalCount = stats.TotalResults
                 });
 
-                var aggregatedResults = AggregateResults(pageSize, sortBy, sortOrder, browseRight, allResult);
+                //Combine all the results
+                var aggregatedResults = AggregateResults(Request, queryResults.SelectMany(r => r.Messages));
 
-                var newOffsets = aggregatedResults.Item2;
-                var prevPageOffsets = !browseRight ? newOffsets : offsetValues.Select(x => x -1);
-                var nextPageOffsets = browseRight ? newOffsets : offsetValues.Select(x => x + 1);
-
-                return Negotiate.WithModel(aggregatedResults.Item1)
-                    .WithPagingLinksAndTotalCount(sortBy, sortOrder, pageSize, allResult.Select(x => x.TotalCount), prevPageOffsets, nextPageOffsets, Request);
+                //Return all the results
+                return Negotiate.WithModel(aggregatedResults)
+                    .WithPagingLinksAndTotalCount(queryResults.Sum(r => r.TotalCount), Request);
             };
 
 
@@ -120,61 +89,54 @@ namespace ServiceControl.CompositeViews.Messages
 
         class PartialQueryResult
         {
-            public int StartOffset { get; set; }
             public MessagesView[] Messages { get; set; }
             public int TotalCount { get; set; }
         }
 
-        async Task<PartialQueryResult[]> DistributeQuery(int pageSize, string sort, string direction, int[] offsetArray, string urlSuffix)
+        async Task<List<PartialQueryResult>> DistributeQuery(Request currentRequest)
         {
-            var queryTasks = secondaries.Zip(offsetArray, (url, offset) => new { url, offset })
-                .Select(x => new { x.offset, request = new HttpRequestMessage(HttpMethod.Get, BuildUrl(x.url, urlSuffix, x.offset, pageSize, sort, direction)) })
-                .Select(async x =>
+            var queryTasks = remotes
+                .Select(instanceUrl => new Uri($"{instanceUrl}{currentRequest.Path}?{currentRequest.Url.Query}"))
+                .Select(requestUri => new HttpRequestMessage(HttpMethod.Get, requestUri))
+                .Select(async request =>
                 {
-                    var rawResponse = await httpClient.SendAsync(x.request);
-                    var result = await ParseResult(rawResponse, x.offset);
-                    return result;
-                })
-                .ToArray();
+                    var rawResponse = await httpClient.SendAsync(request);
+                    return await ParseResult(rawResponse);
+                });
 
             var responses = await Task.WhenAll(queryTasks);
-            return responses;
+            return responses.ToList();
         }
 
-        static Uri BuildUrl(string prefix, string suffix, int offset, int pageSize, string sort, string direction)
+        static async Task<PartialQueryResult> ParseResult(HttpResponseMessage responseMessage)
         {
-            var pageNumber = offset / pageSize + 1;
-            var uri = new Uri($"{prefix}{suffix}?page={pageNumber}&per_page={pageSize}&direction={direction}&sort={sort}");
-            return uri;
-        }
-
-        static async Task<PartialQueryResult> ParseResult(HttpResponseMessage responseMessage, int index)
-        {
-            string body = await responseMessage.Content.ReadAsStringAsync();
-
             var responseStream = await responseMessage.Content.ReadAsStreamAsync();
-            var jsonReader = new Newtonsoft.Json.JsonTextReader(new StreamReader(responseStream));
+            var jsonReader = new JsonTextReader(new StreamReader(responseStream));
             var messages = jsonSerializer.Deserialize<MessagesView[]>(jsonReader);
-
-
 
             var totalCount = responseMessage.Headers.GetValues("Total-Count").Select(int.Parse).Cast<int?>().FirstOrDefault() ?? -1;
 
             return new PartialQueryResult
             {
                 Messages = messages,
-                TotalCount = totalCount,
-                StartOffset = index
+                TotalCount = totalCount
             };
         }
 
-        Tuple<MessagesView[], int[]> AggregateResults(int pageSize, string sort, string sortDirection, bool browseRight, List<PartialQueryResult> partialResults)
+        MessagesView[] AggregateResults(dynamic queryString, IEnumerable<MessagesView> combinedMessages)
         {
-            var heads = new MessagesView[partialResults.Count];
-            var localOffsets = partialResults.Select(x => x.StartOffset % pageSize).ToArray();
-            var newOffsets = partialResults.Select(x => x.StartOffset).ToArray();
+            //Set the default sort to time_sent if not already set
+            string sortBy = queryString.sort_by.HasValue
+                ? queryString.sort_by
+                : "time_sent";
+
+            //Set the default sort direction to `desc` if not already set
+            string sortOrder = queryString.sort_order.HasValue
+                ? queryString.sort_order
+                : "desc";
+
             Func<MessagesView, IComparable> keySelector = m => m.TimeSent;
-            switch (sort)
+            switch (sortBy)
             {
                 case "id":
                 case "message_id":
@@ -206,70 +168,9 @@ namespace ServiceControl.CompositeViews.Messages
                     break;
             }
 
-            var results = new List<MessagesView>();
-            for (var i = 0; i < pageSize; i++)
-            {
-                for (var headIndex = 0; headIndex < heads.Length; headIndex++)
-                {
-                    //Replenish
-                    if (heads[headIndex] == null)
-                    {
-                        if (localOffsets[headIndex] < partialResults[headIndex].Messages.Length && localOffsets[headIndex] >= 0)
-                        {
-                            var messageToken = partialResults[headIndex].Messages[localOffsets[headIndex]];
-                            heads[headIndex] = messageToken;
-                        }
-                    }
-                }
-                //Find one with smallest/largest value depending on sort direction
-                Func<int, bool> comparison;
-                if (sortDirection == "asc" && browseRight || sortDirection == "desc" && !browseRight)
-                {
-                    comparison = x => x < 0;
-                }
-                else
-                {
-                    comparison = x => x > 0;
-                }
-                var next = Min(heads, keySelector, comparison);
-                if (next < 0) //No more messages
-                {
-                    break;
-                }
-                results.Add(partialResults[next].Messages[localOffsets[next]]);
-
-                var offsetIncrement = browseRight ? 1 : -1;
-
-                localOffsets[next] += offsetIncrement;
-                newOffsets[next] += offsetIncrement;
-                heads[next] = null;
-            }
-
-            return Tuple.Create(results.ToArray(), newOffsets);
-        }
-
-        static int Min<T>(MessagesView[] messages, Func<MessagesView, T> sortOrder, Func<int, bool> comparison)
-            where T : IComparable
-        {
-            IComparable minValue = default(T);
-            var anyValue = false;
-            var minIndex = -1;
-
-            for (var i = 0; i < messages.Length; i++)
-            {
-                if (messages[i] == null)
-                {
-                    continue;
-                }
-                var value = sortOrder(messages[i]);
-                if (!anyValue || comparison(value.CompareTo(minValue)))
-                {
-                    minValue = value;
-                    minIndex = i;
-                }
-                anyValue = true;
-            }
-            return minIndex;
+            return sortOrder == "asc"
+                ? combinedMessages.OrderBy(keySelector).ToArray()
+                : combinedMessages.OrderByDescending(keySelector).ToArray();
         }
     }
 }
