d8e6833d064daa44e1e94f1a37987fe2071aee3a
Bob Langley <bob.langley@particular.net>
2018-01-23 10:48:18 -0800

    Subscribe to MessageFailureResolvedByRetry event published by remotes

--- src/ServiceControl/Infrastructure/SubscribeToOwnEvents.cs
+++ src/ServiceControl/Infrastructure/SubscribeToOwnEvents.cs
@@ -7,8 +7,10 @@ namespace ServiceControl.Infrastructure
     using NServiceBus;
     using NServiceBus.Settings;
     using NServiceBus.Transports;
+    using NServiceBus.Unicast;
     using NServiceBus.Unicast.Subscriptions;
     using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+    using ServiceControl.Contracts.MessageFailures;
 
     class SubscribeToOwnEvents
     {
@@ -17,13 +19,35 @@ namespace ServiceControl.Infrastructure
             var localAddress = Settings.LocalAddress();
             var eventTypes = Settings.GetAvailableTypes().Implementing<IEvent>();
 
+            var messageFailureResolvedByRetryType = typeof(MessageFailureResolvedByRetry);
+
             if (TransportDefinition.HasNativePubSubSupport)
             {
                 SubscribeForBrokers(localAddress, eventTypes);
+
+                ServiceControlSettings.RemoteInstances.ForEach(remote =>
+                {
+                    SubscribeForBrokers(Address.Parse(remote.Address), new []{ messageFailureResolvedByRetryType });
+                });
             }
             else
             {
                 SubscribeForNonBrokers(localAddress, eventTypes);
+
+                ServiceControlSettings.RemoteInstances.ForEach(remote =>
+                {
+                    MessageSender.Send(
+                        message: new TransportMessage(
+                            existingId: Guid.NewGuid().ToString(),
+                            existingHeaders: new Dictionary<string, string>
+                            {
+                                { Headers.ControlMessageHeader, string.Empty },
+                                { Headers.MessageIntent, MessageIntentEnum.Subscribe.ToString() },
+                                { Headers.SubscriptionMessageType, messageFailureResolvedByRetryType.AssemblyQualifiedName },
+                                { Headers.ReplyToAddress, localAddress.ToString() }
+                            }),
+                        sendOptions: new SendOptions(Address.Parse(remote.Address)));
+                });
             }
         }
 
@@ -37,9 +61,11 @@ namespace ServiceControl.Infrastructure
             SubscriptionStorage.Subscribe(address, eventTypes.Select(e => new MessageType(e)));
         }
 
+        public ISendMessages MessageSender { get; set; }
         public IManageSubscriptions SubscriptionManager { get; set; }
         public ISubscriptionStorage SubscriptionStorage { get; set; }
         public ReadOnlySettings Settings { get; set; }
+        public ServiceBus.Management.Infrastructure.Settings.Settings ServiceControlSettings { get; set; }
         public TransportDefinition TransportDefinition { get; set; }
     }
 }
