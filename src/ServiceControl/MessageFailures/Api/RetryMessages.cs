6c102f1a9340beef11b8c3ebc2899348352e665e
danielmarbach <daniel.marbach@openplace.net>
2018-01-25 16:58:41 +0100

    When instance_name is provided the retry request is sent to the right destination

--- src/ServiceControl/MessageFailures/Api/RetryMessages.cs
+++ src/ServiceControl/MessageFailures/Api/RetryMessages.cs
@@ -6,11 +6,14 @@
     using Nancy;
     using Nancy.ModelBinding;
     using NServiceBus;
+    using NServiceBus.Logging;
     using ServiceBus.Management.Infrastructure.Nancy.Modules;
     using ServiceControl.Recoverability;
 
     public class RetryMessages : BaseModule
     {
+        private static ILog Logger = LogManager.GetLogger<RetryMessages>();
+
         public RetryMessages()
         {
             Post["/errors/{messageid}/retry"] = parameters =>
@@ -22,10 +25,31 @@
                     return HttpStatusCode.BadRequest;
                 }
 
-                Bus.SendLocal<RetryMessage>(m =>
+                var query = (DynamicDictionary)Request.Query;
+                dynamic name;
+                if (query.TryGetValue("instance_name", out name))
                 {
-                    m.FailedMessageId = failedMessageId;
-                });
+                    var instanceName = (string) name;
+                    var remoteInstanceSettings = Settings.RemoteInstances.FirstOrDefault(r => r.InstanceName == instanceName);
+                    if (remoteInstanceSettings != null)
+                    {
+                        Bus.Send<RetryMessage>(Address.Parse(remoteInstanceSettings.QueueAddress), m =>
+                        {
+                            m.FailedMessageId = failedMessageId;
+                        });
+                    }
+                    else
+                    {
+                        Logger.Warn($"Unable to find remote address for {instanceName}. Possibly it was removed from the settings and deprovisioned.");
+                    }
+                }
+                else
+                {
+                    Bus.SendLocal<RetryMessage>(m =>
+                    {
+                        m.FailedMessageId = failedMessageId;
+                    });
+                }
 
                 return HttpStatusCode.Accepted;
             };
