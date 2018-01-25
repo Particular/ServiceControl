ce3d53a44fe5bdcbc564c04880fb7518bc695568
Sean Feldman <feldman.sean@gmail.com>
2018-01-24 17:03:21 -0700

    Get known endpoints implementation with ScatterGather-based implementation

---
+++ src/ServiceControl/CompositeViews/Endpoints/GetKnownEndpointsApi.cs
@@ -0,0 +1,32 @@
+﻿namespace ServiceControl.CompositeViews.Endpoints
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Nancy;
+    using Raven.Client;
+    using ServiceControl.CompositeViews.Messages;
+    using ServiceControl.EndpointControl;
+
+    public class GetKnownEndpointsApi : ScatterGatherApi<NoInput, KnownEndpointsView>
+    {
+        public override Task<QueryResult<KnownEndpointsView>> LocalQuery(Request request, NoInput input)
+        {
+            using (var session = Store.OpenSession())
+            {
+                RavenQueryStatistics stats;
+
+                var results = session.Query<KnownEndpoint, KnownEndpointIndex>()
+                    .Statistics(out stats)
+                    .Select(x => new KnownEndpointsView
+                    {
+                        Id = x.Id,
+                        HostDisplayName = x.HostDisplayName,
+                        EndpointDetails = x.EndpointDetails
+                    })
+                    .ToList();
+
+                return Task.FromResult(Results(results, stats));
+            }
+        }
+    }
+}
