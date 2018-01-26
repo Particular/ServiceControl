82818d3a0fb68f127b94ae46108a3daabf095550
Bob Langley <bob.langley@particular.net>
2018-01-25 20:27:18 -0800

    Make sure case does not effect calculation

--- src/ServiceControl/Infrastructure/Settings/InstanceIdGenerator.cs
+++ src/ServiceControl/Infrastructure/Settings/InstanceIdGenerator.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// Converts a string to a base64 encoded string using UTF8
         /// </summary>
-        public static string FromApiUrl(string apiUrl) => Convert.ToBase64String(Encoding.UTF8.GetBytes(apiUrl));
+        public static string FromApiUrl(string apiUrl) => Convert.ToBase64String(Encoding.UTF8.GetBytes(apiUrl.ToLowerInvariant()));
 
         /// <summary>
         /// Converts from a base64 encoded string value using UTF8
