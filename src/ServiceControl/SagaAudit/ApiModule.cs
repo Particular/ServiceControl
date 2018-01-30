57878aacae859321100f3a44b8afd54c5f9d771b
danielmarbach <daniel.marbach@openplace.net>
2018-01-30 13:38:33 +0100

    Remove list sagas since it is not used by any client

--- src/ServiceControl/SagaAudit/ApiModule.cs
+++ src/ServiceControl/SagaAudit/ApiModule.cs
@@ -2,18 +2,14 @@ namespace ServiceControl.SagaAudit
 {
     using System;
     using ServiceBus.Management.Infrastructure.Nancy.Modules;
-    using ServiceControl.CompositeViews.Messages;
 
     public class ApiModule : BaseModule
     {
-        public ListSagasApi ListSagasApi { get; set; }
         public GetSagaByIdApi GetSagaByIdApi { get; set; }
 
         public ApiModule()
         {
             Get["/sagas/{id}", true] = (parameters, token) => GetSagaByIdApi.Execute(this, (Guid) parameters.id);
-
-            Get["/sagas", true] = (_, token) => ListSagasApi.Execute(this, NoInput.Instance);
         }
     }
 }
