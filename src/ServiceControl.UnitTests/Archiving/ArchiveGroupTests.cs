24d6dd9d416d3ec24af2348efc59e2c9986ba1fe
WilliamBZA <william.brander@gmail.com>
2018-02-02 11:49:01 +0200

    Add failing test

---
+++ src/ServiceControl.UnitTests/Archiving/ArchiveGroupTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using Raven.Client;
+using ServiceControl.Contracts.Operations;
+using ServiceControl.MessageFailures;
+using ServiceControl.Recoverability;
+using ServiceControl.UnitTests.Recoverability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceControl.UnitTests.Archiving
+{
+    [TestFixture]
+    public class ArchiveGroupTests
+    {
+        [Test]
+        public void ArchiveGroup_skips_over_empty_batches_but_still_completes()
+        {
+            // Arrange
+            using (var documentStore = InMemoryStoreBuilder.GetInMemoryStore())
+            {
+                var groupId = "TestGroup";
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var secondAchiveBatch = new ArchiveBatch { Id = ArchiveBatch.MakeId(groupId, ArchiveType.FailureGroup, 1) };
+                    session.Store(secondAchiveBatch);
+
+                    var archiveOperation = new ArchiveOperation
+                    {
+                        Id = ArchiveOperation.MakeId(groupId, ArchiveType.FailureGroup),
+                        RequestId = groupId,
+                        ArchiveType = ArchiveType.FailureGroup,
+                        TotalNumberOfMessages = 2,
+                        NumberOfMessagesArchived = 0,
+                        Started = DateTime.Now,
+                        GroupName = "Test Group",
+                        NumberOfBatches = 3,
+                        CurrentBatch = 0
+                    };
+                    session.Store(archiveOperation);
+
+                    session.SaveChanges();
+                }
+
+                var testBus = new TestBus();
+                var documentManager = new ArchiveDocumentManager();
+                var archivingManager = new ArchivingManager();
+                var retryingManager = new RetryingManager();
+                var handler = new ArchiveAllInGroupHandler(testBus, documentStore, documentManager, archivingManager, retryingManager);
+
+                var message = new ArchiveAllInGroup { GroupId = groupId };
+
+                // Act
+                handler.Handle(message);
+
+                // Assert
+                using (var session = documentStore.OpenSession())
+                {
+                    var loadedBatch = session.Load<ArchiveBatch>(ArchiveBatch.MakeId(groupId, ArchiveType.FailureGroup, 1));
+                    Assert.IsNull(loadedBatch);
+                }
+            }
+        }
+    }
+}
