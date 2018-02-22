e4cbc78ab8817397af9957abedc433350d18d3e2
Mike Minutillo <michael.minutillo@gmail.com>
2018-02-22 16:47:16 +0800

    Handle casing changes

--- src/ServiceControl/CompositeViews/Messages/MessageViewComparer.cs
+++ src/ServiceControl/CompositeViews/Messages/MessageViewComparer.cs
@@ -19,53 +19,40 @@ namespace ServiceControl.CompositeViews.Messages
                 ? queryString["direction"]
                 : "desc";
 
-            switch (sortBy)
+            Comparer comparer;
+
+            if (!SortByMap.TryGetValue(sortBy, out comparer))
             {
-                case "id":
-                case "message_id":
-                    return sortOrder == "asc" ? ByMessageIdAscending : ByMessageIdDescending;
-                case "message_type":
-                    return sortOrder == "asc" ? ByMessageTypeAscending : ByMessageTypeDescending;
-                case "critical_time":
-                    return sortOrder == "asc" ? ByCriticalTimeAscending : ByCriticalTimeDescending;
-                case "delivery_time":
-                    return sortOrder == "asc" ? ByDeliveryTimeAscending : ByDeliveryTimeDescending;
-                case "time_sent":
-                    return sortOrder == "asc" ? ByTimeSentAscending : ByTimeSentDescending;
-                case "processing_time":
-                    return sortOrder == "asc" ? ByProcessingTimeAscending : ByProcessingTimeDescending;
-                case "processed_at":
-                    return sortOrder == "asc" ? ByProcessedAtAscending : ByProcessedAtDescending;
-                case "status":
-                    return sortOrder == "asc" ? ByStatusAscending : ByStatusDescending;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(sortBy));
+                throw new ArgumentOutOfRangeException(nameof(sortBy));
             }
+
+            return string.Equals(sortOrder, "desc", StringComparison.CurrentCultureIgnoreCase)
+                ? comparer.Reverse()
+                : comparer;
         }
 
-        private static IComparer<MessagesView> ByMessageIdAscending = new Comparer((x, y) => string.Compare(x.MessageId, y.MessageId, StringComparison.Ordinal));
-        private static IComparer<MessagesView> ByMessageIdDescending = new Comparer((x, y) => string.Compare(y.MessageId, x.MessageId, StringComparison.Ordinal));
+        private static Comparer ByMessageIdAscending = new Comparer((x, y) => string.Compare(x.MessageId, y.MessageId, StringComparison.Ordinal));
+        private static Comparer ByMessageTypeAscending = new Comparer((x, y) => string.Compare(x.MessageType, y.MessageType, StringComparison.Ordinal));
+        private static Comparer ByTimeSentAscending = new Comparer((x, y) => Nullable.Compare(x.TimeSent, y.TimeSent));
+        private static Comparer ByCriticalTimeAscending = new Comparer((x, y) => x.CriticalTime.CompareTo(y.CriticalTime));
+        private static Comparer ByDeliveryTimeAscending = new Comparer((x, y) => x.DeliveryTime.CompareTo(y.DeliveryTime));
+        private static Comparer ByProcessingTimeAscending = new Comparer((x, y) => x.ProcessingTime.CompareTo(y.ProcessingTime));
+        private static Comparer ByProcessedAtAscending = new Comparer((x, y) => x.ProcessedAt.CompareTo(y.ProcessedAt));
+        private static Comparer ByStatusAscending = new Comparer((x, y) => x.Status.CompareTo(y.Status));
 
-        private static IComparer<MessagesView> ByMessageTypeAscending = new Comparer((x, y) => string.Compare(x.MessageType, y.MessageType, StringComparison.Ordinal));
-        private static IComparer<MessagesView> ByMessageTypeDescending = new Comparer((x, y) => string.Compare(y.MessageType, x.MessageType, StringComparison.Ordinal));
 
-        private static IComparer<MessagesView> ByTimeSentAscending = new Comparer((x, y) => Nullable.Compare(x.TimeSent, y.TimeSent));
-        private static IComparer<MessagesView> ByTimeSentDescending = new Comparer((x, y) => Nullable.Compare(y.TimeSent, x.TimeSent));
-
-        private static IComparer<MessagesView> ByCriticalTimeAscending = new Comparer((x, y) => x.CriticalTime.CompareTo(y.CriticalTime));
-        private static IComparer<MessagesView> ByCriticalTimeDescending = new Comparer((x, y) => y.CriticalTime.CompareTo(x.CriticalTime));
-
-        private static IComparer<MessagesView> ByDeliveryTimeAscending = new Comparer((x, y) => x.DeliveryTime.CompareTo(y.DeliveryTime));
-        private static IComparer<MessagesView> ByDeliveryTimeDescending = new Comparer((x, y) => y.DeliveryTime.CompareTo(x.DeliveryTime));
-
-        private static IComparer<MessagesView> ByProcessingTimeAscending = new Comparer((x, y) => x.ProcessingTime.CompareTo(y.ProcessingTime));
-        private static IComparer<MessagesView> ByProcessingTimeDescending = new Comparer((x, y) => y.ProcessingTime.CompareTo(x.ProcessingTime));
-
-        private static IComparer<MessagesView> ByProcessedAtAscending = new Comparer((x, y) => x.ProcessedAt.CompareTo(y.ProcessedAt));
-        private static IComparer<MessagesView> ByProcessedAtDescending = new Comparer((x, y) => y.ProcessedAt.CompareTo(x.ProcessedAt));
-
-        private static IComparer<MessagesView> ByStatusAscending = new Comparer((x, y) => x.Status.CompareTo(y.Status));
-        private static IComparer<MessagesView> ByStatusDescending = new Comparer((x, y) => y.Status.CompareTo(x.Status));
+        private static IDictionary<string, Comparer> SortByMap = new Dictionary<string, Comparer>(StringComparer.InvariantCultureIgnoreCase)
+        {
+            ["id"] = ByMessageIdAscending,
+            ["message_id"] = ByMessageIdAscending,
+            ["message_type"] = ByMessageTypeAscending,
+            ["critical_time"] = ByCriticalTimeAscending,
+            ["delivery_time"] = ByDeliveryTimeAscending,
+            ["time_sent"] = ByTimeSentAscending,
+            ["processing_time"] = ByProcessingTimeAscending,
+            ["processed_at"] = ByProcessedAtAscending,
+            ["status"] = ByStatusAscending
+        };
 
 
         class Comparer : IComparer<MessagesView>
@@ -81,6 +68,23 @@ namespace ServiceControl.CompositeViews.Messages
             {
                 return comparerFunc(x, y);
             }
+
+            public IComparer<MessagesView> Reverse()
+            {
+                return new Reverse(this);
+            }
+        }
+
+        class Reverse : IComparer<MessagesView>
+        {
+            private readonly IComparer<MessagesView> inner;
+
+            public Reverse(IComparer<MessagesView> inner)
+            {
+                this.inner = inner;
+            }
+
+            public int Compare(MessagesView x, MessagesView y) => inner.Compare(y, x);
         }
     }
 }
