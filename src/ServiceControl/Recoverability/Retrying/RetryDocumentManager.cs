6cf12ccc763fc8d9ae3d5fa9a5b95a97a68524dd
SzymonPobiega <szymon.pobiega@gmail.com>
2018-03-14 13:39:16 +0100

    Fixed inspections

--- src/ServiceControl/Recoverability/Retrying/RetryDocumentManager.cs
+++ src/ServiceControl/Recoverability/Retrying/RetryDocumentManager.cs
@@ -1,7 +1,6 @@
 namespace ServiceControl.Recoverability
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using NServiceBus.Logging;
