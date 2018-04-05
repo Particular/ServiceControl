d99b916672a6c208c5dbc25a1c96b27e937f13b8
Tomasz Masternak <tomasz.masternak@particular.net>
2018-04-05 15:46:45 +0200

    NewEndpointDetected handler

---
+++ src/ServiceControl/Monitoring/Handler/NewEndpointDetectedHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Monitoring.Handler
+{
+    using NServiceBus;
+    using ServiceControl.Contracts.EndpointControl;
+
+    public class NewEndpointDetectedHandler : IHandleMessages<NewEndpointDetected>
+    {
+        readonly EndpointInstanceMonitoring endpointInstanceMonitoring;
+
+        public NewEndpointDetectedHandler(EndpointInstanceMonitoring endpointInstanceMonitoring)
+        {
+            this.endpointInstanceMonitoring = endpointInstanceMonitoring;
+        }
+
+        public void Handle(NewEndpointDetected message)
+        {
+            var endpointInstanceId = new EndpointInstanceId(message.Endpoint.Name, message.Endpoint.Host, message.Endpoint.HostId);
+
+            endpointInstanceMonitoring.GetOrCreateMonitor(endpointInstanceId, false);
+        }
+    }
+}
