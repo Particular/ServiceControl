a7f2616b7b10317032bab22c40ab3e5afe04c04b
danielmarbach <daniel.marbach@openplace.net>
2018-04-06 17:35:50 +0200

    Switch to string comparison with StringComparison enum

--- src/ServiceControl.Config/UI/SharedInstanceEditor/SharedServiceControlEditorViewModelValidator.cs
+++ src/ServiceControl.Config/UI/SharedInstanceEditor/SharedServiceControlEditorViewModelValidator.cs
@@ -42,7 +42,7 @@ namespace ServiceControl.Config.Validation
                     p.ErrorQueue,
                     p.AuditQueue,
                     p.AuditLogQueue
-                }).Where(queuename => string.Compare(queuename, "!disable", true) != 0)
+                }).Where(queuename => string.Compare(queuename, "!disable", StringComparison.OrdinalIgnoreCase) != 0)
                 .Distinct()
                 .ToList();
         }
