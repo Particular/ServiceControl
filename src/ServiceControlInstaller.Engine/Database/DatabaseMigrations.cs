c323330e15106bd43b08a1e9194942f7030bfe24
Bob Langley <bob.langley@particular.net>
2018-04-20 09:20:09 -0700

    refactored to prevent hang during db upgrade process execution

--- src/ServiceControlInstaller.Engine/Database/DatabaseMigrations.cs
+++ src/ServiceControlInstaller.Engine/Database/DatabaseMigrations.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Diagnostics;
     using System.IO;
+    using System.Text;
+    using System.Threading;
     using ServiceControlInstaller.Engine.Instances;
 
     internal class DatabaseMigrations
@@ -16,52 +18,103 @@
 
         static void RunDataMigration(Action<string> updateProgress, string installPath, string exeName, string serviceName)
         {
-            var processStartupInfo = new ProcessStartInfo
-            {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                FileName = Path.Combine(installPath, exeName),
-                Arguments = $"--database --serviceName={serviceName}",
-                WorkingDirectory = installPath,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            };
+            var fileName = Path.Combine(installPath, exeName);
+            var args = $"--database --serviceName={serviceName}";
 
             var attempts = 0;
-            Process p;
-            var updatingSchema = false;
+            var timeout = (int) TimeSpan.FromMinutes(20).TotalMilliseconds;
+
             do
             {
-                attempts++;
-
-                p = Process.Start(processStartupInfo);
-                if (p == null)
+                using (var p = new Process())
                 {
-                    throw new Exception($"Attempt to launch {exeName} failed.");
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.FileName = fileName;
+                    p.StartInfo.Arguments = args;
+                    p.StartInfo.WorkingDirectory = installPath;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+
+                    var error = new StringBuilder();
+
+                    var updatingSchema = false;
+
+                    using (var outputWaitHandle = new AutoResetEvent(false))
+                    using (var errorWaitHandle = new AutoResetEvent(false))
+                    {
+                        p.OutputDataReceived += (sender, eventArgs) =>
+                        {
+                            if (eventArgs.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+
+                                Debug.WriteLine(eventArgs.Data);
+
+                                if (eventArgs.Data.StartsWith("Updating schema from version"))
+                                {
+                                    updatingSchema = true;
+                                    updateProgress(eventArgs.Data.Replace(":", string.Empty));
+                                }
+                                else if (updatingSchema && eventArgs.Data.StartsWith("OK"))
+                                {
+                                    updatingSchema = false;
+                                    updateProgress(string.Empty);
+                                }
+                            }
+                        };
+
+                        p.ErrorDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                errorWaitHandle.Set();
+                            }
+                            else
+                            {
+                                error.AppendLine(e.Data);
+                            }
+                        };
+
+                        attempts++;
+
+                        Debug.WriteLine($"Attempt {attempts}");
+
+                        p.Start();
+
+                        p.BeginOutputReadLine();
+                        p.BeginErrorReadLine();
+
+                        if (p.WaitForExit(timeout) &&
+                            outputWaitHandle.WaitOne(timeout) &&
+                            errorWaitHandle.WaitOne(timeout))
+                        {
+                            Debug.WriteLine($"Attempt {attempts} exited with code {p.ExitCode}");
+                            if (p.ExitCode == 0)
+                            {
+                                break;
+                            }
+
+                            if (attempts < 2)
+                            {
+                                continue;
+                            }
+
+                            if (p.ExitCode != 0)
+                            {
+                                throw new DatabaseMigrationsException($"{exeName} threw an error when migrating data. Please contact Particular support. The error output from {exeName} was:\r\n {error}");
+                            }
+                        }
+                        else
+                        {
+                            throw new DatabaseMigrationsTimeoutException($"{exeName} timed out while migrating data.");
+                        }
+                    }
                 }
-
-                p.OutputDataReceived += (sender, eventArgs) =>
-                {
-                    if (eventArgs.Data.StartsWith("Updating schema from version"))
-                    {
-                        updatingSchema = true;
-                        updateProgress(eventArgs.Data.Remove(':'));
-                    }
-                    else if (updatingSchema && eventArgs.Data.StartsWith("OK"))
-                    {
-                        updatingSchema = false;
-                        updateProgress(string.Empty);
-                    }
-                };
-
-                p.WaitForExit((int)TimeSpan.FromMinutes(20).TotalMilliseconds);
-            } while (p.ExitCode > 0 && attempts < 2);
-
-            if (p.ExitCode != 0)
-            {
-                var error = p.StandardError.ReadToEnd();
-                throw new DatabaseMigrationsException($"{exeName} threw an error when migrating data. Please contact Particular support. The error output from {exeName} was:\r\n {error}");
-            }
+            } while (attempts < 2);
         }
     }
 }
