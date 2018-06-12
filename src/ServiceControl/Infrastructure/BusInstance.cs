08270a9afe4248b6214d033c3241203ba83b6ecb
danielmarbach <daniel.marbach@openplace.net>
2018-06-12 13:20:15 +0200

    ImportFailedAuditsCommand

--- src/ServiceControl/Infrastructure/BusInstance.cs
+++ src/ServiceControl/Infrastructure/BusInstance.cs
@@ -3,17 +3,21 @@ namespace ServiceBus.Management.Infrastructure
     using System;
     using NServiceBus;
     using ServiceControl.Infrastructure.DomainEvents;
+    using ServiceControl.Operations;
 
     public class BusInstance : IDisposable
     {
-        public BusInstance(IEndpointInstance bus, IDomainEvents domainEvents)
+        public BusInstance(IEndpointInstance bus, IDomainEvents domainEvents, ImportFailedAudits importFailedAudits)
         {
+            ImportFailedAudits = importFailedAudits;
             Bus = bus;
             DomainEvents = domainEvents;
         }
 
         public IEndpointInstance Bus { get; }
         public IDomainEvents DomainEvents { get; }
+        public ImportFailedAudits ImportFailedAudits { get; }
+        
         public void Dispose()
         {
             Bus.Stop().GetAwaiter().GetResult();
