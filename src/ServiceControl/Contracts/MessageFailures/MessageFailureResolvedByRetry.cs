cc5511b34aa0392627efedede7cfe35f60b25d7d
SzymonPobiega <szymon.pobiega@gmail.com>
2018-06-13 09:34:32 +0200

    MessageFailureResolvedByRetry

--- src/ServiceControl/Contracts/MessageFailures/MessageFailureResolvedByRetry.cs
+++ src/ServiceControl/Contracts/MessageFailures/MessageFailureResolvedByRetry.cs
@@ -1,9 +1,10 @@
 ﻿namespace ServiceControl.Contracts.MessageFailures
 {
+    using NServiceBus;
     using ServiceControl.Infrastructure.DomainEvents;
     using ServiceControl.Infrastructure.SignalR;
 
-    public class MessageFailureResolvedByRetry : IDomainEvent, IBusEvent, IUserInterfaceEvent
+    public class MessageFailureResolvedByRetry : IDomainEvent, IEvent, IUserInterfaceEvent
     {
         public string FailedMessageId { get; set; }
         public string[] AlternativeFailedMessageIds { get; set; }
