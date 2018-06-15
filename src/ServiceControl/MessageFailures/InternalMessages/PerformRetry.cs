125ca885f0d98453da1da9c6d9c6a72fdf89d0d7
Mike Minutillo <michael.minutillo@gmail.com>
2018-06-15 13:56:03 +0800

    Remove unused properties

--- src/ServiceControl/MessageFailures/InternalMessages/PerformRetry.cs
+++ src/ServiceControl/MessageFailures/InternalMessages/PerformRetry.cs
@@ -1,12 +1,9 @@
 ﻿namespace ServiceControl.MessageFailures.InternalMessages
 {
-    using System;
     using NServiceBus;
 
     public class PerformRetry : ICommand
     {
         public string FailedMessageId { get; set; }
-        public Address TargetEndpointAddress { get; set; }
-        public Guid RetryId { get; set; }
     }
 }
