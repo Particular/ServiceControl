f02f03edddd546d66a45c6e46350674e6fcd04f5
danielmarbach <daniel.marbach@openplace.net>
2018-06-15 10:11:50 +0200

    For mike

---
+++ src/ServiceControl.AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus.AcceptanceTests.EndpointTemplates
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+    using ObjectBuilder;
+
+    public static class ConfigureExtensions
+    {
+        public static async Task DefineTransport(this EndpointConfiguration config, RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
+        {
+            var transportConfiguration = TestSuiteConstraints.Current.CreateTransportConfiguration();
+            await transportConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, config, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+            runDescriptor.OnTestCompleted(_ => transportConfiguration.Cleanup());
+        }
+
+        public static async Task DefinePersistence(this EndpointConfiguration config, RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
+        {
+            var persistenceConfiguration = TestSuiteConstraints.Current.CreatePersistenceConfiguration();
+            await persistenceConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, config, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+            runDescriptor.OnTestCompleted(_ => persistenceConfiguration.Cleanup());
+        }
+
+        public static void RegisterComponentsAndInheritanceHierarchy(this EndpointConfiguration builder, RunDescriptor runDescriptor)
+        {
+            builder.RegisterComponents(r => { RegisterInheritanceHierarchyOfContextOnContainer(runDescriptor, r); });
+        }
+
+        static void RegisterInheritanceHierarchyOfContextOnContainer(RunDescriptor runDescriptor, IConfigureComponents r)
+        {
+            var type = runDescriptor.ScenarioContext.GetType();
+            while (type != typeof(object))
+            {
+                r.RegisterSingleton(type, runDescriptor.ScenarioContext);
+                type = type.BaseType;
+            }
+        }
+    }
+}
---
+++ src/ServiceControl.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+        public bool SupportsCrossQueueTransactions => true;
+        public bool SupportsNativePubSub => true;
+        public bool SupportsNativeDeferral => true;
+        public bool SupportsOutbox => false;
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointMsmqTransport();
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointInMemoryPersistence();
+    }
+}
