a86828c377fdc2c4e69c3e47e20462812b9cc709
danielmarbach <daniel.marbach@openplace.net>
2018-06-15 09:31:59 +0200

    Raw dispatch

---
+++ src/ServiceControl.AcceptanceTests/DispatchRawMessages.cs
@@ -0,0 +1,63 @@
+﻿namespace ServiceBus.Management.AcceptanceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Extensibility;
+    using NServiceBus.Features;
+    using NServiceBus.Transport;
+
+    abstract class DispatchRawMessages : Feature
+    {
+        protected DispatchRawMessages()
+        {
+            EnableByDefault();
+        }
+        
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.RegisterStartupTask(b => b.Build<DispatchTask>());
+        }
+
+        protected abstract TransportOperations CreateMessage();
+
+        protected virtual Task BeforeDispatch(IMessageSession session)
+        {
+            return Task.FromResult(0);
+        }
+        
+        protected virtual Task AfterDispatch(IMessageSession session)
+        {
+            return Task.FromResult(0);
+        }
+        
+        class DispatchTask : FeatureStartupTask
+        {
+            private IDispatchMessages dispatchMessages;
+            private Func<TransportOperations> operationFactory;
+            private Func<IMessageSession, Task> before;
+            private Func<IMessageSession, Task> after;
+
+            public DispatchTask(IDispatchMessages dispatcher, Func<TransportOperations> operationFactory, Func<IMessageSession, Task> before, Func<IMessageSession, Task> after)
+            {
+                this.after = after;
+                this.before = before;
+                this.operationFactory = operationFactory;
+                dispatchMessages = dispatcher;
+            }
+            
+            protected override async Task OnStart(IMessageSession session)
+            {
+                await before(session).ConfigureAwait(false);
+                await dispatchMessages.Dispatch(operationFactory(), new TransportTransaction(), new ContextBag())
+                    .ConfigureAwait(false);
+                await after(session).ConfigureAwait(false);                
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                return Task.FromResult(0);
+            }
+        }
+    }
+}
---
+++ src/ServiceControl.AcceptanceTests/ScenarioWithEndpointBehaviorExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceBus.Management.AcceptanceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Support;
+
+    public static class ScenarioWithEndpointBehaviorExtensions
+    {
+        public static IScenarioWithEndpointBehavior<TContext> Done<TContext>(this IScenarioWithEndpointBehavior<TContext> endpointBehavior, Func<TContext, Task<bool>> func) where TContext : ScenarioContext
+        {
+            return endpointBehavior.Done(ctx => func(ctx).GetAwaiter().GetResult());
+        }
+    }
+}
