31e6713f6154b8ba8dc94795688e069de989db0b
Mike Minutillo <michael.minutillo@gmail.com>
2018-06-20 12:29:19 +0800

    INeedInitialization Signature

--- src/ServiceControl.SqlServerWithDTC/DisableCallbackQueue.cs
+++ src/ServiceControl.SqlServerWithDTC/DisableCallbackQueue.cs
@@ -1,12 +1,12 @@
 ﻿namespace ServiceControl.Transports.SqlServerWithDTC
 {
     using NServiceBus;
-    using NServiceBus.Configuration.AdvanceExtensibility;
+    using NServiceBus.Configuration.AdvancedExtensibility;
 
     // ReSharper disable once UnusedMember.Global
     public class DisableCallbackQueue : INeedInitialization
     {
-        public void Customize(BusConfiguration configuration)
+        public void Customize(EndpointConfiguration configuration)
         {
             configuration.GetSettings().Set("SqlServer.UseCallbackReceiver", false);
         }
