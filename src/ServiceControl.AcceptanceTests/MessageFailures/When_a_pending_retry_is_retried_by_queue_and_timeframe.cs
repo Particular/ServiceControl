8a87a8ae3ed4a2af6a66d9aac822b3ffb503f2d2
SzymonPobiega <szymon.pobiega@gmail.com>
2018-06-27 10:09:56 +0200

    Fix When_a_pending_retry_is_retried_by_queue_and_timeframe

--- src/ServiceControl.AcceptanceTests/MessageFailures/When_a_pending_retry_is_retried_by_queue_and_timeframe.cs
+++ src/ServiceControl.AcceptanceTests/MessageFailures/When_a_pending_retry_is_retried_by_queue_and_timeframe.cs
@@ -19,50 +19,69 @@
             FailedMessage failedMessage;
 
             await Define<Context>()
-                .WithEndpoint<FailingEndpoint>(b => b.When(bus => bus.SendLocal(new MyMessage())).DoNotFailOnErrorMessages()
-                    .When(async ctx =>
+                .WithEndpoint<FailingEndpoint>(b => b.When(bus => bus.SendLocal(new MyMessage())).DoNotFailOnErrorMessages())
+                .Done(async ctx =>
                 {
                     if (ctx.UniqueMessageId == null)
                     {
                         return false;
                     }
 
-                    var result = await this.TryGet<FailedMessage>($"/api/errors/{ctx.UniqueMessageId}");
-                    failedMessage = result;
-                    if (!result)
+                    if (ctx.State == State.Begin)
                     {
+                        var result = await this.TryGet<FailedMessage>($"/api/errors/{ctx.UniqueMessageId}");
+                        failedMessage = result;
+                        if (result)
+                        {
+                            ctx.State = State.FailedMessageDetected;
+                        }
                         return false;
                     }
 
-                    if (!ctx.RetryAboutToBeSent)
+                    if (ctx.State == State.FailedMessageDetected)
                     {
-                        ctx.RetryAboutToBeSent = true;
+                        ctx.State = State.RetryAboutToBeSent;
                         await this.Post<object>($"/api/errors/{ctx.UniqueMessageId}/retry");
                         return false;
                     }
 
-                    if (failedMessage.Status == FailedMessageStatus.RetryIssued)
+                    if (ctx.State == State.RetryAboutToBeSent)
                     {
-                        var failedMessagesResult = await this.TryGet<FailedMessage[]>("/api/errors");
-                        FailedMessage[] failedMessages = failedMessagesResult;
-                        if (failedMessagesResult)
+                        var result = await this.TryGet<FailedMessage>($"/api/errors/{ctx.UniqueMessageId}");
+                        failedMessage = result;
+                        if (result && result.Item.Status == FailedMessageStatus.RetryIssued)
                         {
-                            return failedMessages.Any(fm => fm.Id == ctx.UniqueMessageId);
+                            var failedMessagesResult = await this.TryGet<FailedMessage[]>("/api/errors");
+                            FailedMessage[] failedMessages = failedMessagesResult;
+                            if (failedMessagesResult)
+                            {
+                                var messagePresent = failedMessages.Any(fm => fm.Id == ctx.UniqueMessageId);
+                                if (messagePresent)
+                                {
+                                    ctx.State = State.SecondRetryAboutToBeSent;
+                                    await this.Post<object>("/api/pendingretries/queues/retry", new
+                                    {
+                                        queueaddress = ctx.FromAddress,
+                                        from = DateTime.UtcNow.AddHours(-1).ToString("o"),
+                                        to = DateTime.UtcNow.AddSeconds(10).ToString("o")
+                                    });
+                                }
+                            }
                         }
+                        return false;
                     }
 
-                    return false;
-                }, async (bus, ctx) =>
-                {
-                    await this.Post<object>("/api/pendingretries/queues/retry", new
-                    {
-                        queueaddress = ctx.FromAddress,
-                        from = DateTime.UtcNow.AddHours(-1).ToString("o"),
-                        to = DateTime.UtcNow.AddSeconds(10).ToString("o")
-                    });
-                }).DoNotFailOnErrorMessages())
-                .Done(ctx => ctx.RetryCount == 2)
+                    return ctx.RetryCount == 2;
+                })
                 .Run();
+        }
+
+        public enum State
+        {
+            Begin,
+            FailedMessageDetected,
+            RetryAboutToBeSent,
+            SecondRetryAboutToBeSent,
         }
 
         public class FailingEndpoint : EndpointConfigurationBuilder
@@ -84,7 +103,7 @@
                 public Task Handle(MyMessage message, IMessageHandlerContext context)
                 {
                     Console.WriteLine("Message Handled");
-                    if (Context.RetryAboutToBeSent)
+                    if (Context.State != State.Begin)
                     {
                         Context.RetryCount++;
                         Context.Retried = true;
@@ -105,7 +124,7 @@
         {
             public string UniqueMessageId { get; set; }
             public bool Retried { get; set; }
-            public bool RetryAboutToBeSent { get; set; }
+            public State State { get; set; }
             public int RetryCount { get; set; }
             public string FromAddress { get; set; }
         }
