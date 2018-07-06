d14168b1e6ea65ed4ca5ba67192b842f056bcb7b
WilliamBZA <william.brander@gmail.com>
2018-07-06 14:26:22 +0200

    Align dropdowns correctly

---
+++ src/ServiceControl.Config/Xaml/Converters/subtractValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ServiceControl.Config.Xaml.Converters
+{
+    public class SubtractValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var subtractOffset = double.Parse(parameter as string ?? "0");
+            return (double)value - subtractOffset;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
