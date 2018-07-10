eb9908b6cab1a026ab4eff748eaa8b25bfe78163
Tim <bussmann.t@gmail.com>
2018-07-10 13:32:33 +0200

    log request body in ApiLogger

--- src/ServiceControl/Infrastructure/OWIN/ApiLogger.cs
+++ src/ServiceControl/Infrastructure/OWIN/ApiLogger.cs
@@ -1,5 +1,7 @@
 ﻿namespace ServiceControl.Infrastructure.OWIN
 {
+    using System;
+    using System.IO;
     using System.Threading.Tasks;
     using Microsoft.Owin;
     using NServiceBus.Logging;
@@ -10,13 +12,40 @@
 
         public override async Task Invoke(IOwinContext context)
         {
+            if (!log.IsDebugEnabled)
+            {
+                await Next.Invoke(context);
+                return;
+            }
+
             log.DebugFormat("Begin {0}: {1}", context.Request.Method, context.Request.Uri.ToString());
+
+            var originalStream = context.Response.Body;
+            var newStream = new MemoryStream();
+            context.Response.Body = newStream;
 
             await Next.Invoke(context);
 
-            log.DebugFormat("End {0}: {1}", context.Request.Method, context.Request.Uri.ToString());
+            newStream.Position = 0;
+            using (var reader = new StreamReader(newStream))
+            {
+                try
+                {
+                    var bodyContent = reader.ReadToEnd();
+                    log.Debug(bodyContent);
+                    log.Debug($"End {context.Request.Method} ({context.Response.StatusCode}): {context.Request.Uri}:{Environment.NewLine}{bodyContent}");
+
+                    newStream.Position = 0;
+                    newStream.CopyTo(originalStream);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
         }
 
-        static ILog log = LogManager.GetLogger<LogApiCalls>();
+        private static ILog log = LogManager.GetLogger<LogApiCalls>();
     }
 }
