415f6daa3a0a17c4df865bd95e884aa8fcaebe07
Tim <bussmann.t@gmail.com>
2018-07-11 10:56:59 +0200

    rollback ApiLogger changes

--- src/ServiceControl/Infrastructure/OWIN/ApiLogger.cs
+++ src/ServiceControl/Infrastructure/OWIN/ApiLogger.cs
@@ -1,7 +1,5 @@
 ﻿namespace ServiceControl.Infrastructure.OWIN
 {
-    using System;
-    using System.IO;
     using System.Threading.Tasks;
     using Microsoft.Owin;
     using NServiceBus.Logging;
@@ -12,45 +10,11 @@
 
         public override async Task Invoke(IOwinContext context)
         {
-            if (!log.IsDebugEnabled)
-            {
-                await Next.Invoke(context).ConfigureAwait(false);
-                return;
-            }
-
-            if (context.Request.Uri.LocalPath.StartsWith("/api/messagestream"))
-            {
-                await Next.Invoke(context).ConfigureAwait(false);
-                return;
-            }
-            
             log.DebugFormat("Begin {0}: {1}", context.Request.Method, context.Request.Uri.ToString());
 
-            var originalStream = context.Response.Body;
-            using (var newStream = new MemoryStream())
-            {
-                context.Response.Body = newStream;
+            await Next.Invoke(context).ConfigureAwait(false);
 
-                await Next.Invoke(context).ConfigureAwait(false);
-
-                newStream.Position = 0;
-                using (var reader = new StreamReader(newStream))
-                {
-                    try
-                    {
-                        var bodyContent = await reader.ReadToEndAsync().ConfigureAwait(false);
-                        log.Debug($"End {context.Request.Method} ({context.Response.StatusCode}): {context.Request.Uri}:{Environment.NewLine}{bodyContent}");
-
-                        newStream.Position = 0;
-                        await newStream.CopyToAsync(originalStream).ConfigureAwait(false);
-                    }
-                    catch (Exception e)
-                    {
-                        log.Debug("Unable to read stream", e);
-                        throw;
-                    }
-                }
-            }
+            log.DebugFormat("End {0} ({1}): {2}", context.Request.Method, context.Response.StatusCode, context.Request.Uri.ToString());
         }
 
         private static ILog log = LogManager.GetLogger<LogApiCalls>();
