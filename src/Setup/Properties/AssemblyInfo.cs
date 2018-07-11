3e0cd9a6a4aec20271d5e7b1fdc156de933df55e
Brandon Ording <bording@gmail.com>
2018-07-11 17:53:11 -0400

    Update Setup

--- src/Setup/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Setup")]
-[assembly: AssemblyProduct("Setup")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: ComVisible(false)]
-[assembly: Guid("905e7aa4-6424-44d4-9296-21cc025a7720")]
