9e98c373f7c858219698152abcd6cce964bfa0f1
danielmarbach <daniel.marbach@openplace.net>
2018-07-18 14:07:44 +0200

    Enabling SQS

---
+++ src/ServiceControl.AcceptanceTests/ConfigureEndpointSQSTransport.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using TestConventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+using ServiceBus.Management.AcceptanceTests;
+
+public class ConfigureEndpointSQSTransport : ITransportIntegration
+{
+    public string Name => "SQS";
+
+    public string TypeName => "ServiceControl.Transports.SQS.SQSTransportCustomization, ServiceControl.Transports.SQS";
+
+    public string ConnectionString { get; set; }
+    
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        configuration.UseSerialization<NewtonsoftSerializer>();
+
+        var transportConfig = configuration.UseTransport<SqsTransport>();
+
+        var routing = transportConfig.Routing();
+        foreach (var publisher in publisherMetadata.Publishers)
+        {
+            foreach (var eventType in publisher.Events)
+            {
+                routing.RegisterPublisher(eventType, publisher.PublisherName);
+            }
+        }
+
+        return Task.FromResult(0);
+    }
+
+    public Task Cleanup()
+    {
+        return Task.FromResult(0);
+    }
+}
---
+++ src/ServiceControl.Transports.SQS/SQSTransportCustomization.cs
@@ -0,0 +1,27 @@
+﻿namespace ServiceControl.Transports.SQS
+{
+    using NServiceBus;
+    using NServiceBus.Raw;
+
+    public class SQSTransportCustomization : TransportCustomization
+    {
+        public override void CustomizeEndpoint(EndpointConfiguration endpointConfig, TransportSettings transportSettings)
+        {           
+            var transport = endpointConfig.UseTransport<SqsTransport>();
+
+            ConfigureTransport(transport, transportSettings);
+        }
+
+        public override void CustomizeRawEndpoint(RawEndpointConfiguration endpointConfig, TransportSettings transportSettings)
+        {
+            var transport = endpointConfig.UseTransport<SqsTransport>();
+            
+            ConfigureTransport(transport, transportSettings);
+        }
+                
+        static void ConfigureTransport(TransportExtensions<SqsTransport> transport, TransportSettings transportSettings)
+        {
+            transport.Transactions(TransportTransactionMode.ReceiveOnly);
+        }
+    }
+}
