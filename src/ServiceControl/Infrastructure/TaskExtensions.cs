852d8d4a3e9339ac678489f1240ac42877bc04d1
Tim <bussmann.t@gmail.com>
2018-07-24 14:18:19 +0200

    add Ignore task extension

--- src/ServiceControl/Infrastructure/TaskExtensions.cs
+++ src/ServiceControl/Infrastructure/TaskExtensions.cs
@@ -4,6 +4,13 @@
 
     public static class TaskEx
     {
+        // ReSharper disable once UnusedParameter.Global
+        // Used to explicitly suppress the compiler warning about
+        // using the returned value from async operations
+        public static void Ignore(this Task task)
+        {
+        }
+
         public static Task CompletedTask = Task.FromResult(0);
     }
 }
