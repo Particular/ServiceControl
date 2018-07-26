fae4697b20f51ba68ab8508dd56f2c94f24852cb
danielmarbach <daniel.marbach@openplace.net>
2018-07-26 15:44:14 +0200

    Provide Connection string and set defaults for hidden settings

--- src/ServiceControl.LoadTests.AuditGenerator/Program.cs
+++ src/ServiceControl.LoadTests.AuditGenerator/Program.cs
@@ -31,6 +31,7 @@
             var minLength = SettingsReader<int>.Read(ConfigRoot, "MinLength", 10000);
             var maxLength = SettingsReader<int>.Read(ConfigRoot, "MaxLength", 20000);
             var endpointName = SettingsReader<string>.Read(ConfigRoot, "EndpointName", "AuditGen");
+            var connectionString = SettingsReader<string>.Read(ConfigRoot, "TransportConnectionString", "");
 
             HostId = Guid.NewGuid().ToString("N");
 
@@ -39,7 +40,14 @@
             config.UseSerialization<NewtonsoftSerializer>();
 
             var customization = (TransportCustomization)Activator.CreateInstance(Type.GetType(customizationTypeName, true));
-            customization.CustomizeEndpoint(config, new TransportSettings());
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = connectionString
+            };
+            transportSettings.Set("TransportSettings.RemoteInstances", Array.Empty<string>());
+            transportSettings.Set("TransportSettings.RemoteTypesToSubscribeTo", Array.Empty<Type>());
+            
+            customization.CustomizeEndpoint(config, transportSettings);
 
             config.UsePersistence<InMemoryPersistence>();
             config.SendFailedMessagesTo("error");
