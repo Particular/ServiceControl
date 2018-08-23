9e9ffb851889b70158abc04e0e5f773c927bf2c1
Kyle Baley <kyle@baley.org>
2018-08-23 11:58:32 -0500

    Update ServiceControlCoreTransports.cs

--- src/ServiceControlInstaller.Engine/Instances/ServiceControlCoreTransports.cs
+++ src/ServiceControlInstaller.Engine/Instances/ServiceControlCoreTransports.cs
@@ -52,7 +52,7 @@
             },
             new TransportInfo
             {
-                Name = "RabbitMQ :: Conventional Routing Topology",
+                Name = "RabbitMQ - Conventional Routing Topology",
                 TypeName = "ServiceControl.Transports.RabbitMQ.RabbitMQConventionalRoutingTransportCustomization, ServiceControl.Transports.RabbitMQ",
                 ZipName = "RabbitMQ",
                 SampleConnectionString = "host=<HOSTNAME>;username=<USERNAME>;password=<PASSWORD>",
@@ -62,7 +62,7 @@
             },
             new TransportInfo
             {
-                Name = "RabbitMQ :: Direct Routing Topology",
+                Name = "RabbitMQ - Direct Routing Topology",
                 TypeName = "ServiceControl.Transports.RabbitMQ.RabbitMQDirectRoutingTransportCustomization, ServiceControl.Transports.RabbitMQ",
                 ZipName = "RabbitMQ",
                 SampleConnectionString = "host=<HOSTNAME>;username=<USERNAME>;password=<PASSWORD>",
