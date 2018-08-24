c8829703baae2491d36c846080a2421e5d46d89e
Kyle Baley <kyle@baley.org>
2018-08-24 14:42:58 -0500

    WIP

---
+++ src/ServiceControl.Transports.ASBS/Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceControl.Transports.ASBS
+{
+    using NServiceBus;
+
+    static class Helper
+    {
+        public static void ConfigureTransport(this TransportExtensions<AzureServiceBusTransport> transport, TransportSettings transportSettings)
+        {
+            transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+            transport.ConnectionString(transportSettings.ConnectionString);
+
+        }
+    }
+}
