b50cac6cdd71429fcd16600ce39cdd44a5345809
danielmarbach <daniel.marbach@openplace.net>
2018-08-24 09:51:17 +0200

    Enable queue schema for SqlTransport

--- src/ServiceControl.Transports.SqlServer/SqlServerTransportCustomization.cs
+++ src/ServiceControl.Transports.SqlServer/SqlServerTransportCustomization.cs
@@ -1,15 +1,17 @@
 ﻿namespace ServiceControl.Transports.SqlServer
 {
+    using System.Data.Common;
     using NServiceBus;
     using NServiceBus.Logging;
     using NServiceBus.Raw;
+    using NServiceBus.Transport.SQLServer;
 
     public class SqlServerTransportCustomization : TransportCustomization
     {
         public override void CustomizeEndpoint(EndpointConfiguration endpointConfig, TransportSettings transportSettings)
         {
             var transport = endpointConfig.UseTransport<SqlServerTransport>();
-            transport.ConnectionString(transportSettings.ConnectionString);
+            ConfigureConnection(transport, transportSettings);
 
             if (transportSettings.GetOrDefault<bool>("TransportSettings.EnableDtc"))
             {
@@ -22,10 +24,28 @@
         public override void CustomizeRawEndpoint(RawEndpointConfiguration endpointConfig, TransportSettings transportSettings)
         {
             var transport = endpointConfig.UseTransport<SqlServerTransport>();
-            transport.ConnectionString(transportSettings.ConnectionString);
+            ConfigureConnection(transport, transportSettings);
             transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
         }
 
+        static void ConfigureConnection(TransportExtensions<SqlServerTransport> transport, TransportSettings transportSettings)
+        {
+            var builder = new DbConnectionStringBuilder
+            {
+                ConnectionString = transportSettings.ConnectionString
+            };
+
+            if (builder.TryGetValue(queueSchemaName, out var customSchema))
+            {
+                builder.Remove(queueSchemaName);
+
+                transport.DefaultSchema((string)customSchema);
+            }
+            
+            transport.ConnectionString(builder.ConnectionString);
+        }
+
+        const string queueSchemaName = "Queue Schema";
         static readonly ILog Logger = LogManager.GetLogger(typeof(SqlServerTransportCustomization));
     }
 }
