1e9a0c48e93914483bd3046f13e3c0d404d907c2
Bob Langley <bob.langley@particular.net>
2018-08-24 20:08:10 -0700

    Replace MessageBodyTransformer in GetAllMessagesApi and SearchEndpointApi

--- src/ServiceControl/CompositeViews/Messages/GetAllMessagesApi.cs
+++ src/ServiceControl/CompositeViews/Messages/GetAllMessagesApi.cs
@@ -3,9 +3,13 @@ namespace ServiceControl.CompositeViews.Messages
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Contracts.Operations;
     using Infrastructure.Extensions;
     using Nancy;
-    using Raven.Client;
+    using NServiceBus;
+    using Raven.Client.Documents;
+    using Raven.Client.Documents.Linq;
+    using SagaAudit;
 
     public class GetAllMessagesApi : ScatterGatherApiMessageView<NoInput>
     {
@@ -18,11 +22,34 @@ namespace ServiceControl.CompositeViews.Messages
                     .Statistics(out var stats)
                     .Sort(request)
                     .Paging(request)
-                    .TransformWith<MessagesViewTransformer, MessagesView>()
+                    .Select(message => new MessagesView
+                    {
+                        Id = message.UniqueMessageId,
+                        MessageId = message.MessageId,
+                        MessageType = message.MessageType,
+                        SendingEndpoint = (EndpointDetails)message.MessageMetadata["SendingEndpoint"],
+                        ReceivingEndpoint = (EndpointDetails)message.MessageMetadata["ReceivingEndpoint"],
+                        TimeSent = message.TimeSent,
+                        ProcessedAt = message.ProcessedAt,
+                        CriticalTime = message.CriticalTime,
+                        ProcessingTime = message.ProcessingTime,
+                        DeliveryTime = message.DeliveryTime,
+                        IsSystemMessage = message.IsSystemMessage,
+                        ConversationId = message.ConversationId,
+                        //the reason the we need to use a KeyValuePair<string, object> is that raven seems to interpret the values and convert them
+                        // to real types. In this case it was the NServiceBus.Temporary.DelayDeliveryWith header to was converted to a timespan
+                        Headers = message.Headers.Select(header => new KeyValuePair<string, object>(header.Key, header.Value)),
+                        Status = message.Status,
+                        MessageIntent = (MessageIntentEnum)message.MessageMetadata["MessageIntent"],
+                        BodyUrl = (string)message.MessageMetadata["BodyUrl"],
+                        BodySize = (int)message.MessageMetadata["ContentLength"],
+                        InvokedSagas = (List<SagaInfo>)message.MessageMetadata["InvokedSagas"],
+                        OriginatesFromSaga = (SagaInfo)message.MessageMetadata["OriginatesFromSaga"]
+                    })
                     .ToListAsync()
                     .ConfigureAwait(false);
 
-                return new QueryResult<List<MessagesView>>(results.ToList(), stats.ToQueryStatsInfo());
+                return new QueryResult<List<MessagesView>>(results, stats.ToQueryStatsInfo());
             }
         }
     }
