5fb5d9ea6703c77bba746e8e5ef4e50a204047d1
Tomasz Masternak <tomasz.masternak@particular.net>
2018-08-27 15:00:48 +0200

    asbs - v6 compatible sanitization for rules and subscriptions

--- src/ServiceControl.Transports.ASBS/ASBSTransportCustomization.cs
+++ src/ServiceControl.Transports.ASBS/ASBSTransportCustomization.cs
@@ -1,13 +1,21 @@
 ﻿namespace ServiceControl.Transports.ASBS
 {
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
     using NServiceBus;
     using NServiceBus.Raw;
 
     public class ASBSTransportCustomization : TransportCustomization
     {
+        const int MaxEntityName = 50;
+
         public override void CustomizeEndpoint(EndpointConfiguration endpointConfig, TransportSettings transportSettings)
         {
             var transport = endpointConfig.UseTransport<AzureServiceBusTransport>();
+
+            transport.SubscriptionNameShortener(n => n.Length > MaxEntityName ? MD5DeterministicNameBuilder.Build(n) : n);
+            transport.RuleNameShortener(n => n.Length > MaxEntityName ? MD5DeterministicNameBuilder.Build(n) : n);
 
             transport.ConfigureTransport(transportSettings);
         }
@@ -18,5 +26,20 @@
 
             transport.ConfigureTransport(transportSettings);
         }
+
+        static class MD5DeterministicNameBuilder
+        {
+            public static string Build(string input)
+            {
+                var inputBytes = Encoding.Default.GetBytes(input);
+                // use MD5 hash to get a 16-byte hash of the string
+                using (var provider = new MD5CryptoServiceProvider())
+                {
+                    var hashBytes = provider.ComputeHash(inputBytes);
+                    return new Guid(hashBytes).ToString();
+                }
+            }
+        }
+
     }
 }
