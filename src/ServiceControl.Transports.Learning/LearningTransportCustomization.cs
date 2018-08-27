fac3d03e34a17974f889da945ba6f674b24ce28c
Daniel Marbach <daniel.marbach@openplace.net>
2018-08-27 15:52:11 +0200

    Added the learning transport (#1385)

---
+++ src/ServiceControl.Transports.Learning/LearningTransportCustomization.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Transports.Learning
+{
+    using NServiceBus;
+    using NServiceBus.Raw;
+
+    public class LearningTransportCustomization : TransportCustomization
+    {
+        public override void CustomizeEndpoint(EndpointConfiguration endpointConfig, TransportSettings transportSettings)
+        {
+            var transport = endpointConfig.UseTransport<LearningTransport>();
+            transport.StorageDirectory(transportSettings.ConnectionString);
+            transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+        }
+
+        public override void CustomizeRawEndpoint(RawEndpointConfiguration endpointConfig, TransportSettings transportSettings)
+        {
+            var transport = endpointConfig.UseTransport<LearningTransport>();
+            transport.StorageDirectory(transportSettings.ConnectionString);
+            transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+        }
+    }
+}
