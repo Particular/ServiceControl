ef8dbfee41967865407676a6a3a4d4ffe36b3675
danielmarbach <daniel.marbach@openplace.net>
2018-09-12 16:17:59 +0200

    Make CheckDeadLetterQueue work on non-english versions of windows

--- src/ServiceControl.Transports.Msmq/CheckDeadLetterQueue.cs
+++ src/ServiceControl.Transports.Msmq/CheckDeadLetterQueue.cs
@@ -13,16 +13,20 @@
         {
             Logger.Debug("MSMQ Dead Letter Queue custom check starting");
 
-            dlqPerformanceCounter = new PerformanceCounter(
-                categoryName: "MSMQ Queue",
-                counterName: "Messages in Queue",
-                instanceName: "Computer Queues",
-                readOnly: true);
+            dlqPerformanceCounter = CreatePerformanceCounter();
         }
 
         public override Task<CheckResult> PerformCheck()
         {
             Logger.Debug("Checking Dead Letter Queue length");
+
+            if (dlqPerformanceCounter == null)
+            {
+                var noCounterAvailable = $"Unable to determine the current Dead Letter Queue length on {Environment.MachineName}. This indicates that the performance counter '{EnglishCounterName}' in category '{EnglishCategoryName}' or its localized version was not found.";
+                Logger.Warn(noCounterAvailable);
+                return CheckResult.Failed(noCounterAvailable);
+            }
+
             var currentValue = dlqPerformanceCounter.NextValue();
 
             if (currentValue <= 0)
@@ -37,7 +41,43 @@
             return CheckResult.Failed(result);
         }
 
+        static PerformanceCounter CreatePerformanceCounter()
+        {
+            // English first to stay backward compatible
+            return CreatePerformanceCounterIfAvailable(EnglishCategoryName, EnglishCounterName) ??
+                   CreateLocalizedPerformanceCounter();
+        }
+
+        static PerformanceCounter CreateLocalizedPerformanceCounter()
+        {
+            var localized = PerformanceCounterHelpers.GetCategoryAndName(EnglishCategoryName, EnglishCounterName);
+            // Fully localized first
+            return CreatePerformanceCounterIfAvailable(localized.Category, localized.Name) ??
+                   CreatePerformanceCounterIfAvailable(EnglishCategoryName, localized.Name) ??
+                   CreatePerformanceCounterIfAvailable(localized.Category, EnglishCounterName);
+        }
+
+        static PerformanceCounter CreatePerformanceCounterIfAvailable(string categoryName, string counterName)
+        {
+            try
+            {
+                return new PerformanceCounter(
+                    categoryName: categoryName,
+                    counterName: counterName,
+                    instanceName: "Computer Queues",
+                    readOnly: true);
+            }
+            catch
+            {
+                Logger.Debug($"Performance counter '{counterName}' with instance name 'Computer Queues' in category '{categoryName}' was not found.");
+                return null;
+            }
+        }
+
         PerformanceCounter dlqPerformanceCounter;
+
+        const string EnglishCategoryName = "MSMQ Queue";
+        const string EnglishCounterName = "Messages in Queue";
 
         static readonly ILog Logger = LogManager.GetLogger(typeof(CheckDeadLetterQueue));
     }
---
+++ src/ServiceControl.Transports.Msmq/PerformanceCounterHelpers.cs
@@ -0,0 +1,59 @@
+﻿namespace ServiceControl.MSMQ.DLQMonitor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using Microsoft.Win32;
+
+    // Inspired by https://wojciechkulik.pl/csharp/get-a-performancecounter-using-english-name
+    static class PerformanceCounterHelpers
+    {
+        [DllImport("pdh.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern UInt32 PdhLookupPerfNameByIndex(string szMachineName, uint dwNameIndex, StringBuilder szNameBuffer, ref uint pcchNameBufferSize);
+
+        public static LocalizedCounterCategoryAndName GetCategoryAndName(string englishCategoryName, string englishCounterName)
+        {
+            // Get list of counters
+            const string perfCountersKey = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Perflib\009";
+            var englishNames = Registry.GetValue(perfCountersKey, "Counter", null) as string[];
+
+            var localizedCategoryName = GetNameByIndex(FindNameId(englishNames, englishCategoryName));
+            var localizedCounterName = GetNameByIndex(FindNameId(englishNames, englishCounterName));
+
+            return new LocalizedCounterCategoryAndName {Category = localizedCategoryName, Name = localizedCounterName};
+        }
+
+        static int FindNameId(IReadOnlyList<string> englishNames, string name)
+        {
+            for (var i = 1; i < englishNames.Count; i += 2)
+            {
+                if (englishNames[i] == name)
+                {
+                    return int.Parse(englishNames[i - 1]);
+                }
+            }
+
+            return -1;
+        }
+
+        static string GetNameByIndex(int id)
+        {
+            if (id < 0)
+            {
+                return null;
+            }
+
+            var buffer = new StringBuilder(1024);
+            var bufSize = (uint)buffer.Capacity;
+            var ret = PdhLookupPerfNameByIndex(null, (uint)id, buffer, ref bufSize);
+            return ret == 0 && buffer.Length != 0 ? buffer.ToString() : null;
+        }
+
+        public struct LocalizedCounterCategoryAndName
+        {
+            public string Category;
+            public string Name;
+        }
+    }
+}
