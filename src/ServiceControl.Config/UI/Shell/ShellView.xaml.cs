d2af85225170f738edaa394cef722f8c5fb46865
Mike Minutillo <michael.minutillo@gmail.com>
2018-10-24 17:34:51 +0800

    Do not show tooltip on hover

--- src/ServiceControl.Config/UI/Shell/ShellView.xaml.cs
+++ src/ServiceControl.Config/UI/Shell/ShellView.xaml.cs
@@ -1,9 +1,7 @@
 ﻿namespace ServiceControl.Config.UI.Shell
 {
     using System;
-    using System.ComponentModel;
     using System.Windows;
-    using System.Windows.Controls;
     using Caliburn.Micro;
     using Events;
 
@@ -27,18 +25,6 @@
                 window.LocationChanged += Window_LocationChanged;
                 window.SizeChanged += Window_SizeChanged;
             };
-
-            var dpd = DependencyPropertyDescriptor.FromProperty(IsMouseOverProperty, typeof(Image));
-            dpd?.AddValueChanged(WarningIcon, OnIsMouseOverChanged);
-            dpd?.AddValueChanged(ErrorIcon, OnIsMouseOverChanged);
-        }
-
-        void OnIsMouseOverChanged(object sender, EventArgs e)
-        {
-            if (WarningIcon.IsMouseOver || ErrorIcon.IsMouseOver)
-            {
-                licenseWarningPopup.IsOpen = true;
-            }
         }
 
         void Window_SizeChanged(object sender, SizeChangedEventArgs e)
