4a0c30dfc2b5979c0d1d4c637e5f7587fe7c7ed7
danielmarbach <daniel.marbach@openplace.net>
2018-11-20 09:59:16 +0100

    Enforce name length

---
+++ src/ServiceControl.AcceptanceTests/SelfVerification/EndpointNameEnforcementTests.cs
@@ -0,0 +1,32 @@
+﻿namespace ServiceBus.Management.AcceptanceTests.SelfVerification
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Customization;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class EndpointNameEnforcementTests : AcceptanceTest
+    {
+        const int endpointNameMaxLength = 77;
+
+        [Test]
+        public void EndpointName_should_not_exceed_maximum_length()
+        {
+            var testTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(IsEndpointClass);
+
+            var violators = testTypes
+                .Where(t => Conventions.EndpointNamingConvention(t).Length > endpointNameMaxLength)
+                .ToList();
+
+            CollectionAssert.IsEmpty(violators, string.Join(",", violators));
+        }
+
+        static bool IsEndpointClass(Type t) => endpointConfigurationBuilderType.IsAssignableFrom(t);
+
+        static Type endpointConfigurationBuilderType = typeof(EndpointConfigurationBuilder);
+    }
+}
