41674a2626661f2219482bb90d6811ffdd249019
SzymonPobiega <szymon.pobiega@gmail.com>
2018-12-11 11:39:54 +0100

    Fix inspections

--- src/ServiceControl.AcceptanceTests/SagaAudit/When_multiple_messages_are_emitted_by_a_saga.cs
+++ src/ServiceControl.AcceptanceTests/SagaAudit/When_multiple_messages_are_emitted_by_a_saga.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
-    using System.Threading;
     using System.Threading.Tasks;
     using EndpointTemplates;
     using Infrastructure.Settings;
