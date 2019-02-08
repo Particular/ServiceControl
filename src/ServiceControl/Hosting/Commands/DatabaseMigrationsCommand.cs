79166edf7bd4efdd27f05dab24360dba96b05917
Daniel Marbach <daniel.marbach@openplace.net>
2019-02-08 08:13:22 +0100

    Stop the bootstrapper properly (#1588)
    Catch OperationCanceledException for shutdown scenarios
    Make command runner infrastructure async

--- src/ServiceControl/Hosting/Commands/DatabaseMigrationsCommand.cs
+++ src/ServiceControl/Hosting/Commands/DatabaseMigrationsCommand.cs
@@ -1,13 +1,15 @@
 ﻿namespace Particular.ServiceControl.Commands
 {
+    using System.Threading.Tasks;
     using Hosting;
     using ServiceBus.Management.Infrastructure.Settings;
 
     class DatabaseMigrationsCommand : AbstractCommand
     {
-        public override void Execute(HostArguments args)
+        public override Task Execute(HostArguments args)
         {
             new DatabaseMigrationsBootstrapper().Run(new Settings(args.ServiceName));
+            return Task.CompletedTask;
         }
     }
 }
