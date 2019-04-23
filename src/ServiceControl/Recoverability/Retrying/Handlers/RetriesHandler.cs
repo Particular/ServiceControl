df18a30e2f82cf4cc5ae8886eb7b6f27a9e30eb2
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-04-23 16:59:04 +0200

    add unit tests

--- src/ServiceControl/Recoverability/Retrying/Handlers/RetriesHandler.cs
+++ src/ServiceControl/Recoverability/Retrying/Handlers/RetriesHandler.cs
@@ -7,6 +7,7 @@ namespace ServiceControl.Recoverability
     using MessageFailures;
     using MessageFailures.Api;
     using MessageFailures.InternalMessages;
+    using MessageRedirects;
     using NServiceBus;
     using NServiceBus.Extensibility;
     using NServiceBus.Routing;
@@ -69,26 +70,29 @@ namespace ServiceControl.Recoverability
         public async Task Handle(RetryWithModifications message, IMessageHandlerContext context)
         {
             FailedMessage failedMessage;
+            MessageRedirectsCollection redirects;
             using (var session = Store.OpenAsyncSession())
             {
                 failedMessage = await session.LoadAsync<FailedMessage>(FailedMessage.MakeDocumentId(message.FailedMessageId))
                     .ConfigureAwait(false);
-                //TODO check for failedMessage.Status = FailedMessageStatus.RetryIssued?
 
                 if (failedMessage == null)
                 {
+                    //TODO: log
                     return;
                 }
-
-                // todo: introduce a new state?
-                failedMessage.Status = FailedMessageStatus.Resolved;
-                // todo: complete "original" message when edited message completes.
 
                 //TODO do we need to prefix the id?
                 var edit = await session.LoadAsync<FailedMessageEdit>(message.FailedMessageId)
                     .ConfigureAwait(false);
                 if (edit == null)
                 {
+                    if (failedMessage.Status != FailedMessageStatus.Unresolved)
+                    {
+                        //TODO log
+                        return;
+                    }
+
                     // create a retries document to prevent concurrent edits
                     await session.StoreAsync(new FailedMessageEdit
                     {
@@ -99,42 +103,38 @@ namespace ServiceControl.Recoverability
                 else if(edit.EditId != context.MessageId)
                 {
                     // this is a concurrent edit -> discard
+                    //TODO log
                     return;
                 }
-                
+
+                // todo: introduce a new state?
+                failedMessage.Status = FailedMessageStatus.Resolved;
+
+                redirects = await MessageRedirectsCollection.GetOrCreate(session)
+                    .ConfigureAwait(false);
+
                 await session.SaveChangesAsync().ConfigureAwait(false);
             }
 
-
-            // send
-
-            //TODO not accessible here
-            //var messageId = CombGuid.Generate();
-            var messageId = Guid.NewGuid().ToString("N");
-
+            var messageId = CombGuid.Generate().ToString();
             var attempt = failedMessage.ProcessingAttempts.Last();
             var headers = HeaderFilter.RemoveErrorMessageHeaders(attempt.Headers);
             headers[Headers.MessageId] = Guid.NewGuid().ToString("D");
             headers.Add("ServiceControl.EditOf", attempt.MessageId);
+
             //TODO: do we need the CorruptedReplyToHeaderStrategy as well?
 
             var body = Convert.FromBase64String(message.NewBody);
             var outgoingMessage = new OutgoingMessage(messageId, headers, body);
-            AddressTag destination = new UnicastAddressTag(attempt.FailureDetails.AddressOfFailingEndpoint);
 
-            //                var redirect = redirects[addressOfFailingEndpoint];
-            //                if (redirect != null)
-            //                {
-            //                    addressOfFailingEndpoint = redirect.ToPhysicalAddress;
-            //                }
+            var destination = CreateDestinationAddress(attempt, redirects);
 
-            //TODO: can we use the incoming transaction?
+            var transportTransaction = context.Extensions.GetOrCreate<TransportTransaction>();
             await Dispatcher.Dispatch(
                     new TransportOperations(new TransportOperation(outgoingMessage, destination)),
-                    new TransportTransaction(),
+                    transportTransaction,
                     new ContextBag())
-                .ConfigureAwait(false);
-            
+                .ConfigureAwait(false); 
         }
 
         public Task Handle(RetryMessagesById message, IMessageHandlerContext context)
@@ -149,6 +149,19 @@ namespace ServiceControl.Recoverability
             Retries.StartRetryForIndex<FailedMessageViewIndex.SortAndFilterOptions, FailedMessageViewIndex>(failedQueueAddress, RetryType.ByQueueAddress, DateTime.UtcNow, m => m.QueueAddress == failedQueueAddress && m.Status == message.Status, $"all messages for failed queue address '{message.QueueAddress}'");
 
             return Task.FromResult(0);
+        }
+
+        static AddressTag CreateDestinationAddress(FailedMessage.ProcessingAttempt attempt, MessageRedirectsCollection redirects)
+        {
+            var addressOfFailingEndpoint = attempt.FailureDetails.AddressOfFailingEndpoint;
+            var redirect = redirects[addressOfFailingEndpoint];
+            if (redirect != null)
+            {
+                addressOfFailingEndpoint = redirect.ToPhysicalAddress;
+            }
+
+            AddressTag destination = new UnicastAddressTag(addressOfFailingEndpoint);
+            return destination;
         }
     }
 
