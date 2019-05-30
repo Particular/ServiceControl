34b5597e73614bde5e13c74cab24bb3a0bf8719a
WilliamBZA <william.brander@gmail.com>
2019-05-30 10:01:33 +0200

    Build asb docker container

--- src/ServiceControl/Infrastructure/Settings/SettingsReader.cs
+++ src/ServiceControl/Infrastructure/Settings/SettingsReader.cs
@@ -9,6 +9,11 @@
 
         public static T Read(string root, string name, T defaultValue = default)
         {
+            if (EnvironmentVariableSettingsReader<T>.TryRead(root, name, out var envValue))
+            {
+                return envValue;
+            }
+
             if (ConfigFileSettingsReader<T>.TryRead(root, name, out var value))
             {
                 return value;
