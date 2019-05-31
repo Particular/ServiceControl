f8588b61e6491381bb2f254d8c7a31f32d42342c
WilliamBZA <william.brander@gmail.com>
2019-05-31 14:25:17 +0200

    Add rabbit conventional container

--- src/builddockerimage.sh
+++ src/builddockerimage.sh
@@ -1,3 +1,4 @@
 msbuild ServiceControl.sln //t:Build //p:Configuration=Release
 
 docker build -f .\dockerfile.asb -t servicecontrolasb .
+docker build -f .\dockerfile.rabbitmq.conventional -t servicecontrolrabbitconventional .
