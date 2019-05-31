18f0cd0e295c735f3313d0508ab3ef1ce42d33f5
WilliamBZA <william.brander@gmail.com>
2019-05-31 15:05:36 +0200

    Add ASQ container

--- src/builddockerimage.ps1
+++ src/builddockerimage.ps1
@@ -3,5 +3,8 @@ msbuild ServiceControl.sln //t:Build //p:Configuration=Release
 docker build -f .\dockerfile.asb.endpoint -t servicecontrolasbendpoint .
 docker build -f .\dockerfile.asb.forwarding -t servicecontrolasbforwarding .
 docker build -f .\dockerfile.asb.asbs -t servicecontrolasbs .
+
 docker build -f .\dockerfile.rabbitmq.conventional -t servicecontrolrabbitconventional .
 docker build -f .\dockerfile.rabbitmq.direct -t servicecontrolrabbitdirect .
+
+docker build -f .\dockerfile.asq -t servicecontrolasq .
