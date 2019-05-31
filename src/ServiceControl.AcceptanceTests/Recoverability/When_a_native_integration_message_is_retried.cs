5d42299878e393f7524aaebe36d4b62e0c6c5c94
Mike Minutillo <mike.minutillo@particular.net>
2019-05-31 16:55:54 +0800

    Fix ASQ failure

--- src/ServiceControl.AcceptanceTests/Recoverability/When_a_native_integration_message_is_retried.cs
+++ src/ServiceControl.AcceptanceTests/Recoverability/When_a_native_integration_message_is_retried.cs
@@ -33,7 +33,11 @@
                 })
                 .Run();
 
-            Assert.False(context.Headers.ContainsKey(Headers.MessageIntent), "Should not add the intent header");
+            // ASQ adds this value to it's message wrapper and writes the incoming message header 
+            if (TransportIntegration.Name != TransportNames.AzureStorageQueue)
+            {
+                Assert.False(context.Headers.ContainsKey(Headers.MessageIntent), "Should not add the intent header");
+            }
 
             //Rabbit defaults the header the header when deserializing the message based on the IBasicProperties.DeliveryMode
             if (TransportIntegration.Name == TransportNames.RabbitMQConventionalRoutingTopology || TransportIntegration.Name == TransportNames.RabbitMQDirectRoutingTopology)
