438dc1e38543a9de37e68c15d21a795f24e16cf8
danielmarbach <daniel.marbach@openplace.net>
2019-06-03 08:27:55 +0200

    Fix scatter gather unit tests

--- src/ServiceControl.UnitTests/ScatterGather/MessageView_ScatterGatherTest.cs
+++ src/ServiceControl.UnitTests/ScatterGather/MessageView_ScatterGatherTest.cs
@@ -19,23 +19,23 @@
 
             var request = new Request("GET", new Url($"http://doesnt/really/matter?{QueryString}"));
 
-            Results = api.AggregateResults(request, GetData().ToArray());
+            Results = api.AggregateResults(request, GetData());
         }
 
-        protected abstract IEnumerable<QueryResult<List<MessagesView>>> GetData();
+        protected abstract QueryResult<IList<MessagesView>>[] GetData();
 
-        protected QueryResult<List<MessagesView>> LocalPage(int page, int pageSize = PageSize)
+        protected QueryResult<IList<MessagesView>> LocalPage(int page, int pageSize = PageSize)
             => GetPage(LocalData(), LocalInstanceID, LocalETag, page, pageSize);
 
-        protected QueryResult<List<MessagesView>> RemotePage(int page, int pageSize = PageSize)
+        protected QueryResult<IList<MessagesView>> RemotePage(int page, int pageSize = PageSize)
             => GetPage(RemoteData(), RemoteInstanceId, RemoteETag, page, pageSize);
 
-        private QueryResult<List<MessagesView>> GetPage(IEnumerable<MessagesView> source, string instanceId, string etag, int page, int pageSize)
+        private QueryResult<IList<MessagesView>> GetPage(IEnumerable<MessagesView> source, string instanceId, string etag, int page, int pageSize)
         {
             var allResults = source.OrderBy(_ => Guid.NewGuid()).ToList();
             var pageOfResults = allResults.Skip((page - 1) * pageSize).Take(pageSize).ToList();
 
-            return new QueryResult<List<MessagesView>>(
+            return new QueryResult<IList<MessagesView>>(
                 pageOfResults,
                 new QueryStatsInfo(etag, allResults.Count))
             {
@@ -59,7 +59,7 @@
             }
         }
 
-        protected QueryResult<List<MessagesView>> Results;
+        protected QueryResult<IList<MessagesView>> Results;
         protected const string LocalInstanceID = nameof(LocalInstanceID);
         protected const string LocalETag = nameof(LocalETag);
         protected const string RemoteInstanceId = nameof(RemoteInstanceId);
@@ -68,7 +68,7 @@
 
         class TestApi : ScatterGatherApiMessageView<NoInput>
         {
-            public override Task<QueryResult<List<MessagesView>>> LocalQuery(Request request, NoInput input)
+            public override Task<QueryResult<IList<MessagesView>>> LocalQuery(Request request, NoInput input)
             {
                 throw new NotImplementedException();
             }
