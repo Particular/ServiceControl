87651572b4f8469a977c5e8eb95dac8726d388db
Andreas Öhlund <andreas.ohlund@particular.net>
2019-06-10 17:56:56 +0200

    usings

--- src/ServiceControl.Audit/Auditing/IEnrichImportedAuditMessages.cs
+++ src/ServiceControl.Audit/Auditing/IEnrichImportedAuditMessages.cs
@@ -1,7 +1,5 @@
 namespace ServiceControl.Audit.Auditing
 {
-    using System.Threading.Tasks;
-
     interface IEnrichImportedAuditMessages
     {
         void Enrich(AuditEnricherContext context);
