5e6170fd8712e8bd7c63a1df014cb84864ff81fb
Mike Minutillo <mike.minutillo@particular.net>
2019-06-11 13:52:47 +0800

    Add connection switching

--- src/ServiceControl.AcceptanceTests/TestSupport/TestSuiteConstraints.cs
+++ src/ServiceControl.AcceptanceTests/TestSupport/TestSuiteConstraints.cs
@@ -1,6 +1,8 @@
 ﻿namespace NServiceBus.AcceptanceTests
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using AcceptanceTesting.Support;
     using ServiceBus.Management.AcceptanceTests;
 
@@ -11,15 +13,61 @@
         public bool SupportsNativePubSub => true;
         public bool SupportsNativeDeferral => true;
         public bool SupportsOutbox => false;
-        public IConfigureEndpointTestExecution CreateTransportConfiguration() => GetTransportIntegrationFromEnvironmentVar();
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => 
+            GetTransportIntegrationFromConnectionFile()
+            ?? GetTransportIntegrationFromEnvironmentVar()
+            ?? new ConfigureEndpointMsmqTransport();
+
         public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointInMemoryPersistence();
 
-        static ITransportIntegration GetTransportIntegrationFromEnvironmentVar()
-        {
-            var transportCustomizationToUseString = Environment.GetEnvironmentVariable("ServiceControl.AcceptanceTests.TransportCustomization") ?? typeof(ConfigureEndpointMsmqTransport).Name;
-            var transportToUse = (ITransportIntegration)Activator.CreateInstance(Type.GetType(transportCustomizationToUseString));
+        static ITransportIntegration GetTransportIntegrationFromEnvironmentVar() => CreateTransportIntegration(
+                Environment.GetEnvironmentVariable("ServiceControl.AcceptanceTests.TransportCustomization"),
+                Environment.GetEnvironmentVariable("ServiceControl.AcceptanceTests.ConnectionString")
+            );
 
-            var connectionString = Environment.GetEnvironmentVariable("ServiceControl.AcceptanceTests.ConnectionString");
+        static ITransportIntegration GetTransportIntegrationFromConnectionFile()
+        {
+            var connectionFile = GetConnectionFile();
+            if (connectionFile == null || !File.Exists(connectionFile))
+            {
+                return null;
+            }
+            
+            var lines = File.ReadLines(connectionFile)
+                .Skip(1) // Name
+                .Take(2) // TransportCustomizationTypeName, ConnectionString
+                .ToArray();
+
+            return CreateTransportIntegration(lines[0], lines[1]);
+
+        }
+
+        static string GetConnectionFile()
+        {
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+            while (directory != null)
+            {
+                if (Directory.EnumerateFiles(directory).Any(file => file.EndsWith(".sln")))
+                {
+                    return Path.Combine(directory, "connection.txt");
+                }
+
+                var parent = Directory.GetParent(directory);
+
+                directory = parent?.FullName;
+            }
+
+            return null;
+        }
+
+        static ITransportIntegration CreateTransportIntegration(string transportCustomizationToUseString, string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(transportCustomizationToUseString))
+            {
+                return null;
+            }
+
+            var transportToUse = (ITransportIntegration)Activator.CreateInstance(Type.GetType(transportCustomizationToUseString));
             if (!string.IsNullOrWhiteSpace(connectionString))
             {
                 transportToUse.ConnectionString = connectionString;
