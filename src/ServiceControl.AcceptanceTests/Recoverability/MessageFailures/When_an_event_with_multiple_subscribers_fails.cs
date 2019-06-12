156be5fdf0ac1092e75264c9426f64d3f58308cf
Mike Minutillo <mike.minutillo@particular.net>
2019-06-12 16:52:25 +0800

    Do not rely on pub-sub

--- src/ServiceControl.AcceptanceTests/Recoverability/MessageFailures/When_an_event_with_multiple_subscribers_fails.cs
+++ src/ServiceControl.AcceptanceTests/Recoverability/MessageFailures/When_an_event_with_multiple_subscribers_fails.cs
@@ -6,8 +6,8 @@
     using System.Threading.Tasks;
     using NServiceBus;
     using NServiceBus.AcceptanceTesting;
-    using NServiceBus.AcceptanceTesting.Customization;
-    using NServiceBus.AcceptanceTests;
+    using NServiceBus.Routing;
+    using NServiceBus.Transport;
     using NUnit.Framework;
     using ServiceBus.Management.AcceptanceTests;
     using ServiceBus.Management.AcceptanceTests.EndpointTemplates;
@@ -21,138 +21,63 @@
             var failedMessages = new List<FailedMessageView>();
 
             await Define<FailingEventContext>()
-                .WithEndpoint<FailingSubscriber1>(behavior => behavior.When(async (bus, ctx) =>
+                .WithEndpoint<SimulateTwoFailedMessagesFromOnePublish>()
+                .Done(async ctx =>
                 {
-                    await bus.Subscribe<SampleEvent>();
-
-                    if (ctx.HasNativePubSubSupport)
-                    {
-                        ctx.Subscriber1Subscribed = true;
-                    }
-                }).DoNotFailOnErrorMessages())
-                .WithEndpoint<FailingSubscriber2>(behavior => behavior.When(async (bus, ctx) =>
-                {
-                    await bus.Subscribe<SampleEvent>();
-
-                    if (ctx.HasNativePubSubSupport)
-                    {
-                        ctx.Subscriber2Subscribed = true;
-                    }
-                }).DoNotFailOnErrorMessages())
-                .WithEndpoint<Publisher>(behavior => behavior
-                    .CustomConfig(cfg => cfg.OnEndpointSubscribed<FailingEventContext>((s, ctx) =>
-                        {
-                            if (s.SubscriberReturnAddress.IndexOf("Subscriber1", StringComparison.OrdinalIgnoreCase) >= 0)
-                            {
-                                ctx.Subscriber1Subscribed = true;
-                            }
-                            else if (s.SubscriberReturnAddress.IndexOf("Subscriber2", StringComparison.OrdinalIgnoreCase) >= 0)
-                            {
-                                ctx.Subscriber2Subscribed = true;
-                            }
-                            else
-                            {
-                                throw new Exception($"Unknown subscriber subscribed to publisher: {s.SubscriberReturnAddress}");
-                            }
-                        })
-                    ).When(
-                        ctx => ctx.Subscriber1Subscribed && ctx.Subscriber2Subscribed,
-                        bus => bus.Publish<SampleEvent>()
-                    ).DoNotFailOnErrorMessages()
-                ).Done(async ctx =>
-                {
-                    if (!ctx.Subscriber1Received || !ctx.Subscriber2Received)
-                    {
-                        return false;
-                    }
-
                     var result = await this.TryGetMany<FailedMessageView>("/api/errors");
                     failedMessages = result;
                     return result && failedMessages.Sum(x => x.NumberOfProcessingAttempts) >= 2;
                 })
                 .Run();
 
-            var subscriber1FailedMessage = failedMessages.SingleOrDefault(msg => msg.ReceivingEndpoint.Name.Contains("Subscriber1"));
-            var subscriber2FailedMessage = failedMessages.SingleOrDefault(msg => msg.ReceivingEndpoint.Name.Contains("Subscriber2"));
+            var subscriber1FailedMessage = failedMessages.SingleOrDefault(msg => msg.QueueAddress.Contains("subscriber1"));
+            var subscriber2FailedMessage = failedMessages.SingleOrDefault(msg => msg.QueueAddress.Contains("subscriber2"));
 
             Assert.IsNotNull(subscriber1FailedMessage, "Subscriber1 did not report failed message");
             Assert.IsNotNull(subscriber2FailedMessage, "Subscriber2 did not report failed message");
             Assert.AreNotSame(subscriber1FailedMessage, subscriber2FailedMessage, "There should be two distinct failed messages");
         }
 
-        public class Publisher : EndpointConfigurationBuilder
+        public class SimulateTwoFailedMessagesFromOnePublish : EndpointConfigurationBuilder
         {
-            public Publisher()
+            public SimulateTwoFailedMessagesFromOnePublish()
             {
                 EndpointSetup<DefaultServer>();
             }
-        }
 
-        public class FailingSubscriber1 : EndpointConfigurationBuilder
-        {
-            public FailingSubscriber1()
+            class SendDuplicateMessages : DispatchRawMessages<FailingEventContext>
             {
-                EndpointSetup<DefaultServer>(c =>
+                protected override TransportOperations CreateMessage(FailingEventContext context)
                 {
-                    var recoverability = c.Recoverability();
-                    recoverability.Immediate(s => s.NumberOfRetries(0));
-                    recoverability.Delayed(s => s.NumberOfRetries(0));
+                    var messageId = Guid.NewGuid().ToString();
 
-                    var routing = c.ConfigureTransport().Routing();
-                    routing.RouteToEndpoint(typeof(SampleEvent), typeof(Publisher));
-                }, metadata => metadata.RegisterPublisherFor<SampleEvent>(typeof(Publisher)));
-            }
+                    var headers = new Dictionary<string, string>
+                    {
+                        [Headers.MessageId] = messageId,
+                        [Headers.ProcessingEndpoint] = "subscriber1",
+                        ["NServiceBus.FailedQ"] = "subscriber1"
+                    };
 
-            public class SampleEventHandler : IHandleMessages<SampleEvent>
-            {
-                public FailingEventContext Context { get; set; }
+                    var headers2 = new Dictionary<string, string>
+                    {
+                        [Headers.MessageId] = messageId,
+                        [Headers.ProcessingEndpoint] = "subscriber2",
+                        ["NServiceBus.FailedQ"] = "subscriber2"
+                    };
 
-                public Task Handle(SampleEvent message, IMessageHandlerContext context)
-                {
-                    Context.Subscriber1Received = true;
-                    throw new Exception("Failing Subscriber 1");
+                    var outgoingMessage = new OutgoingMessage(messageId, headers, new byte[0]);
+                    var outgoingMessage2 = new OutgoingMessage(messageId, headers2, new byte[0]);
+
+                    return new TransportOperations(
+                        new TransportOperation(outgoingMessage, new UnicastAddressTag("error")), 
+                        new TransportOperation(outgoingMessage2, new UnicastAddressTag("error"))
+                        );
                 }
             }
-        }
-
-        public class FailingSubscriber2 : EndpointConfigurationBuilder
-        {
-            public FailingSubscriber2()
-            {
-                EndpointSetup<DefaultServer>(c =>
-                {
-                    var recoverability = c.Recoverability();
-                    recoverability.Immediate(s => s.NumberOfRetries(0));
-                    recoverability.Delayed(s => s.NumberOfRetries(0));
-
-                    var routing = c.ConfigureTransport().Routing();
-                    routing.RouteToEndpoint(typeof(SampleEvent), typeof(Publisher));
-                }, metadata => metadata.RegisterPublisherFor<SampleEvent>(typeof(Publisher)));
-            }
-
-            public class SampleEventHandler : IHandleMessages<SampleEvent>
-            {
-                public FailingEventContext Context { get; set; }
-
-                public Task Handle(SampleEvent message, IMessageHandlerContext context)
-                {
-                    Context.Subscriber2Received = true;
-                    throw new Exception("Failing Subscriber 2");
-                }
-            }
-        }
-
-
-        public class SampleEvent : IEvent
-        {
         }
 
         public class FailingEventContext : ScenarioContext
         {
-            public bool Subscriber1Subscribed { get; set; }
-            public bool Subscriber1Received { get; set; }
-            public bool Subscriber2Subscribed { get; set; }
-            public bool Subscriber2Received { get; set; }
         }
     }
 }
