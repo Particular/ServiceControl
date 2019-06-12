1b02fd117e42f6f3241ac4cb0b9dbc01f0896780
danielmarbach <daniel.marbach@openplace.net>
2019-06-12 17:23:06 +0200

    Very rough check remotes custom check

---
+++ src/ServiceControl/Operations/CheckRemotes.cs
@@ -0,0 +1,86 @@
+﻿namespace ServiceControl.Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus.CustomChecks;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    class CheckRemotes : CustomCheck
+    {
+        RemoteInstanceSetting[] remoteInstanceSetting;
+        readonly Func<HttpClient> httpClientFactory;
+        List<Task> remoteQueryTasks;
+
+        public CheckRemotes(Settings settings, Func<HttpClient> httpClientFactory) : base("ServiceControl Remotes", "Health", TimeSpan.FromSeconds(30))
+        {
+            this.httpClientFactory = httpClientFactory;
+            remoteInstanceSetting = settings.RemoteInstances;
+            remoteQueryTasks = new List<Task>(remoteInstanceSetting.Length);
+        }
+
+        public override async Task<CheckResult> PerformCheck()
+        {
+            var httpClient = httpClientFactory();
+
+            try
+            {
+                using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10)))
+                {
+                    foreach (var remote in remoteInstanceSetting)
+                    {
+                        async Task CheckSuccessStatusCode(HttpClient client, RemoteInstanceSetting remoteSettings, CancellationToken token)
+                        {
+                            try
+                            {
+                                var response = await client.GetAsync(remoteSettings.ApiUri, token).ConfigureAwait(false);
+                                response.EnsureSuccessStatusCode();
+                            }
+                            catch (HttpRequestException e)
+                            {
+                                throw new TimeoutException($"Remote at '{remoteSettings.ApiUri}' doesn't seem to be available. Reason: {e.Message}", e);
+                            }
+                            catch (OperationCanceledException e)
+                            {
+                                throw new TimeoutException($"Remote at '{remoteSettings.ApiUri}' did not respond within the allotted timespan.", e);
+                            }
+                        }
+                        remoteQueryTasks.Add(CheckSuccessStatusCode(httpClient, remote, cancellationTokenSource.Token));
+                    }
+
+                    try
+                    {
+                        await Task.WhenAll(remoteQueryTasks).ConfigureAwait(false);
+                        return CheckResult.Pass;
+                    }
+                    catch (Exception)
+                    {
+                        var builder = new StringBuilder();
+
+                        foreach (var task in remoteQueryTasks)
+                        {
+                            try
+                            {
+                                await task.ConfigureAwait(false);
+                            }
+                            catch (TimeoutException e)
+                            {
+                                builder.AppendLine(e.Message);
+                            }
+                        }
+
+                        return CheckResult.Failed(builder.ToString());
+                    }
+                }
+            }
+            finally
+            {
+                remoteQueryTasks.Clear();
+            }
+            
+        }
+    }
+}
