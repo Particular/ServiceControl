7971d657428f0bade07589c660bab076399f79d9
Daniel Marbach <daniel.marbach@particular.net>
2019-06-13 10:35:06 +0200

    CustomCheck that pings remote instances (#1680)
    * Very rough check remotes custom check
    
    * Make sure we have ServiceControlQueueAddress set for local testing
    
    * Cleanup
    
    * Display query timeout

---
+++ src/ServiceControl/Operations/CheckRemotes.cs
@@ -0,0 +1,87 @@
+﻿namespace ServiceControl.Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus.CustomChecks;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    class CheckRemotes : CustomCheck
+    {
+        public CheckRemotes(Settings settings, Func<HttpClient> httpClientFactory) : base("ServiceControl Remotes", "Health", TimeSpan.FromSeconds(30))
+        {
+            this.httpClientFactory = httpClientFactory;
+            remoteInstanceSetting = settings.RemoteInstances;
+            remoteQueryTasks = new List<Task>(remoteInstanceSetting.Length);
+        }
+
+        public override async Task<CheckResult> PerformCheck()
+        {
+            var httpClient = httpClientFactory();
+
+            try
+            {
+                var queryTimeout = TimeSpan.FromSeconds(10);
+                using (var cancellationTokenSource = new CancellationTokenSource(queryTimeout))
+                {
+                    foreach (var remote in remoteInstanceSetting)
+                    {
+                        remoteQueryTasks.Add(CheckSuccessStatusCode(httpClient, remote, queryTimeout, cancellationTokenSource.Token));
+                    }
+
+                    try
+                    {
+                        await Task.WhenAll(remoteQueryTasks).ConfigureAwait(false);
+                        return CheckResult.Pass;
+                    }
+                    catch (Exception)
+                    {
+                        var builder = new StringBuilder();
+
+                        foreach (var task in remoteQueryTasks)
+                        {
+                            try
+                            {
+                                await task.ConfigureAwait(false);
+                            }
+                            catch (TimeoutException e)
+                            {
+                                builder.AppendLine(e.Message);
+                            }
+                        }
+
+                        return CheckResult.Failed(builder.ToString());
+                    }
+                }
+            }
+            finally
+            {
+                remoteQueryTasks.Clear();
+            }
+        }
+
+        static async Task CheckSuccessStatusCode(HttpClient client, RemoteInstanceSetting remoteSettings, TimeSpan queryTimeout, CancellationToken token)
+        {
+            try
+            {
+                var response = await client.GetAsync(remoteSettings.ApiUri, token).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                throw new TimeoutException($"Remote at '{remoteSettings.ApiUri}' doesn't seem to be available. Reason: {e.Message}", e);
+            }
+            catch (OperationCanceledException e)
+            {
+                throw new TimeoutException($"Remote at '{remoteSettings.ApiUri}' did not respond within the allotted timespan of '{queryTimeout}'.", e);
+            }
+        }
+
+        readonly Func<HttpClient> httpClientFactory;
+        RemoteInstanceSetting[] remoteInstanceSetting;
+        List<Task> remoteQueryTasks;
+    }
+}
