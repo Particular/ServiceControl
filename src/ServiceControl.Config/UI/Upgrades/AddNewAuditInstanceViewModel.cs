a2f7c709e29d581947102ba037cd7d2a557b3852
Hadi Eskandari <h.eskandari@gmail.com>
2019-06-26 13:03:54 +1000

    SC upgrade view/viewmodel for capturing audit info

---
+++ src/ServiceControl.Config/UI/Upgrades/AddNewAuditInstanceViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Config.UI.Upgrades
+{
+    using System.Windows.Input;
+    using Framework.Rx;
+    using InstanceAdd;
+    using Validation;
+
+    public class AddNewAuditInstanceViewModel : RxScreen
+    {
+        public AddNewAuditInstanceViewModel(string serviceControlName)
+        {
+            ServiceControlAudit = new ServiceControlAuditInformation();
+            ServiceControlAudit.ApplyConventionalServiceName(serviceControlName);
+        }
+
+        public ServiceControlAuditInformation ServiceControlAudit { get; set; }
+        public ValidationTemplate ValidationTemplate { get; set; }
+        public ICommand Cancel { get; set; }
+        public ICommand Continue { get; set; }
+        public bool SubmitAttempted { get; set; }
+    }
+}
