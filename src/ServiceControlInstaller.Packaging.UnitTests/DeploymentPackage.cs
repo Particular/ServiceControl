b9db8def61b8d5876418229e8dd85b63251f09b5
Mike Minutillo <mike.minutillo@particular.net>
2019-08-08 13:06:33 +0800

    Test for file size mismatch

---
+++ src/ServiceControlInstaller.Packaging.UnitTests/DeploymentPackage.cs
@@ -0,0 +1,49 @@
+namespace Tests
+{
+    using System;
+    using System.Collections;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class DeploymentPackage
+    {
+        FileInfo zipFile;
+
+        public DeploymentPackage(FileInfo zipFile)
+        {
+            this.zipFile = zipFile;
+            ServiceName = zipFile.Name
+                .Replace("Particular.", "")
+                .Split('-')
+                .First();
+        }
+
+        public override string ToString() => ServiceName.Replace(".", " ");
+        public string ServiceName { get; }
+        public string FullName => zipFile.FullName;
+        public ZipArchive Open() => ZipFile.OpenRead(FullName);
+
+        static IEnumerable All = GetZipFolder()
+            .EnumerateFiles("*.zip")
+            .Select(x => new DeploymentPackage(x));
+
+        static DirectoryInfo GetZipFolder()
+        {
+            var currentFolder = new DirectoryInfo(TestContext.CurrentContext.TestDirectory);
+
+            while (currentFolder != null)
+            {
+                foreach (var folder in currentFolder.EnumerateDirectories("zip", SearchOption.TopDirectoryOnly))
+                {
+                    return folder;
+                }
+
+                currentFolder = currentFolder.Parent;
+            }
+
+            throw new Exception("Cannot find zip folder");
+        }
+    }
+}
---
+++ src/ServiceControlInstaller.Packaging.UnitTests/DeploymentPackageTests.cs
@@ -0,0 +1,34 @@
+namespace Tests
+{
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixtureSource(typeof(DeploymentPackage), "All")]
+    public class DeploymentPackageTests
+    {
+        DeploymentPackage deploymentPackage;
+
+        public DeploymentPackageTests(DeploymentPackage deploymentPackage)
+        {
+            this.deploymentPackage = deploymentPackage;
+        }
+
+        [Test]
+        public void DuplicateAssemblyFileSizesShouldMatch()
+        {
+            using (var zip = deploymentPackage.Open())
+            {
+                var mainEntries = zip.Entries.Where(x => x.FullName.StartsWith(deploymentPackage.ServiceName)).ToList();
+
+                CollectionAssert.IsNotEmpty(mainEntries, $"Expected a {deploymentPackage.ServiceName} folder in {deploymentPackage.FullName}");
+
+                CollectionAssert.IsEmpty(
+                    from mainEntry in mainEntries
+                    join entry in zip.Entries.Except(mainEntries) on mainEntry.Name equals entry.Name
+                    where entry.Length != mainEntry.Length
+                    select entry.FullName, 
+                    $"File sizes should match the ones in the {deploymentPackage.ServiceName} folder. Check versions of dependencies.");
+            }
+        }
+    }
+}
