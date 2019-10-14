8832bd550ba795d4f2d31f01e814f4a78bcee09f
Andreas Öhlund <andreas.ohlund@particular.net>
2019-10-14 11:47:38 +0200

    Cleanup

--- src/ServiceControl.Monitoring.Transports.RabbitMQ/ConnectionFactory.cs
+++ src/ServiceControl.Monitoring.Transports.RabbitMQ/ConnectionFactory.cs
@@ -3,8 +3,7 @@
     using System;
     using System.Net.Security;
     using System.Security.Authentication;
-    using System.Security.Cryptography.X509Certificates;
-    using global::RabbitMQ.Client;
+   using global::RabbitMQ.Client;
 
     class ConnectionFactory
     {
