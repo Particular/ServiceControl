f472f1ffb7d4a3c7dd9ac3e95055716453a2db10
Tomek Masternak <tomasz.masternak@particular.net>
2019-10-16 10:29:49 +0200

    force MenuDropAlignment on startup

--- src/ServiceControl.Config/App.xaml.cs
+++ src/ServiceControl.Config/App.xaml.cs
@@ -4,6 +4,7 @@
     using System.Diagnostics;
     using System.IO;
     using System.Reflection;
+    using System.Windows;
     using UI.MessageBox;
 
     public partial class App
@@ -15,6 +16,23 @@
             if (!Debugger.IsAttached)
             {
                 ExceptionMessageBox.Attach();
+            }
+
+            EnsureStandardPopupAlignment();
+        }
+
+        static void EnsureStandardPopupAlignment()
+        {
+            //HINT: this code enforces MenuDropAlignment to be default. This is important on laptops that enable `Tablet Mode`.
+            //      In such case the dropdowns are moved to the left to not appear under hand when using touch. This behavior
+            //      applies even if the users are not using touch and causes dropdowns to be misaligned. It's fine to override
+            //      the setting at once at startup because the only way to change it by the users is via Control Panel.
+            //      See: https://github.com/Particular/ServiceControl/issues/1374 for more details
+            var dropdownAlignment = typeof(SystemParameters).GetField("_menuDropAlignment", BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (SystemParameters.MenuDropAlignment && dropdownAlignment != null)
+            {
+                dropdownAlignment.SetValue(null, false);
             }
         }
 
