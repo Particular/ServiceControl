a416528674e7a2c3f407f7d81ba0674a8b3fb8bc
Jan Ove Skogheim <janovesk@janovesk.com>
2019-10-17 14:40:38 +0200

    Readd support for /api/upgrade. Always return false

---
+++ src/ServiceControl/Upgrade/Api/UpgradeController.cs
@@ -0,0 +1,32 @@
+﻿namespace ServiceControl.Upgrade
+{
+    using System;
+    using System.Net.Http;
+    using System.Web.Http;
+    using Infrastructure.WebApi;
+
+    public class UpgradeController : ApiController
+    {
+        [Route("upgrade")]
+        [HttpGet]
+        public HttpResponseMessage GetUpgradeStatus()
+        {
+            return Negotiator.FromModel(Request, NotInProgress);
+        }
+
+        static readonly StaleIndexInfo NotInProgress = new StaleIndexInfo
+        {
+            InProgress = false,
+            StartedAt = null
+        };
+
+    }
+    public struct StaleIndexInfo
+    {
+        // ReSharper disable once NotAccessedField.Global
+        public DateTime? StartedAt;
+        // ReSharper disable once NotAccessedField.Global
+        public bool InProgress;
+    }
+}
+
