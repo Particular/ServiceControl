b9825b85d751dbe2df63adb9cccd04b79f5d98ce
Mike Minutillo <mike.minutillo@particular.net>
2019-10-24 17:45:23 +0800

    Add custom check API approvals

--- src/ServiceControl.Audit.UnitTests/API/APIApprovals.cs
+++ src/ServiceControl.Audit.UnitTests/API/APIApprovals.cs
@@ -1,5 +1,8 @@
 ﻿namespace ServiceControl.UnitTests.API
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Web.Http.Controllers;
     using System.Web.Http.Hosting;
@@ -7,6 +10,7 @@
     using Audit.Infrastructure;
     using Audit.Infrastructure.Settings;
     using Audit.Infrastructure.WebApi;
+    using NServiceBus.CustomChecks;
     using NUnit.Framework;
     using Particular.Approvals;
     using PublicApiGenerator;
@@ -44,6 +48,41 @@
             //If any changes have been made to settings, this may break the embedded config in that project, which may need to be updated.
 
             Approver.Verify(new Settings());
+        }
+
+        [Test]
+        public void CustomCheckDetails()
+        {
+            // HINT: Custom checks are documented on the docs site and Id and Category are published in integration events
+            // If any changes have been made to custom checks, this may break customer integration subscribers.
+            Approver.Verify(
+                string.Join(Environment.NewLine,
+                    from check in GetCustomChecks()
+                    orderby check.Category, check.Id
+                    select $"{check.Category}: {check.Id}"
+                )
+            );
+        }
+
+        static IEnumerable<ICustomCheck> GetCustomChecks()
+        {
+            var settings = (object)new Settings();
+
+            var serviceControlTypes = typeof(Bootstrapper).Assembly
+                .GetTypes()
+                .Where(t => t.IsAbstract == false);
+
+            var customCheckTypes = serviceControlTypes.Where(t => typeof(ICustomCheck).IsAssignableFrom(t));
+
+            foreach (var customCheckType in customCheckTypes)
+            {
+                var constructor = customCheckType.GetConstructors().Single();
+                var constructorParameters = constructor.GetParameters()
+                    .Select(p => p.ParameterType == typeof(Settings) ? settings : null)
+                    .ToArray();
+                var instance = (ICustomCheck)constructor.Invoke(constructorParameters);
+                yield return instance;
+            }
         }
     }
 }
