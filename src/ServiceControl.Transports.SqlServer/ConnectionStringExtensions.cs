97157d93015ba9f195977904b8afc9bba96ce724
aleksandr-samila <aleksandr-samila@users.noreply.github.com>
2019-11-11 15:19:59 +0200

    MsSql migration. Initial version (#1820)

---
+++ src/ServiceControl.Transports.SqlServer/ConnectionStringExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceControl.Transports.SqlServer
+{
+    using System.Data.Common;
+
+    static class ConnectionStringExtensions
+    {
+        public static string RemoveCustomSchemaPart(this string connectionString, out string schema)
+        {
+            const string queueSchemaName = "Queue schema";
+
+            var builder = new DbConnectionStringBuilder
+            {
+                ConnectionString = connectionString
+            };
+
+            if (builder.TryGetValue(queueSchemaName, out var customSchema))
+            {
+                builder.Remove(queueSchemaName);
+            }
+
+            schema = (string)customSchema;
+
+            return builder.ConnectionString;
+        }
+    }
+}
