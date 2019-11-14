1a15479f086cff6603757189724319b9c34697e6
aleksandr-samila <aleks.samila@particular.net>
2019-11-14 13:54:02 +0200

    Very initial version

---
+++ src/ServiceControl.AcceptanceTesting/TestSupport/DiscardMessagesBehavior.cs
@@ -0,0 +1,59 @@
+namespace ServiceBus.Management.AcceptanceTests
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Logging;
+    using NServiceBus.Pipeline;
+
+    class DiscardMessagesBehavior : IBehavior<ITransportReceiveContext, ITransportReceiveContext>
+    {
+        public DiscardMessagesBehavior(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        public Task Invoke(ITransportReceiveContext context, Func<ITransportReceiveContext, Task> next)
+        {
+            //Do not filter out CC and HB messages as they can't be stamped
+            if (context.Message.Headers.TryGetValue(Headers.EnclosedMessageTypes, out var messageTypes)
+                && pluginMessages.Any(t => messageTypes.StartsWith(t)))
+            {
+                return next(context);
+            }
+
+            //Do not filter out subscribe messages as they can't be stamped
+            if (context.Message.Headers.TryGetValue(Headers.MessageIntent, out var intent)
+                && intent == MessageIntentEnum.Subscribe.ToString())
+            {
+                return next(context);
+            }
+
+            var currentSession = scenarioContext.TestRunId.ToString();
+            if (!context.Message.Headers.TryGetValue("SC.SessionID", out var session)
+                || session != currentSession)
+            {
+                context.Message.Headers.TryGetValue(Headers.MessageId, out var originalMessageId);
+                context.Message.Headers.TryGetValue(Headers.EnclosedMessageTypes, out var enclosedMessageTypes);
+                log.Debug($"Discarding message '{context.Message.MessageId}'({originalMessageId ?? string.Empty}) because it's session id is '{session}' instead of '{currentSession}' Message Types: {enclosedMessageTypes}.");
+                return Task.FromResult(0);
+            }
+
+            return next(context);
+        }
+
+        ScenarioContext scenarioContext;
+        static ILog log = LogManager.GetLogger<DiscardMessagesBehavior>();
+
+        static string[] pluginMessages =
+        {
+            "ServiceControl.Plugin.CustomChecks.Messages.ReportCustomCheckResult",
+            "ServiceControl.EndpointPlugin.Messages.SagaState.SagaChangeInitiator",
+            "ServiceControl.EndpointPlugin.Messages.SagaState.SagaUpdatedMessage",
+            "ServiceControl.Plugin.Heartbeat.Messages.EndpointHeartbeat",
+            "ServiceControl.Plugin.Heartbeat.Messages.RegisterEndpointStartup"
+        };
+    }
+}
---
+++ src/ServiceControl.AcceptanceTesting/TestSupport/NServiceBusAcceptanceTest.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading;
+    using AcceptanceTesting.Customization;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Base class for all the NSB test that sets up our conventions
+    /// </summary>
+    [TestFixture]
+    // ReSharper disable once PartialTypeWithSinglePart
+    public abstract partial class NServiceBusAcceptanceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+#if NETFRAMEWORK
+            // Hack: prevents SerializationException ... Type 'x' in assembly 'y' is not marked as serializable.
+            // https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/mitigation-deserialization-of-objects-across-app-domains
+            ConfigurationManager.GetSection("X");
+#endif
+            Conventions.EndpointNamingConvention = t =>
+            {
+                var classAndEndpoint = t.FullName.Split('.').Last();
+
+                var testName = classAndEndpoint.Split('+').First();
+
+                testName = testName.Replace("When_", "");
+
+                var endpointBuilder = classAndEndpoint.Split('+').Last();
+
+                testName = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(testName);
+
+                testName = testName.Replace("_", "");
+
+                return testName + "." + endpointBuilder;
+            };
+        }
+    }
+}
