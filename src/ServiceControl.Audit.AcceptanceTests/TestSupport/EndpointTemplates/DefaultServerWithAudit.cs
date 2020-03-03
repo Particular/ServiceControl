978ddd776df29a665aaac63e0d28bbe410db1ea7
Hadi Eskandari <h.eskandari@gmail.com>
2020-03-03 18:03:48 +1100

    Enabled installers for the audit sample

--- src/ServiceControl.Audit.AcceptanceTests/TestSupport/EndpointTemplates/DefaultServerWithAudit.cs
+++ src/ServiceControl.Audit.AcceptanceTests/TestSupport/EndpointTemplates/DefaultServerWithAudit.cs
@@ -14,6 +14,7 @@
             return new DefaultServerBase<Bootstrapper>().GetConfiguration(runDescriptor, endpointConfiguration, builder =>
             {
                 builder.AuditProcessedMessagesTo("audit");
+                builder.EnableInstallers();
 
                 configurationBuilderCustomization(builder);
             });
