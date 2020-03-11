39f03932e174f7eb5053b7a2bbacee26fa0bc7f6
danielmarbach <daniel.marbach@openplace.net>
2020-03-11 13:54:39 +0100

    Map settings in setup bootstrapper

--- src/ServiceControl/SetupBootstrapper.cs
+++ src/ServiceControl/SetupBootstrapper.cs
@@ -37,7 +37,7 @@ namespace Particular.ServiceControl
             var domainEvents = new DomainEvents();
             containerBuilder.RegisterInstance(domainEvents).As<IDomainEvents>();
 
-            var transportSettings = new TransportSettings();
+            var transportSettings = MapSettings(settings);
             containerBuilder.RegisterInstance(transportSettings).SingleInstance();
 
             var loggingSettings = new LoggingSettings(settings.ServiceName);
@@ -52,6 +52,17 @@ namespace Particular.ServiceControl
                 await NServiceBusFactory.Create(settings, settings.LoadTransportCustomization(), transportSettings, loggingSettings, container, documentStore, configuration, false)
                     .ConfigureAwait(false);
             }
+        }
+        
+        static TransportSettings MapSettings(Settings settings)
+        {
+            var transportSettings = new TransportSettings
+            {
+                EndpointName = settings.ServiceName,
+                ConnectionString = settings.TransportConnectionString,
+                MaxConcurrency = settings.MaximumConcurrencyLevel
+            };
+            return transportSettings;
         }
 
         private readonly Settings settings;
