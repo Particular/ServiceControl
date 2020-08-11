9351b043d690314ae298edecd29c2dc20c2c5dc2
WilliamBZA <william.brander@gmail.com>
2020-08-11 15:50:52 +0200

    Batch endpoints

--- src/ServiceControl.Audit/Infrastructure/RavenDB/MigrateKnownEndpoints.cs
+++ src/ServiceControl.Audit/Infrastructure/RavenDB/MigrateKnownEndpoints.cs
@@ -1,15 +1,14 @@
-﻿using NServiceBus;
-using NServiceBus.Features;
-using Raven.Client;
-using ServiceControl.Audit.Monitoring;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServiceControl.Audit.Infrastructure.RavenDB
+﻿namespace ServiceControl.Audit.Infrastructure.RavenDB
 {
+    using NServiceBus;
+    using NServiceBus.Features;
+    using Raven.Abstractions.Data;
+    using Raven.Client;
+    using ServiceControl.Audit.Monitoring;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     class MigrateKnownEndpoints : Feature
     {
         public MigrateKnownEndpoints()
@@ -41,32 +40,56 @@ namespace ServiceControl.Audit.Infrastructure.RavenDB
             }
 
             await WaitForNonStaleIndex(knownEndpointsIndex.Name).ConfigureAwait(false);
-            
-            using (var session = store.OpenAsyncSession())
-            {
-                var endpointsFromIndex = await session.Query<dynamic>(knownEndpointsIndex.Name, true).ToListAsync().ConfigureAwait(false);
-                var knownEndpoints = endpointsFromIndex.Select(endpoint => new KnownEndpoint
-                {
-                    Id = KnownEndpoint.MakeDocumentId(endpoint.Name, Guid.Parse(endpoint.HostId)),
-                    Host = endpoint.Host,
-                    HostId = Guid.Parse(endpoint.HostId),
-                    Name = endpoint.Name,
-                    LastSeen = DateTime.Now // We don't store it anywhere so....?
-                });
 
-                foreach (var endpoint in knownEndpoints)
+            int previouslyDone = 0;
+            do {
+                using (var session = store.OpenAsyncSession())
                 {
-                    await session.StoreAsync(endpoint).ConfigureAwait(false);
+                    var endpointsFromIndex = await session.Query<dynamic>(knownEndpointsIndex.Name, true)
+                        .Take(1024)
+                        .Skip(previouslyDone)
+                        .ToListAsync()
+                        .ConfigureAwait(false);
+
+                    if (endpointsFromIndex.Count == 0)
+                    {
+                        previouslyDone = -1;
+                    }
+                    else
+                    {
+                        previouslyDone += endpointsFromIndex.Count;
+
+                        var knownEndpoints = endpointsFromIndex.Select(endpoint => new KnownEndpoint
+                        {
+                            Id = KnownEndpoint.MakeDocumentId(endpoint.Name, Guid.Parse(endpoint.HostId)),
+                            Host = endpoint.Host,
+                            HostId = Guid.Parse(endpoint.HostId),
+                            Name = endpoint.Name,
+                            LastSeen = DateTime.UtcNow // Set the imported date to be now
+                        });
+
+                        using (var bulkInsert = store.BulkInsert(options: new BulkInsertOptions
+                        {
+                            OverwriteExisting = true
+                        }))
+                        {
+                            foreach (var endpoint in knownEndpoints)
+                            {
+                                bulkInsert.Store(endpoint);
+                            }
+                        }
+
+                        await session.SaveChangesAsync().ConfigureAwait(false);
+                    }
                 }
-
-                await session.SaveChangesAsync().ConfigureAwait(false);
-            }
+            } while (previouslyDone > 0);
 
             await store.AsyncDatabaseCommands.DeleteIndexAsync(knownEndpointsIndex.Name).ConfigureAwait(false);
         }
 
         async Task WaitForNonStaleIndex(string indexName)
         {
+            var maxWaitTime = DateTime.UtcNow.AddHours(1);
             var isStale = true;
             do
             {
@@ -77,7 +100,7 @@ namespace ServiceControl.Audit.Infrastructure.RavenDB
                 {
                     await Task.Delay(1000).ConfigureAwait(false);
                 }
-            } while (isStale);
+            } while (isStale && DateTime.UtcNow <= maxWaitTime);
         }
 
         protected override Task OnStop(IMessageSession session)
