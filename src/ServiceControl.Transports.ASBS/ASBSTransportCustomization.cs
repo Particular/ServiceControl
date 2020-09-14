839853302af2f4e8c39f37ec714bae53196a27cd
Tomek Masternak <tomasz.masternak@particular.net>
2020-09-14 10:14:41 +0200

    asb trasnport, topicname configuration option support

--- src/ServiceControl.Transports.ASBS/ASBSTransportCustomization.cs
+++ src/ServiceControl.Transports.ASBS/ASBSTransportCustomization.cs
@@ -1,5 +1,6 @@
 ﻿namespace ServiceControl.Transports.ASBS
 {
+    using System.Data.Common;
     using NServiceBus;
     using NServiceBus.Raw;
 
@@ -9,12 +10,16 @@
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
 
+            CustomizeEndpoint(transport, transportSettings);
+
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.ReceiveOnly);
         }
 
         public override void CustomizeForMonitoringIngestion(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+
+            CustomizeEndpoint(transport, transportSettings);
 
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.ReceiveOnly);
         }
@@ -23,12 +28,16 @@
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
 
+            CustomizeEndpoint(transport, transportSettings);
+
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.SendsAtomicWithReceive);
         }
 
         public override void CustomizeServiceControlEndpoint(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+
+            CustomizeEndpoint(transport, transportSettings);
 
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.SendsAtomicWithReceive);
         }
@@ -37,12 +46,16 @@
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
 
+            CustomizeEndpoint(transport, transportSettings);
+
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.ReceiveOnly);
         }
 
         public override void CustomizeSendOnlyEndpoint(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+
+            CustomizeEndpoint(transport, transportSettings);
 
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.ReceiveOnly);
         }
@@ -51,6 +64,8 @@
         {
             var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
 
+            CustomizeEndpoint(transport, transportSettings);
+
             transport.ConfigureTransport(transportSettings, TransportTransactionMode.ReceiveOnly);
         }
 
@@ -58,5 +73,19 @@
         {
             return new QueueLengthProvider();
         }
+
+        void CustomizeEndpoint(TransportExtensions<AzureServiceBusTransport> transport, TransportSettings transportSettings)
+        {
+            var connectionString = transportSettings.ConnectionString;
+
+            var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
+
+            if (builder.TryGetValue(TopicNamePart, out var topicName))
+            {
+                transport.TopicName((string)topicName);
+            }
+        }
+
+        static string TopicNamePart = "TopicName";
     }
 }
