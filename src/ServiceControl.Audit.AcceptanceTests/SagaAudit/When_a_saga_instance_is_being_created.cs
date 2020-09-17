61be5fcb838f63070a42c0fa8acc146863ef81e0
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-09-17 09:55:23 +0200

    Add tests for processing saga audit data in the audit instance

---
+++ src/ServiceControl.Audit.AcceptanceTests/SagaAudit/When_a_saga_instance_is_being_created.cs
@@ -0,0 +1,86 @@
+﻿namespace ServiceControl.Audit.AcceptanceTests.SagaAudit
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Audit.SagaAudit;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+    using TestSupport.EndpointTemplates;
+
+    [RunOnAllTransports]
+    class When_a_saga_instance_is_being_created : AcceptanceTest
+    {
+        [Test]
+        public async Task Saga_audit_trail_should_contain_the_state_change()
+        {
+            SagaHistory sagaHistory = null;
+
+            var context = await Define<MyContext>()
+                .WithEndpoint<SagaEndpoint>(b => b.When((bus, c) => bus.SendLocal(new StartSagaMessage {Id = "Id"})))
+                .Done(async c =>
+                {
+                    var result = await this.TryGet<SagaHistory>($"/api/sagas/{c.SagaId}");
+                    sagaHistory = result;
+                    return c.InitiatingMessageReceived && result;
+                })
+                .Run();
+
+            Assert.NotNull(sagaHistory);
+
+            Assert.AreEqual(context.SagaId, sagaHistory.SagaId);
+            Assert.AreEqual(typeof(MySaga).FullName, sagaHistory.SagaType);
+
+            var change = sagaHistory.Changes.Single();
+
+            Assert.AreEqual(SagaStateChangeStatus.New, change.Status);
+            Assert.AreEqual(typeof(StartSagaMessage).FullName, change.InitiatingMessage.MessageType);
+        }
+
+        public class SagaEndpoint : EndpointConfigurationBuilder
+        {
+            public SagaEndpoint()
+            {
+                EndpointSetup<DefaultServerWithAudit>(
+                    c => c.AuditSagaStateChanges("audit"));
+            }
+        }
+
+        public class MySaga : Saga<MySagaData>,
+            IAmStartedByMessages<StartSagaMessage>
+        {
+            public MyContext Context { get; set; }
+
+            public Task Handle(StartSagaMessage message, IMessageHandlerContext context)
+            {
+                Context.SagaId = Data.Id;
+                Context.InitiatingMessageReceived = true;
+                return Task.FromResult(0);
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<MySagaData> mapper)
+            {
+                mapper.ConfigureMapping<StartSagaMessage>(msg => msg.Id).ToSaga(saga => saga.MessageId);
+            }
+        }
+
+        public class MySagaData : ContainSagaData
+        {
+            public string MessageId { get; set; }
+        }
+
+        public class StartSagaMessage : ICommand
+        {
+            public string Id { get; set; }
+        }
+
+        public class MyContext : ScenarioContext
+        {
+            public string MessageId { get; set; }
+            public Guid SagaId { get; set; }
+            public bool InitiatingMessageReceived { get; set; }
+        }
+    }
+}
