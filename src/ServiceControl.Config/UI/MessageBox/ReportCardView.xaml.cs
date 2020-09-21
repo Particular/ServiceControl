4e2147c155f9d354cc5c78acd2798143a0284ec6
seanfarmar <s.farmar@gmail.com>
2020-09-21 11:55:26 +0100

    added a button to copy exception details to the clipboard

--- src/ServiceControl.Config/UI/MessageBox/ReportCardView.xaml.cs
+++ src/ServiceControl.Config/UI/MessageBox/ReportCardView.xaml.cs
@@ -1,10 +1,45 @@
-﻿namespace ServiceControl.Config.UI.MessageBox
+﻿using System.Text;
+using System.Windows;
+
+namespace ServiceControl.Config.UI.MessageBox
 {
     public partial class ReportCardView
     {
         public ReportCardView()
         {
             InitializeComponent();
+        }
+
+        void CopyClick(object sender, RoutedEventArgs e)
+        {
+            ReportCardViewModel vm = (ReportCardViewModel)DataContext;
+
+            if (vm == null) return;
+            
+            var messages = new StringBuilder();
+            messages.Append(vm.Title).Append(":");
+            messages.AppendLine();
+            messages.AppendLine(vm.ErrorsMessage);
+
+            if (vm.HasWarnings)
+            {
+                foreach (var warning in vm.Warnings)
+                {
+                    messages.AppendLine(warning);
+                }
+            }
+
+            if (vm.HasErrors)
+            {
+                foreach (var error in vm.Errors)
+                {
+                    messages.AppendLine(error);
+                }
+            }
+
+            messages.AppendLine(Footer.Text);
+
+            Clipboard.SetText(messages.ToString());
         }
     }
 }
