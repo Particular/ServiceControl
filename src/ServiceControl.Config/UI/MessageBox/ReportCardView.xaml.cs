d217e25cb7e606b0e3a1feddb4ea978c97798ce2
seanfarmar <s.farmar@gmail.com>
2020-09-21 13:53:13 +0100

    Removed null check based on PR feedback

--- src/ServiceControl.Config/UI/MessageBox/ReportCardView.xaml.cs
+++ src/ServiceControl.Config/UI/MessageBox/ReportCardView.xaml.cs
@@ -13,8 +13,6 @@ namespace ServiceControl.Config.UI.MessageBox
         void CopyClick(object sender, RoutedEventArgs e)
         {
             ReportCardViewModel vm = (ReportCardViewModel)DataContext;
-
-            if (vm == null) return;
             
             var messages = new StringBuilder();
             messages.Append(vm.Title).Append(":");
