682c60729e057efa5f81e7f0d568d3365b17062f
Michał Wójcik <wojcik.mike@gmail.com>
2020-09-22 17:13:00 +0200

    Doing some more changes to the ServiceControl code

--- src/ServiceControl/CompositeViews/Messages/MessagesBodyTransformer.cs
+++ src/ServiceControl/CompositeViews/Messages/MessagesBodyTransformer.cs
@@ -1,34 +1,35 @@
-namespace ServiceControl.CompositeViews.Messages
-{
-    using System.Linq;
-    using Raven.Client.Documents.Indexes;
-
-    public class MessagesBodyTransformer : AbstractTransformerCreationTask<MessagesViewTransformer.Result>
-    {
-        public MessagesBodyTransformer()
-        {
-            TransformResults = messages => from message in messages
-                let metadata =
-                    message.ProcessingAttempts != null
-                        ? message.ProcessingAttempts.Last().MessageMetadata
-                        : message.MessageMetadata
-                select new
-                {
-                    MessageId = metadata["MessageId"],
-                    Body = metadata["Body"],
-                    BodySize = (int)metadata["ContentLength"],
-                    ContentType = metadata["ContentType"],
-                    BodyNotStored = (bool)metadata["BodyNotStored"]
-                };
-        }
-
-        public class Result
-        {
-            public string MessageId { get; set; }
-            public string Body { get; set; }
-            public string ContentType { get; set; }
-            public int BodySize { get; set; }
-            public bool BodyNotStored { get; set; }
-        }
-    }
-}
+//TODO: RAVEN5 Transofrmers are gone
+// namespace ServiceControl.CompositeViews.Messages
+// {
+//     using System.Linq;
+//     using Raven.Client.Documents.Indexes;
+//
+//     public class MessagesBodyTransformer : AbstractTransformerCreationTask<MessagesViewTransformer.Result>
+//     {
+//         public MessagesBodyTransformer()
+//         {
+//             TransformResults = messages => from message in messages
+//                 let metadata =
+//                     message.ProcessingAttempts != null
+//                         ? message.ProcessingAttempts.Last().MessageMetadata
+//                         : message.MessageMetadata
+//                 select new
+//                 {
+//                     MessageId = metadata["MessageId"],
+//                     Body = metadata["Body"],
+//                     BodySize = (int)metadata["ContentLength"],
+//                     ContentType = metadata["ContentType"],
+//                     BodyNotStored = (bool)metadata["BodyNotStored"]
+//                 };
+//         }
+//
+//         public class Result
+//         {
+//             public string MessageId { get; set; }
+//             public string Body { get; set; }
+//             public string ContentType { get; set; }
+//             public int BodySize { get; set; }
+//             public bool BodyNotStored { get; set; }
+//         }
+//     }
+// }
