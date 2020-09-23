929c2ebe77d32f37c9a370517d1f7dcff268d82f
Michał Wójcik <wojcik.mike@gmail.com>
2020-09-23 12:04:10 +0200

    Trying to change the GetAllMessagesApi to compile and make test green

--- src/ServiceControl.Audit/Auditing/MessagesView/GetAllMessagesApi.cs
+++ src/ServiceControl.Audit/Auditing/MessagesView/GetAllMessagesApi.cs
@@ -3,7 +3,10 @@ namespace ServiceControl.Audit.Auditing.MessagesView
     using System.Collections.Generic;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Infrastructure.Extensions;
     using Raven.Client.Documents;
+    using Raven.Client.Documents.Linq;
+    using ServiceControl.Infrastructure.Extensions;
 
     class GetAllMessagesApi : ApiBaseNoInput<IList<MessagesView>>
     {
@@ -11,23 +14,26 @@ namespace ServiceControl.Audit.Auditing.MessagesView
         {
         }
 
-        protected override Task<QueryResult<IList<MessagesView>>> Query(HttpRequestMessage request)
+        protected override async Task<QueryResult<IList<MessagesView>>> Query(HttpRequestMessage request)
         {
-            return Task.FromResult(QueryResult<IList<MessagesView>>.Empty());
             // TODO: RAVEN5 - No Transformers
-            //using (var session = Store.OpenAsyncSession())
-            //{
-            //    var results = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
-            //        .IncludeSystemMessagesWhere(request)
-            //        .Statistics(out var stats)
-            //        .Sort(request)
-            //        .Paging(request)
-            //        .TransformWith<MessagesViewTransformer, MessagesView>()
-            //        .ToListAsync()
-            //        .ConfigureAwait(false);
+            using (var session = Store.OpenAsyncSession())
+            {
+                var results = await session.Query<ProcessedMessage>()
+                    //.IncludeSystemMessagesWhere(request)
+                    .Statistics(out var stats)
+                    //.Sort(request)
+                    //.Paging(request)
+                    //.TransformWith<MessagesViewTransformer, MessagesView>()
+                    .Select(x => new MessagesView()
+                    {
+                        MessageId = x.Id
+                    })
+                    .ToListAsync()
+                    .ConfigureAwait(false);
 
-            //    return new QueryResult<IList<MessagesView>>(results, stats.ToQueryStatsInfo());
-            //}
+                return new QueryResult<IList<MessagesView>>(results, stats.ToQueryStatsInfo());
+            }
         }
     }
 }
