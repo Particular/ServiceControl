047fa0d7c65e660fa3fdaf84c2d7a967036f24e0
Mike Minutillo <mike.minutillo@particular.net>
2020-09-25 16:18:13 +0800

    Re-Enable Body Storage

--- src/ServiceControl.Audit/Auditing/GetBodyByIdApi.cs
+++ src/ServiceControl.Audit/Auditing/GetBodyByIdApi.cs
@@ -4,98 +4,36 @@
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
-    using BodyStorage;
     using MessagesView;
     using Raven.Client.Documents;
 
     class GetBodyByIdApi : IApi
     {
-        public GetBodyByIdApi(IDocumentStore documentStore, IBodyStorage bodyStorage)
+        public GetBodyByIdApi(IDocumentStore documentStore)
         {
             this.documentStore = documentStore;
-            this.bodyStorage = bodyStorage;
         }
 
         public async Task<HttpResponseMessage> Execute(HttpRequestMessage request, string messageId)
         {
-            messageId = messageId?.Replace("/", @"\");
-            var indexResponse = await TryFetchFromIndex(request, messageId).ConfigureAwait(false);
-            // when fetching result from index is successful go ahead
-            if (indexResponse.IsSuccessStatusCode)
+            using (var session = documentStore.OpenAsyncSession())
             {
-                return indexResponse;
-            }
+                var attachment = await session.Advanced.Attachments.GetAsync($"ProcessedMessages/{messageId}", "body")
+                    .ConfigureAwait(false);
 
-            // try to fetch from body
-            var bodyStorageResponse = await TryFetchFromStorage(request, messageId).ConfigureAwait(false);
-            // if found return, if not the result from the index takes precedence to by backward compatible
-            return bodyStorageResponse.IsSuccessStatusCode ? bodyStorageResponse : indexResponse;
-        }
-
-        async Task<HttpResponseMessage> TryFetchFromStorage(HttpRequestMessage request, string messageId)
-        {
-            //We want to continue using attachments for now
-#pragma warning disable 618
-            var result = await bodyStorage.TryFetch(messageId).ConfigureAwait(false);
-#pragma warning restore 618
-            if (result.HasResult)
-            {
                 var response = request.CreateResponse(HttpStatusCode.OK);
-                var content = new StreamContent(result.Stream);
-                content.Headers.ContentType = MediaTypeHeaderValue.Parse(result.ContentType);
-                content.Headers.ContentLength = result.BodySize;
-                response.Headers.ETag = new EntityTagHeaderValue($"\"{result.Etag}\"");
+                
+                var content = new StreamContent(attachment.Stream);
+                await content.LoadIntoBufferAsync(attachment.Details.Size).ConfigureAwait(false);
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse(attachment.Details.ContentType);
+                content.Headers.ContentLength = attachment.Details.Size;
+                //response.Headers.ETag = new EntityTagHeaderValue($"\"FRED\"");
                 response.Content = content;
+                
                 return response;
             }
-
-            return request.CreateResponse(HttpStatusCode.NotFound);
         }
-
-        async Task<HttpResponseMessage> TryFetchFromIndex(HttpRequestMessage request, string messageId)
-        {
-            await Task.Yield();
-            return request.CreateResponse(HttpStatusCode.NotFound);
-
-            // TODO: RAVEN5 - There are no Transformers in Raven 5
-            //using (var session = documentStore.OpenAsyncSession())
-            //{
-            //    var message = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
-            //        .Statistics(out var stats)
-            //        .TransformWith<MessagesBodyTransformer, MessagesBodyTransformer.Result>()
-            //        .FirstOrDefaultAsync(f => f.MessageId == messageId)
-            //        .ConfigureAwait(false);
-
-            //    if (message == null)
-            //    {
-            //        return request.CreateResponse(HttpStatusCode.NotFound);
-            //    }
-
-            //    if (message.BodyNotStored && message.Body == null)
-            //    {
-            //        return request.CreateResponse(HttpStatusCode.NoContent);
-            //    }
-
-            //    if (message.Body == null)
-            //    {
-            //        return request.CreateResponse(HttpStatusCode.NotFound);
-            //    }
-
-            //    var response = request.CreateResponse(HttpStatusCode.OK);
-            //    var content = new StringContent(message.Body);
-            //    content.Headers.ContentType = MediaTypeHeaderValue.Parse(message.ContentType);
-            //    content.Headers.ContentLength = message.BodySize;
-            //    if (stats.ResultEtag.HasValue)
-            //    {
-            //        response.Headers.ETag = new EntityTagHeaderValue($"\"{stats.ResultEtag}\"");
-            //    }
-            //    response.Content = content;
-            //    return response;
-            //}
-        }
-
 
         IDocumentStore documentStore;
-        IBodyStorage bodyStorage;
     }
 }
