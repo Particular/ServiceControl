b32ea46e888f7fba51e7fdbafb15af16b3e8b5f9
Mike Minutillo <mike.minutillo@particular.net>
2020-09-25 12:09:13 +0800

    Re-Enable Endpoint filtered messages api

--- src/ServiceControl.Audit/Auditing/MessagesView/GetAllMessagesForEndpointApi.cs
+++ src/ServiceControl.Audit/Auditing/MessagesView/GetAllMessagesForEndpointApi.cs
@@ -13,24 +13,22 @@ namespace ServiceControl.Audit.Auditing.MessagesView
         {
         }
 
-        protected override Task<QueryResult<IList<MessagesView>>> Query(HttpRequestMessage request, string input)
+        protected override async Task<QueryResult<IList<MessagesView>>> Query(HttpRequestMessage request, string input)
         {
-            return Task.FromResult(QueryResult<IList<MessagesView>>.Empty());
-            // TODO: RAVEN5 - No Transformers
-            //using (var session = Store.OpenAsyncSession())
-            //{
-            //    var results = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
-            //        .IncludeSystemMessagesWhere(request)
-            //        .Where(m => m.ReceivingEndpointName == input)
-            //        .Statistics(out var stats)
-            //        .Sort(request)
-            //        .Paging(request)
-            //        .TransformWith<MessagesViewTransformer, MessagesView>()
-            //        .ToListAsync()
-            //        .ConfigureAwait(false);
+            using (var session = Store.OpenAsyncSession())
+            {
+                var results = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
+                    .IncludeSystemMessagesWhere(request)
+                    .Where(m => m.ReceivingEndpointName == input, true)
+                    .Statistics(out var stats)
+                    .Sort(request)
+                    .Paging(request)
+                    .ToMessagesView()
+                    .ToListAsync()
+                    .ConfigureAwait(false);
 
-            //    return new QueryResult<IList<MessagesView>>(results, stats.ToQueryStatsInfo());
-            //}
+                return new QueryResult<IList<MessagesView>>(results, stats.ToQueryStatsInfo());
+            }
         }
     }
 }
