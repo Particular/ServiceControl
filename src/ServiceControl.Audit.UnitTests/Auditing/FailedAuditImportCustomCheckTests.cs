b77cb630976295d0f1a575cb23a4c42567c5f07c
Mike Minutillo <mike.minutillo@particular.net>
2020-09-28 13:52:14 +0800

    Clean up Raven-based unit tests

--- src/ServiceControl.Audit.UnitTests/Auditing/FailedAuditImportCustomCheckTests.cs
+++ src/ServiceControl.Audit.UnitTests/Auditing/FailedAuditImportCustomCheckTests.cs
@@ -1,51 +1,52 @@
-﻿// TODO: RAVEN5 - Need to come up with a strategy for In Memory Store Tests
-//namespace ServiceControl.Audit.UnitTests.Auditing
-//{
-//    using System.Threading.Tasks;
-//    using Audit.Auditing;
-//    using NServiceBus.CustomChecks;
-//    using NUnit.Framework;
+﻿//TODO: RAVEN5 - Need to come up with a strategy for In Memory Store Tests
+namespace ServiceControl.Audit.UnitTests.Auditing
+{
+    using System.Threading.Tasks;
+    using Audit.Auditing;
+    using NServiceBus.CustomChecks;
+    using NUnit.Framework;
+    using Raven.TestDriver;
 
-//    [TestFixture]
-//    public class FailedAuditImportCustomCheckTests
-//    {
-//        [Test]
-//        public async Task Pass_if_no_failed_imports()
-//        {
-//            using (var store = InMemoryStoreBuilder.GetInMemoryStore())
-//            {
-//                store.ExecuteIndex(new FailedAuditImportIndex());
+    [TestFixture]
+    public class FailedAuditImportCustomCheckTests : RavenTestDriver
+    {
+        [Test]
+        public async Task Pass_if_no_failed_imports()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.ExecuteIndexAsync(new FailedAuditImportIndex());
 
-//                var customCheck = new FailedAuditImportCustomCheck(store);
+                var customCheck = new FailedAuditImportCustomCheck(store);
 
-//                var result = await customCheck.PerformCheck();
+                var result = await customCheck.PerformCheck();
 
-//                Assert.AreEqual(CheckResult.Pass, result);
-//            }
-//        }
+                Assert.AreEqual(CheckResult.Pass, result);
+            }
+        }
 
-//        [Test]
-//        public async Task Fail_if_failed_imports()
-//        {
-//            using (var store = InMemoryStoreBuilder.GetInMemoryStore())
-//            {
-//                store.ExecuteIndex(new FailedAuditImportIndex());
+        [Test]
+        public async Task Fail_if_failed_imports()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.ExecuteIndexAsync(new FailedAuditImportIndex());
 
-//                using (var session = store.OpenAsyncSession())
-//                {
-//                    await session.StoreAsync(new FailedAuditImport());
-//                    await session.SaveChangesAsync();
-//                }
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new FailedAuditImport());
+                    await session.SaveChangesAsync();
+                }
 
-//                store.WaitForIndexing();
+                WaitForIndexing(store);
 
-//                var customCheck = new FailedAuditImportCustomCheck(store);
+                var customCheck = new FailedAuditImportCustomCheck(store);
 
-//                var result = await customCheck.PerformCheck();
+                var result = await customCheck.PerformCheck();
 
-//                Assert.IsTrue(result.HasFailed);
-//                StringAssert.StartsWith("One or more audit messages have failed to import properly into ServiceControl.Audit and have been stored in the ServiceControl.Audit database.", result.FailureReason);
-//            }
-//        }
-//    }
-//}
+                Assert.IsTrue(result.HasFailed);
+                StringAssert.StartsWith("One or more audit messages have failed to import properly into ServiceControl.Audit and have been stored in the ServiceControl.Audit database.", result.FailureReason);
+            }
+        }
+    }
+}
