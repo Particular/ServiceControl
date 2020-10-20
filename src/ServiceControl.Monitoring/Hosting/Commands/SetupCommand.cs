4b73d4e8ce0a2cb21c81e8ea2f293df06da5fdcd
WilliamBZA <william.brander@gmail.com>
2020-10-20 08:52:59 +0200

    Import license

--- src/ServiceControl.Monitoring/Hosting/Commands/SetupCommand.cs
+++ src/ServiceControl.Monitoring/Hosting/Commands/SetupCommand.cs
@@ -36,16 +36,17 @@ namespace ServiceControl.Monitoring
         {
             if (!string.IsNullOrWhiteSpace(settings.LicenseFileText))
             {
-                Logger.WarnFormat("LicenseText Speficied: '{0}'", settings.LicenseFileText);
                 if (!LicenseManager.IsLicenseValidForServiceControlInit(settings.LicenseFileText, out var errorMessageForLicenseText))
                 {
                     Logger.Error(errorMessageForLicenseText);
                     return false;
                 }
-            }
-            else
-            {
-                Logger.Warn("No license text specified");
+
+                if (!LicenseManager.TryImportLicenseFromText(settings.LicenseFileText, out var importErrorMessage))
+                {
+                    Logger.Error(importErrorMessage);
+                    return false;
+                }
             }
 
             // Validate license:
