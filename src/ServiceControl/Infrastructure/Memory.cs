79e95331fcc80e948b953bba4ec8ad7719045d66
danielmarbach <daniel.marbach@openplace.net>
2020-10-29 10:13:50 +0100

    Pool memory in Body Storage

---
+++ src/ServiceControl/Infrastructure/Memory.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceControl.Infrastructure
+{
+    using Microsoft.IO;
+
+    public static class Memory
+    {
+        public static readonly RecyclableMemoryStreamManager Manager = new RecyclableMemoryStreamManager();
+    }
+}
