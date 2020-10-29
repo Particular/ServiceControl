a0dbb68c439bc22d0393d8540a3e0587b4e6e165
danielmarbach <daniel.marbach@openplace.net>
2020-10-29 10:03:18 +0100

    Align namespace

--- src/ServiceControl.Audit/Infrastructure/Memory.cs
+++ src/ServiceControl.Audit/Infrastructure/Memory.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.IO;
-
-namespace ServiceControl.Audit.Infrastructure
+﻿namespace ServiceControl.Audit.Infrastructure
 {
+    using Microsoft.IO;
+    
     public static class Memory
     {
         public static readonly RecyclableMemoryStreamManager Manager = new RecyclableMemoryStreamManager();
