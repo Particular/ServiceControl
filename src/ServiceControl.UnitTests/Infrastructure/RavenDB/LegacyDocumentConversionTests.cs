978372afa2026f3f005793950452613127713272
Mike Minutillo <mike.minutillo@particular.net>
2020-10-30 11:15:21 +0800

    Isolate legacy document conversion and test

---
+++ src/ServiceControl.UnitTests/Infrastructure/RavenDB/LegacyDocumentConversionTests.cs
@@ -0,0 +1,63 @@
+﻿namespace ServiceControl.UnitTests.Infrastructure.RavenDB
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NServiceBus.Unicast.Subscriptions;
+    using NUnit.Framework;
+    using Raven.Client;
+    using Raven.Client.Documents;
+    using Raven.TestDriver;
+    using ServiceControl.Infrastructure.RavenDB.Subscriptions;
+
+    [TestFixture]
+    class LegacyDocumentConversionTests : RavenTestDriver
+    {
+        protected override void PreInitialize(IDocumentStore documentStore)
+        {
+            base.PreInitialize(documentStore);
+            LegacyDocumentConversion.Install(documentStore);
+        }
+
+        [TestCase("NServiceBus.Subscription, NServiceBus.Core")]
+        [TestCase("NServiceBus.Subscription, NServiceBus.RavenDB")]
+        public async Task CanLoadSubscriptionsWithLegacyClrTypes(string legacyClrType)
+        {
+            var store = GetDocumentStore();
+            var legacySubscription = CreateLegacySubscription();
+
+            // Save the subscription and change the CLR Type
+            using (var session = store.OpenAsyncSession())
+            {
+                await session.StoreAsync(legacySubscription);
+                session.Advanced.GetMetadataFor(legacySubscription)[Constants.Documents.Metadata.RavenClrType] = legacyClrType;
+                await session.SaveChangesAsync();
+            }
+
+            // Now load the subscription and it should be loaded as the correct type
+            using (var session = store.OpenAsyncSession())
+            {
+                var loadedSubscription = await session.LoadAsync<object>(legacySubscription.Id);
+                Assert.That(loadedSubscription, Is.Not.Null, "Subscription should be loaded");
+                Assert.IsTrue(loadedSubscription is Subscription, "Incorrect CLR Type should be converted to correct type");
+            }
+
+        }
+
+        private static Subscription CreateLegacySubscription()
+        {
+            return new Subscription
+            {
+                Id = "SubscriptionId",
+                MessageType = new MessageType("EventType", "1.0"),
+                Subscribers = new List<SubscriptionClient>
+                {
+                    new SubscriptionClient
+                    {
+                        Endpoint = "SubscriberName",
+                        TransportAddress = "subscriber@MACHINE"
+                    }
+                }
+            };
+        }
+    }
+}
--- src/ServiceControl/Infrastructure/RavenDB/Subscriptions/SubscriptionStorage.cs
+++ src/ServiceControl/Infrastructure/RavenDB/Subscriptions/SubscriptionStorage.cs
@@ -5,11 +5,7 @@
     using NServiceBus.Features;
     using NServiceBus.Settings;
     using NServiceBus.Transport;
-    using Raven.Client;
     using Raven.Client.Documents;
-    using System.Linq;
-    using Raven.Client.Documents.Session;
-    using Sparrow.Json;
 
     class SubscriptionStorage : Feature
     {
@@ -26,71 +22,11 @@
         protected override void Setup(FeatureConfigurationContext context)
         {
             var store = context.Settings.Get<IDocumentStore>();
-            store.OnBeforeConversionToDocument += BeforeConversionToDocument;
-            store.OnAfterConversionToEntity += StoreOnOnAfterConversionToEntity;
+            LegacyDocumentConversion.Install(store);
 
-            store.Conventions.FindClrType = (id, doc) =>
-            {
-                if (doc.TryGet(Constants.Documents.Metadata.Key, out BlittableJsonReaderObject metadata) &&
-                    metadata.TryGet(Constants.Documents.Metadata.RavenClrType, out string clrType))
-                {
-                    // The CLR type cannot be assumed to be always there
-                    if (clrType == null)
-                    {
-                        return null;
-                    }
-
-                    //TODO:RAVEN5 Need to add a test that handles subscription types with wrong metadata
-                    if (clrType.EndsWith(".Subscription, NServiceBus.Core"))
-                    {
-                        clrType = $"{typeof(Subscription).FullName}, {typeof(Subscription).Assembly.GetName().Name}";
-                    }
-                    else if (clrType.EndsWith(".Subscription, NServiceBus.RavenDB"))
-                    {
-                        clrType = $"{typeof(Subscription).FullName}, {typeof(Subscription).Assembly.GetName().Name}";
-                    }
-
-                    return clrType;
-                }
-
-                return null;
-            };
             context.Container.ConfigureComponent<SubscriptionPersister>(DependencyLifecycle.SingleInstance);
             context.Container.ConfigureComponent<PrimeSubscriptions>(DependencyLifecycle.SingleInstance);
             context.RegisterStartupTask(b => b.Build<PrimeSubscriptions>());
-        }
-
-        private void StoreOnOnAfterConversionToEntity(object sender, AfterConversionToEntityEventArgs e)
-        {
-            if (!(e.Entity is Subscription subscription))
-            {
-                return;
-            }
-
-            var clients = e.Document["Clients"];
-
-            if (clients != null)
-            {
-                var converted = LegacyAddress.ParseMultipleToSubscriptionClient(subscription.LegacySubscriptions);
-
-                var legacySubscriptions = converted.Except(subscription.Subscribers).ToArray();
-                foreach (var legacySubscription in legacySubscriptions)
-                {
-                    subscription.Subscribers.Add(legacySubscription);
-                }
-            }
-        }
-
-        private void BeforeConversionToDocument(object sender, BeforeConversionToDocumentEventArgs e)
-        {
-            if (!(e.Entity is Subscription subscription))
-            {
-                return;
-            }
-
-            var converted = LegacyAddress.ConvertMultipleToLegacyAddress(subscription.Subscribers);
-            subscription.LegacySubscriptions.Clear();
-            subscription.LegacySubscriptions.AddRange(converted);
         }
 
         class PrimeSubscriptions : FeatureStartupTask
