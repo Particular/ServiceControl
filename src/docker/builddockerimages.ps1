cede67f30119a5cd8364d6d80333364134b2387f
Dennis <dvdstelt@gmail.com>
2020-11-30 11:14:53 +0100

    Changed filenames in buildscript

--- src/docker/builddockerimages.ps1
+++ src/docker/builddockerimages.ps1
@@ -1,57 +1,57 @@
-msbuild ../ServiceControl.sln /t:Build /p:Configuration=Release
+msbuild ../servicecontrol/ServiceControl.sln /t:Build /p:Configuration=Release
 
-docker build -f .\dockerfile.asb.asbs -t particular/servicecontrolasbs ./../
-docker build -f .\dockerfile.asb.asbs.init -t particular/servicecontrolasbs.init ./../
-docker build -f .\dockerfile.asb.asbs.audit -t particular/servicecontrolasbs.audit ./../
-docker build -f .\dockerfile.asb.asbs.audit.init -t particular/servicecontrolasbs.audit.init ./../
-docker build -f .\dockerfile.asb.asbs.monitoring -t particular/servicecontrolasbs.monitoring ./../
-docker build -f .\dockerfile.asb.asbs.monitoring.init -t particular/servicecontrolasbs.monitoring.init ./../
+docker build -f .\servicecontrol.asb.asbs -t particular/servicecontrol/asbs ./../
+docker build -f .\servicecontrol.asb.asbs.init -t particular/servicecontrol/asbs.init ./../
+docker build -f .\servicecontrol.asb.asbs.audit -t particular/servicecontrol/asbs.audit ./../
+docker build -f .\servicecontrol.asb.asbs.audit.init -t particular/servicecontrol/asbs.audit.init ./../
+docker build -f .\servicecontrol.asb.asbs.monitoring -t particular/servicecontrol/asbs.monitoring ./../
+docker build -f .\servicecontrol.asb.asbs.monitoring.init -t particular/servicecontrol/asbs.monitoring.init ./../
 
-docker build -f .\dockerfile.asb.endpoint -t particular/servicecontrolasbendpoint ./../
-docker build -f .\dockerfile.asb.endpoint.init -t particular/servicecontrolasbendpoint.init ./../
-docker build -f .\dockerfile.asb.endpoint.audit -t particular/servicecontrolasbendpoint.audit ./../
-docker build -f .\dockerfile.asb.endpoint.audit.init -t particular/servicecontrolasbendpoint.audit.init ./../
-docker build -f .\dockerfile.asb.endpoint.monitoring -t particular/servicecontrolasbendpoint.monitoring ./../
-docker build -f .\dockerfile.asb.endpoint.monitoring.init -t particular/servicecontrolasbendpoint.monitoring.init ./../
+docker build -f .\servicecontrol.asb.endpoint -t particular/servicecontrol/asb/endpoint ./../
+docker build -f .\servicecontrol.asb.endpoint.init -t particular/servicecontrol/asb/endpoint.init ./../
+docker build -f .\servicecontrol.asb.endpoint.audit -t particular/servicecontrol/asb/endpoint.audit ./../
+docker build -f .\servicecontrol.asb.endpoint.audit.init -t particular/servicecontrol/asb/endpoint.audit.init ./../
+docker build -f .\servicecontrol.asb.endpoint.monitoring -t particular/servicecontrol/asb/endpoint.monitoring ./../
+docker build -f .\servicecontrol.asb.endpoint.monitoring.init -t particular/servicecontrol/asb/endpoint.monitoring.init ./../
 
-docker build -f .\dockerfile.asb.forwarding -t particular/servicecontrolasbforwarding ./../
-docker build -f .\dockerfile.asb.forwarding.init -t particular/servicecontrolasbforwarding.init ./../
-docker build -f .\dockerfile.asb.forwarding.audit -t particular/servicecontrolasbforwarding.audit ./../
-docker build -f .\dockerfile.asb.forwarding.audit.init -t particular/servicecontrolasbforwarding.audit.init ./../
-docker build -f .\dockerfile.asb.forwarding.monitoring -t particular/servicecontrolasbforwarding.monitoring ./../
-docker build -f .\dockerfile.asb.forwarding.monitoring.init -t particular/servicecontrolasbforwarding.monitoring.init ./../
+docker build -f .\servicecontrol.asb.forwarding -t particular/servicecontrol/asb/forwarding ./../
+docker build -f .\servicecontrol.asb.forwarding.init -t particular/servicecontrol/asb/forwarding.init ./../
+docker build -f .\servicecontrol.asb.forwarding.audit -t particular/servicecontrol/asb/forwarding.audit ./../
+docker build -f .\servicecontrol.asb.forwarding.audit.init -t particular/servicecontrol/asb/forwarding.audit.init ./../
+docker build -f .\servicecontrol.asb.forwarding.monitoring -t particular/servicecontrol/asb/forwarding.monitoring ./../
+docker build -f .\servicecontrol.asb.forwarding.monitoring.init -t particular/servicecontrol/asb/forwarding.monitoring.init ./../
 
-docker build -f .\dockerfile.rabbitmq.conventional -t particular/servicecontrolrabbitconventional ./../
-docker build -f .\dockerfile.rabbitmq.conventional.init -t particular/servicecontrolrabbitconventional.init ./../
-docker build -f .\dockerfile.rabbitmq.conventional.audit -t particular/servicecontrolrabbitconventional.audit ./../
-docker build -f .\dockerfile.rabbitmq.conventional.audit.init -t particular/servicecontrolrabbitconventional.audit.init ./../
-docker build -f .\dockerfile.rabbitmq.conventional.monitoring -t particular/servicecontrolrabbitconventional.monitoring ./../
-docker build -f .\dockerfile.rabbitmq.conventional.monitoring.init -t particular/servicecontrolrabbitconventional.monitoring.init ./../
+docker build -f .\servicecontrol.rabbitmq.conventional -t particular/servicecontrol/rabbitmq/conventional ./../
+docker build -f .\servicecontrol.rabbitmq.conventional.init -t particular/servicecontrol/rabbitmq/conventional.init ./../
+docker build -f .\servicecontrol.rabbitmq.conventional.audit -t particular/servicecontrol/rabbitmq/conventional.audit ./../
+docker build -f .\servicecontrol.rabbitmq.conventional.audit.init -t particular/servicecontrol/rabbitmq/conventional.audit.init ./../
+docker build -f .\servicecontrol.rabbitmq.conventional.monitoring -t particular/servicecontrol/rabbitmq/conventional.monitoring ./../
+docker build -f .\servicecontrol.rabbitmq.conventional.monitoring.init -t particular/servicecontrol/rabbitmq/conventional.monitoring.init ./../
 
-docker build -f .\dockerfile.rabbitmq.direct -t particular/servicecontrolrabbitdirect ./../
-docker build -f .\dockerfile.rabbitmq.direct.init -t particular/servicecontrolrabbitdirect.init ./../
-docker build -f .\dockerfile.rabbitmq.direct.audit -t particular/servicecontrolrabbitdirect.audit ./../
-docker build -f .\dockerfile.rabbitmq.direct.audit.init -t particular/servicecontrolrabbitdirect.audit.init ./../
-docker build -f .\dockerfile.rabbitmq.direct.monitoring -t particular/servicecontrolrabbitdirect.monitoring ./../
-docker build -f .\dockerfile.rabbitmq.direct.monitoring.init -t particular/servicecontrolrabbitdirect.monitoring.init ./../
+docker build -f .\servicecontrol.rabbitmq.direct -t particular/servicecontrol/rabbitmq/direct ./../
+docker build -f .\servicecontrol.rabbitmq.direct.init -t particular/servicecontrol/rabbitmq/direct.init ./../
+docker build -f .\servicecontrol.rabbitmq.direct.audit -t particular/servicecontrol/rabbitmq/direct.audit ./../
+docker build -f .\servicecontrol.rabbitmq.direct.audit.init -t particular/servicecontrol/rabbitmq/direct.audit.init ./../
+docker build -f .\servicecontrol.rabbitmq.direct.monitoring -t particular/servicecontrol/rabbitmq/direct.monitoring ./../
+docker build -f .\servicecontrol.rabbitmq.direct.monitoring.init -t particular/servicecontrol/rabbitmq/direct.monitoring.init ./../
 
-docker build -f .\dockerfile.asq -t particular/servicecontrolasq ./../
-docker build -f .\dockerfile.asq.init -t particular/servicecontrolasq.init ./../
-docker build -f .\dockerfile.asq.audit -t particular/servicecontrolasq.audit ./../
-docker build -f .\dockerfile.asq.audit.init -t particular/servicecontrolasq.audit.init ./../
-docker build -f .\dockerfile.asq.monitoring -t particular/servicecontrolasq.monitoring ./../
-docker build -f .\dockerfile.asq.monitoring.init -t particular/servicecontrolasq.monitoring.init ./../
+docker build -f .\servicecontrol.asq -t particular/servicecontrol/asq ./../
+docker build -f .\servicecontrol.asq.init -t particular/servicecontrol/asq.init ./../
+docker build -f .\servicecontrol.asq.audit -t particular/servicecontrol/asq.audit ./../
+docker build -f .\servicecontrol.asq.audit.init -t particular/servicecontrol/asq.audit.init ./../
+docker build -f .\servicecontrol.asq.monitoring -t particular/servicecontrol/asq.monitoring ./../
+docker build -f .\servicecontrol.asq.monitoring.init -t particular/servicecontrol/asq.monitoring.init ./../
 
-docker build -f .\dockerfile.sqlserver -t particular/servicecontrolsql ./../
-docker build -f .\dockerfile.sqlserver.init -t particular/servicecontrolsql.init ./../
-docker build -f .\dockerfile.sqlserver.audit -t particular/servicecontrolsql.audit ./../
-docker build -f .\dockerfile.sqlserver.audit.init -t particular/servicecontrolsql.audit.init ./../
-docker build -f .\dockerfile.sqlserver.monitoring -t particular/servicecontrolsql.monitoring ./../
-docker build -f .\dockerfile.sqlserver.monitoring.init -t particular/servicecontrolsql.monitoring.init ./../
+docker build -f .\servicecontrol.sqlserver -t particular/servicecontrol/sql ./../
+docker build -f .\servicecontrol.sqlserver.init -t particular/servicecontrol/sql.init ./../
+docker build -f .\servicecontrol.sqlserver.audit -t particular/servicecontrol/sql.audit ./../
+docker build -f .\servicecontrol.sqlserver.audit.init -t particular/servicecontrol/sql.audit.init ./../
+docker build -f .\servicecontrol.sqlserver.monitoring -t particular/servicecontrol/sql.monitoring ./../
+docker build -f .\servicecontrol.sqlserver.monitoring.init -t particular/servicecontrol/sql.monitoring.init ./../
 
-docker build -f .\dockerfile.sqs -t particular/servicecontrolsqs ./../
-docker build -f .\dockerfile.sqs.init -t particular/servicecontrolsqs.init ./../
-docker build -f .\dockerfile.sqs.audit -t particular/servicecontrolsqs.audit ./../
-docker build -f .\dockerfile.sqs.audit.init -t particular/servicecontrolsqs.audit.init ./../
-docker build -f .\dockerfile.sqs.monitoring -t particular/servicecontrolsqs.monitoring ./../
-docker build -f .\dockerfile.sqs.monitoring.init -t particular/servicecontrolsqs.monitoring.init ./../
+docker build -f .\servicecontrol.sqs -t particular/servicecontrol/sqs ./../
+docker build -f .\servicecontrol.sqs.init -t particular/servicecontrol/sqs.init ./../
+docker build -f .\servicecontrol.sqs.audit -t particular/servicecontrol/sqs.audit ./../
+docker build -f .\servicecontrol.sqs.audit.init -t particular/servicecontrol/sqs.audit.init ./../
+docker build -f .\servicecontrol.sqs.monitoring -t particular/servicecontrol/sqs.monitoring ./../
+docker build -f .\servicecontrol.sqs.monitoring.init -t particular/servicecontrol/sqs.monitoring.init ./../
