16911a006b4c6f8bcea72c5f2dfc3e9ddab33ce1
mauroservienti <mauro.servienti@gmail.com>
2020-12-01 09:35:58 +0100

    Use only Mono.Cecil

--- src/ServiceControlInstaller.Engine/FileSystem/ReleaseDateReader.cs
+++ src/ServiceControlInstaller.Engine/FileSystem/ReleaseDateReader.cs
@@ -9,66 +9,27 @@
     {
         public static bool TryReadReleaseDateAttribute(string exe, out DateTime releaseDate)
         {
-            releaseDate = DateTime.MinValue;
-            var tempDomain = AppDomain.CreateDomain("TemporaryAppDomain");
             try
             {
-                var loaderType = typeof(AssemblyReleaseDateReader);
-                var loader = (AssemblyReleaseDateReader)tempDomain.CreateInstanceFrom(Assembly.GetExecutingAssembly().Location, loaderType.FullName).Unwrap();
-                releaseDate = loader.GetReleaseDate(exe);
-                return true;
+                using (var assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly(exe))
+                {
+                    var customAttribute = assemblyDefinition.CustomAttributes.SingleOrDefault(ca => ca.AttributeType.Name == "ReleaseDateAttribute");
+                    var constructorArgument = customAttribute?.ConstructorArguments[0];
+                    var dateString = (string)constructorArgument?.Value;
+                    if (!string.IsNullOrWhiteSpace(dateString))
+                    {
+                        releaseDate = DateTime.ParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                        return true;
+                    }
+                }
             }
             catch
             {
-                try
-                {
-                    return TryReadReleaseDateAttributeUsingMonoCecil(exe, out releaseDate);
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-            finally
-            {
-                AppDomain.Unload(tempDomain);
-            }
-        }
-
-        static bool TryReadReleaseDateAttributeUsingMonoCecil(string exe, out DateTime releaseDate)
-        {
-            using (var assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly(exe))
-            {
-                var customAttribute = assemblyDefinition.CustomAttributes.SingleOrDefault(ca => ca.AttributeType.Name == "ReleaseDateAttribute");
-                var constructorArgument = customAttribute?.ConstructorArguments[0];
-                var dateString = (string)constructorArgument?.Value;
-                if (!string.IsNullOrWhiteSpace(dateString))
-                {
-                    releaseDate = DateTime.ParseExact(dateString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                    return true;
-                }
+                //NOP
             }
 
             releaseDate = DateTime.MinValue;
             return false;
-        }
-
-        class AssemblyReleaseDateReader : MarshalByRefObject
-        {
-            internal DateTime GetReleaseDate(string assemblyPath)
-            {
-                try
-                {
-                    var assembly = Assembly.ReflectionOnlyLoadFrom(assemblyPath);
-                    var releaseDateAttribute = assembly.GetCustomAttributesData().FirstOrDefault(p => p.Constructor?.ReflectedType?.Name == "ReleaseDateAttribute");
-                    var x = (string)releaseDateAttribute?.ConstructorArguments[0].Value;
-                    return DateTime.ParseExact(x, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                }
-                catch (Exception)
-                {
-                    return DateTime.MinValue;
-                }
-            }
         }
     }
 }
