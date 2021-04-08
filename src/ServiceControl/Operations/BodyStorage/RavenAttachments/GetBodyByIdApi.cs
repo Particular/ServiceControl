8dee04cedcaa6c44bce50321847dd1e10bd7db4f
danielmarbach <daniel.marbach@openplace.net>
2021-04-08 23:20:11 +0200

    Query the index first and then fallback to the body

--- src/ServiceControl/Operations/BodyStorage/RavenAttachments/GetBodyByIdApi.cs
+++ src/ServiceControl/Operations/BodyStorage/RavenAttachments/GetBodyByIdApi.cs
@@ -20,61 +20,66 @@
         {
             var messageId = input;
             messageId = messageId?.Replace("/", @"\");
-            string contentType;
-            int bodySize;
+            var indexResponse = await TryFetchFromIndex(request, messageId).ConfigureAwait(false);
+            // when fetching result from index is successful go ahead
+            if (indexResponse.IsSuccessStatusCode)
+            {
+                return indexResponse;
+            }
 
+            // try to fetch from body
+            var bodyStorageResponse = await TryFetchFromStorage(request, messageId).ConfigureAwait(false);
+            // if found return, if not the result from the index takes precedence to by backward compatible
+            return bodyStorageResponse.IsSuccessStatusCode ? bodyStorageResponse : indexResponse;
+        }
+
+        async Task<HttpResponseMessage> TryFetchFromStorage(HttpRequestMessage request, string messageId)
+        {
             //We want to continue using attachments for now
 #pragma warning disable 618
             var result = await bodyStorage.TryFetch(messageId).ConfigureAwait(false);
 #pragma warning restore 618
-            Etag currentEtag;
-
-            HttpContent content;
-            if (!result.HasResult)
+            if (result.HasResult)
             {
-                using (var session = documentStore.OpenAsyncSession())
+                var response = request.CreateResponse(HttpStatusCode.OK);
+                var content = new StreamContent(result.Stream);
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse(result.ContentType);
+                content.Headers.ContentLength = result.BodySize;
+                response.Headers.ETag = new EntityTagHeaderValue($"\"{result.Etag}\"");
+                response.Content = content;
+            }
+
+            return request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        async Task<HttpResponseMessage> TryFetchFromIndex(HttpRequestMessage request, string messageId)
+        {
+            using (var session = documentStore.OpenAsyncSession())
+            {
+                var message = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
+                    .Statistics(out var stats)
+                    .TransformWith<MessagesBodyTransformer, MessagesBodyTransformer.Result>()
+                    .FirstOrDefaultAsync(f => f.MessageId == messageId)
+                    .ConfigureAwait(false);
+
+                if (message == null)
                 {
-                    var message = await session.Query<MessagesViewIndex.SortAndFilterOptions, MessagesViewIndex>()
-                        .Statistics(out var stats)
-                        .TransformWith<MessagesBodyTransformer, MessagesBodyTransformer.Result>()
-                        .FirstOrDefaultAsync(f => f.MessageId == messageId)
-                        .ConfigureAwait(false);
-
-                    if (message == null)
-                    {
-                        return request.CreateResponse(HttpStatusCode.NotFound);
-                    }
-
-                    if (message.BodyNotStored)
-                    {
-                        return request.CreateResponse(HttpStatusCode.NoContent);
-                    }
-
-                    if (message.Body == null)
-                    {
-                        return request.CreateResponse(HttpStatusCode.NotFound);
-                    }
-
-                    content = new StringContent(message.Body);
-                    contentType = message.ContentType;
-                    bodySize = message.BodySize;
-                    currentEtag = stats.IndexEtag;
+                    return request.CreateResponse(HttpStatusCode.NotFound);
                 }
-            }
-            else
-            {
-                content = new StreamContent(result.Stream);
-                contentType = result.ContentType;
-                bodySize = result.BodySize;
-                currentEtag = result.Etag;
-            }
 
-            var response = request.CreateResponse(HttpStatusCode.OK);
-            content.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
-            content.Headers.ContentLength = bodySize;
-            response.Headers.ETag = new EntityTagHeaderValue($"\"{currentEtag}\"");
-            response.Content = content;
-            return response;
+                if (message.BodyNotStored && message.Body == null)
+                {
+                    return request.CreateResponse(HttpStatusCode.NoContent);
+                }
+
+                var response = request.CreateResponse(HttpStatusCode.OK);
+                var content = new StringContent(message.Body);
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse(message.ContentType);
+                content.Headers.ContentLength = message.BodySize;
+                response.Headers.ETag = new EntityTagHeaderValue($"\"{stats.IndexEtag}\"");
+                response.Content = content;
+                return response;
+            }
         }
 
         readonly IBodyStorage bodyStorage;
