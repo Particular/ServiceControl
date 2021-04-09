3aca597040a5930d3b420ec8c23107d3e61b0c75
Ramon Smits <ramon.smits@gmail.com>
2021-04-09 15:44:38 +0200

    Temporaritly use Azure Container Registry by prefixing tags with `particular.azurecr.io` and only building ASBS images.

--- src/docker/builddockerimages.ps1
+++ src/docker/builddockerimages.ps1
@@ -3,26 +3,26 @@
 #
 msbuild ../ServiceControl.sln /t:Build /p:Configuration=Release
 
-docker build -f .\servicecontrol.azureservicebus.dockerfile -t particular/servicecontrol.azureservicebus ./../
-docker build -f .\servicecontrol.azureservicebus.audit.dockerfile -t particular/servicecontrol.azureservicebus.audit ./../
-docker build -f .\servicecontrol.azureservicebus.monitoring.dockerfile -t particular/servicecontrol.azureservicebus.monitoring ./../
+docker build -f .\servicecontrol.azureservicebus.dockerfile -t particular.azurecr.io/servicecontrol.azureservicebus ./../
+docker build -f .\servicecontrol.azureservicebus.audit.dockerfile -t particular.azurecr.io/servicecontrol.azureservicebus.audit ./../
+docker build -f .\servicecontrol.azureservicebus.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.azureservicebus.monitoring ./../
 
-docker build -f .\servicecontrol.rabbitmq.conventional.dockerfile -t particular/servicecontrol.rabbitmq.conventional ./../
-docker build -f .\servicecontrol.rabbitmq.conventional.audit.dockerfile -t particular/servicecontrol.rabbitmq.conventional.audit ./../
-docker build -f .\servicecontrol.rabbitmq.conventional.monitoring.dockerfile -t particular/servicecontrol.rabbitmq.conventional.monitoring ./../
+# docker build -f .\servicecontrol.rabbitmq.conventional.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.conventional ./../
+# docker build -f .\servicecontrol.rabbitmq.conventional.audit.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.conventional.audit ./../
+# docker build -f .\servicecontrol.rabbitmq.conventional.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.conventional.monitoring ./../
 
-docker build -f .\servicecontrol.rabbitmq.direct.dockerfile -t particular/servicecontrol.rabbitmq.direct ./../
-docker build -f .\servicecontrol.rabbitmq.direct.audit.dockerfile -t particular/servicecontrol.rabbitmq.direct.audit ./../
-docker build -f .\servicecontrol.rabbitmq.direct.monitoring.dockerfile -t particular/servicecontrol.rabbitmq.direct.monitoring ./../
+# docker build -f .\servicecontrol.rabbitmq.direct.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.direct ./../
+# docker build -f .\servicecontrol.rabbitmq.direct.audit.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.direct.audit ./../
+# docker build -f .\servicecontrol.rabbitmq.direct.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.rabbitmq.direct.monitoring ./../
 
-docker build -f .\servicecontrol.azurestoragequeues.dockerfile -t particular/servicecontrol.azurestoragequeues ./../
-docker build -f .\servicecontrol.azurestoragequeues.audit.dockerfile -t particular/servicecontrol.azurestoragequeues.audit ./../
-docker build -f .\servicecontrol.azurestoragequeues.monitoring.dockerfile -t particular/servicecontrol.azurestoragequeues.monitoring ./../
+# docker build -f .\servicecontrol.azurestoragequeues.dockerfile -t particular.azurecr.io/servicecontrol.azurestoragequeues ./../
+# docker build -f .\servicecontrol.azurestoragequeues.audit.dockerfile -t particular.azurecr.io/servicecontrol.azurestoragequeues.audit ./../
+# docker build -f .\servicecontrol.azurestoragequeues.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.azurestoragequeues.monitoring ./../
 
-docker build -f .\servicecontrol.sqlserver.dockerfile -t particular/servicecontrol.sql ./../
-docker build -f .\servicecontrol.sqlserver.audit.dockerfile -t particular/servicecontrol.sql.audit ./../
-docker build -f .\servicecontrol.sqlserver.monitoring.dockerfile -t particular/servicecontrol.sql.monitoring ./../
+# docker build -f .\servicecontrol.sqlserver.dockerfile -t particular.azurecr.io/servicecontrol.sql ./../
+# docker build -f .\servicecontrol.sqlserver.audit.dockerfile -t particular.azurecr.io/servicecontrol.sql.audit ./../
+# docker build -f .\servicecontrol.sqlserver.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.sql.monitoring ./../
 
-docker build -f .\servicecontrol.amazonsqs.dockerfile -t particular/servicecontrol.amazonsqs ./../
-docker build -f .\servicecontrol.amazonsqs.audit.dockerfile -t particular/servicecontrol.amazonsqs.audit ./../
-docker build -f .\servicecontrol.amazonsqs.monitoring.dockerfile -t particular/servicecontrol.amazonsqs.monitoring ./../
+# docker build -f .\servicecontrol.amazonsqs.dockerfile -t particular.azurecr.io/servicecontrol.amazonsqs ./../
+# docker build -f .\servicecontrol.amazonsqs.audit.dockerfile -t particular.azurecr.io/servicecontrol.amazonsqs.audit ./../
+# docker build -f .\servicecontrol.amazonsqs.monitoring.dockerfile -t particular.azurecr.io/servicecontrol.amazonsqs.monitoring ./../
