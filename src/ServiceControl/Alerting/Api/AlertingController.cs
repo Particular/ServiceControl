e93d97d3fb04ee110c026fb28fca9bab48bf49d5
Tomek Masternak <tomasz.masternak@particular.net>
2021-04-13 16:08:20 +0200

    alerting api scafolding

---
+++ src/ServiceControl/Alerting/Api/AlertingController.cs
@@ -0,0 +1,32 @@
+﻿namespace ServiceControl.Alerting.Api
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Infrastructure.WebApi;
+
+    public class AlertingController : ApiController
+    {
+
+        [Route("alerting")]
+        [HttpGet]
+        public Task<HttpResponseMessage> GetAlertingSettings()
+        {
+            var settings = new
+            {
+                SmtpAddress = "smtp.test.net",
+                SmtpPort = 1234,
+                AuthorizationAccount = "account",
+                //TODO: check if the password is set and convert to a flag
+                AuthroizationPassword = "",
+                EnableSSL = false,
+                AlertingEnabled = true
+            };
+
+            //TODO: add etag
+            var result = Negotiator.FromModel(Request, settings);
+
+            return Task.FromResult(result);
+        }
+    }
+}
