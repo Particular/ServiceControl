2c011d78584a2e6eb7290010cb5200c5753fa76d
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-04-15 14:25:40 +0200

    First draft of e-mail sending

---
+++ src/ServiceControl/Infrastructure/Mail/MailNotificationFeature.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceControl.Infrastructure.Mail
+{
+    using NServiceBus;
+    using NServiceBus.Features;
+    using Raven.Client;
+
+    class MailNotificationFeature : Feature
+    {
+        public MailNotificationFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Container.ConfigureComponent(b => new CustomChecksMailNotification(b.Build<IDocumentStore>()), DependencyLifecycle.SingleInstance);
+        }
+    }
+}
