03e3452a2d370ed75ffdff81798deadfe1df8fa0
Tomek Masternak <tomasz.masternak@particular.net>
2021-04-15 10:59:32 +0200

    alerting configuration controller

---
+++ src/ServiceControl/Alerting/Api/UpdateAlertingSettingsRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceControl.Alerting.Api
+{
+    using Newtonsoft.Json;
+
+    public class UpdateAlertingSettingsRequest
+    {
+        [JsonProperty(PropertyName = "smtp_server")]
+        public string SmtpServer { get; set; }
+
+        [JsonProperty(PropertyName = "smtp_port")]
+        public int SmtpPort { get; set; }
+
+        [JsonProperty(PropertyName = "authorization_account")]
+        public string AuthorizationAccount { get; set; }
+
+        [JsonProperty(PropertyName = "authorization_password")]
+        public string AuthorizationPassword { get; set; }
+
+        [JsonProperty(PropertyName = "enable_ssl")]
+        public bool EnableSSL { get; set; }
+
+        [JsonProperty(PropertyName = "alerting_enabled")]
+        public bool AlertingEnabled { get; set; }
+    }
+}
