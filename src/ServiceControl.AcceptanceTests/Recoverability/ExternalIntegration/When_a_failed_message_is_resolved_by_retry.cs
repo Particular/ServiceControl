c9eca29717d581fb41015539e71244f893f82868
Tomek Masternak <tomasz.masternak@particular.net>
2021-04-20 12:07:08 +0200

    moving other tests to the base class approach

--- src/ServiceControl.AcceptanceTests/Recoverability/ExternalIntegration/When_a_failed_message_is_resolved_by_retry.cs
+++ src/ServiceControl.AcceptanceTests/Recoverability/ExternalIntegration/When_a_failed_message_is_resolved_by_retry.cs
@@ -1,39 +1,26 @@
 ﻿namespace ServiceControl.AcceptanceTests.Recoverability.ExternalIntegration
 {
-    using System;
     using System.Threading.Tasks;
     using AcceptanceTesting;
-    using Infrastructure;
     using NServiceBus;
     using NServiceBus.AcceptanceTesting;
-    using NServiceBus.Settings;
-    using ServiceBus.Management.Infrastructure.Settings;
     using NUnit.Framework;
-    using ServiceControl.Contracts;
+    using Contracts;
     using ServiceControl.MessageFailures;
-    using TestSupport;
-    using TestSupport.EndpointTemplates;
     using Newtonsoft.Json;
 
-    class When_a_failed_message_is_resolved_by_retry : AcceptanceTest
+    class When_a_failed_message_is_resolved_by_retry : When_a_message_failed
     {
         [Test]
         public async Task Should_publish_notification()
         {
-            CustomConfiguration = config => config.OnEndpointSubscribed<MyContext>((s, ctx) =>
+            CustomConfiguration = config => config.OnEndpointSubscribed<Context>((s, ctx) =>
             {
-                if (s.SubscriberReturnAddress.IndexOf("ExternalProcessor", StringComparison.OrdinalIgnoreCase) >= 0)
-                {
-                    ctx.ExternalProcessorSubscribed = true;
-                }
+                ctx.ExternalProcessorSubscribed = s.SubscriberReturnAddress.Contains(nameof(ExternalProcessor));
             });
 
-            var context = await Define<MyContext>()
-                .WithEndpoint<Receiver>(b => b.When(c => c.ExternalProcessorSubscribed, async bus =>
-                {
-                    await bus.SendLocal<MyMessage>(m => m.MessageNumber = 1)
-                        .ConfigureAwait(false);
-                }).DoNotFailOnErrorMessages())
+            var context = await Define<Context>()
+                .WithEndpoint<ErrorSender>()
                 .WithEndpoint<ExternalProcessor>(b => b.When(async (bus, c) =>
                 {
                     await bus.Subscribe<MessageFailureResolvedByRetry>();
@@ -43,116 +30,25 @@
                         c.ExternalProcessorSubscribed = true;
                     }
                 }))
-                .Do("WaitUntilErrorsContainsFaildMessage", async ctx =>
+                .Do("WaitUntilErrorsContainsFailedMessage", async ctx =>
                 {
-                    if (ctx.FailedMessageId == null)
-                    {
-                        return false;
-                    }
-
-                    FailedMessage failedMessage = await this.TryGet<FailedMessage>($"/api/errors/{ctx.FailedMessageId}");
-                    if (failedMessage == null)
-                    {
-                        return false;
-                    }
-
-                    return true;
+                   return await this.TryGet<FailedMessage>($"/api/errors/{ErrorSender.FailedMessageId}") != null;
                 })
+                .Do("WaitForExternalProcessorToSubscribe", ctx => Task.FromResult(ctx.ExternalProcessorSubscribed))
                 .Do("Retry", async ctx =>
                 {
-                    ctx.AboutToSendRetry = true;
-                    ctx.FailProcessing = false;
-                    await this.Post<object>($"/api/errors/{ctx.FailedMessageId}/retry");
+                    await this.Post<object>($"/api/errors/{ErrorSender.FailedMessageId}/retry");
                 })
                 .Do("EnsureRetried", async ctx =>
                 {
-                    return await this.TryGet<FailedMessage>($"/api/errors/{ctx.FailedMessageId}",
+                    return await this.TryGet<FailedMessage>($"/api/errors/{ErrorSender.FailedMessageId}",
                         e => e.Status == FailedMessageStatus.Resolved);
                 })
                 .Done(ctx => ctx.EventDelivered) //Done when sequence is finished
                 .Run();
 
             var deserializedEvent = JsonConvert.DeserializeObject<MessageFailureResolvedByRetry>(context.Event);
-            Assert.IsTrue(deserializedEvent.FailedMessageId == context.FailedMessageId);
-        }
-
-        public class Receiver : EndpointConfigurationBuilder
-        {
-            public Receiver()
-            {
-                EndpointSetup<DefaultServer>(c =>
-                {
-                    c.NoDelayedRetries();
-                    c.ReportSuccessfulRetriesToServiceControl();
-                });
-            }
-
-            public class MyMessageHandler : IHandleMessages<MyMessage>
-            {
-                public MyContext Context { get; set; }
-                public ReadOnlySettings Settings { get; set; }
-
-                public Task Handle(MyMessage message, IMessageHandlerContext context)
-                {
-                    var messageId = context.MessageId.Replace(@"\", "-");
-
-                    var uniqueMessageId = DeterministicGuid.MakeId(messageId, Settings.EndpointName()).ToString();
-
-                    if (message.MessageNumber == 1)
-                    {
-                        Context.FailedMessageId = uniqueMessageId;
-                    }
-
-                    if (Context.FailProcessing)
-                    {
-                        throw new Exception("Simulated exception");
-                    }
-
-                    return Task.FromResult(0);
-                }
-            }
-        }
-
-        public class ExternalProcessor : EndpointConfigurationBuilder
-        {
-            public ExternalProcessor()
-            {
-                EndpointSetup<DefaultServer>(c =>
-                {
-                    var routing = c.ConfigureTransport().Routing();
-                    routing.RouteToEndpoint(typeof(MessageFailureResolvedByRetry).Assembly, Settings.DEFAULT_SERVICE_NAME);
-                }, publisherMetadata => { publisherMetadata.RegisterPublisherFor<MessageFailureResolvedByRetry>(Settings.DEFAULT_SERVICE_NAME); });
-            }
-
-            public class FailureHandler : IHandleMessages<MessageFailureResolvedByRetry>
-            {
-                public MyContext Context { get; set; }
-
-                public Task Handle(MessageFailureResolvedByRetry message, IMessageHandlerContext context)
-                {
-                    var serializedMessage = JsonConvert.SerializeObject(message);
-                    Context.Event = serializedMessage;
-                    Context.EventDelivered = true;
-                    return Task.FromResult(0);
-                }
-            }
-        }
-
-        public class MyMessage : ICommand
-        {
-            public int MessageNumber { get; set; }
-        }
-
-        public class MyContext : ScenarioContext, ISequenceContext
-        {
-            public string FailedMessageId { get; set; }
-            public string GroupId { get; set; }
-            public bool FailProcessing { get; set; } = true;
-            public int Step { get; set; }
-            public bool ExternalProcessorSubscribed { get; set; }
-            public string Event { get; set; }
-            public bool EventDelivered { get; set; }
-            public bool AboutToSendRetry { get; internal set; }
+            Assert.IsTrue(deserializedEvent?.FailedMessageId == ErrorSender.FailedMessageId.ToString());
         }
     }
 }
