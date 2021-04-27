a62829b199746cc06b81ac07c541df833ee48029
Tomek Masternak <tomasz.masternak@particular.net>
2021-04-27 14:22:35 +0200

    adding warning note when recovering from smtp send failure

--- src/ServiceControl/Notifications/Mail/EmailThrottlingState.cs
+++ src/ServiceControl/Notifications/Mail/EmailThrottlingState.cs
@@ -4,7 +4,8 @@
 
     public class EmailThrottlingState
     {
-        public SemaphoreSlim Semaphore = new SemaphoreSlim(1);
+        public SemaphoreSlim Semaphore { get; } = new SemaphoreSlim(1);
+        public string RetriedMessageId { get; set; }
         bool throttling;
 
         public bool IsThrottling() => Volatile.Read(ref throttling);
