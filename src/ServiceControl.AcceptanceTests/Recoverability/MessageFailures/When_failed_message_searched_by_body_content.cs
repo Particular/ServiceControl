a26af9aadf0817445e4ca54b1e9517586a10673a
danielmarbach <daniel.marbach@openplace.net>
2021-04-27 12:43:49 +0200

    Coverage of search by body for failed messages

---
+++ src/ServiceControl.AcceptanceTests/Recoverability/MessageFailures/When_failed_message_searched_by_body_content.cs
@@ -0,0 +1,135 @@
+﻿namespace ServiceControl.AcceptanceTests.Recoverability.MessageFailures
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using CompositeViews.Messages;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Customization;
+    using NServiceBus.Settings;
+    using NUnit.Framework;
+    using TestSupport.EndpointTemplates;
+
+    class When_failed_message_searched_by_body_content : AcceptanceTest
+    {
+        [Test]
+        public async Task Should_be_found_when_fulltext_search_enabled()
+        {
+            // setting it even if it is the default
+            SetSettings = settings => settings.EnableFullTextSearchOnBodies = true;
+
+            var searchString = "42";
+
+            var context = await Define<MyContext>()
+                .WithEndpoint<Sender>(b => b.When((bus, c) => bus.Send(new MyMessage
+                {
+                    Something = "Somewhere in the body is the answer to all of the questions. 42"
+                })))
+                .WithEndpoint<Receiver>(b => b.DoNotFailOnErrorMessages())
+                .Done(async c =>
+                {
+                    if (c.MessageId != null && await this.TryGetMany<MessagesView>($"/api/messages/search/{c.MessageId}"))
+                    {
+                        c.MessageIngested = true;
+                    }
+
+                    if (!c.MessageIngested)
+                    {
+                        return false;
+                    }
+                    c.MessageFound = await this.TryGetMany<MessagesView>($"/api/messages/search/{searchString}");
+                    return true;
+                })
+                .Run();
+
+            Assert.IsTrue(context.MessageFound);
+        }
+
+        [Test]
+        public async Task Should_not_be_found_when_fulltext_search_disabled()
+        {
+            SetSettings = settings => settings.EnableFullTextSearchOnBodies = false;
+
+            var searchString = "42";
+
+            var context = await Define<MyContext>()
+                .WithEndpoint<Sender>(b => b.When((bus, c) => bus.Send(new MyMessage
+                {
+                    Something = "Somewhere in the body is the answer to all of the questions. 42"
+                })))
+                .WithEndpoint<Receiver>(b => b.DoNotFailOnErrorMessages())
+                .Done(async c =>
+                {
+                    if (c.MessageId != null && await this.TryGetMany<MessagesView>($"/api/messages/search/{c.MessageId}"))
+                    {
+                        c.MessageIngested = true;
+                    }
+
+                    if (!c.MessageIngested)
+                    {
+                        return false;
+                    }
+
+                    c.MessageFound = await this.TryGetMany<MessagesView>($"/api/messages/search/{searchString}");
+                    return true;
+                })
+                .Run();
+
+            Assert.IsTrue(context.MessageIngested);
+            Assert.IsFalse(context.MessageFound);
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    var routing = c.ConfigureTransport().Routing();
+                    routing.RouteToEndpoint(typeof(MyMessage), typeof(Receiver));
+                });
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    var recoverability = c.Recoverability();
+                    recoverability.Immediate(s => s.NumberOfRetries(0));
+                    recoverability.Delayed(s => s.NumberOfRetries(0));
+                });
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                public MyContext Context { get; set; }
+
+                public ReadOnlySettings Settings { get; set; }
+
+                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    Context.MessageId = context.MessageId;
+                    throw new Exception("Simulated exception");
+                }
+            }
+        }
+
+        public class MyMessage : ICommand
+        {
+            public string Something { get; set; }
+        }
+
+        public class MyContext : ScenarioContext
+        {
+            public string MessageId { get; set; }
+
+            public bool MessageIngested { get; set; }
+
+            public bool MessageFound { get; set; }
+        }
+    }
+}
