088af5e106b21d7fbfe129c009cc807ac04c8cf6
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-05-17 09:25:19 +0200

    Use header name constant

--- src/ServiceControl/ExternalIntegrations/RemoveVersionInformationBehavior.cs
+++ src/ServiceControl/ExternalIntegrations/RemoveVersionInformationBehavior.cs
@@ -8,21 +8,20 @@
     {
         public override Task Invoke(IOutgoingPhysicalMessageContext context, Func<Task> next)
         {
-            if (context.Headers.TryGetValue(EnclosedTypesHeaderName, out var qualifiedTypeName)
+            if (context.Headers.TryGetValue(NServiceBus.Headers.EnclosedMessageTypes, out var qualifiedTypeName)
                 && qualifiedTypeName.EndsWith(ContractsAssemblyIdentifier, StringComparison.OrdinalIgnoreCase))
             {
                 int startIndex = qualifiedTypeName.IndexOf(VersionPart, StringComparison.Ordinal);
                 if (startIndex >= 0)
                 {
                     qualifiedTypeName = qualifiedTypeName.Substring(0, startIndex);
-                    context.Headers[EnclosedTypesHeaderName] = qualifiedTypeName;
+                    context.Headers[NServiceBus.Headers.EnclosedMessageTypes] = qualifiedTypeName;
                 }
             }
 
             return next();
         }
 
-        const string EnclosedTypesHeaderName = "NServiceBus.EnclosedMessageTypes";
         const string ContractsAssemblyIdentifier = "ServiceControl.Contracts, Version=3.0.0.0, Culture=neutral, PublicKeyToken=6f2d506f609d9f4d";
         const string VersionPart = ", Version=3.0.0.0, Culture=neutral, PublicKeyToken=6f2d506f609d9f4d";
     }
