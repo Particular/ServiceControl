d6b2b3c8fc69687a4ae2a14df68d3b74d3d9703e
Tomek Masternak <tomasz.masternak@particular.net>
2021-05-24 18:47:11 +0200

    making api work

--- src/ServiceControl.Monitoring/Hosting/Commands/RunCommand.cs
+++ src/ServiceControl.Monitoring/Hosting/Commands/RunCommand.cs
@@ -7,6 +7,7 @@ namespace ServiceControl.Monitoring
     using Autofac;
     using Autofac.Extensions.DependencyInjection;
     using Autofac.Features.ResolveAnything;
+    using Http.Diagrams;
     using Infrastructure.OWIN;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -37,6 +38,8 @@ namespace ServiceControl.Monitoring
                         containerBuilder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource(type => type.Assembly == typeof(Bootstrapper).Assembly && type.GetInterfaces().Any() == false));
                         containerBuilder.RegisterInstance(settings);
                         containerBuilder.Register(c => buildQueueLengthProvider(c.Resolve<QueueLengthStore>())).As<IProvideQueueLength>().SingleInstance();
+                        
+                        containerBuilder.RegisterType<DiagramApiController>().InstancePerLifetimeScope();
 
                         IContainer container = null;
                         containerBuilder.RegisterBuildCallback(c => container = c);
