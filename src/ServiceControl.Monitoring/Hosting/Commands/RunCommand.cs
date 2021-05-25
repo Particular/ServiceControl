ef1e52d277beb85f8277898fbe8f4fd66de5b9a0
Mike Minutillo <mike.minutillo@particular.net>
2021-05-25 15:28:39 +0800

    Enable running as a windows service

--- src/ServiceControl.Monitoring/Hosting/Commands/RunCommand.cs
+++ src/ServiceControl.Monitoring/Hosting/Commands/RunCommand.cs
@@ -1,7 +1,6 @@
 namespace ServiceControl.Monitoring
 {
-    //using System;
-    //using System.ServiceProcess;
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using Autofac;
@@ -23,7 +22,7 @@ namespace ServiceControl.Monitoring
         {
             //RunAsWindowsService can't be a property on Settings class because it
             //would be exposed as app.config configurable option and break ATT approvals
-            //var runAsWindowsService = !Environment.UserInteractive && !settings.Portable;
+            var runAsWindowsService = !Environment.UserInteractive && !settings.Portable;
 
             var transportCustomization = settings.LoadTransportCustomization();
             var buildQueueLengthProvider = Bootstrapper.QueueLengthProviderBuilder(settings.ConnectionString, transportCustomization);
@@ -63,78 +62,18 @@ namespace ServiceControl.Monitoring
                         configuration);
 
                     return configuration;
-
                 });
-            return host.RunConsoleAsync();
 
-            //if (runAsWindowsService)
-            //{
-            //    host.RunAsService();
+            if (runAsWindowsService)
+            {
+                host.UseWindowsService();
+            }
+            else
+            {
+                host.UseConsoleLifetime();
+            }
 
-            //    using (var service = new Host { Settings = settings, ServiceName = settings.ServiceName })
-            //    {
-            //        //HINT: this calls-back to Windows Service Control Manager (SCM) and hangs
-            //        //      until service reports it has stopped.
-            //        //      SCM takes over and calls OnStart and OnStop on the service instance. 
-            //        ServiceBase.Run(service);
-            //    }
-            //}
-            //else
-            //{
-            //}
-
-            //return Task.CompletedTask;
+            return host.Build().RunAsync();
         }
-
-        //async Task RunAsConsoleApp(Settings settings)
-        //{
-        //    using (var service = new Host
-        //    {
-        //        Settings = settings,
-        //        ServiceName = settings.ServiceName
-        //    })
-        //    {
-        //        var tcs = new TaskCompletionSource<bool>();
-
-        //        Action done = () =>
-        //        {
-        //            service.OnStopping = () => { };
-        //            tcs.SetResult(true);
-        //        };
-
-        //        service.OnStopping = done;
-
-        //        OnConsoleCancel.Run(done);
-
-        //        service.Start();
-
-        //        Console.WriteLine("Press Ctrl+C to exit");
-
-        //        await tcs.Task.ConfigureAwait(false);
-        //    }
-        //}
-
-        //class OnConsoleCancel
-        //{
-        //    OnConsoleCancel(Action action)
-        //    {
-        //        this.action = action;
-        //    }
-
-        //    public static void Run(Action action)
-        //    {
-        //        var onCancelAction = new OnConsoleCancel(action);
-        //        Console.CancelKeyPress += onCancelAction.ConsoleOnCancelKeyPress;
-        //    }
-
-        //    void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
-        //    {
-        //        action();
-        //        e.Cancel = true;
-        //        Console.CancelKeyPress -= ConsoleOnCancelKeyPress;
-        //    }
-
-        //    Action action;
-        //}
     }
 }
