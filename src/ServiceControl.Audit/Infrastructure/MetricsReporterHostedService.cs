a577cc720819540d0c5470f3847acf9888dc05f6
Tomek Masternak <tomasz.masternak@particular.net>
2021-05-26 11:14:40 +0200

    metrics reported moved to hosted service

---
+++ src/ServiceControl.Audit/Infrastructure/MetricsReporterHostedService.cs
@@ -0,0 +1,30 @@
+﻿namespace ServiceControl.Audit.Infrastructure
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using NServiceBus.Logging;
+    using ServiceControl.Infrastructure.Metrics;
+
+    class MetricsReporterHostedService : IHostedService
+    {
+        readonly Metrics metrics;
+        MetricsReporter reporter;
+
+        public MetricsReporterHostedService(Metrics metrics) => this.metrics = metrics;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var metricsLog = LogManager.GetLogger("Metrics");
+
+            reporter = new MetricsReporter(metrics, x => metricsLog.Info(x), TimeSpan.FromSeconds(5));
+
+            reporter.Start();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => reporter.Stop();
+    }
+}
