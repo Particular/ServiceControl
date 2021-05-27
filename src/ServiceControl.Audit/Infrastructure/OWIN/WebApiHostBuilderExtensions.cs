389d71462a5adf678b74642c3ff0e21ca393b17b
Tomek Masternak <tomasz.masternak@particular.net>
2021-05-27 10:11:36 +0200

    generic host extension method for web api

---
+++ src/ServiceControl.Audit/Infrastructure/OWIN/WebApiHostBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace ServiceControl.Audit.Infrastructure.OWIN
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web.Http.Controllers;
+    using Auditing.MessagesView;
+    using Autofac;
+    using Autofac.Core.Activators.Reflection;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using WebApi;
+
+    public static class WebApiHostBuilderExtensions
+    {
+        public static IHostBuilder UseWebApi(this IHostBuilder hostBuilder,
+            List<Action<ContainerBuilder>> registrations, bool startOwinHost)
+        {
+            registrations.Add(RegisterInternalWebApiControllers);
+            registrations.Add(cb => cb.RegisterModule<ApisModule>());
+
+            if (startOwinHost)
+            {
+                hostBuilder.ConfigureServices((ctx, serviceCollection) =>
+                {
+                    serviceCollection.AddHostedService<WebApiHostedService>();
+                });
+            }
+
+            return hostBuilder;
+        }
+
+        static void RegisterInternalWebApiControllers(ContainerBuilder containerBuilder)
+        {
+            var controllerTypes = Assembly.GetExecutingAssembly().DefinedTypes
+                .Where(t => typeof(IHttpController).IsAssignableFrom(t) && t.Name.EndsWith("Controller", StringComparison.Ordinal));
+
+            foreach (var controllerType in controllerTypes)
+            {
+                containerBuilder.RegisterType(controllerType).FindConstructorsWith(new AllConstructorFinder());
+            }
+        }
+
+        class AllConstructorFinder : IConstructorFinder
+        {
+            public ConstructorInfo[] FindConstructors(Type targetType)
+            {
+                var result = Cache.GetOrAdd(targetType, t => t.GetTypeInfo().DeclaredConstructors.ToArray());
+
+                return result.Length > 0 ? result : throw new Exception($"No constructor found for type {targetType.FullName}");
+            }
+
+            static readonly ConcurrentDictionary<Type, ConstructorInfo[]> Cache = new ConcurrentDictionary<Type, ConstructorInfo[]>();
+        }
+    }
+}
