941082584253a2d29adbb9dc851a7e68eae701a9
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-06-01 13:38:37 +0200

    Adding externally owned dependency resolver that prevents premature disposal of the container and all its content

--- src/ServiceControl/Infrastructure/OWIN/Startup.cs
+++ src/ServiceControl/Infrastructure/OWIN/Startup.cs
@@ -5,6 +5,7 @@
     using System.Net.Http.Headers;
     using System.Reflection;
     using System.Web.Http;
+    using System.Web.Http.Dependencies;
     using System.Web.Http.Dispatcher;
     using Autofac;
     using Autofac.Integration.WebApi;
@@ -51,7 +52,7 @@
                 jsonMediaTypeFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.particular.1+json"));
                 config.Formatters.Remove(config.Formatters.XmlFormatter);
 
-                config.DependencyResolver = new AutofacWebApiDependencyResolver(lifetimeScope);
+                config.DependencyResolver = new ExternallyOwnedContainerDependencyResolver(new AutofacWebApiDependencyResolver(lifetimeScope));
 
                 config.MessageHandlers.Add(new XParticularVersionHttpHandler());
                 config.MessageHandlers.Add(new CompressionEncodingHttpHandler());
@@ -85,6 +86,27 @@
 
         readonly ILifetimeScope lifetimeScope;
         readonly List<Assembly> assemblies;
+    }
+
+    class ExternallyOwnedContainerDependencyResolver : System.Web.Http.Dependencies.IDependencyResolver
+    {
+        System.Web.Http.Dependencies.IDependencyResolver impl;
+
+        public ExternallyOwnedContainerDependencyResolver(System.Web.Http.Dependencies.IDependencyResolver impl)
+        {
+            this.impl = impl;
+        }
+
+        public void Dispose()
+        {
+            //NOOP We don't dispose the underlying container
+        }
+
+        public object GetService(Type serviceType) => impl.GetService(serviceType);
+
+        public IEnumerable<object> GetServices(Type serviceType) => impl.GetServices(serviceType);
+
+        public IDependencyScope BeginScope() => impl.BeginScope();
     }
 
     class OnlyExecutingAssemblyResolver : DefaultAssembliesResolver
