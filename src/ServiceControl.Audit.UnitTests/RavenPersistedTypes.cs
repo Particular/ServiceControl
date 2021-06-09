789ff1402e94f0ad49381a1a55fed8bc8d98f5fe
Tomek Masternak <tomasz.masternak@particular.net>
2021-06-09 13:19:59 +0200

    raven document types approval tests

---
+++ src/ServiceControl.Audit.UnitTests/RavenPersistedTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceControl.UnitTests
+{
+    using System;
+    using System.Linq;
+    using Audit.Infrastructure;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using Raven.Client.Indexes;
+
+    class RavenPersistedTypes
+    {
+        [Test]
+        public void Verify()
+        {
+            var documentTypes = typeof(Bootstrapper).Assembly.GetTypes()
+                .Where(type => typeof(AbstractIndexCreationTask).IsAssignableFrom(type))
+                .SelectMany(indexType => indexType.BaseType?.GenericTypeArguments)
+                .Distinct();
+
+            var documentTypeNames = string.Join(Environment.NewLine, documentTypes.Select(t => t.AssemblyQualifiedName));
+
+            Approver.Verify(documentTypeNames);
+        }
+    }
+}
---
+++ src/ServiceControl.UnitTests/RavenPersistedTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceControl.UnitTests
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using Particular.ServiceControl;
+    using Raven.Client.Indexes;
+
+    class RavenPersistedTypes
+    {
+        [Test]
+        public void Verify()
+        {
+            var documentTypes = typeof(Bootstrapper).Assembly.GetTypes()
+                .Where(type => typeof(AbstractIndexCreationTask).IsAssignableFrom(type))
+                .SelectMany(indexType => indexType.BaseType?.GenericTypeArguments)
+                .Distinct();
+
+            var documentTypeNames = string.Join(Environment.NewLine, documentTypes.Select(t => t.AssemblyQualifiedName));
+
+            Approver.Verify(documentTypeNames);
+        }
+    }
+}
