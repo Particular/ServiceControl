53576cee7684cbdfc38f3db2db29cd015bb3d04f
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-07-05 14:28:21 +0200

    Add more descriptive comments for the legacy successful retry confirmation messages

--- src/ServiceControl/Contracts/MessageFailures/MessageFailureResolvedByRetry.cs
+++ src/ServiceControl/Contracts/MessageFailures/MessageFailureResolvedByRetry.cs
@@ -2,7 +2,10 @@
 {
     using NServiceBus;
 
-    // Comes from unconverted legacy instances
+    /// <summary>
+    /// A message used by ServiceControl before Audit split (before 4.0) to inform that the audited message has been successfully retried. When upgrading from 3.x to 4.20 and higher
+    /// these legacy messages may still be in the input queue so a handler for these is needed.
+    /// </summary>
     public class MessageFailureResolvedByRetry : IMessage
     {
         public string FailedMessageId { get; set; }
