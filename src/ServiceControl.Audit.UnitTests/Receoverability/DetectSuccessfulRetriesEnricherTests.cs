ca5a18403680da0ad82a56cbd229f7bfc08daa61
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-07-14 12:04:04 +0200

    Replace acceptance test with a set of unit test for the detect successful retry behavior

---
+++ src/ServiceControl.Audit.UnitTests/Receoverability/DetectSuccessfulRetriesEnricherTests.cs
@@ -0,0 +1,81 @@
+﻿namespace ServiceControl.Audit.UnitTests.Receoverability
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Audit.Auditing;
+    using Contracts.MessageFailures;
+    using NServiceBus;
+    using NServiceBus.Transport;
+    using NUnit.Framework;
+    using Recoverability;
+
+    [TestFixture]
+    public class DetectSuccessfulRetriesEnricherTests
+    {
+        [Test]
+        public void It_does_not_sent_acknowledgement_if_audit_comes_from_new_endpoint_version()
+        {
+            var enricher = new DetectSuccessfulRetriesEnricher();
+
+            var headers = new Dictionary<string, string>
+            {
+                ["ServiceControl.Retry.UniqueMessageId"] = "MyId",
+                ["ServiceControl.Retry.AcknowledgementQueue"] = "ErrorQueue",
+                ["ServiceControl.Retry.AcknowledgementSent"] = "true"
+            };
+
+            var outgoingCommands = new List<ICommand>();
+            var transportOperations = new List<TransportOperation>();
+            var metadata = new Dictionary<string, object>();
+
+            enricher.Enrich(new AuditEnricherContext(headers, outgoingCommands, transportOperations, metadata));
+
+            CollectionAssert.IsEmpty(outgoingCommands);
+            CollectionAssert.IsEmpty(transportOperations);
+        }
+
+        [Test]
+        public void It_sends_legacy_command_if_retry_comes_from_old_ServiceControl()
+        {
+            var enricher = new DetectSuccessfulRetriesEnricher();
+
+            var headers = new Dictionary<string, string>
+            {
+                ["ServiceControl.Retry.UniqueMessageId"] = "MyId"
+            };
+
+            var outgoingCommands = new List<ICommand>();
+            var transportOperations = new List<TransportOperation>();
+            var metadata = new Dictionary<string, object>();
+
+            enricher.Enrich(new AuditEnricherContext(headers, outgoingCommands, transportOperations, metadata));
+
+            CollectionAssert.IsNotEmpty(outgoingCommands);
+            CollectionAssert.AllItemsAreInstancesOfType(outgoingCommands, typeof(MarkMessageFailureResolvedByRetry));
+            CollectionAssert.IsEmpty(transportOperations);
+        }
+
+        [Test]
+        public void It_sends_acknowledgement_if_audit_comes_from_old_endpoint_version()
+        {
+            var enricher = new DetectSuccessfulRetriesEnricher();
+
+            var headers = new Dictionary<string, string>
+            {
+                ["ServiceControl.Retry.UniqueMessageId"] = "MyId",
+                ["ServiceControl.Retry.AcknowledgementQueue"] = "ErrorQueue"
+            };
+
+            var outgoingCommands = new List<ICommand>();
+            var transportOperations = new List<TransportOperation>();
+            var metadata = new Dictionary<string, object>();
+
+            enricher.Enrich(new AuditEnricherContext(headers, outgoingCommands, transportOperations, metadata));
+
+            CollectionAssert.IsEmpty(outgoingCommands);
+
+            var ack = transportOperations.Single();
+            Assert.IsTrue(ack.Message.Headers.ContainsKey("ServiceControl.Retry.Successful"));
+        }
+    }
+}
--- src/ServiceControl.Audit/Recoverability/FailedMessagesFeature.cs
+++ src/ServiceControl.Audit/Recoverability/FailedMessagesFeature.cs
@@ -1,15 +1,7 @@
 ﻿namespace ServiceControl.Audit.Recoverability
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Auditing;
-    using Contracts.MessageFailures;
-    using Infrastructure;
     using NServiceBus;
     using NServiceBus.Features;
-    using NServiceBus.Routing;
-    using NServiceBus.Transport;
 
     class FailedMessagesFeature : Feature
     {
@@ -20,84 +12,7 @@
 
         protected override void Setup(FeatureConfigurationContext context)
         {
-            context.Container.ConfigureComponent<DetectSuccessfullRetriesEnricher>(DependencyLifecycle.SingleInstance);
-        }
-
-        class DetectSuccessfullRetriesEnricher : IEnrichImportedAuditMessages
-        {
-            public void Enrich(AuditEnricherContext context)
-            {
-                var headers = context.Headers;
-                var isOldRetry = headers.TryGetValue("ServiceControl.RetryId", out _);
-                var isNewRetry = headers.TryGetValue("ServiceControl.Retry.UniqueMessageId", out var newRetryMessageId);
-                var isAckHandled = headers.ContainsKey("ServiceControl.Retry.AcknowledgementSent");
-                var hasAckQueue = headers.TryGetValue("ServiceControl.Retry.AcknowledgementQueue", out var ackQueue);
-
-                var hasBeenRetried = isOldRetry || isNewRetry;
-
-                context.Metadata.Add("IsRetried", hasBeenRetried);
-
-                if (!hasBeenRetried || isAckHandled)
-                {
-                    //The message has not been sent for retry from ServiceControl or the endpoint indicated that is already has sent a retry acknowledgement to the 
-                    //ServiceControl main instance. Nothing to do.
-                    return;
-                }
-
-                if (hasAckQueue && isNewRetry)
-                {
-                    //The message has been sent for retry from ServiceControl 4.20 or higher (has the ACK queue header) but the endpoint did not recognize the header
-                    //and did not send the acknowledgment. We send it here to the acknowledgment queue.
-                    var ackMessage = new OutgoingMessage(Guid.NewGuid().ToString(), new Dictionary<string, string>
-                    {
-                        ["ServiceControl.Retry.Successful"] = DateTimeExtensions.ToWireFormattedString(DateTime.UtcNow),
-                        ["ServiceControl.Retry.UniqueMessageId"] = newRetryMessageId
-                    }, new byte[0]);
-                    var ackOperation = new TransportOperation(ackMessage, new UnicastAddressTag(ackQueue));
-                    context.AddForSend(ackOperation);
-                }
-                else
-                {
-                    //The message has been sent for retry from ServiceControl older than 4.20. Regardless which version the endpoint was, we need to send a legacy confirmation
-                    //message because the main instance of ServiceControl may still be on version lower than 4.19.
-                    context.AddForSend(new MarkMessageFailureResolvedByRetry
-                    {
-                        FailedMessageId = isOldRetry ? headers.UniqueId() : newRetryMessageId,
-                        AlternativeFailedMessageIds = GetAlternativeUniqueMessageId(headers).ToArray()
-                    });
-                }
-            }
-
-            IEnumerable<string> GetAlternativeUniqueMessageId(IReadOnlyDictionary<string, string> headers)
-            {
-                var messageId = headers.MessageId();
-                if (headers.TryGetValue(Headers.ProcessingEndpoint, out var processingEndpoint))
-                {
-                    yield return DeterministicGuid.MakeId(messageId, processingEndpoint).ToString();
-                }
-
-                if (headers.TryGetValue("NServiceBus.FailedQ", out var failedQ))
-                {
-                    yield return DeterministicGuid.MakeId(messageId, ExtractQueueNameForLegacyReasons(failedQ)).ToString();
-                }
-
-                if (headers.TryGetValue(Headers.ReplyToAddress, out var replyToAddress))
-                {
-                    yield return DeterministicGuid.MakeId(messageId, ExtractQueueNameForLegacyReasons(replyToAddress)).ToString();
-                }
-            }
-
-            static string ExtractQueueNameForLegacyReasons(string address)
-            {
-                var atIndex = address?.IndexOf("@", StringComparison.InvariantCulture);
-
-                if (atIndex.HasValue && atIndex.Value > -1)
-                {
-                    return address.Substring(0, atIndex.Value);
-                }
-
-                return address;
-            }
+            context.Container.ConfigureComponent<DetectSuccessfulRetriesEnricher>(DependencyLifecycle.SingleInstance);
         }
     }
 }
