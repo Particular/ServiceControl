ba5c3552796c5d216574e7c8ef12b10806df86ba
williambza <william.brander@gmail.com>
2021-11-03 09:37:45 +0200

    Revert System.Threading.Channel upgrade

--- src/ServiceControlInstaller.Packaging.UnitTests/DeploymentPackageTests.cs
+++ src/ServiceControlInstaller.Packaging.UnitTests/DeploymentPackageTests.cs
@@ -27,7 +27,13 @@ namespace Tests
                 CollectionAssert.IsEmpty(
                     from mainEntry in mainEntries
                     join entry in zip.Entries.Except(mainEntries) on mainEntry.Name equals entry.Name
-                    where entry.Length != mainEntry.Length
+                    where entry.Length != mainEntry.Length &&
+                        entry.FullName != "Transports/MSMQ/System.Threading.Channels.dll" // NServiceBus.Transport.Msmq references V5.0.0 of this assembly,
+                                                                                          // but only for the native delayed delivery codepath so is not used by ServiceControl.
+                                                                                          // Updating the reference will in turn break NServiceBus.Transport.RabbitMQ
+                                                                                          // as that package references V4.7.1.
+                                                                                          // It should therefore be safe to explicitly exclude this assembly mismatch from
+                                                                                          // the test.
                     select entry.FullName,
                     $"File sizes should match the ones in the {deploymentPackage.ServiceName} folder. Check versions of dependencies.");
             }
