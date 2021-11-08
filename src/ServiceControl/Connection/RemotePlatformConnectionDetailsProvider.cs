cbbdcc4c1c0ac77cabc4a47c9414b19efefe6821
Mike Minutillo <mike.minutillo@particular.net>
2021-11-08 13:07:09 +0800

    Manage remotes in parallel

--- src/ServiceControl/Connection/RemotePlatformConnectionDetailsProvider.cs
+++ src/ServiceControl/Connection/RemotePlatformConnectionDetailsProvider.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
     using Newtonsoft.Json;
+    using NServiceBus.Logging;
     using ServiceBus.Management.Infrastructure.Settings;
 
     class RemotePlatformConnectionDetailsProvider : IProvidePlatformConnectionDetails
@@ -18,28 +20,39 @@
             this.httpClientFactory = httpClientFactory;
         }
 
-        public async Task ProvideConnectionDetails(PlatformConnectionDetails connection)
+        public Task ProvideConnectionDetails(PlatformConnectionDetails connection) =>
+            Task.WhenAll(
+                settings.RemoteInstances
+                    .Select(remote => UpdateFromRemote(remote, connection))
+            );
+
+        async Task UpdateFromRemote(RemoteInstanceSetting remote, PlatformConnectionDetails connection)
         {
             using (var client = httpClientFactory())
             {
-                // TODO: Do this in parallel
-                foreach (var remoteInstance in settings.RemoteInstances)
+                try
                 {
-                    // TODO: Handle failures properly
-                    var result = await client.GetStringAsync($"{remoteInstance.ApiUri}/connection")
+                    var result = await client.GetStringAsync($"{remote.ApiUri}/connection")
                         .ConfigureAwait(false);
                     var dictionary = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(result);
                     if (dictionary == null)
                     {
-                        // TODO: Handle this condition properly
-                        continue;
+                        Log.Warn($"Unexpected response from ${remote.ApiUri}/connection: ${result}");
+                        return;
                     }
+
                     foreach (var kvp in dictionary)
                     {
                         connection.Add(kvp.Key, kvp.Value);
                     }
                 }
+                catch (Exception ex)
+                {
+                    Log.Error($"Unable to get connection details from ${remote.ApiUri}/connection", ex);
+                }
             }
         }
+
+        static readonly ILog Log = LogManager.GetLogger<RemotePlatformConnectionDetailsProvider>();
     }
 }
