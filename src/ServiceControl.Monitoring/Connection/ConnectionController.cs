6a6c83c3e2b4e7a34cd88c2bd7e725d9e32e8ade
Mike Minutillo <mike.minutillo@particular.net>
2021-11-12 17:25:46 +0800

    Add connection api for monitoring

---
+++ src/ServiceControl.Monitoring/Connection/ConnectionController.cs
@@ -0,0 +1,32 @@
+﻿namespace ServiceControl.Monitoring.Connection
+{
+    using System;
+    using System.Web.Http;
+    using Newtonsoft.Json;
+    using NServiceBus;
+    using NServiceBus.Settings;
+
+    public class ConnectionController : ApiController
+    {
+        readonly JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings();
+        readonly string mainInputQueue;
+        readonly TimeSpan defaultInterval = TimeSpan.FromSeconds(1);
+
+        public ConnectionController(ReadOnlySettings nsbSettings) => mainInputQueue = nsbSettings.LocalAddress();
+
+        [Route("connection")]
+        [HttpGet]
+        public IHttpActionResult GetConnectionDetails() =>
+            Json(
+                new
+                {
+                    Metrics = new
+                    {
+                        MetricsQueue = mainInputQueue,
+                        Interval = defaultInterval
+                    }
+                },
+                jsonSerializerSettings
+        );
+    }
+}
