f5d7956a8ad886b31975e58d356d2e66724be8d4
Mike Minutillo <mike.minutillo@particular.net>
2021-11-24 12:10:03 +0800

    Remove unused class

--- src/ServiceControl/Connection/PlatformConnectionQueryStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceControl.Connection
-{
-    using System.Collections.Concurrent;
-
-    public class PlatformConnectionQueryStatus
-    {
-        public ConcurrentBag<string> Exceptions { get; set; } = new ConcurrentBag<string>();
-    }
-}
