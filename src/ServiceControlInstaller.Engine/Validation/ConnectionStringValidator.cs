10bcac4eef6c966583b5b42f57a55547487cf2a8
Dave Rael <dave@raelyard.com>
2022-01-14 05:57:00 -0700

    Use Microsoft.Data.SqlClient for Validation
    The Sql Transport uses Microsoft.Data.SqlClient for interacting with Sql
    Server databases. This is good and necessary to be able to connect to
    Azure Sql using Azure Active Directory credentials. This works fine
    having installed a Service Control instance with the Sql Transport.
    
    Installing an instance, though, fails via both the Service Control
    Management tool and the PowerShell modules. This is because the
    ServiceControlInstallerEngine library validates connection strings by
    attempting to connect to the database specified. It does so using
    System.Data.SqlClient, which does not support the use of Azure Active
    Directory authentication types in the connection string.
    
    To make a seamless experience when using Sql for transport, it's
    necessary for such connection strings to successfully validate.

--- src/ServiceControlInstaller.Engine/Validation/ConnectionStringValidator.cs
+++ src/ServiceControlInstaller.Engine/Validation/ConnectionStringValidator.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Data.Common;
-    using System.Data.SqlClient;
+    using Microsoft.Data.SqlClient;
     using System.Linq;
     using Accounts;
     using Instances;
