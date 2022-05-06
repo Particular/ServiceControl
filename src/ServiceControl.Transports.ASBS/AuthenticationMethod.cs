4ee03aa848a9413542a5b5f30fba005462e3b789
Mike Minutillo <mike.minutillo@particular.net>
2022-05-06 13:35:57 +0800

    Turn casts into polymorphic dispatch (#2855)

--- src/ServiceControl.Transports.ASBS/AuthenticationMethod.cs
+++ src/ServiceControl.Transports.ASBS/AuthenticationMethod.cs
@@ -1,6 +1,11 @@
 ﻿namespace ServiceControl.Transports.ASBS
 {
+    using Azure.Messaging.ServiceBus.Administration;
+    using NServiceBus;
+
     public abstract class AuthenticationMethod
     {
+        public abstract ServiceBusAdministrationClient BuildManagementClient();
+        public abstract void ConfigureConnection(TransportExtensions<AzureServiceBusTransport> transport);
     }
 }
--- src/ServiceControl.Transports.ASBS/TokenCredentialAuthentication.cs
+++ src/ServiceControl.Transports.ASBS/TokenCredentialAuthentication.cs
@@ -1,6 +1,8 @@
 ﻿namespace ServiceControl.Transports.ASBS
 {
     using Azure.Core;
+    using Azure.Messaging.ServiceBus.Administration;
+    using NServiceBus;
 
     public class TokenCredentialAuthentication : AuthenticationMethod
     {
@@ -13,5 +15,14 @@
         public string FullyQualifiedNamespace { get; }
 
         public TokenCredential Credential { get; }
+
+        public override ServiceBusAdministrationClient BuildManagementClient()
+            => new ServiceBusAdministrationClient(FullyQualifiedNamespace, Credential);
+
+        public override void ConfigureConnection(TransportExtensions<AzureServiceBusTransport> transport)
+        {
+            transport.ConnectionString(FullyQualifiedNamespace);
+            transport.CustomTokenCredential(Credential);
+        }
     }
 }
