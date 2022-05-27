ee3b4d69e8aeaddecd8881f753f26e122f274f0f
cquirosj <christian.quiros@particular.net>
2022-05-27 06:09:03 -0600

    Removing teardown

--- src/ServiceControl.Audit.AcceptanceTests/Auditing/SetupCommand_CharacterizationTests.cs
+++ src/ServiceControl.Audit.AcceptanceTests/Auditing/SetupCommand_CharacterizationTests.cs
@@ -132,52 +132,5 @@
             }
         }
         #endregion FakeTransport
-
-        #region TearDown
-        [TearDown]
-        public void TearDown() => DeleteFolder(_workingDirectory);
-
-        static void DeleteFolder(string path)
-        {
-            DirectoryInfo emptyTempDirectory = null;
-
-            if (!Directory.Exists(path))
-            {
-                return;
-            }
-
-            try
-            {
-                emptyTempDirectory = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
-                emptyTempDirectory.Create();
-                var arguments = $"\"{emptyTempDirectory.FullName}\" \"{path.TrimEnd('\\')}\" /W:1  /R:1 /FFT /MIR /NFL";
-                using (var process = Process.Start(new ProcessStartInfo("robocopy")
-                {
-                    Arguments = arguments,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    CreateNoWindow = true
-                }))
-                {
-                    process?.WaitForExit();
-                }
-
-                using (var windowsIdentity = WindowsIdentity.GetCurrent())
-                {
-                    var directorySecurity = new DirectorySecurity();
-                    directorySecurity.SetOwner(windowsIdentity.User);
-                    Directory.SetAccessControl(path, directorySecurity);
-                }
-
-                if (!(Directory.GetFiles(path).Any() || Directory.GetDirectories(path).Any()))
-                {
-                    Directory.Delete(path);
-                }
-            }
-            finally
-            {
-                emptyTempDirectory?.Delete();
-            }
-        }
-        #endregion TearDown
     }
 }
