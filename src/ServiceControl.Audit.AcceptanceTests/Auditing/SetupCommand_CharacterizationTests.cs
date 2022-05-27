106247a043f73e9e346f20710e9f471b52d0e607
cquirosj <christian.quiros@particular.net>
2022-05-27 06:43:14 -0600

    disabling test

--- src/ServiceControl.Audit.AcceptanceTests/Auditing/SetupCommand_CharacterizationTests.cs
+++ src/ServiceControl.Audit.AcceptanceTests/Auditing/SetupCommand_CharacterizationTests.cs
@@ -39,6 +39,7 @@
         public const string TransportInitializedSpyFileName = "initialized.txt";
 
         [Test]
+        [Ignore("Temporarily disabling it to check if it is affecting the CI")]
         public async Task VerifySetupCommandBehavior()
         {
             #region Arrange
@@ -132,5 +133,52 @@
             }
         }
         #endregion FakeTransport
+
+        #region TearDown
+        [TearDown]
+        public void TearDown() => DeleteFolder(_workingDirectory);
+
+        static void DeleteFolder(string path)
+        {
+            DirectoryInfo emptyTempDirectory = null;
+
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
+            try
+            {
+                emptyTempDirectory = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+                emptyTempDirectory.Create();
+                var arguments = $"\"{emptyTempDirectory.FullName}\" \"{path.TrimEnd('\\')}\" /W:1  /R:1 /FFT /MIR /NFL";
+                using (var process = Process.Start(new ProcessStartInfo("robocopy")
+                {
+                    Arguments = arguments,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true
+                }))
+                {
+                    process?.WaitForExit();
+                }
+
+                using (var windowsIdentity = WindowsIdentity.GetCurrent())
+                {
+                    var directorySecurity = new DirectorySecurity();
+                    directorySecurity.SetOwner(windowsIdentity.User);
+                    Directory.SetAccessControl(path, directorySecurity);
+                }
+
+                if (!(Directory.GetFiles(path).Any() || Directory.GetDirectories(path).Any()))
+                {
+                    Directory.Delete(path);
+                }
+            }
+            finally
+            {
+                emptyTempDirectory?.Delete();
+            }
+        }
+        #endregion TearDown
     }
 }
