b4c439da5e15e3dc3ec39a1e1aaad728399a68d2
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-06-01 17:16:39 +0200

    add verification test for controller types

---
+++ src/ServiceControl.Audit.UnitTests/Verification/ControllerConventionsTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceControl.Audit.UnitTests.Verification
+{
+    using System.Linq;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Dispatcher;
+    using Audit.Auditing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ControllerConventionsTest
+    {
+        [Test]
+        public void All_controllers_should_match_convention()
+        {
+            var allControllers = typeof(FailedAuditImport).Assembly.GetTypes().Where(t => typeof(IHttpController).IsAssignableFrom(t)).ToArray();
+            Assert.IsNotEmpty(allControllers);
+            Assert.IsTrue(allControllers.All(c => c.Name.EndsWith(DefaultHttpControllerSelector.ControllerSuffix)));
+        }
+    }
+}
