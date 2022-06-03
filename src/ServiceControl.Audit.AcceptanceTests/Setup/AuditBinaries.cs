fdbe21bc78e61c00c05949b1fa1441f40ca552e1
cquirosj <christian.quiros@particular.net>
2022-06-02 23:42:04 -0600

    Audit instance setup command integration test

---
+++ src/ServiceControl.Audit.AcceptanceTests/Setup/AuditBinaries.cs
@@ -0,0 +1,101 @@
+﻿namespace ServiceControl.Audit.AcceptanceTests.Setup
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using NUnit.Framework;
+    using ServiceControlInstaller.Engine.FileSystem;
+    using ServiceControlInstaller.Engine.Instances;
+    using ServiceControlInstaller.Engine.ReportCard;
+
+    public class AuditBinaries : IDisposable
+    {
+        readonly ServiceControlAuditNewInstance installer = null;
+
+        public static AuditBinaries CopyAndConfigureForSqlT()
+        {
+            var tempRandomDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempRandomDirectory);
+
+            var dbInstanceForSqlT = SqlLocalDb.CreateNewIn(tempRandomDirectory);
+
+            var transportInfo = ServiceControlCoreTransports.All.First(t => t.Name == TransportNames.SQLServer);
+
+            return new AuditBinaries(tempRandomDirectory, transportInfo, dbInstanceForSqlT);
+        }
+
+        public SqlLocalDb SqlTDb { get; } = null;
+
+        AuditBinaries(string workingDirectory, TransportInfo transportInfo, SqlLocalDb sqlLocalDb)
+        {
+            WorkingDirectory = workingDirectory;
+            SqlTDb = sqlLocalDb;
+
+            var deploymentCache = FindZipFolder();
+
+            var zipInfo = ServiceControlAuditZipInfo.Find(deploymentCache);
+
+            installer = new ServiceControlAuditNewInstance()
+            {
+                DBPath = Path.Combine(workingDirectory, "Database"),
+                LogPath = Path.Combine(workingDirectory, "Logs"),
+                InstallPath = Path.Combine(workingDirectory, "Binaries"),
+                AuditQueue = "audittest",
+                ForwardAuditMessages = false,
+                ForwardErrorMessages = false,
+                ErrorQueue = "testerror",
+                TransportPackage = transportInfo,
+                ConnectionString = SqlTDb.ConnectionString,
+                ReportCard = new ReportCard(),
+                AuditRetentionPeriod = TimeSpan.FromDays(1),
+                ServiceControlQueueAddress = "Particular.ServiceControl",
+                Version = zipInfo.Version
+            };
+
+            installer.CopyFiles(zipInfo.FilePath);
+            installer.WriteConfigurationFile();
+        }
+
+        public void ExecuteSetupCommand()
+        {
+            installer.SetupInstance();
+
+            var errors = string.Join(Environment.NewLine, installer.ReportCard.Errors);
+
+            Assert.IsTrue(errors.Length == 0, $"ServiceControl.Audit.exe --setup command execution failed with the following errors: {errors}");
+        }
+
+        public string AuditQueueName => installer.AuditQueue;
+        public string ErrorQueue => installer.AuditQueue;
+        public string WorkingDirectory { get; } = string.Empty;
+
+        string FindZipFolder()
+        {
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+
+            while (true)
+            {
+                if (Directory.EnumerateDirectories(directory).Any(d => d.EndsWith("Zip", StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return $@"{directory}\zip";
+                }
+
+                var parent = Directory.GetParent(directory);
+
+                if (parent == null)
+                {
+                    throw new Exception("Unable to locate the folder named 'Zip' that contains the platform zip files for this build.");
+                }
+
+                directory = parent.FullName;
+            }
+        }
+
+        public void Dispose()
+        {
+            SqlTDb.Detach();
+            DirectoryDeleter.Delete(WorkingDirectory);
+        }
+    }
+
+}
---
+++ src/ServiceControl.Audit.AcceptanceTests/Setup/DirectoryDeleter.cs
@@ -0,0 +1,53 @@
+﻿namespace ServiceControl.Audit.AcceptanceTests.Setup
+{
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Security.AccessControl;
+    using System.Security.Principal;
+
+    static class DirectoryDeleter
+    {
+        public static void Delete(string path)
+        {
+            DirectoryInfo emptyTempDirectory = null;
+
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
+            try
+            {
+                emptyTempDirectory = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+                emptyTempDirectory.Create();
+                var arguments = $"\"{emptyTempDirectory.FullName}\" \"{path.TrimEnd('\\')}\" /W:1  /R:1 /FFT /MIR /NFL";
+                using (var process = Process.Start(new ProcessStartInfo("robocopy")
+                {
+                    Arguments = arguments,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true
+                }))
+                {
+                    process?.WaitForExit();
+                }
+
+                using (var windowsIdentity = WindowsIdentity.GetCurrent())
+                {
+                    var directorySecurity = new DirectorySecurity();
+                    directorySecurity.SetOwner(windowsIdentity.User);
+                    Directory.SetAccessControl(path, directorySecurity);
+                }
+
+                if (!(Directory.GetFiles(path).Any() || Directory.GetDirectories(path).Any()))
+                {
+                    Directory.Delete(path);
+                }
+            }
+            finally
+            {
+                emptyTempDirectory?.Delete();
+            }
+        }
+    }
+}
