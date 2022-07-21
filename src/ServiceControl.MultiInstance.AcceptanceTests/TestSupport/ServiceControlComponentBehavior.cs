d0dd596096f88e93f80c0f194d6f35d197a06773
Mike Minutillo <mike.minutillo@particular.net>
2022-07-21 11:18:28 +0800

    Enable data store switching for multi-instance tests

--- src/ServiceControl.MultiInstance.AcceptanceTests/TestSupport/ServiceControlComponentBehavior.cs
+++ src/ServiceControl.MultiInstance.AcceptanceTests/TestSupport/ServiceControlComponentBehavior.cs
@@ -12,13 +12,14 @@ namespace ServiceControl.MultiInstance.AcceptanceTests.TestSupport
 
     class ServiceControlComponentBehavior : IComponentBehavior, IAcceptanceTestInfrastructureProviderMultiInstance
     {
-        public ServiceControlComponentBehavior(ITransportIntegration transportToUse, Action<EndpointConfiguration> customEndpointConfiguration, Action<EndpointConfiguration> customAuditEndpointConfiguration, Action<Settings> customServiceControlSettings, Action<Audit.Infrastructure.Settings.Settings> customServiceControlAuditSettings)
+        public ServiceControlComponentBehavior(ITransportIntegration transportToUse, DataStoreConfiguration dataStoreConfiguration, Action<EndpointConfiguration> customEndpointConfiguration, Action<EndpointConfiguration> customAuditEndpointConfiguration, Action<Settings> customServiceControlSettings, Action<Audit.Infrastructure.Settings.Settings> customServiceControlAuditSettings)
         {
             this.customServiceControlAuditSettings = customServiceControlAuditSettings;
             this.customServiceControlSettings = customServiceControlSettings;
             this.customEndpointConfiguration = customEndpointConfiguration;
             this.customAuditEndpointConfiguration = customAuditEndpointConfiguration;
             transportIntegration = transportToUse;
+            this.dataStoreConfiguration = dataStoreConfiguration;
         }
 
         public Dictionary<string, HttpClient> HttpClients => runner.HttpClients;
@@ -27,12 +28,13 @@ namespace ServiceControl.MultiInstance.AcceptanceTests.TestSupport
 
         public async Task<ComponentRunner> CreateRunner(RunDescriptor run)
         {
-            runner = new ServiceControlComponentRunner(transportIntegration, customEndpointConfiguration, customAuditEndpointConfiguration, customServiceControlSettings, customServiceControlAuditSettings);
+            runner = new ServiceControlComponentRunner(transportIntegration, dataStoreConfiguration, customEndpointConfiguration, customAuditEndpointConfiguration, customServiceControlSettings, customServiceControlAuditSettings);
             await runner.Initialize(run).ConfigureAwait(false);
             return runner;
         }
 
         ITransportIntegration transportIntegration;
+        DataStoreConfiguration dataStoreConfiguration;
         Action<EndpointConfiguration> customEndpointConfiguration;
         Action<EndpointConfiguration> customAuditEndpointConfiguration;
         ServiceControlComponentRunner runner;
