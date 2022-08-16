8858ed7b95fc320357165d06c8099c3ef624f682
Mike Minutillo <mike.minutillo@particular.net>
2022-08-16 16:21:42 +0800

    Add test for feature toggles module

---
+++ src/ServiceControl.Config.Tests/FeatureTogglesModulesTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ServiceControl.Config.Tests
+{
+    using Autofac;
+    using Framework.Modules;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FeatureTogglesModulesTests
+    {
+        [Test]
+        public void FeatureTogglePropertiesAreInjected()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new FeatureTogglesModule());
+            builder.RegisterType<FakeClass>();
+
+            var container = builder.Build();
+
+            var featureToggles = container.Resolve<FeatureToggles>();
+            featureToggles.Enable("SomeFeature");
+
+            var injectionTarget = container.Resolve<FakeClass>();
+
+            Assert.IsTrue(injectionTarget.SomeFeatureIsEnabled, "Property with activated feature toggle should be set.");
+            Assert.IsFalse(injectionTarget.SomeUnrelatedFeatureIsEnabled, "Property without activated feature toggle should be ignored.");
+        }
+
+        class FakeClass
+        {
+            [FeatureToggle("SomeFeature")]
+            public bool SomeFeatureIsEnabled { get; set; }
+
+            [FeatureToggle("UnrelatedFeature")]
+            public bool SomeUnrelatedFeatureIsEnabled { get; set; }
+        }
+    }
+}
