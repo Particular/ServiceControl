4a03ee76462ffa77ca1f04239759841bc41c4007
Hadi Eskandari <H.Eskandari@Gmail.com>
2022-08-26 16:10:02 +1000

    remove test that writes settings and test parsing logic (#3003)

--- src/ServiceControl.UnitTests/Infrastructure/Settings/SettingsTests.cs
+++ src/ServiceControl.UnitTests/Infrastructure/Settings/SettingsTests.cs
@@ -8,25 +8,12 @@
     [TestFixture]
     public class SettingsTests
     {
-        [SetUp]
-        public void WriteSetting()
-        {
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            config.AppSettings.Settings.Clear();
-            // Remote instances
-            config.AppSettings.Settings.Add("ServiceControl/RemoteInstances", "[{'api_uri':'http://instance1'},{'api_uri':'http://instance2'}]'");
-            // Various mandatory settings
-            config.AppSettings.Settings.Add("ServiceControl/ForwardErrorMessages", "false");
-            config.AppSettings.Settings.Add("ServiceControl/ErrorRetentionPeriod", 10.ToString());
-            config.Save(ConfigurationSaveMode.Modified);
-            ConfigurationManager.RefreshSection("appSettings");
-        }
-
         [Test]
         public void Should_read_RemoteInstances_from_serialized_json()
         {
-            var settings = new Settings();
-            var remoteInstances = settings.RemoteInstances;
+            var configValue = "[{'api_uri':'http://instance1'},{'api_uri':'http://instance2'}]'";
+            var remoteInstances = Settings.ParseRemoteInstances(configValue);
+
             CollectionAssert.AreEqual(remoteInstances, new[]
             {
                 new RemoteInstanceSetting
