e6a00a43b19233dcf3f2db16bfddadc944e57277
Andreas Öhlund <andreas.ohlund@particular.net>
2022-09-01 13:56:04 +0200

    Setup sql

--- src/ServiceControl.Audit.Persistence.Tests.SqlServer/PersistenceTestsConfiguration.cs
+++ src/ServiceControl.Audit.Persistence.Tests.SqlServer/PersistenceTestsConfiguration.cs
@@ -1,14 +1,30 @@
 ﻿namespace ServiceControl.Audit.Persistence.Tests
 {
+    using System.Data.SqlClient;
     using System.Threading.Tasks;
+    using Dapper;
+    using Microsoft.Extensions.DependencyInjection;
+    using ServiceControl.Audit.Infrastructure.Settings;
 
     partial class PersistenceTestsConfiguration
     {
         public IAuditDataStore AuditDataStore { get; protected set; }
 
-        public Task Configure()
+        public async Task Configure()
         {
-            return Task.CompletedTask;
+            connectionString = SettingsReader<string>.Read("ServiceControl.Audit", "SqlStorageConnectionString", "");
+            await SetupSqlPersistence.SetupAuditTables(connectionString).ConfigureAwait(false);
+
+            var settings = new Settings
+            {
+                SqlStorageConnectionString = connectionString,
+                DataStoreType = DataStoreType.SqlDb
+            };
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(settings);
+            serviceCollection.AddServiceControlAuditPersistence(settings);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            AuditDataStore = serviceProvider.GetRequiredService<IAuditDataStore>();
         }
 
         public Task CompleteDBOperation()
@@ -16,11 +32,21 @@
             return Task.CompletedTask;
         }
 
-        public Task Cleanup()
+        public async Task Cleanup()
         {
-            return Task.CompletedTask;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dropConstraints = "EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'";
+                var dropTables = "EXEC sp_msforeachtable 'DROP TABLE ?'";
+
+                await connection.OpenAsync().ConfigureAwait(false);
+                await connection.ExecuteAsync(dropConstraints).ConfigureAwait(false);
+                await connection.ExecuteAsync(dropTables).ConfigureAwait(false);
+            }
         }
 
         public override string ToString() => "SqlServer";
+
+        string connectionString;
     }
 }
