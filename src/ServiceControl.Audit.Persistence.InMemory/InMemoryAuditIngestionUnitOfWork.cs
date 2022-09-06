e918e26cf9fa95a77e34142edebf545c246bec4a
Andreas Öhlund <andreas.ohlund@particular.net>
2022-09-06 09:56:52 +0200

    Use storage

--- src/ServiceControl.Audit.Persistence.InMemory/InMemoryAuditIngestionUnitOfWork.cs
+++ src/ServiceControl.Audit.Persistence.InMemory/InMemoryAuditIngestionUnitOfWork.cs
@@ -35,6 +35,5 @@
         }
 
         InMemoryAuditDataStore dataStore;
-
     }
 }
--- src/ServiceControl.Audit.Persistence.InMemory/InMemoryFailedAuditStorage.cs
+++ src/ServiceControl.Audit.Persistence.InMemory/InMemoryFailedAuditStorage.cs
@@ -1,34 +1,34 @@
 ﻿namespace ServiceControl.Audit.Persistence.InMemory
 {
     using System;
-    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
     using ServiceControl.Audit.Auditing;
 
     class InMemoryFailedAuditStorage : IFailedAuditStorage
     {
+        public InMemoryFailedAuditStorage(InMemoryAuditDataStore dataStore) => this.dataStore = dataStore;
+
         public async Task ProcessFailedMessages(Func<FailedTransportMessage, Func<CancellationToken, Task>, CancellationToken, Task> onMessage, CancellationToken cancellationToken)
         {
-            foreach (var failedMessage in failedMessages)
-
+            foreach (var failedMessage in dataStore.failedAuditImports)
             {
                 FailedTransportMessage transportMessage = failedMessage.Message;
 
                 await onMessage(transportMessage, (_) => { return Task.CompletedTask; }, cancellationToken).ConfigureAwait(false);
             }
 
-            failedMessages.Clear();
+            dataStore.failedAuditImports.Clear();
         }
 
 
         public Task Store(dynamic failure)
         {
-            failedMessages.Add(failure);
+            dataStore.failedAuditImports.Add(failure);
 
             return Task.CompletedTask;
         }
 
-        List<dynamic> failedMessages = new List<dynamic>();
+        InMemoryAuditDataStore dataStore;
     }
 }
