73285f5fcaddd6235725b36a09810c3ddfaf8465
Andreas Öhlund <andreas.ohlund@particular.net>
2022-09-12 09:52:10 +0200

    Extract classes to separate files

---
+++ src/ServiceControlInstaller.Engine/Configuration/ServiceControl/ServiceControlAuditAppConfig.cs
@@ -0,0 +1,91 @@
+﻿namespace ServiceControlInstaller.Engine.Configuration.ServiceControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Instances;
+
+    public class ServiceControlAuditAppConfig : AppConfig
+    {
+        public ServiceControlAuditAppConfig(IServiceControlAuditInstance instance) : base(Path.Combine(instance.InstallPath, $"{Constants.ServiceControlAuditExe}.config"))
+        {
+            details = instance;
+        }
+
+        protected override void UpdateSettings()
+        {
+            Config.ConnectionStrings.ConnectionStrings.Set("NServiceBus/Transport", details.ConnectionString);
+            var settings = Config.AppSettings.Settings;
+            var version = details.Version;
+            settings.Set(AuditInstanceSettingsList.Port, details.Port.ToString());
+            settings.Set(AuditInstanceSettingsList.DatabaseMaintenancePort, details.DatabaseMaintenancePort.ToString(), version);
+            settings.Set(AuditInstanceSettingsList.HostName, details.HostName);
+            settings.Set(AuditInstanceSettingsList.LogPath, details.LogPath);
+            settings.Set(AuditInstanceSettingsList.DBPath, details.DBPath);
+            settings.Set(AuditInstanceSettingsList.ForwardAuditMessages, details.ForwardAuditMessages.ToString());
+            settings.Set(AuditInstanceSettingsList.TransportType, details.TransportPackage.TypeName, version);
+            settings.Set(AuditInstanceSettingsList.AuditQueue, details.AuditQueue);
+            settings.Set(AuditInstanceSettingsList.AuditLogQueue, details.ForwardAuditMessages ? details.AuditLogQueue : null);
+            settings.Set(AuditInstanceSettingsList.AuditRetentionPeriod, details.AuditRetentionPeriod.ToString(), version);
+            settings.Set(AuditInstanceSettingsList.ServiceControlQueueAddress, details.ServiceControlQueueAddress);
+            settings.Set(AuditInstanceSettingsList.EnableFullTextSearchOnBodies, details.EnableFullTextSearchOnBodies.ToString(), version);
+
+            // Add Settings for performance tuning
+            // See https://github.com/Particular/ServiceControl/issues/655
+            if (!settings.AllKeys.Contains("Raven/Esent/MaxVerPages"))
+            {
+                settings.Add("Raven/Esent/MaxVerPages", "2048");
+            }
+        }
+
+
+        public override void EnableMaintenanceMode()
+        {
+            var settings = Config.AppSettings.Settings;
+            settings.Set(AuditInstanceSettingsList.MaintenanceMode, bool.TrueString, details.Version);
+            Config.Save();
+        }
+
+        public override void DisableMaintenanceMode()
+        {
+            var settings = Config.AppSettings.Settings;
+            settings.Remove(AuditInstanceSettingsList.MaintenanceMode.Name);
+            Config.Save();
+        }
+
+        public override IEnumerable<string> RavenDataPaths()
+        {
+            string[] keys =
+            {
+                "Raven/IndexStoragePath",
+                "Raven/CompiledIndexCacheDirectory",
+                "Raven/Esent/LogsPath",
+                AuditInstanceSettingsList.DBPath.Name
+            };
+
+            var settings = Config.AppSettings.Settings;
+            foreach (var key in keys)
+            {
+                if (!settings.AllKeys.Contains(key, StringComparer.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                var folderpath = settings[key].Value;
+                yield return folderpath.StartsWith("~") //Raven uses ~ to indicate path is relative to bin folder e.g. ~/Data/Logs
+                    ? Path.Combine(details.InstallPath, folderpath.Remove(0, 1))
+                    : folderpath;
+            }
+        }
+
+        public override void SetTransportType(string transportTypeName)
+        {
+            var settings = Config.AppSettings.Settings;
+            var version = details.Version;
+            settings.Set(AuditInstanceSettingsList.TransportType, transportTypeName, version);
+        }
+
+        IServiceControlAuditInstance details;
+    }
+}
---
+++ src/ServiceControlInstaller.Engine/Instances/ServiceControlAuditNewInstance.cs
@@ -0,0 +1,89 @@
+﻿namespace ServiceControlInstaller.Engine.Instances
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Configuration.ServiceControl;
+    using FileSystem;
+    using Queues;
+    using Services;
+    using Validation;
+
+    public class ServiceControlAuditNewInstance : ServiceControlInstallableBase, IServiceControlAuditInstance
+    {
+        public ServiceControlAuditNewInstance()
+        {
+            var appDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var zipInfo = ServiceControlZipInfo.Find(appDirectory);
+            Version = zipInfo.Version;
+        }
+
+        public string ServiceControlQueueAddress { get; set; }
+
+        public override void WriteConfigurationFile()
+        {
+            var appConfig = new ServiceControlAuditAppConfig(this);
+            appConfig.Save();
+        }
+
+        public override string DirectoryName => "ServiceControl.Audit";
+
+        [XmlElement(typeof(XmlTimeSpan))]
+        public TimeSpan AuditRetentionPeriod { get; set; }
+
+        internal override WindowsServiceDetails GetWindowsServiceDetails()
+        {
+            return new WindowsServiceDetails
+            {
+                ServiceAccount = ServiceAccount,
+                ServiceAccountPwd = ServiceAccountPwd,
+                DisplayName = DisplayName,
+                Name = Name,
+                ImagePath = $"\"{Path.Combine(InstallPath, Constants.ServiceControlAuditExe)}\" --serviceName={Name}",
+                ServiceDescription = ServiceDescription
+            };
+        }
+
+        protected override void RunQueueCreation()
+        {
+            QueueCreation.RunQueueCreation(this);
+        }
+
+        protected override void ValidateMaintenancePort()
+        {
+            DatabaseMaintenancePortValidator.Validate(this);
+        }
+
+        protected override void ValidateQueueNames()
+        {
+            QueueNameValidator.Validate(this);
+        }
+
+        protected override void ValidateServiceAccount()
+        {
+            ServiceAccountValidation.Validate(this);
+        }
+
+        protected override void ValidateConnectionString()
+        {
+            ConnectionStringValidator.Validate(this);
+        }
+
+        public static ServiceControlAuditNewInstance Load(string path)
+        {
+            ServiceControlAuditNewInstance instanceData;
+            var serializer = new XmlSerializer(typeof(ServiceControlAuditNewInstance));
+            using (var stream = File.OpenRead(path))
+            {
+                instanceData = (ServiceControlAuditNewInstance)serializer.Deserialize(stream);
+            }
+
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            return instanceData;
+        }
+    }
+}
