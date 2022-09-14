11baf4c75f4d77061d87a6cbe262ac50d940fbf6
Andreas Öhlund <andreas.ohlund@particular.net>
2022-09-14 08:44:25 +0200

    Add failing test

---
+++ src/ServiceControlInstaller.Engine.UnitTests/Configuration/AuditInstanceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceControlInstaller.Engine.UnitTests.Configuration
+{
+    using NUnit.Framework;
+    using ServiceControlInstaller.Engine.Instances;
+
+    [TestFixture]
+    class AuditInstanceTests
+    {
+        [Test]
+        public void Should_default_to_raven35_when_no_config_entry_exists()
+        {
+            var instance = new ServiceControlAuditInstance(null);
+
+            instance.Reload();
+
+            StringAssert.EndsWith("RavenDb", instance.PersistenceType);
+        }
+    }
+}
