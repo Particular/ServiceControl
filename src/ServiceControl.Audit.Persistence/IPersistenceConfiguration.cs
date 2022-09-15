b59e8779a225fa634ab4cf45fd3399d8bf1e2aaf
Mauro Servienti <mauro.servienti@gmail.com>
2022-09-15 15:41:30 +0200

    Move persistence interfaces to the seam project
    Co-Authored-By: Andreas Öhlund <andreas.ohlund@particular.net>

---
+++ src/ServiceControl.Audit.Persistence/IPersistenceConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceControl.Audit.Persistence
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using ServiceControl.Audit.Infrastructure.Settings;
+
+    interface IPersistenceConfiguration
+    {
+        void ConfigureServices(IServiceCollection serviceCollection, Settings settings, bool maintenanceMode, bool isSetup);
+    }
+}
