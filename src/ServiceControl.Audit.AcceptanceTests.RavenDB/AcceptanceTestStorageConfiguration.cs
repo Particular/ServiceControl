34da324a51954a46b23e8950cfddba7e38a44659
Andreas Öhlund <andreas.ohlund@particular.net>
2022-09-19 10:27:59 +0200

    Make atts run

--- src/ServiceControl.Audit.AcceptanceTests.RavenDB/AcceptanceTestStorageConfiguration.cs
+++ src/ServiceControl.Audit.AcceptanceTests.RavenDB/AcceptanceTestStorageConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿namespace ServiceControl.Audit.AcceptanceTests
 {
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.NetworkInformation;
     using System.Threading.Tasks;
     using ServiceControl.Audit.Persistence.RavenDb;
 
@@ -11,6 +13,8 @@
         public void CustomizeSettings(IDictionary<string, string> settings)
         {
             settings["RavenDb/RunInMemory"] = bool.TrueString;
+            settings["RavenDb/MaintenancePort"] = FindAvailablePort(33334).ToString();
+            settings["RavenDb/Hostname"] = "localhost";
         }
 
         public Task Configure()
@@ -18,6 +22,24 @@
             PersistenceType = typeof(RavenDbPersistenceConfiguration).AssemblyQualifiedName;
 
             return Task.CompletedTask;
+        }
+
+        static int FindAvailablePort(int startPort)
+        {
+            var activeTcpListeners = IPGlobalProperties
+                .GetIPGlobalProperties()
+                .GetActiveTcpListeners();
+
+            for (var port = startPort; port < startPort + 1024; port++)
+            {
+                var portCopy = port;
+                if (activeTcpListeners.All(endPoint => endPoint.Port != portCopy))
+                {
+                    return port;
+                }
+            }
+
+            return startPort;
         }
 
         public Task Cleanup() => Task.CompletedTask;
