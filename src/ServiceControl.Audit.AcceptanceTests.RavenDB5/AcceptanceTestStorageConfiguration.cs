748dff07d6431ef7cc1a5e7725f2a58370926410
Mauro Servienti <mauro.servienti@gmail.com>
2022-09-28 15:33:45 +0200

    Provide the required configuration to run ATT

--- src/ServiceControl.Audit.AcceptanceTests.RavenDB5/AcceptanceTestStorageConfiguration.cs
+++ src/ServiceControl.Audit.AcceptanceTests.RavenDB5/AcceptanceTestStorageConfiguration.cs
@@ -1,17 +1,46 @@
 ﻿namespace ServiceControl.Audit.AcceptanceTests
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.NetworkInformation;
     using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Raven.Embedded;
     using ServiceControl.Audit.Persistence.RavenDb;
 
     partial class AcceptanceTestStorageConfiguration
     {
         public string PersistenceType { get; protected set; }
 
-#pragma warning disable IDE0060 // Remove unused parameter
         public void CustomizeSettings(IDictionary<string, string> settings)
-#pragma warning restore IDE0060 // Remove unused parameter
         {
+            var dbPath = Path.Combine(TestContext.CurrentContext.WorkDirectory, "Tests", "AuditData");
+            Console.WriteLine($"DB Path: {dbPath}");
+
+            settings["ServiceControl/Audit/RavenDb5/UseEmbeddedInstance"] = bool.TrueString;
+            settings["ServiceControl.Audit/DbPath"] = dbPath;
+            settings["ServiceControl.Audit/DatabaseMaintenancePort"] = FindAvailablePort(33334).ToString();
+            settings["ServiceControl.Audit/HostName"] = "localhost";
+        }
+
+        static int FindAvailablePort(int startPort)
+        {
+            var activeTcpListeners = IPGlobalProperties
+                .GetIPGlobalProperties()
+                .GetActiveTcpListeners();
+
+            for (var port = startPort; port < startPort + 1024; port++)
+            {
+                var portCopy = port;
+                if (activeTcpListeners.All(endPoint => endPoint.Port != portCopy))
+                {
+                    return port;
+                }
+            }
+
+            return startPort;
         }
 
         public Task Configure()
@@ -21,6 +50,11 @@
             return Task.CompletedTask;
         }
 
-        public Task Cleanup() => Task.CompletedTask;
+        public Task Cleanup()
+        {
+            EmbeddedServer.Instance.Dispose();
+
+            return Task.CompletedTask;
+        }
     }
 }
