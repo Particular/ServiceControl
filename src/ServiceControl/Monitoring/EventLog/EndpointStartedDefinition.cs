06e669735326c40e17241d570791fb384d27a2cb
michaelf <6224270+TraGicCode@users.noreply.github.com>
2022-10-03 18:31:13 -0500

    Add missing periods to events

--- src/ServiceControl/Monitoring/EventLog/EndpointStartedDefinition.cs
+++ src/ServiceControl/Monitoring/EventLog/EndpointStartedDefinition.cs
@@ -7,7 +7,7 @@
     {
         public EndpointStartedDefinition()
         {
-            Description(m => $"Endpoint '{m.EndpointDetails.Name}' started on host {m.EndpointDetails.Host}");
+            Description(m => $"Endpoint '{m.EndpointDetails.Name}' started on host {m.EndpointDetails.Host}.");
 
             RelatesToEndpoint(m => m.EndpointDetails.Name);
             RelatesToHost(m => m.EndpointDetails.HostId);
