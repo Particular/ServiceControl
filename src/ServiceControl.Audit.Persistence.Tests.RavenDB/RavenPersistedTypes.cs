1e53d2a9143f7e9c1ae1dd5a7eec558f1cd484f6
Mauro Servienti <mauro.servienti@gmail.com>
2022-10-07 10:59:41 +0200

    Test that RavenDB 3.5 persistence can load types with not anymore valid full type names

--- src/ServiceControl.Audit.Persistence.Tests.RavenDB/RavenPersistedTypes.cs
+++ src/ServiceControl.Audit.Persistence.Tests.RavenDB/RavenPersistedTypes.cs
@@ -1,12 +1,17 @@
 ﻿namespace ServiceControl.UnitTests
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using NServiceBus;
     using NUnit.Framework;
     using Particular.Approvals;
     using Raven.Abstractions.Data;
+    using Raven.Client.Connection.Async;
     using Raven.Client.Indexes;
+    using ServiceControl.Audit.Auditing;
+    using ServiceControl.Audit.Monitoring;
     using ServiceControl.Audit.Persistence.Infrastructure;
     using ServiceControl.Audit.Persistence.Monitoring;
     using ServiceControl.Audit.Persistence.RavenDb;
@@ -32,42 +37,82 @@
         [Test]
         public async Task CanLoadLegacyTypes()
         {
-            var hostId = Guid.NewGuid();
+            var messageId = Guid.NewGuid().ToString();
             var endpointName = "Sales";
-            var endpointId = KnownEndpoint.MakeDocumentId(endpointName, hostId);
-            var knownEndpoint = new KnownEndpoint()
+            var message = MakeMessage(messageId: messageId, processingEndpoint: endpointName);
+            message.MessageMetadata.Add("ReceivingEndpoint", new EndpointDetails()
             {
-                Id = endpointId,
-                Host = "Sales.Host",
-                HostId = hostId,
-                LastSeen = DateTime.Now,
-                Name = endpointName
-            };
+                Host = "sender@machine",
+                HostId = Guid.NewGuid(),
+                Name = "Sender"
+            });
 
-            using (var session = configuration.DocumentStore.OpenAsyncSession())
-            {
-                await session.StoreAsync(knownEndpoint);
-                await session.SaveChangesAsync();
-            }
+            await IngestProcessedMessagesAudits(message);
 
-            await configuration.CompleteDBOperation();
-
-            _ = await configuration.DocumentStore.AsyncDatabaseCommands.PatchAsync(endpointId, new ScriptedPatchRequest()
+            _ = await configuration.DocumentStore.AsyncDatabaseCommands.PatchAsync(message.Id, new ScriptedPatchRequest()
             {
                 Script =
 @"
-            var metadata = this['@metadata'];
-            metadata['Raven-Clr-Type'] = 'This.Is.All.Wrong.SagaHistory, WrongAssembly'
-            "
+var messageMetadata = this.MessageMetadata;
+messageMetadata['ReceivingEndpoint']['$type'] = 'This.Is.The.Wrong.EndpointDetails, InTheWrongAssembly';
+"
             });
 
             await configuration.CompleteDBOperation();
 
-            var endpoints = await DataStore.QueryKnownEndpoints();
-            var results = endpoints.Results.ToList();
+            var messages = await DataStore.GetMessages(false, new Audit.Infrastructure.PagingInfo(), new Audit.Infrastructure.SortInfo("Id", "asc"));
 
-            Assert.That(results.Count, Is.EqualTo(1));
-            Assert.That(results[0].Id, Is.EqualTo(DeterministicGuid.MakeId(endpointName, hostId.ToString())));
+            Assert.That(messages, Is.Not.Null);
+            Assert.That(messages.Results, Is.Not.Null);
+            Assert.That(messages.Results.Single().Id, Is.EqualTo(message.UniqueMessageId));
+        }
+
+        ProcessedMessage MakeMessage(
+            string messageId = null,
+            MessageIntentEnum intent = MessageIntentEnum.Send,
+            string conversationId = null,
+            string processingEndpoint = null
+        )
+        {
+            messageId = messageId ?? Guid.NewGuid().ToString();
+            conversationId = conversationId ?? Guid.NewGuid().ToString();
+            processingEndpoint = processingEndpoint ?? "SomeEndpoint";
+
+            var metadata = new Dictionary<string, object>
+            {
+                { "MessageId", messageId },
+                { "MessageIntent", intent },
+                { "CriticalTime", TimeSpan.FromSeconds(5) },
+                { "ProcessingTime", TimeSpan.FromSeconds(1) },
+                { "DeliveryTime", TimeSpan.FromSeconds(4) },
+                { "IsSystemMessage", false },
+                { "MessageType", "MyMessageType" },
+                { "IsRetried", false },
+                { "ConversationId", conversationId },
+                //{ "ContentLength", 10}
+            };
+
+            var headers = new Dictionary<string, string>
+            {
+                { Headers.MessageId, messageId },
+                { Headers.ProcessingEndpoint, processingEndpoint },
+                { Headers.MessageIntent, intent.ToString() },
+                { Headers.ConversationId, conversationId }
+            };
+
+
+            return new ProcessedMessage(headers, metadata);
+        }
+
+        async Task IngestProcessedMessagesAudits(params ProcessedMessage[] processedMessages)
+        {
+            var unitOfWork = StartAuditUnitOfWork(processedMessages.Length);
+            foreach (var processedMessage in processedMessages)
+            {
+                await unitOfWork.RecordProcessedMessage(processedMessage);
+            }
+            await unitOfWork.DisposeAsync();
+            await configuration.CompleteDBOperation();
         }
     }
 }
