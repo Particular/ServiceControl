532b6d7389d8e4549c9791073d86dcd8184360d4
Andreas Öhlund <andreas.ohlund@particular.net>
2022-10-11 15:37:24 +0200

    Show host name on heart beat events (#3107)
    * Add missing periods to events
    
    * (GH-2769) Show host name of endpoint associated with heartbeat stops and starts
    
    Closes #2769
    
    Co-authored-by: michaelf <6224270+TraGicCode@users.noreply.github.com>

--- src/ServiceControl/Monitoring/EventLog/HeartbeatingEndpointDetectedDefinition.cs
+++ src/ServiceControl/Monitoring/EventLog/HeartbeatingEndpointDetectedDefinition.cs
@@ -7,7 +7,7 @@
     {
         public HeartbeatingEndpointDetectedDefinition()
         {
-            Description(m => $"Endpoint {m.Endpoint.Name} running on host {m.Endpoint.Host} has been confirmed to have heartbeats enabled");
+            Description(m => $"Endpoint {m.Endpoint.Name} running on host {m.Endpoint.Host} has been confirmed to have heartbeats enabled.");
 
             RelatesToEndpoint(m => m.Endpoint.Host);
             RelatesToHost(m => m.Endpoint.HostId);
