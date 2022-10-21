2f65c49bccbad5cee179312aa9a47818749d6ed9
Andreas Öhlund <andreas.ohlund@particular.net>
2022-10-21 11:30:38 +0200

    Display persister name in SCMU (#3140)

--- src/ServiceControlInstaller.Engine/Instances/PersistenceManifest.cs
+++ src/ServiceControlInstaller.Engine/Instances/PersistenceManifest.cs
@@ -8,6 +8,8 @@
 
         public string Name { get; set; }
 
+        public string DisplayName { get; set; }
+
         public string Description { get; set; }
 
         public string TypeName { get; set; }
