b050907711cfa982645e6db1b0f8da452dace6d1
Brandon Ording <bording@gmail.com>
2022-11-15 13:25:56 -0500

    Remove unused constants (#3205)

--- src/ServiceControlInstaller.Engine/Configuration/ServiceControl/SettingConstants.cs
+++ src/ServiceControlInstaller.Engine/Configuration/ServiceControl/SettingConstants.cs
@@ -3,16 +3,10 @@
     public class SettingConstants
     {
         public const int ErrorRetentionPeriodMaxInDays = 45;
-        public const int ErrorRetentionPeriodMaxInHours = 1080;
-
         public const int ErrorRetentionPeriodMinInDays = 5;
-        public const int ErrorRetentionPeriodMinInHours = 240;
 
         public const int AuditRetentionPeriodMaxInDays = 365;
-        public const int AuditRetentionPeriodMaxInHours = 8760;
-
         public const int AuditRetentionPeriodMinInDays = 1;
-        public const int AuditRetentionPeriodMinInHours = 1;
 
         public const int AuditRetentionPeriodDefaultInDaysForUI = 7;
 
