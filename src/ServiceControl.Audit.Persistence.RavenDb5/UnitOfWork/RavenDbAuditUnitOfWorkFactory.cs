c2f610fec5edc946749d9115a6283165056d9f2c
Szymon Pobiega <szymon.pobiega@gmail.com>
2023-01-10 08:20:34 +0100

    Audit ingestion stopping draft

--- src/ServiceControl.Audit.Persistence.RavenDb5/UnitOfWork/RavenDbAuditUnitOfWorkFactory.cs
+++ src/ServiceControl.Audit.Persistence.RavenDb5/UnitOfWork/RavenDbAuditUnitOfWorkFactory.cs
@@ -1,17 +1,78 @@
 ﻿namespace ServiceControl.Audit.Persistence.RavenDb.UnitOfWork
 {
     using System;
+    using System.IO;
     using Persistence.UnitOfWork;
     using Raven.Client.Documents.BulkInsert;
-    using ServiceControl.Audit.Persistence.RavenDb;
+    using RavenDb;
+    using NServiceBus.Logging;
+    using NServiceBus.CustomChecks;
+    using System.Threading.Tasks;
+
+    class AuditStorageCustomCheck : CustomCheck
+    {
+        public AuditStorageCustomCheck(State stateHolder, DatabaseConfiguration databaseConfiguration)
+            : base("Audit Message Ingestion Process", "ServiceControl Health", TimeSpan.FromSeconds(5))
+        {
+            this.stateHolder = stateHolder;
+            this.databaseConfiguration = databaseConfiguration;
+        }
+
+        public override Task<CheckResult> PerformCheck()
+        {
+            var percentageThreshold = databaseConfiguration.CriticalDataSpaceRemainingThreshold / 100m;
+
+            var dataPathRoot = Path.GetPathRoot(databaseConfiguration.ServerConfiguration.DbPath);
+            if (dataPathRoot == null)
+            {
+                stateHolder.CanIngestMore = true;
+                return successResult;
+            }
+
+            Logger.Debug($"Check ServiceControl data drive space starting. Threshold {percentageThreshold:P0}");
+
+            var dataDriveInfo = new DriveInfo(dataPathRoot);
+            var availableFreeSpace = (decimal)dataDriveInfo.AvailableFreeSpace;
+            var totalSpace = (decimal)dataDriveInfo.TotalSize;
+
+            var percentRemaining = (decimal)dataDriveInfo.AvailableFreeSpace / dataDriveInfo.TotalSize;
+
+            if (Logger.IsDebugEnabled)
+            {
+                Logger.Debug($"Free space: {availableFreeSpace} | Total: {totalSpace} | Percent remaining {percentRemaining:P0}");
+            }
+
+            if (percentRemaining > percentageThreshold)
+            {
+                stateHolder.CanIngestMore = true;
+                return successResult;
+            }
+
+            var message = $"{percentRemaining:P0} disk space remaining on data drive '{dataDriveInfo.VolumeLabel} ({dataDriveInfo.RootDirectory})' on '{Environment.MachineName}'.";
+            stateHolder.CanIngestMore = false;
+            return CheckResult.Failed(message);
+        }
+
+        readonly State stateHolder;
+        private readonly DatabaseConfiguration databaseConfiguration;
+        static Task<CheckResult> successResult = Task.FromResult(CheckResult.Pass);
+        static readonly ILog Logger = LogManager.GetLogger(typeof(AuditStorageCustomCheck));
+
+        public class State
+        {
+            public bool CanIngestMore { get; set; } = true;
+        }
+    }
 
     class RavenDbAuditIngestionUnitOfWorkFactory : IAuditIngestionUnitOfWorkFactory
     {
-        public RavenDbAuditIngestionUnitOfWorkFactory(IRavenDbDocumentStoreProvider documentStoreProvider, IRavenDbSessionProvider sessionProvider, DatabaseConfiguration databaseConfiguration)
+        public RavenDbAuditIngestionUnitOfWorkFactory(IRavenDbDocumentStoreProvider documentStoreProvider, IRavenDbSessionProvider sessionProvider,
+            DatabaseConfiguration databaseConfiguration, AuditStorageCustomCheck.State customCheckState)
         {
             this.documentStoreProvider = documentStoreProvider;
             this.sessionProvider = sessionProvider;
             this.databaseConfiguration = databaseConfiguration;
+            this.customCheckState = customCheckState;
         }
 
         public IAuditIngestionUnitOfWork StartNew(int batchSize)
@@ -25,8 +86,14 @@
             );
         }
 
+        public bool CanIngestMore()
+        {
+            return customCheckState.CanIngestMore;
+        }
+
         readonly IRavenDbDocumentStoreProvider documentStoreProvider;
         readonly IRavenDbSessionProvider sessionProvider;
         readonly DatabaseConfiguration databaseConfiguration;
+        private readonly AuditStorageCustomCheck.State customCheckState;
     }
 }
--- src/ServiceControl.Audit.Persistence/UnitOfWork/IAuditIngestionUnitOfWorkFactory.cs
+++ src/ServiceControl.Audit.Persistence/UnitOfWork/IAuditIngestionUnitOfWorkFactory.cs
@@ -2,6 +2,7 @@
 {
     public interface IAuditIngestionUnitOfWorkFactory
     {
-        IAuditIngestionUnitOfWork StartNew(int batchSize);
+        IAuditIngestionUnitOfWork StartNew(int batchSize); //Throws if not enough space or some other problem preventing from writing data
+        bool CanIngestMore();
     }
 }
