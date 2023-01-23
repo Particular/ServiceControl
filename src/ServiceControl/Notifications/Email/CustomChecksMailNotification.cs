01d16bae560e70c1a3e58746b29c751f8264e5a6
Tomek Masternak <tomasz.masternak@particular.net>
2023-01-23 09:47:38 +0100

    turning off email notifications

--- src/ServiceControl/Notifications/Email/CustomChecksMailNotification.cs
+++ src/ServiceControl/Notifications/Email/CustomChecksMailNotification.cs
@@ -1,16 +1,17 @@
 ﻿namespace ServiceControl.Notifications.Email
 {
-    using System;
-    using System.Linq;
+    //using System;
+    //using System.Linq;
     using System.Threading.Tasks;
     using Contracts.CustomChecks;
     using Infrastructure.DomainEvents;
     using NServiceBus;
-    using NServiceBus.Logging;
+    //using NServiceBus.Logging;
     using ServiceBus.Management.Infrastructure.Settings;
 
     class CustomChecksMailNotification : IDomainHandler<CustomCheckFailed>, IDomainHandler<CustomCheckSucceeded>
     {
+#pragma warning disable IDE0052
         readonly IMessageSession messageSession;
         readonly EmailThrottlingState throttlingState;
         readonly string instanceName;
@@ -27,6 +28,7 @@
             "Audit Message Ingestion",
             "Error Message Ingestion"
         };
+#pragma warning restore IDE0052
 
         public CustomChecksMailNotification(IMessageSession messageSession, Settings settings, EmailThrottlingState throttlingState)
         {
@@ -44,54 +46,54 @@
 
         public Task Handle(CustomCheckFailed domainEvent)
         {
-            if (IsHealthCheck(domainEvent.CustomCheckId))
-            {
-                if (throttlingState.IsThrottling())
-                {
-                    log.Warn("Email notification throttled");
-                    return Task.CompletedTask;
-                }
+            //            if (IsHealthCheck(domainEvent.CustomCheckId))
+            //            {
+            //                if (throttlingState.IsThrottling())
+            //                {
+            //                    log.Warn("Email notification throttled");
+            //                    return Task.CompletedTask;
+            //                }
 
-                return messageSession.SendLocal(new SendEmailNotification
-                {
-                    FailureNotification = true,
-                    Subject = $"[{instanceName}] health check failed",
-                    Body = $@"{domainEvent.Category} check for ServiceControl instance {instanceName} at {instanceAddress}.
+            //                return messageSession.SendLocal(new SendEmailNotification
+            //                {
+            //                    FailureNotification = true,
+            //                    Subject = $"[{instanceName}] health check failed",
+            //                    Body = $@"{domainEvent.Category} check for ServiceControl instance {instanceName} at {instanceAddress}.
 
-{domainEvent.CustomCheckId} failed on {domainEvent.FailedAt}. 
+            //{domainEvent.CustomCheckId} failed on {domainEvent.FailedAt}. 
 
-{domainEvent.FailureReason}"
-                });
-            }
+            //{domainEvent.FailureReason}"
+            //                });
+            //            }
 
             return Task.CompletedTask;
         }
 
         public Task Handle(CustomCheckSucceeded domainEvent)
         {
-            if (IsHealthCheck(domainEvent.CustomCheckId))
-            {
-                if (throttlingState.IsThrottling())
-                {
-                    log.Warn("Email notification throttled");
-                    return Task.CompletedTask;
-                }
+            //            if (IsHealthCheck(domainEvent.CustomCheckId))
+            //            {
+            //                if (throttlingState.IsThrottling())
+            //                {
+            //                    log.Warn("Email notification throttled");
+            //                    return Task.CompletedTask;
+            //                }
 
-                return messageSession.SendLocal(new SendEmailNotification
-                {
-                    FailureNotification = false,
-                    Subject = $"[{instanceName}] health check succeeded",
-                    Body = $@"{domainEvent.Category} check for ServiceControl instance {instanceName} at {instanceAddress}.
+            //                return messageSession.SendLocal(new SendEmailNotification
+            //                {
+            //                    FailureNotification = false,
+            //                    Subject = $"[{instanceName}] health check succeeded",
+            //                    Body = $@"{domainEvent.Category} check for ServiceControl instance {instanceName} at {instanceAddress}.
 
-{domainEvent.CustomCheckId} succeeded on {domainEvent.SucceededAt}."
-                });
-            }
+            //{domainEvent.CustomCheckId} succeeded on {domainEvent.SucceededAt}."
+            //                });
+            //            }
 
             return Task.CompletedTask;
         }
 
-        bool IsHealthCheck(string checkId) => serviceControlHealthCustomCheckIds.Any(id => string.Equals(id, checkId, StringComparison.InvariantCultureIgnoreCase));
+        //bool IsHealthCheck(string checkId) => serviceControlHealthCustomCheckIds.Any(id => string.Equals(id, checkId, StringComparison.InvariantCultureIgnoreCase));
 
-        static ILog log = LogManager.GetLogger<CustomChecksMailNotification>();
+        //static ILog log = LogManager.GetLogger<CustomChecksMailNotification>();
     }
 }
