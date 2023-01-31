a02b0dc79ad144eb436c96ff7195b9be7a2ed1cf
Tomasz Masternak <tomasz.masternak@particular.net>
2023-01-31 07:33:52 +0000

    Maximal size for ProcessingAttempts collection in FailedMessages  (#3372)
    * baseline for att
    
    * failing test
    
    * passing test
    
    * tweak
    
    * fixing the other test
    
    * keep the last n processing attempts also for already existing collections
    
    * Update src/ServiceControl.Persistence.RavenDb/UnitOfWork/RavenDbRecoverabilityIngestionUnitOfWork.cs
    
    Co-authored-by: Szymon Pobiega <szymon.pobiega@gmail.com>
    
    ---------
    
    Co-authored-by: Szymon Pobiega <szymon.pobiega@gmail.com>

---
+++ src/ServiceControl.AcceptanceTests/Recoverability/MessageFailures/When_a_messages_fails_multiple_times.cs
@@ -0,0 +1,123 @@
+﻿namespace ServiceControl.AcceptanceTests.Recoverability
+{
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Routing;
+    using NServiceBus.Transport;
+    using NServiceBus;
+    using NUnit.Framework;
+    using ServiceControl.AcceptanceTesting;
+    using System.Collections.Generic;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using TestSupport.EndpointTemplates;
+    using ServiceControl.Infrastructure;
+    using ServiceControl.MessageFailures;
+
+    class When_a_messages_fails_multiple_times : AcceptanceTest
+    {
+        const int NumberOfFailedAttempts = 20;
+        const int MaximalNumberOfStoredFailedAttempts = 10;
+        const string AttemptIdHeaderKey = "testing.failed_attempt_no";
+
+        [Test]
+        public async Task Should_store_only_the_latest_processing_attempts()
+        {
+            FailedMessage result = null;
+
+            var context = await Define<TestContext>()
+                .WithEndpoint<AnEndpoint>()
+                .Done(async c =>
+                {
+                    if (string.IsNullOrWhiteSpace(c.UniqueMessageId))
+                    {
+                        return false;
+                    }
+
+                    result = await this.TryGet<FailedMessage>($"/api/errors/{c.UniqueMessageId}");
+
+                    var failureTimes = result?.ProcessingAttempts.Select(pa => pa.Headers["n_service_bus.time_of_failure"]).ToArray();
+
+                    return result != null && AreSame(failureTimes, c.LatestFailureTimes.ToArray());
+
+                    bool AreSame(string[] a, string[] b)
+                    {
+                        if (a.Length != b.Length)
+                        {
+                            return false;
+                        }
+
+                        for (var i = 0; i < a.Length; i++)
+                        {
+                            if (a[i] != b[i])
+                            {
+                                return false;
+                            }
+                        }
+
+                        return true;
+                    }
+                })
+                .Run();
+        }
+
+        class TestContext : ScenarioContext
+        {
+            public string UniqueMessageId { get; set; }
+            public List<string> LatestFailureTimes { get; set; } = new List<string>();
+        }
+
+        class AnEndpoint : EndpointConfigurationBuilder
+        {
+            public AnEndpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            class FailedMessagesSender : DispatchRawMessages<TestContext>
+            {
+                protected override TransportOperations CreateMessage(TestContext context)
+                {
+                    var endpointName = NServiceBus.AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(AnEndpoint));
+                    var messageId = Guid.NewGuid().ToString();
+                    var earliestTimeOfFailure = DateTime.UtcNow;
+
+                    context.UniqueMessageId = DeterministicGuid.MakeId(messageId, endpointName).ToString();
+
+                    var transportOperations = Enumerable.Range(0, NumberOfFailedAttempts)
+                        .Select(i =>
+                        {
+                            var timeOfFailure = DateTimeExtensions.ToWireFormattedString(earliestTimeOfFailure.Add(TimeSpan.FromMinutes(i)));
+
+                            var headers = new Dictionary<string, string>
+                            {
+                                [Headers.MessageId] = messageId,
+                                [Headers.EnclosedMessageTypes] = typeof(MyMessage).FullName,
+                                ["NServiceBus.FailedQ"] = endpointName,
+                                ["$.diagnostics.hostid"] = Guid.NewGuid().ToString(),
+                                ["NServiceBus.TimeOfFailure"] = timeOfFailure,
+
+                                [AttemptIdHeaderKey] = (i + 1).ToString()
+                            };
+
+                            context.LatestFailureTimes.Add(timeOfFailure);
+
+                            return new TransportOperation(new OutgoingMessage(messageId, headers, Array.Empty<byte>()), new UnicastAddressTag("error"));
+                        })
+                        .ToArray();
+
+                    context.LatestFailureTimes = context.LatestFailureTimes
+                        .Skip(context.LatestFailureTimes.Count - MaximalNumberOfStoredFailedAttempts)
+                        .Take(MaximalNumberOfStoredFailedAttempts)
+                        .ToList();
+
+                    return new TransportOperations(transportOperations);
+                }
+            }
+
+            class MyMessage : ICommand
+            {
+            }
+        }
+    }
+}
--- src/ServiceControl.Persistence.RavenDb/UnitOfWork/RavenDbRecoverabilityIngestionUnitOfWork.cs
+++ src/ServiceControl.Persistence.RavenDb/UnitOfWork/RavenDbRecoverabilityIngestionUnitOfWork.cs
@@ -57,6 +57,8 @@
             var serializedGroups = RavenJToken.FromObject(groups);
             var serializedAttempt = RavenJToken.FromObject(processingAttempt, Serializer);
 
+            //HINT: RavenDB 3.5 is using Lodash v4.13.1 to provide javascript utility functions
+            //      https://ravendb.net/docs/article-page/3.5/csharp/client-api/commands/patches/how-to-use-javascript-to-patch-your-documents#methods-objects-and-variables
             return new ScriptedPatchCommandData
             {
                 Key = documentId,
@@ -64,14 +66,32 @@
                 {
                     Script = $@"this.{nameof(FailedMessage.Status)} = status;
                                 this.{nameof(FailedMessage.FailureGroups)} = failureGroups;
+                                
+                                var newAttempts = this.{nameof(FailedMessage.ProcessingAttempts)};
+
+                                //De-duplicate attempts by AttemptedAt value
 
                                 var duplicateIndex = _.findIndex(this.{nameof(FailedMessage.ProcessingAttempts)}, function(a){{
                                     return a.{nameof(FailedMessage.ProcessingAttempt.AttemptedAt)} === attempt.{nameof(FailedMessage.ProcessingAttempt.AttemptedAt)};
                                 }});
 
                                 if(duplicateIndex === -1){{
-                                    this.{nameof(FailedMessage.ProcessingAttempts)} = _.union(this.{nameof(FailedMessage.ProcessingAttempts)}, [attempt]);
-                                }}",
+                                    newAttempts = _.union(newAttempts, [attempt]);
+                                }}
+
+                                //Trim to the latest MaxProcessingAttempts 
+                                
+                                newAttempts = _.sortBy(newAttempts, function(a) {{
+                                    return a.{nameof(FailedMessage.ProcessingAttempt.AttemptedAt)};
+                                }});
+                                
+                                if(newAttempts.length > {MaxProcessingAttempts})
+                                {{
+                                    newAttempts = _.slice(newAttempts, newAttempts.length - {MaxProcessingAttempts}, newAttempts.length); 
+                                }}
+
+                                this.{nameof(FailedMessage.ProcessingAttempts)} = newAttempts;
+                                ",
                     Values = new Dictionary<string, object>
                     {
                         {"status", (int)FailedMessageStatus.Unresolved},
@@ -110,6 +130,7 @@
                                     }}");
         }
 
+        static int MaxProcessingAttempts = 10;
         static readonly RavenJObject FailedMessageMetadata;
         static readonly JsonSerializer Serializer;
     }
