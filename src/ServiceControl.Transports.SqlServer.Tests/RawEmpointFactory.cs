c11f14d77bb16b96a20e4f9ec7a9526da773eeee
cquirosj <christian.quiros@particular.net>
2023-02-07 01:19:48 -0600

    SqlT InitializeQueueLengthProvider setup complete

---
+++ src/ServiceControl.Transports.SqlServer.Tests/RawEmpointFactory.cs
@@ -0,0 +1,38 @@
+﻿namespace ServiceControl.Transports.SqlServer.Tests
+{
+    using NServiceBus.Raw;
+    using NServiceBus.Transport;
+    using System.Threading.Tasks;
+    using System;
+
+    class RawEndpointFactory
+    {
+        public RawEndpointFactory(TransportSettings transportSettings, TransportCustomization transportCustomization)
+        {
+            this.transportSettings = transportSettings;
+            //this.settings = settings;
+            this.transportCustomization = transportCustomization;
+        }
+
+        public RawEndpointConfiguration CreateAuditIngestor(string name, Func<MessageContext, IDispatchMessages, Task> onMessage)
+        {
+            var config = RawEndpointConfiguration.Create(name, onMessage, $"{transportSettings.EndpointName}.Errors");
+            //config.LimitMessageProcessingConcurrencyTo(0);
+
+            transportCustomization.CustomizeForAuditIngestion(config, transportSettings);
+            return config;
+        }
+
+        public RawEndpointConfiguration CreateFailedAuditsSender(string name)
+        {
+            var config = RawEndpointConfiguration.CreateSendOnly(name);
+
+            transportCustomization.CustomizeRawSendOnlyEndpoint(config, transportSettings);
+            return config;
+        }
+
+        //Settings.Settings settings;
+        TransportCustomization transportCustomization;
+        TransportSettings transportSettings;
+    }
+}
---
+++ src/ServiceControl.Transports.SqlServer.Tests/TransportTestsConfiguration.cs
@@ -0,0 +1,59 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Raw;
+    using ServiceControl.Transports.SqlServer.Tests;
+    using Transports;
+    using Transports.SqlServer;
+
+    partial class TransportTestsConfiguration
+    {
+        public IProvideQueueLength InitializeQueueLengthProvider(string queueName, Action<QueueLengthEntry> onQueueLengthReported)
+        {
+            var tempRandomDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempRandomDirectory);
+
+            var dbInstanceForSqlT = SqlLocalDb.CreateNewIn(tempRandomDirectory);
+
+            var transportSettings = new TransportSettings
+            {
+                EndpointName = queueName,
+                ConnectionString = dbInstanceForSqlT.ConnectionString,
+                MaxConcurrency = 1
+            };
+
+            var factory = new RawEndpointFactory(transportSettings, customizations);
+
+            var config = factory.CreateAuditIngestor(queueName, (context, dispatcher) => Task.CompletedTask);
+
+            config.AutoCreateQueues(new string[] { $"{queueName}.Errors" });
+            //No need to start the raw endpoint to create queues
+            RawEndpoint.Create(config).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var endpointConfiguration = new EndpointConfiguration(queueName);
+
+            endpointConfiguration.EnableInstallers();
+            customizations.CustomizeForMonitoringIngestion(endpointConfiguration, transportSettings);
+            var queueLengthProvider = customizations.CreateQueueLengthProvider();
+
+            queueLengthProvider.Initialize(dbInstanceForSqlT.ConnectionString, (qle, _) => onQueueLengthReported(qle.First()));
+
+            return queueLengthProvider;
+        }
+
+        public Task Cleanup() => Task.CompletedTask;
+
+        public Task Configure()
+        {
+            customizations = new SqlServerTransportCustomization();
+
+            return Task.CompletedTask;
+        }
+
+        SqlServerTransportCustomization customizations;
+    }
+}
