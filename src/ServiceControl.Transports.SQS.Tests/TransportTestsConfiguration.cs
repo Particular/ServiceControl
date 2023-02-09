667b7b3b3416a17039cc630860fa9457f39bd3ee
cquirosj <christian.quiros@particular.net>
2023-02-08 22:45:38 -0600

    Add SQS transport test

---
+++ src/ServiceControl.Transports.SQS.Tests/TransportTestsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Raw;
+    using Transports;
+    using Transports.SQS;
+
+    partial class TransportTestsConfiguration
+    {
+        public IProvideQueueLength InitializeQueueLengthProvider(Action<QueueLengthEntry[], EndpointToQueueMapping> store)
+        {
+            var queueLengthProvider = customizations.CreateQueueLengthProvider();
+
+            queueLengthProvider.Initialize(connectionString, store);
+
+            return queueLengthProvider;
+        }
+
+        public Task Cleanup() => Task.CompletedTask;
+
+        public Task Configure()
+        {
+            customizations = new SQSTransportCustomization();
+            connectionString = Environment.GetEnvironmentVariable("ServiceControl.TransportTests.SQS.ConnectionString");
+
+            return Task.CompletedTask;
+        }
+
+        public void ApplyTransportConfig(RawEndpointConfiguration c)
+        {
+            c.UseTransport<SqsTransport>()
+                .ConnectionString(connectionString);
+        }
+
+        string connectionString;
+        SQSTransportCustomization customizations;
+    }
+}
