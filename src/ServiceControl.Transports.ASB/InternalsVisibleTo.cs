e004ece2820475a99f5fbd6ea2cabd41452b27b9
cquirosj <christian.quiros@particular.net>
2023-02-09 09:46:13 -0600

    Add ASBEndpointTopology and ASBForwardingTopology

--- src/ServiceControl.Transports.ASB/InternalsVisibleTo.cs
+++ src/ServiceControl.Transports.ASB/InternalsVisibleTo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ServiceControl.Transports.UnitTests")]
+[assembly: InternalsVisibleTo("ServiceControl.Transports.ASBEndpointTopology.Tests")]
---
+++ src/ServiceControl.Transports.UnitTests/TransportTestsConfiguration.cs
@@ -0,0 +1,44 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Raw;
+    using Transports;
+    using Transports.ASB;
+
+    partial class TransportTestsConfiguration
+    {
+        public IProvideQueueLength InitializeQueueLengthProvider(Action<QueueLengthEntry[], EndpointToQueueMapping> store)
+        {
+            var queueLengthProvider = customizations.CreateQueueLengthProvider();
+
+            queueLengthProvider.Initialize(connectionString, store);
+
+            return queueLengthProvider;
+        }
+
+        public Task Cleanup() => Task.CompletedTask;
+
+        public Task Configure()
+        {
+            customizations = new ASBEndpointTopologyTransportCustomization();
+            connectionString = Environment.GetEnvironmentVariable("ServiceControl.TransportTests.ASBS.ConnectionString");
+
+            return Task.CompletedTask;
+        }
+
+        public void ApplyTransportConfig(RawEndpointConfiguration c)
+        {
+#pragma warning disable CS0618
+            var transport = c.UseTransport<AzureServiceBusTransport>()
+#pragma warning restore CS0618
+                .ConnectionString(connectionString);
+
+            transport.UseEndpointOrientedTopology();
+        }
+
+        string connectionString;
+        ASBEndpointTopologyTransportCustomization customizations;
+    }
+}
