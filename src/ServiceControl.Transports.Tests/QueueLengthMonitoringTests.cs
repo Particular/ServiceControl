7cc801f2455a3b3b93d27e78be5ea917b0d7cc44
cquirosj <christian.quiros@particular.net>
2023-02-08 23:44:17 -0600

    marke RabbitMQ tests work

--- src/ServiceControl.Transports.Tests/QueueLengthMonitoringTests.cs
+++ src/ServiceControl.Transports.Tests/QueueLengthMonitoringTests.cs
@@ -16,7 +16,7 @@
         [Test]
         public async Task Should_report_queue_length()
         {
-            var queueName = $"testqueue-{TestContext.CurrentContext.Test.ID}";
+            var queueName = $"testqueue-{System.IO.Path.GetRandomFileName().Replace(".", string.Empty)}";
 
             var onQueueLengthEntryReceived = CreateTaskCompletionSource<QueueLengthEntry>();
 
