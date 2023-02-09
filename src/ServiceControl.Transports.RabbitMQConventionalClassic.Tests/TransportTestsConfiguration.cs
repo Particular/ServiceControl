f54311b8b46c31a37ff75bd2d3221501ae75e91d
cquirosj <christian.quiros@particular.net>
2023-02-08 21:50:43 -0600

    Add RabbitMQ conventional routing classic queues tests

---
+++ src/ServiceControl.Transports.RabbitMQConventionalClassic.Tests/TransportTestsConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Raw;
+    using ServiceControl.Transports.RabbitMQ;
+    using Transports;
+
+    partial class TransportTestsConfiguration
+    {
+        public IProvideQueueLength InitializeQueueLengthProvider(Action<QueueLengthEntry[], EndpointToQueueMapping> store)
+        {
+            var queueLengthProvider = customizations.CreateQueueLengthProvider();
+
+            queueLengthProvider.Initialize(connectionString, store);
+
+            return queueLengthProvider;
+        }
+
+        public Task Cleanup() => Task.CompletedTask;
+
+        public Task Configure()
+        {
+            customizations = new RabbitMQQuorumConventionalRoutingTransportCustomization();
+            connectionString = Environment.GetEnvironmentVariable("ServiceControl.TransportTests.RabbitMQ.ConnectionString");
+
+            return Task.CompletedTask;
+        }
+
+        public void ApplyTransportConfig(RawEndpointConfiguration c)
+        {
+            c.UseTransport<RabbitMQTransport>()
+                .UseConventionalRoutingTopology(QueueType.Classic)
+                .ConnectionString(connectionString);
+        }
+
+        string connectionString;
+        RabbitMQQuorumConventionalRoutingTransportCustomization customizations;
+    }
+}
