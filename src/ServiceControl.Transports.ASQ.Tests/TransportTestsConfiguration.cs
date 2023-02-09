4e5ae5b4ad2ce5f2a989fb0a73f46d0806d38738
cquirosj <christian.quiros@particular.net>
2023-02-08 22:34:43 -0600

    Add Azure Storage Queues transport test

---
+++ src/ServiceControl.Transports.ASQ.Tests/TransportTestsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Raw;
+    using Transports;
+    using Transports.ASQ;
+
+    partial class TransportTestsConfiguration
+    {
+        public IProvideQueueLength InitializeQueueLengthProvider(Action<QueueLengthEntry[], EndpointToQueueMapping> store)
+        {
+            var queueLengthProvider = customizations.CreateQueueLengthProvider();
+
+            queueLengthProvider.Initialize(connectionString, store);
+
+            return queueLengthProvider;
+        }
+
+        public Task Cleanup() => Task.CompletedTask;
+
+        public Task Configure()
+        {
+            customizations = new ASQTransportCustomization();
+            connectionString = Environment.GetEnvironmentVariable("ServiceControl.TransportTests.ASQ.ConnectionString");
+
+            return Task.CompletedTask;
+        }
+
+        public void ApplyTransportConfig(RawEndpointConfiguration c)
+        {
+            c.UseTransport<AzureStorageQueueTransport>()
+                .ConnectionString(connectionString);
+        }
+
+        string connectionString;
+        ASQTransportCustomization customizations;
+    }
+}
