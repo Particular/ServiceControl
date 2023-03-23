a24f176387dd7894244d672bc4fb556e56754b3b
Adam Wright <awright18@gmail.com>
2023-03-23 07:55:14 -0500

    Add FooView for testing

---
+++ src/ServiceControl.Config/UI/InstanceAdd/FooView.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ServiceControl.Config.UI.InstanceAdd
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for FooView.xaml
+    /// </summary>
+    public partial class FooView : UserControl
+    {
+        public FooView()
+        {
+            InitializeComponent();
+        }
+    }
+}
