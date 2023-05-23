96e0cfb1617daa4977b73800571837a2b0e3211a
Mauro Servienti <mauro.servienti@gmail.com>
2023-05-23 09:43:27 +0200

    Test that transports can be loaded from root (production env layout)

---
+++ src/ServiceControl.Audit.UnitTests/TransportManifestCanLoadFromRoot.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Audit.UnitTests
+{
+    using NUnit.Framework;
+    using ServiceControl.Transports;
+
+    [TestFixture]
+    public class TransportManifestCanLoadFromRoot
+    {
+        const string transportName = "learningTransport";
+        const string transportType = "ServiceControl.Transports.Learning.LearningTransportCustomization, ServiceControl.Transports.Learning";
+        //const string transportAlias = "ServiceControl.Transports.LegacyAzureServiceBus.EndpointOrientedTopologyAzureServiceBusTransport, ServiceControl.Transports.LegacyAzureServiceBus";
+        //const string transportFolderName = "AzureServiceBus";
+
+        [Test]
+        public void Should_find_transport_type_by_name()
+        {
+            var _transportType = TransportManifestLibrary.Find(transportName);
+
+            Assert.AreEqual(transportType, _transportType);
+        }
+    }
+}
