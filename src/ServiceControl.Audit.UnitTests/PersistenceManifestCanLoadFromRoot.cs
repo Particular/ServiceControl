db995b9f1457439a97e8f339f63eb4e4dad8e150
Jo Palac <j_palac@hotmail.com>
2023-05-24 12:39:09 +1000

    Added persistence test for loading from root

---
+++ src/ServiceControl.Audit.UnitTests/PersistenceManifestCanLoadFromRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceControl.Audit.UnitTests
+{
+    using NUnit.Framework;
+    using ServiceControl.Audit.Persistence;
+
+    [TestFixture]
+    public class PersistenceManifestCanLoadFromRoot
+    {
+        const string persistenceName = "RavenDB35";
+        const string persistenceType = "ServiceControl.Audit.Persistence.RavenDb.RavenDbPersistenceConfiguration, ServiceControl.Audit.Persistence.RavenDb";
+
+        [Test]
+        public void Should_find_persistence_type_by_name()
+        {
+            var _persistenceType = PersistenceManifestLibrary.Find(persistenceName);
+
+            Assert.AreEqual(persistenceType, _persistenceType);
+        }
+    }
+}
