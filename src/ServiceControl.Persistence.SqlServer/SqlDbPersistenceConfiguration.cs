e0c712e81ec1012694226f853a50f4a2f97ca5b7
Mauro Servienti <mauro.servienti@gmail.com>
2023-05-26 09:39:45 +0200

    Lift settings readers into a separate project

--- src/ServiceControl.Persistence.SqlServer/SqlDbPersistenceConfiguration.cs
+++ src/ServiceControl.Persistence.SqlServer/SqlDbPersistenceConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿namespace ServiceControl.Persistence.SqlServer
 {
     using Microsoft.Extensions.DependencyInjection;
+    using ServiceBus.Management.Infrastructure.Settings;
     using ServiceControl.Persistence.UnitOfWork;
 
     public class SqlDbPersistenceConfiguration : IPersistenceConfiguration
@@ -8,7 +9,7 @@
         public void ConfigureServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton(sp =>
-                new SqlDbConnectionManager(sp.GetRequiredService<Settings>().SqlStorageConnectionString));
+                new SqlDbConnectionManager(SettingsReader<string>.Read("SqlStorageConnectionString")));
             serviceCollection.AddSingleton<IMonitoringDataStore, SqlDbMonitoringDataStore>();
             serviceCollection.AddSingleton<ICustomChecksDataStore, SqlDbCustomCheckDataStore>();
             serviceCollection.AddPartialUnitOfWorkFactory<SqlIngestionUnitOfWorkFactory>();
