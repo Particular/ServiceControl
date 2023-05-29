8f188d639da3494f8c3ff6a97c1fde2df646683b
Jo Palac <j_palac@hotmail.com>
2023-05-29 13:56:40 +1000

    Added binding for moved ravendb types

--- src/ServiceControl/Infrastructure/RavenDB/MigratedTypeAwareBinder.cs
+++ src/ServiceControl/Infrastructure/RavenDB/MigratedTypeAwareBinder.cs
@@ -1,8 +1,17 @@
 ﻿namespace ServiceControl.Infrastructure.RavenDB
 {
     using System;
+    using System.Linq;
     using Raven.Imports.Newtonsoft.Json.Serialization;
     using SagaAudit;
+    using ServiceControl.Contracts.CustomChecks;
+    using ServiceControl.Contracts.Operations;
+    using ServiceControl.MessageAuditing;
+    using ServiceControl.MessageFailures;
+    using ServiceControl.Operations;
+    using ServiceControl.Persistence;
+    using ServiceControl.Recoverability;
+    using static ServiceControl.MessageFailures.FailedMessage;
 
     class MigratedTypeAwareBinder : DefaultSerializationBinder
     {
@@ -13,7 +22,45 @@
                 return typeof(SagaInfo);
             }
 
-            return base.BindToType(assemblyName, typeName);
+            var className = GetClassName(typeName);
+            switch (className)
+            {
+                case nameof(CustomCheck):
+                    return typeof(CustomCheck);
+                case nameof(CustomCheckDetail):
+                    return typeof(CustomCheckDetail);
+                case nameof(EndpointDetails):
+                    return typeof(EndpointDetails);
+                case nameof(ExceptionDetails):
+                    return typeof(ExceptionDetails);
+                case nameof(FailedMessage):
+                    return typeof(FailedMessage);
+                case nameof(FailureDetails):
+                    return typeof(FailureDetails);
+                case nameof(FailureGroup):
+                    return typeof(FailureGroup);
+                case nameof(GroupComment):
+                    return typeof(GroupComment);
+                case nameof(KnownEndpoint):
+                    return typeof(KnownEndpoint);
+                case nameof(ProcessedMessage):
+                    return typeof(ProcessedMessage);
+                case nameof(FailedMessageRetry):
+                    return typeof(FailedMessageRetry);
+                case nameof(RetryBatch):
+                    return typeof(RetryBatch);
+                case nameof(RetryBatchNowForwarding):
+                    return typeof(RetryBatchNowForwarding);
+                case nameof(SagaInfo):
+                    return typeof(SagaInfo);
+                default:
+                    return base.BindToType(assemblyName, typeName);
+            }
+        }
+
+        string GetClassName(string typeName)
+        {
+            return typeName.Split('.').Last();
         }
     }
 }
