b5b1cfb4c9e46128f0bab2b3a07d397b15989ff7
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-06-14 20:30:30 -0700

    use string interpolation and built in mechanism to generate ids

--- src/ServiceControl/Recoverability/Editing/EditHandler.cs
+++ src/ServiceControl/Recoverability/Editing/EditHandler.cs
@@ -58,7 +58,7 @@
                 }
                 else if (edit.EditId != context.MessageId)
                 {
-                    log.WarnFormat("Discarding edit & retry request because the failure (FailedMessages/{1}) has already been edited edited in FailedMessageEdit/{2}", message.FailedMessageId, edit.EditId);
+                    log.WarnFormat($"Discarding edit & retry request because the failure ({FailedMessage.MakeDocumentId(message.FailedMessageId)}) has already been edited by {FailedMessageEdit.MakeDocumentId(message.FailedMessageId)}");
                     return;
                 }
 
