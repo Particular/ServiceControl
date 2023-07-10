ce95691b32a4ae09c66f41d62b8f4cd746d2cf4e
David Boike <david.boike@gmail.com>
2023-07-10 11:27:35 -0500

    RavenBootstrapper

--- src/ServiceControl.Persistence.RavenDb/Infrastructure/Migrations/IDataMigration.cs
+++ src/ServiceControl.Persistence.RavenDb/Infrastructure/Migrations/IDataMigration.cs
@@ -3,7 +3,7 @@
     using System.Threading.Tasks;
     using Raven.Client;
 
-    public interface IDataMigration
+    interface IDataMigration
     {
         Task Migrate(IDocumentStore store);
     }
--- src/ServiceControl.Persistence.RavenDb/RavenDbInstaller.cs
+++ src/ServiceControl.Persistence.RavenDb/RavenDbInstaller.cs
@@ -2,6 +2,7 @@
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using NServiceBus.Logging;
     using Raven.Client.Embedded;
     using Raven.Client.Indexes;
 
@@ -25,11 +26,19 @@
                 .ConfigureAwait(false);
             Logger.Info("Index creation complete");
 
-            Logger.Info("Data migrations starting");
+            Logger.Info("Data migrations starting **TODO NOT IMPLEMENTED YET**");
 
-            var endpointMigrations = new MigrateKnownEndpoints(documentStore);
-            await endpointMigrations.Migrate(cancellationToken: cancellationToken)
-                .ConfigureAwait(false);
+            // TODO: Figure out migrations
+            // This was copied from audit code:
+            ////var endpointMigrations = new MigrateKnownEndpoints(documentStore);
+            ////await endpointMigrations.Migrate(cancellationToken: cancellationToken)
+            ////    .ConfigureAwait(false);
+            // While this was copied from EmbeddedRavenDbHostedService in main ServiceControl project, but list was injected
+            ////foreach (var migration in dataMigrations)
+            ////{
+            ////    await migration.Migrate(documentStore)
+            ////        .ConfigureAwait(false);
+            ////}
 
             Logger.Info("Data migrations complete");
 
