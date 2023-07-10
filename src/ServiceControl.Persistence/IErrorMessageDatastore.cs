cce0741a75e96cc4739ee6db8a477deb90dda46a
Tomek Masternak <tomasz.masternak@particular.net>
2023-07-10 14:53:36 +0200

    more apis

--- src/ServiceControl.Persistence/IErrorMessageDatastore.cs
+++ src/ServiceControl.Persistence/IErrorMessageDatastore.cs
@@ -8,5 +8,7 @@
     public interface IErrorMessageDataStore
     {
         Task<QueryResult<IList<MessagesView>>> GetAllMessages(PagingInfo pagingInfo, SortInfo sortInfo, bool includeSystemMessages);
+        Task<QueryResult<IList<MessagesView>>> GetAllMessagesForEndpoint(string endpointName, PagingInfo pagingInfo, SortInfo sortInfo, bool includeSystemMessages);
+        Task<QueryResult<IList<MessagesView>>> GetAllMessagesByConversation(string conversationId, PagingInfo pagingInfo, SortInfo sortInfo, bool includeSystemMessages);
     }
 }
