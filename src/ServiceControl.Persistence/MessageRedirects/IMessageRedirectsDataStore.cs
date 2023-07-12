6ea27b8913e0bba95781bbc48039036ab2a15f04
David Boike <david.boike@gmail.com>
2023-07-12 13:30:40 -0500

    MessageRedirects

---
+++ src/ServiceControl.Persistence/MessageRedirects/IMessageRedirectsDataStore.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceControl.Persistence.MessageRedirects
+{
+    using System.Threading.Tasks;
+
+    public interface IMessageRedirectsDataStore
+    {
+        Task<MessageRedirectsCollection> GetOrCreate();
+        Task Save(MessageRedirectsCollection redirects);
+    }
+}
---
+++ src/ServiceControl.Persistence/MessageRedirects/MessageRedirectsCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceControl.Persistence.MessageRedirects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "Legacy stored data")]
+    public class MessageRedirectsCollection
+    {
+        public string ETag { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public MessageRedirect this[string from] => Redirects.SingleOrDefault(r => r.FromPhysicalAddress == from);
+        public MessageRedirect this[Guid id] => Redirects.SingleOrDefault(r => r.MessageRedirectId == id);
+
+        public List<MessageRedirect> Redirects { get; set; } = new List<MessageRedirect>();
+    }
+}
