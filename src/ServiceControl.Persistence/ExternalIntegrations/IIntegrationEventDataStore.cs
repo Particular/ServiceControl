50424803c05970b991c261da5a6881c34800c774
Ramon Smits <ramon.smits@gmail.com>
2023-07-12 11:56:34 +0200

    class IntegrationEventWriter : IDomainHandler<IDomainEvent>

---
+++ src/ServiceControl.Persistence/ExternalIntegrations/IIntegrationEventDataStore.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceControl.Persistence
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using ServiceControl.ExternalIntegrations;
+
+    public interface IIntegrationEventDataStore
+    {
+        Task StoreDispatchRequest(IEnumerable<ExternalIntegrationDispatchRequest> dispatchRequests);
+    }
+}
+
