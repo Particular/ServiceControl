d6b3811b09d4fef52ac21180cb273ce1dad25c96
Ramon Smits <ramon.smits@gmail.com>
2023-07-13 17:04:47 +0200

    Added TODO for observation

--- src/ServiceControl.Persistence/IPersistenceLifecycle.cs
+++ src/ServiceControl.Persistence/IPersistenceLifecycle.cs
@@ -3,7 +3,7 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IPersistenceLifecycle
+    public interface IPersistenceLifecycle // TODO: We don't need this, implementations should just implement IHostedService
     {
         Task Start(CancellationToken cancellationToken = default);
         Task Stop(CancellationToken cancellationToken = default);
