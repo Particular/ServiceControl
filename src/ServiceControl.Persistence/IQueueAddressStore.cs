5ef1027e9a52a3279dead0fa81f26c5143e3e8f1
Ramon Smits <ramon.smits@gmail.com>
2023-07-13 13:00:45 +0200

    🚧 QueueAddressController

---
+++ src/ServiceControl.Persistence/IQueueAddressStore.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Persistence
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using ServiceControl.MessageFailures;
+    using ServiceControl.Persistence.Infrastructure;
+
+    public interface IQueueAddressStore
+    {
+        Task<QueryResult<IList<QueueAddress>>> GetAddresses(PagingInfo pagingInfo);
+        Task<QueryResult<IList<QueueAddress>>> GetAddressesBySearchTerm(string search, PagingInfo pagingInfo);
+    }
+}
