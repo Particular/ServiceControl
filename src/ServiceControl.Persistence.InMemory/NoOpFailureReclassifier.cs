b2ceb07da3d9c410ca0b5eee7db98c105142a18a
David Boike <david.boike@gmail.com>
2023-07-13 12:40:03 -0500

    Move failed messaged reclassification to Raven35 project wholesale, other persisters shouldn't (hopefully) need a real implementation

---
+++ src/ServiceControl.Persistence.InMemory/NoOpFailureReclassifier.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceControl.Persistence.InMemory
+{
+    using System.Threading.Tasks;
+
+    class NoOpFailureReclassifier : IReclassifyFailedMessages
+    {
+        public Task<int> ReclassifyFailedMessages(bool force) => Task.FromResult(0);
+    }
+}
