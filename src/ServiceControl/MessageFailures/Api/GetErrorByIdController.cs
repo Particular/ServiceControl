c13815e7ed2425396049492040bda5075c58ba6f
Ramon Smits <ramon.smits@gmail.com>
2023-07-13 11:36:24 +0200

    🚧 GetErrorByIdController

--- src/ServiceControl/MessageFailures/Api/GetErrorByIdController.cs
+++ src/ServiceControl/MessageFailures/Api/GetErrorByIdController.cs
@@ -1,108 +1,51 @@
 ﻿namespace ServiceControl.MessageFailures.Api
 {
     using System;
-    using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Threading.Tasks;
     using System.Web.Http;
     using Infrastructure.WebApi;
-    using NServiceBus.Logging;
-    using Raven.Client;
-    using ServiceControl.Operations;
+    using Persistence;
 
     class GetErrorByIdController : ApiController
     {
-        public GetErrorByIdController(IDocumentStore documentStore)
+        public GetErrorByIdController(IErrorMessageDataStore dataStore)
         {
-            this.documentStore = documentStore;
+            this.dataStore = dataStore;
         }
 
         [Route("errors/{failedmessageid:guid}")]
         [HttpGet]
         public async Task<HttpResponseMessage> ErrorBy(Guid failedMessageId)
         {
-            using (var session = documentStore.OpenAsyncSession())
+            var result = await dataStore.ErrorBy(failedMessageId)
+                .ConfigureAwait(false);
+
+            if (result == null)
             {
-                var message = await session.LoadAsync<FailedMessage>(failedMessageId).ConfigureAwait(false);
-
-                if (message == null)
-                {
-                    return Request.CreateResponse(HttpStatusCode.NotFound);
-                }
-
-                return Negotiator.FromModel(Request, message);
+                return Request.CreateResponse(HttpStatusCode.NotFound);
             }
+
+            return Negotiator.FromModel(Request, result);
         }
 
         [Route("errors/last/{failedmessageid:guid}")]
         [HttpGet]
         public async Task<HttpResponseMessage> ErrorLastBy(Guid failedMessageId)
         {
-            using (var session = documentStore.OpenAsyncSession())
+            var result = await dataStore.ErrorLastBy(failedMessageId)
+                .ConfigureAwait(false);
+
+            if (result == null)
             {
-                var message = await session.LoadAsync<FailedMessage>(failedMessageId).ConfigureAwait(false);
-
-                if (message == null)
-                {
-                    return Request.CreateResponse(HttpStatusCode.NotFound);
-                }
-
-                var result = Map(message, session);
-
-                return Negotiator.FromModel(Request, result);
+                return Request.CreateResponse(HttpStatusCode.NotFound);
             }
+
+
+            return Negotiator.FromModel(Request, result);
         }
 
-        static FailedMessageView Map(FailedMessage message, IAsyncDocumentSession session)
-        {
-            var processingAttempt = message.ProcessingAttempts.Last();
-
-            var metadata = processingAttempt.MessageMetadata;
-            var failureDetails = processingAttempt.FailureDetails;
-            var wasEdited = message.ProcessingAttempts.Last().Headers.ContainsKey("ServiceControl.EditOf");
-
-            var failedMsgView = new FailedMessageView
-            {
-                Id = message.UniqueMessageId,
-                MessageType = metadata.GetAsStringOrNull("MessageType"),
-                IsSystemMessage = metadata.GetOrDefault<bool>("IsSystemMessage"),
-                TimeSent = metadata.GetAsNullableDatetime("TimeSent"),
-                MessageId = metadata.GetAsStringOrNull("MessageId"),
-                Exception = failureDetails.Exception,
-                QueueAddress = failureDetails.AddressOfFailingEndpoint,
-                NumberOfProcessingAttempts = message.ProcessingAttempts.Count,
-                Status = message.Status,
-                TimeOfFailure = failureDetails.TimeOfFailure,
-                LastModified = session.Advanced.GetMetadataFor(message)["Last-Modified"].Value<DateTime>(),
-                Edited = wasEdited,
-                EditOf = wasEdited ? message.ProcessingAttempts.Last().Headers["ServiceControl.EditOf"] : ""
-            };
-
-            try
-            {
-                failedMsgView.SendingEndpoint = metadata.GetOrDefault<EndpointDetails>("SendingEndpoint");
-            }
-            catch (Exception ex)
-            {
-                Logger.Warn($"Unable to parse SendingEndpoint from metadata for messageId {message.UniqueMessageId}", ex);
-                failedMsgView.SendingEndpoint = EndpointDetailsParser.SendingEndpoint(processingAttempt.Headers);
-            }
-
-            try
-            {
-                failedMsgView.ReceivingEndpoint = metadata.GetOrDefault<EndpointDetails>("ReceivingEndpoint");
-            }
-            catch (Exception ex)
-            {
-                Logger.Warn($"Unable to parse ReceivingEndpoint from metadata for messageId {message.UniqueMessageId}", ex);
-                failedMsgView.ReceivingEndpoint = EndpointDetailsParser.ReceivingEndpoint(processingAttempt.Headers);
-            }
-
-            return failedMsgView;
-        }
-
-        readonly IDocumentStore documentStore;
-        static readonly ILog Logger = LogManager.GetLogger(typeof(GetErrorByIdController));
+        readonly IErrorMessageDataStore dataStore;
     }
 }
