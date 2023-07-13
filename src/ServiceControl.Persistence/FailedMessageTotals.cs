89f2326ccf790483c7a0eb0e8266d7bc7f23b9eb
Ramon Smits <ramon.smits@gmail.com>
2023-07-13 14:40:34 +0200

    Moved FailedMessageViewIndexNotifications to RavenDB project.  FailedMessageViewIndexNotifications now has the RavenDB observer code in it and and refactored FailedMessageNotificationsHostedService to contain the generic observer logic

---
+++ src/ServiceControl.Persistence/FailedMessageTotals.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceControl.Persistence
+{
+    public class FailedMessageTotals
+    {
+        public int UnresolvedTotal;
+        public int ArchivedTotal;
+    }
+}
---
+++ src/ServiceControl.Persistence/IFailedMessageDataStore.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceControl.Persistence
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IFailedMessageDataStore
+    {
+        IDisposable Subscribe(Func<FailedMessageTotals, Task> callback);
+    }
+}
