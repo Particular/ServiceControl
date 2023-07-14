cb77b3f05bf7588bd283761b7a3ad5897482c5da
Tomek Masternak <tomasz.masternak@particular.net>
2023-07-14 10:23:11 +0200

    ImportFailedErrors

---
+++ src/ServiceControl.Persistence/IFailedErrorImportDataStore.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceControl.Persistence
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ServiceControl.Operations;
+
+    public interface IFailedErrorImportDataStore
+    {
+        Task ProcessFailedErrorImports(Func<FailedTransportMessage, Task> processMessage, CancellationToken cancellationToken);
+    }
+}
--- src/ServiceControl/Operations/ImportFailedErrors.cs
+++ src/ServiceControl/Operations/ImportFailedErrors.cs
@@ -1,21 +1,18 @@
 ﻿namespace ServiceControl.Operations
 {
-    using System;
     using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
     using NServiceBus.Extensibility;
-    using NServiceBus.Logging;
-    using NServiceBus.Raw;
     using NServiceBus.Transport;
-    using Raven.Client;
+    using Persistence;
     using ServiceBus.Management.Infrastructure.Settings;
     using ServiceControl.Transports;
 
     class ImportFailedErrors
     {
         public ImportFailedErrors(
-            IDocumentStore store,
+            IFailedErrorImportDataStore store,
             ErrorIngestor errorIngestor,
             Settings settings,
             TransportCustomization transportCustomization,
@@ -37,58 +34,20 @@
                 await errorIngestor.VerifyCanReachForwardingAddress(dispatcher).ConfigureAwait(false);
             }
 
-            var succeeded = 0;
-            var failed = 0;
-            using (var session = store.OpenAsyncSession())
+            await store.ProcessFailedErrorImports(async transportMessage =>
             {
-                var query = session.Query<FailedErrorImport, FailedErrorImportIndex>();
-                using (var stream = await session.Advanced.StreamAsync(query, cancellationToken)
-                    .ConfigureAwait(false))
-                {
-                    while (!cancellationToken.IsCancellationRequested && await stream.MoveNextAsync().ConfigureAwait(false))
-                    {
-                        var transportMessage = stream.Current.Document.Message;
-                        try
-                        {
-                            var messageContext = new MessageContext(transportMessage.Id, transportMessage.Headers, transportMessage.Body, EmptyTransaction, EmptyTokenSource, EmptyContextBag);
-                            var taskCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-                            messageContext.SetTaskCompletionSource(taskCompletionSource);
+                var messageContext = new MessageContext(transportMessage.Id, transportMessage.Headers,
+                    transportMessage.Body, EmptyTransaction, EmptyTokenSource, EmptyContextBag);
+                var taskCompletionSource =
+                    new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                messageContext.SetTaskCompletionSource(taskCompletionSource);
 
-                            await errorIngestor.Ingest(new List<MessageContext> { messageContext }, dispatcher).ConfigureAwait(false);
-
-                            await taskCompletionSource.Task.ConfigureAwait(false);
-
-                            await store.AsyncDatabaseCommands.DeleteAsync(stream.Current.Key, null, cancellationToken)
-                                .ConfigureAwait(false);
-                            succeeded++;
-
-                            if (Logger.IsDebugEnabled)
-                            {
-                                Logger.Debug($"Successfully re-imported failed error message {transportMessage.Id}.");
-                            }
-                        }
-                        catch (OperationCanceledException)
-                        {
-                            //  no-op
-                        }
-                        catch (Exception e)
-                        {
-                            Logger.Error($"Error while attempting to re-import failed error message {transportMessage.Id}.", e);
-                            failed++;
-                        }
-                    }
-                }
-            }
-
-            Logger.Info($"Done re-importing failed errors. Successfully re-imported {succeeded} messages. Failed re-importing {failed} messages.");
-
-            if (failed > 0)
-            {
-                Logger.Warn($"{failed} messages could not be re-imported. This could indicate a problem with the data. Contact Particular support if you need help with recovering the messages.");
-            }
+                await errorIngestor.Ingest(new List<MessageContext> {messageContext}, dispatcher).ConfigureAwait(false);
+                await taskCompletionSource.Task.ConfigureAwait(false);
+            }, cancellationToken);
         }
 
-        readonly IDocumentStore store;
+        readonly IFailedErrorImportDataStore store;
         readonly ErrorIngestor errorIngestor;
         readonly Settings settings;
         readonly TransportCustomization transportCustomization;
@@ -96,6 +55,5 @@
         static readonly TransportTransaction EmptyTransaction = new TransportTransaction();
         static readonly CancellationTokenSource EmptyTokenSource = new CancellationTokenSource();
         static readonly ContextBag EmptyContextBag = new ContextBag();
-        static readonly ILog Logger = LogManager.GetLogger(typeof(ImportFailedErrors));
     }
 }
