5428428390ff7d9a85cd3b61d97dccf073d855b0
Ramon Smits <ramon.smits@gmail.com>
2023-07-14 09:36:00 +0200

    🧹 Mark unused code obsolete with a todo to address later

--- src/ServiceControl/MessageFailures/InternalMessages/ReclassificationOfErrorMessageComplete.cs
+++ src/ServiceControl/MessageFailures/InternalMessages/ReclassificationOfErrorMessageComplete.cs
@@ -1,7 +1,9 @@
 ﻿namespace ServiceControl.MessageFailures.InternalMessages
 {
+    using System;
     using Infrastructure.DomainEvents;
 
+    [Obsolete("Only used by legacy RavenDB35 storage engine")] // TODO: how to deal with this domain event
     public class ReclassificationOfErrorMessageComplete : IDomainEvent
     {
         public int NumberofMessageReclassified { get; set; }
