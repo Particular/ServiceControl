cf8f61a269e1f5521755af84c963ef0ee0362259
Ramon Smits <ramon.smits@gmail.com>
2023-07-18 17:48:38 +0200

    ArchiveAllInGroupHandler no longer is dependant on IDomainEvents to pass into IArchiveMessages dependency

--- src/ServiceControl/Recoverability/Archiving/ArchiveAllInGroupHandler.cs
+++ src/ServiceControl/Recoverability/Archiving/ArchiveAllInGroupHandler.cs
@@ -1,35 +1,32 @@
 namespace ServiceControl.Recoverability
 {
     using System.Threading.Tasks;
-    using Infrastructure.DomainEvents;
     using NServiceBus;
     using NServiceBus.Logging;
     using ServiceControl.Persistence.Recoverability;
 
     class ArchiveAllInGroupHandler : IHandleMessages<ArchiveAllInGroup>
     {
-        public ArchiveAllInGroupHandler(IArchiveMessages archiver, IDomainEvents domainEvents, RetryingManager retryingManager)
+        public ArchiveAllInGroupHandler(IArchiveMessages archiver, RetryingManager retryingManager)
         {
             this.archiver = archiver;
             this.retryingManager = retryingManager;
-            this.domainEvents = domainEvents;
         }
 
         public async Task Handle(ArchiveAllInGroup message, IMessageHandlerContext context)
         {
             if (retryingManager.IsRetryInProgressFor(message.GroupId))
             {
-                logger.Warn($"Attempt to archive a group ({message.GroupId}) which is currently in the process of being retried");
+                Log.Warn($"Attempt to archive a group ({message.GroupId}) which is currently in the process of being retried");
                 return;
             }
 
-            await archiver.ArchiveAllInGroup(message.GroupId, domainEvents).ConfigureAwait(false);
+            await archiver.ArchiveAllInGroup(message.GroupId).ConfigureAwait(false);
         }
 
-        IArchiveMessages archiver;
-        IDomainEvents domainEvents;
-        RetryingManager retryingManager;
+        readonly IArchiveMessages archiver;
+        readonly RetryingManager retryingManager;
 
-        static ILog logger = LogManager.GetLogger<ArchiveAllInGroupHandler>();
+        static ILog Log = LogManager.GetLogger<ArchiveAllInGroupHandler>();
     }
 }
