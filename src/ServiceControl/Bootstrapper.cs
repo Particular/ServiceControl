d3ebf6cb16c274768d9774bcfca07bdd1b0103c4
Ramon Smits <ramon.smits@gmail.com>
2023-07-19 20:03:57 +0200

    ServiceProviderOptions.ValidateOnBuild

--- src/ServiceControl/Bootstrapper.cs
+++ src/ServiceControl/Bootstrapper.cs
@@ -93,6 +93,11 @@ namespace Particular.ServiceControl
 
             HostBuilder = new HostBuilder();
             HostBuilder
+                .UseDefaultServiceProvider(c => // TODO: Remove when done testing
+                {
+                    c.ValidateOnBuild = false;
+                    c.ValidateScopes = false;
+                })
                 .ConfigureLogging(builder =>
                 {
                     builder.ClearProviders();
