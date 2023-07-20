3001cc19007f0675205e214af3b38a20cd6370e5
Ramon Smits <ramon.smits@gmail.com>
2023-07-20 14:25:47 +0200

    Removing TODO: The HTTP API will not be any different.

--- src/ServiceControl/CompositeViews/Messages/SearchApi.cs
+++ src/ServiceControl/CompositeViews/Messages/SearchApi.cs
@@ -16,7 +16,6 @@ namespace ServiceControl.CompositeViews.Messages
 
         protected override Task<QueryResult<IList<MessagesView>>> LocalQuery(HttpRequestMessage request, string input)
         {
-            // TODO: Will the INPUT format be identical between RavenDB 3.x and 5.x? Also, what if we want to support a different storage engine?
             var pagingInfo = request.GetPagingInfo();
             var sortInfo = request.GetSortInfo();
             return DataStore.GetAllMessagesForSearch(input, pagingInfo, sortInfo);
