e282fa20def9c96c2a8e612ed2fc26eb8123d408
David Boike <david.boike@gmail.com>
2023-07-21 13:01:09 -0500

    Create Servicecontrol.Persistence.Tests.RavenDb and move bootstrapper test there

--- src/ServiceControl.Persistence.RavenDb/InternalsVisibleTo.cs
+++ src/ServiceControl.Persistence.RavenDb/InternalsVisibleTo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ServiceControl.UnitTests")]
 [assembly: InternalsVisibleTo("ServiceControl.PersistenceTests")]
+[assembly: InternalsVisibleTo("ServiceControl.Persistence.Tests.RavenDb")]
