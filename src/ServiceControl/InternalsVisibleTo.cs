3baab6925707fb181aa5faaa7f0a13a34c37fadb
Ramon Smits <ramon.smits@gmail.com>
2023-07-28 10:57:17 +0200

    Persistence tests now using shared project and a project per persister implementation

--- src/ServiceControl/InternalsVisibleTo.cs
+++ src/ServiceControl/InternalsVisibleTo.cs
@@ -5,6 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("ServiceControl.AcceptanceTests.RavenDB")]
 [assembly: InternalsVisibleTo("ServiceControl.AcceptanceTesting")]
 [assembly: InternalsVisibleTo("ServiceControl.PersistenceTests")]
+[assembly: InternalsVisibleTo("ServiceControl.Persistence.Tests.RavenDB")]
 [assembly: InternalsVisibleTo("ServiceControl.MultiInstance.AcceptanceTests")]
 [assembly: InternalsVisibleTo("ServiceControl.Persistence.InMemory")]
 [assembly: InternalsVisibleTo("ServiceControl.Persistence.RavenDb")]
