523643007e1206b8d3ad7361fe41459f38644046
Ramon Smits <ramon.smits@gmail.com>
2023-08-04 14:44:29 +0200

    📚 AggressivelyCacheFor does not allow passing TimeSpan.Zero, defined new 5 minute default

--- src/ServiceControl.Persistence.RavenDb/Editing/NotificationsManager.cs
+++ src/ServiceControl.Persistence.RavenDb/Editing/NotificationsManager.cs
@@ -7,13 +7,16 @@
 
     class NotificationsManager : AbstractSessionManager, INotificationsManager
     {
+        static readonly TimeSpan CacheTimeoutDefault = TimeSpan.FromMinutes(5); // TODO: Copied value from SendEmailNotificationHandler.cacheTimeout, Raven requires this to be at least 1 second
+
         public NotificationsManager(IAsyncDocumentSession session) : base(session)
         {
         }
 
         public async Task<NotificationsSettings> LoadSettings(TimeSpan? cacheTimeout = null)
         {
-            using (Session.Advanced.DocumentStore.AggressivelyCacheFor(cacheTimeout ?? TimeSpan.Zero))
+
+            using (Session.Advanced.DocumentStore.AggressivelyCacheFor(cacheTimeout ?? CacheTimeoutDefault))
             {
                 var settings = await Session
                     .LoadAsync<NotificationsSettings>(NotificationsSettings.SingleDocumentId);
