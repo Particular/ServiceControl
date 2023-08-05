db3049a69989fa1e8c76d3e02fe4cb8af1f23861
Ramon Smits <ramon.smits@gmail.com>
2023-08-05 16:17:00 +0200

    Added assert to validate if passed string value already has the prefix as that indicates that code is using the storage identifier (Id property) instead of the logical key

--- src/ServiceControl.Persistence.RavenDb/DocumentIdGenerators/FailedMessageIdGenerator.cs
+++ src/ServiceControl.Persistence.RavenDb/DocumentIdGenerators/FailedMessageIdGenerator.cs
@@ -1,8 +1,16 @@
-﻿static class FailedMessageIdGenerator
+﻿using System.Diagnostics;
+
+static class FailedMessageIdGenerator
 {
     public const string CollectionName = "FailedMessages";
 
     //[Obsolete("Use Guid.Parse")] TODO: As these are all guids... we don't need these generators.. Unless these are MessageIdentifiers as these can have any string value
-    public static string MakeDocumentId(string messageUniqueId) => $"{CollectionName}/{messageUniqueId}";
+    public static string MakeDocumentId(string messageUniqueId)
+    {
+        Debug.Assert(!HasPrefix(messageUniqueId), $"value has {CollectionName}/ prefix"); // TODO: Could potentially be removed when all tests are green but no harm as its only included on Debug builds
+        return $"{CollectionName}/{messageUniqueId}";
+    }
+
     public static string GetMessageIdFromDocumentId(string failedMessageDocumentId) => failedMessageDocumentId.Substring(CollectionName.Length + 1);
+    static bool HasPrefix(string value) => value.StartsWith(CollectionName + "/");
 }
