60bc8fdf091efcb0866759eb33c9759cc39697ee
Ramon Smits <ramon.smits@gmail.com>
2023-08-31 13:53:37 +0200

    🔨  Remove ServiceControl.Persistence.InMemory

--- src/ServiceControl.Persistence.InMemory/NoOpFailureReclassifier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceControl.Persistence.InMemory
-{
-    using System.Threading.Tasks;
-
-    class NoOpFailureReclassifier : IReclassifyFailedMessages
-    {
-        public Task<int> ReclassifyFailedMessages(bool force) => Task.FromResult(0);
-    }
-}
