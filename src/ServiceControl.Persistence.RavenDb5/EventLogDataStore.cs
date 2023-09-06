ed0c11373e0f0d55e9932098a8d314318ad08822
Ramon Smits <ramon.smits@gmail.com>
2023-09-06 15:58:28 +0200

    more namespaces

--- src/ServiceControl.Persistence.RavenDb5/EventLogDataStore.cs
+++ src/ServiceControl.Persistence.RavenDb5/EventLogDataStore.cs
@@ -6,7 +6,6 @@
     using Persistence.Infrastructure;
     using Raven.Client;
     using Raven.Client.Documents;
-    using Raven.Client.Linq;
 
     class EventLogDataStore : IEventLogDataStore
     {
