9b8a82a860023a08d846c4e740471394370074b4
Tomek Masternak <tomasz.masternak@particular.net>
2023-09-08 09:19:35 +0200

    small comment

--- src/ServiceControl.Persistence.RavenDb5/ExternalIntegrationRequestsDataStore.cs
+++ src/ServiceControl.Persistence.RavenDb5/ExternalIntegrationRequestsDataStore.cs
@@ -139,6 +139,7 @@
                 if (awaitingDispatching.Count == 0)
                 {
                     //TODO: this should ensure we query again if the result is potentially stale
+                    //      if ☝️ is not true we will need to use/parse the ChangeVector when document is written and compare to ResultEtag
                     return stats.IsStale;
                 }
 
