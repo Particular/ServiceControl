6b01993326ffbc6db2a1b43acc88fe31216c84ef
David Boike <david.boike@gmail.com>
2023-09-12 11:18:52 -0500

    Patches in RetryBatchesDataStore

--- src/ServiceControl.Persistence.RavenDb5/RetryBatchesDataStore.cs
+++ src/ServiceControl.Persistence.RavenDb5/RetryBatchesDataStore.cs
@@ -5,11 +5,12 @@
     using System.Threading.Tasks;
     using MessageFailures;
     using NServiceBus.Logging;
-    using ServiceControl.Recoverability;
     using Raven.Client.Documents;
+    using Raven.Client.Documents.Commands;
     using Raven.Client.Documents.Commands.Batches;
     using Raven.Client.Documents.Operations;
     using Raven.Client.Exceptions;
+    using ServiceControl.Recoverability;
 
     class RetryBatchesDataStore : IRetryBatchesDataStore
     {
@@ -42,17 +43,14 @@
 
                 Log.Warn($"Attempt {1} of {maxStagingAttempts} to stage a retry message {failedMessage.UniqueMessageId} failed", e);
 
-                commands[commandIndex] = new PatchCommandData
+                commands[commandIndex] = new PatchCommandData(failedMessageRetry.Id, null, new PatchRequest
                 {
-                    Patches = new[]
+                    Script = @"this.StageAttempts = args.Value",
+                    Values =
                     {
-                        new PatchRequest
-                        {
-                            Type = PatchCommandType.Set, Name = "StageAttempts", Value = 1
-                        }
-                    },
-                    Key = failedMessageRetry.Id
-                };
+                        {"Value", 1 }
+                    }
+                });
 
                 commandIndex++;
             }
@@ -60,7 +58,10 @@
 
             try
             {
-                await documentStore.AsyncDatabaseCommands.BatchAsync(commands);
+                using var session = documentStore.OpenAsyncSession();
+
+                var batch = new SingleNodeBatchCommand(documentStore.Conventions, session.Advanced.Context, commands);
+                await session.Advanced.RequestExecutor.ExecuteAsync(batch, session.Advanced.Context);
             }
             catch (ConcurrencyException)
             {
@@ -74,16 +75,10 @@
         {
             try
             {
-                await documentStore.AsyncDatabaseCommands.PatchAsync(message.Id,
-                    new[]
-                    {
-                        new PatchRequest
-                        {
-                            Type = PatchCommandType.Set,
-                            Name = "StageAttempts",
-                            Value = message.StageAttempts + 1
-                        }
-                    });
+                await documentStore.Operations.SendAsync(new PatchOperation(message.Id, null, new PatchRequest
+                {
+                    Script = @"this.StageAttempts += 1"
+                }));
             }
             catch (ConcurrencyException)
             {
@@ -91,6 +86,10 @@
             }
         }
 
-        public Task DeleteFailedMessageRetry(string uniqueMessageId) => documentStore.AsyncDatabaseCommands.DeleteAsync(FailedMessageRetry.MakeDocumentId(uniqueMessageId), null);
+        public async Task DeleteFailedMessageRetry(string uniqueMessageId)
+        {
+            using var session = documentStore.OpenAsyncSession();
+            await session.Advanced.RequestExecutor.ExecuteAsync(new DeleteDocumentCommand(FailedMessageRetry.MakeDocumentId(uniqueMessageId), null), session.Advanced.Context);
+        }
     }
 }
