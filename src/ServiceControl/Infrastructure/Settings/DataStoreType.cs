049aefec244fd01919390e910932b5b8d6355e4c
David Boike <david.boike@gmail.com>
2023-09-14 16:08:28 -0500

    Magic string tests need to be persister-specific

--- src/ServiceControl/Infrastructure/Settings/DataStoreType.cs
+++ src/ServiceControl/Infrastructure/Settings/DataStoreType.cs
@@ -3,12 +3,14 @@
     public enum DataStoreType
     {
         InMemory = 1,
-        RavenDB35 = 2
+        RavenDB35 = 2,
+        RavenDB5 = 3,
     }
 
     public static class DataStoreConfig
     {
         public static string InMemoryPersistenceTypeFullyQualifiedName = "ServiceControl.Persistence.InMemory.InMemoryPersistenceConfiguration, ServiceControl.Persistence.InMemory";
         public static string RavenDB35PersistenceTypeFullyQualifiedName = "ServiceControl.Persistence.RavenDb.RavenDbPersistenceConfiguration, ServiceControl.Persistence.RavenDb";
+        public static string RavenDB5PersistenceTypeFullyQualifiedName = "ServiceControl.Persistence.RavenDb.RavenDbPersistenceConfiguration, ServiceControl.Persistence.RavenDb5";
     }
 }
