c1ce57607751b7b741b8eeb65eb34b9b9f85afd4
Tomek Masternak <tomasz.masternak@particular.net>
2023-09-15 13:49:49 +0200

    EmbeddedDatabase test

---
+++ src/ServiceControl.Persistence.Tests.RavenDb5/EmbeddedDatabaseTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceControl.Persistence.Infrastructure;
+using ServiceControl.Persistence.RavenDb;
+using ServiceControl.Persistence.RavenDb5;
+
+[TestFixture]
+class EmbeddedDatabaseTests
+{
+    [Test]
+    public async Task CanStart()
+    {
+        using (var embeddedDatabase = EmbeddedDatabase.Start(new RavenDBPersisterSettings { }))
+        {
+
+            using (var documentStore = await embeddedDatabase.Connect(CancellationToken.None))
+            {
+                var store = new EventLogDataStore(documentStore);
+
+                _ = await store.GetEventLogItems(new PagingInfo());
+            }
+        }
+    }
+}
