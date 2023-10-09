497771a4f7a3525cf93ba4caeeef81abaf7bb9f6
David Boike <david.boike@gmail.com>
2023-10-09 09:09:07 -0500

    Remove RavenDB 3.5 project directories (All removals)

--- src/ServiceControl.Persistence.RavenDb/DocumentIdGenerators/MessageBodyIdGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿static class MessageBodyIdGenerator
-{
-    const string CollectionName = "messagebodies";
-
-    public static string MakeDocumentId(string messageUniqueId)
-    {
-        return $"{CollectionName}/{messageUniqueId}";
-    }
-}
