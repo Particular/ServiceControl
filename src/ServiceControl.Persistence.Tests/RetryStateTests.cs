a91eb08d14db618038dd22a48219bb8d15b21f6d
Ramon Smits <ramon.smits@gmail.com>
2023-10-11 10:16:44 +0200

    Replaces with `Repeat` https://docs.nunit.org/articles/nunit/writing-tests/attributes/repeat.html

--- src/ServiceControl.Persistence.Tests/RetryStateTests.cs
+++ src/ServiceControl.Persistence.Tests/RetryStateTests.cs
@@ -116,40 +116,9 @@
         public void Unregister() => AppDomain.CurrentDomain.FirstChanceException -= TraceWriteFirstChanceException;
 
         [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        [TestCase(6)]
-        [TestCase(7)]
-        [TestCase(8)]
-        [TestCase(9)]
-        [TestCase(10)]
-        [TestCase(11)]
-        [TestCase(12)]
-        [TestCase(13)]
-        [TestCase(14)]
-        [TestCase(15)]
-        [TestCase(16)]
-        [TestCase(17)]
-        [TestCase(18)]
-        [TestCase(19)]
-        [TestCase(20)]
-        [TestCase(21)]
-        [TestCase(22)]
-        [TestCase(23)]
-        [TestCase(24)]
-        [TestCase(25)]
-        [TestCase(26)]
-        [TestCase(27)]
-        [TestCase(28)]
-        [TestCase(29)]
-        [TestCase(30)]
-        public async Task When_there_is_one_poison_message_it_is_removed_from_batch_and_the_status_is_Complete(int num)
+        [Repeat(30)]
+        public async Task When_there_is_one_poison_message_it_is_removed_from_batch_and_the_status_is_Complete()
         {
-            TestContext.Out.WriteLine($"Heisenbug Test Number {num}");
-
             var domainEvents = new FakeDomainEvents();
             var retryManager = new RetryingManager(domainEvents);
 
