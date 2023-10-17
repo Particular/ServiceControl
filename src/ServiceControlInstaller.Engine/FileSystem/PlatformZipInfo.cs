c1b1ec886dc954202b2f37ee87bf65d6c08447b8
David Boike <david.boike@gmail.com>
2023-10-17 17:20:30 -0500

    Unsure about these changes

--- src/ServiceControlInstaller.Engine/FileSystem/PlatformZipInfo.cs
+++ src/ServiceControlInstaller.Engine/FileSystem/PlatformZipInfo.cs
@@ -4,21 +4,19 @@
 
     public class PlatformZipInfo
     {
-        public static PlatformZipInfo Empty = new PlatformZipInfo(string.Empty, string.Empty, string.Empty, null, false);
+        public static PlatformZipInfo Empty = new PlatformZipInfo(string.Empty, string.Empty, string.Empty, null);
 
-        public PlatformZipInfo(string mainEntrypoint, string name, string filePath, Version version, bool present)
+        public PlatformZipInfo(string mainEntrypoint, string name, string filePath, Version version)
         {
             Name = name;
             MainEntrypoint = mainEntrypoint;
             FilePath = filePath;
             Version = version;
-            Present = present;
         }
 
         public string MainEntrypoint { get; }
         public string Name { get; }
         public string FilePath { get; }
         public Version Version { get; }
-        public bool Present { get; }
     }
 }
--- src/ServiceControlInstaller.Engine/FileSystem/PlatformZipInfoExtensions.cs
+++ src/ServiceControlInstaller.Engine/FileSystem/PlatformZipInfoExtensions.cs
@@ -1,18 +1,12 @@
 ﻿namespace ServiceControlInstaller.Engine.FileSystem
 {
     using System;
-    using System.IO;
     using Ionic.Zip;
 
     public static class PlatformZipInfoExtensions
     {
         public static void ValidateZip(this PlatformZipInfo zipInfo)
         {
-            if (!zipInfo.Present)
-            {
-                throw new FileNotFoundException($"No Zip file found at: {zipInfo.FilePath}");
-            }
-
             if (!ZipFile.CheckZip(zipInfo.FilePath))
             {
                 throw new Exception($"Corrupt Zip File - {zipInfo.FilePath}");
