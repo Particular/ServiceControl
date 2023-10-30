678d3ad2308c992870a3cc82bfef5011f8c66177
David Boike <david.boike@gmail.com>
2023-10-30 16:20:44 -0500

    Remove transport names that now have no references

--- src/ServiceControlInstaller.Engine/Instances/TransportNames.cs
+++ src/ServiceControlInstaller.Engine/Instances/TransportNames.cs
@@ -2,23 +2,8 @@ namespace ServiceControlInstaller.Engine.Instances
 {
     public static class TransportNames
     {
-        public const string AzureServiceBus = "Azure Service Bus";
 
-        public const string AzureServiceBusEndpointOrientedTopologyDeprecated = "Azure Service Bus - Endpoint-oriented topology (Legacy)";
 
-        public const string AzureServiceBusEndpointOrientedTopologyLegacy = "Azure Service Bus - Endpoint-oriented topology (Legacy)";
-        // for backward compatibility
-        public const string AzureServiceBusEndpointOrientedTopologyOld = "Azure Service Bus - Endpoint-oriented topology (Old)";
-
-        public const string AzureServiceBusForwardingTopologyDeprecated = "Azure Service Bus - Forwarding topology (Legacy)";
-
-        public const string AzureServiceBusForwardingTopologyLegacy = "Azure Service Bus - Forwarding topology (Legacy)";
-        // for backward compatibility
-        public const string AzureServiceBusForwardingTopologyOld = "Azure Service Bus - Forwarding topology (Old)";
-
-        public const string AmazonSQS = "AmazonSQS";
-
-        public const string AzureStorageQueue = "Azure Storage Queue";
 
         public const string MSMQ = "MSMQ";
 
