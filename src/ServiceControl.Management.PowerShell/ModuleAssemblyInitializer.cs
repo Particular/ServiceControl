c65658d216e5e5b732750b79cd367457f5db9ef3
Brandon Ording <bording@gmail.com>
2023-11-09 16:44:06 -0500

    Add assembly load context for InstallerEngine

---
+++ src/ServiceControl.Management.PowerShell/ModuleAssemblyInitializer.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceControl.Management.PowerShell
+{
+    using System.Management.Automation;
+    using System.Reflection;
+    using System.Runtime.Loader;
+
+    public class ModuleAssemblyInitializer : IModuleAssemblyInitializer, IModuleAssemblyCleanup
+    {
+        static readonly InstallerEngineAssemblyLoadContext installerEngineLoadContext = new();
+
+        public void OnImport() => AssemblyLoadContext.Default.Resolving += Resolve;
+
+        public void OnRemove(PSModuleInfo psModuleInfo) => AssemblyLoadContext.Default.Resolving -= Resolve;
+
+        static Assembly Resolve(AssemblyLoadContext defaultLoadContext, AssemblyName assemblyName) => installerEngineLoadContext.LoadFromAssemblyName(assemblyName);
+    }
+}
