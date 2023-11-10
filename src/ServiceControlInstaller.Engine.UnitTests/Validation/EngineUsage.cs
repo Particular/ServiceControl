a9b1b1f8b8cd148d31da670a5f909f25bcd5fc75
David Boike <david.boike@gmail.com>
2023-11-10 11:57:07 -0600

    Update Engine approval

--- src/ServiceControlInstaller.Engine.UnitTests/Validation/EngineUsage.cs
+++ src/ServiceControlInstaller.Engine.UnitTests/Validation/EngineUsage.cs
@@ -14,8 +14,12 @@
             var srcPath = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", ".."));
             StringAssert.EndsWith("src", srcPath);
 
-            var paths = Directory.GetFiles(srcPath, "ServiceControlInstaller.Engine.dll", SearchOption.AllDirectories)
-                .Select(path => path.Substring(srcPath.Length + 1).Split(Path.DirectorySeparatorChar).First()) // Get project directory
+            var repoRoot = Path.GetFullPath(Path.Combine(srcPath, ".."));
+
+            // It would probably be more correct to parse csproj files but this is a good enough check for now.
+            var paths = Directory.GetFiles(repoRoot, "ServiceControlInstaller.Engine.dll", SearchOption.AllDirectories)
+                .Select(path => path.Substring(repoRoot.Length + 1))
+                .Select(GetImportantPart)
                 .Distinct()
                 .OrderBy(x => x)
                 .ToArray();
@@ -26,7 +30,26 @@
             // transport package, persistence package, and RavenDB server. Introducing this dependency to other places in the 
             // build chain has big impacts on build speed and space available on disk during CI. Tests that need to use anything
             // in the Engine assembly should only be performed in these approved projects.
+            Console.WriteLine(verifyText);
             Approver.Verify(verifyText);
+        }
+
+        string GetImportantPart(string path)
+        {
+            var binLocation = path.IndexOf($"{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}");
+            if (binLocation > 0)
+            {
+                path = path.Substring(0, binLocation);
+            }
+
+
+            var objLocation = path.IndexOf($"{Path.DirectorySeparatorChar}obj{Path.DirectorySeparatorChar}");
+            if (objLocation > 0)
+            {
+                path = path.Substring(0, objLocation);
+            }
+
+            return path.Replace(Path.DirectorySeparatorChar, '/');
         }
     }
 }
