f67a3c52ed09c0fcc6f652ecf1cbb0b7c983a01e
David Boike <david.boike@gmail.com>
2023-11-20 15:06:43 -0600

    Test to make sure self-contained RavenDB is correctly used

--- src/ServiceControlInstaller.Engine.UnitTests/Validation/RuntimeNetVersionTest.cs
+++ src/ServiceControlInstaller.Engine.UnitTests/Validation/RuntimeNetVersionTest.cs
@@ -46,6 +46,24 @@
             }
         }
 
+        [Test]
+        public void CheckForSelfContainedRavenDB()
+        {
+            bool isCI = Environment.GetEnvironmentVariable("CI") == "true";
+            bool isLocal = !isCI;
+
+            var ravenServerPath = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "..", "..", "deploy", "RavenDBServer"));
+            var ravenStudio = Path.Combine(ravenServerPath, "Raven.Studio.zip");
+            var runtimes = Path.Combine(ravenServerPath, "runtimes");
+            var systemCollections = Path.Combine(ravenServerPath, "System.Collections.dll");
+            var aspNetCoreHttp = Path.Combine(ravenServerPath, "Microsoft.AspNetCore.Http.dll");
+
+            FileAssert.Exists(ravenStudio); // No matter what
+            Assert.That(Directory.Exists(runtimes), Is.EqualTo(isLocal));  // Only in local development
+            Assert.That(File.Exists(systemCollections), Is.EqualTo(isCI)); // Only on CI
+            Assert.That(File.Exists(aspNetCoreHttp), Is.EqualTo(isCI));    // Only on CI
+        }
+
         static RavenServerVersions GetFromRavenServerRuntimeConfig()
         {
             // First get RavenDB package version
