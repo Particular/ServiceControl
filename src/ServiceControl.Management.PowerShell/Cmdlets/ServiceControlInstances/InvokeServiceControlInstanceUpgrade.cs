769a0618218fc49819ac63aede8ae211f2bb4886
Tomek Masternak <tomasz.masternak@particular.net>
2023-11-22 15:13:00 +0100

    formatting

--- src/ServiceControl.Management.PowerShell/Cmdlets/ServiceControlInstances/InvokeServiceControlInstanceUpgrade.cs
+++ src/ServiceControl.Management.PowerShell/Cmdlets/ServiceControlInstances/InvokeServiceControlInstanceUpgrade.cs
@@ -16,10 +16,8 @@ namespace ServiceControl.Management.PowerShell
         [Parameter(Mandatory = false, HelpMessage = "Do not automatically create new queues")]
         public SwitchParameter SkipQueueCreation { get; set; }
 
-
         [Parameter(Mandatory = false, HelpMessage = "Perform upgrade even if the current instance is using obsolete, incompatible RavenDB 3.5 storage engine. Replaces the database with a brand new one, removing all data previously stored.")]
         public SwitchParameter Force { get; set; }
-
 
         protected override void BeginProcessing()
         {
