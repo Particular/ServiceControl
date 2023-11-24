98a93aea9d950bbf6bd2612607357ee6e8973df8
Daniel Marbach <daniel.marbach@openplace.net>
2023-11-24 10:53:50 +0100

    More rabbitmq

--- src/ServiceControl.Transports.RabbitMQ/RabbitMQDirectRoutingTransportCustomization.cs
+++ src/ServiceControl.Transports.RabbitMQ/RabbitMQDirectRoutingTransportCustomization.cs
@@ -1,69 +1,64 @@
 ﻿namespace ServiceControl.Transports.RabbitMQ
 {
+    using System;
     using NServiceBus;
-    using NServiceBus.Raw;
 
-    public abstract class RabbitMQDirectRoutingTransportCustomization : TransportCustomization
+    public abstract class RabbitMQDirectRoutingTransportCustomization : TransportCustomization<RabbitMQTransport>
     {
         readonly QueueType queueType;
 
-        protected RabbitMQDirectRoutingTransportCustomization(QueueType queueType)
+        protected RabbitMQDirectRoutingTransportCustomization(QueueType queueType) => this.queueType = queueType;
+
+        protected override void CustomizeTransportSpecificSendOnlyEndpointSettings(
+            EndpointConfiguration endpointConfiguration, RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            this.queueType = queueType;
         }
 
-        protected override void CustomizeTransportSpecificSendOnlyEndpointSettings(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+        protected override void CustomizeTransportSpecificServiceControlEndpointSettings(
+            EndpointConfiguration endpointConfiguration, RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            CustomizeEndpoint(endpointConfiguration, transportSettings, queueType);
         }
 
-        protected override void CustomizeTransportSpecificServiceControlEndpointSettings(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+        protected override void CustomizeRawSendOnlyEndpoint(RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            CustomizeEndpoint(endpointConfiguration, transportSettings, queueType);
         }
 
-        protected override void CustomizeRawSendOnlyEndpoint(RawEndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+        protected override void CustomizeForQueueIngestion(RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            CustomizeRawEndpoint(endpointConfiguration, transportSettings, queueType);
         }
 
-        protected override void CustomizeForQueueIngestion(RawEndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+        protected override void CustomizeTransportSpecificMonitoringEndpointSettings(
+            EndpointConfiguration endpointConfiguration, RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            CustomizeRawEndpoint(endpointConfiguration, transportSettings, queueType);
         }
 
-        protected override void CustomizeTransportSpecificMonitoringEndpointSettings(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+        protected override void CustomizeForReturnToSenderIngestion(RabbitMQTransport transportDefinition,
+            TransportSettings transportSettings)
         {
-            CustomizeEndpoint(endpointConfiguration, transportSettings, queueType);
         }
 
-        public override void CustomizeForReturnToSenderIngestion(RawEndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
-        {
-            CustomizeRawEndpoint(endpointConfiguration, transportSettings, queueType);
-        }
+        public override IProvideQueueLength CreateQueueLengthProvider() => new QueueLengthProvider();
 
-        public override IProvideQueueLength CreateQueueLengthProvider()
+        protected override RabbitMQTransport CreateTransport(TransportSettings transportSettings)
         {
-            return new QueueLengthProvider();
-        }
+            if (transportSettings.ConnectionString == null)
+            {
+                throw new InvalidOperationException("Connection string not configured");
+            }
 
-        static void CustomizeEndpoint(EndpointConfiguration endpointConfig, TransportSettings transportSettings, QueueType queueType)
-        {
-            var transport = endpointConfig.UseTransport<RabbitMQTransport>();
-            ConfigureTransport(transport, transportSettings, queueType);
-        }
-
-        static void CustomizeRawEndpoint(RawEndpointConfiguration endpointConfig, TransportSettings transportSettings, QueueType queueType)
-        {
-            var transport = endpointConfig.UseTransport<RabbitMQTransport>();
-            ConfigureTransport(transport, transportSettings, queueType);
-        }
-
-        static void ConfigureTransport(TransportExtensions<RabbitMQTransport> transport, TransportSettings transportSettings, QueueType queueType)
-        {
-            transport.UseDirectRoutingTopology(queueType, routingKeyConvention: type => type.FullName.Replace(".", "-"));
-            transport.Transactions(TransportTransactionMode.ReceiveOnly);
-            transport.ApplyConnectionString(transportSettings.ConnectionString);
+            var transport =
+                new RabbitMQTransport(
+                    RoutingTopology.Direct(queueType, routingKeyConvention: type => type.FullName.Replace(".", "-")),
+                    transportSettings.ConnectionString)
+                {
+                    TransportTransactionMode = TransportTransactionMode.ReceiveOnly
+                };
+            return transport;
         }
     }
 }
