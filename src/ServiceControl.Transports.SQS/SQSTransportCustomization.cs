f746485b5ada01236424eae33f5ea6d236a08315
Daniel Marbach <daniel.marbach@openplace.net>
2023-11-24 09:11:47 +0100

    SQS WIP

--- src/ServiceControl.Transports.SQS/SQSTransportCustomization.cs
+++ src/ServiceControl.Transports.SQS/SQSTransportCustomization.cs
@@ -10,11 +10,10 @@
     using Amazon.SimpleNotificationService;
     using Amazon.SQS;
     using NServiceBus;
-    using NServiceBus.Configuration.AdvancedExtensibility;
     using NServiceBus.Logging;
     using NServiceBus.Raw;
 
-    public class SQSTransportCustomization : TransportCustomization
+    public class SQSTransportCustomization : TransportCustomization<SqsTransport>
     {
         protected override void CustomizeTransportSpecificSendOnlyEndpointSettings(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
         {
@@ -51,9 +50,112 @@
             CustomizeRawEndpoint(endpointConfiguration, transportSettings);
         }
 
-        public override IProvideQueueLength CreateQueueLengthProvider()
+        public override IProvideQueueLength CreateQueueLengthProvider() => new QueueLengthProvider();
+
+        protected override SqsTransport CreateTransport(TransportSettings transportSettings)
         {
-            return new QueueLengthProvider();
+            // TODO NSB8 is this still needed?
+            //HINT: This is needed to make Core doesn't load a connection string value from the app.config.
+            //      This prevents SQS from throwing on startup.
+            // var connectionString = transport.GetSettings().Get("NServiceBus.TransportConnectionString");
+            //
+            // connectionString.GetType()
+            //     .GetField("GetValue", BindingFlags.NonPublic | BindingFlags.Instance)
+            //     ?.SetValue(connectionString, (Func<string>)(() => null));
+
+            var builder = new DbConnectionStringBuilder { ConnectionString = transportSettings.ConnectionString };
+
+            IAmazonSQS sqsClient = null;
+            IAmazonSimpleNotificationService snsClient = null;
+
+            bool alwaysLoadFromEnvironmentVariable = false;
+            if (builder.ContainsKey("AccessKeyId") || builder.ContainsKey("SecretAccessKey"))
+            {
+                PromoteEnvironmentVariableFromConnectionString(builder, "AccessKeyId", "AWS_ACCESS_KEY_ID");
+                PromoteEnvironmentVariableFromConnectionString(builder, "SecretAccessKey", "AWS_SECRET_ACCESS_KEY");
+
+                // if the user provided the access key and secret access key they should always be loaded from environment credentials
+                alwaysLoadFromEnvironmentVariable = true;
+                sqsClient = new AmazonSQSClient(new EnvironmentVariablesAWSCredentials());
+                snsClient = new AmazonSimpleNotificationServiceClient(new EnvironmentVariablesAWSCredentials());
+            }
+            else
+            {
+                //See https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/net-dg-config-creds.html#creds-assign
+                log.Info(
+                    "BasicAWSCredentials have not been supplied in the connection string. Attempting to use existing environment or IAM role credentials for SQS Client.");
+                sqsClient = new AmazonSQSClient();
+                snsClient = new AmazonSimpleNotificationServiceClient();
+            }
+
+            string region = PromoteEnvironmentVariableFromConnectionString(builder, "Region", "AWS_REGION");
+
+            RegionEndpoint awsRegion = RegionEndpoint.EnumerableAllRegions
+                .SingleOrDefault(x => x.SystemName == region);
+
+            if (awsRegion == null)
+            {
+                throw new ArgumentException($"Unknown region: \"{region}\"");
+            }
+
+            var transport =
+                new SqsTransport(sqsClient, snsClient)
+                {
+                    TransportTransactionMode = TransportTransactionMode.ReceiveOnly
+                };
+
+            if (builder.TryGetValue("QueueNamePrefix", out object queueNamePrefix))
+            {
+                string queueNamePrefixAsString = (string)queueNamePrefix;
+                if (!string.IsNullOrEmpty(queueNamePrefixAsString))
+                {
+                    transport.QueueNamePrefix = queueNamePrefixAsString;
+                }
+            }
+
+            if (builder.TryGetValue("TopicNamePrefix", out object topicNamePrefix))
+            {
+                string topicNamePrefixAsString = (string)topicNamePrefix;
+                if (!string.IsNullOrEmpty(topicNamePrefixAsString))
+                {
+                    transport.TopicNamePrefix = topicNamePrefixAsString;
+                }
+            }
+
+            if (builder.TryGetValue("S3BucketForLargeMessages", out object bucketForLargeMessages))
+            {
+                string bucketForLargeMessagesAsString = (string)bucketForLargeMessages;
+                if (!string.IsNullOrEmpty(bucketForLargeMessagesAsString))
+                {
+                    string keyPrefixAsString = string.Empty;
+                    if (builder.TryGetValue("S3KeyPrefix", out object keyPrefix))
+                    {
+                        keyPrefixAsString = (string)keyPrefix;
+                    }
+
+                    IAmazonS3 s3Client = null;
+                    if (alwaysLoadFromEnvironmentVariable)
+                    {
+                        s3Client = new AmazonS3Client(new EnvironmentVariablesAWSCredentials());
+                    }
+                    else
+                    {
+                        log.Info(
+                            "BasicAWSCredentials have not been supplied in the connection string. Attempting to use existing environment or IAM role credentials for S3 Client.");
+                        s3Client = new AmazonS3Client();
+                    }
+
+                    transport.S3 = new S3Settings(bucketForLargeMessagesAsString, keyPrefixAsString, s3Client);
+                }
+            }
+
+            if (builder.TryGetValue("DoNotWrapOutgoingMessages", out object doNotWrapOutgoingMessages) &&
+                bool.TryParse(doNotWrapOutgoingMessages.ToString(), out bool doNotWrapOutgoingMessagesAsBool))
+            {
+                transport.DoNotWrapOutgoingMessages = doNotWrapOutgoingMessagesAsBool;
+            }
+
+            return transport;
         }
 
         static void CustomizeRawEndpoint(RawEndpointConfiguration endpointConfig, TransportSettings transportSettings)
