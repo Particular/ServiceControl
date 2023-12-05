06ab3b3e1572484ce2e30120b1806777e8793cf5
Brandon Ording <bording@gmail.com>
2023-12-05 16:33:56 -0500

    Update MD5 usage

--- src/ServiceControl.Transports.ASBS/Helper.cs
+++ src/ServiceControl.Transports.ASBS/Helper.cs
@@ -9,10 +9,8 @@
     {
         public static void ConfigureNameShorteners(this AzureServiceBusTransport transport)
         {
-#pragma warning disable 618
             transport.SubscriptionNamingConvention = n => n.Length > MaxEntityName ? MD5DeterministicNameBuilder.Build(n) : n;
             transport.SubscriptionRuleNamingConvention = n => n.FullName.Length > MaxEntityName ? MD5DeterministicNameBuilder.Build(n.FullName) : n.FullName;
-#pragma warning restore 618
         }
 
         const int MaxEntityName = 50;
@@ -22,12 +20,12 @@
             public static string Build(string input)
             {
                 var inputBytes = Encoding.Default.GetBytes(input);
+
                 // use MD5 hash to get a 16-byte hash of the string
-                using (var provider = new MD5CryptoServiceProvider())
-                {
-                    var hashBytes = provider.ComputeHash(inputBytes);
-                    return new Guid(hashBytes).ToString();
-                }
+                var hashBytes = MD5.HashData(inputBytes);
+
+                // generate a guid from the hash:
+                return new Guid(hashBytes).ToString();
             }
         }
     }
