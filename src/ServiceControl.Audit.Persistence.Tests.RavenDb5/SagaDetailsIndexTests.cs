96d540085136e803c35214e87574e11d02542e2f
WilliamBZA <WilliamBZA@users.noreply.github.com>
2023-12-06 12:14:13 +0200

    Add test structure for checking that incorrect index is deleted (#3883)
    * Add test structure for checking that incorrect index is deleted
    
    * Add a test for saga details index replacement at start
    
    ---------
    
    Co-authored-by: Szymon Pobiega <szymon.pobiega@gmail.com>

--- src/ServiceControl.Audit.Persistence.Tests.RavenDb5/SagaDetailsIndexTests.cs
+++ src/ServiceControl.Audit.Persistence.Tests.RavenDb5/SagaDetailsIndexTests.cs
@@ -1,14 +1,73 @@
 ﻿namespace ServiceControl.Audit.Persistence.Tests
 {
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
     using NUnit.Framework;
+    using Raven.Client.Documents.Indexes;
     using Raven.Client.Documents.Operations.Indexes;
     using ServiceControl.SagaAudit;
 
     [TestFixture]
     class SagaDetailsIndexTests : PersistenceTestFixture
     {
+        [Test]
+        public async Task Deletes_Index_That_Does_Not_Have_Cap_of_50000()
+        {
+            var indexWithout50000capDefinition = new IndexDefinition
+            {
+                Name = "SagaDetailsIndex",
+                Maps = new System.Collections.Generic.HashSet<string>
+                {
+                    @"from doc in docs
+                                         select new
+                                         {
+                                             doc.SagaId,
+                                             Id = doc.SagaId,
+                                             doc.SagaType,
+                                             Changes = new[]
+                                             {
+                        new
+                        {
+                            Endpoint = doc.Endpoint,
+                            FinishTime = doc.FinishTime,
+                            InitiatingMessage = doc.InitiatingMessage,
+                            OutgoingMessages = doc.OutgoingMessages,
+                            StartTime = doc.StartTime,
+                            StateAfterChange = doc.StateAfterChange,
+                            Status = doc.Status
+                        }
+                    }
+}"
+                },
+                Reduce = @"from result in results
+                                group result by result.SagaId
+                into g
+                                let first = g.First()
+                                select new
+                                {
+                                    Id = first.SagaId,
+                                    SagaId = first.SagaId,
+                                    SagaType = first.SagaType,
+                                    Changes = g.SelectMany(x => x.Changes)
+                                        .OrderByDescending(x => x.FinishTime)
+                                        .ToList()
+                                }"
+            };
+
+            var putIndexesOp = new PutIndexesOperation(indexWithout50000capDefinition);
+
+            // Execute the operation by passing it to Maintenance.Send
+            configuration.DocumentStore.Maintenance.Send(putIndexesOp);
+
+            await RavenDb.DatabaseSetup.RecreateSagaDetailsIndex(configuration.DocumentStore, CancellationToken.None);
+
+            var sagaDetailsIndexOperation = new GetIndexOperation("SagaDetailsIndex");
+            var sagaDetailsIndexDefinition = await configuration.DocumentStore.Maintenance.SendAsync(sagaDetailsIndexOperation);
+
+            Assert.IsTrue(sagaDetailsIndexDefinition.Reduce.Contains("Take(50000)"), "The SagaDetails index definition does not contain a .Take(50000) to limit the number of saga state changes that are reduced by the map/reduce");
+        }
+
         [Test]
         public async Task Should_only_reduce_the_last_50000_saga_state_changes()
         {
