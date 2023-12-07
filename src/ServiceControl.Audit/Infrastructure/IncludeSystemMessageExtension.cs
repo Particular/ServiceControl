746e77b108f7cff24548b28eb880e8a3dd3e9295
danielmarbach <daniel.marbach@openplace.net>
2023-12-07 09:42:11 +0100

    Move extensions

---
+++ src/ServiceControl.Audit/Infrastructure/IncludeSystemMessageExtension.cs
@@ -0,0 +1,13 @@
+namespace ServiceControl.Audit.Infrastructure;
+
+using System.Net.Http;
+
+public static class IncludeSystemMessageExtension
+{
+    public static bool GetIncludeSystemMessages(this HttpRequestMessage request)
+    {
+        var includeSystemMessages = request.GetQueryStringValue("include_system_messages", false);
+
+        return includeSystemMessages;
+    }
+}
