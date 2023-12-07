c7f9b05fe9d2afe030d685c652ddc00926dea24a
danielmarbach <daniel.marbach@openplace.net>
2023-12-07 09:57:21 +0100

    Revert "Move extensions"
    This reverts commit 066d7453852060fcc25fac6c12f7fcff344fdbb2.

--- src/ServiceControl.Audit.Persistence/Infrastructure/SortInfo.cs
+++ src/ServiceControl.Audit.Persistence/Infrastructure/SortInfo.cs
@@ -1,5 +1,8 @@
 ﻿namespace ServiceControl.Audit.Infrastructure
 {
+    using System.Collections.Generic;
+    using System.Net.Http;
+
     public class SortInfo
     {
         public string Direction { get; }
@@ -10,5 +13,38 @@
             Sort = sort;
             Direction = direction;
         }
+    }
+
+    public static class SortInfoExtension
+    {
+        public static SortInfo GetSortInfo(this HttpRequestMessage request, string defaultSortDirection = "desc")
+        {
+            var direction = request.GetQueryStringValue("direction", defaultSortDirection);
+            if (direction is not "asc" and not "desc")
+            {
+                direction = defaultSortDirection;
+            }
+
+            var sort = request.GetQueryStringValue("sort", "time_sent");
+            if (!AllowableSortOptions.Contains(sort))
+            {
+                sort = "time_sent";
+            }
+
+            return new SortInfo(sort, direction);
+        }
+
+        static HashSet<string> AllowableSortOptions =
+        [
+            "processed_at",
+            "id",
+            "message_type",
+            "time_sent",
+            "critical_time",
+            "delivery_time",
+            "processing_time",
+            "status",
+            "message_id"
+        ];
     }
 }
