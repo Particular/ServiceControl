c5f2d043e0967e574fbbe0aa99b658ca6dffb014
danielmarbach <daniel.marbach@openplace.net>
2023-12-11 16:33:50 +0100

    removing duplicated default value assignment from SortInfo and make the object more poco like to be similar to what we had before
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl.Persistence/Infrastructure/SortInfo.cs
+++ src/ServiceControl.Persistence/Infrastructure/SortInfo.cs
@@ -1,26 +1,11 @@
 ﻿namespace ServiceControl.Persistence.Infrastructure
 {
-    using System.Collections.Frozen;
-    using System.Collections.Generic;
     using System.Diagnostics;
 
     [DebuggerDisplay("{Sort} {Direction}")]
     public class SortInfo(string sort, string direction = "desc")
     {
-        public string Direction { get; } = direction is not "asc" and not "desc" ? "desc" : direction;
-        public string Sort { get; } = !AllowableSortOptions.Contains(sort) ? "time_sent" : sort;
-
-        static readonly FrozenSet<string> AllowableSortOptions = new HashSet<string>
-        {
-            "processed_at",
-            "id",
-            "message_type",
-            "time_sent",
-            "critical_time",
-            "delivery_time",
-            "processing_time",
-            "status",
-            "message_id"
-        }.ToFrozenSet();
+        public string Direction { get; } = direction;
+        public string Sort { get; } = sort;
     }
 }
