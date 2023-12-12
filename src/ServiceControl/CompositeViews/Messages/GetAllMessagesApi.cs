04b19b21f80a64391e862686e098110d2dd5e3e2
danielmarbach <daniel.marbach@openplace.net>
2023-12-12 17:58:49 +0100

    Some fiddling around

--- src/ServiceControl/CompositeViews/Messages/GetAllMessagesApi.cs
+++ src/ServiceControl/CompositeViews/Messages/GetAllMessagesApi.cs
@@ -4,17 +4,18 @@ namespace ServiceControl.CompositeViews.Messages
     using System.Collections.Generic;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
     using ServiceBus.Management.Infrastructure.Settings;
     using ServiceControl.Persistence;
     using ServiceControl.Persistence.Infrastructure;
 
     class GetAllMessagesApi : ScatterGatherApiMessageView<IErrorMessageDataStore, NoInput>
     {
-        public GetAllMessagesApi(IErrorMessageDataStore dataStore, Settings settings, Func<HttpClient> httpClientFactory) : base(dataStore, settings, httpClientFactory)
+        public GetAllMessagesApi(IErrorMessageDataStore dataStore, Settings settings, IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor) : base(dataStore, settings, httpClientFactory, httpContextAccessor)
         {
         }
 
-        protected override Task<QueryResult<IList<MessagesView>>> LocalQuery(HttpRequestMessage request, NoInput input)
+        protected override Task<QueryResult<IList<MessagesView>>> LocalQuery(NoInput input)
         {
             var pagingInfo = request.GetPagingInfo();
             var sortInfo = request.GetSortInfo();
