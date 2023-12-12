3a09fb7a1463759da6a2cd5670bc1c689dca7866
danielmarbach <daniel.marbach@openplace.net>
2023-12-12 17:58:49 +0100

    Some fiddling around

--- src/ServiceControl.Persistence/Infrastructure/QueryResult.cs
+++ src/ServiceControl.Persistence/Infrastructure/QueryResult.cs
@@ -1,30 +1,14 @@
 namespace ServiceControl.Persistence.Infrastructure
 {
-    public class QueryResult
-    {
-        protected QueryResult(object results, QueryStatsInfo queryStatsInfo)
-        {
-            DynamicResults = results;
-            QueryStats = queryStatsInfo;
-        }
-
-
-        public object DynamicResults { get; }
-        public string InstanceId { get; set; }
-
-        public QueryStatsInfo QueryStats { get; }
-    }
-
-    public class QueryResult<TOut> : QueryResult
+    public class QueryResult<TOut>(TOut results, QueryStatsInfo queryStatsInfo)
         where TOut : class
     {
-        public QueryResult(TOut results, QueryStatsInfo queryStatsInfo) : base(results, queryStatsInfo)
-        {
-            Results = results;
-        }
+        public TOut Results { get; } = results;
 
-        public TOut Results { get; }
+        public string InstanceId { get; set; }
 
-        public static QueryResult<TOut> Empty() => new QueryResult<TOut>(null, QueryStatsInfo.Zero);
+        public QueryStatsInfo QueryStats { get; } = queryStatsInfo;
+
+        public static QueryResult<TOut> Empty() => new(null, QueryStatsInfo.Zero);
     }
 }
