f76285f6a47be1572995b7bfabf908b180e6db80
danielmarbach <daniel.marbach@openplace.net>
2023-12-12 17:58:49 +0100

    Some fiddling around

--- src/ServiceControl/CompositeViews/AuditCounts/GetAuditCountsForEndpointApi.cs
+++ src/ServiceControl/CompositeViews/AuditCounts/GetAuditCountsForEndpointApi.cs
@@ -1,10 +1,10 @@
 ﻿namespace ServiceControl.CompositeViews.MessageCounting
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
     using ServiceBus.Management.Infrastructure.Settings;
     using ServiceControl.CompositeViews.Messages;
     using ServiceControl.Persistence;
@@ -12,22 +12,19 @@
 
     class GetAuditCountsForEndpointApi : ScatterGatherApi<IErrorMessageDataStore, string, IList<AuditCount>>
     {
-        static readonly IList<AuditCount> empty = new List<AuditCount>().AsReadOnly();
+        static readonly IList<AuditCount> Empty = new List<AuditCount>(0).AsReadOnly();
 
-        public GetAuditCountsForEndpointApi(IErrorMessageDataStore dataStore, Settings settings, Func<HttpClient> httpClientFactory)
-            : base(dataStore, settings, httpClientFactory)
+        public GetAuditCountsForEndpointApi(IErrorMessageDataStore dataStore, Settings settings, IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+            : base(dataStore, settings, httpClientFactory, httpContextAccessor)
         {
         }
 
-        protected override Task<QueryResult<IList<AuditCount>>> LocalQuery(HttpRequestMessage request, string input)
-        {
+        protected override Task<QueryResult<IList<AuditCount>>> LocalQuery(string input) =>
             // Will never be implemented on the primary instance
-            return Task.FromResult(new QueryResult<IList<AuditCount>>(empty, QueryStatsInfo.Zero));
-        }
+            Task.FromResult(new QueryResult<IList<AuditCount>>(Empty, QueryStatsInfo.Zero));
 
-        protected override IList<AuditCount> ProcessResults(HttpRequestMessage request, QueryResult<IList<AuditCount>>[] results)
-        {
-            return results.SelectMany(r => r.Results)
+        protected override IList<AuditCount> ProcessResults(QueryResult<IList<AuditCount>>[] results) =>
+            results.SelectMany(r => r.Results)
                 .GroupBy(r => r.UtcDate)
                 .Select(g => new AuditCount
                 {
@@ -36,6 +33,5 @@
                 })
                 .OrderBy(r => r.UtcDate)
                 .ToList();
-        }
     }
 }
