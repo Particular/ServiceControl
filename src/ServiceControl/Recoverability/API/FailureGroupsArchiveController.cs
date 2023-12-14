71c3e99ba957a724a397ce168baa1d8925fdb248
Brandon Ording <bording@gmail.com>
2023-12-14 18:05:29 -0500

    Convert remaining controllers

--- src/ServiceControl/Recoverability/API/FailureGroupsArchiveController.cs
+++ src/ServiceControl/Recoverability/API/FailureGroupsArchiveController.cs
@@ -1,28 +1,21 @@
 ﻿namespace ServiceControl.Recoverability.API
 {
-    using System.Net;
-    using System.Net.Http;
     using System.Threading.Tasks;
-    using System.Web.Http;
+    using Microsoft.AspNetCore.Mvc;
     using NServiceBus;
     using ServiceControl.Persistence.Recoverability;
 
-    class FailureGroupsArchiveController : ApiController
+    [ApiController]
+    class FailureGroupsArchiveController(IMessageSession bus, IArchiveMessages archiver) : ControllerBase
     {
-        public FailureGroupsArchiveController(IMessageSession bus, IArchiveMessages archiver)
-        {
-            this.bus = bus;
-            this.archiver = archiver;
-        }
-
-
         [Route("recoverability/groups/{groupId}/errors/archive")]
         [HttpPost]
-        public async Task<HttpResponseMessage> ArchiveGroupErrors(string groupId)
+        public async Task<IActionResult> ArchiveGroupErrors(string groupId)
         {
             if (string.IsNullOrWhiteSpace(groupId))
             {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "missing groupId");
+                //TODO compare to CreateErrorResponse that as here before
+                return BadRequest("missing groupId");
             }
 
             if (!archiver.IsOperationInProgressFor(groupId, ArchiveType.FailureGroup))
@@ -32,10 +25,7 @@
                 await bus.SendLocal<ArchiveAllInGroup>(m => { m.GroupId = groupId; });
             }
 
-            return Request.CreateResponse(HttpStatusCode.Accepted);
+            return Accepted();
         }
-
-        readonly IMessageSession bus;
-        readonly IArchiveMessages archiver;
     }
 }
