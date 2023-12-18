08d19bc6db29f2f479c6adf6be3a3101cb07c76e
danielmarbach <daniel.marbach@openplace.net>
2023-12-18 11:32:33 +0100

    Start migrating over some SignalR stuff
    Co-authored-by: Mauro Servienti <mauro.servienti@gmail.com>

--- src/ServiceControl/Infrastructure/WebApi/WebApiHostBuilderExtensions.cs
+++ src/ServiceControl/Infrastructure/WebApi/WebApiHostBuilderExtensions.cs
@@ -24,6 +24,7 @@
 
             hostBuilder.Services.AddCors(options => options.AddDefaultPolicy(Cors.GetDefaultPolicy()));
 
+            hostBuilder.Services.AddRouting();
             hostBuilder.Services.AddControllers();
             hostBuilder.Services.AddSignalR();
         }
