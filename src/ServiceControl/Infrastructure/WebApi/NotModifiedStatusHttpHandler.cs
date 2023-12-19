216b533ee70f44c519c0497bd365040f1fff6041
danielmarbach <daniel.marbach@openplace.net>
2023-12-19 17:00:25 +0100

    Fix the not modified handler
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl/Infrastructure/WebApi/NotModifiedStatusHttpHandler.cs
+++ src/ServiceControl/Infrastructure/WebApi/NotModifiedStatusHttpHandler.cs
@@ -9,55 +9,29 @@
 
     class NotModifiedStatusHttpHandler : IResultFilter
     {
-        static bool IfNoneMatch(RequestHeaders requestHeaders, ResponseHeaders responseHeaders)
-        {
-            return responseHeaders.ETag != null && requestHeaders.IfNoneMatch.Contains(responseHeaders.ETag);
-        }
+        static bool IfNoneMatch(RequestHeaders requestHeaders, ResponseHeaders responseHeaders) =>
+            responseHeaders.ETag != null && requestHeaders.IfNoneMatch.Contains(responseHeaders.ETag);
 
-        static bool IfNotModifiedSince(DateTimeOffset? ifModifiedSince, DateTimeOffset? lastModified)
-        {
-            if (lastModified == null)
-            {
-                return false;
-            }
-
-            return lastModified <= ifModifiedSince;
-        }
-
-//         // currently lastModified is not supported without returning a content which would violate the HTTP spec
-//         // it can be resurrected once ASP.NET Core is in place.
-// #pragma warning disable IDE0060 // Remove unused parameter
-//         static HttpResponseMessage Get304ResponseMessage(HttpResponseHeaders responseHeaders,
-//             DateTimeOffset? lastModified, HttpRequestMessage request)
-// #pragma warning restore IDE0060 // Remove unused parameter
-//         {
-//             var response = request.CreateResponse(HttpStatusCode.NotModified);
-//
-//             if (responseHeaders.ETag.Tag != null)
-//             {
-//                 response.Headers.ETag = responseHeaders.ETag;
-//             }
-//
-//             return response;
-//         }
+        static bool IfNotModifiedSince(DateTimeOffset? ifModifiedSince, DateTimeOffset? lastModified) =>
+            lastModified <= ifModifiedSince;
 
         public void OnResultExecuting(ResultExecutingContext context)
         {
-            // TODO how do we this?
-            // if (!context.HttpContext IsSuccessStatusCode)
-            // {
-            //     return response;
-            // }
+            var statusCode = context.HttpContext.Response.StatusCode;
+            if (statusCode is < 200 or > 299)
+            {
+                return;
+            }
 
             var requestHeaders = context.HttpContext.Request.GetTypedHeaders();
             var responseHeaders = context.HttpContext.Response.GetTypedHeaders();
 
             var ifNoneMatch = IfNoneMatch(requestHeaders, responseHeaders);
-            var ifNotModifiedSince = IfNotModifiedSince(requestHeaders.IfModifiedSince, responseHeaders?.LastModified);
+            var ifNotModifiedSince = IfNotModifiedSince(requestHeaders.IfModifiedSince, responseHeaders.LastModified);
 
             if (ifNoneMatch || ifNotModifiedSince)
             {
-                // TODO previously we wewre creating a brand new response, now we're adding more headers than before
+                // TODO previously we were creating a brand new response, now we're adding more headers than before
                 context.Result = new StatusCodeResult((int)HttpStatusCode.NotModified);
             }
         }
