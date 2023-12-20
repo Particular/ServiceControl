bfcc344cbd32e5c0a9e994b9b4b81ba1582971dc
danielmarbach <daniel.marbach@openplace.net>
2023-12-20 17:21:29 +0100

    Make the controller dependencies test compilable

--- src/ServiceControl.Persistence.Tests/ControllerDependencies.cs
+++ src/ServiceControl.Persistence.Tests/ControllerDependencies.cs
@@ -1,15 +1,13 @@
 ﻿namespace ServiceControl.Persistence.Tests
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Net.Http;
     using System.Reflection;
     using System.Threading.Tasks;
-    using System.Web.Http.Controllers;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
     using NServiceBus;
     using NUnit.Framework;
     using Particular.ServiceControl;
@@ -33,52 +31,44 @@
 
             var assembly = Assembly.GetAssembly(typeof(WebApiHostBuilderExtensions));
             var controllerTypes = assembly.DefinedTypes
-                .Where(type => typeof(IHttpController).IsAssignableFrom(type) &&
+                .Where(type => typeof(ControllerBase).IsAssignableFrom(type) &&
                                type.Name.EndsWith("Controller", StringComparison.Ordinal))
                 .ToArray();
 
-            var hostBuilder = new HostBuilder()
-                .ConfigureServices(serviceCollection =>
-                {
-                    serviceCollection.AddSingleton<Func<HttpClient>>(() => new HttpClient());
-                    serviceCollection.AddSingleton<IDomainEvents, DomainEvents>();
-                    serviceCollection.AddSingleton(new LoggingSettings("test"));
-
-                    testPersistence.Configure(serviceCollection);
-                })
-                .UseNServiceBus(_ =>
-                {
-                    var config = new EndpointConfiguration("test");
-                    config.UseTransport<LearningTransport>().StorageDirectory(Path.Combine(TestContext.CurrentContext.WorkDirectory, "DependencyTest"));
-                    return config;
-                })
-                .ConfigureServices(serviceCollection =>
-                {
-                    // This test never starts the ServiceControl instance, that means (with NServiceBus v8) that the transport is never initialized.
-                    // Some of the controllers, though, access through other dependencies the raw endpoint which tries to configure NServiceBus
-                    // The NServiceBus ReceiveComponent fails because the transport is not started and the ReceiveAddress is not properly registered
-                    // The component registration must happen AFTER UseNServiceBus because we want to override components registered by NServiceBus
-                    serviceCollection.AddSingleton(new ReceiveAddresses("mainReceiveAddress"));
-                })
-                .UseServiceControlComponents(new Settings(), ServiceControlMainInstance.Components);
+            var hostBuilder = WebApplication.CreateBuilder();
+            hostBuilder.Services.AddHttpClient();
+            hostBuilder.Services.AddHttpContextAccessor();
+            hostBuilder.Services.AddSingleton<IDomainEvents, DomainEvents>();
+            hostBuilder.Services.AddSingleton(new LoggingSettings("test"));
+            testPersistence.Configure(hostBuilder.Services);
+            hostBuilder.Host.UseNServiceBus(_ =>
+            {
+                var config = new EndpointConfiguration("test");
+                config.UseTransport<LearningTransport>()
+                    .StorageDirectory(Path.Combine(TestContext.CurrentContext.WorkDirectory, "DependencyTest"));
+                return config;
+            });
+            // This test never starts the ServiceControl instance, that means (with NServiceBus v8) that the transport is never initialized.
+            // Some of the controllers, though, access through other dependencies the raw endpoint which tries to configure NServiceBus
+            // The NServiceBus ReceiveComponent fails because the transport is not started and the ReceiveAddress is not properly registered
+            // The component registration must happen AFTER UseNServiceBus because we want to override components registered by NServiceBus
+            hostBuilder.Services.AddSingleton(new ReceiveAddresses("mainReceiveAddress"));
+            hostBuilder.UseServiceControlComponents(new Settings(errorRetentionPeriod: TimeSpan.FromDays(7), forwardErrorMessages: false), ServiceControlMainInstance.Components);
+            hostBuilder.UseWebApi([assembly], string.Empty, false);
 
             // Act
-            using (var host = hostBuilder
-                       .UseWebApi([assembly], string.Empty, false)
-                       .Build())
+            await using var host = hostBuilder.Build();
+            await host.Services.GetRequiredService<IPersistenceLifecycle>().Initialize();
+
+            // Assert
+            Assert.That(host, Is.Not.Null);
+
+            // Make sure the list isn't suddenly empty
+            Assert.That(controllerTypes.Length, Is.GreaterThan(10));
+            foreach (var controllerType in controllerTypes)
             {
-                await host.Services.GetRequiredService<IPersistenceLifecycle>().Initialize();
-
-                // Assert
-                Assert.That(host, Is.Not.Null);
-
-                // Make sure the list isn't suddenly empty
-                Assert.That(controllerTypes.Length, Is.GreaterThan(10));
-                foreach (var controllerType in controllerTypes)
-                {
-                    Console.WriteLine($"Getting service {controllerType.FullName}");
-                    Assert.That(host.Services.GetService(controllerType), Is.Not.Null);
-                }
+                Console.WriteLine($"Getting service {controllerType.FullName}");
+                Assert.That(host.Services.GetService(controllerType), Is.Not.Null);
             }
         }
     }
