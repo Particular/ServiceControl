7709307e89851861185b379355d371c47285208e
Brandon Ording <bording@gmail.com>
2023-12-22 15:40:09 -0500

    Don't try to load MSMQ transport

--- src/ServiceControl.Transports/DevelopmentTransportLocations.cs
+++ src/ServiceControl.Transports/DevelopmentTransportLocations.cs
@@ -20,7 +20,7 @@
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.ASBS"));
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.ASQ"));
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.Learning"));
-                ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.Msmq"));
+                //ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.Msmq")); TODO Uncomment when we add MSMQ back in
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.RabbitMQ"));
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.SqlServer"));
                 ManifestFiles.Add(BuildManifestPath(srcFolder, "ServiceControl.Transports.SQS"));
