e226df50dcbdfda34bdc14c6612e7a3e2d1552d3
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2024-01-05 15:46:53 -0800

    Add keydownevent  to handle indeterminate state

--- src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
+++ src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
@@ -1,10 +1,23 @@
 ﻿namespace ServiceControl.Config.UI.InstanceAdd
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     public partial class ServiceControlAddView
     {
         public ServiceControlAddView()
         {
             InitializeComponent();
+        }
+        public void CheckBoxError_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+            chk.Indeterminate += (sender, e) => chk.IsChecked = false;
+        }
+        public void CheckBoxAudit_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+            chk.Indeterminate += (sender, e) => chk.IsChecked = false;
         }
     }
 }
