ece93bed8fdceecf034bfac83cb6bc6818463e07
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2024-01-05 20:44:23 -0800

    handle the checked event

--- src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
+++ src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
@@ -11,10 +11,13 @@
         }
         public void CheckBoxError_Indeterminate(object sender, RoutedEventArgs e)
         {
-            var chk = sender as CheckBox;
-            chk.Indeterminate += (sender, e) => chk.IsChecked = false;
+            CheckBox_Indeterminate(sender, e);
         }
         public void CheckBoxAudit_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            CheckBox_Indeterminate(sender, e);
+        }
+        public void CheckBox_Indeterminate(object sender, RoutedEventArgs e)
         {
             var chk = sender as CheckBox;
             chk.Indeterminate += (sender, e) => chk.IsChecked = false;
