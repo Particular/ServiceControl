ae4b6d029be0488ea874a7cc49454817afc09162
Jayanthi <jayanthi.sourirajan@particular.net>
2024-01-09 15:38:56 -0800

    enable  three state attribute in checkbox (#3907)
    * enable  three state attribute in checkbox
    ---------
    
    Co-authored-by: Kyle Baley <kyle@baley.org>

--- src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
+++ src/ServiceControl.Config/UI/InstanceAdd/ServiceControlAddView.xaml.cs
@@ -1,10 +1,26 @@
 ﻿namespace ServiceControl.Config.UI.InstanceAdd
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     public partial class ServiceControlAddView
     {
         public ServiceControlAddView()
         {
             InitializeComponent();
+        }
+        public void CheckBoxError_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            CheckBox_Indeterminate(sender, e);
+        }
+        public void CheckBoxAudit_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            CheckBox_Indeterminate(sender, e);
+        }
+        public void CheckBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+            chk.Indeterminate += (sender, e) => chk.IsChecked = false;
         }
     }
 }
