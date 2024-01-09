945a61085d01953423b1f79be99222918688936b
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-09 11:23:02 +0100

    Move the responsibility of starting the instance to the Bootstrapper

--- src/ServiceControl/Hosting/Commands/RunCommand.cs
+++ src/ServiceControl/Hosting/Commands/RunCommand.cs
@@ -1,13 +1,7 @@
 ﻿namespace Particular.ServiceControl.Commands
 {
     using System.Threading.Tasks;
-    using global::ServiceControl.Infrastructure.OWIN;
-    using global::ServiceControl.Infrastructure.SignalR;
-    using global::ServiceControl.Persistence;
     using Hosting;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
     using NServiceBus;
     using ServiceBus.Management.Infrastructure.Settings;
 
@@ -25,22 +19,7 @@
             var loggingSettings = new LoggingSettings(args.ServiceName);
 
             var bootstrapper = new Bootstrapper(settings, endpointConfiguration, loggingSettings);
-            var hostBuilder = bootstrapper.HostBuilder;
-
-            using var app = hostBuilder.Build();
-
-            // TODO move these into central class to re-use?
-            app.UseResponseCompression();
-            app.UseMiddleware<BodyUrlRouteFix>();
-            app.UseMiddleware<LogApiCalls>();
-            app.MapHub<MessageStreamerHub>("/api/messagestream");
-            app.UseCors();
-            app.UseRouting();
-            app.MapControllers();
-
-            // Initialized IDocumentStore, this is needed as many hosted services have (indirect) dependencies on it.
-            await app.Services.GetRequiredService<IPersistenceLifecycle>().Initialize();
-            await app.RunAsync(settings.RootUrl);
+            await bootstrapper.Boot();
         }
     }
 }
