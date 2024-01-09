9a98c5adfe18380454c68f81946f55a95f1d27ac
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-09 11:23:24 +0100

    More TODOs

--- src/ServiceControl/Hosting/Commands/SetupCommand.cs
+++ src/ServiceControl/Hosting/Commands/SetupCommand.cs
@@ -9,6 +9,7 @@
         public override async Task Execute(HostArguments args, Settings settings)
         {
             settings.SkipQueueCreation = args.SkipQueueCreation;
+            // TODO: Update to use the same pattern as the main Bootstrapper
             await new SetupBootstrapper(settings).Run();
         }
     }
