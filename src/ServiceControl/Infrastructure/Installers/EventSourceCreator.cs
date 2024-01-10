5651815923e91ce266920a61e7f4c59f777bbc24
danielmarbach <daniel.marbach@openplace.net>
2024-01-10 17:49:03 +0100

    Added the application parts into the acceptance test to make sure all controllers are properly discovered
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl/Infrastructure/Installers/EventSourceCreator.cs
+++ src/ServiceControl/Infrastructure/Installers/EventSourceCreator.cs
@@ -1,13 +1,13 @@
-﻿using System.Runtime.InteropServices;
-
-namespace ServiceBus.Management.Infrastructure.Installers
+﻿namespace ServiceBus.Management.Infrastructure.Installers
 {
+    using System.Runtime.InteropServices;
     using System.Diagnostics;
 
     public static class EventSourceCreator
     {
         public static void Create()
         {
+            // TODO: Figure a way to achieve something but in the linux way
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !EventLog.SourceExists(SourceName))
             {
                 EventLog.CreateEventSource(SourceName, null);
