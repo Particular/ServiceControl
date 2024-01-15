1f9917e7039ecc81c2227c436ee162ef793c0085
Ramon Smits <ramon.smits@gmail.com>
2024-01-15 17:48:45 +0100

    Using custom lifecycle(s) that invoke `IPersistenceLifecycle.Initialize()` as part of `IHostLifetime.WaitForStartAsync(..)` so that the application will not timeout in the (Windows) Service Control Manager and kill the process.

---
+++ src/ServiceControl/Hosting/Commands/CustomLivetimes/HostBuilderLifetimeExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Particular.ServiceControl.Commands
+{
+    using global::ServiceControl.Persistence;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Hosting.Internal;
+    using Microsoft.Extensions.Hosting.WindowsServices;
+
+    static class HostBuilderLifetimeExtensions
+    {
+        public static void SetupLifetime(this IHostBuilder hostBuilder, bool runAsWindowsService)
+        {
+            if (runAsWindowsService)
+            {
+                hostBuilder.UseWindowsService();
+                hostBuilder.ConfigureServices(s =>
+                {
+                    s.AddSingleton<WindowsServiceLifetime>();
+                    s.AddSingleton<IHostLifetime>(b => new PersisterInitializingLifetimeWrapper(b.GetRequiredService<IPersistenceLifecycle>(), b.GetRequiredService<WindowsServiceLifetime>()));
+                });
+            }
+            else
+            {
+                hostBuilder.UseConsoleLifetime();
+                hostBuilder.ConfigureServices(s =>
+                {
+                    s.AddSingleton<ConsoleLifetime>();
+                    s.AddSingleton<IHostLifetime>(b => new PersisterInitializingLifetimeWrapper(b.GetRequiredService<IPersistenceLifecycle>(), b.GetRequiredService<ConsoleLifetime>()));
+                });
+            }
+        }
+    }
+}
---
+++ src/ServiceControl/Hosting/Commands/CustomLivetimes/PersisterInitializingLifetimeWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using ServiceControl.Persistence;
+
+
+sealed class PersisterInitializingLifetimeWrapper : IHostLifetime
+{
+    readonly IPersistenceLifecycle persistenceLifecycle;
+    readonly IHostLifetime implementation;
+
+    public PersisterInitializingLifetimeWrapper(
+        IPersistenceLifecycle persistenceLifecycle,
+        IHostLifetime implementation
+        )
+    {
+        this.persistenceLifecycle = persistenceLifecycle;
+        this.implementation = implementation;
+    }
+
+    public async Task WaitForStartAsync(CancellationToken cancellationToken)
+    {
+        await implementation.WaitForStartAsync(cancellationToken);
+        await persistenceLifecycle.Initialize(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => implementation.StopAsync(cancellationToken);
+}
