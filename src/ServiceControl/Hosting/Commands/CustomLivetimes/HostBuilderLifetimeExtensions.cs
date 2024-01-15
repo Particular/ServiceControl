9b745a8fc74a1f8bcc167fa08c12f792c82a20ca
Ramon Smits <ramon.smits@gmail.com>
2024-01-15 17:48:45 +0100

    Using custom lifecycle(s) that invoke `IPersistenceLifecycle.Initialize()` as part of `IHostLifetime.WaitForStartAsync(..)` so that the application will not timeout in the (Windows) Service Control Manager and kill the process.

---
+++ src/ServiceControl/Hosting/Commands/CustomLivetimes/HostBuilderLifetimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Particular.ServiceControl.Commands
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    static class HostBuilderLifetimeExtensions
+    {
+        public static void SetupLifetime(this IHostBuilder hostBuilder, bool runAsWindowsService)
+        {
+            if (runAsWindowsService)
+            {
+                hostBuilder.UseWindowsService();
+
+                hostBuilder.ConfigureServices(s =>
+                {
+                    s.AddSingleton<IHostLifetime, PersisterInitializingWindowsServiceLifetime>();
+                });
+            }
+            else
+            {
+                hostBuilder.UseConsoleLifetime();
+
+                hostBuilder.ConfigureServices(s =>
+                {
+                    s.AddSingleton<IHostLifetime, PersisterInitializingConsoleLifetime>();
+                });
+            }
+        }
+    }
+}
