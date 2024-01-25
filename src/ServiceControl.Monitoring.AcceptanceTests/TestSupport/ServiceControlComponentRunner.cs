f470391f6ecbbfcfc409a0bd33328f6e4eaa606f
Daniel Marbach <daniel.marbach@openplace.net>
2024-01-25 16:35:52 +0000

    Discover the controllers in the acceptance tests
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl.Monitoring.AcceptanceTests/TestSupport/ServiceControlComponentRunner.cs
+++ src/ServiceControl.Monitoring.AcceptanceTests/TestSupport/ServiceControlComponentRunner.cs
@@ -140,6 +140,12 @@ namespace ServiceControl.Monitoring.AcceptanceTests.TestSupport
                 // This facilitates receiving the test server anywhere where DI is available
                 hostBuilder.Services.AddSingleton(provider => (TestServer)provider.GetRequiredService<IServer>());
 
+                // By default ASP.NET Core uses entry point assembly to discover controllers from. When running
+                // inside a test runner the runner exe becomes the entry point which obviously has no controllers in it ;)
+                // so we are explicitly registering all necessary application parts.
+                var addControllers = hostBuilder.Services.AddControllers();
+                addControllers.AddApplicationPart(typeof(WebApplicationBuilderExtensions).Assembly);
+
                 host = hostBuilder.Build();
                 host.UseServiceControlMonitoring();
                 await host.StartAsync();
