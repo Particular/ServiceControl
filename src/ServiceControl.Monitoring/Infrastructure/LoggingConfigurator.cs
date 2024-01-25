3d39ce9010ba0d6ae17b799d32886adf98a29245
Daniel Marbach <daniel.marbach@openplace.net>
2024-01-25 15:41:36 +0000

    Use self instead of bootstrapper
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl.Monitoring/Infrastructure/LoggingConfigurator.cs
+++ src/ServiceControl.Monitoring/Infrastructure/LoggingConfigurator.cs
@@ -23,7 +23,7 @@
                 return;
             }
 
-            var version = FileVersionInfo.GetVersionInfo(typeof(Bootstrapper).Assembly.Location).ProductVersion;
+            var version = FileVersionInfo.GetVersionInfo(typeof(LoggingConfigurator).Assembly.Location).ProductVersion;
             var nlogConfig = new LoggingConfiguration();
             var simpleLayout = new SimpleLayout("${longdate}|${threadid}|${level}|${logger}|${message}${onexception:|${exception:format=tostring}}");
             var header = $@"-------------------------------------------------------------
