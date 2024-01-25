4d3e4997d2b438c9db8f1bf7f4e841e2e3c541d1
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-25 16:03:18 +0100

    Align the startup to the primary instance
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

---
+++ src/ServiceControl.Monitoring/WebApplicationExtensions.cs
@@ -0,0 +1,25 @@
+namespace ServiceControl.Monitoring.Infrastructure;
+
+using Microsoft.AspNetCore.Builder;
+using OWIN;
+
+public static class WebApplicationExtensions
+{
+    public static void UseServiceControlMonitoring(this WebApplication appBuilder)
+    {
+        appBuilder.UseMiddleware<LogApiCalls>();
+
+        appBuilder.UseCors(policyBuilder =>
+        {
+            // TODO verify that the default is no headers and no methods allowed
+            //builder.AllowAnyHeader();
+            //builder.AllowAnyMethod();
+            policyBuilder.AllowAnyOrigin();
+            policyBuilder.WithExposedHeaders(["ETag", "Last-Modified", "Link", "Total-Count", "X-Particular-Version"]);
+            policyBuilder.WithHeaders(["Origin", "X-Requested-With", "Content-Type", "Accept"]);
+            policyBuilder.WithMethods(["POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH"]);
+        });
+
+        appBuilder.MapControllers();
+    }
+}
