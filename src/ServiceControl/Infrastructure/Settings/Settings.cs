bca6e97baa80bfcfcea2b15062bbcb18a1c390d4
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-26 09:33:41 +0100

    Revert changes to parsing logic
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- src/ServiceControl/Infrastructure/Settings/Settings.cs
+++ src/ServiceControl/Infrastructure/Settings/Settings.cs
@@ -357,11 +357,8 @@ namespace ServiceBus.Management.Infrastructure.Settings
             return Array.Empty<RemoteInstanceSetting>();
         }
 
-        internal static RemoteInstanceSetting[] ParseRemoteInstances(string value)
-        {
-            var sanitizedValue = value.Replace("'", "\"");
-            return JsonSerializer.Deserialize<RemoteInstanceSetting[]>(sanitizedValue, SerializerOptions.Default) ?? [];
-        }
+        internal static RemoteInstanceSetting[] ParseRemoteInstances(string value) =>
+            JsonSerializer.Deserialize<RemoteInstanceSetting[]>(value, SerializerOptions.Default) ?? [];
 
         static string Subscope(string address)
         {
