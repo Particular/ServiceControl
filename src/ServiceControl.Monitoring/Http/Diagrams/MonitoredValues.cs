2ab5e9185cf714c9cb69ea61c77823d6769cba5c
Mauro Servienti <mauro.servienti@gmail.com>
2024-01-26 10:19:32 +0100

    make System.Text.Json even happier
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- src/ServiceControl.Monitoring/Http/Diagrams/MonitoredValues.cs
+++ src/ServiceControl.Monitoring/Http/Diagrams/MonitoredValues.cs
@@ -1,7 +1,9 @@
 namespace ServiceControl.Monitoring.Http.Diagrams
 {
     using System;
+    using System.Text.Json.Serialization;
 
+    [JsonDerivedType(typeof(MonitoredValuesWithTimings))]
     public class MonitoredValues
     {
         public double? Average { get; set; }
