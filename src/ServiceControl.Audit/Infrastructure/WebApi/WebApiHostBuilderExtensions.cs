29626c9df665d2c1ed8e7aa213ead39ed2ab2281
danielmarbach <daniel.marbach@openplace.net>
2024-01-26 18:00:19 +0100

    Convert large portion of Api usages into direct controller stuff
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl.Audit/Infrastructure/WebApi/WebApiHostBuilderExtensions.cs
+++ src/ServiceControl.Audit/Infrastructure/WebApi/WebApiHostBuilderExtensions.cs
@@ -1,10 +1,5 @@
 ﻿namespace ServiceControl.Audit.Infrastructure.WebApi
 {
-    using System;
-    using System.Linq;
-    using System.Reflection;
-    using System.Web.Http.Controllers;
-    using Auditing.MessagesView;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using OWIN;
@@ -13,20 +8,6 @@
     {
         public static IHostBuilder UseWebApi(this IHostBuilder hostBuilder, string rootUrl, bool startOwinHost)
         {
-            hostBuilder.ConfigureServices(services =>
-            {
-                RegisterInternalWebApiControllers(services);
-                var apiTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(IApi).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
-                foreach (var apiType in apiTypes)
-                {
-                    services.AddTransient(apiType);
-                    foreach (var i in apiType.GetInterfaces())
-                    {
-                        services.AddTransient(i, sp => sp.GetRequiredService(apiType));
-                    }
-                }
-            });
-
             if (startOwinHost)
             {
                 hostBuilder.ConfigureServices((ctx, serviceCollection) =>
@@ -40,17 +21,6 @@
             }
 
             return hostBuilder;
-        }
-
-        static void RegisterInternalWebApiControllers(IServiceCollection serviceCollection)
-        {
-            var controllerTypes = Assembly.GetExecutingAssembly().DefinedTypes
-                .Where(t => typeof(IHttpController).IsAssignableFrom(t) && t.Name.EndsWith("Controller", StringComparison.Ordinal));
-
-            foreach (var controllerType in controllerTypes)
-            {
-                serviceCollection.AddScoped(controllerType);
-            }
         }
     }
 }
--- src/ServiceControl.Audit/Monitoring/KnownEndpoints/KnownEndpointsController.cs
+++ src/ServiceControl.Audit/Monitoring/KnownEndpoints/KnownEndpointsController.cs
@@ -1,19 +1,23 @@
 namespace ServiceControl.Audit.Monitoring
 {
-    using System.Net.Http;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Infrastructure;
+    using Infrastructure.WebApi;
     using Microsoft.AspNetCore.Mvc;
+    using Persistence;
 
     [ApiController]
     [Route("api")]
-    public class KnownEndpointsController : ControllerBase
+    public class KnownEndpointsController(IAuditDataStore dataStore) : ControllerBase
     {
-        public KnownEndpointsController(GetKnownEndpointsApi knownEndpointsApi) => getKnownEndpointsApi = knownEndpointsApi;
-
         [Route("endpoints/known")]
         [HttpGet]
-        public Task<HttpResponseMessage> GetAll() => getKnownEndpointsApi.Execute(this);
-
-        readonly GetKnownEndpointsApi getKnownEndpointsApi;
+        public async Task<IList<KnownEndpointsView>> GetAll([FromQuery] PagingInfo pagingInfo)
+        {
+            var result = await dataStore.QueryKnownEndpoints();
+            Response.WithQueryResults(result.QueryStats, pagingInfo);
+            return result.Results;
+        }
     }
 }
