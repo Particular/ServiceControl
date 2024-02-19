7d333d4f41d70c4fd9df3519b85764cb17f64680
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-19 16:52:59 +1100

    Add initial throughput persistence library

---
+++ src/Throughput.Persistence/IPersistenceInstaller.cs
@@ -0,0 +1,9 @@
+﻿namespace Throughput.Persistence;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+public interface IPersistenceInstaller
+{
+    Task Install(CancellationToken cancellationToken = default);
+}
