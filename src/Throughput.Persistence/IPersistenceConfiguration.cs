16e20ca79c0fbfd1fe0a50e504b3cd6d22f3ecde
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-19 16:52:59 +1100

    Add initial throughput persistence library

---
+++ src/Throughput.Persistence/IPersistenceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Throughput.Persistence;
+
+using System.Collections.Generic;
+
+public interface IPersistenceConfiguration
+{
+    string Name { get; }
+
+    IEnumerable<string> ConfigurationKeys { get; }
+
+    IPersistence Create(PersistenceSettings settings);
+}
