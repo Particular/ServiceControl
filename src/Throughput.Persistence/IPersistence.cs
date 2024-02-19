c7cc0e5efdf685652590377126df5be5331443c0
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-19 16:52:59 +1100

    Add initial throughput persistence library

---
+++ src/Throughput.Persistence/IPersistence.cs
@@ -0,0 +1,9 @@
+﻿namespace Throughput.Persistence;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public interface IPersistence
+{
+    IPersistenceLifecycle Configure(IServiceCollection serviceCollection);
+    IPersistenceInstaller CreateInstaller();
+}
