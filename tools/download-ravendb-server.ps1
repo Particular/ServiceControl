b3f0b9576fdb4a4195a979a9cd8500e023732d2d
Daniel Marbach <daniel.marbach@openplace.net>
2024-02-19 14:13:26 +0100

    Download Linux specific binaries

--- tools/download-ravendb-server.ps1
+++ tools/download-ravendb-server.ps1
@@ -8,21 +8,36 @@
 # artifact during development, but has a condition to skip that step on a CI server so that the
 # self-contained version is used instead.
 
+$runnerOs = $Env:RUNNER_OS ?? "Windows"
 $version = (Select-Xml -Path src/Directory.Packages.props -XPath "/Project/ItemGroup/PackageVersion[@Include='RavenDB.Embedded']/@Version" | Select-Object -ExpandProperty Node).Value
 Write-Output "In Directory.Packages.props, RavenDB.Embedded is using version '$version'"
-
-$downloadUrl = "https://daily-builds.s3.amazonaws.com/RavenDB-$($version)-windows-x64.zip"
 $tempPath = [System.IO.Path]::GetTempPath()
+
+# Default values assume windows
 $zipPath = Join-Path $tempPath "ravendb.zip"
+$unzipPath = Join-Path $tempPath "ravendb-extracted"
+$downloadUrl = "https://daily-builds.s3.amazonaws.com/RavenDB-$($version)-windows-x64.zip"
+$serverPath = Join-Path $unzipPath "Server"
+
+if (Test-Path $unzipPath) { Remove-Item $unzipPath -Force -Recurse }
+New-Item -ItemType Directory -Path $unzipPath
+
+if ($runnerOs -eq "Linux") {
+  $downloadUrl = "https://daily-builds.s3.amazonaws.com/RavenDB-$($version)-linux-x64.tar.bz2"
+  $zipPath = Join-Path $tempPath "ravendb.tar.bz2"
+  $serverPath = Join-Path $unzipPath "RavenDB/Server"
+}
+
 Write-Output "Downloading RavenDB binaries from $downloadUrl to $zipPath"
 Invoke-WebRequest $downloadUrl -OutFile $zipPath
 
-Write-Output "Unzipping archive..."
-$unzipPath = Join-Path $tempPath "ravendb-extracted"
-if (Test-Path $unzipPath) { Remove-Item $unzipPath -Force -Recurse }
-Expand-Archive $zipPath $unzipPath
-
-$serverPath = Join-Path $unzipPath "Server"
+Write-Output "Extracting archive..."
+if ($runnerOs -eq "Linux") {
+  tar -jxf $zipPath -C $unzipPath
+}
+else {
+  Expand-Archive $zipPath $unzipPath
+}
 $deployPath = Join-Path $PWD.Path deploy RavenDBServer
 if (Test-Path $deployPath ) { Remove-Item $deployPath -Force -Recurse }
 Write-Output "Copying '$serverPath' to '$deployPath'"
