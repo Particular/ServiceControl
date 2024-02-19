c8589c47533c9f12a9ebdc2f4e2a2f774c402f05
Jo Palac <j_palac@hotmail.com>
2024-02-19 14:33:54 +1000

    Started

--- src/ServiceControl/WebApplicationBuilderExtension.cs
+++ src/ServiceControl/WebApplicationBuilderExtension.cs
@@ -124,6 +124,8 @@ namespace Particular.ServiceControl
             }
 
             hostBuilder.AddServiceControlComponents(settings, ServiceControlMainInstance.Components);
+
+            hostBuilder.AddThroughputCollector();
         }
 
         static TransportSettings MapSettings(Settings settings)
