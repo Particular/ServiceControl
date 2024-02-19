dbfe4007790b153c0af44c3bf300e54f26850467
Jo Palac <j_palac@hotmail.com>
2024-02-19 18:11:16 +1000

    move extension to license project

---
+++ src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Particular.License
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Particular.License.Contracts;
+
+    public static class ThroughputCollectorHostBuilderExtensions
+    {
+        public static IHostApplicationBuilder AddThroughputCollector(this IHostApplicationBuilder hostBuilder, string broker, string serviceControlAPI, string auditQueue, string errorQueue)
+        {
+            var services = hostBuilder.Services;
+            services.AddSingleton(new LicenseData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue });
+            services.AddHostedService<ThroughputCollectorHostedService>();
+            return hostBuilder;
+        }
+    }
+}
