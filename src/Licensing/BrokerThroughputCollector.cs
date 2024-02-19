d5c6ab293ccc49cf8f427c65895d7c7af874a2c2
Jo Palac <j_palac@hotmail.com>
2024-02-19 14:33:54 +1000

    Started

---
+++ src/Licensing/BrokerThroughputCollector.cs
@@ -0,0 +1,21 @@
+﻿namespace Particular.License
+{
+    using Microsoft.Extensions.Logging;
+
+    class BrokerThroughputCollector
+    {
+        public BrokerThroughputCollector(string broker, ILogger logger)
+        {
+            this.broker = broker;
+            this.logger = logger;
+        }
+
+        public void GatherThroughput()
+        {
+            logger.LogInformation($"Gathering throughput from broker {broker}");
+        }
+
+        readonly string broker;
+        readonly ILogger logger;
+    }
+}
