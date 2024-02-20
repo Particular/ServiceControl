bf7084460e878c8205ab09090c013e0d5124dc49
Jo Palac <j_palac@hotmail.com>
2024-02-20 10:29:24 +1000

    added required settings

---
+++ src/Licensing/Contracts/PlatformData.cs
@@ -0,0 +1,12 @@
+﻿namespace Particular.License.Contracts
+{
+    class PlatformData
+    {
+        public required string ServiceControlAPI { get; set; }
+        public required string Broker { get; set; }
+        public required string ErrorQueue { get; set; }
+        public required string AuditQueue { get; set; } //NOTE can we get this?
+        public required string TransportConnectionString { get; set; }
+        public required string PersistenceType { get; set; }
+    }
+}
