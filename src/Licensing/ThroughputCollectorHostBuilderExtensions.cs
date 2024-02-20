1d5ceb2c1bd3dd610cfb46849ffb625995862a28
Jo Palac <j_palac@hotmail.com>
2024-02-20 15:19:57 +1000

    injecting logging

--- src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
+++ src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
     using Particular.License.Contracts;
 
     public static class ThroughputCollectorHostBuilderExtensions
@@ -9,8 +10,8 @@
         public static IHostApplicationBuilder AddThroughputCollector(this IHostApplicationBuilder hostBuilder, string broker, string serviceControlAPI, string errorQueue, string auditQueue, string transportConnectionString, string persistenceType)
         {
             var services = hostBuilder.Services;
-            var platformData = new PlatformData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue, TransportConnectionString = transportConnectionString, PersistenceType = persistenceType };
-            services.AddHostedService(serviceProvider => new ThroughputCollectorHostedService(platformData));
+            services.AddHostedService(provider => new ThroughputCollectorHostedService(provider.GetRequiredService<ILoggerFactory>(),
+                                                                                        new PlatformData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue, TransportConnectionString = transportConnectionString, PersistenceType = persistenceType }));
             return hostBuilder;
         }
     }
