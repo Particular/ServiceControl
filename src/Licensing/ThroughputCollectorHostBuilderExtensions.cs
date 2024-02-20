ed42c42301f9a4dde1b4798474a842ebf85cf254
Jo Palac <j_palac@hotmail.com>
2024-02-20 10:29:24 +1000

    added required settings

--- src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
+++ src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
@@ -6,11 +6,11 @@
 
     public static class ThroughputCollectorHostBuilderExtensions
     {
-        public static IHostApplicationBuilder AddThroughputCollector(this IHostApplicationBuilder hostBuilder, string broker, string serviceControlAPI, string auditQueue, string errorQueue)
+        public static IHostApplicationBuilder AddThroughputCollector(this IHostApplicationBuilder hostBuilder, string broker, string serviceControlAPI, string errorQueue, string auditQueue, string transportConnectionString, string persistenceType)
         {
             var services = hostBuilder.Services;
-            services.AddSingleton(new LicenseData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue });
-            services.AddHostedService<ThroughputCollectorHostedService>();
+            var platformData = new PlatformData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue, TransportConnectionString = transportConnectionString, PersistenceType = persistenceType };
+            services.AddHostedService(serviceProvider => new ThroughputCollectorHostedService(platformData));
             return hostBuilder;
         }
     }
