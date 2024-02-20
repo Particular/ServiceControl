89cb70d7b93daa495388edd1ed5884cf80ad4f2c
Jo Palac <j_palac@hotmail.com>
2024-02-20 15:19:57 +1000

    injecting logging

---
+++ src/Licensing/MonitoringThroughputCollector.cs
@@ -0,0 +1,20 @@
+﻿namespace Particular.License
+{
+    using Microsoft.Extensions.Logging;
+
+    class MonitoringThroughputCollector
+    {
+        public MonitoringThroughputCollector(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void GatherThroughput()
+        {
+            //This may be called as part of the main instance receiving data from monitoring
+            logger.LogInformation($"Gathering throughput from Monitoring/");
+        }
+
+        readonly ILogger logger;
+    }
+}
