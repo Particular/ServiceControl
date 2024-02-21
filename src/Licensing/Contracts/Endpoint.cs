b9bf0700fa2faf696d7654fe470c95355b3e68b2
Jo Palac <j_palac@hotmail.com>
2024-02-21 13:30:25 +1000

    added endpoint data struct

---
+++ src/Licensing/Contracts/Endpoint.cs
@@ -0,0 +1,34 @@
+﻿namespace Particular.License.Contracts
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class Endpoint
+    {
+        public Endpoint(string name, string queue, ThroughputSource throughputSource)
+        {
+            Name = name;
+            Queue = queue;
+            ThroughputSource = throughputSource;
+            DailyThroughput = [];
+        }
+
+        public string Name { get; set; }
+        public string Queue { get; set; }
+        public ThroughputSource ThroughputSource { get; set; }
+        public string Scope { get; set; } //used for SQLServer endpoints, perhaps for others too
+        public string[] EndpointIndicators { get; set; }
+        public List<Throughput> DailyThroughput { get; set; }
+    }
+
+    public class Throughput
+    {
+        public DateTime Date { get; set; }
+        public int TotalThroughput { get; set; }
+    }
+
+    public enum ThroughputSource
+    {
+        Broker,
+        Monitoring,
+        Audit
+    }
+}
