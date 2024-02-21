b1057a65575004deb5e6a74615310a8799d9912a
Jo Palac <j_palac@hotmail.com>
2024-02-21 11:24:34 +1000

    split to 3 hosted services

--- src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
+++ src/Licensing/ThroughputCollectorHostBuilderExtensions.cs
@@ -2,16 +2,22 @@
 {
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
     using Particular.License.Contracts;
+    using Particular.License.Throughput.Audit;
+    using Particular.License.Throughput.Broker;
 
     public static class ThroughputCollectorHostBuilderExtensions
     {
         public static IHostApplicationBuilder AddThroughputCollector(this IHostApplicationBuilder hostBuilder, string broker, string serviceControlAPI, string errorQueue, string auditQueue, string transportConnectionString, string persistenceType)
         {
             var services = hostBuilder.Services;
-            services.AddHostedService(provider => new ThroughputCollectorHostedService(provider.GetRequiredService<ILoggerFactory>(),
-                                                                                        new PlatformData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue, TransportConnectionString = transportConnectionString, PersistenceType = persistenceType }));
+            services.AddSingleton(new PlatformData { Broker = broker, ServiceControlAPI = serviceControlAPI, AuditQueue = auditQueue, ErrorQueue = errorQueue, TransportConnectionString = transportConnectionString, PersistenceType = persistenceType });
+            services.AddHostedService<AuditThroughputCollectorHostedService>();
+            services.AddHostedService<BrokerThroughputCollectorHostedService>();
+
+            //TODO add relevant persistence here, based on passed in persistenceType.
+            //Will probably also need to initialize it as part of the setup/install process of the instance
+
             return hostBuilder;
         }
     }
