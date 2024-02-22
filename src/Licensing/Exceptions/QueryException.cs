65a5245e0a8ce4d05f4cddcb07462bb18ff04969
Jo Palac <j_palac@hotmail.com>
2024-02-22 11:56:22 +1000

    started adding audit

---
+++ src/Licensing/Exceptions/QueryException.cs
@@ -0,0 +1,16 @@
+﻿
+public class QueryException : ApplicationException
+{
+    public QueryFailureReason Reason { get; set; }
+
+    public QueryException(QueryFailureReason reason, string message, Exception? innerException = null) : base(message, innerException)
+    {
+        Reason = reason;
+    }
+}
+
+public enum QueryFailureReason
+{
+    Auth,
+    InvalidEnvironment
+}
