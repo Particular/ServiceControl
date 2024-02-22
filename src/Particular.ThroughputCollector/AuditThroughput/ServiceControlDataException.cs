04f201c74231f66c5a242504caa639ed25be4b8d
Jo Palac <j_palac@hotmail.com>
2024-02-22 11:56:22 +1000

    started adding audit

---
+++ src/Particular.ThroughputCollector/AuditThroughput/ServiceControlDataException.cs
@@ -0,0 +1,17 @@
+﻿namespace Particular.License.Throughput.Audit
+{
+    using System;
+
+    class ServiceControlDataException : Exception
+    {
+        public string Url { get; }
+        public int Attempts { get; }
+
+        public ServiceControlDataException(string url, int tryCount, Exception inner)
+            : base(inner.Message + $" (Attempted operation {tryCount} total times)", inner)
+        {
+            Url = url;
+            Attempts = tryCount;
+        }
+    }
+}
