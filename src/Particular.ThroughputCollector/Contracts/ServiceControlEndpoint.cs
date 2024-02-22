f165e6acc1c30bfece788c1bba2cd5b947b3e664
Jo Palac <j_palac@hotmail.com>
2024-02-22 16:23:35 +1000

    audit progress

---
+++ src/Particular.ThroughputCollector/Contracts/ServiceControlEndpoint.cs
@@ -0,0 +1,14 @@
+﻿namespace Particular.ThroughputCollector.Contracts
+{
+    using System.Net;
+
+    class ServiceControlEndpoint
+    {
+        public string Name { get; set; } = "";
+        public string UrlName => WebUtility.UrlEncode(Name);
+        public bool HeartbeatsEnabled { get; set; }
+        public bool ReceivingHeartbeats { get; set; }
+        public bool CheckHourlyAuditDataIfNoMonitoringData { get; set; }
+        public AuditCount[] AuditCounts { get; set; } = Array.Empty<AuditCount>();
+    }
+}
