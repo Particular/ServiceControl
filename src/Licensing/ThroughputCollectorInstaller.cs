44d9edeb58c7e6caf6b1b9d574a2129a19d3eb91
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-22 11:38:20 +1100

    Register installation task for throughput collector

---
+++ src/Licensing/ThroughputCollectorInstaller.cs
@@ -0,0 +1,16 @@
+﻿namespace Particular.License;
+
+using System.Threading.Tasks;
+
+public static class ThroughputCollectorInstaller
+{
+    public static async Task Install(string persistenceType)
+    {
+        var persistenceConfiguration = PersistenceConfigurationFactory.LoadPersistenceConfiguration(persistenceType);
+        var persistenceSettings = persistenceConfiguration.BuildPersistenceSettings();
+        var persistence = persistenceConfiguration.Create(persistenceSettings);
+        var installer = persistence.CreateInstaller();
+
+        await installer.Install().ConfigureAwait(false);
+    }
+}
