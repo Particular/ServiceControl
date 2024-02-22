5b5a1cb12370137d7848ff5f00742d9d2c87322b
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-22 11:38:20 +1100

    Register installation task for throughput collector

---
+++ src/ServiceControl/Licensing/ThroughputComponent.cs
@@ -0,0 +1,27 @@
+
+namespace Particular.ServiceControl;
+
+using global::ServiceControl.Persistence;
+using global::ServiceControl.Transports;
+using Microsoft.Extensions.Hosting;
+using Particular.License;
+using ServiceBus.Management.Infrastructure.Settings;
+
+class ThroughputComponent : ServiceControlComponent
+{
+    public override void Configure(Settings settings, IHostApplicationBuilder hostBuilder)
+    {
+        hostBuilder.AddThroughputCollector(
+            broker: TransportManifestLibrary.GetName(settings.TransportType),
+            serviceControlAPI: settings.ApiUrl,
+            errorQueue: settings.ErrorQueue,
+            auditQueue: "?",
+            transportConnectionString: settings.TransportConnectionString,
+            persistenceType: PersistenceManifestLibrary.GetName(settings.PersistenceType));
+    }
+
+    public override void Setup(Settings settings, IComponentInstallationContext context)
+    {
+        context.RegisterInstallationTask(() => ThroughputCollectorInstaller.Install(PersistenceManifestLibrary.GetName(settings.PersistenceType)));
+    }
+}
