1b00571df25ad5ff4b781ccc3ca0a3dbaddf5646
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-22 11:38:20 +1100

    Register installation task for throughput collector

--- src/Throughput.Persistence/PersistenceSettings.cs
+++ src/Throughput.Persistence/PersistenceSettings.cs
@@ -3,18 +3,9 @@
 using System;
 using System.Collections.Generic;
 
-public class PersistenceSettings(
-    TimeSpan auditRetentionPeriod,
-    bool enableFullTextSearchOnBodies,
-    int maxBodySizeToStore)
+public class PersistenceSettings()
 {
     public bool MaintenanceMode { get; set; }
-
-    public TimeSpan AuditRetentionPeriod { get; set; } = auditRetentionPeriod;
-
-    public bool EnableFullTextSearchOnBodies { get; set; } = enableFullTextSearchOnBodies;
-
-    public int MaxBodySizeToStore { get; set; } = maxBodySizeToStore;
 
     public IDictionary<string, string> PersisterSpecificSettings { get; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 }
