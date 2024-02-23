3f67f6f7ce94a23d1196a913130bc713027c5bba
Brandon Ording <bording@gmail.com>
2024-02-23 17:53:04 -0500

    Ensure that the correct config files are read

---
+++ src/ServiceControl.UnitTests/PrivateReflectionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace ServiceControl.UnitTests
+{
+    using System.Configuration;
+    using System.Reflection;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PrivateReflectionTests
+    {
+        [Test]
+        public void Should_be_able_to_add_connection_string_to_collection()
+        {
+            var connectionString = new ConnectionStringSettings("reflection-test", "test-connection-string");
+
+            Assert.Throws<ConfigurationErrorsException>(() => ConfigurationManager.ConnectionStrings.Add(connectionString));
+
+            var type = typeof(ConfigurationElementCollection);
+            var field = type.GetField("_readOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Assert.That(field, Is.Not.Null);
+
+            field?.SetValue(ConfigurationManager.ConnectionStrings, false);
+
+            Assert.DoesNotThrow(() => ConfigurationManager.ConnectionStrings.Add(new ConnectionStringSettings("reflection-test", "test-connection-string")));
+        }
+    }
+}
--- src/ServiceControl/Program.cs
+++ src/ServiceControl/Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace Particular.ServiceControl
 {
     using System;
+    using System.Configuration;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
     using System.Runtime.Loader;
     using System.Threading.Tasks;
@@ -21,6 +23,8 @@
         {
             AssemblyLoadContext.Default.Resolving += ResolveAssembly;
             AppDomain.CurrentDomain.UnhandledException += (s, e) => Logger.Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
+
+            ReadExeConfiguration();
 
             var arguments = new HostArguments(args);
 
@@ -70,6 +74,33 @@
             }
 
             return null;
+        }
+
+        static void ReadExeConfiguration()
+        {
+            // ConfigurationManager on .NET is looking for {assembly}.dll.config files, but all previous versions of ServiceControl will have {assembly}.exe.config instead.
+            // This code reads in the exe.config files and adds all the values into the ConfigurationManager's collections.
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var location = Path.GetDirectoryName(assembly.Location);
+            var assemblyName = assembly.GetName();
+            var exePath = Path.Combine(location, $"{assemblyName.Name}.exe");
+            var configuration = ConfigurationManager.OpenExeConfiguration(exePath);
+
+            foreach (var key in configuration.AppSettings.Settings.AllKeys)
+            {
+                ConfigurationManager.AppSettings.Set(key, configuration.AppSettings.Settings[key].Value);
+            }
+
+            // This reflection is required because the connection strings collection has had its read only flag set, so it won't let anything be added to it.
+            var type = typeof(ConfigurationElementCollection);
+            var field = type.GetField("_readOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+            field?.SetValue(ConfigurationManager.ConnectionStrings, false);
+
+            foreach (var connectionStringSetting in configuration.ConnectionStrings.ConnectionStrings.Cast<ConnectionStringSettings>())
+            {
+                ConfigurationManager.ConnectionStrings.Add(connectionStringSetting);
+            }
         }
 
         static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
