d5eb8a5eb03df4fc832f5f1d25edf3007a982469
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 14:38:50 +1100

    Remove unnecessary using statement

--- src/ServiceControl/HostApplicationBuilderExtensions.cs
+++ src/ServiceControl/HostApplicationBuilderExtensions.cs
@@ -28,7 +28,6 @@ namespace Particular.ServiceControl
     using ServiceBus.Management.Infrastructure;
     using ServiceBus.Management.Infrastructure.Installers;
     using ServiceBus.Management.Infrastructure.Settings;
-    using Particular.ThroughputCollector;
 
     static class HostApplicationBuilderExtensions
     {
