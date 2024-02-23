2f5d2a8744204e507a743919723d9ac011ab997c
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 13:54:49 +1100

    Standardize on Particular.ThroughputCollector name for namespaces, folders, projects

---
+++ src/Particular.ThroughputCollector.Persistence/IPersistenceConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Particular.ThroughputCollector.Persistence;
+
+using System.Collections.Generic;
+
+public interface IPersistenceConfiguration
+{
+    string Name { get; }
+
+    IEnumerable<string> ConfigurationKeys { get; }
+
+    IPersistence Create(PersistenceSettings settings);
+}
