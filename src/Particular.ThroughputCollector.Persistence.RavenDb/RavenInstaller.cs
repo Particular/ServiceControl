a50ca9c197c8538dec2f8fd59bb4db122e013c0b
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 13:54:49 +1100

    Standardize on Particular.ThroughputCollector name for namespaces, folders, projects

---
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/RavenInstaller.cs
@@ -0,0 +1,13 @@
+﻿namespace Particular.ThroughputCollector.Persistence.RavenDb;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+class RavenInstaller(IRavenPersistenceLifecycle lifecycle) : IPersistenceInstaller
+{
+    public async Task Install(CancellationToken cancellationToken)
+    {
+        await lifecycle.Start(cancellationToken).ConfigureAwait(false);
+        await lifecycle.Stop(cancellationToken).ConfigureAwait(false);
+    }
+}
