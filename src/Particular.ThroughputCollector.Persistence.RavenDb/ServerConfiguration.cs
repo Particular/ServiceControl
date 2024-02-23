041ddfa68586679b7628f4325657f874cc7b978e
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 13:54:49 +1100

    Standardize on Particular.ThroughputCollector name for namespaces, folders, projects

---
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/ServerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace Particular.ThroughputCollector.Persistence.RavenDb;
+
+public class ServerConfiguration
+{
+    public ServerConfiguration(string connectionString)
+    {
+        UseEmbeddedServer = false;
+        ConnectionString = connectionString;
+    }
+
+    public ServerConfiguration(string dbPath, string serverUrl, string logPath, string logsMode)
+    {
+        UseEmbeddedServer = true;
+        DbPath = dbPath;
+        ServerUrl = serverUrl;
+        LogPath = logPath;
+        LogsMode = logsMode;
+    }
+
+    public string ConnectionString { get; } = string.Empty;
+
+    public bool UseEmbeddedServer { get; }
+    public string DbPath { get; } = string.Empty;
+    public string ServerUrl { get; } = string.Empty;
+    public string LogPath { get; } = string.Empty;
+    public string LogsMode { get; } = string.Empty;
+}
---
+++ src/Particular.ThroughputCollector.Persistence/IThroughputDataStore.cs
@@ -0,0 +1,15 @@
+﻿namespace Particular.ThroughputCollector.Persistence;
+
+//using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+//using ServiceControl.Audit.Auditing;
+//using ServiceControl.Audit.Auditing.MessagesView;
+//using ServiceControl.Audit.Infrastructure;
+//using ServiceControl.Audit.Monitoring;
+//using ServiceControl.SagaAudit;
+
+public interface IThroughputDataStore
+{
+    Task<QueryResult<IList<KnownEndpoint>>> QueryKnownEndpoints();
+}
