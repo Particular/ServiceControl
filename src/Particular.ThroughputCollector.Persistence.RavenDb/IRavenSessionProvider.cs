42453b3e980887346665b41721b577d687902a9c
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 13:54:49 +1100

    Standardize on Particular.ThroughputCollector name for namespaces, folders, projects

---
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/IRavenSessionProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Particular.ThroughputCollector.Persistence.RavenDb;
+
+using Raven.Client.Documents.Session;
+
+interface IRavenSessionProvider
+{
+    IAsyncDocumentSession OpenSession();
+}
