6a0a0275c22fd6bad3b3b50cdb9ec1530802a381
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-23 13:54:49 +1100

    Standardize on Particular.ThroughputCollector name for namespaces, folders, projects

--- src/Throughput.Persistence/IPersistence.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Throughput.Persistence;
-
-using Microsoft.Extensions.DependencyInjection;
-
-public interface IPersistence
-{
-    IPersistenceLifecycle Configure(IServiceCollection serviceCollection);
-    IPersistenceInstaller CreateInstaller();
-}
