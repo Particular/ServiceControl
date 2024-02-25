fbc1c04adaf5a65e51a459447df2897661dbda76
Jo Palac <j_palac@hotmail.com>
2024-02-26 09:36:42 +1000

    rebase fix

--- src/ServiceControl/HostApplicationBuilderExtensions.cs
+++ src/ServiceControl/HostApplicationBuilderExtensions.cs
@@ -120,8 +120,6 @@ namespace Particular.ServiceControl
             }
 
             hostBuilder.AddServiceControlComponents(settings, ServiceControlMainInstance.Components);
-
-            hostBuilder.AddThroughputCollector();
         }
 
         static TransportSettings MapSettings(Settings settings)
