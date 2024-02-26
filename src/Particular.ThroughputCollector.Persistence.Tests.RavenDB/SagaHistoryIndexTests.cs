24c43c6f5a9b5cbcfb9fd75908274911715251f6
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.Tests.RavenDB/SagaHistoryIndexTests.cs
@@ -0,0 +1,55 @@
+﻿namespace ServiceControl.Audit.Persistence.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Raven.Client.Documents;
+    using ServiceControl.SagaAudit;
+
+    [TestFixture]
+    class SagaHistoryIndexTests : PersistenceTestFixture
+    {
+        [Test]
+        public async Task Should_only_index_saga_id_property()
+        {
+            var sagaType = "MySagaType";
+            var sagaState = "some-saga-state";
+
+            await IngestSagaAudits(new SagaSnapshot
+            {
+                SagaId = Guid.NewGuid(),
+                SagaType = sagaType,
+                Status = SagaStateChangeStatus.New,
+                StateAfterChange = sagaState
+            });
+
+            await configuration.CompleteDBOperation();
+
+            using (var session = configuration.DocumentStore.OpenAsyncSession())
+            {
+                var sagaTypeResults = await session.Query<SagaHistory, SagaDetailsIndex>()
+                    .Search(s => s.SagaType, sagaType)
+                    .ToListAsync();
+
+                Assert.AreEqual(0, sagaTypeResults.Count);
+
+                var sagaChangeResults = await session.Query<SagaHistory, SagaDetailsIndex>()
+                    .Search(s => s.Changes, sagaState)
+                    .ToListAsync();
+
+                Assert.AreEqual(0, sagaChangeResults.Count);
+            }
+        }
+
+        async Task IngestSagaAudits(params SagaSnapshot[] snapshots)
+        {
+            var unitOfWork = StartAuditUnitOfWork(snapshots.Length);
+            foreach (var snapshot in snapshots)
+            {
+                await unitOfWork.RecordSagaSnapshot(snapshot);
+            }
+            await unitOfWork.DisposeAsync();
+            await configuration.CompleteDBOperation();
+        }
+    }
+}
