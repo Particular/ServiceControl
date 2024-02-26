a8a598fe2435a81b07f2942b3592d9e6192e218f
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryPersistenceInstaller.cs
@@ -0,0 +1,9 @@
+﻿namespace Particular.ThroughputCollector.Persistence.InMemory;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+public class InMemoryPersistenceInstaller : IPersistenceInstaller
+{
+    public Task Install(CancellationToken cancellationToken) => Task.CompletedTask;
+}
