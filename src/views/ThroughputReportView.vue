d67f5c83cf736cb18749a0150b3ff5ca11be2364
John Simons <john.simons@particular.net>
2024-02-27 09:30:15 +1000

    Selecting tab

--- src/views/ThroughputReportView.vue
+++ src/views/ThroughputReportView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import routeLinks from "@/router/routeLinks";
+import isRouteSelected from "@/composables/isRouteSelected";
 </script>
 
 <template>
@@ -12,13 +13,13 @@ import routeLinks from "@/router/routeLinks";
     <div class="row">
       <div class="col-sm-12">
         <div class="nav tabs">
-          <h5 class="nav-item">
+          <h5 class="nav-item" :class="{ active: isRouteSelected(routeLinks.throughputReport.root) }">
             <RouterLink :to="routeLinks.throughputReport.endpoints.link">Endpoints</RouterLink>
           </h5>
-          <h5 class="nav-item">
+          <h5 class="nav-item" :class="{ active: isRouteSelected(routeLinks.throughputReport.setup.link) }">
             <RouterLink :to="routeLinks.throughputReport.setup.link">Setup</RouterLink>
           </h5>
-          <h5 class="nav-item">
+          <h5 class="nav-item" :class="{ active: isRouteSelected(routeLinks.throughputReport.report.link) }">
             <RouterLink :to="routeLinks.throughputReport.report.link">Report</RouterLink>
           </h5>
         </div>
