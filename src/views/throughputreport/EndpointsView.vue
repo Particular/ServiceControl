796798ccf754d2a4af1d09b369df8b6598217a03
John Simons <john.simons@particular.net>
2024-02-27 09:25:20 +1000

    Adding fake data and more filtering

--- src/views/throughputreport/EndpointsView.vue
+++ src/views/throughputreport/EndpointsView.vue
@@ -1,51 +1,217 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SpDropDown, { type Item } from "@/views/throughputreport/sp-DropDown.vue";
+import { computed, reactive } from "vue";
+import { data } from "@/views/throughputreport/randomData";
+
+enum DataSource {
+  "wellKnownEndpoint" = "ServiceControl",
+  "broker" = "Broker",
+}
+
+enum NameFilterType {
+  beginsWith = "Begins with",
+  contains = "Contains",
+  endsWith = "Ends with",
+}
+
+interface ThroughputEndpoint {
+  source: DataSource;
+  name: string;
+  throughputValue: number;
+  sendOnly?: boolean;
+  doNotInclude?: boolean;
+}
+
+interface SortData {
+  text: string;
+  value: string;
+  comparer: (a: ThroughputEndpoint, b: ThroughputEndpoint) => number;
+}
+
+const sortData: SortData[] = [
+  { text: "By name", comparer: (a: ThroughputEndpoint, b: ThroughputEndpoint) => a.name.localeCompare(b.name) },
+  { text: "By throughput", comparer: (a: ThroughputEndpoint, b: ThroughputEndpoint) => a.throughputValue - b.throughputValue },
+  { text: "By source", comparer: (a: ThroughputEndpoint, b: ThroughputEndpoint) => a.source.localeCompare(b.source) },
+].flatMap((item) => {
+  return [
+    { text: item.text, value: item.text, comparer: item.comparer },
+    { text: `${item.text} (Descending)`, value: `${item.text} (Descending)`, comparer: (a, b) => -item.comparer(a, b) },
+  ];
+});
+
+const dataChanges = reactive(new Map(data.map((item) => [item.name, { sendOnly: item.sendOnly, doNotInclude: item.doNotInclude }])));
+const filterData = reactive({ display: "", name: "", nameFilterType: NameFilterType.beginsWith, sort: "" });
+
+function displayFilterChanged(item: Item) {
+  filterData.display = item.value;
+}
+
+function nameFilterChanged(event: Event) {
+  filterData.name = (event.target as HTMLInputElement).value;
+}
+
+const displayFilterData = [
+  { value: "", text: "All" },
+  { value: DataSource.broker, text: "Broker queues only" },
+  { value: DataSource.wellKnownEndpoint, text: "Known Endpoints to ServiceControl" },
+];
+
+const filterNameOptions = [
+  { text: NameFilterType.beginsWith, filter: (a: ThroughputEndpoint) => a.name.toLowerCase().startsWith(filterData.name.toLowerCase()) },
+  { text: NameFilterType.contains, filter: (a: ThroughputEndpoint) => a.name.toLowerCase().includes(filterData.name.toLowerCase()) },
+  { text: NameFilterType.endsWith, filter: (a: ThroughputEndpoint) => a.name.toLowerCase().endsWith(filterData.name.toLowerCase()) },
+];
+const filteredData = computed(() => {
+  const sortItem = sortData.find((value) => value.value === filterData.sort);
+
+  return (data as ThroughputEndpoint[])
+    .filter((row) => !row.name || filterNameOptions.find((v) => v.text === filterData.nameFilterType)?.filter(row))
+    .filter((row) => {
+      return !filterData.display || row.source === filterData.display;
+    })
+    .sort(sortItem?.comparer);
+});
+
+function sortChanged(item: Item) {
+  filterData.sort = item.value;
+}
+
+function searchTypeChanged(event: Event) {
+  filterData.nameFilterType = (event.target as HTMLInputElement).value as NameFilterType;
+}
+
+function markResultsAsDoNotInclude(value: boolean) {
+  filteredData.value.forEach((item) => {
+    updateDataChanged(item.name, (item) => (item.doNotInclude = value));
+  });
+}
+
+function markResultsAsDoSendOnly(value: boolean) {
+  filteredData.value.forEach((item) => {
+    updateDataChanged(item.name, (item) => (item.sendOnly = value));
+  });
+}
+
+function updateSendOnlyCheckbox(event: Event, name: string) {
+  const checked = (event.target as HTMLInputElement).checked;
+
+  updateDataChanged(name, (item) => (item.sendOnly = checked));
+}
+
+function updateDoNotIncludeCheckbox(event: Event, name: string) {
+  const checked = (event.target as HTMLInputElement).checked;
+
+  updateDataChanged(name, (item) => (item.doNotInclude = checked));
+}
+
+function updateDataChanged(name: string, action: (item: { doNotInclude: boolean; sendOnly: boolean }) => void) {
+  const item = dataChanges.get(name);
+  if (item) {
+    action(item);
+  }
+}
+</script>
 
 <template>
   <div class="box">
     <div class="row">
       <div class="col">
-        <div class="card">
-          <div class="card-header">Filters</div>
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <div class="card-text">
-              <div>
-                <label>Search by name: <input type="text" placeholder="endpoint name" /></label>
-              </div>
-              <div class="form-floating">
-                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                  <option value="1" selected>All</option>
-                  <option value="2">Retrieved from Broker</option>
-                  <option value="3">Known to ServiceControl</option>
-                </select>
-                <label for="floatingSelect">Display</label>
-              </div>
-            </div>
+        <div class="text-search-container">
+          <div>
+            <select class="form-select text-search format-text" @change="searchTypeChanged">
+              <option v-for="item in filterNameOptions" :value="item.text" :key="item.text">{{ item.text }}</option>
+            </select>
+          </div>
+          <div>
+            <input type="text" class="form-control format-text" :value="filterData.name" @input="nameFilterChanged" placeholder="Filter by name..." />
           </div>
         </div>
       </div>
-      <div class="col">Sort</div>
+      <div class="col">
+        <sp-drop-down label="Display" :select-item="displayFilterData.find((v) => v.value === filterData.display)" :callback="displayFilterChanged" :items="displayFilterData" />
+      </div>
+      <div class="col">
+        <sp-drop-down label="Sort" :select-item="sortData.find((v) => v.value === filterData.sort)" :callback="sortChanged" :items="sortData" />
+      </div>
+    </div>
+    <div class="row results">
+      <div class="col format-showing-results">
+        <div>Showing {{ filteredData.length }} of {{ data.length }} result(s)</div>
+      </div>
+      <div class="col"></div>
+      <div class="col">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Mark results as</button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#" @click.prevent="() => markResultsAsDoNotInclude(true)">Do not include</a></li>
+            <li><a class="dropdown-item" href="#" @click.prevent="() => markResultsAsDoNotInclude(false)">Include</a></li>
+            <li><a class="dropdown-item" href="#" @click.prevent="() => markResultsAsDoSendOnly(true)">Send only</a></li>
+            <li><a class="dropdown-item" href="#" @click.prevent="() => markResultsAsDoSendOnly(false)">Not Send only</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="box">
-    <div class="row">
-      <div class="col">Endpoint (Queue)</div>
-      <div class="col">Max daily throughput this month</div>
-      <div class="col">Is Send only?</div>
-      <div class="col">Do not include?</div>
-    </div>
-    <div class="row">
-      <div class="col">My.Endpoint</div>
-      <div class="col">1234</div>
-      <div class="col">
-        <div class="form-check"><input class="form-check-input" type="checkbox" value="" /><label class="form-check-label" for="">Send only</label></div>
-      </div>
-      <div class="col">
-        <div class="form-check"><input class="form-check-input" type="checkbox" value="" /><label class="form-check-label" for="">Do not include</label></div>
-      </div>
-    </div>
-  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Source</th>
+        <th scope="col">Endpoint (Queue)</th>
+        <th scope="col">Max daily throughput<br />this month</th>
+        <th scope="col">Is Send only <i class="fa fa-info-circle info" title="Queues with 0 throughput are candidates to be send only endpoint." /></th>
+        <th scope="col">Do not include <i class="fa fa-info-circle info" title="What queues are part of NServiceBus" /></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in filteredData" :key="row.name">
+        <td class="col">{{ row.source }}</td>
+        <td class="col">{{ row.name }}</td>
+        <td class="col">{{ row.throughputValue }}</td>
+        <td class="col">
+          <div v-if="row.throughputValue === 0" class="form-check">
+            <input class="form-check-input" type="checkbox" :checked="dataChanges.get(row.name)?.sendOnly" @change="(e) => updateSendOnlyCheckbox(e, row.name)" :id="`sendonly[${row.name}]`" /><label
+              class="form-check-label override-font"
+              :for="`sendonly[${row.name}]`"
+              >Send only</label
+            >
+          </div>
+        </td>
+        <td class="col">
+          <div v-if="row.source !== DataSource.wellKnownEndpoint" class="form-check">
+            <input class="form-check-input" type="checkbox" :checked="dataChanges.get(row.name)?.doNotInclude" @change="(e) => updateDoNotIncludeCheckbox(e, row.name)" :id="`doNotInclude[${row.name}]`" /><label
+              class="form-check-label override-font"
+              :for="`doNotInclude[${row.name}]`"
+              >Do not include</label
+            >
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<style scoped></style>
+<style scoped>
+.format-showing-results {
+  display: flex;
+  align-items: flex-end;
+}
+.results {
+  margin-top: 5px;
+}
+.format-text {
+  font-weight: unset;
+  font-size: 14px;
+  min-width: 120px;
+}
+.text-search-container {
+  display: flex;
+  flex-direction: row;
+}
+.text-search {
+  width: 130px;
+}
+.info {
+  color: dodgerblue;
+}
+</style>
---
+++ src/views/throughputreport/sp-DropDown.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+export interface Item {
+  value: string;
+  text: string;
+}
+
+const props = defineProps<{
+  label: string;
+  selectItem?: Item;
+  items: Item[];
+  callback: (item: Item) => void;
+}>();
+</script>
+
+<template>
+  <div class="dropdown">
+    <label class="control-label">{{ props.label }}:</label>
+    <button type="button" class="btn btn-dropdown dropdown-toggle sp-btn-menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ props.selectItem?.text ?? props.items[0].text }}</button>
+    <ul class="dropdown-menu">
+      <li v-for="item in props.items" :key="item.value">
+        <a href="#" @click.prevent="callback(item)">{{ item.text }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.dropdown .dropdown-menu {
+  top: 36px;
+  margin-left: 72px;
+}
+
+.btn.btn-dropdown {
+  padding: 8px 16px;
+}
+
+.btn.dropdown-toggle::after {
+  vertical-align: middle;
+}
+
+ul.dropdown-menu li a span {
+  color: #aaa;
+}
+</style>
