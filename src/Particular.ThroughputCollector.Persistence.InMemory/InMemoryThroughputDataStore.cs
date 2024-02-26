901042b874ce8b8fe2d4d4aeb5ddaaba1bf64354
Jo Palac <j_palac@hotmail.com>
2024-02-26 17:18:10 +1000

    started adding persistence methods

--- src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryThroughputDataStore.cs
+++ src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryThroughputDataStore.cs
@@ -1,22 +1,54 @@
 ﻿namespace Particular.ThroughputCollector.Persistence.InMemory;
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Particular.ThroughputCollector.Persistence;
 
 class InMemoryThroughputDataStore : IThroughputDataStore
 {
-    public List<KnownEndpoint> knownEndpoints;
+    public List<Endpoint> endpoints;
 
     public InMemoryThroughputDataStore()
     {
-        knownEndpoints = [];
+        endpoints = [];
     }
 
-    public async Task<QueryResult<IList<KnownEndpoint>>> QueryKnownEndpoints()
+    public async Task<IReadOnlyList<Endpoint>> GetAllEndpoints()
     {
-        await Task.Delay(1);
-        return new QueryResult<IList<KnownEndpoint>>(knownEndpoints, new QueryStatsInfo(string.Empty, knownEndpoints.Count));
+        return await Task.FromResult(endpoints);
+    }
+
+    public async Task<Endpoint> GetEndpointByNameOrQueue(string nameOrQueue)
+    {
+        var endpoint = endpoints.FirstOrDefault(w => w.Name == nameOrQueue || w.Queue == nameOrQueue);
+
+        return await Task.FromResult(endpoint);
+    }
+    public async Task RecordEndpointThroughput(Endpoint endpoint)
+    {
+        var existingEndpoint = endpoints.FirstOrDefault(w => w.Name == endpoint.Name);
+
+        if (existingEndpoint == null)
+        {
+            endpoints.Add(endpoint);
+        }
+        else
+        {
+            existingEndpoint.ThroughputSource = endpoint.ThroughputSource;
+            //TODO should anything else be updated here or only the daily throughput?
+
+            if (existingEndpoint.DailyThroughput == null)
+            {
+                existingEndpoint.DailyThroughput = endpoint.DailyThroughput;
+            }
+            else if (endpoint.DailyThroughput != null)
+            {
+                existingEndpoint.DailyThroughput.Concat(endpoint.DailyThroughput);
+            }
+        }
+
+        await Task.CompletedTask;
     }
 
     //object TryGet(Dictionary<string, object> metadata, string key)
