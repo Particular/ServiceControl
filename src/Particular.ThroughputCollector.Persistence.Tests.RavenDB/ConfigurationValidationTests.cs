dad3f9d1f732a6045518a8a695e68e23e70a2c31
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.Tests.RavenDB/ConfigurationValidationTests.cs
@@ -0,0 +1,102 @@
+﻿namespace Particular.ThroughputCollector.UnitTests
+{
+    //using System;
+    //using NUnit.Framework;
+    //using Particular.ThroughputCollector.Persistence.RavenDb;
+
+    class ConfigurationValidationTests
+    {
+        //[Test]
+        //public void Should_apply_persistence_settings()
+        //{
+        //    var settings = BuildSettings();
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.ConnectionStringKey] = "connection string";
+
+        //    var configuration = RavenPersistenceConfiguration.GetDatabaseConfiguration(settings);
+
+        //    Assert.AreEqual(settings.AuditRetentionPeriod, configuration.AuditRetentionPeriod);
+        //    Assert.AreEqual(settings.MaxBodySizeToStore, configuration.MaxBodySizeToStore);
+        //    Assert.AreEqual(settings.EnableFullTextSearchOnBodies, configuration.EnableFullTextSearch);
+        //}
+
+        //[Test]
+        //public void Should_support_external_server()
+        //{
+        //    var settings = BuildSettings();
+        //    var connectionString = "http://someserver:44444";
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.ConnectionStringKey] = connectionString;
+
+        //    var configuration = RavenPersistenceConfiguration.GetDatabaseConfiguration(settings);
+
+        //    Assert.False(configuration.ServerConfiguration.UseEmbeddedServer);
+        //    Assert.AreEqual(connectionString, configuration.ServerConfiguration.ConnectionString);
+        //}
+
+        //[Test]
+        //public void Should_support_embedded_server()
+        //{
+        //    var settings = BuildSettings();
+        //    var dpPath = "c://db-path";
+        //    var logPath = "c://log-path";
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabasePathKey] = dpPath;
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabaseMaintenancePortKey] = "11111";
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.LogPathKey] = logPath;
+        //    var configuration = RavenPersistenceConfiguration.GetDatabaseConfiguration(settings);
+
+        //    Assert.True(configuration.ServerConfiguration.UseEmbeddedServer);
+        //    Assert.AreEqual(dpPath, configuration.ServerConfiguration.DbPath);
+        //    Assert.AreEqual(logPath, configuration.ServerConfiguration.LogPath);
+        //    Assert.AreEqual("http://localhost:11111", configuration.ServerConfiguration.ServerUrl);
+        //}
+
+        //[Test]
+        //public void Should_throw_if_port_is_missing()
+        //{
+        //    var settings = BuildSettings();
+        //    var dpPath = "c://some-path";
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabasePathKey] = dpPath;
+
+        //    Assert.Throws<InvalidOperationException>(() => RavenPersistenceConfiguration.GetDatabaseConfiguration(settings));
+        //}
+
+        //[Test]
+        //public void Should_throw_if_port_is_not_an_integer()
+        //{
+        //    var settings = BuildSettings();
+        //    var dpPath = "c://some-path";
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabasePathKey] = dpPath;
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabaseMaintenancePortKey] = "not an int";
+
+        //    Assert.Throws<InvalidOperationException>(() => RavenPersistenceConfiguration.GetDatabaseConfiguration(settings));
+        //}
+
+        //[Test]
+        //public void Should_throw_if_no_path_or_connection_string_is_configured()
+        //{
+        //    var settings = BuildSettings();
+
+        //    Assert.Throws<InvalidOperationException>(() => RavenPersistenceConfiguration.GetDatabaseConfiguration(settings));
+        //}
+
+        //[Test]
+        //public void Should_throw_if_both_path_or_connection_string_is_configured()
+        //{
+        //    var settings = BuildSettings();
+
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.DatabasePathKey] = "path";
+        //    settings.PersisterSpecificSettings[RavenPersistenceConfiguration.ConnectionStringKey] = "connection string";
+
+        //    Assert.Throws<InvalidOperationException>(() => RavenPersistenceConfiguration.GetDatabaseConfiguration(settings));
+        //}
+
+        //PersistenceSettings BuildSettings()
+        //{
+        //    return new PersistenceSettings(TimeSpan.FromMinutes(2), true, 100000);
+        //}
+    }
+}
---
+++ src/Particular.ThroughputCollector.Persistence.Tests/InMemory/PersistenceTestsConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace Particular.ThroughputCollector.Persistence.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Particular.ThroughputCollector.Persistence.InMemory;
+
+    partial class PersistenceTestsConfiguration
+    {
+        public IThroughputDataStore AuditDataStore { get; protected set; }
+
+        public Task Configure(Action<PersistenceSettings> setSettings)
+        {
+            var config = new InMemoryPersistenceConfiguration();
+            var serviceCollection = new ServiceCollection();
+            var settings = new PersistenceSettings();
+
+            setSettings(settings);
+            var persistence = config.Create(settings);
+            persistence.Configure(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            AuditDataStore = serviceProvider.GetRequiredService<IThroughputDataStore>();
+            return Task.CompletedTask;
+        }
+
+        public Task CompleteDBOperation()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Cleanup()
+        {
+            return Task.CompletedTask;
+        }
+
+        public string Name => "InMemory";
+    }
+}
