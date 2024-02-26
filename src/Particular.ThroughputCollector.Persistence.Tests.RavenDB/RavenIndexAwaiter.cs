ddae6c283f71eb890a4e0dd8f57425211604cff5
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.Tests.RavenDB/RavenIndexAwaiter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+
+public static class RavenIndexAwaiter
+{
+    public static void WaitForIndexing(this IDocumentStore store)
+    {
+        store.WaitForIndexing(10);
+    }
+
+    static void WaitForIndexing(this IDocumentStore store, int secondsToWait)
+    {
+        Assert.True(SpinWait.SpinUntil(() => store.Maintenance.Send(new GetStatisticsOperation()).StaleIndexes.Length == 0, TimeSpan.FromSeconds(secondsToWait)));
+    }
+}
