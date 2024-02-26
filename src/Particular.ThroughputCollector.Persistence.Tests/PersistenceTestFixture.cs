61fd384982ff7a8e40f4a562e293f58d07579304
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.Tests/PersistenceTestFixture.cs
@@ -0,0 +1,54 @@
+﻿namespace Particular.ThroughputCollector.Persistence.Tests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    abstract class PersistenceTestFixture
+    {
+        public Action<PersistenceSettings> SetSettings = _ => { };
+
+        [SetUp]
+        public virtual Task Setup()
+        {
+            configuration = new PersistenceTestsConfiguration();
+
+            return configuration.Configure(SetSettings);
+        }
+
+        [TearDown]
+        public virtual Task Cleanup()
+        {
+            return configuration?.Cleanup();
+        }
+
+        protected string GetManifestPath()
+        {
+            var currentFolder = new DirectoryInfo(TestContext.CurrentContext.TestDirectory);
+
+            while (currentFolder != null)
+            {
+                var file = currentFolder.EnumerateFiles("*.sln", SearchOption.TopDirectoryOnly)
+                    .SingleOrDefault();
+
+                if (file != null)
+                {
+                    return Path.Combine(file.Directory.FullName, $"Particular.ThroughputCollector.Persistence.{PersisterName}", "persistence.manifest");
+                }
+
+                currentFolder = currentFolder.Parent;
+            }
+
+            throw new Exception($"Cannot find manifest folder for {PersisterName}");
+        }
+
+        protected string PersisterName => configuration.Name;
+
+        protected IThroughputDataStore DataStore => configuration.AuditDataStore;
+
+        protected PersistenceTestsConfiguration configuration;
+    }
+}
