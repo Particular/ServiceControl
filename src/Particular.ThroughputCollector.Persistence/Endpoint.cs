7953c7629e8073c13e7bb3c2cf79eca61d24be4b
Jo Palac <j_palac@hotmail.com>
2024-02-26 17:18:10 +1000

    started adding persistence methods

---
+++ src/Particular.ThroughputCollector.Persistence/Endpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Particular.ThroughputCollector.Persistence;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+public record Endpoint
+{
+    public string Name { get; set; }
+    public string Queue { get; set; }
+    public string ThroughputSource { get; set; }
+    public string Scope { get; set; } //used for SQLServer endpoints, perhaps for others too
+    public string[] EndpointIndicators { get; set; }
+    public bool UserIndicatedSendOnly { get; set; }
+    public bool UserIndicatedToIgnore { get; set; }
+
+    public List<EndpointThroughput> DailyThroughput { get; set; }
+
+    //public const string CollectionName = "Endpoints";
+}
--- src/Particular.ThroughputCollector/Contracts/Endpoint.cs
+++ src/Particular.ThroughputCollector/Contracts/Endpoint.cs
@@ -1,36 +1,36 @@
-﻿namespace Particular.ThroughputCollector.Contracts
-{
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public class Endpoint
-    {
-        public Endpoint(string name, string queue, ThroughputSource throughputSource)
-        {
-            Name = name;
-            Queue = queue;
-            ThroughputSource = throughputSource;
-            DailyThroughput = [];
-        }
+﻿//namespace Particular.ThroughputCollector.Contracts
+//{
+//#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+//    public class Endpoint
+//    {
+//        public Endpoint(string name, string queue, ThroughputSource throughputSource)
+//        {
+//            Name = name;
+//            Queue = queue;
+//            ThroughputSource = throughputSource;
+//            DailyThroughput = [];
+//        }
 
-        public string Name { get; set; }
-        public string Queue { get; set; }
-        public ThroughputSource ThroughputSource { get; set; }
-        public string Scope { get; set; } //used for SQLServer endpoints, perhaps for others too
-        public string[] EndpointIndicators { get; set; }
-        public bool UserIndicatedSendOnly { get; set; }
-        public bool UserIndicatedNotNSB { get; set; }
-        public List<Throughput> DailyThroughput { get; set; }
-    }
+//        public string Name { get; set; }
+//        public string Queue { get; set; }
+//        public ThroughputSource ThroughputSource { get; set; }
+//        public string Scope { get; set; } //used for SQLServer endpoints, perhaps for others too
+//        public string[] EndpointIndicators { get; set; }
+//        public bool UserIndicatedSendOnly { get; set; }
+//        public bool UserIndicatedToIgnore { get; set; }
+//        public List<EndpointThroughput> DailyThroughput { get; set; }
+//    }
 
-    public class Throughput
-    {
-        public DateTime Date { get; set; }
-        public int TotalThroughput { get; set; }
-    }
+//    public class EndpointThroughput
+//    {
+//        public DateTime DateUTC { get; set; }
+//        public int TotalThroughput { get; set; }
+//    }
 
-    public enum ThroughputSource
-    {
-        Broker,
-        Monitoring,
-        Audit
-    }
-}
+//    public enum ThroughputSource
+//    {
+//        Broker,
+//        Monitoring,
+//        Audit
+//    }
+//}
