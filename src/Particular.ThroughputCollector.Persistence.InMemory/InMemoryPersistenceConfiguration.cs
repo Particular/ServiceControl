a60359f0af7adade4eaa8133dc0f2aaddd5dfc7e
Jo Palac <j_palac@hotmail.com>
2024-02-26 13:47:32 +1000

    added in memory persistence and test projects

---
+++ src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryPersistenceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Particular.ThroughputCollector.Persistence.InMemory;
+
+using System.Collections.Generic;
+
+public class InMemoryPersistenceConfiguration : IPersistenceConfiguration
+{
+    public string Name => "InMemory";
+
+    public IEnumerable<string> ConfigurationKeys => new string[0];
+
+    public IPersistence Create(PersistenceSettings settings) => new InMemoryPersistence(settings);
+}
+
