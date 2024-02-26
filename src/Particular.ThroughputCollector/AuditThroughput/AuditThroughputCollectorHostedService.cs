c7d7920607aa843cf66ade844e4a1f62f234ff3a
Jo Palac <j_palac@hotmail.com>
2024-02-26 17:18:10 +1000

    started adding persistence methods

--- src/Particular.ThroughputCollector/AuditThroughput/AuditThroughputCollectorHostedService.cs
+++ src/Particular.ThroughputCollector/AuditThroughput/AuditThroughputCollectorHostedService.cs
@@ -5,14 +5,16 @@
     using Microsoft.Extensions.Logging;
     using Particular.ThroughputCollector.Contracts;
     using Particular.ThroughputCollector.Infrastructure;
+    using Particular.ThroughputCollector.Persistence;
     using Particular.ThroughputCollector.Shared;
 
     class AuditThroughputCollectorHostedService : IHostedService
     {
-        public AuditThroughputCollectorHostedService(ILoggerFactory loggerFactory, ThroughputSettings throughputSettings)
+        public AuditThroughputCollectorHostedService(ILoggerFactory loggerFactory, ThroughputSettings throughputSettings, IThroughputDataStore dataStore)
         {
             logger = loggerFactory.CreateLogger<AuditThroughputCollectorHostedService>();
             this.throughputSettings = throughputSettings;
+            this.dataStore = dataStore;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -31,23 +33,24 @@
 
         async Task GatherThroughput(CancellationToken cancellationToken)
         {
-            logger.LogInformation($"Gathering throughput from audit");
+            DateTime utcDateTime = DateTime.UtcNow.Date;
+            logger.LogInformation($"Gathering throughput from audit for ");
 
             try
             {
-                if (!ThroughputRecordedForYesterday())
+                var httpFactory = await HttpAuth.CreateHttpClientFactory(throughputSettings.ServiceControlAPI, logger, configureNewClient: c => c.Timeout = TimeSpan.FromSeconds(30), cancellationToken: cancellationToken).ConfigureAwait(false);
+                var primary = new ServiceControlClient("ServiceControl", throughputSettings.ServiceControlAPI, httpFactory, logger);
+                await primary.CheckEndpoint(content => content.Contains("\"known_endpoints_url\"") && content.Contains("\"endpoints_messages_url\""), cancellationToken).ConfigureAwait(false); //TODO do we need this since we know the SC url?
+                var knownEndpoints = await Commands.GetKnownEndpoints(primary, logger, cancellationToken).ConfigureAwait(false);
+
+                if (!knownEndpoints.Any())
                 {
-                    var httpFactory = await HttpAuth.CreateHttpClientFactory(throughputSettings.ServiceControlAPI, logger, configureNewClient: c => c.Timeout = TimeSpan.FromSeconds(30), cancellationToken: cancellationToken).ConfigureAwait(false);
-                    var primary = new ServiceControlClient("ServiceControl", throughputSettings.ServiceControlAPI, httpFactory, logger);
-                    await primary.CheckEndpoint(content => content.Contains("\"known_endpoints_url\"") && content.Contains("\"endpoints_messages_url\""), cancellationToken).ConfigureAwait(false); //TODO do we need this since we know the SC url?
-                    var knownEndpoints = await Commands.GetKnownEndpoints(primary, logger, cancellationToken).ConfigureAwait(false);
+                    throw new HaltException(HaltReason.InvalidEnvironment, "Successfully connected to ServiceControl API but no known endpoints could be found.");
+                }
 
-                    if (!knownEndpoints.Any())
-                    {
-                        throw new HaltException(HaltReason.InvalidEnvironment, "Successfully connected to ServiceControl API but no known endpoints could be found.");
-                    }
-
-                    foreach (var endpoint in knownEndpoints)
+                foreach (var endpoint in knownEndpoints)
+                {
+                    if (!await ThroughputRecordedForYesterday(endpoint.Name, utcDateTime.AddDays(-1)).ConfigureAwait(false))
                     {
                         //TODO for each endpoint record the audit count for the day we are currently doing
                         //TODO can we assume that everyone is using the new version of audits hence they have audit counts?
@@ -60,14 +63,16 @@
             }
         }
 
-        bool ThroughputRecordedForYesterday()
+        async Task<bool> ThroughputRecordedForYesterday(string endpointName, DateTime utcDateTime)
         {
-            //TODO
-            return false;
+            var endpoint = await dataStore.GetEndpointByNameOrQueue(endpointName).ConfigureAwait(false);
+
+            return endpoint?.DailyThroughput?.Any(a => a.DateUTC == utcDateTime) ?? false;
         }
 
         readonly ILogger logger;
         ThroughputSettings throughputSettings;
         Timer? auditThroughputGatherTimer;
+        IThroughputDataStore dataStore;
     }
 }
