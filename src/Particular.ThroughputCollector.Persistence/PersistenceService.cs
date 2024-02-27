de26c328f13a8fd9ab9d1ffc946026afcbe04fd5
Andreas Bednarz <andreas.bednarz@particular.net>
2024-02-28 10:19:51 +1100

    Register throughput collector's persistence in DI

---
+++ src/Particular.ThroughputCollector.Persistence/PersistenceService.cs
@@ -0,0 +1,12 @@
+﻿namespace Particular.ThroughputCollector.Persistence;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+public abstract class PersistenceService : IHostedService
+{
+    public abstract Task StartAsync(CancellationToken cancellationToken);
+
+    public abstract Task StopAsync(CancellationToken cancellationToken);
+}
