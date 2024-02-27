154965d7fc6ded18251101b380d35a42dcea712a
Brandon Ording <bording@gmail.com>
2024-02-27 16:15:34 -0500

    Stop adding GC setting to app.config that isn't used

--- src/ServiceControlInstaller.Engine/Configuration/ServiceControl/AppConfig.cs
+++ src/ServiceControlInstaller.Engine/Configuration/ServiceControl/AppConfig.cs
@@ -2,7 +2,6 @@ namespace ServiceControlInstaller.Engine.Configuration.ServiceControl
 {
     using System.Configuration;
     using System.Linq;
-    using System.Xml.Linq;
     using NuGet.Versioning;
 
     public abstract class AppConfig : AppConfigWrapper
@@ -15,36 +14,14 @@ namespace ServiceControlInstaller.Engine.Configuration.ServiceControl
         {
             UpdateSettings();
 
-            UpdateRuntimeSection();
-
             Config.Save();
         }
-
 
         public abstract void EnableMaintenanceMode();
 
         public abstract void DisableMaintenanceMode();
 
         public abstract void SetTransportType(string transportTypeName);
-
-        void UpdateRuntimeSection()
-        {
-            var runtimesection = Config.GetSection("runtime");
-            var runtimeXml = XDocument.Parse(runtimesection.SectionInformation.GetRawXml() ?? "<runtime/>");
-
-            // Set gcServer Value if it does not exist
-            var gcServer = runtimeXml.Descendants("gcServer").SingleOrDefault();
-            if (gcServer == null) //So no config so we can set
-            {
-                gcServer = new XElement("gcServer");
-                gcServer.SetAttributeValue("enabled", "true");
-                if (runtimeXml.Root != null)
-                {
-                    runtimeXml.Root.Add(gcServer);
-                    runtimesection.SectionInformation.SetRawXml(runtimeXml.Root.ToString());
-                }
-            }
-        }
 
         protected abstract void UpdateSettings();
 
