e25e71ed41ba31c72b3e9ebeb069635b9c8da7df
Daniel Marbach <daniel.marbach@openplace.net>
2024-02-28 17:37:27 +0000

    Usings

--- src/ServiceControl.Transports.SqlServer/SqlServerTransportCustomization.cs
+++ src/ServiceControl.Transports.SqlServer/SqlServerTransportCustomization.cs
@@ -2,6 +2,7 @@
 {
     using System.Linq;
     using System.Reflection;
+    using System.Runtime.CompilerServices;
     using NServiceBus;
     using NServiceBus.Configuration.AdvancedExtensibility;
     using NServiceBus.Logging;
