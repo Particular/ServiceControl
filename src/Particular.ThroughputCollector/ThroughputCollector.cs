55b0c1fa45c4191c95da166d8e441947f2e7cbc6
Jo Palac <j_palac@hotmail.com>
2024-02-28 14:59:46 +1000

    started on api

---
+++ src/Particular.ThroughputCollector/ThroughputCollector.cs
@@ -0,0 +1,20 @@
+﻿namespace Particular.ThroughputCollector
+{
+    using Particular.ThroughputCollector.Contracts;
+    using Particular.ThroughputCollector.Persistence;
+
+    public class ThroughputCollector : IThroughputCollector
+    {
+        public ThroughputCollector(IThroughputDataStore dataStore)
+        {
+            this.dataStore = dataStore;
+        }
+
+        public List<EndpointThroughputSummary> GetThroughputSummary()
+        {
+            return [];
+        }
+
+        readonly IThroughputDataStore dataStore;
+    }
+}
