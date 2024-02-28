5519198c8d6ecff39e0d0fb09f863d2f1c8d5c95
Jo Palac <j_palac@hotmail.com>
2024-02-28 14:59:46 +1000

    started on api

---
+++ src/Particular.ThroughputCollector/Contracts/EndpointThroughputSummary.cs
@@ -0,0 +1,15 @@
+﻿namespace Particular.ThroughputCollector.Contracts
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    public class EndpointThroughputSummary
+    {
+        public string Name { get; set; }
+        public string Queue { get; set; }
+        public string ThroughputSource { get; set; }
+        public bool IsKnownEndpoint { get; set; }
+        public bool? UserIndicatedSendOnly { get; set; }
+        public bool? UserIndicatedToIgnore { get; set; }
+        public long? MaxDailyThroughputForThisMonth { get; set; }
+    }
+}
