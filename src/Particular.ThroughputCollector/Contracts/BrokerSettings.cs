bab32017c198a52c0bad954afc0cd32a0e8913b0
Jo Palac <j_palac@hotmail.com>
2024-02-29 16:35:21 +1000

    progress on api methods

---
+++ src/Particular.ThroughputCollector/Contracts/BrokerSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Particular.ThroughputCollector.Contracts
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    public class BrokerSettings
+    {
+        public Broker Broker { get; set; }
+        public string IsKnownEndpoint { get; set; }
+        public bool? UserIndicatedSendOnly { get; set; }
+        public bool? UserIndicatedToIgnore { get; set; }
+        public long? MaxDailyThroughputForThisMonth { get; set; }
+    }
+}
