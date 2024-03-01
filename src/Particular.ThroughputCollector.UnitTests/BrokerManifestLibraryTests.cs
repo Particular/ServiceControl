22919955f0a45a96daa5e4e01796b601c1c92118
Jo Palac <j_palac@hotmail.com>
2024-03-01 16:34:17 +1000

    broker settings setup and methods, tests for broker and persistence manifests

---
+++ src/Particular.ThroughputCollector.UnitTests/BrokerManifestLibraryTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Particular.ThroughputCollector.UnitTests
+{
+    using NUnit.Framework;
+    using Particular.ThroughputCollector.Contracts;
+
+    [TestFixture]
+    public class BrokerManifestLibraryTests
+    {
+        const Broker broker = Broker.AzureServiceBus;
+
+        [Test]
+        public void Should_find_broker_by_name()
+        {
+            var _broker = BrokerManifestLibrary.Find(broker);
+
+            Assert.That(_broker, Is.Not.Null);
+            Assert.That(_broker.Broker, Is.EqualTo(_broker.Broker));
+        }
+
+        [Test]
+        public void Should_return_null_for_not_found_broker()
+        {
+            var _noneBroker = Broker.None;
+            var _broker = BrokerManifestLibrary.Find(_noneBroker);
+
+            Assert.That(_broker, Is.Null);
+        }
+    }
+}
