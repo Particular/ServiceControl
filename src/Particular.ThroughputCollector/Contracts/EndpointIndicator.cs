fc13b65714bcb9e39faff5e447d983c5d2039e28
Jo Palac <j_palac@hotmail.com>
2024-03-01 16:34:17 +1000

    broker settings setup and methods, tests for broker and persistence manifests

--- src/Particular.ThroughputCollector/Contracts/EndpointIndicator.cs
+++ src/Particular.ThroughputCollector/Contracts/EndpointIndicator.cs
@@ -1,5 +1,8 @@
 ﻿namespace Particular.ThroughputCollector.Contracts
 {
+    using System.Text.Json.Serialization;
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum EndpointIndicator
     {
         ConventionalTopologyBinding,
