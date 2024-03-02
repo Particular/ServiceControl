6e9aadf16d51992c6d5edf06f26f3bd8ad7b2523
Brandon Ording <bording@gmail.com>
2024-03-01 19:33:03 -0500

    Add MSMQ back

--- src/ServiceControl.Config.Tests/EditInstance/EditAuditInstance/ConnectionString.cs
+++ src/ServiceControl.Config.Tests/EditInstance/EditAuditInstance/ConnectionString.cs
@@ -40,38 +40,37 @@
 
     class EditAuditConnectionStringsTests
     {
-        // TODO uncomment tests when MSMQ is added back
-        //[Test]
-        //public void MSMQ_transport_is_selected()
-        //{
-        //    var viewModel = Given_an_audit_instance()
-        //        .When_MSQMQ_transport_is_selected();
+        [Test]
+        public void MSMQ_transport_is_selected()
+        {
+            var viewModel = Given_an_audit_instance()
+                .When_MSQMQ_transport_is_selected();
 
-        //    Assert.IsFalse(viewModel.ShowConnectionString);
-        //    Assert.AreEqual("MSMQ", viewModel.SelectedTransport.Name);
-        //    Assert.IsEmpty(viewModel.SampleConnectionString);
-        //    Assert.IsNull(viewModel.TransportWarning);
-        //}
+            Assert.IsFalse(viewModel.ShowConnectionString);
+            Assert.AreEqual("MSMQ", viewModel.SelectedTransport.Name);
+            Assert.IsEmpty(viewModel.SampleConnectionString);
+            Assert.IsNull(viewModel.TransportWarning);
+        }
 
-        //[TestAllTransportsExcept("MSMQ")]
-        //public void Non_MSMQ_transport_is_selected(string transportInfoName)
-        //{
-        //    var viewModel = Given_an_audit_instance()
-        //        .When_a_transport_is_selected(transportInfoName);
+        [TestAllTransportsExcept("MSMQ")]
+        public void Non_MSMQ_transport_is_selected(string transportInfoName)
+        {
+            var viewModel = Given_an_audit_instance()
+                .When_a_transport_is_selected(transportInfoName);
 
-        //    Assert.IsTrue(viewModel.ShowConnectionString);
-        //    StringAssert.StartsWith(transportInfoName, viewModel.SelectedTransport.Name);
-        //    Assert.IsNotEmpty(viewModel.SampleConnectionString);
-        //    if (transportInfoName is "SQLServer" or "AmazonSQS" or "AzureStorageQueue")
-        //    {
-        //        Assert.IsNotNull(viewModel.TransportWarning);
-        //        Assert.IsNotEmpty(viewModel.TransportWarning);
-        //    }
-        //    else
-        //    {
-        //        Assert.IsNull(viewModel.TransportWarning);
-        //    }
-        //}
+            Assert.IsTrue(viewModel.ShowConnectionString);
+            StringAssert.StartsWith(transportInfoName, viewModel.SelectedTransport.Name);
+            Assert.IsNotEmpty(viewModel.SampleConnectionString);
+            if (transportInfoName is "SQLServer" or "AmazonSQS" or "AzureStorageQueue")
+            {
+                Assert.IsNotNull(viewModel.TransportWarning);
+                Assert.IsNotEmpty(viewModel.TransportWarning);
+            }
+            else
+            {
+                Assert.IsNull(viewModel.TransportWarning);
+            }
+        }
 
         [Test]
         public void Screen_Loaded()
