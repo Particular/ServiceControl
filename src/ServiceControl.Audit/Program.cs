0bbfa12da6f02902f900b3aa3cca79b135ae67d7
Brandon Ording <bording@gmail.com>
2024-03-04 13:08:03 -0500

    React to PR feedback

--- src/ServiceControl.Audit/Program.cs
+++ src/ServiceControl.Audit/Program.cs
@@ -1,9 +1,7 @@
 ﻿namespace ServiceControl.Audit
 {
     using System;
-    using System.Configuration;
     using System.IO;
-    using System.Linq;
     using System.Reflection;
     using System.Runtime.Loader;
     using System.Threading.Tasks;
@@ -14,6 +12,7 @@
     using Microsoft.Extensions.Hosting.WindowsServices;
     using NServiceBus.Logging;
     using ServiceControl.Audit.Persistence;
+    using ServiceControl.Configuration;
     using ServiceControl.Transports;
 
     class Program
@@ -25,7 +24,7 @@
             AssemblyLoadContext.Default.Resolving += ResolveAssembly;
             AppDomain.CurrentDomain.UnhandledException += (s, e) => Logger.Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
 
-            ReadExeConfiguration();
+            ExeConfiguration.PopulateAppSettings(Assembly.GetExecutingAssembly());
 
             var arguments = new HostArguments(args);
 
@@ -75,33 +74,6 @@
             }
 
             return null;
-        }
-
-        static void ReadExeConfiguration()
-        {
-            // ConfigurationManager on .NET is looking for {assembly}.dll.config files, but all previous versions of ServiceControl will have {assembly}.exe.config instead.
-            // This code reads in the exe.config files and adds all the values into the ConfigurationManager's collections.
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var location = Path.GetDirectoryName(assembly.Location);
-            var assemblyName = assembly.GetName();
-            var exePath = Path.Combine(location, $"{assemblyName.Name}.exe");
-            var configuration = ConfigurationManager.OpenExeConfiguration(exePath);
-
-            foreach (var key in configuration.AppSettings.Settings.AllKeys)
-            {
-                ConfigurationManager.AppSettings.Set(key, configuration.AppSettings.Settings[key].Value);
-            }
-
-            // This reflection is required because the connection strings collection has had its read only flag set, so it won't let anything be added to it.
-            var type = typeof(ConfigurationElementCollection);
-            var field = type.GetField("_readOnly", BindingFlags.NonPublic | BindingFlags.Instance);
-            field?.SetValue(ConfigurationManager.ConnectionStrings, false);
-
-            foreach (var connectionStringSetting in configuration.ConnectionStrings.ConnectionStrings.Cast<ConnectionStringSettings>())
-            {
-                ConfigurationManager.ConnectionStrings.Add(connectionStringSetting);
-            }
         }
 
         static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
