89a83240644e6d66e16b121e2c0bd1bf3036d244
Jo Palac <j_palac@hotmail.com>
2024-03-06 16:36:38 +1000

    throughput summary, settings adjusted, more tests

--- src/Particular.ThroughputCollector/Contracts/BrokerSettings.cs
+++ src/Particular.ThroughputCollector/Contracts/BrokerSettings.cs
@@ -14,4 +14,37 @@
         public required string Name { get; set; }
         public required string Description { get; set; }
     }
+
+    public static class ServiceControlSettings
+    {
+        public static string API = "ThroughputCollector/ServiceControl/ApiUrl";
+        public static string Queue = "ThroughputCollector/ServiceControl/Queue";
+    }
+
+    public static class SqlServerSettings
+    {
+        public static string ConnectionString = "ThroughputCollector/SqlServer/ConnectionString";
+        public static string AdditionalCatalogs = "ThroughputCollector/SqlServer/AdditionalCatalogs";
+    }
+
+    public static class RabbitMQSettings
+    {
+        public static string API = "ThroughputCollector/RabbitMQ/ApiUrl";
+        public static string UserName = "ThroughputCollector/RabbitMQ/UserName";
+        public static string Password = "ThroughputCollector/RabbitMQ/Password";
+    }
+
+    public static class AzureServiceBusSettings
+    {
+        public static string ResourceId = "ThroughputCollector/ASB/ResourceId";
+        public static string ClientId = "ThroughputCollector/ASB/ClientId";
+        public static string ClientSecret = "ThroughputCollector/ASB/ClientSecret";
+    }
+
+    public static class AmazonSQSSettings
+    {
+        public static string Profile = "ThroughputCollector/AmazonSQS/Profile";
+        public static string Region = "ThroughputCollector/AmazonSQS/Region";
+        public static string Prefix = "ThroughputCollector/AmazonSQS/Prefix";
+    }
 }
