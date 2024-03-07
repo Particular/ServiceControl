bd890ba18550b02a0778c9ece388282e5b56a93d
Jo Palac <j_palac@hotmail.com>
2024-03-07 19:10:17 +1000

    cleanup of broker settings

--- src/Particular.ThroughputCollector/Contracts/BrokerSettings.cs
+++ src/Particular.ThroughputCollector/Contracts/BrokerSettings.cs
@@ -14,40 +14,4 @@
         public required string Name { get; set; }
         public required string Description { get; set; }
     }
-
-    public static class ServiceControlSettings
-    {
-        public static string API = "ServiceControl/ApiUrl";
-        public static string Queue = "ServiceControl/Queue";
-    }
-
-    public static class SqlServerSettings
-    {
-        public static string ConnectionString = "SqlServer/ConnectionString";
-        public static string AdditionalCatalogs = "SqlServer/AdditionalCatalogs";
-    }
-
-    public static class RabbitMQSettings
-    {
-        public static string API = "RabbitMQ/ApiUrl";
-        public static string UserName = "RabbitMQ/UserName";
-        public static string Password = "RabbitMQ/Password";
-    }
-
-    public static class AzureServiceBusSettings
-    {
-        public static string ServiceBusName = "ASB/ServiceBusName";
-        public static string ClientId = "ASB/ClientId";
-        public static string ClientSecret = "ASB/ClientSecret";
-        public static string TenantId = "ASB/TenantId";
-        public static string SubscriptionId = "ASB/SubscriptionId";
-        public static string ManagementUrl = "ASB/ManagementUrl";
-    }
-
-    public static class AmazonSQSSettings
-    {
-        public static string Profile = "AmazonSQS/Profile";
-        public static string Region = "AmazonSQS/Region";
-        public static string Prefix = "AmazonSQS/Prefix";
-    }
 }
