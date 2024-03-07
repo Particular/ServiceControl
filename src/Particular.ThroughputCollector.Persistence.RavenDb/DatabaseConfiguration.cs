ebbc76f97ac5c913a342d8036b5f0d36743b5d38
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-08 03:04:11 +1100

    Refactor throughput persistence layer Implement RavenDb installation for throughput component

--- src/Particular.ThroughputCollector.Persistence.RavenDb/DatabaseConfiguration.cs
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/DatabaseConfiguration.cs
@@ -1,26 +1,6 @@
 ﻿namespace Particular.ThroughputCollector.Persistence.RavenDb;
 
-using System;
-
-public class DatabaseConfiguration(string name,
-    int expirationProcessTimerInSeconds,
-    bool enableFullTextSearch,
-    TimeSpan auditRetentionPeriod,
-    int maxBodySizeToStore,
-    int minimumStorageLeftRequiredForIngestion,
-    ServerConfiguration serverConfiguration)
+public class DatabaseConfiguration(string name)
 {
     public string Name { get; } = name;
-
-    public int ExpirationProcessTimerInSeconds { get; } = expirationProcessTimerInSeconds;
-
-    public bool EnableFullTextSearch { get; } = enableFullTextSearch;
-
-    public ServerConfiguration ServerConfiguration { get; } = serverConfiguration;
-
-    public TimeSpan AuditRetentionPeriod { get; } = auditRetentionPeriod;
-
-    public int MaxBodySizeToStore { get; } = maxBodySizeToStore;
-
-    public int MinimumStorageLeftRequiredForIngestion { get; internal set; } = minimumStorageLeftRequiredForIngestion;
 }
