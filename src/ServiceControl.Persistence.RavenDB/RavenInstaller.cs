fb232e92c6a3c16432f2ac1111a79c1902f64650
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-07 15:26:29 +1100

    Make MS DI container available to SC component's installation tasks Add persistence installer to DI container

--- src/ServiceControl.Persistence.RavenDB/RavenInstaller.cs
+++ src/ServiceControl.Persistence.RavenDB/RavenInstaller.cs
@@ -1,25 +1,16 @@
-﻿namespace ServiceControl.Persistence.RavenDB
+﻿namespace ServiceControl.Persistence.RavenDB;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceControl.Persistence;
+
+class RavenInstaller(IServiceProvider serviceProvider) : IPersistenceInstaller
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Microsoft.Extensions.DependencyInjection;
-    using ServiceControl.Persistence;
-
-    class RavenInstaller : IPersistenceInstaller
+    public async Task Install(CancellationToken cancellationToken)
     {
-        public RavenInstaller(ServiceCollection services)
-        {
-            this.services = services;
-        }
-
-        public async Task Install(CancellationToken cancellationToken)
-        {
-            using var serviceProvider = services.BuildServiceProvider();
-
-            var lifecycle = serviceProvider.GetRequiredService<IPersistenceLifecycle>();
-            await lifecycle.Initialize(cancellationToken);
-        }
-
-        readonly ServiceCollection services;
+        var lifecycle = serviceProvider.GetRequiredService<IPersistenceLifecycle>();
+        await lifecycle.Initialize(cancellationToken);
     }
 }
