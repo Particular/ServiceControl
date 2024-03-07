dab8a2749b9fe590c058debd806d3b062956da70
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-08 03:04:11 +1100

    Refactor throughput persistence layer Implement RavenDb installation for throughput component

--- src/Particular.ThroughputCollector.Persistence.RavenDb/RavenPersistence.cs
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/RavenPersistence.cs
@@ -1,42 +1,19 @@
 ﻿namespace Particular.ThroughputCollector.Persistence.RavenDb;
 
 using Microsoft.Extensions.DependencyInjection;
-//using Persistence.UnitOfWork;
-//using RavenDB.CustomChecks;
-//using UnitOfWork;
 
 class RavenPersistence(DatabaseConfiguration databaseConfiguration) : IPersistence
 {
-    public PersistenceService Configure(IServiceCollection serviceCollection)
+    public IServiceCollection Configure(IServiceCollection services)
     {
-        serviceCollection.AddSingleton(databaseConfiguration);
-        serviceCollection.AddSingleton<IRavenSessionProvider, RavenSessionProvider>();
-        serviceCollection.AddSingleton<IThroughputDataStore, ThroughputDataStore>();
+        services.AddSingleton(databaseConfiguration);
+        services.AddSingleton<IThroughputDataStore, ThroughputDataStore>();
+        services.AddSingleton<IPersistenceInstaller, RavenInstaller>(provider => new RavenInstaller(provider, databaseConfiguration));
+
         //serviceCollection.AddSingleton<IAuditIngestionUnitOfWorkFactory, RavenAuditIngestionUnitOfWorkFactory>();
         //serviceCollection.AddSingleton<IFailedAuditStorage, RavenFailedAuditStorage>();
         //serviceCollection.AddSingleton<CheckMinimumStorageRequiredForAuditIngestion.State>();
 
-        var persistenceService = CreateService();
-
-        if (persistenceService is IRavenDocumentStoreProvider provider)
-        {
-            serviceCollection.AddSingleton(_ => provider);
-        }
-
-        return persistenceService;
-    }
-
-    public IPersistenceInstaller CreateInstaller() => new RavenInstaller(CreateService());
-
-    PersistenceService CreateService()
-    {
-        var serverConfiguration = databaseConfiguration.ServerConfiguration;
-
-        if (serverConfiguration.UseEmbeddedServer)
-        {
-            return new RavenEmbeddedPersistenceLifecycle(databaseConfiguration);
-        }
-
-        return new RavenExternalPersistenceLifecycle(databaseConfiguration);
+        return services;
     }
 }
