f57d67dae2e4331aca1c1001a5d1c7762879742d
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-07 15:26:29 +1100

    Make MS DI container available to SC component's installation tasks Add persistence installer to DI container

--- src/ServiceControl.Persistence.RavenDB/RavenPersistence.cs
+++ src/ServiceControl.Persistence.RavenDB/RavenPersistence.cs
@@ -1,95 +1,98 @@
-﻿namespace ServiceControl.Persistence.RavenDB
+namespace ServiceControl.Persistence.RavenDB;
+
+using CustomChecks;
+using MessageFailures;
+using MessageRedirects;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+using Operations.BodyStorage;
+using Operations.BodyStorage.RavenAttachments;
+using Persistence.MessageRedirects;
+using Persistence.Recoverability;
+using Recoverability;
+using SagaAudit;
+using ServiceControl.CustomChecks;
+using ServiceControl.Infrastructure.RavenDB.Subscriptions;
+using ServiceControl.Recoverability;
+using UnitOfWork;
+
+internal class RavenPersistence(RavenPersisterSettings settings) : IPersistence
 {
-    using CustomChecks;
-    using MessageRedirects;
-    using Microsoft.Extensions.DependencyInjection;
-    using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
-    using Persistence.Recoverability;
-    using Recoverability;
-    using ServiceControl.CustomChecks;
-    using ServiceControl.Infrastructure.RavenDB.Subscriptions;
-    using ServiceControl.MessageFailures;
-    using ServiceControl.Operations.BodyStorage;
-    using ServiceControl.Operations.BodyStorage.RavenAttachments;
-    using ServiceControl.Persistence.MessageRedirects;
-    using ServiceControl.Persistence.UnitOfWork;
-    using ServiceControl.Recoverability;
-    using ServiceControl.SagaAudit;
-
-    class RavenPersistence(RavenPersisterSettings settings) : IPersistence
+    public void AddPersistence(IServiceCollection services)
     {
-        public void AddPersistence(IServiceCollection services)
+        ConfigureLifecycle(services);
+
+        if (settings.MaintenanceMode)
         {
-            ConfigureLifecycle(services);
-
-            if (settings.MaintenanceMode)
-            {
-                return;
-            }
-
-            services.AddSingleton<IServiceControlSubscriptionStorage, RavenSubscriptionStorage>();
-            services.AddSingleton<ISubscriptionStorage>(p => p.GetRequiredService<IServiceControlSubscriptionStorage>());
-
-            services.AddUnitOfWorkFactory<RavenIngestionUnitOfWorkFactory>();
-            services.AddSingleton<ExpirationManager>();
-            services.AddSingleton<MinimumRequiredStorageState>();
-            services.AddSingleton<IBodyStorage, RavenAttachmentsBodyStorage>();
-
-            services.AddSingleton<FailedMessageViewIndexNotifications>();
-            services.AddSingleton<IFailedMessageViewIndexNotifications>(p => p.GetRequiredService<FailedMessageViewIndexNotifications>());
-            services.AddHostedService(p => p.GetRequiredService<FailedMessageViewIndexNotifications>());
-
-            services.AddSingleton<ExternalIntegrationRequestsDataStore>();
-            services.AddSingleton<IExternalIntegrationRequestsDataStore>(p => p.GetRequiredService<ExternalIntegrationRequestsDataStore>());
-            services.AddHostedService(p => p.GetRequiredService<ExternalIntegrationRequestsDataStore>());
-
-            services.AddCustomCheck<CheckRavenDBIndexErrors>();
-            services.AddCustomCheck<CheckRavenDBIndexLag>();
-            services.AddCustomCheck<CheckFreeDiskSpace>();
-            services.AddCustomCheck<CheckMinimumStorageRequiredForIngestion>();
-
-            services.AddSingleton<OperationsManager>();
-
-            services.AddSingleton<IArchiveMessages, MessageArchiver>();
-            services.AddSingleton<ICustomChecksDataStore, RavenCustomCheckDataStore>();
-            services.AddSingleton<IErrorMessageDataStore, ErrorMessagesDataStore>();
-            services.AddSingleton<IEventLogDataStore, EventLogDataStore>();
-            services.AddSingleton<IFailedErrorImportDataStore, FailedErrorImportDataStore>();
-            services.AddSingleton<IGroupsDataStore, GroupsDataStore>();
-            services.AddSingleton<IMessageRedirectsDataStore, MessageRedirectsDataStore>();
-            services.AddSingleton<IMonitoringDataStore, RavenMonitoringDataStore>();
-            services.AddSingleton<IQueueAddressStore, QueueAddressStore>();
-            services.AddSingleton<IRetryBatchesDataStore, RetryBatchesDataStore>();
-            services.AddSingleton<IRetryDocumentDataStore, RetryDocumentDataStore>();
-            services.AddSingleton<IRetryHistoryDataStore, RetryHistoryDataStore>();
-
-            // Forward saga audit messages and warn in ServiceControl 5, remove in 6
-            services.AddSingleton<ISagaAuditDataStore, SagaAuditDeprecationDataStore>();
-            services.AddCustomCheck<SagaAuditDestinationCustomCheck>();
-            services.AddSingleton<SagaAuditDestinationCustomCheck.State>();
+            return;
         }
 
-        public void AddInstaller(IServiceCollection services) => ConfigureLifecycle(services);
+        services.AddSingleton<IServiceControlSubscriptionStorage, RavenSubscriptionStorage>();
+        services.AddSingleton<ISubscriptionStorage>(p => p.GetRequiredService<IServiceControlSubscriptionStorage>());
 
-        void ConfigureLifecycle(IServiceCollection services)
+        services.AddUnitOfWorkFactory<RavenIngestionUnitOfWorkFactory>();
+        services.AddSingleton<ExpirationManager>();
+        services.AddSingleton<MinimumRequiredStorageState>();
+        services.AddSingleton<IBodyStorage, RavenAttachmentsBodyStorage>();
+
+        services.AddSingleton<FailedMessageViewIndexNotifications>();
+        services.AddSingleton<IFailedMessageViewIndexNotifications>(p =>
+            p.GetRequiredService<FailedMessageViewIndexNotifications>());
+        services.AddHostedService(p => p.GetRequiredService<FailedMessageViewIndexNotifications>());
+
+        services.AddSingleton<ExternalIntegrationRequestsDataStore>();
+        services.AddSingleton<IExternalIntegrationRequestsDataStore>(p =>
+            p.GetRequiredService<ExternalIntegrationRequestsDataStore>());
+        services.AddHostedService(p => p.GetRequiredService<ExternalIntegrationRequestsDataStore>());
+
+        services.AddCustomCheck<CheckRavenDBIndexErrors>();
+        services.AddCustomCheck<CheckRavenDBIndexLag>();
+        services.AddCustomCheck<CheckFreeDiskSpace>();
+        services.AddCustomCheck<CheckMinimumStorageRequiredForIngestion>();
+
+        services.AddSingleton<OperationsManager>();
+
+        services.AddSingleton<IArchiveMessages, MessageArchiver>();
+        services.AddSingleton<ICustomChecksDataStore, RavenCustomCheckDataStore>();
+        services.AddSingleton<IErrorMessageDataStore, ErrorMessagesDataStore>();
+        services.AddSingleton<IEventLogDataStore, EventLogDataStore>();
+        services.AddSingleton<IFailedErrorImportDataStore, FailedErrorImportDataStore>();
+        services.AddSingleton<IGroupsDataStore, GroupsDataStore>();
+        services.AddSingleton<IMessageRedirectsDataStore, MessageRedirectsDataStore>();
+        services.AddSingleton<IMonitoringDataStore, RavenMonitoringDataStore>();
+        services.AddSingleton<IQueueAddressStore, QueueAddressStore>();
+        services.AddSingleton<IRetryBatchesDataStore, RetryBatchesDataStore>();
+        services.AddSingleton<IRetryDocumentDataStore, RetryDocumentDataStore>();
+        services.AddSingleton<IRetryHistoryDataStore, RetryHistoryDataStore>();
+
+        // Forward saga audit messages and warn in ServiceControl 5, remove in 6
+        services.AddSingleton<ISagaAuditDataStore, SagaAuditDeprecationDataStore>();
+        services.AddCustomCheck<SagaAuditDestinationCustomCheck>();
+        services.AddSingleton<SagaAuditDestinationCustomCheck.State>();
+    }
+
+    public void AddInstaller(IServiceCollection services) => ConfigureLifecycle(services);
+
+    private void ConfigureLifecycle(IServiceCollection services)
+    {
+        services.AddSingleton<PersistenceSettings>(settings);
+        services.AddSingleton(settings);
+
+        services.AddSingleton<IRavenSessionProvider, RavenSessionProvider>();
+        services.AddHostedService<RavenPersistenceLifecycleHostedService>();
+
+        if (settings.UseEmbeddedServer)
         {
-            services.AddSingleton<PersistenceSettings>(settings);
-            services.AddSingleton(settings);
-
-            services.AddSingleton<IRavenSessionProvider, RavenSessionProvider>();
-            services.AddHostedService<RavenPersistenceLifecycleHostedService>();
-
-            if (settings.UseEmbeddedServer)
-            {
-                services.AddSingleton<RavenEmbeddedPersistenceLifecycle>();
-                services.AddSingleton<IRavenPersistenceLifecycle>(b => b.GetService<RavenEmbeddedPersistenceLifecycle>());
-                services.AddSingleton<IRavenDocumentStoreProvider>(provider => provider.GetRequiredService<RavenEmbeddedPersistenceLifecycle>());
-                return;
-            }
-
-            services.AddSingleton<RavenExternalPersistenceLifecycle>();
-            services.AddSingleton<IRavenPersistenceLifecycle>(b => b.GetService<RavenExternalPersistenceLifecycle>());
-            services.AddSingleton<IRavenDocumentStoreProvider>(provider => provider.GetRequiredService<RavenExternalPersistenceLifecycle>());
+            services.AddSingleton<RavenEmbeddedPersistenceLifecycle>();
+            services.AddSingleton<IRavenPersistenceLifecycle>(b => b.GetService<RavenEmbeddedPersistenceLifecycle>());
+            services.AddSingleton<IRavenDocumentStoreProvider>(provider =>
+                provider.GetRequiredService<RavenEmbeddedPersistenceLifecycle>());
+            return;
         }
+
+        services.AddSingleton<RavenExternalPersistenceLifecycle>();
+        services.AddSingleton<IRavenPersistenceLifecycle>(b => b.GetService<RavenExternalPersistenceLifecycle>());
+        services.AddSingleton<IRavenDocumentStoreProvider>(provider =>
+            provider.GetRequiredService<RavenExternalPersistenceLifecycle>());
     }
 }
