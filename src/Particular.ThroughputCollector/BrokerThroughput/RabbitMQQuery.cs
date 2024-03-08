dfe4b1422971c3c0ccf6be7b74b960cb246be546
John Simons <john.simons@particular.net>
2024-03-08 17:08:29 +1000

    Added the other transport queries shells

---
+++ src/Particular.ThroughputCollector/BrokerThroughput/RabbitMQQuery.cs
@@ -0,0 +1,16 @@
+namespace Particular.ThroughputCollector.Broker;
+
+using System.Collections.Frozen;
+using Persistence;
+
+public class RabbitMQQuery : IThroughputQuery
+{
+    public void Initialise(FrozenDictionary<string, string> settings) => throw new NotImplementedException();
+
+    public IAsyncEnumerable<EndpointThroughput> GetThroughputPerDay(string queueName, DateTime startDate,
+        CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+
+    public IAsyncEnumerable<string> GetQueueNames(CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+}
