de471cd7eaa1b28d8f28e52e662b1734b3a4541f
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-09 09:14:43 +1100

    Fix PlatformConnectionTests.ExposesConnectionDetails test Added placeholder test to throughput RavenDb persistence tests

--- src/Particular.ThroughputCollector/ServiceCollectionExtensions.cs
+++ src/Particular.ThroughputCollector/ServiceCollectionExtensions.cs
@@ -7,46 +7,55 @@ using Particular.ThroughputCollector.Persistence;
 
 static class ServiceCollectionExtensions
 {
-    static string? registeredPersistence = null;
+    static readonly HashSet<string> persistenceTypes = [];
 
+    /// <remarks>
+    /// It is possible for multiple different hosts to be created by Service Control and its associated test infrastructure,
+    /// which means AddPersistence can be called multiple times and potentially with different persistence types
+    /// </remarks>
     public static IServiceCollection AddPersistence(this IServiceCollection services, string persistenceType)
     {
-        if (!string.IsNullOrEmpty(registeredPersistence))
+        if (persistenceTypes.Count == 0)
         {
-            throw new InvalidOperationException("A persistence type has already been registered");
+            // Only add the assembly resolver if this is the first time the method has been called
+            AssemblyLoadContext.Default.Resolving += ResolvePersistenceAssembly;
         }
 
-        registeredPersistence = persistenceType;
-
-        AssemblyLoadContext.Default.Resolving += ResolvePersistenceAssembly;
+        persistenceTypes.Add(persistenceType);
 
         var persistenceConfiguration = PersistenceConfigurationFactory.LoadPersistenceConfiguration(persistenceType);
         var persistenceSettings = persistenceConfiguration.BuildPersistenceSettings();
         var persistence = persistenceConfiguration.Create(persistenceSettings);
-        persistence.Configure(services);
 
-        services.AddSingleton(persistence);
+        if (!services.IsServiceRegistered(persistence.GetType()))
+        {
+            persistence.Configure(services);
+            services.AddSingleton(persistence);
+        }
 
         return services;
     }
 
+    public static bool IsServiceRegistered(this IServiceCollection services, Type serviceType) => services.Any(serviceDescriptor => serviceDescriptor.ServiceType == serviceType);
+
     static Assembly? ResolvePersistenceAssembly(AssemblyLoadContext loadContext, AssemblyName assemblyName)
     {
-        if (registeredPersistence == null)
+        foreach (var persistenceType in persistenceTypes)
         {
-            return null;
+            var persistenceFolder = PersistenceManifestLibrary.GetPersistenceFolder(persistenceType);
+            if (persistenceFolder == null)
+            {
+                continue;
+            }
+
+            var path = Path.Combine(persistenceFolder, $"{assemblyName.Name}.dll");
+
+            if (File.Exists(path))
+            {
+                return loadContext.LoadFromAssemblyPath(path);
+            }
         }
 
-        var persistenceFolder = PersistenceManifestLibrary.GetPersistenceFolder(registeredPersistence);
-        if (persistenceFolder == null)
-        {
-            return null;
-        }
-
-        var path = Path.Combine(persistenceFolder, $"{assemblyName.Name}.dll");
-
-        return File.Exists(path)
-            ? loadContext.LoadFromAssemblyPath(path)
-            : null;
+        return null;
     }
 }
