ee27c6f691db073331db6642c00cb836167971b4
Jo Palac <j_palac@hotmail.com>
2024-03-12 09:57:03 +1000

    added missing api function and started on report generation

--- src/Particular.ThroughputCollector/ThroughputCollector.cs
+++ src/Particular.ThroughputCollector/ThroughputCollector.cs
@@ -1,10 +1,12 @@
 ﻿namespace Particular.ThroughputCollector
 {
     using System.Linq;
-    using System.Net.WebSockets;
+    using System.Threading.Tasks;
     using Particular.ThroughputCollector.Contracts;
     using Particular.ThroughputCollector.Persistence;
     using Particular.ThroughputCollector.Shared;
+
+    //#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     public class ThroughputCollector : IThroughputCollector
     {
@@ -14,13 +16,35 @@
             this.throughputSettings = throughputSettings;
         }
 
-        bool UserIndicatedSendOnly(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.UserIndicatedSendOnly == true);
-        bool UserIndicatedToIgnore(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.UserIndicatedToIgnore == true);
-        bool IsKnownEndpoint(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.EndpointIndicators != null && s.EndpointIndicators.Contains(EndpointIndicator.KnownEndpoint.ToString()));
+        public async Task<BrokerSettings> GetBrokerSettingsInformation()
+        {
+            var brokerSettings = BrokerSettingsLibrary.Find(throughputSettings.Broker);
 
-        //get the max throughput recorded for the endpoints - shouldn't matter where it comes from (ie broker or service control)
-        long? MaxDailyThroughput(IGrouping<string, Endpoint> endpoint) => endpoint.Where(w => w.DailyThroughput != null).SelectMany(s => s.DailyThroughput).MaxBy(m => m.TotalThroughput)?.TotalThroughput;
-        bool ThroughputExistsForThisPeriod(IGrouping<string, Endpoint> endpoint, int days) => endpoint.Where(w => w.DailyThroughput != null).SelectMany(s => s.DailyThroughput).Any(m => m.DateUTC <= DateTime.UtcNow && m.DateUTC >= DateTime.UtcNow.AddDays(-days));
+            brokerSettings ??= new BrokerSettings { Broker = throughputSettings.Broker };
+
+            return await Task.FromResult(brokerSettings);
+        }
+
+        public async Task<BrokerSettingsTestResult> TestBrokerSettings()
+        {
+            //TODO
+            return await Task.FromResult(new BrokerSettingsTestResult { Broker = throughputSettings.Broker, ConnectionSuccessful = true });
+        }
+
+        public async Task UpdateUserSelectionOnEndpointThroughput(List<EndpointThroughputSummary> endpointThroughputs)
+        {
+            await dataStore.UpdateUserIndicationOnEndpoints(endpointThroughputs.Select(e =>
+            {
+                return new Endpoint
+                {
+                    Name = e.Name,
+                    UserIndicatedSendOnly = e.UserIndicatedSendOnly,
+                    UserIndicatedToIgnore = e.UserIndicatedToIgnore,
+                };
+            }).ToList());
+
+            await Task.CompletedTask;
+        }
 
         public async Task<List<EndpointThroughputSummary>> GetThroughputSummary(int? days)
         {
@@ -48,43 +72,66 @@
             return await Task.FromResult(endpointSummaries);
         }
 
-        public async Task UpdateUserSelectionOnEndpointThroughput(List<EndpointThroughputSummary> endpointThroughputs)
+        public async Task<ReportGenerationState> GetReportGenerationState()
         {
-            await dataStore.UpdateUserIndicationOnEndpoints(endpointThroughputs.Select(e =>
-            {
-                return new Endpoint
-                {
-                    Name = e.Name,
-                    UserIndicatedSendOnly = e.UserIndicatedSendOnly,
-                    UserIndicatedToIgnore = e.UserIndicatedToIgnore,
-                };
-            }).ToList());
-
-            await Task.CompletedTask;
+            //TODO
+            return await Task.FromResult(new ReportGenerationState { Broker = throughputSettings.Broker, ConnectionToBrokerWorking = true, EnoughDataToReportOn = true, ReportCanBeGenerated = true });
         }
 
-        public async Task<SignedReport> GenerateThroughputReport(int? days)
+        public async Task<SignedReport> GenerateThroughputReport(int? days, string? prefix, string[]? masks)
         {
             //TODO
 
-            var throughputReport = new SignedReport();
+            days ??= daysToReportOn;
+
+            //get ones with prefix only
+
+            //generate masks and mask the names
+
+            //get all data that we have, including daily values
+
+            var report = new Report
+            {
+                StartTime = new DateTimeOffset(DateTime.UtcNow.Date.AddDays(-(int)days), TimeSpan.Zero),
+                EndTime = new DateTimeOffset(DateTime.UtcNow.Date, TimeSpan.Zero),
+                ReportDuration = TimeSpan.FromDays(1),
+                CustomerName = throughputSettings.CustomerName, //who the license is registeredTo
+                ReportMethod = Mask("TODO"),
+                ScopeType = "TODO",
+                Prefix = prefix,
+                MessageTransport = throughputSettings.Broker.ToString(),// "TODO",
+                ToolVersion = "1",
+            };
+
+
+
+            var throughputReport = new SignedReport() { ReportData = report, Signature = GetSignature() };
             return await Task.FromResult(throughputReport);
         }
 
-        public async Task<BrokerSettings> GetBrokerSettingsInformation()
+
+        string Mask(string stringToMask)
         {
-            var brokerSettings = BrokerSettingsLibrary.Find(throughputSettings.Broker);
+            foreach (var (mask, replacement) in masks)
+            {
+                stringToMask = stringToMask.Replace(mask, replacement, StringComparison.OrdinalIgnoreCase);
+            }
 
-            brokerSettings ??= new BrokerSettings { Broker = throughputSettings.Broker };
-
-            return await Task.FromResult(brokerSettings);
+            return stringToMask;
         }
 
-        public async Task<BrokerSettingsTestResult> TestBrokerSettings()
+        string GetSignature()
         {
-            //TODO
-            return await Task.FromResult(new BrokerSettingsTestResult { Broker = throughputSettings.Broker, ConnectionSuccessful = true });
+            return string.Empty; //TODO
         }
+
+        bool UserIndicatedSendOnly(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.UserIndicatedSendOnly == true);
+        bool UserIndicatedToIgnore(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.UserIndicatedToIgnore == true);
+        bool IsKnownEndpoint(IGrouping<string, Endpoint> endpoint) => endpoint.Any(s => s.EndpointIndicators != null && s.EndpointIndicators.Contains(EndpointIndicator.KnownEndpoint.ToString()));
+
+        //get the max throughput recorded for the endpoints - shouldn't matter where it comes from (ie broker or service control)
+        long? MaxDailyThroughput(IGrouping<string, Endpoint> endpoint) => endpoint.Where(w => w.DailyThroughput != null).SelectMany(s => s.DailyThroughput).MaxBy(m => m.TotalThroughput)?.TotalThroughput;
+        bool ThroughputExistsForThisPeriod(IGrouping<string, Endpoint> endpoint, int days) => endpoint.Where(w => w.DailyThroughput != null).SelectMany(s => s.DailyThroughput).Any(m => m.DateUTC <= DateTime.UtcNow && m.DateUTC >= DateTime.UtcNow.AddDays(-days));
 
         async Task<List<Endpoint>> GetRelevantEndpoints()
         {
@@ -97,5 +144,6 @@
         readonly IThroughputDataStore dataStore;
         readonly ThroughputSettings throughputSettings;
         readonly int daysToReportOn = 30;
+        (string Mask, string Replacement)[] masks = [];
     }
 }
