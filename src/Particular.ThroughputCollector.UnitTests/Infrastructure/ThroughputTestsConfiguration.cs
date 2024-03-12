3aaa046594824b3aeb7426b23dba6555d1c4ae59
Jo Palac <j_palac@hotmail.com>
2024-03-12 17:45:24 +1000

    Remove days option as not limiting the report to days Add serviceControl Version to throughput settings Pass in SP version in the call to generate the report

--- src/Particular.ThroughputCollector.UnitTests/Infrastructure/ThroughputTestsConfiguration.cs
+++ src/Particular.ThroughputCollector.UnitTests/Infrastructure/ThroughputTestsConfiguration.cs
@@ -23,7 +23,7 @@
             var persistence = config.Create(settings);
             persistence.Configure(serviceCollection);
 
-            var throughputSettings = new ThroughputSettings(broker: Broker.ServiceControl, transportConnectionString: "", serviceControlAPI: "http://localhost:33333/api", serviceControlQueue: "Particular.ServiceControl", errorQueue: "error", persistenceType: "InMemory", customerName: "TestCustomer", auditQueue: "audit");
+            var throughputSettings = new ThroughputSettings(broker: Broker.ServiceControl, transportConnectionString: "", serviceControlAPI: "http://localhost:33333/api", serviceControlQueue: "Particular.ServiceControl", errorQueue: "error", persistenceType: "InMemory", customerName: "TestCustomer", serviceControlVersion: "5.0.1", auditQueue: "audit");
             setThroughputSettings(throughputSettings);
             serviceCollection.AddSingleton(throughputSettings);
             serviceCollection.AddSingleton<IThroughputCollector, ThroughputCollector>();
