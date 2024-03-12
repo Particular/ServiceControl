b002c2b175f8311352fa9705b2bcb1d9d0d93ca9
Jo Palac <j_palac@hotmail.com>
2024-03-12 17:45:24 +1000

    Remove days option as not limiting the report to days Add serviceControl Version to throughput settings Pass in SP version in the call to generate the report

---
+++ src/ServiceControl/Infrastructure/ServiceControlVersion.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Infrastructure
+{
+    using ServiceControl.Infrastructure.WebApi;
+    using System.Reflection;
+
+    public class ServiceControlVersion
+    {
+        public static string GetFileVersion()
+        {
+            var customAttributes = typeof(XParticularVersionHttpHandler).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
+
+            if (customAttributes.Length >= 1)
+            {
+                var fileVersionAttribute = (AssemblyInformationalVersionAttribute)customAttributes[0];
+                var informationalVersion = fileVersionAttribute.InformationalVersion;
+                return informationalVersion.Split('+')[0];
+            }
+
+            return typeof(XParticularVersionHttpHandler).Assembly.GetName().Version.ToString(4);
+        }
+    }
+}
