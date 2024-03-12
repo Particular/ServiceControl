527c34478e78e96b05836e1274113706572e29d4
Daniel Marbach <daniel.marbach@openplace.net>
2024-03-12 10:10:35 +0100

    Apply suggestions from code review
    Co-authored-by: Mauro Servienti <mauro.servienti@gmail.com>

--- src/ServiceControl.AcceptanceTests/TestSupport/WebApplicationBuilderExtensions.cs
+++ src/ServiceControl.AcceptanceTests/TestSupport/WebApplicationBuilderExtensions.cs
@@ -24,7 +24,7 @@ static class WebApplicationBuilderExtensions
         // This facilitates receiving the test server anywhere where DI is available
         hostBuilder.Services.AddSingleton(provider => (TestServer)provider.GetRequiredService<IServer>());
 
-        // By default ASP.NET Core uses entry point assembly to discover controllers from. When running
+        // By default, ASP.NET Core uses the entry point assembly to discover controllers. When running
         // inside a test runner the runner exe becomes the entry point which obviously has no controllers in it ;)
         // so we are explicitly registering all necessary application parts.
         var addControllers = hostBuilder.Services.AddControllers();
