f6fef8869658f0f53cddf6c9c2d58441c58376e7
Jo Palac <j_palac@hotmail.com>
2024-03-13 12:21:59 +1000

    Changed user indicators to be a string Added method to see if report can be generated Cleaned up tests

--- src/Particular.ThroughputCollector/IThroughputCollector.cs
+++ src/Particular.ThroughputCollector/IThroughputCollector.cs
@@ -5,7 +5,7 @@
     public interface IThroughputCollector
     {
         Task<List<EndpointThroughputSummary>> GetThroughputSummary();
-        Task UpdateUserSelectionOnEndpointThroughput(List<EndpointThroughputSummary> endpointsThroughputSummary);
+        Task UpdateUserIndicatorsOnEndpoints(List<EndpointThroughputSummary> endpointsThroughputSummary);
         Task<BrokerSettings> GetBrokerSettingsInformation();
         Task<BrokerSettingsTestResult> TestBrokerSettings();
         Task<SignedReport> GenerateThroughputReport(string? prefix, string[]? masks, string? spVersion);
