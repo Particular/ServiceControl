255b0ddc8520e36f0982663080abb7facfbf5de3
John Simons <john.simons@particular.net>
2024-03-13 13:50:24 +1000

    Adding scopes

--- src/Particular.ThroughputCollector/BrokerThroughput/SqlServer/QueueTableName.cs
+++ src/Particular.ThroughputCollector/BrokerThroughput/SqlServer/QueueTableName.cs
@@ -5,6 +5,7 @@ using ThroughputCollector.Broker;
 public class QueueTableName(DatabaseDetails databaseDetails, string tableSchema, string tableName)
     : IQueueName
 {
+    public Func<string?> GetScope { get; set; } = () => null;
     public DatabaseDetails DatabaseDetails { get; } = databaseDetails;
     public string Schema { get; } = tableSchema;
     public string Name { get; } = tableName;
@@ -12,4 +13,7 @@ public class QueueTableName(DatabaseDetails databaseDetails, string tableSchema,
     public string FullName => $"[{Schema}].[{Name}]";
 
     public string QueueName => $"[{DatabaseDetails.DatabaseName}].{FullName}";
+
+    public string DatabaseNameAndSchema => $"[{DatabaseDetails.DatabaseName}].[{Schema}]";
+
 }
