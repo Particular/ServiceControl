8759e249b5c6fe27202ee8377c3607b80bce44cf
Jo Palac <j_palac@hotmail.com>
2024-03-14 10:00:20 +1000

    Added BrokerData to store report wide settings/analytical data from the broker

---
+++ src/Particular.ThroughputCollector.Persistence/BrokerData.cs
@@ -0,0 +1,10 @@
+﻿namespace Particular.ThroughputCollector.Persistence;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+public record BrokerData
+{
+    public Broker Broker { get; set; }
+    public string ScopeType { get; set; }
+    public string Version { get; set; }
+}
