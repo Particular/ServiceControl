ad99bb4762a317aa107e3fa8941b1bf07243cbe7
Jo Palac <j_palac@hotmail.com>
2024-03-14 10:00:20 +1000

    Added BrokerData to store report wide settings/analytical data from the broker

---
+++ src/Particular.ThroughputCollector.Persistence.Tests/BrokerDataTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Particular.ThroughputCollector.Persistence.Tests
+{
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class BrokerDataTests : PersistenceTestFixture
+    {
+        [Test]
+        public async Task Should_add_new_broker_data_when_none_exists_for_broker()
+        {
+            var broker = Broker.RabbitMQ;
+
+            var existingBrokerData = await DataStore.GetBrokerData(broker);
+
+            Assert.That(existingBrokerData, Is.Null);
+
+            await DataStore.SaveBrokerData(broker, null, "1.2");
+
+            existingBrokerData = await DataStore.GetBrokerData(broker);
+
+            Assert.That(existingBrokerData, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task Should_update_existing_broker_data_for_the_same_broker()
+        {
+            var broker = Broker.AmazonSQS;
+
+            var existingBrokerData = await DataStore.GetBrokerData(broker);
+
+            Assert.That(existingBrokerData, Is.Null);
+
+            await DataStore.SaveBrokerData(broker, null, "1.2");
+            existingBrokerData = await DataStore.GetBrokerData(broker);
+
+            Assert.That(existingBrokerData, Is.Not.Null);
+            Assert.That(existingBrokerData.Version, Is.EqualTo("1.2"));
+            Assert.That(existingBrokerData.ScopeType, Is.Null);
+
+            await DataStore.SaveBrokerData(broker, "scope", null);
+            existingBrokerData = await DataStore.GetBrokerData(broker);
+
+            Assert.That(existingBrokerData, Is.Not.Null);
+            Assert.That(existingBrokerData.Version, Is.EqualTo("1.2"));
+            Assert.That(existingBrokerData.ScopeType, Is.EqualTo("scope"));
+        }
+    }
+}
