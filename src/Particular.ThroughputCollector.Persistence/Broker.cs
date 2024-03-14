58550817f8f98d8834a091be18284df73631f620
Jo Palac <j_palac@hotmail.com>
2024-03-14 10:00:20 +1000

    Added BrokerData to store report wide settings/analytical data from the broker

---
+++ src/Particular.ThroughputCollector.Persistence/Broker.cs
@@ -0,0 +1,11 @@
+﻿namespace Particular.ThroughputCollector.Persistence
+{
+    public enum Broker
+    {
+        ServiceControl,
+        AmazonSQS,
+        RabbitMQ,
+        AzureServiceBus,
+        SqlServer
+    }
+}
