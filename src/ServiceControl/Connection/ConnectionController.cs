ec0b8e500d0d27cdda36534e081124e19a82dc64
danielmarbach <daniel.marbach@openplace.net>
2024-03-15 11:24:10 +0100

    Connection Details parsing is case insensitive with PropertyNameCaseInsensitive = true in the platform connector package

--- src/ServiceControl/Connection/ConnectionController.cs
+++ src/ServiceControl/Connection/ConnectionController.cs
@@ -1,5 +1,7 @@
 ﻿namespace ServiceControl.Connection
 {
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Text.Json;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
@@ -13,11 +15,12 @@
         public async Task<IActionResult> GetConnectionDetails()
         {
             var platformConnectionDetails = await builder.BuildPlatformConnection();
-            // TODO why do these properties need to be lower cased?
-            var connectionDetails = new { settings = platformConnectionDetails.ToDictionary(), errors = platformConnectionDetails.Errors };
+            var connectionDetails = new ConnectionDetails(platformConnectionDetails.ToDictionary(), platformConnectionDetails.Errors);
             // by default snake case is used for serialization so we take care of explicitly serializing here
             var content = JsonSerializer.Serialize(connectionDetails);
             return Content(content, "application/json");
         }
+
+        public record ConnectionDetails(IDictionary<string, object> Settings, ConcurrentBag<string> Errors);
     }
 }
