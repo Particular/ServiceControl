485ab1dea8b620260609e0effd3f31b69a20a931
Jo Palac <j_palac@hotmail.com>
2024-03-19 15:58:38 +1000

    touchups

--- src/Particular.ThroughputCollector/ThroughputCollector.cs
+++ src/Particular.ThroughputCollector/ThroughputCollector.cs
@@ -6,8 +6,6 @@
     using Particular.ThroughputCollector.Persistence;
     using Particular.ThroughputCollector.Shared;
 
-    //#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
     public class ThroughputCollector : IThroughputCollector
     {
         public ThroughputCollector(IThroughputDataStore dataStore, ThroughputSettings throughputSettings)
