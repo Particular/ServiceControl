7ae586c1ec5e60f5c54d6d59c0bafa800a2c1739
Jo Palac <j_palac@hotmail.com>
2024-03-20 17:21:52 +1000

    Take out contracts into their own project so can be shared around

--- src/Particular.ThroughputCollector.Contracts/BrokerData.cs
+++ src/Particular.ThroughputCollector.Contracts/BrokerData.cs
@@ -1,4 +1,4 @@
-﻿namespace Particular.ThroughputCollector.Persistence;
+﻿namespace Particular.ThroughputCollector.Contracts;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
