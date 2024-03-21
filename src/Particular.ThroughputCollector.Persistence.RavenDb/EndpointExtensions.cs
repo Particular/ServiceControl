295deff060e994c7023817d78a0b099204b45d9c
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-22 05:41:48 +1100

    Implement IThroughputDataStore.RecordEndpointThroughput and IThroughputDataStore.SaveEndpoint

--- src/Particular.ThroughputCollector.Persistence.RavenDb/EndpointExtensions.cs
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/EndpointExtensions.cs
@@ -9,5 +9,32 @@ static class EndpointExtensions
 
     public static string GenerateDocumentId(this EndpointDocument endpoint) => endpoint.EndpointId.GenerateDocumentId();
 
-    public static Endpoint ToEndpoint(this EndpointDocument endpointDocument) => new(endpointDocument.EndpointId);
+    public static Endpoint ToEndpoint(this EndpointDocument endpointDocument) => new(endpointDocument.EndpointId)
+    {
+        SanitizedName = endpointDocument.SanitizedName,
+        EndpointIndicators = [.. endpointDocument.EndpointIndicators],
+        UserIndicator = endpointDocument.UserIndicator,
+        Scope = endpointDocument.Scope
+    };
+
+    public static EndpointDocument ToEndpointDocument(this Endpoint endpoint)
+    {
+        var document = new EndpointDocument(endpoint.Id)
+        {
+            SanitizedName = endpoint.SanitizedName,
+            UserIndicator = endpoint.UserIndicator
+        };
+
+        foreach (var indicator in endpoint.EndpointIndicators)
+        {
+            document.EndpointIndicators.Add(indicator);
+        }
+
+        if (!string.IsNullOrEmpty(endpoint.Scope))
+        {
+            document.Scope = endpoint.Scope;
+        }
+
+        return document;
+    }
 }
