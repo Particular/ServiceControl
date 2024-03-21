b3f62f714a4de96f8a26e9e7f13d7368997b1ab9
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-22 04:16:28 +1100

    Implement IThroughputDataStore.GetAllEndpoints

---
+++ src/Particular.ThroughputCollector.Persistence.RavenDb/Models/EndpointDocument.cs
@@ -0,0 +1,8 @@
+﻿namespace Particular.ThroughputCollector.Persistence.RavenDb.Models;
+
+using Particular.ThroughputCollector.Contracts;
+
+class EndpointDocument(EndpointIdentifier id)
+{
+    public EndpointIdentifier EndpointId { get; } = id;
+}
