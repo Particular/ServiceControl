344b148782abaec567bf3e5ee3a914d4e20cd78a
Jo Palac <j_palac@hotmail.com>
2024-03-21 10:59:36 +1000

    Cleanup of reportgenerationstate and implementation of TestConnectionSettings for audit

--- src/Particular.ThroughputCollector.UnitTests/Infrastructure/ThroughputTestsConfiguration.cs
+++ src/Particular.ThroughputCollector.UnitTests/Infrastructure/ThroughputTestsConfiguration.cs
@@ -1,11 +1,14 @@
 ﻿namespace Particular.ThroughputCollector.UnitTests.Infrastructure
 {
     using System;
+    using System.Reflection.Metadata.Ecma335;
     using System.Threading.Tasks;
     using Microsoft.Extensions.DependencyInjection;
     using Particular.ThroughputCollector.Infrastructure;
     using Particular.ThroughputCollector.Persistence;
     using Particular.ThroughputCollector.Persistence.InMemory;
+    using ServiceControl.Api;
+    using ServiceControl.Api.Contracts;
 
     partial class ThroughputTestsConfiguration
     {
@@ -25,6 +28,7 @@
             var throughputSettings = new ThroughputSettings(broker: Contracts.Broker.None, transportConnectionString: "", serviceControlAPI: "http://localhost:33333/api", serviceControlQueue: "Particular.ServiceControl", errorQueue: "error", persistenceType: "InMemory", customerName: "TestCustomer", serviceControlVersion: "5.0.1", auditQueue: "audit");
             setThroughputSettings(throughputSettings);
             serviceCollection.AddSingleton(throughputSettings);
+            serviceCollection.AddSingleton<IConfigurationApi, FakeConfigurationApi>();
             serviceCollection.AddSingleton<IThroughputCollector, ThroughputCollector>();
             //serviceCollection.AddHostedService<AuditThroughputCollectorHostedService>();
             //serviceCollection.AddHostedService<BrokerThroughputCollectorHostedService>();
@@ -42,5 +46,16 @@
         {
             return Task.CompletedTask;
         }
+    }
+
+    class FakeConfigurationApi : IConfigurationApi
+    {
+        public object GetConfig() => throw new NotImplementedException();
+        public Task<RemoteConfiguration[]> GetRemoteConfigs()
+        {
+            return Task.FromResult<RemoteConfiguration[]>([new RemoteConfiguration { ApiUri = "http://localhost:44444", Status = "online", Version = "5.1.0" }]);
+        }
+
+        public RootUrls GetUrls(string baseUrl) => throw new NotImplementedException();
     }
 }
