6660d8a459c9990d5fb0b46996660935bf2bbd6d
Andreas Bednarz <andreas.bednarz@particular.net>
2024-03-22 03:08:21 +1100

    Propagate CancellationToken to ConfigurationApi.GetRemoteConfigs

--- src/ServiceControl/Infrastructure/Api/ConfigurationApi.cs
+++ src/ServiceControl/Infrastructure/Api/ConfigurationApi.cs
@@ -1,135 +1,134 @@
-﻿namespace ServiceControl.Infrastructure.Api
+﻿namespace ServiceControl.Infrastructure.Api;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+using Configuration;
+using Particular.ServiceControl.Licensing;
+using ServiceBus.Management.Infrastructure.Settings;
+using ServiceControl.Api;
+using ServiceControl.Api.Contracts;
+
+class ConfigurationApi(ActiveLicense license,
+    LoggingSettings loggingSettings,
+    Settings settings,
+    IHttpClientFactory httpClientFactory) : IConfigurationApi
 {
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Net.Http;
-    using System.Text.Json.Nodes;
-    using System.Threading.Tasks;
-    using Configuration;
-    using Particular.ServiceControl.Licensing;
-    using ServiceBus.Management.Infrastructure.Settings;
-    using ServiceControl.Api;
-    using ServiceControl.Api.Contracts;
-
-    class ConfigurationApi(ActiveLicense license,
-        LoggingSettings loggingSettings,
-        Settings settings,
-        IHttpClientFactory httpClientFactory) : IConfigurationApi
+    public RootUrls GetUrls(string baseUrl)
     {
-        public RootUrls GetUrls(string baseUrl)
+        var model = new RootUrls
         {
-            var model = new RootUrls
-            {
-                EndpointsUrl = baseUrl + "endpoints",
-                KnownEndpointsUrl = "/endpoints/known", // relative URI to allow proxying
-                SagasUrl = baseUrl + "sagas",
-                ErrorsUrl = baseUrl + "errors/{?page,per_page,direction,sort}",
-                EndpointsErrorUrl = baseUrl + "endpoints/{name}/errors/{?page,per_page,direction,sort}",
-                MessageSearchUrl =
-                    baseUrl + "messages/search/{keyword}/{?page,per_page,direction,sort}",
-                EndpointsMessageSearchUrl =
-                    baseUrl +
-                    "endpoints/{name}/messages/search/{keyword}/{?page,per_page,direction,sort}",
-                EndpointsMessagesUrl =
-                    baseUrl + "endpoints/{name}/messages/{?page,per_page,direction,sort}",
-                AuditCountUrl = baseUrl + "endpoints/{name}/audit-count",
-                Name = SettingsReader.Read(Settings.SettingsRootNamespace, "Name", "ServiceControl"),
-                Description = SettingsReader.Read(Settings.SettingsRootNamespace, "Description", "The management backend for the Particular Service Platform"),
-                LicenseStatus = license.IsValid ? "valid" : "invalid",
-                LicenseDetails = baseUrl + "license",
-                Configuration = baseUrl + "configuration",
-                RemoteConfiguration = baseUrl + "configuration/remotes",
-                EventLogItems = baseUrl + "eventlogitems",
-                ArchivedGroupsUrl = baseUrl + "errors/groups/{classifier?}",
-                GetArchiveGroup = baseUrl + "archive/groups/id/{groupId}",
-            };
+            EndpointsUrl = baseUrl + "endpoints",
+            KnownEndpointsUrl = "/endpoints/known", // relative URI to allow proxying
+            SagasUrl = baseUrl + "sagas",
+            ErrorsUrl = baseUrl + "errors/{?page,per_page,direction,sort}",
+            EndpointsErrorUrl = baseUrl + "endpoints/{name}/errors/{?page,per_page,direction,sort}",
+            MessageSearchUrl =
+                baseUrl + "messages/search/{keyword}/{?page,per_page,direction,sort}",
+            EndpointsMessageSearchUrl =
+                baseUrl +
+                "endpoints/{name}/messages/search/{keyword}/{?page,per_page,direction,sort}",
+            EndpointsMessagesUrl =
+                baseUrl + "endpoints/{name}/messages/{?page,per_page,direction,sort}",
+            AuditCountUrl = baseUrl + "endpoints/{name}/audit-count",
+            Name = SettingsReader.Read(Settings.SettingsRootNamespace, "Name", "ServiceControl"),
+            Description = SettingsReader.Read(Settings.SettingsRootNamespace, "Description", "The management backend for the Particular Service Platform"),
+            LicenseStatus = license.IsValid ? "valid" : "invalid",
+            LicenseDetails = baseUrl + "license",
+            Configuration = baseUrl + "configuration",
+            RemoteConfiguration = baseUrl + "configuration/remotes",
+            EventLogItems = baseUrl + "eventlogitems",
+            ArchivedGroupsUrl = baseUrl + "errors/groups/{classifier?}",
+            GetArchiveGroup = baseUrl + "archive/groups/id/{groupId}",
+        };
 
-            return model;
-        }
+        return model;
+    }
 
 
-        public object GetConfig()
+    public object GetConfig()
+    {
+        object content = new
         {
-            object content = new
+            Host = new
             {
-                Host = new
+                settings.ServiceName,
+                Logging = new
                 {
-                    settings.ServiceName,
-                    Logging = new
-                    {
-                        loggingSettings.LogPath,
-                        LoggingLevel = loggingSettings.LoggingLevel.Name,
-                    }
-                },
-                DataRetention = new
-                {
-                    settings.AuditRetentionPeriod,
-                    settings.ErrorRetentionPeriod
-                },
-                PerformanceTunning = new
-                {
-                    settings.HttpDefaultConnectionLimit,
-                    settings.ExternalIntegrationsDispatchingBatchSize
-                },
-                PersistenceSettings = settings.PersisterSpecificSettings,
-                Transport = new
-                {
-                    settings.TransportType,
-                    settings.ErrorLogQueue,
-                    settings.ErrorQueue,
-                    settings.ForwardErrorMessages
-                },
-                Plugins = new
-                {
-                    settings.HeartbeatGracePeriod
+                    loggingSettings.LogPath,
+                    LoggingLevel = loggingSettings.LoggingLevel.Name,
                 }
-            };
+            },
+            DataRetention = new
+            {
+                settings.AuditRetentionPeriod,
+                settings.ErrorRetentionPeriod
+            },
+            PerformanceTunning = new
+            {
+                settings.HttpDefaultConnectionLimit,
+                settings.ExternalIntegrationsDispatchingBatchSize
+            },
+            PersistenceSettings = settings.PersisterSpecificSettings,
+            Transport = new
+            {
+                settings.TransportType,
+                settings.ErrorLogQueue,
+                settings.ErrorQueue,
+                settings.ForwardErrorMessages
+            },
+            Plugins = new
+            {
+                settings.HeartbeatGracePeriod
+            }
+        };
 
-            return content;
-        }
+        return content;
+    }
 
-        public async Task<object> GetRemoteConfigs()
-        {
-            var remotes = settings.RemoteInstances;
-            var tasks = remotes
-                .Select(async remote =>
+    public async Task<object> GetRemoteConfigs(CancellationToken cancellationToken = default)
+    {
+        var remotes = settings.RemoteInstances;
+        var tasks = remotes
+            .Select(async remote =>
+            {
+                string status = remote.TemporarilyUnavailable ? "unavailable" : "online";
+                var version = "Unknown";
+                HttpClient httpClient = httpClientFactory.CreateClient(remote.InstanceId);
+                JsonNode config = null;
+
+                try
                 {
-                    string status = remote.TemporarilyUnavailable ? "unavailable" : "online";
-                    var version = "Unknown";
-                    HttpClient httpClient = httpClientFactory.CreateClient(remote.InstanceId);
-                    JsonNode config = null;
+                    using var response = await httpClient.GetAsync("/api/configuration", cancellationToken);
 
-                    try
+                    if (response.Headers.TryGetValues("X-Particular-Version", out var values))
                     {
-                        HttpResponseMessage response = await httpClient.GetAsync("/api/configuration");
-
-                        if (response.Headers.TryGetValues("X-Particular-Version", out var values))
-                        {
-                            version = values.FirstOrDefault() ?? "Missing";
-                        }
-
-                        await using Stream stream = await response.Content.ReadAsStreamAsync();
-                        config = await JsonNode.ParseAsync(stream);
-                    }
-                    catch (Exception)
-                    {
-                        status = "error";
+                        version = values.FirstOrDefault() ?? "Missing";
                     }
 
-                    return new
-                    {
-                        ApiUri = remote.BaseAddress,
-                        Version = version,
-                        Status = status,
-                        Configuration = config
-                    };
-                })
-                .ToArray();
+                    await using Stream stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+                    config = await JsonNode.ParseAsync(stream, cancellationToken: cancellationToken);
+                }
+                catch (Exception)
+                {
+                    status = "error";
+                }
 
-            var results = await Task.WhenAll(tasks);
+                return new
+                {
+                    ApiUri = remote.BaseAddress,
+                    Version = version,
+                    Status = status,
+                    Configuration = config
+                };
+            });
 
-            return results;
-        }
+        var results = await Task.WhenAll(tasks);
+
+        return results;
     }
 }
