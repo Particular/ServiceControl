7901ddc12a63cd80b3080bcbaf21b011752a3ab0
John Simons <john.simons@particular.net>
2024-03-22 19:49:01 +1000

    Refactor to use PeriodicTimer
    Which is async

--- src/Particular.ThroughputCollector/AuditThroughput/AuditThroughputCollectorHostedService.cs
+++ src/Particular.ThroughputCollector/AuditThroughput/AuditThroughputCollectorHostedService.cs
@@ -1,106 +1,99 @@
-﻿namespace Particular.ThroughputCollector.Audit;
+﻿namespace Particular.ThroughputCollector.AuditThroughput;
 
 using Contracts;
+using Infrastructure;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Particular.ThroughputCollector.AuditThroughput;
-using Particular.ThroughputCollector.Infrastructure;
-using Particular.ThroughputCollector.Shared;
 using Persistence;
 using ServiceControl.Api;
+using Shared;
 
-class AuditThroughputCollectorHostedService : BackgroundService
+class AuditThroughputCollectorHostedService(
+    ILogger<AuditThroughputCollectorHostedService> logger,
+    ThroughputSettings throughputSettings,
+    IThroughputDataStore dataStore,
+    IConfigurationApi configurationApi,
+    IEndpointsApi endpointsApi,
+    IAuditCountApi auditCountApi,
+    TimeProvider timeProvider) : BackgroundService
 {
-    public AuditThroughputCollectorHostedService(
-        ILogger<AuditThroughputCollectorHostedService> logger,
-        ThroughputSettings throughputSettings,
-        IThroughputDataStore dataStore,
-        IConfigurationApi configurationApi,
-        IEndpointsApi endpointsApi,
-        IAuditCountApi auditCountApi,
-        TimeProvider timeProvider)
-    {
-        this.logger = logger;
-        this.throughputSettings = throughputSettings;
-        this.dataStore = dataStore;
-        this.configurationApi = configurationApi;
-        this.endpointsApi = endpointsApi;
-        this.auditCountApi = auditCountApi;
-        this.timeProvider = timeProvider;
-        backgroundTimer = timeProvider.CreateTimer(async _ => await GatherThroughput(), null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
-    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        logger.LogInformation("Starting AuditThroughputCollector Service");
+        logger.LogInformation($"Starting {nameof(AuditThroughputCollectorHostedService)}");
 
-        var tcs = new TaskCompletionSource();
+        await Task.Delay(TimeSpan.FromSeconds(20), stoppingToken);
 
-        cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
-        cancellationTokenSource.Token.Register(_ => tcs.SetResult(), null);
-
-        backgroundTimer.Change(TimeSpan.FromSeconds(30), TimeSpan.FromDays(1));
-
-        await tcs.Task;
-    }
-
-    async Task GatherThroughput()
-    {
-        var cancellationToken = cancellationTokenSource.Token;
-
-        var utcYesterday = DateOnly.FromDateTime(timeProvider.GetUtcNow().DateTime).AddDays(-1);
-        logger.LogInformation($"Gathering throughput from audit for {utcYesterday.ToShortDateString()}");
+        using PeriodicTimer timer = new(TimeSpan.FromDays(1), timeProvider);
 
         try
         {
-            await VerifyAuditInstances(cancellationToken);
-
-            var knownEndpoints = await AuditCommands.GetKnownEndpoints(endpointsApi);
-            var knownEndpointsLookup = knownEndpoints
-                .ToDictionary(knownEndpoint => new EndpointIdentifier(knownEndpoint.Name, ThroughputSource.Audit));
-
-            if (!knownEndpoints.Any())
+            do
             {
-                logger.LogWarning("No known endpoints could be found.");
-            }
-
-            foreach (var tuple in await dataStore.GetEndpoints(knownEndpointsLookup.Keys, cancellationToken))
-            {
-                var endpointId = tuple.Id;
-                var endpoint = tuple.Endpoint;
-
-                if (endpoint?.LastCollectedDate == utcYesterday)
+                try
                 {
-                    continue;
+                    await GatherThroughput(stoppingToken);
                 }
-
-                var auditCounts = (await AuditCommands.GetAuditCountForEndpoint(auditCountApi, knownEndpointsLookup[endpointId].UrlName)).ToList();
-
-                if (endpoint == null)
+                catch (Exception ex) when (ex is not OperationCanceledException)
                 {
-                    endpoint = ConvertToEndpoint(knownEndpointsLookup[endpointId], auditCounts);
-                    await dataStore.SaveEndpoint(endpoint, cancellationToken);
+                    logger.LogError(ex, "Failed to gather throughput from audit");
                 }
-
-                var missingAuditThroughput = auditCounts
-                    .Where(auditCount => auditCount.UtcDate > endpoint.LastCollectedDate &&
-                                         auditCount.UtcDate < DateOnly.FromDateTime(DateTime.UtcNow))
-                    .Select(auditCount => new EndpointThroughput
-                    {
-                        DateUTC = auditCount.UtcDate,
-                        TotalThroughput = auditCount.Count
-                    });
-
-                await dataStore.RecordEndpointThroughput(endpoint.Id, missingAuditThroughput, cancellationToken);
-            }
+            } while (await timer.WaitForNextTickAsync(stoppingToken));
         }
         catch (OperationCanceledException)
         {
-            logger.LogInformation("Canceled gathering audit throughput");
+            logger.LogInformation($"Stopping {nameof(AuditThroughputCollectorHostedService)} timer");
         }
     }
 
-    Endpoint ConvertToEndpoint(ServiceControlEndpoint scEndpoint, IEnumerable<AuditCount> auditCounts) =>
+    async Task GatherThroughput(CancellationToken cancellationToken)
+    {
+        var utcYesterday = DateOnly.FromDateTime(timeProvider.GetUtcNow().DateTime).AddDays(-1);
+        logger.LogInformation($"Gathering throughput from audit for {utcYesterday.ToShortDateString}");
+
+        await VerifyAuditInstances(cancellationToken);
+
+        var knownEndpoints = (await AuditCommands.GetKnownEndpoints(endpointsApi)).ToArray();
+        var knownEndpointsLookup = knownEndpoints
+            .ToDictionary(knownEndpoint => new EndpointIdentifier(knownEndpoint.Name, ThroughputSource.Audit));
+
+        if (!knownEndpoints.Any())
+        {
+            logger.LogWarning("No known endpoints could be found.");
+        }
+
+        foreach (var tuple in await dataStore.GetEndpoints(knownEndpointsLookup.Keys, cancellationToken))
+        {
+            var endpointId = tuple.Id;
+            var endpoint = tuple.Endpoint;
+
+            if (endpoint?.LastCollectedDate == utcYesterday)
+            {
+                continue;
+            }
+
+            var auditCounts = (await AuditCommands.GetAuditCountForEndpoint(auditCountApi, knownEndpointsLookup[endpointId].UrlName)).ToList();
+
+            if (endpoint == null)
+            {
+                endpoint = ConvertToEndpoint(knownEndpointsLookup[endpointId], auditCounts);
+                await dataStore.SaveEndpoint(endpoint, cancellationToken);
+            }
+
+            var missingAuditThroughput = auditCounts
+                .Where(auditCount => auditCount.UtcDate > endpoint.LastCollectedDate &&
+                                     auditCount.UtcDate < DateOnly.FromDateTime(DateTime.UtcNow))
+                .Select(auditCount => new EndpointThroughput
+                {
+                    DateUTC = auditCount.UtcDate,
+                    TotalThroughput = auditCount.Count
+                });
+
+            await dataStore.RecordEndpointThroughput(endpoint.Id, missingAuditThroughput, cancellationToken);
+        }
+    }
+
+    Endpoint ConvertToEndpoint(ServiceControlEndpoint scEndpoint, List<AuditCount> auditCounts) =>
         new(scEndpoint.Name, ThroughputSource.Audit)
         {
             SanitizedName = EndpointNameSanitizer.SanitizeEndpointName(scEndpoint.Name, throughputSettings.Broker),
@@ -132,20 +125,4 @@ class AuditThroughputCollectorHostedService : BackgroundService
             logger.LogWarning($"At least one ServiceControl Audit instance is either not running the required version ({AuditCommands.MinAuditCountsVersion}) or is not configured for at least 2 days of retention. Audit throughput will not be available.");
         }
     }
-
-    public override void Dispose()
-    {
-        backgroundTimer.Dispose();
-        base.Dispose();
-    }
-
-    readonly ILogger<AuditThroughputCollectorHostedService> logger;
-    readonly ThroughputSettings throughputSettings;
-    readonly IThroughputDataStore dataStore;
-    readonly IConfigurationApi configurationApi;
-    readonly IEndpointsApi endpointsApi;
-    readonly IAuditCountApi auditCountApi;
-    readonly TimeProvider timeProvider;
-    readonly ITimer backgroundTimer;
-    CancellationTokenSource cancellationTokenSource = new();
 }
