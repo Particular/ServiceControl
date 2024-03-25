ff4539e96bb62258882273d67c7f1207c3371e2e
Jo Palac <j_palac@hotmail.com>
2024-03-25 10:43:53 +1000

    return service control settings for when not using a broker

--- src/Particular.ThroughputCollector.Contracts/ThroughputConnectionSettings.cs
+++ src/Particular.ThroughputCollector.Contracts/ThroughputConnectionSettings.cs
@@ -2,14 +2,14 @@
 
 using System.Text.Json.Serialization;
 
-public class BrokerSettings
+public class ThroughputConnectionSettings
 {
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public Broker Broker { get; set; }
-    public List<BrokerSetting> Settings { get; set; } = [];
+    public List<ThroughputConnectionSetting> Settings { get; set; } = [];
 }
 
-public class BrokerSetting(string name, string description)
+public class ThroughputConnectionSetting(string name, string description)
 {
     public string Name { get; } = name;
     public string Description { get; } = description;
