d74b481632cd0312c0f9bb9d9251a1ba21ca4d1e
Brandon Ording <bording@gmail.com>
2024-03-26 17:25:24 -0400

    Simplify instance entry point methods

--- src/ServiceControl/Program.cs
+++ src/ServiceControl/Program.cs
@@ -1,77 +1,27 @@
-﻿namespace Particular.ServiceControl
+﻿using System;
+using System.Reflection;
+using NServiceBus.Logging;
+using Particular.ServiceControl;
+using Particular.ServiceControl.Hosting;
+using ServiceBus.Management.Infrastructure.Settings;
+using ServiceControl.Configuration;
+using ServiceControl.Hosting.Commands;
+
+AppDomain.CurrentDomain.UnhandledException += (s, e) => LogManager.GetLogger(typeof(Program)).Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
+
+ExeConfiguration.PopulateAppSettings(Assembly.GetExecutingAssembly());
+
+var arguments = new HostArguments(args);
+
+if (arguments.Help)
 {
-    using System;
-    using System.IO;
-    using System.Reflection;
-    using System.Runtime.Loader;
-    using System.Threading.Tasks;
-    using global::ServiceControl.Configuration;
-    using global::ServiceControl.Hosting.Commands;
-    using global::ServiceControl.Persistence;
-    using global::ServiceControl.Transports;
-    using Hosting;
-    using NServiceBus.Logging;
-    using ServiceBus.Management.Infrastructure.Settings;
-
-    class Program
-    {
-        static Settings settings;
-
-        static async Task Main(string[] args)
-        {
-            AssemblyLoadContext.Default.Resolving += ResolveAssembly;
-            AppDomain.CurrentDomain.UnhandledException += (s, e) => LogManager.GetLogger(typeof(Program)).Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
-
-            ExeConfiguration.PopulateAppSettings(Assembly.GetExecutingAssembly());
-
-            var arguments = new HostArguments(args);
-
-            if (arguments.Help)
-            {
-                arguments.PrintUsage();
-                return;
-            }
-
-            var loggingSettings = new LoggingSettings();
-            LoggingConfigurator.ConfigureLogging(loggingSettings);
-
-            settings = new Settings(arguments.ServiceName, loggingSettings: loggingSettings);
-
-            await new CommandRunner(arguments.Commands).Execute(arguments, settings);
-        }
-
-        static Assembly ResolveAssembly(AssemblyLoadContext loadContext, AssemblyName assemblyName)
-        {
-            if (settings == null)
-            {
-                return null;
-            }
-
-            var transportFolder = TransportManifestLibrary.GetTransportFolder(settings.TransportType);
-            var assembly = TryLoadAssembly(loadContext, transportFolder, assemblyName);
-
-            if (assembly == null)
-            {
-                var persistenceFolder = PersistenceManifestLibrary.GetPersistenceFolder(settings.PersistenceType);
-                assembly = TryLoadAssembly(loadContext, persistenceFolder, assemblyName);
-            }
-
-            return assembly;
-        }
-
-        static Assembly TryLoadAssembly(AssemblyLoadContext loadContext, string folderPath, AssemblyName assemblyName)
-        {
-            if (folderPath != null)
-            {
-                var path = Path.Combine(folderPath, $"{assemblyName.Name}.dll");
-
-                if (File.Exists(path))
-                {
-                    return loadContext.LoadFromAssemblyPath(path);
-                }
-            }
-
-            return null;
-        }
-    }
+    arguments.PrintUsage();
+    return;
 }
+
+var loggingSettings = new LoggingSettings();
+LoggingConfigurator.ConfigureLogging(loggingSettings);
+
+var settings = new Settings(arguments.ServiceName, loggingSettings: loggingSettings);
+
+await new CommandRunner(arguments.Commands).Execute(arguments, settings);
