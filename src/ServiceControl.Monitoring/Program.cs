b0720fa9dfec611aae78c98a6392b6188c8705ac
Brandon Ording <bording@gmail.com>
2024-03-26 17:25:24 -0400

    Simplify instance entry point methods

--- src/ServiceControl.Monitoring/Program.cs
+++ src/ServiceControl.Monitoring/Program.cs
@@ -1,62 +1,19 @@
-namespace ServiceControl.Monitoring
-{
-    using System;
-    using System.IO;
-    using System.Reflection;
-    using System.Runtime.Loader;
-    using System.Threading.Tasks;
-    using NServiceBus.Logging;
-    using ServiceControl.Configuration;
-    using ServiceControl.Transports;
+using System;
+using System.Reflection;
+using NServiceBus.Logging;
+using ServiceControl.Configuration;
+using ServiceControl.Monitoring;
 
-    static class Program
-    {
-        static Settings settings;
+AppDomain.CurrentDomain.UnhandledException += (s, e) => LogManager.GetLogger(typeof(Program)).Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
 
-        static async Task Main(string[] args)
-        {
-            AssemblyLoadContext.Default.Resolving += ResolveAssembly;
-            AppDomain.CurrentDomain.UnhandledException += (s, e) => LogManager.GetLogger(typeof(Program)).Error("Unhandled exception was caught.", e.ExceptionObject as Exception);
+ExeConfiguration.PopulateAppSettings(Assembly.GetExecutingAssembly());
 
-            ExeConfiguration.PopulateAppSettings(Assembly.GetExecutingAssembly());
+var arguments = new HostArguments(args);
 
-            var arguments = new HostArguments(args);
+var loggingSettings = new LoggingSettings();
+LoggingConfigurator.ConfigureLogging(loggingSettings);
 
-            var loggingSettings = new LoggingSettings();
-            LoggingConfigurator.ConfigureLogging(loggingSettings);
+var settings = new Settings(loggingSettings);
+arguments.ApplyOverridesTo(settings);
 
-            settings = new Settings(loggingSettings);
-            arguments.ApplyOverridesTo(settings);
-
-            await new CommandRunner(arguments.Commands).Execute(settings);
-        }
-
-        static Assembly ResolveAssembly(AssemblyLoadContext loadContext, AssemblyName assemblyName)
-        {
-            if (settings == null)
-            {
-                return null;
-            }
-
-            var transportFolder = TransportManifestLibrary.GetTransportFolder(settings.TransportType);
-            var assembly = TryLoadAssembly(loadContext, transportFolder, assemblyName);
-
-            return assembly;
-        }
-
-        static Assembly TryLoadAssembly(AssemblyLoadContext loadContext, string folderPath, AssemblyName assemblyName)
-        {
-            if (folderPath != null)
-            {
-                var path = Path.Combine(folderPath, $"{assemblyName.Name}.dll");
-
-                if (File.Exists(path))
-                {
-                    return loadContext.LoadFromAssemblyPath(path);
-                }
-            }
-
-            return null;
-        }
-    }
-}
+await new CommandRunner(arguments.Commands).Execute(settings);
