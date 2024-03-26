fe678bfbfc294018620d4151e1eef4d28b15b937
Jo Palac <j_palac@hotmail.com>
2024-03-26 16:53:07 +1000

    added report data to report and started on report generator tests

---
+++ src/Particular.ThroughputCollector.Contracts/EnvironmentData.cs
@@ -0,0 +1,10 @@
+﻿namespace Particular.ThroughputCollector.Contracts;
+using System.Text.Json.Serialization;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum EnvironmentData
+{
+    AuditEnabled,
+    MonitoringEnabled,
+    Version
+}
