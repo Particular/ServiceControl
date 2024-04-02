429bcbaf653a6ff0bf4eedc4c0785b093a018c01
Andreas Bednarz <andreas.bednarz@particular.net>
2024-04-02 17:25:23 +1100

    Use the `PlatformEndpoint` EndpointIndicator to filter out platform endpoints instead of passing through the endpoint names in configuration

--- src/Particular.ThroughputCollector.Persistence/PersistenceSettings.cs
+++ src/Particular.ThroughputCollector.Persistence/PersistenceSettings.cs
@@ -5,7 +5,5 @@ using System.Collections.Generic;
 
 public class PersistenceSettings()
 {
-    public HashSet<string> PlatformEndpointNames { get; } = [];
-
     public IDictionary<string, string> PersisterSpecificSettings { get; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 }
