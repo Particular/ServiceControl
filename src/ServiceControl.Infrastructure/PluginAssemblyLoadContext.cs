fbf007b3183930fba5369e6d34966c8cbf501d8b
Brandon Ording <bording@gmail.com>
2024-04-02 16:57:53 -0400

    Move existing assembly check to after a plugin copy has been found

--- src/ServiceControl.Infrastructure/PluginAssemblyLoadContext.cs
+++ src/ServiceControl.Infrastructure/PluginAssemblyLoadContext.cs
@@ -9,18 +9,22 @@ public class PluginAssemblyLoadContext(string assemblyPath) : AssemblyLoadContex
 
     protected override Assembly Load(AssemblyName assemblyName)
     {
-        foreach (var assembly in Default.Assemblies)
-        {
-            if (assembly.FullName == assemblyName.FullName)
-            {
-                return null;
-            }
-        }
-
         var assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
 
         if (assemblyPath is not null)
         {
+            // Before loading the assembly from the plugin folder, we should check if the assembly has already been loaded by the default context.
+            // This is necessary because we don't have a clean separation of dependencies, so both the instances and the plugins can use the same dependencies.
+            // If we let the plugin context load a separate copy, then we run into problems, where the same type in each context are considered different types.
+            // Since we ensure we are using the same version of dependencies in every project, it should be okay to use the already loaded copy.
+            foreach (var assembly in Default.Assemblies)
+            {
+                if (assembly.FullName == assemblyName.FullName)
+                {
+                    return null;
+                }
+            }
+
             return LoadFromAssemblyPath(assemblyPath);
         }
 
