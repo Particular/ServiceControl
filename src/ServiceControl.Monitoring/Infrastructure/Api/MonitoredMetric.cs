788c88d814376c982ff10ba4f20ba868e1b904f7
Jo Palac <j_palac@hotmail.com>
2024-04-02 17:22:49 +1000

    sending throughput from monitoring instance

---
+++ src/ServiceControl.Monitoring/Infrastructure/Api/MonitoredMetric.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceControl.Monitoring.Infrastructure.Api
+{
+    using System;
+    using ServiceControl.Monitoring.Http.Diagrams;
+    using System.Collections.Generic;
+
+    class MonitoredMetric<T>
+    {
+        public Type StoreType { get; set; }
+        public string ReturnName { get; set; }
+        public Aggregation<T> Aggregate { get; set; }
+    }
+
+    delegate MonitoredValues Aggregation<T>(List<IntervalsStore<T>.IntervalsBreakdown> intervals, HistoryPeriod period);
+}
