5f4b36c81004e33fd060dff1ccd7099ec0d91aac
Jo Palac <j_palac@hotmail.com>
2024-04-02 17:22:49 +1000

    sending throughput from monitoring instance

---
+++ src/ServiceControl.Monitoring/Infrastructure/Api/IEndpointMetricsApi.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceControl.Monitoring.Infrastructure.Api
+{
+    using ServiceControl.Monitoring.Http.Diagrams;
+
+    public interface IEndpointMetricsApi
+    {
+        MonitoredEndpoint[] GetAllEndpointsMetrics(int? history = null);
+
+        MonitoredEndpointDetails GetSingleEndpointMetrics(string endpointName, int? history = null);
+
+        void DeleteEndpointInstance(string endpointName, string instanceId);
+
+        int DisconnectedEndpointCount();
+    }
+}
