eb5be8ae1397eb430cd2858fa635224952fee66e
John Simons <john.simons@particular.net>
2024-04-03 14:49:08 +1000

    Remove statics

--- src/ServiceControl/Infrastructure/Api/AuditCountApi.cs
+++ src/ServiceControl/Infrastructure/Api/AuditCountApi.cs
@@ -5,13 +5,11 @@
     using ServiceControl.CompositeViews.MessageCounting;
     using ServiceControl.Persistence.Infrastructure;
     using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
 
     class AuditCountApi(GetAuditCountsForEndpointApi auditCountsForEndpointApi) : IAuditCountApi
     {
-        public async Task<IList<AuditCount>> GetEndpointAuditCounts(string endpoint)
-        {
-            return (await auditCountsForEndpointApi.Execute(new(new PagingInfo(), endpoint), $"/api/endpoints/{endpoint}/audit-count")).Results;
-        }
+        public async Task<IList<AuditCount>> GetEndpointAuditCounts(string endpoint, CancellationToken token) => (await auditCountsForEndpointApi.Execute(new(new PagingInfo(), endpoint), $"/api/endpoints/{endpoint}/audit-count")).Results;
     }
 }
