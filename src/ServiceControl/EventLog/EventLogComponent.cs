03a25e549516619d12582a1d03e49785f8e13c31
John Simons <john.simons@particular.net>
2024-04-03 17:46:37 +1000

    Adding testing method to brokers

--- src/ServiceControl/EventLog/EventLogComponent.cs
+++ src/ServiceControl/EventLog/EventLogComponent.cs
@@ -1,14 +1,16 @@
 ﻿namespace ServiceControl.EventLog
 {
+    using Infrastructure.DomainEvents;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Particular.ServiceControl;
     using ServiceBus.Management.Infrastructure.Settings;
-    using ServiceControl.Infrastructure.DomainEvents;
+    using Transports;
 
     class EventLogComponent : ServiceControlComponent
     {
-        public override void Configure(Settings settings, IHostApplicationBuilder hostBuilder)
+        public override void Configure(Settings settings, ITransportCustomization transportCustomization,
+            IHostApplicationBuilder hostBuilder)
         {
             var services = hostBuilder.Services;
             services.AddSingleton<EventLogMappings>();
