c135aa7b295161bf3e21154ce237e6f47489e058
Jo Palac <j_palac@hotmail.com>
2024-04-08 15:16:54 +1000

    Added AuditQuery tests

---
+++ src/Particular.ThroughputCollector.UnitTests/Infrastructure/FakeConfigurationApi.cs
@@ -0,0 +1,17 @@
+﻿namespace Particular.ThroughputCollector.UnitTests.Infrastructure
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ServiceControl.Api;
+    using ServiceControl.Api.Contracts;
+
+    class FakeConfigurationApi : IConfigurationApi
+    {
+        public object GetConfig() => throw new NotImplementedException();
+
+        public Task<object> GetRemoteConfigs(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+        public RootUrls GetUrls(string baseUrl) => throw new NotImplementedException();
+    }
+}
