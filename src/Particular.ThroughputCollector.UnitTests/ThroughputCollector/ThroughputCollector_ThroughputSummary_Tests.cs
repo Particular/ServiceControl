860c2e8e9c2e14cdcd2aa7f90b0b21eeabfcfc33
Jo Palac <j_palac@hotmail.com>
2024-04-08 15:16:54 +1000

    Added AuditQuery tests

--- src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_ThroughputSummary_Tests.cs
+++ src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_ThroughputSummary_Tests.cs
@@ -2,9 +2,11 @@
 
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Particular.ThroughputCollector.Contracts;
 using Particular.ThroughputCollector.UnitTests.Infrastructure;
+using ServiceControl.Api;
 
 [TestFixture]
 class ThroughputCollector_ThroughputSummary_Tests : ThroughputCollectorTestFixture
@@ -14,6 +16,13 @@ class ThroughputCollector_ThroughputSummary_Tests : ThroughputCollectorTestFixtu
     public override Task Setup()
     {
         SetThroughputSettings = s => s.Broker = broker;
+
+        SetExtraDependencies = d =>
+        {
+            d.AddSingleton<IConfigurationApi, FakeConfigurationApi>();
+            d.AddSingleton<IEndpointsApi, FakeEndpointApi>();
+            d.AddSingleton<IAuditCountApi, FakeAuditCountApi>();
+        };
 
         return base.Setup();
     }
