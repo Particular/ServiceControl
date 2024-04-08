2758260143341afbaa6b2d2607ecc2586ebfa760
Jo Palac <j_palac@hotmail.com>
2024-04-08 15:16:54 +1000

    Added AuditQuery tests

---
+++ src/Particular.ThroughputCollector.UnitTests/Infrastructure/FakeEndpointApi.cs
@@ -0,0 +1,11 @@
+﻿namespace Particular.ThroughputCollector.UnitTests.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using ServiceControl.Api;
+
+    class FakeEndpointApi : IEndpointsApi
+    {
+        public List<ServiceControl.Api.Contracts.Endpoint> GetEndpoints() => throw new NotImplementedException();
+    }
+}
--- src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_Report_EnvironmentData_Tests.cs
+++ src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_Report_EnvironmentData_Tests.cs
@@ -2,9 +2,11 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Particular.ThroughputCollector.Contracts;
 using Particular.ThroughputCollector.UnitTests.Infrastructure;
+using ServiceControl.Api;
 
 [TestFixture]
 class ThroughputCollector_Report_EnvironmentData_Tests : ThroughputCollectorTestFixture
@@ -13,6 +15,13 @@ class ThroughputCollector_Report_EnvironmentData_Tests : ThroughputCollectorTest
     public override Task Setup()
     {
         SetThroughputSettings = s => s.Broker = broker;
+
+        SetExtraDependencies = d =>
+        {
+            d.AddSingleton<IConfigurationApi, FakeConfigurationApi>();
+            d.AddSingleton<IEndpointsApi, FakeEndpointApi>();
+            d.AddSingleton<IAuditCountApi, FakeAuditCountApi>();
+        };
 
         return base.Setup();
     }
