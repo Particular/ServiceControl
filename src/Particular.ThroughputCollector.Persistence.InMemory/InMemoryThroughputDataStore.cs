dce6662c884c2e03fffbf7f84f1551d2c62b3d33
John Simons <john.simons@particular.net>
2024-04-10 09:08:13 +1000

    introduce sanitized name postfix

--- src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryThroughputDataStore.cs
+++ src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryThroughputDataStore.cs
@@ -8,7 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Contracts;
 
-class InMemoryThroughputDataStore() : IThroughputDataStore
+public class InMemoryThroughputDataStore : IThroughputDataStore
 {
     private readonly EndpointCollection endpoints = [];
     private readonly Dictionary<EndpointIdentifier, ThroughputData> allThroughput = [];
