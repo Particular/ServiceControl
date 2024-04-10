75179878c7bd50dd929dbc9d7207469aecd10df9
Jo Palac <j_palac@hotmail.com>
2024-04-10 10:12:16 +1000

    Added tests for monitoring throughput

--- src/Particular.ThroughputCollector/MonitoringThroughput/MonitoringThroughputFeature.cs
+++ src/Particular.ThroughputCollector/MonitoringThroughput/MonitoringThroughputFeature.cs
@@ -6,16 +6,11 @@ using System.Threading;
 using System;
 using NServiceBus.Features;
 using NServiceBus.Transport;
-using System.Diagnostics;
 using Microsoft.Extensions.Logging;
-using Particular.ThroughputCollector.Persistence;
-using System.Text.Json;
-using Particular.ThroughputCollector.Contracts;
-using Endpoint = Contracts.Endpoint;
 using Particular.ThroughputCollector.Shared;
 using Microsoft.Extensions.DependencyInjection;
 
-public class MonitoringThroughputFeature : Feature
+class MonitoringThroughputFeature : Feature
 {
     public MonitoringThroughputFeature()
     {
@@ -38,44 +33,14 @@ public class MonitoringThroughputFeature : Feature
             onMessage: OnMessage);
     }
 
-    public async Task OnMessage(IServiceProvider serviceProvider, MessageContext context, CancellationToken cancellationToken)
+    async Task OnMessage(IServiceProvider serviceProvider, MessageContext context, CancellationToken cancellationToken)
     {
         var logger = serviceProvider.GetRequiredService<ILogger<MonitoringThroughputFeature>>();
+        var monitoringService = serviceProvider.GetRequiredService<MonitoringService>();
 
         try
         {
-            RecordEndpointThroughputData? message;
-            using (Stream stream = new MemoryStream(context.Body.ToArray()))
-            {
-                message = await JsonSerializer.DeserializeAsync<RecordEndpointThroughputData>(stream, cancellationToken: cancellationToken);
-            }
-
-            if (message != null && message.EndpointThroughputData != null)
-            {
-                Debug.WriteLine($"Throughput data from {message.StartDateTime:yyyy-MM-dd HH:mm} to {message.EndDateTime:yyyy-MM-dd HH:mm} for {message.EndpointThroughputData?.Length} endpoint(s)");
-
-                message.EndpointThroughputData?.ToList().ForEach(async e =>
-                {
-                    var dataStore = serviceProvider.GetRequiredService<IThroughputDataStore>();
-                    var endpoint = await dataStore.GetEndpoint(e.Name, ThroughputSource.Monitoring, cancellationToken);
-                    if (endpoint == null)
-                    {
-                        endpoint = new Endpoint(e.Name, ThroughputSource.Monitoring)
-                        {
-                            SanitizedName = EndpointNameSanitizer.SanitizeEndpointName(e.Name, serviceProvider.GetRequiredService<ThroughputSettings>().Broker),
-                            EndpointIndicators = [EndpointIndicator.KnownEndpoint.ToString()],
-                        };
-                        await dataStore.SaveEndpoint(endpoint, cancellationToken);
-                    }
-
-                    if (e.Throughput > 0)
-                    {
-                        var endpointThroughput = new EndpointDailyThroughput(DateOnly.FromDateTime(message.EndDateTime), e.Throughput);
-
-                        await dataStore.RecordEndpointThroughput(e.Name, ThroughputSource.Monitoring, [endpointThroughput], cancellationToken);
-                    }
-                });
-            }
+            await monitoringService.RecordMonitoringThroughput(context.Body.ToArray(), cancellationToken);
         }
         catch (Exception ex)
         {
