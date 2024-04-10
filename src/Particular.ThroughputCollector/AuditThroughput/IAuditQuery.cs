f0ccf62891e14fd397627cd31fe6abe5808abd22
Jo Palac <j_palac@hotmail.com>
2024-04-10 15:35:16 +1000

    audit service unit tests

---
+++ src/Particular.ThroughputCollector/AuditThroughput/IAuditQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace Particular.ThroughputCollector.AuditThroughput
+{
+    using NuGet.Versioning;
+    using Particular.ThroughputCollector.Contracts;
+
+    public interface IAuditQuery
+    {
+        SemanticVersion MinAuditCountsVersion { get; }
+        Func<RemoteInstanceInformation, bool> ValidRemoteInstances { get; }
+
+        IEnumerable<ServiceControlEndpoint> GetKnownEndpoints();
+
+        Task<IEnumerable<AuditCount>> GetAuditCountForEndpoint(string endpointUrlName, CancellationToken cancellationToken);
+        Task<List<RemoteInstanceInformation>> GetAuditRemotes(CancellationToken cancellationToken);
+        Task<ConnectionSettingsTestResult> TestAuditConnection(CancellationToken cancellationToken);
+
+    }
+}
