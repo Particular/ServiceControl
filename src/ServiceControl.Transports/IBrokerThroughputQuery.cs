4a942bf443bd457061b0ac88a5571e686a4365cf
John Simons <john.simons@particular.net>
2024-04-10 18:24:47 +1000

    Handle initialisation errors differently

--- src/ServiceControl.Transports/IBrokerThroughputQuery.cs
+++ src/ServiceControl.Transports/IBrokerThroughputQuery.cs
@@ -6,6 +6,7 @@ using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 #pragma warning disable CA1711
 public class DefaultBrokerQueue(string queueName) : IBrokerQueue
@@ -19,6 +20,7 @@ public class DefaultBrokerQueue(string queueName) : IBrokerQueue
 
 public interface IBrokerThroughputQuery
 {
+    bool HasInitialisationErrors(out string errorMessage);
     void Initialise(FrozenDictionary<string, string> settings);
     IAsyncEnumerable<QueueThroughput> GetThroughputPerDay(IBrokerQueue brokerQueue, DateOnly startDate,
         CancellationToken cancellationToken);
@@ -28,6 +30,72 @@ public interface IBrokerThroughputQuery
     string? ScopeType { get; }
     KeyDescriptionPair[] Settings { get; }
     Task<(bool Success, List<string> Errors)> TestConnection(CancellationToken cancellationToken);
+}
+
+public abstract class BrokerThroughputQuery(ILogger logger, string transport) : IBrokerThroughputQuery
+{
+    protected readonly List<string> InitialiseErrors = [];
+
+    public bool HasInitialisationErrors(out string errorMessage)
+    {
+        errorMessage = string.Empty;
+
+        if (InitialiseErrors.Count == 0)
+        {
+            return false;
+        }
+
+        errorMessage = string.Join('\n', InitialiseErrors);
+
+        return true;
+    }
+
+    public void Initialise(FrozenDictionary<string, string> settings)
+    {
+        InitialiseErrors.Clear();
+
+        try
+        {
+            InitialiseCore(settings);
+        }
+        catch (Exception e)
+        {
+            InitialiseErrors.Add(e.Message);
+            logger.LogError(e, $"Failed to initialise {GetType().Name}");
+        }
+    }
+
+    protected abstract void InitialiseCore(FrozenDictionary<string, string> settings);
+
+    public abstract IAsyncEnumerable<QueueThroughput> GetThroughputPerDay(IBrokerQueue brokerQueue, DateOnly startDate,
+        CancellationToken cancellationToken);
+
+    public abstract IAsyncEnumerable<IBrokerQueue> GetQueueNames(CancellationToken cancellationToken);
+
+    public Dictionary<string, string> Data { get; set; } = [];
+    public string MessageTransport => transport;
+    public string? ScopeType { get; set; }
+    public abstract KeyDescriptionPair[] Settings { get; }
+
+    public async Task<(bool Success, List<string> Errors)> TestConnection(CancellationToken cancellationToken)
+    {
+        if (InitialiseErrors.Count > 0)
+        {
+            return (false, InitialiseErrors);
+        }
+
+        try
+        {
+            return await TestConnectionCore(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Test connection failed");
+            return (false, [ex.Message]);
+        }
+    }
+
+    public abstract Task<(bool Success, List<string> Errors)> TestConnectionCore(CancellationToken cancellationToken);
 }
 
 public readonly struct KeyDescriptionPair(string key, string description)
