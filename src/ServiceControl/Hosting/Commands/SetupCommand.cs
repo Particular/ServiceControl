7c1639e09b4e25ec1ef91956e3af1d2a7eee0bc4
John Simons <john.simons@particular.net>
2024-04-15 17:09:54 +1000

    Rollback change

--- src/ServiceControl/Hosting/Commands/SetupCommand.cs
+++ src/ServiceControl/Hosting/Commands/SetupCommand.cs
@@ -40,7 +40,7 @@ namespace ServiceControl.Hosting.Commands
                 await installationTask(host.Services);
             }
 
-            await host.RunAsync();
+            await host.StartAsync();
 
             if (settings.SkipQueueCreation)
             {
