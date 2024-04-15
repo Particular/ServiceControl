93233f1c22eb35e3b3b5622c7b5be445d2b010a6
danielmarbach <danielmarbach@users.noreply.github.com>
2024-04-15 15:27:00 +0200

    Try to untangle slightly the hierarchy mess

--- src/ServiceControl.Config/Framework/RaygunFeedBack.cs
+++ src/ServiceControl.Config/Framework/RaygunFeedBack.cs
@@ -1,42 +1,58 @@
-﻿namespace ServiceControl.Config.Framework
+﻿#nullable enable
+
+namespace ServiceControl.Config.Framework
 {
     using System;
     using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Reflection;
     using System.Threading.Tasks;
     using Extensions;
     using Mindscape.Raygun4Net;
 
-    public class RaygunFeedback : RaygunReporter
+    public sealed class RaygunFeedback : IRaygunUserProvider
     {
         public RaygunFeedback()
         {
-            InitializeTrackingId();
+            trackingId = GetOrSetTrackingId();
+
+            raygunClient = new RaygunClient(raygunSettings, this);
+
+            init = Task.Run(async () =>
+            {
+                enabled = await TryInitializeRaygunClientWithCredentials() ||
+                          await TryInitializeRaygunClientWithCredentials(CredentialCache.DefaultCredentials) ||
+                          await TryInitializeRaygunClientWithCredentials(CredentialCache.DefaultNetworkCredentials);
+                version = GetVersion();
+            });
         }
 
-        void InitializeTrackingId()
+        static Guid GetOrSetTrackingId()
         {
-            var trackerlocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Particular");
-            if (!Directory.Exists(trackerlocation))
+            var trackerLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Particular");
+            if (!Directory.Exists(trackerLocation))
             {
-                Directory.CreateDirectory(trackerlocation);
+                Directory.CreateDirectory(trackerLocation);
             }
 
-            var trackingFile = new FileInfo(Path.Combine(trackerlocation, ".feedbackid"));
+            var trackingId = Guid.NewGuid();
+            var trackingFile = new FileInfo(Path.Combine(trackerLocation, ".feedbackid"));
             if (!trackingFile.Exists || !Guid.TryParse(File.ReadAllText(trackingFile.FullName), out trackingId))
             {
                 File.WriteAllText(trackingFile.FullName, trackingId.BareString());
             }
+            return trackingId;
         }
 
         public Task SendFeedBack(string emailAddress, string message, bool includeSystemInfo)
         {
+            var userInfo = ((IRaygunUserProvider)this).GetUser()!;
+            userInfo.Email = emailAddress;
+
             var raygunMessage = RaygunMessageBuilder.New(new RaygunSettings())
-                .SetUser(new RaygunIdentifierMessage(trackingId.BareString())
-                {
-                    Email = emailAddress,
-                    UUID = trackingId.BareString()
-                })
-                .SetVersion(Version)
+                .SetUser(userInfo)
+                .SetVersion(version)
                 .SetExceptionDetails(new Feedback(message));
 
             if (includeSystemInfo)
@@ -51,12 +67,11 @@
 
         public Task SendException(Exception ex, bool includeSystemInfo)
         {
+            var userInfo = ((IRaygunUserProvider)this).GetUser()!;
+
             var raygunMessage = RaygunMessageBuilder.New(new RaygunSettings())
-                .SetUser(new RaygunIdentifierMessage(trackingId.BareString())
-                {
-                    UUID = trackingId.BareString()
-                })
-                .SetVersion(Version)
+                .SetUser(userInfo)
+                .SetVersion(version)
                 .SetExceptionDetails(ex);
 
             if (includeSystemInfo)
@@ -69,13 +84,62 @@
             return raygunClient.Send(m);
         }
 
-        Guid trackingId = Guid.NewGuid();
-    }
-
-    class Feedback : Exception
-    {
-        public Feedback(string message) : base(message)
+        RaygunIdentifierMessage IRaygunUserProvider.GetUser() => new(trackingId.BareString())
         {
+            UUID = trackingId.BareString()
+        };
+
+        public bool Enabled
+        {
+            get
+            {
+                if (!init.IsCompleted)
+                {
+                    init.Wait();
+                }
+
+                return enabled;
+            }
         }
+
+        async Task<bool> TryInitializeRaygunClientWithCredentials(ICredentials? credentials = default)
+        {
+            HttpClient? http = null;
+            try
+            {
+                http = new HttpClient(new HttpClientHandler { Credentials = credentials })
+                {
+                    Timeout = TimeSpan.FromSeconds(5)
+                };
+                using var response = await http.GetAsync(RaygunUrl);
+                response.EnsureSuccessStatusCode();
+
+                raygunClient = new RaygunClient(raygunSettings, http, this);
+                return true;
+            }
+            catch
+            {
+                http?.Dispose();
+                return false;
+            }
+        }
+
+        static string? GetVersion()
+        {
+            var assemblyInfo = Assembly.GetExecutingAssembly().GetAttribute<AssemblyInformationalVersionAttribute>();
+            var versionParts = assemblyInfo?.InformationalVersion.Split('+');
+            return versionParts?[0];
+        }
+
+        bool enabled;
+        string? version;
+        RaygunClient raygunClient;
+        readonly Task init;
+        readonly Guid trackingId;
+        readonly RaygunSettings raygunSettings = new() { ApiKey = "zdm49nndHCXZ3NVzM8Kzug==" };
+
+        const string RaygunUrl = "https://raygun.io";
+
+        sealed class Feedback(string message) : Exception(message);
     }
 }
