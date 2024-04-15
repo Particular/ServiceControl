19c33512aca45f27fa90d643650a4bc9b961843d
John Simons <john.simons@particular.net>
2024-04-15 11:19:46 +1000

    Adding tests

--- src/Particular.ThroughputCollector.Contracts/ConnectionSettingsTestResult.cs
+++ src/Particular.ThroughputCollector.Contracts/ConnectionSettingsTestResult.cs
@@ -4,5 +4,5 @@ public class ConnectionSettingsTestResult
 {
     public bool ConnectionSuccessful { get; set; }
     public List<string> ConnectionErrorMessages { get; set; } = [];
+    public string Diagnostics { get; set; } = string.Empty;
 }
-
