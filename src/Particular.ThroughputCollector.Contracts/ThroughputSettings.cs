292c61b03a196db4feded688ca21113e84ac4afe
Andreas Bednarz <andreas.bednarz@particular.net>
2024-04-16 14:06:41 +1000

    Move throughput collector's persistence assembly resolution to be done by the ThroughputComponent in the primary instance host.  This allows the component's persistence assembly to be loaded into the same AssemblyLoadContext as the host's persistence assembly.

--- src/Particular.ThroughputCollector.Contracts/ThroughputSettings.cs
+++ src/Particular.ThroughputCollector.Contracts/ThroughputSettings.cs
@@ -6,11 +6,10 @@ public class ThroughputSettings
 {
     public static readonly SettingsRootNamespace SettingsNamespace = new("ThroughputCollector");
 
-    public ThroughputSettings(string serviceControlQueue, string errorQueue, string persistenceType, string transportType, string customerName, string serviceControlVersion)
+    public ThroughputSettings(string serviceControlQueue, string errorQueue, string transportType, string customerName, string serviceControlVersion)
     {
         ServiceControlQueue = serviceControlQueue;
         ErrorQueue = errorQueue;
-        PersistenceType = persistenceType;
         TransportType = transportType;
         CustomerName = customerName;
         ServiceControlVersion = serviceControlVersion;
@@ -18,7 +17,6 @@ public class ThroughputSettings
 
     public string ErrorQueue { get; }
     public string ServiceControlQueue { get; }
-    public string PersistenceType { get; set; }
     public string TransportType { get; set; }
     public string CustomerName { get; }
     public string ServiceControlVersion { get; }
--- src/Particular.ThroughputCollector/ServiceCollectionExtensions.cs
+++ src/Particular.ThroughputCollector/ServiceCollectionExtensions.cs
@@ -1,32 +1,13 @@
 ﻿namespace Particular.ThroughputCollector;
 
-using System.Reflection;
-using System.Runtime.Loader;
 using Microsoft.Extensions.DependencyInjection;
 using Particular.ThroughputCollector.Persistence;
 
-static class ServiceCollectionExtensions
+public static class ServiceCollectionExtensions
 {
-    static readonly HashSet<string> PersistenceTypes = [];
-
-    /// <remarks>
-    /// It is possible for multiple different hosts to be created by Service Control and its associated test infrastructure,
-    /// which means AddPersistence can be called multiple times and potentially with different persistence types
-    /// </remarks>
-    public static IServiceCollection AddPersistence(this IServiceCollection services, string persistenceType)
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IPersistenceConfiguration persistenceConfiguration)
     {
-        if (PersistenceTypes.Count == 0)
-        {
-            // Only add the assembly resolver if this is the first time the method has been called
-            AssemblyLoadContext.Default.Resolving += ResolvePersistenceAssembly;
-        }
-
-        PersistenceTypes.Add(persistenceType);
-
-        var persistenceConfiguration = PersistenceConfigurationFactory.LoadPersistenceConfiguration(persistenceType);
         var persistenceSettings = persistenceConfiguration.BuildPersistenceSettings();
-        services.AddSingleton(persistenceSettings);
-
         var persistence = persistenceConfiguration.Create(persistenceSettings);
 
         if (!services.IsServiceRegistered(persistence.GetType()))
@@ -38,26 +19,5 @@ static class ServiceCollectionExtensions
         return services;
     }
 
-    public static bool IsServiceRegistered(this IServiceCollection services, Type serviceType) => services.Any(serviceDescriptor => serviceDescriptor.ServiceType == serviceType);
-
-    static Assembly? ResolvePersistenceAssembly(AssemblyLoadContext loadContext, AssemblyName assemblyName)
-    {
-        foreach (var persistenceType in PersistenceTypes)
-        {
-            var manifest = PersistenceManifestLibrary.Find(persistenceType);
-            if (manifest?.Location == null)
-            {
-                continue;
-            }
-
-            var path = Path.Combine(manifest.Location, $"{assemblyName.Name}.dll");
-
-            if (File.Exists(path))
-            {
-                return loadContext.LoadFromAssemblyPath(path);
-            }
-        }
-
-        return null;
-    }
+    static bool IsServiceRegistered(this IServiceCollection services, Type serviceType) => services.Any(serviceDescriptor => serviceDescriptor.ServiceType == serviceType);
 }
