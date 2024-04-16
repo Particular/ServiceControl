2b876bb77d65bf17f2300a85f6f1f4b9e7324895
John Simons <john.simons@particular.net>
2024-04-17 08:45:34 +1000

    Moving entry point to be explicit

--- src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryPersistence.cs
+++ src/Particular.ThroughputCollector.Persistence.InMemory/InMemoryPersistence.cs
@@ -1,15 +1,12 @@
 ﻿namespace Particular.ThroughputCollector.Persistence.InMemory;
 
 using Microsoft.Extensions.DependencyInjection;
-using Particular.ThroughputCollector.Persistence;
 
-
-public class InMemoryPersistence : IPersistence
+public class InMemoryPersistence
 {
     public IServiceCollection Configure(IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<InMemoryThroughputDataStore>();
-        serviceCollection.AddSingleton<IThroughputDataStore>(sp => sp.GetRequiredService<InMemoryThroughputDataStore>());
+        serviceCollection.AddSingleton<IThroughputDataStore, InMemoryThroughputDataStore>();
         serviceCollection.AddSingleton<IPersistenceInstaller, InMemoryPersistenceInstaller>();
 
         return serviceCollection;
