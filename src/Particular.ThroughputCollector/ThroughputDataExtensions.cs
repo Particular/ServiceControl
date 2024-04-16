0653a5f6f8a37e7c4968dc8e18501aaa66dd9c26
John Simons <john.simons@particular.net>
2024-04-16 15:25:08 +1000

    Fix issues with multiple enumerations

--- src/Particular.ThroughputCollector/ThroughputDataExtensions.cs
+++ src/Particular.ThroughputCollector/ThroughputDataExtensions.cs
@@ -5,13 +5,13 @@ using Particular.ThroughputCollector.Contracts;
 
 static class ThroughputDataExtensions
 {
-    public static IEnumerable<EndpointDailyThroughput> FromSource(this IEnumerable<ThroughputData> throughputs, ThroughputSource source) => throughputs
+    public static IEnumerable<EndpointDailyThroughput> FromSource(this List<ThroughputData> throughputs, ThroughputSource source) => throughputs
         .SingleOrDefault(td => td.ThroughputSource == source)?
         .Select(kvp => new EndpointDailyThroughput(kvp.Key, kvp.Value)) ?? [];
 
-    public static long Sum(this IEnumerable<ThroughputData> throughputs) => throughputs.SelectMany(t => t).Sum(kvp => kvp.Value);
+    public static long Sum(this List<ThroughputData> throughputs) => throughputs.SelectMany(t => t).Sum(kvp => kvp.Value);
 
-    public static long Max(this IEnumerable<ThroughputData> throughputs) =>
+    public static long Max(this List<ThroughputData> throughputs) =>
         throughputs.Any()
             ? throughputs.SelectMany(t => t).Max(kvp => kvp.Value)
             : 0;
