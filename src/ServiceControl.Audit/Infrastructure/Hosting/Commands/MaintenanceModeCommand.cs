f6f648bc42f407d0b81ccf9a2c9d3aa8ce624485
Mauro Servienti <mauro.servienti@gmail.com>
2024-04-18 23:07:55 +0200

    AddWindowsService when running the Audit instance in maintenance mode (#4099)
    * AddWindowsService when running the Audit instance in maintenance mode
    
    * Align Primary and Audit instances maintenance mode

--- src/ServiceControl.Audit/Infrastructure/Hosting/Commands/MaintenanceModeCommand.cs
+++ src/ServiceControl.Audit/Infrastructure/Hosting/Commands/MaintenanceModeCommand.cs
@@ -1,11 +1,9 @@
 ﻿namespace ServiceControl.Audit.Infrastructure.Hosting.Commands
 {
-    using System;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Hosting.WindowsServices;
     using Persistence;
-    using ServiceControl.Audit.Infrastructure.Settings;
+    using Settings;
 
     class MaintenanceModeCommand : AbstractCommand
     {
@@ -19,20 +17,10 @@
             var hostBuilder = Host.CreateApplicationBuilder();
             hostBuilder.Services.AddPersistence(persistenceSettings, persistenceConfiguration);
 
+            hostBuilder.Services.AddWindowsService();
+
             var host = hostBuilder.Build();
-
-            if (WindowsServiceHelpers.IsWindowsService())
-            {
-                await host.RunAsync();
-            }
-            else
-            {
-                await Console.Out.WriteLineAsync("Running in Maintenance Mode - Press CTRL+C to exit");
-
-                await host.RunAsync();
-
-                await Console.Out.WriteLineAsync("Disposing persister (this might take a while)...");
-            }
+            await host.RunAsync();
         }
     }
 }
