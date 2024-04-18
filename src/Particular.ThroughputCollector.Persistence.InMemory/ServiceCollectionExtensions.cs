c6a8453d282491f574486f5be39457fb093eed69
John Simons <john.simons@particular.net>
2024-04-18 14:29:18 +1000

    Change to use inmemory persistence temporarily

---
+++ src/Particular.ThroughputCollector.Persistence.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Particular.ThroughputCollector.Persistence.InMemory;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddThroughputInMemoryPersistence(this IServiceCollection services)
+    {
+        services.AddSingleton<IThroughputDataStore, InMemoryThroughputDataStore>();
+        services.AddSingleton<IPersistenceInstaller, InMemoryPersistenceInstaller>();
+
+        return services;
+    }
+}
