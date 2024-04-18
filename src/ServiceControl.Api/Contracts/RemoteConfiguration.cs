a759decb8300d82a54a86e1073c62ff740003f41
Jo Palac <j_palac@hotmail.com>
2024-04-18 16:17:17 +1000

    Change anonymous type to a shated typed type

---
+++ src/ServiceControl.Api/Contracts/RemoteConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceControl.Api.Contracts
+{
+    using System.Text.Json.Nodes;
+
+    public class RemoteConfiguration
+    {
+        public string ApiUri { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+        public JsonNode Configuration { get; set; }
+    }
+}
