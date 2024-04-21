06ed533232d877dbdf30f26ee865338d244c1da2
Andreas Bednarz <andreas.bednarz@particular.net>
2024-04-21 15:33:34 +1000

    Implement storing broker and audit service metadata related to throughput collection

---
+++ src/Particular.ThroughputCollector.Persistence.Tests/AuditServiceMetadataTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Particular.ThroughputCollector.Persistence.Tests;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Particular.ThroughputCollector.Contracts;
+
+[TestFixture]
+class AuditServiceMetadataTests : PersistenceTestFixture
+{
+    [Test]
+    public async Task Should_retrieve_saved_audit_service_metadata()
+    {
+        //Arrange
+        var expectedAuditServiceMetadata = new AuditServiceMetadata(
+            new Dictionary<string, int> { ["Some version"] = 2 },
+            new Dictionary<string, int> { ["Some transport"] = 3 });
+        await DataStore.SaveAuditServiceMetadata(expectedAuditServiceMetadata);
+
+        //Act
+        var retrievedAuditServiceMetadata = await DataStore.GetAuditServiceMetadata();
+
+        //Assert
+        Assert.That(retrievedAuditServiceMetadata, Is.Not.Null);
+        Assert.That(retrievedAuditServiceMetadata.Transports, Is.EquivalentTo(expectedAuditServiceMetadata.Transports));
+        Assert.That(retrievedAuditServiceMetadata.Versions, Is.EquivalentTo(expectedAuditServiceMetadata.Versions));
+    }
+
+    [Test]
+    public async Task Should_update_existing_audit_service_metadata_if_already_exists()
+    {
+        // Arrange
+        var oldAuditServiceMetadata = new AuditServiceMetadata(
+            new Dictionary<string, int> { ["Some version"] = 2 },
+            new Dictionary<string, int> { ["Some transport"] = 3 });
+        await DataStore.SaveAuditServiceMetadata(oldAuditServiceMetadata);
+
+        // Act
+        var expectedAuditServiceMetadata = new AuditServiceMetadata(
+            new Dictionary<string, int> { ["Some version"] = 2, ["New version"] = 1 },
+            new Dictionary<string, int> { ["Some transport"] = 4 });
+        await DataStore.SaveAuditServiceMetadata(expectedAuditServiceMetadata);
+        var retrievedAuditServiceMetadata = await DataStore.GetAuditServiceMetadata();
+
+        // Assert
+        Assert.That(retrievedAuditServiceMetadata, Is.Not.Null);
+        Assert.That(retrievedAuditServiceMetadata.Transports, Is.EquivalentTo(expectedAuditServiceMetadata.Transports));
+        Assert.That(retrievedAuditServiceMetadata.Versions, Is.EquivalentTo(expectedAuditServiceMetadata.Versions));
+    }
+}
---
+++ src/Particular.ThroughputCollector.Persistence.Tests/BrokerMetadataTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Particular.ThroughputCollector.Persistence.Tests;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Particular.ThroughputCollector.Contracts;
+
+[TestFixture]
+class BrokerMetadataTests : PersistenceTestFixture
+{
+    [Test]
+    public async Task Should_retrieve_saved_broker_metadata()
+    {
+        //Arrange
+        var expectedBrokerMetadata = new BrokerMetadata("Some scope", new Dictionary<string, string> { ["Some key"] = "Some value" });
+        await DataStore.SaveBrokerMetadata(expectedBrokerMetadata);
+
+        //Act
+        var retrievedBrokerMetadata = await DataStore.GetBrokerMetadata();
+
+        //Assert
+        Assert.That(retrievedBrokerMetadata, Is.Not.Null);
+        Assert.That(retrievedBrokerMetadata.ScopeType, Is.EqualTo(expectedBrokerMetadata.ScopeType));
+        Assert.That(retrievedBrokerMetadata.Data, Is.EquivalentTo(expectedBrokerMetadata.Data));
+    }
+
+    [Test]
+    public async Task Should_update_existing_broker_metadata_if_already_exists()
+    {
+        // Arrange
+        var oldBrokerMetadata = new BrokerMetadata("Some scope", new Dictionary<string, string> { ["Some key"] = "Some value" });
+        await DataStore.SaveBrokerMetadata(oldBrokerMetadata);
+
+        // Act
+        var expectedBrokerMetadata = new BrokerMetadata("New scope", new Dictionary<string, string> { ["New key"] = "New value" });
+        await DataStore.SaveBrokerMetadata(expectedBrokerMetadata);
+        var retrievedBrokerMetadata = await DataStore.GetBrokerMetadata();
+
+        // Assert
+        Assert.That(retrievedBrokerMetadata, Is.Not.Null);
+        Assert.That(retrievedBrokerMetadata.ScopeType, Is.EqualTo(expectedBrokerMetadata.ScopeType));
+        Assert.That(retrievedBrokerMetadata.Data, Is.EquivalentTo(expectedBrokerMetadata.Data));
+    }
+}
