dc937c708cd654bdd45b17951b614d4d6efb9483
Jo Palac <j_palac@hotmail.com>
2024-04-24 09:09:27 +1000

    Change to have masks stored and reused during report generation

--- src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_Report_Indicator_Tests.cs
+++ src/Particular.ThroughputCollector.UnitTests/ThroughputCollector/ThroughputCollector_Report_Indicator_Tests.cs
@@ -31,7 +31,7 @@ class ThroughputCollector_Report_Indicator_Tests : ThroughputCollectorTestFixtur
             .Build();
 
         // Act
-        var report = await ThroughputCollector.GenerateThroughputReport([], "", default);
+        var report = await ThroughputCollector.GenerateThroughputReport("", default);
 
         // Assert
         Assert.That(report, Is.Not.Null);
@@ -53,7 +53,7 @@ class ThroughputCollector_Report_Indicator_Tests : ThroughputCollectorTestFixtur
             .Build();
 
         // Act
-        var report = await ThroughputCollector.GenerateThroughputReport([], "", default);
+        var report = await ThroughputCollector.GenerateThroughputReport("", default);
 
         // Assert
         Assert.That(report, Is.Not.Null);
