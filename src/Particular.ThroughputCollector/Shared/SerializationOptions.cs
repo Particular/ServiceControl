203b49f68ff398a9e2232d93aa56217997d8c802
Jo Palac <j_palac@hotmail.com>
2024-04-24 21:13:51 +1000

    serialization cleanup

---
+++ src/Particular.ThroughputCollector/Shared/SerializationOptions.cs
@@ -0,0 +1,35 @@
+﻿namespace Particular.ThroughputCollector.Shared;
+
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+public static class SerializationOptions
+{
+    public static readonly JsonSerializerOptions SerializeIndented = new()
+    {
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
+    public static readonly JsonSerializerOptions SerializeNotIndented = new()
+    {
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
+    public static readonly JsonSerializerOptions DeserializeNotIndentedOptions = new()
+    {
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
+    public static readonly JsonSerializerOptions DeserializeDefaultOptions = new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+}
