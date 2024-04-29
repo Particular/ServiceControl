6f0543796b10d3c5616d37e99510c5ee208a9c5d
Mauro Servienti <mauro.servienti@gmail.com>
2024-04-29 16:28:16 +0200

    Add the Particular-ServicePulse-Version to the audit instance accepted headers

--- src/ServiceControl.Audit/Infrastructure/WebApi/Cors.cs
+++ src/ServiceControl.Audit/Infrastructure/WebApi/Cors.cs
@@ -10,7 +10,7 @@
 
             builder.AllowAnyOrigin();
             builder.WithExposedHeaders(["ETag", "Last-Modified", "Link", "Total-Count", "X-Particular-Version"]);
-            builder.WithHeaders(["Origin", "X-Requested-With", "Content-Type", "Accept"]);
+            builder.WithHeaders(["Origin", "X-Requested-With", "Content-Type", "Accept", "Particular-ServicePulse-Version"]);
             builder.WithMethods(["POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH"]);
 
             return builder.Build();
