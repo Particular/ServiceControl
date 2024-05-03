fb1df43539d0df44dab9c6ca289ae765f9160f7e
Andreas Bednarz <andreas.bednarz@particular.net>
2024-05-03 15:50:20 +1000

    Remove throughput collector persistence installer since SC primary instance will now take care of it

--- src/Particular.ThroughputCollector.Persistence/IPersistenceInstaller.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Particular.ThroughputCollector.Persistence;
-
-using System.Threading;
-using System.Threading.Tasks;
-
-public interface IPersistenceInstaller
-{
-    Task Install(CancellationToken cancellationToken = default);
-}
