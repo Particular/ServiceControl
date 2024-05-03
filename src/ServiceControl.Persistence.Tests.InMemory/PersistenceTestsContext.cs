5bd93604ee1eacd6964467f2e0316118db927a91
Andreas Bednarz <andreas.bednarz@particular.net>
2024-05-03 18:42:01 +1000

    Add InMemory tests for throughput collector in SC primary persistence tests

---
+++ src/ServiceControl.Persistence.Tests.InMemory/PersistenceTestsContext.cs
@@ -0,0 +1,28 @@
+﻿namespace ServiceControl.Persistence.Tests;
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Particular.ThroughputCollector.Persistence.InMemory;
+using ServiceControl.Persistence.Tests.InMemory;
+
+public class PersistenceTestsContext : IPersistenceTestsContext
+{
+    public PersistenceSettings PersistenceSettings { get; private set; }
+
+    public Task Setup(IHostApplicationBuilder hostBuilder)
+    {
+        hostBuilder.Services.AddThroughputInMemoryPersistence();
+
+        PersistenceSettings = new InMemoryPersistenceSettings();
+
+        return Task.CompletedTask;
+    }
+
+    public Task PostSetup(IHost host) => Task.CompletedTask;
+
+    public Task TearDown() => Task.CompletedTask;
+
+    public void CompleteDatabaseOperation()
+    {
+    }
+}
