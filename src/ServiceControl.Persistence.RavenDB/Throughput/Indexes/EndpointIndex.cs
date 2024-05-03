54b66a0aad8adf9c808f2b031f6a94ec78a0b869
Andreas Bednarz <andreas.bednarz@particular.net>
2024-05-03 15:49:34 +1000

    Add throughput data store to ServiceControl RavenDB persistence

---
+++ src/ServiceControl.Persistence.RavenDB/Throughput/Indexes/EndpointIndex.cs
@@ -0,0 +1,17 @@
+#nullable enable
+namespace ServiceControl.Persistence.RavenDB.Throughput.Indexes;
+
+using System.Linq;
+using Raven.Client.Documents.Indexes;
+using Particular.LicensingComponent.Contracts;
+
+class EndpointIndex : AbstractIndexCreationTask<Endpoint>
+{
+    public EndpointIndex() => Map = messages =>
+        from message in messages
+        select new
+        {
+            message.Id.ThroughputSource,
+            message.Id.Name,
+        };
+}
