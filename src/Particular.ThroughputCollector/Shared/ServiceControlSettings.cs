d57ad2d97f48d156dffc451da9275b4ed168576f
Jo Palac <j_palac@hotmail.com>
2024-05-03 16:20:15 +1000

    settings message update

--- src/Particular.ThroughputCollector/Shared/ServiceControlSettings.cs
+++ src/Particular.ThroughputCollector/Shared/ServiceControlSettings.cs
@@ -7,7 +7,7 @@
         public static readonly string MessageTransport = "ServiceControl";
 
         static string Queue = $"{SettingsHelper.SettingsNamespace}/{PlatformEndpointHelper.ServiceControlThroughputDataQueueSetting}";
-        static string QueueDescription = "Service Control throughput processing queue";
+        static string QueueDescription = $"Service Control throughput processing queue. This setting must match the {PlatformEndpointHelper.ServiceControlThroughputDataQueueSetting} setting on the Monitoring instance.";
 
         public static List<ThroughputConnectionSetting> GetServiceControlConnectionSettings()
         {
