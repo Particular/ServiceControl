e73b9a49fb9db135f69bdd7008322cb7836693ab
Jo Palac <j_palac@hotmail.com>
2024-05-15 14:13:01 +1000

    update to display error on non broker connections

--- src/views/throughputreport/SetupView.vue
+++ src/views/throughputreport/SetupView.vue
@@ -17,12 +17,38 @@ onMounted(async () => {
   <div class="box">
     <div class="row">
       <template v-if="!isBrokerTransport">
-        <div class="intro">
-          <p>You are using a transport that does not support automatic throughput collection.</p>
+        <div class="intro">          
+          <template v-if="testResults?.audit_connection_result.connection_successful">
+            <div>
+              <h6><i style="color: green" class="fa fa-check"></i> Successfully connected to Audit instances for throughput collection.</h6>
+            </div>
+          </template>
+          <template v-else>
+            <div>
+              <h6><i style="color: red" class="fa fa-times"></i> The connection to Audit instances was not successful.</h6>
+            </div>
+          </template>
+          <template v-if="testResults?.monitoring_connection_result.connection_successful">
+            <div>
+              <h6><i style="color: green" class="fa fa-check"></i> Successfully connected to Monitoring for throughput collection.</h6>
+            </div>
+          </template>
+          <template v-else>
+            <div>
+              <h6><i style="color: red" class="fa fa-times"></i> The connection to Monitoring was not successful.</h6>
+            </div>
+          </template>          
           <p>
+            <p>You are using a transport that does not support automatic throughput collection.</p>
             In order for ServicePulse to collect throughput data from your endpoints, you need to ensure that either Audit or Monitoring (metrics) are enabled on all your endpoints.<br />
             Read the <a href="https://docs.particular.net/servicecontrol/audit-instances/">Audit documentation</a> and the <a href="https://docs.particular.net/monitoring/metrics/">Monitoring documentation</a> for setup instructions .
           </p>
+          <template v-if="!testResults?.audit_connection_result.connection_successful || !testResults?.monitoring_connection_result.connection_successful">
+            <p>
+              You may have not setup all the connection settings, have a look at the <RouterLink :to="routeLinks.throughput.setup.connectionSetup.link">Connection Setup</RouterLink> tab.<br />
+              If you have set the settings but are still having issues, look at the <RouterLink :to="routeLinks.throughput.setup.diagnostics.link">Diagnostics</RouterLink> tab for more information on how to fix the issue.
+            </p>
+          </template>
         </div>
       </template>
       <template v-else>
