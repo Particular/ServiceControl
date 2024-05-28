8a32975be91a15f315adf0e645830d70f97b0fb7
Andreas Öhlund <andreas.ohlund@particular.net>
2024-05-28 13:57:55 +0200

    Add test to verify that default azure credentials can be used with ASB

---
+++ src/ServiceControl.Transports.ASBS.Tests/DefaultCredentialTests.cs
@@ -0,0 +1,54 @@
+﻿namespace ServiceControl.Transport.Tests
+{
+    using System.Threading.Tasks;
+    using Azure.Messaging.ServiceBus;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+    using ServiceControl.Transports;
+
+    class DefaultCredentialTests : TransportTestFixture
+    {
+        [Test]
+        public async Task Should_authenticate_using_default_credentials_when_FQNS_is_used()
+        {
+            var endpointName = NServiceBus.AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(DefaultCredentialEndpoint));
+
+            var connectionStringProperties = ServiceBusConnectionStringProperties.Parse(configuration.ConnectionString);
+
+            var fullyQualifiedNamespace = connectionStringProperties.FullyQualifiedNamespace;
+
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = fullyQualifiedNamespace,
+                MaxConcurrency = 1,
+                EndpointName = endpointName
+            };
+
+            await configuration.TransportCustomization.ProvisionQueues(transportSettings, []);
+
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<DefaultCredentialEndpoint>(c => c.CustomConfig(ec =>
+                {
+                    configuration.TransportCustomization.CustomizePrimaryEndpoint(ec, transportSettings);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsTrue(ctx.EndpointsStarted);
+        }
+
+        public class Context : ScenarioContext
+        {
+        }
+
+        public class DefaultCredentialEndpoint : EndpointConfigurationBuilder
+        {
+            public DefaultCredentialEndpoint() =>
+                EndpointSetup<BasicEndpointSetup>(c =>
+                {
+                    c.UsePersistence<NonDurablePersistence>();
+                });
+        }
+    }
+}
