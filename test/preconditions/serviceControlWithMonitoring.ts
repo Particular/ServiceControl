cbe7006723e308e80612b9ff593cd941ee21e238
Christian <christian.quiros@particular.net>
2024-05-29 07:02:12 -0600

    Default mocked responses for /licencing APIs (#1863)
    In order to fix broken build

--- test/preconditions/serviceControlWithMonitoring.ts
+++ test/preconditions/serviceControlWithMonitoring.ts
@@ -1,6 +1,8 @@
 import { monitoredEndpointList } from "@/../test/mocks/monitored-endpoint-template";
 import * as precondition from ".";
 import { SetupFactoryOptions } from "../driver";
+import EndpointThroughputSummary from "@/resources/EndpointThroughputSummary";
+import ReportGenerationState from "@/resources/ReportGenerationState";
 
 export const serviceControlWithMonitoring = async ({ driver }: SetupFactoryOptions) => {
   //Service control requests minimum setup. Todo: encapsulate for reuse.
@@ -43,4 +45,51 @@ export const serviceControlWithMonitoring = async ({ driver }: SetupFactoryOptio
 
   //http://localhost:33633/monitored-endpoints
   await driver.setUp(precondition.hasNoMonitoredEndpoints);
+
+  //Default handler for /api/licensing/report/available
+  driver.mockEndpoint(`${window.defaultConfig.service_control_url}licensing/report/available`, {
+    body: <ReportGenerationState>{
+      transport: "LearningTransport",
+      report_can_be_generated: true,
+      reason: "",
+    },
+    method: "get",
+    status: 200,
+  });
+
+  //Default handler for /api/licensing/endpoints
+  driver.mockEndpoint(`${window.defaultConfig.service_control_url}licensing/endpoints`, {
+    body: [<EndpointThroughputSummary>{
+      name: "",
+      is_known_endpoint: true,
+      user_indicator: "",
+      max_daily_throughput: 10,
+    }],
+    method: "get",
+    status: 200,
+  });
+
+  //Default handler for /api/licensing/settings/test
+  driver.mockEndpoint(`${window.defaultConfig.service_control_url}licensing/settings/test`, {
+    body: <ConnectionTestResults>{
+      transport: "",
+      audit_connection_result: <ConnectionSettingsTestResult>{
+        connection_successful: true,
+        connection_error_messages: [],
+        diagnostics: "",
+      },
+      monitoring_connection_result: <ConnectionSettingsTestResult>{
+        connection_successful: true,
+        connection_error_messages: [],
+        diagnostics: "",
+      },
+      broker_connection_result: <ConnectionSettingsTestResult>{
+        connection_successful: true,
+        connection_error_messages: [],
+        diagnostics: "",
+      },
+    },
+    method: "get",
+    status: 200,
+  });
 };
