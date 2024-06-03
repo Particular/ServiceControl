11271b8a02206e08e81b853ee5a6370c12e4e68a
David Boike <david.boike@gmail.com>
2024-06-03 17:31:20 -0500

    Check for Raven server version, license, and cluster configuration in exernally-managed mode

--- src/ServiceControl.Persistence.RavenDB/RavenExternalPersistenceLifecycle.cs
+++ src/ServiceControl.Persistence.RavenDB/RavenExternalPersistenceLifecycle.cs
@@ -3,10 +3,16 @@
 namespace ServiceControl.Persistence.RavenDB
 {
     using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
     using Raven.Client.Documents;
     using Raven.Client.Documents.Conventions;
+    using Raven.Client.Http;
+    using Raven.Client.ServerWide.Operations;
+    using Sparrow.Json;
 
     sealed class RavenExternalPersistenceLifecycle(RavenPersisterSettings settings) : IRavenPersistenceLifecycle, IRavenDocumentStoreProvider, IDisposable
     {
@@ -46,14 +52,67 @@ namespace ServiceControl.Persistence.RavenDB
 
                 documentStore = store.Initialize();
 
+                var build = await store.Maintenance.Server.SendAsync(new GetBuildNumberOperation(), cancellationToken);
+                var embeddedVersion = typeof(Raven.Embedded.EmbeddedServer).Assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>().First().InformationalVersion;
+                if (embeddedVersion != build.FullVersion)
+                {
+                    throw new Exception($"ServiceControl expects RavenDB Server version {embeddedVersion} but the server is using {build.FullVersion}.");
+                }
+
+                var licenseResponse = await store.Maintenance.Server.SendAsync(new GetLicenseOperation(), cancellationToken);
+                if (licenseResponse.LicensedTo != "ParticularNservicebus (Israel)")
+                {
+                    throw new Exception("Wrong license");
+                }
+
                 var databaseSetup = new DatabaseSetup(settings);
                 await databaseSetup.Execute(store, cancellationToken).ConfigureAwait(false);
+
+                // Must go after the database setup, as database must exist
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    if (session.Advanced.RequestExecutor.Topology.Nodes.Count > 1)
+                    {
+                        throw new Exception("ServiceControl expects to run against a single-node RavenDB database. Cluster configurations are not supported.");
+                    }
+                }
             }
             finally
             {
                 initializeSemaphore.Release();
             }
         }
+
+        class GetLicenseOperation : IServerOperation<LicenseResult>
+        {
+            public RavenCommand<LicenseResult> GetCommand(DocumentConventions conventions, JsonOperationContext context) => new GetLicenseCommand();
+        }
+
+        class GetLicenseCommand : RavenCommand<LicenseResult>
+        {
+            public override bool IsReadRequest => true;
+
+            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+            {
+                url = $"{node.Url}/license/status";
+                return new HttpRequestMessage { Method = HttpMethod.Get };
+            }
+
+            public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
+            {
+                if (response == null)
+                {
+                    return;
+                }
+
+                if (response.TryGet<string>("Id", out var id) && response.TryGet<string>("LicensedTo", out var licensedTo))
+                {
+                    Result = new LicenseResult(id, licensedTo);
+                }
+            }
+        }
+
+        record class LicenseResult(string Id, string LicensedTo);
 
         public void Dispose() => documentStore?.Dispose();
 
