b13d75869224d5403a49479d422b2c3af89f6809
David Boike <david.boike@gmail.com>
2024-06-18 11:49:31 -0500

    Restore SetupMode = None, make all args ENV vars (#4237)
    * Restore SetupMode = None, make all args ENV vars
    
    * Add test

---
+++ src/ServiceControl.UnitTests/Infrastructure/DockerfileTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceControl.UnitTests.Infrastructure
+{
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using NUnit.Framework;
+
+    public class DockerfileTests
+    {
+        [Test]
+        public void DontUseRavenArgs()
+        {
+            var srcDir = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", ".."));
+            var ravenContainerDockerfilePath = Path.Combine(srcDir, "ServiceControl.RavenDB", "Dockerfile");
+            var contents = File.ReadAllText(ravenContainerDockerfilePath);
+            var setsRavenArgs = Regex.IsMatch(contents, @"RAVEN_ARGS=", RegexOptions.IgnoreCase);
+
+            Assert.That(setsRavenArgs, Is.False, "Our Dockerfile should not set RAVEN_ARGS to anything, and leave that entirely to the end user. Instead, replace any --Setting.Name=value with an env var RAVEN_Setting_Name=value.");
+        }
+    }
+}
