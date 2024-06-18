18ebce7e83dac57dc40f28c6a6384165b774d787
John Simons <john.simons@particular.net>
2024-06-18 17:40:40 +1000

    Make setup Di compatible

--- src/ServiceControl/ComponentInstallationContext.cs
+++ src/ServiceControl/ComponentInstallationContext.cs
@@ -2,21 +2,16 @@ namespace Particular.ServiceControl
 {
     using System;
     using System.Collections.Generic;
-    using System.Reflection;
     using System.Threading.Tasks;
 
     class ComponentInstallationContext : IComponentInstallationContext
     {
         public List<string> Queues { get; } = [];
 
-        public List<Assembly> IndexAssemblies { get; } = [];
-
-        public List<Func<Task>> InstallationTasks { get; } = [];
+        public List<Func<IServiceProvider, Task>> InstallationTasks { get; } = [];
 
         public void CreateQueue(string queueName) => Queues.Add(queueName);
 
-        public void AddIndexAssembly(Assembly assembly) => IndexAssemblies.Add(assembly);
-
-        public void RegisterInstallationTask(Func<Task> setupTask) => InstallationTasks.Add(setupTask);
+        public void RegisterInstallationTask(Func<IServiceProvider, Task> setupTask) => InstallationTasks.Add(setupTask);
     }
 }
--- src/ServiceControl/ServiceControlComponent.cs
+++ src/ServiceControl/ServiceControlComponent.cs
@@ -1,14 +1,14 @@
-
 namespace Particular.ServiceControl
 {
+    using global::ServiceControl.Transports;
     using Microsoft.Extensions.Hosting;
     using ServiceBus.Management.Infrastructure.Settings;
 
     abstract class ServiceControlComponent
     {
-        public abstract void Configure(Settings settings, IHostApplicationBuilder hostBuilder);
+        public abstract void Configure(Settings settings, ITransportCustomization transportCustomization, IHostApplicationBuilder hostBuilder);
 
-        public virtual void Setup(Settings settings, IComponentInstallationContext context)
+        public virtual void Setup(Settings settings, IComponentInstallationContext context, IHostApplicationBuilder hostBuilder)
         {
         }
     }
