70671bcf54214bfa1e7da7724ee00ebbafb86f77
John Simons <john.simons@particular.net>
2024-06-19 09:39:25 +1000

    Removed unused code

--- src/ServiceControl/ComponentInstallationContext.cs
+++ src/ServiceControl/ComponentInstallationContext.cs
@@ -1,17 +1,11 @@
 namespace Particular.ServiceControl
 {
-    using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
 
     class ComponentInstallationContext : IComponentInstallationContext
     {
         public List<string> Queues { get; } = [];
 
-        public List<Func<IServiceProvider, Task>> InstallationTasks { get; } = [];
-
         public void CreateQueue(string queueName) => Queues.Add(queueName);
-
-        public void RegisterInstallationTask(Func<IServiceProvider, Task> setupTask) => InstallationTasks.Add(setupTask);
     }
 }
--- src/ServiceControl/IComponentInstallationContext.cs
+++ src/ServiceControl/IComponentInstallationContext.cs
@@ -1,11 +1,7 @@
 namespace Particular.ServiceControl
 {
-    using System;
-    using System.Threading.Tasks;
-
     interface IComponentInstallationContext
     {
         void CreateQueue(string queueName);
-        void RegisterInstallationTask(Func<IServiceProvider, Task> setupTask);
     }
 }
