ff46651bad9a44a01a1ac92ef277bb93328c5382
Christian <christian.quiros+github@gmail.com>
2024-06-19 15:09:40 -0600

    remove line duplicate in tests

--- test/specs/monitoring/history-period.spec.ts
+++ test/specs/monitoring/history-period.spec.ts
@@ -154,9 +154,8 @@ describe("FEATURE: Endpoint history periods", () => {
 
         // Simulate the time passing for the last millisecond to make the selected history period time now be elapsed
         await vi.advanceTimersByTimeAsync(1);
-        expect(await endpointSparklineValues("Endpoint1")).toEqual(["12", "9.56", "13.24", "81", "220"]);
-        expect(await endpointSparklineValues("Endpoint1")).toEqual(["12", "9.56", "13.24", "81", "220"]);       
-        
+        expect(await endpointSparklineValues("Endpoint1")).toEqual(["12", "9.56", "13.24", "81", "220"]);        
+                
         vi.useRealTimers();
       });
     });
