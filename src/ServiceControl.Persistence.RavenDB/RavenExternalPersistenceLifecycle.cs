b30febe07895f695ad261945535644bf41004e6d
David Boike <david.boike@gmail.com>
2024-06-20 15:19:45 -0500

    Load RavenDB client certificate if located next to the executable (#4247)
    * Load RavenDB client certificate if located next to the executable
    
    * Use entry assembly location
    
    * Tweaks
    
    ---------
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/ServiceControl.Persistence.RavenDB/RavenExternalPersistenceLifecycle.cs
+++ src/ServiceControl.Persistence.RavenDB/RavenExternalPersistenceLifecycle.cs
@@ -3,6 +3,9 @@
 namespace ServiceControl.Persistence.RavenDB
 {
     using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Security.Cryptography.X509Certificates;
     using System.Threading;
     using System.Threading.Tasks;
     using Raven.Client.Documents;
@@ -35,10 +38,21 @@ namespace ServiceControl.Persistence.RavenDB
             {
                 await initializeSemaphore.WaitAsync(cancellationToken);
 
+                // Look for raven-client-certificate.pfx in same directory as application code
+                var applicationDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? string.Empty;
+                var certificatePath = Path.Combine(applicationDirectory, "raven-client-certificate.pfx");
+                X509Certificate2? certificate = null;
+
+                if (File.Exists(certificatePath))
+                {
+                    certificate = new X509Certificate2(certificatePath);
+                }
+
                 var store = new DocumentStore
                 {
                     Database = settings.DatabaseName,
                     Urls = [settings.ConnectionString],
+                    Certificate = certificate,
                     Conventions = new DocumentConventions
                     {
                         SaveEnumsAsIntegers = true
