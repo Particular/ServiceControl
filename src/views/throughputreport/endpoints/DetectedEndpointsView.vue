c46251ff3fe7ccba64cad5e59471c1a5cde4d475
John Simons <john.simons@particular.net>
2024-06-24 17:09:23 +1000

    Update src/Frontend/src/views/throughputreport/endpoints/DetectedEndpointsView.vue
    Co-authored-by: Jo Palac <jpalac@users.noreply.github.com>

--- src/views/throughputreport/endpoints/DetectedEndpointsView.vue
+++ src/views/throughputreport/endpoints/DetectedEndpointsView.vue
@@ -12,7 +12,7 @@ const store = useThroughputStore();
   <template v-if="!store.isBrokerTransport">
     <template v-if="store.hasErrors">
       <div class="errorContainer text-center">
-        <h6><i style="color: red" class="fa fa-times"></i> There are some error collecting usage data.</h6>
+        <h6><i style="color: red" class="fa fa-times"></i> There were some errors collecting usage data.</h6>
         <p>
           You may have not setup all the connection settings, have a look at <RouterLink :to="routeLinks.throughput.setup.connectionSetup.link">Connection Setup in Configuration</RouterLink>.<br />
           If you have set all the connection settings but are still having issues, look at the <RouterLink :to="routeLinks.throughput.setup.diagnostics.link">Diagnostics in Configuration</RouterLink> for more information on how to fix them.
