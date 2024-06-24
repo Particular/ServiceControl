50792d73a970730a535d63d0970dfdb6dbe606ed
John Simons <john.simons@particular.net>
2024-06-24 12:02:09 +1000

    Update src/Frontend/src/views/throughputreport/setup/ConnectionSetupView.vue
    Co-authored-by: Jo Palac <jpalac@users.noreply.github.com>

--- src/views/throughputreport/setup/ConnectionSetupView.vue
+++ src/views/throughputreport/setup/ConnectionSetupView.vue
@@ -34,7 +34,7 @@ onMounted(async () => {
         </p>
         <ConfigurationCode :settings="settingsInfo?.broker_settings ?? []">
           <template #configInstructions>
-            <div>Paste the settings above in the <code>ServiceControl.exe.config</code> file of the ServiceControl Error instance.</div>
+            <div>Paste the settings above into the <code>ServiceControl.exe.config</code> file of the ServiceControl Error instance.</div>
           </template>
           <template #environmentVariableInstructions>
             <div>Execute the above instructions in a terminal to set the environment variables, these variables need to be set for the user that in running the ServiceControl Error instance.</div>
