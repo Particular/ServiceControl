63517503e12bce4de67a783ee5098dcb84d518b8
Tomek Masternak <tomasz.masternak@particular.net>
2024-06-26 19:07:41 +0200

    adapter transport scafolding

---
+++ src/ServiceControl.Transports.MT.RabbitMQ/RabbitMQAdapter.cs
@@ -0,0 +1,24 @@
+﻿namespace ServiceControl.Transports.MT.RabbitMQ;
+
+using NServiceBus.Transport;
+
+class RabbitMQAdapter : TransportDefinition
+{
+#pragma warning disable IDE0052
+    readonly string connectionString;
+#pragma warning restore IDE0052
+
+    public RabbitMQAdapter(string connectionString) : base(TransportTransactionMode.ReceiveOnly, false, false, false)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public override Task<TransportInfrastructure> Initialize(HostSettings hostSettings, ReceiveSettings[] receivers, string[] sendingAddresses,
+        CancellationToken cancellationToken = new CancellationToken()) =>
+        throw new NotImplementedException();
+
+    public override IReadOnlyCollection<TransportTransactionMode> GetSupportedTransactionModes() => new[]
+    {
+        TransportTransactionMode.ReceiveOnly
+    };
+}
---
+++ src/ServiceControl.Transports.MT.RabbitMQ/RabbitMQTransportAdapterCustomization.cs
@@ -0,0 +1,50 @@
+﻿namespace ServiceControl.Transports.MT.RabbitMQ;
+
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Features;
+using NServiceBus.Transport;
+
+public class RabbitMQTransportAdapterCustomization : ITransportCustomization
+{
+    public void CustomizeAuditEndpoint(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings) { }
+
+    public void AddTransportForAudit(IServiceCollection services, TransportSettings transportSettings) { }
+
+    public void CustomizeMonitoringEndpoint(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings) { }
+
+    public void AddTransportForMonitoring(IServiceCollection services, TransportSettings transportSettings) { }
+
+    public void CustomizePrimaryEndpoint(EndpointConfiguration endpointConfiguration, TransportSettings transportSettings)
+    {
+        endpointConfiguration.DisableFeature<Audit>();
+        endpointConfiguration.DisableFeature<AutoSubscribe>();
+        endpointConfiguration.DisableFeature<Outbox>();
+        endpointConfiguration.DisableFeature<Sagas>();
+        endpointConfiguration.SendFailedMessagesTo(transportSettings.ErrorQueue);
+
+        if (transportSettings.ConnectionString == null)
+        {
+            throw new InvalidOperationException("Connection string not configured");
+        }
+
+        var transport = new RabbitMQAdapter(transportSettings.ConnectionString) { TransportTransactionMode = TransportTransactionMode.ReceiveOnly };
+
+        endpointConfiguration.UseTransport(transport);
+    }
+
+    public void AddTransportForPrimary(IServiceCollection services, TransportSettings transportSettings)
+    {
+        services.AddSingleton<ITransportCustomization>(this);
+        services.AddSingleton(transportSettings);
+        //services.AddSingleton<IBrokerThroughputQuery, RabbitMQQuery>();
+    }
+
+    public Task ProvisionQueues(TransportSettings transportSettings, IEnumerable<string> additionalQueues) => Task.CompletedTask;
+
+    public Task<TransportInfrastructure> CreateTransportInfrastructure(string name, TransportSettings transportSettings,
+        OnMessage onMessage = null,
+        OnError onError = null, Func<string, Exception, Task> onCriticalError = null,
+        TransportTransactionMode preferredTransactionMode = TransportTransactionMode.ReceiveOnly) =>
+        //HINT: this is called by primary instance only when message is returned to the endpoint's queueu
+        throw new NotImplementedException();
+}
