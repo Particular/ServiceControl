846934cdc156b55b77c27c7469d9b2dfd88c7bd0
Christian <christian.quiros+github@gmail.com>
2024-06-26 14:46:33 -0600

    Removing duplicate call to useServiceControl

--- src/components/BackendChecksNotifications.vue
+++ src/components/BackendChecksNotifications.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import { onMounted, computed, watch } from "vue";
+import { computed, watch } from "vue";
 import { useRouter } from "vue-router";
 import "bootstrap";
 import { monitoringUrl, serviceControlUrl, useIsMonitoringDisabled } from "@/composables/serviceServiceControlUrls";
 import "highlight.js/styles/github-dark.css";
-import { useServiceControl, monitoringConnectionState, connectionState, environment } from "@/composables/serviceServiceControl";
+import { monitoringConnectionState, connectionState, environment } from "@/composables/serviceServiceControl";
 import routeLinks from "@/router/routeLinks";
 import { useShowToast } from "@/composables/toast";
 import { TYPE } from "vue-toastification";
 
 const router = useRouter();
-
-onMounted(async () => {
-  await useServiceControl();
-});
 
 const primaryConnectionFailure = computed(() => connectionState.unableToConnect);
 const monitoringConnectionFailure = computed(() => monitoringConnectionState.unableToConnect);
@@ -50,7 +46,6 @@ watch(monitoringConnectionFailure, (newValue, oldValue) => {
 watch(
   () => environment.is_compatible_with_sc,
   (newValue) => {
-    console.debug("New Value: ", newValue);
     if (newValue === false) {
       useShowToast(TYPE.ERROR, "Error", `You are using Service Control version ${environment.sc_version}. Please, upgrade to version ${environment.minimum_supported_sc_version} or higher to unlock new functionality in ServicePulse.`);
     }
