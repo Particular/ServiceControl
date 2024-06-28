2e1748a989a5232c8d32e935aa123b64e1c32e98
David Boike <david.boike@gmail.com>
2024-06-28 15:07:00 -0500

    Map reverse proxy headers

--- src/ServiceControl.Audit/WebApplicationExtensions.cs
+++ src/ServiceControl.Audit/WebApplicationExtensions.cs
@@ -2,11 +2,13 @@ namespace ServiceControl.Audit;
 
 using Infrastructure.WebApi;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 
 public static class WebApplicationExtensions
 {
     public static void UseServiceControlAudit(this WebApplication app)
     {
+        app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
         app.UseResponseCompression();
         app.UseMiddleware<BodyUrlRouteFix>();
         app.UseHttpLogging();
