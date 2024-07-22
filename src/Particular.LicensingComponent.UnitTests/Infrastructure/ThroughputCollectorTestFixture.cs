77e905109f565d5e64468c8be10a9c120009e656
Jo Palac <jpalac@users.noreply.github.com>
2024-07-22 10:43:33 +1000

    Disable report download if using a broker but there is no broker data for last 30 days (#4314)
    * Disable report generation if no broker data for last 30 days, even if audit or monitoring data exists
    
    * Match UserIndicator enum to ServicePulse
    
    * add test for no broker
    
    * Provide different reason if Usage report generation requires broker data
    
    * Remove extraneous $ from reason why usage report can't be generated
    
    * Remove broker name from reason why report can't be generated, due to it not matching how ServicePulse refers to brokers
    
    ---------
    
    Co-authored-by: Andreas Bednarz <andreas.bednarz@particular.net>

--- src/Particular.LicensingComponent.UnitTests/Infrastructure/ThroughputCollectorTestFixture.cs
+++ src/Particular.LicensingComponent.UnitTests/Infrastructure/ThroughputCollectorTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿namespace Particular.LicensingComponent.UnitTests.Infrastructure
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using Contracts;
     using Microsoft.Extensions.DependencyInjection;
@@ -16,21 +17,24 @@
         [SetUp]
         public virtual Task Setup()
         {
-            configuration = new ThroughputTestsConfiguration();
+            var testMethod = GetType().GetMethod(TestContext.CurrentContext.Test.MethodName);
+            var attribute = testMethod?.GetCustomAttributes(typeof(UseNonBrokerTransportAttribute), false).FirstOrDefault();
 
-            return configuration.Configure(SetThroughputSettings, SetExtraDependencies);
+            return configuration.Configure(SetThroughputSettings, SetExtraDependencies, attribute is not UseNonBrokerTransportAttribute);
         }
 
         [TearDown]
-        public virtual Task Cleanup()
-        {
-            return configuration?.Cleanup();
-        }
+        public virtual Task Cleanup() => configuration?.Cleanup();
 
         protected ILicensingDataStore DataStore => configuration.LicensingDataStore;
 
         protected IThroughputCollector ThroughputCollector => configuration.ThroughputCollector;
 
-        protected ThroughputTestsConfiguration configuration;
+        protected ThroughputTestsConfiguration configuration = new();
+
+        [AttributeUsage(AttributeTargets.Method)]
+        public class UseNonBrokerTransportAttribute : Attribute
+        {
+        }
     }
 }
