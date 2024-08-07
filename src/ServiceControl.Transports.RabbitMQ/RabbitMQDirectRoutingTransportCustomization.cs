8711eda4b868b0a18dc841cb2774b34a97d8c33b
Ramon Smits <ramon.smits@gmail.com>
2024-08-07 14:01:04 +0200

    readability: Named args for new bool param

--- src/ServiceControl.Transports.RabbitMQ/RabbitMQDirectRoutingTransportCustomization.cs
+++ src/ServiceControl.Transports.RabbitMQ/RabbitMQDirectRoutingTransportCustomization.cs
@@ -17,7 +17,6 @@
         protected override void CustomizeTransportForAuditEndpoint(EndpointConfiguration endpointConfiguration, RabbitMQTransport transportDefinition, TransportSettings transportSettings) { }
 
         protected override void CustomizeTransportForMonitoringEndpoint(EndpointConfiguration endpointConfiguration, RabbitMQTransport transportDefinition, TransportSettings transportSettings) { }
-
         protected override RabbitMQTransport CreateTransport(TransportSettings transportSettings, TransportTransactionMode preferredTransactionMode = TransportTransactionMode.ReceiveOnly)
         {
             if (transportSettings.ConnectionString == null)
@@ -25,7 +24,7 @@
                 throw new InvalidOperationException("Connection string not configured");
             }
 
-            var transport = new RabbitMQTransport(RoutingTopology.Direct(queueType, routingKeyConvention: type => type.FullName.Replace(".", "-")), transportSettings.ConnectionString, false);
+            var transport = new RabbitMQTransport(RoutingTopology.Direct(queueType, routingKeyConvention: type => type.FullName.Replace(".", "-")), transportSettings.ConnectionString, enableDelayedDelivery: false);
             transport.TransportTransactionMode = transport.GetSupportedTransactionModes().Contains(preferredTransactionMode) ? preferredTransactionMode : TransportTransactionMode.ReceiveOnly;
 
             return transport;
