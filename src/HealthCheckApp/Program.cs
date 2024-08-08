e7db2b2086173e84de10760cbb9663184a9bcba0
David Boike <david.boike@gmail.com>
2024-08-08 16:30:58 -0500

    Use chiseled image

---
+++ src/HealthCheckApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using var http = new HttpClient();
+
+var url = args.FirstOrDefault();
+
+if (string.IsNullOrEmpty(url))
+{
+    throw new Exception("Missing URL");
+}
+
+var response = await http.GetAsync(url).ConfigureAwait(false);
+response.EnsureSuccessStatusCode();
+
+if (response.Content.Headers.ContentType?.MediaType != "application/json" || response.Content.Headers.ContentLength == 0)
+{
+    throw new Exception("Invalid ContentType or Length");
+}
+
+Console.WriteLine("OK");
+return 0;
