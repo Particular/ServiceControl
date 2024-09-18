d6cff86a202d143d071c70c6ff4cccc1060592fd
Phil Bastian <phil.bastian@particular.net>
2024-09-18 10:09:13 +0800

    fix query to work with postgres syntax

--- src/ServiceControl.Transports.PostgreSql/PostgreSqlTable.cs
+++ src/ServiceControl.Transports.PostgreSql/PostgreSqlTable.cs
@@ -6,19 +6,15 @@ namespace ServiceControl.Transports.PostgreSql
     {
         public PostgreSqlTable(string name, string schema)
         {
-            //HINT: The query approximates queue length value based on max and min
-            //      of RowVersion IDENTITY(1,1) column. There are couple of scenarios
-            //      that might lead to the approximation being off. More details here:
-            //      https://docs.microsoft.com/en-us/sql/t-sql/statements/create-table-transact-sql-identity-property?view=sql-server-ver15#remarks
-            //
-            //      Min and Max values return NULL when no rows are found.
-            fullTableName = $"{name}.{schema}";
+            //HINT: The query approximates queue length value based on max and min of the table sequence.
+            fullTableName = $"\"{schema}\".\"{name}\"";
             //TODO: Postgres should we add NOLOCK?
             LengthQuery = $$"""
-                            IF (EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{{schema}}' AND TABLE_NAME = '{{name}}'))
-                              SELECT COALESCE(cast(max(seq) - min(seq) + 1 AS int), 0) Id FROM {0}
+                            SELECT CASE WHEN (EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{{schema}}' AND TABLE_NAME = '{{name}}')) THEN
+                              COALESCE(cast(max(seq) - min(seq) + 1 AS int), 0)
                             ELSE
-                              SELECT -1;
+                              -1
+                            END AS Id FROM {{fullTableName}}
                             """;
         }
 
