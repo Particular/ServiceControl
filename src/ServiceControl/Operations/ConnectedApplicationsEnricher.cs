1851bcb32dba80f9c0780de95e861174c89dafe2
WilliamBZA <william.brander@gmail.com>
2024-09-19 14:07:51 +0200

    Add enricher to extract connected applications

---
+++ src/ServiceControl/Operations/ConnectedApplicationsEnricher.cs
@@ -0,0 +1,27 @@
+﻿namespace ServiceControl.Operations
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    class ConnectedApplicationsEnricher(Settings settings) : IEnrichImportedErrorMessages
+    {
+        static List<string> MassTransitHeaders =
+        [
+            "MassTransitHeader1",
+            "MassTransitHeader2",
+            "MassTransitHeader3"
+        ];
+
+        public void Enrich(ErrorEnricherContext context)
+        {
+            if (!alreadyAddedMassTransit && context.Headers.Any(incomingHeader => MassTransitHeaders.Contains(incomingHeader.Key)))
+            {
+                alreadyAddedMassTransit = true;
+                settings.ConnectedApplications.Add("MassTransitConnector");
+            }
+        }
+
+        static bool alreadyAddedMassTransit = false;
+    }
+}
