f4a7345d4bfbc53c291e19e4fb69b80d5d25ca1c
Christian <christian.quiros+github@gmamil.com>
2024-09-24 20:05:22 +0000

    bug fix #4462 SQLTransport QueueLegth
    Always logs 'table does not exist'

--- src/ServiceControl.Transports.SqlServer/SqlNameHelper.cs
+++ src/ServiceControl.Transports.SqlServer/SqlNameHelper.cs
@@ -15,5 +15,19 @@ namespace ServiceControl.Transports.SqlServer
 
             return prefix + name.Replace(suffix, suffix + suffix) + suffix;
         }
+
+        public static string Unquote(string quotedString)
+        {
+            if (quotedString == null)
+            {
+                return null;
+            }
+            if (!quotedString.StartsWith(prefix) || !quotedString.EndsWith(suffix))
+            {
+                return quotedString;
+            }
+            return quotedString
+                .Substring(prefix.Length, quotedString.Length - prefix.Length - suffix.Length).Replace(suffix + suffix, suffix);
+        }
     }
 }
--- src/ServiceControl.Transports.SqlServer/SqlTable.cs
+++ src/ServiceControl.Transports.SqlServer/SqlTable.cs
@@ -1,12 +1,12 @@
 ﻿#nullable enable
 namespace ServiceControl.Transports.SqlServer
 {
-    using System.Linq;
 
     class SqlTable
     {
         SqlTable(string name, string schema, string? catalog)
         {
+            var unquotedSchema = SqlNameHelper.Unquote(schema);
             var quotedName = SqlNameHelper.Quote(name);
             var quotedSchema = SqlNameHelper.Quote(schema);
             //HINT: The query approximates queue length value based on max and min
@@ -20,7 +20,7 @@ namespace ServiceControl.Transports.SqlServer
                 _fullTableName = $"{quotedSchema}.{quotedName}";
 
                 LengthQuery = $"""
-                               IF (EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{schema}' AND TABLE_NAME = '{name}'))
+                               IF (EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{unquotedSchema}' AND TABLE_NAME = '{name}'))
                                  SELECT isnull(cast(max([RowVersion]) - min([RowVersion]) + 1 AS int), 0) FROM {_fullTableName} WITH (nolock)
                                ELSE
                                  SELECT -1;
@@ -32,7 +32,7 @@ namespace ServiceControl.Transports.SqlServer
                 _fullTableName = $"{quotedCatalog}.{quotedSchema}.{quotedName}";
 
                 LengthQuery = $"""
-                               IF (EXISTS (SELECT TABLE_NAME FROM {quotedCatalog}.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{schema}' AND TABLE_NAME = '{name}'))
+                               IF (EXISTS (SELECT TABLE_NAME FROM {quotedCatalog}.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{unquotedSchema}' AND TABLE_NAME = '{name}'))
                                  SELECT isnull(cast(max([RowVersion]) - min([RowVersion]) + 1 AS int), 0) FROM {_fullTableName} WITH (nolock)
                                ELSE
                                  SELECT -1;
@@ -51,9 +51,9 @@ namespace ServiceControl.Transports.SqlServer
             var parts = address.Split('@');
 
             return new SqlTable(
-                parts[0],
-                parts.Length > 1 ? parts[1] : defaultSchema,
-                parts.Length > 2 ? parts[2] : null
+                name: parts[0],
+                schema: parts.Length > 1 ? parts[1] : defaultSchema,
+                catalog: parts.Length > 2 ? parts[2] : null
             );
         }
 
