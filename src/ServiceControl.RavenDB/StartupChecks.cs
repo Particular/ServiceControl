4ee804b75d712039016217f9676731cce5f45769
Brandon Ording <bording@gmail.com>
2024-09-26 18:12:43 -0400

    Remove cluster startup check

--- src/ServiceControl.RavenDB/StartupChecks.cs
+++ src/ServiceControl.RavenDB/StartupChecks.cs
@@ -3,10 +3,7 @@
     using System.Reflection;
     using System.Threading;
     using Raven.Client.Documents;
-    using Raven.Client.Documents.Conventions;
-    using Raven.Client.Http;
     using Raven.Client.ServerWide.Operations;
-    using Sparrow.Json;
 
     public static class StartupChecks
     {
@@ -14,21 +11,10 @@
         {
             var build = await store.Maintenance.Server.SendAsync(new GetBuildNumberOperation(), cancellationToken);
             var embeddedVersion = typeof(Raven.Embedded.EmbeddedServer).Assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>().First().InformationalVersion;
+
             if (embeddedVersion != build.FullVersion)
             {
                 throw new Exception($"ServiceControl expects RavenDB Server version {embeddedVersion} but the server is using {build.FullVersion}.");
-            }
-        }
-
-        // Must go after the database setup, as database must exist
-        public static void EnsureSingleNodeTopology(IDocumentStore store)
-        {
-            using (var session = store.OpenAsyncSession())
-            {
-                if (session.Advanced.RequestExecutor.Topology.Nodes.Count > 1)
-                {
-                    throw new Exception("ServiceControl expects to run against a single-node RavenDB database. Cluster configurations are not supported.");
-                }
             }
         }
     }
