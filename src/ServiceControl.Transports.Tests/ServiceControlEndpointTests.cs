5672ca0c084b65c90cacc395f4a4c219cda3efce
Phil Bastian <phil.bastian@particular.net>
2024-10-03 16:24:01 +0800

    initial tests for max concurrency setting

--- src/ServiceControl.Transports.Tests/ServiceControlEndpointTests.cs
+++ src/ServiceControl.Transports.Tests/ServiceControlEndpointTests.cs
@@ -32,6 +32,32 @@
             Assert.That(ctx.EndpointsStarted, Is.True);
         }
 
+        [TestCase(15)]
+        [TestCase(null)]
+        public async Task Should_set_max_concurrency_for_ProvisionQueues(int? setConcurrency)
+        {
+            var endpointName = NServiceBus.AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(ServiceControlEndpoint));
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = configuration.ConnectionString,
+                MaxConcurrency = setConcurrency,
+                EndpointName = endpointName
+            };
+
+            await configuration.TransportCustomization.ProvisionQueues(transportSettings, []);
+
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<ServiceControlEndpoint>(c => c.CustomConfig(ec =>
+                {
+                    configuration.TransportCustomization.CustomizePrimaryEndpoint(ec, transportSettings);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.That(ctx.EndpointsStarted, Is.True);
+            Assert.That(transportSettings.MaxConcurrency == (setConcurrency ?? 1));
+        }
+
         public class Context : ScenarioContext
         {
         }
