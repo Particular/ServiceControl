eb6010cc2b39e8da1d00d80de975337f815abac3
Phil Bastian <phil.bastian@particular.net>
2024-10-03 16:24:01 +0800

    initial tests for max concurrency setting
    (cherry picked from commit 5672ca0c084b65c90cacc395f4a4c219cda3efce)

---
+++ src/ServiceControl.Transports.SqlServer.Tests/SqlServerTransportTests.cs
@@ -0,0 +1,83 @@
+﻿namespace ServiceControl.Transports.PostgreSql.Tests
+{
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+    using ServiceControl.Transport.Tests;
+
+    [TestFixture]
+    class SqlServerTransportTests : TransportTestFixture
+    {
+        [TestCase(15)]
+        [TestCase(null)]
+        public async Task Should_set_max_concurrency_for_audit(int? setConcurrency)
+        {
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = configuration.ConnectionString,
+                MaxConcurrency = setConcurrency
+            };
+
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<SendOnlyEndpoint>(c => c.CustomConfig(ec =>
+                {
+                    configuration.TransportCustomization.CustomizeAuditEndpoint(ec, transportSettings);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.That(transportSettings.MaxConcurrency == (setConcurrency ?? 10));
+        }
+
+        [TestCase(15)]
+        [TestCase(null)]
+        public async Task Should_set_max_concurrency_for_monitoring(int? setConcurrency)
+        {
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = configuration.ConnectionString,
+                MaxConcurrency = setConcurrency
+            };
+
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<SendOnlyEndpoint>(c => c.CustomConfig(ec =>
+                {
+                    configuration.TransportCustomization.CustomizeMonitoringEndpoint(ec, transportSettings);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.That(transportSettings.MaxConcurrency == (setConcurrency ?? 10));
+        }
+
+        [TestCase(15)]
+        [TestCase(null)]
+        public async Task Should_set_max_concurrency_for_primary(int? setConcurrency)
+        {
+            var transportSettings = new TransportSettings
+            {
+                ConnectionString = configuration.ConnectionString,
+                MaxConcurrency = setConcurrency
+            };
+
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<SendOnlyEndpoint>(c => c.CustomConfig(ec =>
+                {
+                    configuration.TransportCustomization.CustomizePrimaryEndpoint(ec, transportSettings);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.That(transportSettings.MaxConcurrency == (setConcurrency ?? 10));
+        }
+
+        public class Context : ScenarioContext
+        {
+        }
+
+        public class SendOnlyEndpoint : EndpointConfigurationBuilder
+        {
+            public SendOnlyEndpoint() => EndpointSetup<BasicEndpointSetup>();
+        }
+    }
+}
