143efd079069c73b275876b035400c9102a63b07
Brandon Ording <bording@gmail.com>
2024-10-04 15:57:20 -0400

    Update version check to consider product version instead

--- src/ServiceControl.RavenDB/StartupChecks.cs
+++ src/ServiceControl.RavenDB/StartupChecks.cs
@@ -10,11 +10,14 @@
         public static async Task EnsureServerVersion(IDocumentStore store, CancellationToken cancellationToken = default)
         {
             var build = await store.Maintenance.Server.SendAsync(new GetBuildNumberOperation(), cancellationToken);
-            var embeddedVersion = typeof(Raven.Embedded.EmbeddedServer).Assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>().First().InformationalVersion;
 
-            if (embeddedVersion != build.FullVersion)
+            var clientVersion = typeof(Raven.Client.Constants).Assembly.GetCustomAttributes<AssemblyInformationalVersionAttribute>().First().InformationalVersion;
+            var parts = clientVersion.Split('.');
+            var clientProductVersion = $"{parts[0]}.{parts[1]}";
+
+            if (clientProductVersion != build.ProductVersion)
             {
-                throw new Exception($"ServiceControl expects RavenDB Server version {embeddedVersion} but the server is using {build.FullVersion}.");
+                throw new Exception($"ServiceControl expects RavenDB Server version {clientProductVersion} but the server is using {build.ProductVersion}.");
             }
         }
     }
