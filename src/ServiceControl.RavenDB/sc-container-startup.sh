8ea846f45520a3d3d43b2a66af0ad1243883e499
Brandon Ording <bording@gmail.com>
2024-10-04 18:39:46 -0400

    Update script to handle recursive permissions checks

--- src/ServiceControl.RavenDB/sc-container-startup.sh
+++ src/ServiceControl.RavenDB/sc-container-startup.sh
@@ -4,15 +4,22 @@ set -euo pipefail
 LEGACY_PATH="/opt/RavenDB/Server/RavenData"
 NEW_PATH="$RAVEN_DataDir"
 
-if [ -d "$LEGACY_PATH" ]; then
-  echo "Unmigrated data detected. See the upgrade guide for details on how to fix this."
+if [[ -d "$LEGACY_PATH" ]]; then
+  echo "RavenDB data is being mounted to the wrong location. See the upgrade guide for details on how to fix this."
   exit 1
 fi
 
-read -r permissions username groupname <<<$(stat -c "%a %U %G" $NEW_PATH)
+FILECOUNT = find $NEW_PATH \! -writable -print0 | grep -zc .
 
-if [[ $permissions =~ ..[0-5] ]] && { [[ $groupname != "ravendb" ]] || [[ $permissions =~ .[0-5]. ]]; } && { [[ $username != "ravendb" ]] || [[ $permissions =~ [0-5].. ]]; } then
-  echo "The permissions of the mounted data are not correct. See the upgrade guide for details on how to fix this."
+if [[ $FILECOUNT -ne 0 ]]; then
+  echo "RavenDB data is not writable by the ravendb user. See the upgrade guide for details on how to fix this."
+  exit 1
+fi
+
+FILECOUNT = find $NEW_PATH \! -readable -print0 | grep -zc .
+
+if [[ $FILECOUNT -ne 0 ]]; then
+  echo "RavenDB data is not readable by the ravendb user. See the upgrade guide for details on how to fix this."
   exit 1
 fi
 
