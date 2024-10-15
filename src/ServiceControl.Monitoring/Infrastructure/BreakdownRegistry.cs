f3a7039f4fcfde03907444225353c30192775a7f
Christian <christian.quiros+github@gmail.com>
2024-10-15 11:33:02 -0600

    Remove cached lookup

--- src/ServiceControl.Monitoring/Infrastructure/BreakdownRegistry.cs
+++ src/ServiceControl.Monitoring/Infrastructure/BreakdownRegistry.cs
@@ -5,67 +5,27 @@ namespace ServiceControl.Monitoring.Infrastructure
     using System.Collections.Generic;
     using System.Linq;
 
-    public abstract class BreakdownRegistry<BreakdownT>
+    public abstract class BreakdownRegistry<TBreakdown>(Func<TBreakdown, string> endpointNameExtractor)
     {
-        protected BreakdownRegistry(Func<BreakdownT, string> endpointNameExtractor)
-        {
-            this.endpointNameExtractor = endpointNameExtractor;
-        }
+        public void Record(TBreakdown breakdown) => AddBreakdown(breakdown, breakdowns);
 
-        public void Record(BreakdownT breakdown)
-        {
-            if (AddBreakdown(breakdown, breakdowns))
-            {
-                UpdateLookups();
-            }
-        }
+        protected virtual bool AddBreakdown(TBreakdown breakdown, ConcurrentDictionary<TBreakdown, TBreakdown> existingBreakdowns) => existingBreakdowns.TryAdd(breakdown, breakdown);
 
-        protected virtual bool AddBreakdown(BreakdownT breakdown, ConcurrentDictionary<BreakdownT, BreakdownT> existingBreakdowns)
-        {
-            if (existingBreakdowns.ContainsKey(breakdown))
-            {
-                return false;
-            }
+        public IReadOnlyDictionary<string, TBreakdown[]> GetGroupedByEndpointName() =>
+            breakdowns.Values
+                .GroupBy(endpointNameExtractor)
+                .ToDictionary(g => g.Key, g => g.Select(i => i).ToArray());
 
-            existingBreakdowns.TryAdd(breakdown, breakdown);
+        public TBreakdown[] GetForEndpointName(string endpointName) => GetGroupedByEndpointName().TryGetValue(endpointName, out var endpointBreakdowns) ? endpointBreakdowns : [];
 
-            return true;
-        }
-
-        public IReadOnlyDictionary<string, BreakdownT[]> GetGroupedByEndpointName()
-        {
-            return lookup;
-        }
-
-        public BreakdownT[] GetForEndpointName(string endpointName)
-        {
-            if (lookup.TryGetValue(endpointName, out var endpointBreakdowns))
-            {
-                return endpointBreakdowns;
-            }
-
-            return [];
-        }
-
-        public void RemoveBreakdowns(IEnumerable<BreakdownT> breakdownsToRemove)
+        protected void RemoveBreakdowns(IEnumerable<TBreakdown> breakdownsToRemove)
         {
             foreach (var breakdown in breakdownsToRemove)
             {
-                breakdowns.Remove(breakdown, out _);
+                _ = breakdowns.Remove(breakdown, out _);
             }
-            UpdateLookups();
         }
 
-        void UpdateLookups()
-        {
-            lookup = new ConcurrentDictionary<string, BreakdownT[]>(breakdowns.Values
-                .GroupBy(b => endpointNameExtractor(b))
-                .ToDictionary(g => g.Key, g => g.Select(i => i).ToArray()));
-        }
-
-        ConcurrentDictionary<BreakdownT, BreakdownT> breakdowns = [];
-        ConcurrentDictionary<string, BreakdownT[]> lookup = [];
-
-        Func<BreakdownT, string> endpointNameExtractor;
+        readonly ConcurrentDictionary<TBreakdown, TBreakdown> breakdowns = [];
     }
 }
