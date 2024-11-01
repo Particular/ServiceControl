dbb0a761ddc0f30ff478c3e955b3e74ae7b8cb78
Phil Bastian <phil.bastian@particular.net>
2024-11-01 13:50:18 +0800

    record connected application by heartbeat control message rather than looking at failed message headers

---
+++ src/ServiceControl/Monitoring/HeartbeatMonitoring/InternalMessages/ConnectedApplication.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceControl.Monitoring;
+
+using NServiceBus;
+
+public class ConnectedApplication : IMessage
+{
+    public string Application { get; set; }
+    public string[] ErrorQueues { get; set; }
+}
---
+++ src/ServiceControl/Monitoring/Transport/ConnectedApplicationHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceControl.Monitoring.Transport
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using ServiceControl.Monitoring;
+    using ServiceControl.Persistence;
+
+    public class ConnectedApplicationHandler(IConnectedApplicationsDataStore connectedApplicationsDataStore) : IHandleMessages<ConnectedApplication>
+    {
+        public Task Handle(ConnectedApplication message, IMessageHandlerContext context)
+        {
+            _ = connectedApplicationsDataStore.Add(message.Application);
+
+            return Task.CompletedTask;
+        }
+    }
+}
