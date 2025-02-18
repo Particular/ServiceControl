1305f0fdda60cc629e6aa6d8d873fb7778388665
Mauro Servienti <mauro.servienti@gmail.com>
2025-02-18 17:13:52 +0100

    When running the installer engine (via SCMU or PowerShell) we can assume it is Windows, and thus we can set the shutdown timeout to 2 minutes

--- src/ServiceControlInstaller.Engine/Configuration/ServiceControl/ServiceControlAppConfig.cs
+++ src/ServiceControlInstaller.Engine/Configuration/ServiceControl/ServiceControlAppConfig.cs
@@ -32,6 +32,12 @@
             settings.Set(ServiceControlSettings.EnableFullTextSearchOnBodies, details.EnableFullTextSearchOnBodies.ToString(), version);
             settings.Set(ServiceControlSettings.RemoteInstances, RemoteInstanceConverter.ToJson(details.RemoteInstances), version);
 
+            // Windows services allow a maximum of 125 seconds when stopping a service.
+            // When shutting down or restarting the OS we have no control over the
+            // shutdown timeout. This is by the installer engine that is run _only_ on
+            // Windows via SCMU or PowerShell
+            settings.Set(ServiceControlSettings.ShutdownTimeout, "00:02:00");
+
             // Retired settings
             settings.RemoveIfRetired(ServiceControlSettings.AuditQueue, version);
             settings.RemoveIfRetired(ServiceControlSettings.AuditLogQueue, version);
