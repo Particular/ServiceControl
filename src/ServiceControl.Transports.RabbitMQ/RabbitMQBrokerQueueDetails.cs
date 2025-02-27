27ed3f9d4689d6e7344a147554824d8237bdb152
Brandon Ording <bording@gmail.com>
2025-02-26 19:10:02 -0500

    Formatting

--- src/ServiceControl.Transports.RabbitMQ/RabbitMQBrokerQueueDetails.cs
+++ src/ServiceControl.Transports.RabbitMQ/RabbitMQBrokerQueueDetails.cs
@@ -2,22 +2,27 @@
 namespace ServiceControl.Transports.RabbitMQ;
 
 using System.Collections.Generic;
-using ServiceControl.Transports.BrokerThroughput;
 using NServiceBus.Transport.RabbitMQ.ManagementApi;
+using ServiceControl.Transports.BrokerThroughput;
 
 class RabbitMQBrokerQueueDetails(Queue queue) : IBrokerQueue
 {
     public string QueueName { get; } = queue.Name;
+
     public string SanitizedName => QueueName;
+
     public string? Scope => null;
-    //public string VHost { get; } = queue.Vhost;
+
     public List<string> EndpointIndicators { get; } = [];
+
     long? AckedMessages { get; set; } = queue.MessageStats?.Ack;
+
     long Baseline { get; set; } = queue.MessageStats?.Ack ?? 0;
 
     public long CalculateThroughputFrom(RabbitMQBrokerQueueDetails newReading)
     {
         var newlyAckedMessages = 0L;
+
         if (newReading.AckedMessages is null)
         {
             return newlyAckedMessages;
@@ -28,6 +33,7 @@ class RabbitMQBrokerQueueDetails(Queue queue) : IBrokerQueue
             newlyAckedMessages = newReading.AckedMessages.Value - Baseline;
             AckedMessages += newlyAckedMessages;
         }
+
         Baseline = newReading.AckedMessages.Value;
 
         return newlyAckedMessages;
