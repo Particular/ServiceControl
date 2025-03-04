8eb5b48bd4d3cfb57ba90acb2e0eddf713131297
Mauro Servienti <mauro.servienti@gmail.com>
2025-03-04 13:57:20 +0100

    Add the ShutdownTimeout to the Monitoring instance

--- src/ServiceControlInstaller.Engine/Configuration/Monitoring/AppConfig.cs
+++ src/ServiceControlInstaller.Engine/Configuration/Monitoring/AppConfig.cs
@@ -22,6 +22,12 @@
             settings.Set(SettingsList.TransportType, details.TransportPackage.Name, version);
             settings.Set(SettingsList.ErrorQueue, details.ErrorQueue);
 
+            // Windows services allow a maximum of 125 seconds when stopping a service.
+            // When shutting down or restarting the OS we have no control over the
+            // shutdown timeout. This is by the installer engine that is run _only_ on
+            // Windows via SCMU or PowerShell
+            settings.Set(SettingsList.ShutdownTimeout, "00:02:00", version);
+
             // Retired settings
             settings.RemoveIfRetired(SettingsList.EndpointName, version);
 
