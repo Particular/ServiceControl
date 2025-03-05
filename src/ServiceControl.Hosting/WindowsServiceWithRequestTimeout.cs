f8737dd6df442b4561ebed4a1662d80480afd1b8
Ramon Smits <ramon.smits@gmail.com>
2025-03-05 19:17:23 +0100

    Remove TODO, tested lifetime without constructor on stop and on shutdown

--- src/ServiceControl.Hosting/WindowsServiceWithRequestTimeout.cs
+++ src/ServiceControl.Hosting/WindowsServiceWithRequestTimeout.cs
@@ -13,12 +13,6 @@ sealed class WindowsServiceWithRequestTimeout : WindowsServiceLifetime
     static readonly TimeSpan CancellationDuration = TimeSpan.FromSeconds(5);
     readonly HostOptions hostOptions;
 
-    // TODO: This constructor should not be needed and exist for backwards compability in the runtime
-    // public WindowsServiceWithRequestTimeout(IHostEnvironment environment, IHostApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory, IOptions<HostOptions> optionsAccessor)
-    //     : this(environment, applicationLifetime, loggerFactory, optionsAccessor, Options.Create(new WindowsServiceLifetimeOptions()))
-    // {
-    // }
-
     public WindowsServiceWithRequestTimeout(IHostEnvironment environment, IHostApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory, IOptions<HostOptions> optionsAccessor, IOptions<WindowsServiceLifetimeOptions> windowsServiceOptionsAccessor)
         : base(environment, applicationLifetime, loggerFactory, optionsAccessor, windowsServiceOptionsAccessor)
     {
