d24734fb847d144a625a8b1d9433e493b3715f0a
Andreas Ohlund <andreas.ohlund@particular.net>
2025-03-05 11:01:26 +0100

    Suppress

--- src/SetupProcessFake/Program.cs
+++ src/SetupProcessFake/Program.cs
@@ -12,7 +12,9 @@ if (args.Any(a => a == "non-zero-exit-code"))
 
 if (args.Any(a => a == "delay"))
 {
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
     await Task.Delay(TimeSpan.FromSeconds(5));
+#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
 }
 
 return 0;
