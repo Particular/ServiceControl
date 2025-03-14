ac0b16d9655ef52135e449fa107058688387e431
Ramon Smits <ramon.smits@gmail.com>
2025-03-14 15:17:11 +0100

    MemoryInformationRetriever init supports now supports external and embedded instances

--- src/ServiceControl.Persistence.RavenDB/MemoryInformationRetriever.cs
+++ src/ServiceControl.Persistence.RavenDB/MemoryInformationRetriever.cs
@@ -11,8 +11,8 @@ class MemoryInformationRetriever(RavenPersisterSettings persisterSettings)
     // What does a connection string look like? Is it only a URI or could it contain other stuff?
     // The primary instance has only the concept of a connection string (vs the Audit instance having
     // both a ServiceUrl and a ConnectionString). If the connection string contain always only the
-    // server URL, this code is safe, otherwise it need to be adjusted to extract the server URL. 
-    readonly HttpClient client = new() { BaseAddress = new Uri(persisterSettings.ConnectionString) };
+    // server URL, this code is safe, otherwise it need to be adjusted to extract the server URL.
+    readonly HttpClient client = new() { BaseAddress = new Uri(persisterSettings.ServerUrl ?? persisterSettings.ConnectionString) };
 
     record ResponseDto
     {
