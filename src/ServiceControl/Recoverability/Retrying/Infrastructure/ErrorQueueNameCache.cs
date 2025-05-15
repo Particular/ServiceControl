799556a07890187b4888a86d0d488932a82a34c0
Adam Furmanek <adam.furmanek@particular.net>
2025-05-15 14:39:39 +0200

    Setting proper queue name on edit & retry (#4973)
    * Setting proper queue name on edit & retry
    
    * Update src/ServiceControl.Persistence.Tests/Recoverability/EditMessageTests.cs
    
    Co-authored-by: Laila Bougria <laila.bougria@particular.net>
    
    * Update src/ServiceControl.Persistence.Tests/Recoverability/EditMessageTests.cs
    
    Co-authored-by: Laila Bougria <laila.bougria@particular.net>
    
    * ⚜️ small code improvements
    
    ---------
    
    Co-authored-by: Laila Bougria <laila.bougria@particular.net>
    Co-authored-by: Ramon Smits <ramon.smits@gmail.com>

---
+++ src/ServiceControl/Recoverability/Retrying/Infrastructure/ErrorQueueNameCache.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceControl.Recoverability;
+
+using System;
+
+class ErrorQueueNameCache
+{
+    string resolvedErrorAddress;
+
+    public string ResolvedErrorAddress
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(resolvedErrorAddress))
+            {
+                throw new InvalidOperationException($"{nameof(ResolvedErrorAddress)} is not set. Please set it before accessing.");
+            }
+
+            return resolvedErrorAddress;
+        }
+        set => resolvedErrorAddress = value;
+    }
+}
