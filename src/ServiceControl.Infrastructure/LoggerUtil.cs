e9f287e1af3e39087ca5332410898b6d714de941
JasonTaylorDev <hello@jasontaylor.dev>
2025-06-13 11:45:22 +1000

    Remove test code

--- src/ServiceControl.Infrastructure/LoggerUtil.cs
+++ src/ServiceControl.Infrastructure/LoggerUtil.cs
@@ -16,9 +16,7 @@
         public static ILogger<T> CreateStaticLogger<T>(LogLevel level = LogLevel.Information)
         {
             var factory = LoggerFactory.Create(configure => configure.BuildLogger(level));
-            var logger = factory.CreateLogger<T>();
-            logger.LogError("JTD: This is a test log message.");
-            return logger;
+            return factory.CreateLogger<T>();
         }
 
         public static ILogger CreateStaticLogger(Type type, LogLevel level = LogLevel.Information)
