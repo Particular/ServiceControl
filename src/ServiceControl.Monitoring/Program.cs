005968d492cc02852c676a1c41e146c52fe676bb
JasonTaylorDev <hello@jasontaylor.dev>
2025-06-17 10:10:46 +1000

    Properly shutdown NLog in Program.cs

--- src/ServiceControl.Monitoring/Program.cs
+++ src/ServiceControl.Monitoring/Program.cs
@@ -41,4 +41,5 @@ finally
 {
     // The following log statement is meant to leave a trail in the logs to determine if the process was killed
     logger.LogInformation("Shutdown complete");
+    NLog.LogManager.Shutdown();
 }
