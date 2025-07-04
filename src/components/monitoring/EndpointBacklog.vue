f66cfd314a02d7686e4fdfb3bf6d3d36ab1131fd
Jason Taylor <hello@jasontaylor.dev>
2025-07-04 16:43:10 +1000

    Restore original spacing

--- src/components/monitoring/EndpointBacklog.vue
+++ src/components/monitoring/EndpointBacklog.vue
@@ -25,7 +25,7 @@ const endpoint = defineModel<ExtendedEndpointDetails>({
     <div class="no-side-padding graph-values">
       <div aria-label="queue-length-values" class="queue-length-values">
         <div aria-label="metric-header">
-          <span class="metric-digest-header" v-tippy="QueueLength.tooltip">{{ QueueLength.label }}</span>
+          <span class="metric-digest-header" v-tippy="QueueLength.tooltip"> {{ QueueLength.label }} </span>
         </div>
         <div aria-label="metric-current-value" class="metric-digest-value current">
           <div v-if="!endpoint.isStale && !endpoint.isScMonitoringDisconnected">
