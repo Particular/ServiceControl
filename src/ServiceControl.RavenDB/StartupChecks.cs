1083f4d5af2ada6adbb155e91d3e1bd81c90d8e3
Ramon Smits <ramon.smits@gmail.com>
2025-09-09 11:19:31 +0200

    Added information regarding RavenDB version compatibility policy

--- src/ServiceControl.RavenDB/StartupChecks.cs
+++ src/ServiceControl.RavenDB/StartupChecks.cs
@@ -9,6 +9,16 @@
     {
         public static async Task EnsureServerVersion(IDocumentStore store, CancellationToken cancellationToken = default)
         {
+            // RavenDB compatibility policy is that the major/minor version of the server must be
+            // equal or higher than the client and ignores the patch version.
+            //
+            // https://docs.ravendb.net/6.2/client-api/faq/backward-compatibility/#compatibility---ravendb-42-and-higher
+            //
+            // > Starting with version 4.2, RavenDB clients are compatible with any server of their own version and higher.
+            // > E.g. -
+            // >
+            // > Client 4.2 is compatible with Server 4.2, Server 4.5, Server 5.2, and any other server of a higher version.
+
             var build = await store.Maintenance.Server.SendAsync(new GetBuildNumberOperation(), cancellationToken);
             var serverProductVersion = new Version(build.ProductVersion);
 
