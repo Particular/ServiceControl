55f69c5a74882e1e758ca1f55a5fdeb73a9f3c87
John Simons <john.simons@particular.net>
2025-10-09 12:05:27 +1000

    Log a warning if a call comes that is not handled

--- test/drivers/vitest/setup.ts
+++ test/drivers/vitest/setup.ts
@@ -22,9 +22,8 @@ beforeAll(() => {
   console.log("Starting mock server");
 
   mockServer.listen({
-    onUnhandledRequest: (request, { error }) => {
-      console.log("Unhandled %s %s", request.method, request.url);
-      error();
+    onUnhandledRequest: (request) => {
+      console.warn("Unhandled %s %s", request.method, request.url);
     },
   });
 });
