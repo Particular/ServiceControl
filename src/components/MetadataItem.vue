58fb9b386ef4de171e0b34d053d8c2aaf89eb4f7
John Simons <john.simons@particular.net>
2025-10-10 11:06:15 +1000

    Add MetadataItem component

---
+++ src/components/MetadataItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import FAIcon from "@/components/FAIcon.vue";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+  icon?: IconDefinition;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <span class="metadata">
+    <FAIcon v-if="icon" :icon="icon" class="icon" />
+    <slot />
+  </span>
+</template>
+
+<style scoped>
+.metadata {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.25rem;
+}
+
+.icon {
+  margin-right: 0.25rem;
+}
+</style>
