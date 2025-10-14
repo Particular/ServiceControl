ca87e48c0bd151eeaf79d21b7856b3e4d6dc0ad2
John Simons <john.simons@particular.net>
2025-10-14 11:18:29 +1000

    Address feedback

--- src/stores/CustomChecksStore.ts
+++ src/stores/CustomChecksStore.ts
@@ -1,7 +1,8 @@
 import { useDeleteFromServiceControl, useTypedFetchFromServiceControl } from "@/composables/serviceServiceControlUrls";
 import CustomCheck from "@/resources/CustomCheck";
 import { acceptHMRUpdate, defineStore } from "pinia";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
+import { useCounter } from "@vueuse/core";
 
 export const useCustomChecksStore = defineStore("CustomChecksStore", () => {
   const prefix = "customchecks/";
@@ -9,10 +10,12 @@ export const useCustomChecksStore = defineStore("CustomChecksStore", () => {
   const pageNumber = ref(1);
   const failingCount = ref(0);
   const failedChecks = ref<CustomCheck[]>([]);
-  let pauseRefresh = false;
+
+  const { count, inc, dec } = useCounter();
+  const skipRefresh = computed(() => count.value > 0);
 
   const refresh = async () => {
-    if (pauseRefresh) {
+    if (skipRefresh.value) {
       return;
     }
     try {
@@ -29,15 +32,18 @@ export const useCustomChecksStore = defineStore("CustomChecksStore", () => {
   watch(pageNumber, () => refresh());
 
   async function dismissCustomCheck(id: string) {
-    pauseRefresh = true;
-    // NOTE: If it takes more than the refresh interval for ServiceControl to delete the check it will reappear
-    failedChecks.value = failedChecks.value.filter((x) => x.id !== id);
-    failingCount.value--;
+    try {
+      inc();
+      // NOTE: If it takes more than the refresh interval for ServiceControl to delete the check it will reappear
+      failedChecks.value = failedChecks.value.filter((x) => x.id !== id);
+      failingCount.value--;
 
-    // HINT: This is required to handle the difference between ServiceControl 4 and 5
-    const guid = id.toLocaleLowerCase().startsWith(prefix) ? id.substring(prefix.length) : id;
-    await useDeleteFromServiceControl(`${prefix}${guid}`);
-    pauseRefresh = false;
+      // HINT: This is required to handle the difference between ServiceControl 4 and 5
+      const guid = id.toLocaleLowerCase().startsWith(prefix) ? id.substring(prefix.length) : id;
+      await useDeleteFromServiceControl(`${prefix}${guid}`);
+    } finally {
+      dec();
+    }
   }
 
   return {
