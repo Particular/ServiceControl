c26fc2f3d7b870b53810ec760878c87d1c947366
Jason Taylor <hello@jasontaylor.dev>
2025-11-26 10:30:44 +1000

    Add ServicePulse-specific OIDC configuration and endpoint

---
+++ src/ServiceControl/Authentication/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿namespace ServiceControl.Authentication
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using ServiceBus.Management.Infrastructure.Settings;
+
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController(Settings settings) : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("configuration")]
+        public ActionResult<AuthConfig> Configuration()
+        {
+            var info = new AuthConfig
+            {
+                Enabled = settings.OpenIdConnectSettings.ServicePulseEnabled,
+                ClientId = settings.OpenIdConnectSettings.ServicePulseClientId,
+                Authority = settings.OpenIdConnectSettings.ServicePulseAuthority,
+                ApiScope = settings.OpenIdConnectSettings.ServicePulseApiScope
+            };
+
+            return Ok(info);
+        }
+    }
+
+    public class AuthConfig
+    {
+        public bool Enabled { get; set; }
+        public string ClientId { get; set; }
+        public string Authority { get; set; }
+        public string ApiScope { get; set; }
+    }
+}
