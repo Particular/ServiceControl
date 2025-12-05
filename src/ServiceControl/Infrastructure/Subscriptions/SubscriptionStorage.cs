630dfd20b19f5579dfcc622b3c820c3c4a167115
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-05 17:54:28 +0100

    Adjust subscription persistence

--- src/ServiceControl/Infrastructure/Subscriptions/SubscriptionStorage.cs
+++ src/ServiceControl/Infrastructure/Subscriptions/SubscriptionStorage.cs
@@ -8,36 +8,17 @@
     using NServiceBus.Transport;
     using ServiceControl.Persistence;
 
-    class SubscriptionStorage : Feature
+    sealed class SubscriptionStorage : Feature
     {
-        SubscriptionStorage()
+        public SubscriptionStorage() => Prerequisite(c => c.Settings.Get<TransportDefinition>().SupportsPublishSubscribe == false, "The transport supports native pub sub");
+
+        protected override void Setup(FeatureConfigurationContext context) => context.RegisterStartupTask(b => new PrimeSubscriptions(b.GetRequiredService<IServiceControlSubscriptionStorage>()));
+
+        class PrimeSubscriptions(IServiceControlSubscriptionStorage persister) : FeatureStartupTask
         {
-            Prerequisite(c => c.Settings.Get<TransportDefinition>().SupportsPublishSubscribe == false, "The transport supports native pub sub");
-        }
+            protected override Task OnStart(IMessageSession session, CancellationToken cancellationToken = default) => persister?.Initialize() ?? Task.CompletedTask;
 
-        protected override void Setup(FeatureConfigurationContext context)
-        {
-            context.RegisterStartupTask(b => new PrimeSubscriptions(b.GetRequiredService<IServiceControlSubscriptionStorage>()));
-        }
-
-        class PrimeSubscriptions : FeatureStartupTask
-        {
-            public IServiceControlSubscriptionStorage persister;
-
-            public PrimeSubscriptions(IServiceControlSubscriptionStorage persister)
-            {
-                this.persister = persister;
-            }
-
-            protected override Task OnStart(IMessageSession session, CancellationToken cancellationToken = default)
-            {
-                return persister?.Initialize() ?? Task.CompletedTask;
-            }
-
-            protected override Task OnStop(IMessageSession session, CancellationToken cancellationToken = default)
-            {
-                return Task.CompletedTask;
-            }
+            protected override Task OnStop(IMessageSession session, CancellationToken cancellationToken = default) => Task.CompletedTask;
         }
     }
 }
