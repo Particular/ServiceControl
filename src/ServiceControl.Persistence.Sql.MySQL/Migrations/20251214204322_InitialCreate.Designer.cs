6e761e1d05ecfd18c516ba57834f63316e95dd9b
John Simons <john.simons@particular.net>
2025-12-15 10:10:56 +1000

    Generate initial MySQL database migration

--- src/ServiceControl.Persistence.Sql.MySQL/Migrations/20251214204322_InitialCreate.Designer.cs
+++ src/ServiceControl.Persistence.Sql.MySQL/Migrations/20251214204322_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServiceControl.Persistence.Sql.MySQL;
 
@@ -11,9 +12,11 @@ using ServiceControl.Persistence.Sql.MySQL;
 namespace ServiceControl.Persistence.Sql.MySQL.Migrations
 {
     [DbContext(typeof(MySqlDbContext))]
-    partial class MySqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214204322_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/ServiceControl.Persistence.Sql.MySQL/MySqlDbContextFactory.cs
+++ src/ServiceControl.Persistence.Sql.MySQL/MySqlDbContextFactory.cs
@@ -12,7 +12,8 @@ class MySqlDbContextFactory : IDesignTimeDbContextFactory<MySqlDbContext>
 
         // Use a dummy connection string for design-time operations
         var connectionString = "Server=localhost;Database=servicecontrol;User=root;Password=mysql";
-        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        // Use a fixed server version for design-time to avoid connection attempts
+        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)));
 
         return new MySqlDbContext(optionsBuilder.Options);
     }
