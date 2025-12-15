fed1ce4b1a97fa96dc873c3555b1c35d488eb1d1
John Simons <john.simons@particular.net>
2025-12-15 10:10:56 +1000

    Generate initial PostgreSQL database migration

--- src/ServiceControl.Persistence.Sql.PostgreSQL/Migrations/20251214204335_InitialCreate.Designer.cs
+++ src/ServiceControl.Persistence.Sql.PostgreSQL/Migrations/20251214204335_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ServiceControl.Persistence.Sql.PostgreSQL;
@@ -11,9 +12,11 @@ using ServiceControl.Persistence.Sql.PostgreSQL;
 namespace ServiceControl.Persistence.Sql.PostgreSQL.Migrations
 {
     [DbContext(typeof(PostgreSqlDbContext))]
-    partial class PostgreSqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214204335_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
