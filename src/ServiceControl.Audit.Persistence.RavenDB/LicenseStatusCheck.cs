5ea391cf2987833fa3d68b90aaa6bf530ba3530f
Mauro Servienti <mauro.servienti@gmail.com>
2025-12-17 17:14:32 +0100

    Add a license status check to ensure the RavenDB licence is correctly applied after the database is created before configuring expiration

---
+++ src/ServiceControl.Audit.Persistence.RavenDB/LicenseStatusCheck.cs
@@ -0,0 +1,47 @@
+namespace ServiceControl.Audit.Persistence.RavenDB;
+
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+static class LicenseStatusCheck
+{
+    class LicenseStatusFragment
+    {
+        public string Id { get; set; }
+        public string LicensedTo { get; set; }
+        public string Status { get; set; }
+        public bool Expired { get; set; }
+    }
+
+    public static async Task WaitForLicenseOrThrow(DatabaseConfiguration configuration, CancellationToken cancellationToken)
+    {
+        var client = new HttpClient() { BaseAddress = new Uri(configuration.ServerConfiguration.ConnectionString ?? configuration.ServerConfiguration.ServerUrl) };
+        var licenseCorrectlySetup = false;
+        var attempts = 0;
+        while (!licenseCorrectlySetup)
+        {
+            var httpResponse = await client.GetAsync("/license/status", cancellationToken);
+            var responseJsonString = await httpResponse.Content.ReadAsStringAsync(cancellationToken);
+            var licenseStatus = JsonSerializer.Deserialize<LicenseStatusFragment>(responseJsonString);
+            if (licenseStatus.Expired)
+            {
+                throw new NotSupportedException("The current RavenDB license is expired. Please, contact support");
+            }
+
+            if (licenseStatus.LicensedTo != null && licenseStatus.Id != null)
+            {
+                licenseCorrectlySetup = true;
+            }
+
+            if (++attempts > 10)
+            {
+                throw new NotSupportedException("Cannot validate the current RavenDB license. Please, contact support");
+            }
+
+            await Task.Delay(500, cancellationToken);
+        }
+    }
+}
