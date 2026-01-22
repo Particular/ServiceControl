4fbe19111d9573a4f9bd2203bfdfe920a362fb42
Mauro Servienti <mauro.servienti@gmail.com>
2026-01-22 21:23:08 +0100

    The license status check fails when checking license against a secured RavenDB instance  (#5274)
    * Use the internal RavenDB HTTP client
    
    * extract the server URL from the document store
    
    * Refactor license status check with cancellation token
    
    * Fix whitespace
    
    * Make the primary instance LicenseStatusCheck behave like the audit instance one
    
    ---------
    
    Co-authored-by: Ramon Smits <ramon.smits@gmail.com>

--- src/ServiceControl.Audit.Persistence.RavenDB/LicenseStatusCheck.cs
+++ src/ServiceControl.Audit.Persistence.RavenDB/LicenseStatusCheck.cs
@@ -1,41 +1,45 @@
 namespace ServiceControl.Audit.Persistence.RavenDB;
 
 using System;
-using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Raven.Client.Documents;
 
 static class LicenseStatusCheck
 {
     record LicenseStatusFragment(string Id, string LicensedTo, string Status, bool Expired);
 
-    public static async Task WaitForLicenseOrThrow(DatabaseConfiguration configuration, CancellationToken cancellationToken)
+    public static async Task WaitForLicenseOrThrow(IDocumentStore documentStore, CancellationToken cancellationToken)
     {
-        using var client = new HttpClient
+        var ravenConfiguredHttpClient = documentStore.GetRequestExecutor().HttpClient;
+        var licenseCheckUrl = documentStore.Urls[0].TrimEnd('/') + "/license/status";
+
+        using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        cts.CancelAfter(30_000);
+
+        try
         {
-            BaseAddress = new Uri(configuration.ServerConfiguration.ConnectionString ?? configuration.ServerConfiguration.ServerUrl)
-        };
-
-        // Not linking to the incoming cancellationToken to ensure no OperationCancelledException prevents the last InvalidOperationException to be thrown
-        using var cts = new CancellationTokenSource(30_000);
-        while (!cts.IsCancellationRequested)
-        {
-            var httpResponse = await client.GetAsync("license/status", cancellationToken);
-            var licenseStatus = await httpResponse.Content.ReadFromJsonAsync<LicenseStatusFragment>(cancellationToken);
-            if (licenseStatus.Expired)
+            while (true)
             {
-                throw new InvalidOperationException("The current RavenDB license is expired. Please, contact support");
-            }
+                var httpResponse = await ravenConfiguredHttpClient.GetAsync(licenseCheckUrl, cts.Token);
+                var licenseStatus = await httpResponse.Content.ReadFromJsonAsync<LicenseStatusFragment>(cts.Token);
+                if (licenseStatus.Expired)
+                {
+                    throw new InvalidOperationException("The current RavenDB license is expired. Please, contact support");
+                }
 
-            if (licenseStatus.LicensedTo != null && licenseStatus.Id != null)
-            {
-                return;
-            }
+                if (licenseStatus.LicensedTo != null && licenseStatus.Id != null)
+                {
+                    return;
+                }
 
-            await Task.Delay(200, cancellationToken);
+                await Task.Delay(200, cts.Token);
+            }
         }
-
-        throw new InvalidOperationException("Cannot validate the current RavenDB license. Please, contact support");
+        catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested)
+        {
+            throw new InvalidOperationException("Cannot validate the current RavenDB license. Please, contact support");
+        }
     }
 }
