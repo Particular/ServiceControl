30ef2edf32993aad30ebe4a8c0bbc1e87d23e42c
Mike Minutillo <mike.minutillo@particular.net>
2026-02-06 15:08:11 +0800

    Add abstraction for environment data providers

---
+++ src/Particular.LicensingComponent.Contracts/IEnvironmentDataProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Particular.LicensingComponent.Contracts;
+
+/// <summary>
+/// Provides environment data that is included in usage reports
+/// </summary>
+public interface IEnvironmentDataProvider
+{
+    IEnumerable<(string key, string value)> GetData();
+}
