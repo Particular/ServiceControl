0186f529b3b897a4fd3f8c641f19cd53a66f3e5a
John Simons <john.simons@particular.net>
2026-02-14 13:49:06 +1000

    Adds CreatedOn indexes for performance
    Adds indexes to the CreatedOn column in the ProcessedMessages and SagaSnapshots tables.
    
    SQL Server doesn't use native partitioning.
    These indexes improve query performance when using retention policies.
    Specifically addresses the MIN() queries used by the retention cleaner.

--- src/ServiceControl.Audit.Persistence.Sql.SqlServer/SqlServerAuditDbContext.cs
+++ src/ServiceControl.Audit.Persistence.Sql.SqlServer/SqlServerAuditDbContext.cs
@@ -1,11 +1,20 @@
 namespace ServiceControl.Audit.Persistence.Sql.SqlServer;
 
 using Core.DbContexts;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 public class SqlServerAuditDbContext : AuditDbContextBase
 {
     public SqlServerAuditDbContext(DbContextOptions<SqlServerAuditDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreatingProvider(ModelBuilder modelBuilder)
+    {
+        // SQL Server doesn't use native partitioning, so it needs an index on CreatedOn
+        // for efficient MIN() queries used by the retention cleaner
+        modelBuilder.Entity<ProcessedMessageEntity>().HasIndex(e => e.CreatedOn);
+        modelBuilder.Entity<SagaSnapshotEntity>().HasIndex(e => e.CreatedOn);
     }
 }
