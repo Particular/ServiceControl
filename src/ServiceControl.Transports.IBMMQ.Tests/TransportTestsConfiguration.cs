48c6285327fa2d17f15c1ba3c678b24072fc95ac
Ramon Smits <ramon.smits@gmail.com>
2026-02-24 12:59:56 +0100

    Make queue length test work for IBMMQ transport

--- src/ServiceControl.Transports.IBMMQ.Tests/TransportTestsConfiguration.cs
+++ src/ServiceControl.Transports.IBMMQ.Tests/TransportTestsConfiguration.cs
@@ -1,49 +1,51 @@
-﻿using System;
+﻿namespace ServiceControl.Transport.Tests;
+
+using System;
+using System.Threading.Tasks;
+using Transports;
+using Transports.IBMMQ;
 using NServiceBus;
 using NServiceBus.Transport.IbmMq;
-using ServiceControl.Transports;
-using ServiceControl.Transports.IBMMQ;
+using NUnit.Framework;
 
-namespace ServiceControl.Transport.Tests
+[SetUpFixture]
+public class BootstrapFixture
 {
-    using System;
-    using System.Threading.Tasks;
-    using Transports;
-    using Transports.IBMMQ;
-
-    partial class TransportTestsConfiguration
-    {
-        public string ConnectionString { get; private set; }
-
-        public ITransportCustomization TransportCustomization { get; private set; }
-
-        public Task Configure()
-        {
-            TransportCustomization = new TestIBMMQTransportCustomization();
-            ConnectionString = Environment.GetEnvironmentVariable(ConnectionStringKey);
-
-            if (string.IsNullOrEmpty(ConnectionString))
-            {
-                throw new Exception($"Environment variable {ConnectionStringKey} is required for IBM MQ transport tests to run");
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public Task Cleanup() => Task.CompletedTask;
-
-        static string ConnectionStringKey = "ServiceControl_TransportTests_IBMMQ_ConnectionString";
-    }
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests() => TransportTestFixture.QueueNameSeparator = '.';
 }
 
+class TransportTestsConfiguration
+{
+    public string ConnectionString { get; private set; }
+
+    public ITransportCustomization TransportCustomization { get; private set; }
+
+    public Task Configure()
+    {
+        TransportCustomization = new TestIBMMQTransportCustomization();
+        ConnectionString = Environment.GetEnvironmentVariable(ConnectionStringKey);
+
+        if (string.IsNullOrEmpty(ConnectionString))
+        {
+            throw new Exception($"Environment variable {ConnectionStringKey} is required for IBM MQ transport tests to run");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task Cleanup() => Task.CompletedTask;
+
+    static string ConnectionStringKey = "ServiceControl_TransportTests_IBMMQ_ConnectionString";
+}
 
 sealed class TestIBMMQTransportCustomization : IBMMQTransportCustomization
 {
     protected override IbmMqTransport CreateTransport(TransportSettings transportSettings, TransportTransactionMode preferredTransactionMode = TransportTransactionMode.ReceiveOnly)
     {
         transportSettings.Set<Action<IbmMqTransportOptions>>(o => o.ResourceNameSanitizer = name => name
-            .Replace("ServiceControlMonitoring", "SCM") // Mitigate max queue name length
-            .Replace("-", ".") // dash is an illegal char
+                .Replace("ServiceControlMonitoring", "SCM") // Mitigate max queue name length
+                .Replace("-", ".") // dash is an illegal char
         );
         return base.CreateTransport(transportSettings, preferredTransactionMode);
     }
