965c92ad6f0df5d1cc2aa61dd12442380f87f93a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-05-11 11:31:28 +0200

    Disable assembly scanner in monitoring too

--- src/ServiceControl.Monitoring/HostApplicationBuilderExtensions.cs
+++ src/ServiceControl.Monitoring/HostApplicationBuilderExtensions.cs
@@ -81,6 +81,8 @@ public static class HostApplicationBuilderExtensions
 
     static void ConfigureEndpoint(EndpointConfiguration config, Func<ICriticalErrorContext, CancellationToken, Task> onCriticalError, ITransportCustomization transportCustomization, TransportSettings transportSettings, Settings settings, IServiceCollection services)
     {
+        config.AssemblyScanner().Disable = true;
+
         config.Handlers.ServiceControlMonitoringAssembly.AddAll();
 
         transportCustomization.CustomizeMonitoringEndpoint(config, transportSettings);
