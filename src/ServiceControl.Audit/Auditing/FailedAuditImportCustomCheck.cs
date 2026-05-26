90bd8c13132b33b0fc5109a7917a008ddeaf21bc
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-05-26 16:00:16 +0200

    Cosmetics on audit custom checks

--- src/ServiceControl.Audit/Auditing/FailedAuditImportCustomCheck.cs
+++ src/ServiceControl.Audit/Auditing/FailedAuditImportCustomCheck.cs
@@ -7,15 +7,8 @@ namespace ServiceControl.Audit.Auditing
     using NServiceBus.CustomChecks;
     using ServiceControl.Audit.Persistence;
 
-    class FailedAuditImportCustomCheck : CustomCheck
+    class FailedAuditImportCustomCheck(IFailedAuditStorage store, ILogger<FailedAuditImportCustomCheck> logger) : CustomCheck("Audit Message Ingestion", "ServiceControl.Audit Health", TimeSpan.FromHours(1))
     {
-        public FailedAuditImportCustomCheck(IFailedAuditStorage store, ILogger<FailedAuditImportCustomCheck> logger)
-            : base("Audit Message Ingestion", "ServiceControl.Audit Health", TimeSpan.FromHours(1))
-        {
-            this.store = store;
-            this.logger = logger;
-        }
-
         public override async Task<CheckResult> PerformCheck(CancellationToken cancellationToken = default)
         {
             var count = await store.GetFailedAuditsCount();
@@ -28,11 +21,7 @@ namespace ServiceControl.Audit.Auditing
             return CheckResult.Pass;
         }
 
-        readonly IFailedAuditStorage store;
-
         const string message = @"One or more audit messages have failed to import properly into ServiceControl.Audit and have been stored in the ServiceControl.Audit database.
 The import of these messages could have failed for a number of reasons and ServiceControl.Audit is not able to automatically reimport them. For guidance on how to resolve this see https://docs.particular.net/servicecontrol/import-failed-messages";
-
-        readonly ILogger<FailedAuditImportCustomCheck> logger;
     }
 }
