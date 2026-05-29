d09b2c4de1c14be8c36ba4c9367c0290dbea23c9
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-05-29 11:03:00 +0200

    Enhance assembly loading logic to prioritize default context for shared dependencies

--- src/ServiceControl.Infrastructure/PluginAssemblyLoadContext.cs
+++ src/ServiceControl.Infrastructure/PluginAssemblyLoadContext.cs
@@ -1,5 +1,7 @@
 ﻿namespace ServiceControl.Infrastructure;
 
+using System;
+using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
 
@@ -10,21 +12,33 @@ public class PluginAssemblyLoadContext(string assemblyPath) : AssemblyLoadContex
     protected override Assembly Load(AssemblyName assemblyName)
     {
         var assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
-
         if (assemblyPath is not null)
         {
-            // Before loading the assembly from the plugin folder, we should check if the assembly has already been loaded by the default context.
+            // Before loading the assembly from the plugin folder, we should give the default context a chance to resolve shared dependencies.
             // This is necessary because we don't have a clean separation of dependencies, so both the instances and the plugins can use the same dependencies.
             // If we let the plugin context load a separate copy, then we run into problems, where the same type in each context are considered different types.
-            // Since we ensure we are using the same version of dependencies in every project, it should be okay to use the already loaded copy.
+            // Since we ensure we are using the same version of dependencies in every project, it should be okay to use the default context copy.
             foreach (var assembly in Default.Assemblies)
             {
                 var loadedAssembly = assembly.GetName();
 
-                if (loadedAssembly.Name == assemblyName.Name && loadedAssembly.Version >= assemblyName.Version)
+                if (loadedAssembly.Name == assemblyName.Name)
                 {
-                    return null;
+                    return loadedAssembly.Version >= assemblyName.Version ? assembly : LoadFromAssemblyPath(assemblyPath);
                 }
+            }
+
+            try
+            {
+                var defaultAssembly = Default.LoadFromAssemblyName(assemblyName);
+                if (defaultAssembly.GetName().Version >= assemblyName.Version)
+                {
+                    return defaultAssembly;
+                }
+            }
+            catch (Exception exception) when (exception is FileNotFoundException or FileLoadException)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
             }
 
             return LoadFromAssemblyPath(assemblyPath);
