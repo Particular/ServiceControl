09a60ee0014e8733587f2e2cea632b16975402d4
John Simons <john.simons@particular.net>
2026-07-16 14:03:37 +1000

    Warn about incomplete EFCore persistence implementations

--- src/ServiceControl.Persistence.EFCore.PostgreSql/PostgreSqlPersistenceConfiguration.cs
+++ src/ServiceControl.Persistence.EFCore.PostgreSql/PostgreSqlPersistenceConfiguration.cs
@@ -1,11 +1,19 @@
 namespace ServiceControl.Persistence.EFCore.PostgreSql;
 
+using Microsoft.Extensions.Logging;
+using ServiceControl.Infrastructure;
 using ServiceControl.Persistence.EFCore.Abstractions;
 
 class PostgreSqlPersistenceConfiguration : EFPersistenceConfigurationBase
 {
-    public override IPersistence Create(PersistenceSettings settings) =>
-        new PostgreSqlPersistence((PostgreSqlPersisterSettings)settings);
+    public override IPersistence Create(PersistenceSettings settings)
+    {
+        // Temporary until the persister is fully implemented
+        LoggerUtil.CreateStaticLogger<PostgreSqlPersistenceConfiguration>()
+            .LogError("The PostgreSQL persistence is still under development and is not ready for use");
+
+        return new PostgreSqlPersistence((PostgreSqlPersisterSettings)settings);
+    }
 
     protected override EFPersisterSettings CreateSettings(string connectionString) =>
         new PostgreSqlPersisterSettings { ConnectionString = connectionString };
