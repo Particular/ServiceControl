528dbb4fcda3d180b395593e40f8bb1d8079cc17
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-07-27 15:31:51 +0800

    updates to address review comments

--- src/ServiceControl.Persistence.EFCore/Implementation/EndpointSettingsStore.cs
+++ src/ServiceControl.Persistence.EFCore/Implementation/EndpointSettingsStore.cs
@@ -9,32 +9,15 @@ public class EndpointSettingsStore(IServiceScopeFactory scopeFactory) : DataStor
     public IAsyncEnumerable<EndpointSettings> GetAllEndpointSettings(CancellationToken cancellationToken)
         => ExecuteWithDbContext(context => context.EndpointSettings.Select(row => new EndpointSettings { Name = row.Name, TrackInstances = row.TrackInstances }).AsAsyncEnumerable(), cancellationToken);
 
-    public Task UpdateEndpointSettings(EndpointSettings settings, CancellationToken token) => ExecuteWithDbContext(async context =>
-    {
-        var entity = await context.EndpointSettings.FindAsync([settings.Name], cancellationToken: token);
-        if (entity == null)
-        {
-            entity = new EndpointSettingsEntity() { Name = settings.Name, TrackInstances = settings.TrackInstances };
-            context.EndpointSettings.Add(entity);
-            try
-            {
-                await context.SaveChangesAsync(token);
-                return;
-            }
-            catch (DbUpdateException)
-            {
-                //this probably failed because of key conflict so try again
-            }
-
-            await context.Entry(entity).ReloadAsync(token);
-        }
-
-        entity.TrackInstances = settings.TrackInstances;
-        await context.SaveChangesAsync(token);
-    });
+    public Task UpdateEndpointSettings(EndpointSettings settings, CancellationToken token) =>
+        ExecuteWithDbContext(async context =>
+            await context.UpsertAsync([settings.Name],
+                () => new EndpointSettingsEntity() { Name = settings.Name, TrackInstances = settings.TrackInstances },
+                entity => entity.TrackInstances = settings.TrackInstances,
+                token
+            ));
 
     public Task Delete(string name, CancellationToken cancellationToken) =>
         ExecuteWithDbContext(context => context.EndpointSettings.Where(x => x.Name == name)
             .ExecuteDeleteAsync(cancellationToken));
-
 }
--- src/ServiceControl.Persistence.EFCore/Implementation/QueueAddressStore.cs
+++ src/ServiceControl.Persistence.EFCore/Implementation/QueueAddressStore.cs
@@ -23,4 +23,7 @@ public class QueueAddressStore(IServiceScopeFactory scopeFactory) : DataStoreBas
 
             return new QueryResult<IList<QueueAddress>>(items, new QueryStatsInfo("", query.Count(), false));
         });
+
+    public Task<QueryResult<IList<QueueAddress>>> GetAddressesBySearchTerm(string search, PagingInfo pagingInfo) =>
+        throw new NotImplementedException();
 }
