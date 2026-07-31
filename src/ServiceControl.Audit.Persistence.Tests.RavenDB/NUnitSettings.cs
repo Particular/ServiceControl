a3a72737f4e411bd508275998147641ae3b37533
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-07-31 11:03:11 +0800

    back out audit parallelisation

--- src/ServiceControl.Audit.Persistence.Tests.RavenDB/NUnitSettings.cs
+++
@@ -1,4 +0,0 @@
-using NUnit.Framework;
-
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-[assembly: LevelOfParallelism(4)]
--- src/TestHelper/PortUtility.cs
+++ src/TestHelper/PortUtility.cs
@@ -1,36 +1,26 @@
 ﻿namespace TestHelper
 {
-    using System;
-    using System.Net;
-    using System.Net.Sockets;
+    using System.Linq;
+    using System.Net.NetworkInformation;
 
     public static class PortUtility
     {
         public static int FindAvailablePort(int startPort)
         {
-            const int searchRange = 1024;
+            var activeTcpListeners = IPGlobalProperties
+                .GetIPGlobalProperties()
+                .GetActiveTcpListeners();
 
-            // Multiple test hosts can start embedded RavenDB instances at the same time in CI.
-            // Offset the initial probe per process to reduce cross-process collisions.
-            var processOffset = Environment.ProcessId % searchRange;
-
-            for (var attempt = 0; attempt < searchRange; attempt++)
+            for (var port = startPort; port < startPort + 1024; port++)
             {
-                var port = startPort + ((processOffset + attempt) % searchRange);
-
-                try
+                var portCopy = port;
+                if (activeTcpListeners.All(endPoint => endPoint.Port != portCopy))
                 {
-                    using var listener = new TcpListener(IPAddress.Loopback, port);
-                    listener.Start();
-                    return ((IPEndPoint)listener.LocalEndpoint).Port;
-                }
-                catch (SocketException)
-                {
-                    // Port is not currently available, try the next one.
+                    return port;
                 }
             }
 
-            return startPort + processOffset;
+            return startPort;
         }
     }
 }
