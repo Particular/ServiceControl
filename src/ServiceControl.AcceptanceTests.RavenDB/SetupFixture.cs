4fcd528c9ddcfd316176efd1063a6ca2bf62d550
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-08-05 14:22:28 +0800

    give setup loop an exit condition on failure

--- src/ServiceControl.AcceptanceTests.RavenDB/SetupFixture.cs
+++ src/ServiceControl.AcceptanceTests.RavenDB/SetupFixture.cs
@@ -1,7 +1,9 @@
 namespace ServiceControl.AcceptanceTests.RavenDB;
 
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
@@ -16,9 +18,11 @@ public class SetupFixture
             ServiceBus.Management.Infrastructure.Installers.EventSourceCreator.Create();
 
             //There is a delay for this becoming true, tests will fall over if they interleave in the wrong way.
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(60));
             while (!EventLog.SourceExists(ServiceBus.Management.Infrastructure.Installers.EventSourceCreator.SourceName))
             {
-                await Task.Delay(500);
+                cts.Token.ThrowIfCancellationRequested();
+                await Task.Delay(500, CancellationToken.None);
             }
         }
     }
