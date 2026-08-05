20f562dbba5a4621dc260e9f57544e837ee51322
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-08-05 14:01:01 +0800

    elimitate timing from startup code

--- src/ServiceControl.AcceptanceTests.RavenDB/SetupFixture.cs
+++ src/ServiceControl.AcceptanceTests.RavenDB/SetupFixture.cs
@@ -1,17 +1,19 @@
 namespace ServiceControl.AcceptanceTests.RavenDB;
 
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 [SetUpFixture]
 public class SetupFixture
 {
     [OneTimeSetUp]
-    public void Setup()
+    public async Task Setup()
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             ServiceBus.Management.Infrastructure.Installers.EventSourceCreator.Create();
+            await Task.Delay(3000);
         }
     }
 }
