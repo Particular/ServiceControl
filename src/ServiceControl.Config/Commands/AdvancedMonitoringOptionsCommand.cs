9ea0228b36f4a3fffd346e04e5be6250a3fcca7b
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2026-08-06 07:35:16 -0700

    auto refresh while adding and removing instances.

--- src/ServiceControl.Config/Commands/AdvancedMonitoringOptionsCommand.cs
+++ src/ServiceControl.Config/Commands/AdvancedMonitoringOptionsCommand.cs
@@ -2,6 +2,8 @@ namespace ServiceControl.Config.Commands
 {
     using System;
     using System.Threading.Tasks;
+    using Caliburn.Micro;
+    using Events;
     using Framework;
     using Framework.Commands;
     using ServiceControlInstaller.Engine.Instances;
@@ -10,20 +12,22 @@ namespace ServiceControl.Config.Commands
 
     class AdvancedMonitoringOptionsCommand : AwaitableAbstractCommand<InstanceDetailsViewModel>
     {
-        public AdvancedMonitoringOptionsCommand(IServiceControlWindowManager windowManager, Func<BaseService, MonitoringAdvancedViewModel> advancedOptionsModel)
+        public AdvancedMonitoringOptionsCommand(IServiceControlWindowManager windowManager, IEventAggregator eventAggregator, Func<BaseService, MonitoringAdvancedViewModel> advancedOptionsModel)
         {
             this.windowManager = windowManager;
+            this.eventAggregator = eventAggregator;
             this.advancedOptionsModel = advancedOptionsModel;
         }
 
-        public override Task ExecuteAsync(InstanceDetailsViewModel viewModel)
+        public override async Task ExecuteAsync(InstanceDetailsViewModel viewModel)
         {
             var screen = advancedOptionsModel(viewModel.MonitoringInstance);
-
-            return windowManager.ShowInnerDialog(screen);
+            await windowManager.ShowInnerDialog(screen);
+            await eventAggregator.PublishOnUIThreadAsync(new RefreshInstances());
         }
 
         readonly Func<BaseService, MonitoringAdvancedViewModel> advancedOptionsModel;
+        readonly IEventAggregator eventAggregator;
         readonly IServiceControlWindowManager windowManager;
     }
 }
