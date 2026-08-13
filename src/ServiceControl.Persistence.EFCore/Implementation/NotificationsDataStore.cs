fdde15f96030982a94c9fbe5815b33dd8beb10d0
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-08-13 11:35:56 +0800

    Use base Data Store

--- src/ServiceControl.Persistence.EFCore/Implementation/NotificationsDataStore.cs
+++ src/ServiceControl.Persistence.EFCore/Implementation/NotificationsDataStore.cs
@@ -1,25 +1,18 @@
 namespace ServiceControl.Persistence.EFCore.Implementation;
 
-using DbContexts;
 using Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Notifications;
 
-public class NotificationsDataStore(IServiceScopeFactory scopeFactory) : INotificationsDataStore
+public class NotificationsDataStore(IServiceScopeFactory scopeFactory) : DataStoreBase(scopeFactory), INotificationsDataStore
 {
-    public async Task<NotificationsSettings> LoadSettings(CancellationToken cancellationToken = default)
-    {
-        await using var scope = scopeFactory.CreateAsyncScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<ServiceControlDbContext>();
-        var email = await dbContext.GetSetting<EmailNotifications>(SettingKeys.NotificationEmails, cancellationToken);
+    public Task<NotificationsSettings> LoadSettings(CancellationToken cancellationToken = default) =>
+        ExecuteWithDbContext(async (dbContext, ct) =>
+        {
+            var email = await dbContext.GetSetting<EmailNotifications>(SettingKeys.NotificationEmails, ct);
+            return new NotificationsSettings { Email = email ?? new EmailNotifications() };
+        }, cancellationToken);
 
-        return new NotificationsSettings { Email = email ?? new EmailNotifications() };
-    }
-
-    public async Task SaveSettings(NotificationsSettings settings, CancellationToken cancellationToken = default)
-    {
-        await using var scope = scopeFactory.CreateAsyncScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<ServiceControlDbContext>();
-        await dbContext.StoreSetting(SettingKeys.NotificationEmails, settings.Email, cancellationToken);
-    }
+    public Task SaveSettings(NotificationsSettings settings, CancellationToken cancellationToken = default) =>
+        ExecuteWithDbContext((dbContext, ct) => dbContext.StoreSetting(SettingKeys.NotificationEmails, settings.Email, ct), cancellationToken);
 }
